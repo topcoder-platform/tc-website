b6de4617d3d7127de6122f4a6042fd52001ca8a6
apps <apps@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-04-03 20:24:19 +0000

    no message

--- src/main/com/topcoder/common/TCContext.java
+++ src/main/com/topcoder/common/TCContext.java
@@ -42,5 +42,15 @@ public class TCContext {
     env.put(javax.naming.Context.PROVIDER_URL, ApplicationServer.CONTEST_HOST_URL);
     return new InitialContext(env);
   }
+  
+  ////////////////////////////////////////////////////////////////////////////////
+  public static final InitialContext getMPSQASInitial() throws NamingException {
+  ////////////////////////////////////////////////////////////////////////////////
+    Hashtable env = new Hashtable();
+    env.put(javax.naming.Context.INITIAL_CONTEXT_FACTORY, ApplicationServer.JNDI_FACTORY);
+    env.put(javax.naming.Context.PROVIDER_URL, ApplicationServer.MPSQAS_HOST_URL);
+    return new InitialContext(env);
+  }
+  
 
 }
