5e62ee6b6a878267c370b9e38db05e933ccf6623
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-06-05 02:52:05 +0000

    cleaned up and added stuff for the data servlet's authentication

--- web_module/trunk/scripts/query/oltp_query_refresh.sh
+++ web_module/trunk/scripts/query/oltp_query_refresh.sh
@@ -1,4 +1,4 @@
-java QueryLoader TC_EMAIL 50 "Query_Metadata" 0 0 "
+java com.topcoder.utilities.QueryLoader TC_EMAIL 50 "Query_Metadata" 0 0 "
 SELECT i.input_id AS input_id,
 i.input_code AS input_code,
 i.input_desc AS input_desc
@@ -9,7 +9,7 @@ WHERE cq.query_id = qi.query_id
 AND qi.input_id = i.input_id
 AND cq.command_id = @ci@
 "
-java QueryLoader TC_EMAIL 51 "Email_Queries" 0 0 "
+java com.topcoder.utilities.QueryLoader TC_EMAIL 51 "Email_Queries" 0 0 "
 SELECT c.command_id AS command_id,
 c.command_desc AS command_desc
 FROM command AS c
@@ -19,14 +19,14 @@ c.command_id >
 FROM command AS c2
 WHERE c2.command_desc = 'email_queries')
 "
-java QueryLoader TC_EMAIL 52 "Recent_Jobs" 0 0 "
+java com.topcoder.utilities.QueryLoader TC_EMAIL 52 "Recent_Jobs" 0 0 "
 SELECT sched_job_id,
 start_after_date
 FROM sched_job
 WHERE start_after_date > DATETIME (@sd@) YEAR TO DAY
 ORDER BY start_after_date DESC
 "
-java QueryLoader TC_EMAIL 53 "Jobs_By_Date" 0 0 "
+java com.topcoder.utilities.QueryLoader TC_EMAIL 53 "Jobs_By_Date" 0 0 "
 SELECT sched_job_id,
 start_after_date
 FROM sched_job
@@ -34,22 +34,22 @@ WHERE start_after_date > DATETIME(@sd@) YEAR TO DAY
 AND start_after_date < DATETIME(@ed@) YEAR TO DAY
 ORDER BY start_after_date DESC
 "
-java QueryLoader TC_EMAIL 54 "Jobs_By_Sender" 0 0 "
+java com.topcoder.utilities.QueryLoader TC_EMAIL 54 "Jobs_By_Sender" 0 0 "
 SELECT sched_email_job_id
 FROM sched_email_job
 WHERE from_address='@email@'
 "
-java QueryLoader TC_EMAIL 55 "Jobs_By_Subject" 0 0 "
+java com.topcoder.utilities.QueryLoader TC_EMAIL 55 "Jobs_By_Subject" 0 0 "
 SELECT sched_email_job_id
 FROM sched_email_job
 WHERE subject LIKE '%@subject@%'
 "
-java QueryLoader TC_EMAIL 56 "Query_Description" 0 0 "
+java com.topcoder.utilities.QueryLoader TC_EMAIL 56 "Query_Description" 0 0 "
 SELECT command_desc
 FROM command
 WHERE command_id = @ci@
 "
-java QueryLoader TC_EMAIL 57 "Job_Log" 0 0 "
+java com.topcoder.utilities.QueryLoader TC_EMAIL 57 "Job_Log" 0 0 "
 SELECT sched_job_detail_status_name,
 reason,
 sched_job_detail_id
@@ -59,7 +59,7 @@ WHERE sjd.sched_job_detail_status_id = sjdslu.sched_job_detail_status_id
 AND sched_job_id = @ji@
 "
 
-java QueryLoader TC_EMAIL 58 "Notify Active" 0 0 "
+java com.topcoder.utilities.QueryLoader TC_EMAIL 58 "Notify Active" 0 0 "
 SELECT
   LOWER(u.email) AS email_address
   ,u.handle
@@ -74,7 +74,7 @@ ORDER BY
   1"
 
 
-java QueryLoader TC_EMAIL 59 "Notify Active US" 0 0 "
+java com.topcoder.utilities.QueryLoader TC_EMAIL 59 "Notify Active US" 0 0 "
 SELECT
   LOWER(u.email) AS email_address
   ,u.handle
@@ -88,4 +88,13 @@ WHERE
   AND c.country_code = '840'
 ORDER BY
   1"
+
+java com.topcoder.utilities.QueryLoader "OLTP" 60 "Authenticate_Data_User" 0 0 "
+SELECT 'yep'
+  FROM sector s
+       ,permission p
+ WHERE s.sector_desc = '@sector@'
+   AND s.sector_id = p.sector_id
+   AND p.secure_object_id = @cr@
+"
 
