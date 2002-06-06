2d6b268a99bc23a528e892fe1b9b76cf3206cf5e
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-06-06 17:50:29 +0000

    added query info for getting school membership info

--- scripts/query/oltp_query_schema_refresh.sql
+++ scripts/query/oltp_query_schema_refresh.sql
@@ -10,6 +10,7 @@ INSERT INTO command VALUES(30,"email_queries",1);
 INSERT INTO command VALUES(31,'Next Competition',1);
 INSERT INTO command VALUES(32,'Member Development or Employment Opportunities',1);
 INSERT INTO command VALUES(33,'authenticate_data_user',1);
+INSERT INTO command VALUES(34,'school_membership',1);
 INSERT INTO command VALUES(35,'Competition Results',1);
 INSERT INTO command VALUES(36,'TopCoder News and Events',1);
 
@@ -28,6 +29,7 @@ INSERT INTO command_query_xref VALUES(27,55,1);
 INSERT INTO command_query_xref VALUES(28,56,1);
 INSERT INTO command_query_xref VALUES(30,51,1);
 INSERT INTO command_query_xref VALUES(33,60,1);
+INSERT INTO command_query_xref VALUES(34,63,1);
 INSERT INTO command_query_xref VALUES(35,61,1);
 INSERT INTO command_query_xref VALUES(36,62,1);
 
@@ -40,6 +42,9 @@ INSERT INTO input_lu VALUES(19,"ji",1001,"Job ID");
 INSERT INTO input_lu VALUES(20,"email",1005,"E-mail Address");
 INSERT INTO input_lu VALUES(21,"subject",1005,"Subject");
 INSERT INTO input_lu VALUES(22,"sector",1005,"Sector Desc");
+INSERT INTO input_lu VALUES(7, "nr", 1001, "Number of records");
+INSERT INTO input_lu VALUES(8, "sr", 1001, "Start rank");
+INSERT INTO input_lu VALUES(9, "er", 1001, "End rank");
 
 INSERT INTO query_input_xref (query_id, optional, default_value, input_id, sort_order) VALUES(50,"N",null,18,1);
 INSERT INTO query_input_xref (query_id, optional, default_value, input_id, sort_order) VALUES(52,"N",null,5,1);
