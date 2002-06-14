e845ea20a423201aff8bb862c894bbab6da35c4f
grunt <grunt@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-06-14 07:24:54 +0000

    put the actual class in

--- web_module/trunk/src/main/com/topcoder/common/web/XProperties.java
+++ web_module/trunk/src/main/com/topcoder/common/web/XProperties.java
@@ -1,94 +1,161 @@
-package com.topcoder.common;
-
-import com.topcoder.web.pacts.ejb.PactsServices.PactsServicesBean;
-
-import java.util.*;
-import java.io;
-import javax.naming.*;
-import org.apache.log4j.*;
-
-
-public class TCContext {
-
-    private static Category log = Category.getInstance(TCContext.class.getName());
-
-    ////////////////////////////////////////////////////////////////////////////////
-    public static final InitialContext getInitial() throws NamingException {
-    ////////////////////////////////////////////////////////////////////////////////
-      Hashtable env = new Hashtable();
-      env.put(javax.naming.Context.INITIAL_CONTEXT_FACTORY, ApplicationServer.JNDI_FACTORY);
-      env.put(javax.naming.Context.PROVIDER_URL, ApplicationServer.HOST_URL);
-      return new InitialContext(env);
-    }
+import java.util.Properties;
+/**
+* A subclass of Properties that allows recursive
+* references for property values. For example,
+*
+* <pre><code>
+* A=12345678
+* B={A}90
+* C={B} plus more
+* </code></pre>
+*
+* will result in <code>getProperty("C")</code>
+* returning the value "1234567890 plus more".
+*
+* @author: Chris Mair
+*/
+public class XProperties extends Properties {
 
 
-
-    /**
-     * Instatiates a context specifically for the Pacts Message Queue
-     *
-     * @author   Jason N. Evans (grunt)
-     * @version  1.00, 06/14/2002
-     * @throws  java.io.FileNotFoundException If there is some problem locating the properties file
-     * @throws  java.io.IOException If there is some problem reading the properties file
-     * @throws  java.lang.Exception For any other exception
-     */
-    ////////////////////////////////////////////////////////////////////////////////
-    public static final InitialContext getPactsInitial() throws NamingException {
-    ////////////////////////////////////////////////////////////////////////////////
-
-        Hashtable env = new Hashtable();
-        try {
-          Properties prop = new XProperties();
-          java.io.InputStream in = new java.io.FileInputStream("Sample.properties");
-          prop.load(in);
-
-          env.put(javax.naming.Context.INITIAL_CONTEXT_FACTORY, prop.getProperty("JNDI_FACTORY"));
-          env.put(javax.naming.Context.PROVIDER_URL, prop.getProperty("HOST_URL"));
-        ) catch (java.io.FileNotFoundException exception1) {
-            log.error("Error locating properties file for Pacts context");
-        ) catch (java.io.IOException exception2) {
-            log.error("Error reading properties file for Pacts context");
-        } catch (java.lang.Exception exception3) {
-            log.error("Error getting context for Pacts");
-        }
-      return new InitialContext(env);
-    }
+// The prefix and suffix for constant names
+// within property values
+private static final String START_CONST = "{";
+private static final String END_CONST = "}";
 
 
-  ////////////////////////////////////////////////////////////////////////////////
-  public static final InitialContext getInitial(String url) throws NamingException {
-  ////////////////////////////////////////////////////////////////////////////////
-    Hashtable env = new Hashtable();
-    env.put(javax.naming.Context.INITIAL_CONTEXT_FACTORY, ApplicationServer.JNDI_FACTORY);
-    env.put(javax.naming.Context.PROVIDER_URL, url);
-    return new InitialContext(env);
-  }
+// The maximum depth for recursive substitution
+// of constants within property values
+// (e.g., A={B} .. B={C} .. C={D} .. etc.)
+private static final int MAX_SUBST_DEPTH = 5;
 
 
-  ////////////////////////////////////////////////////////////////////////////////
-  public static final InitialContext getLocalInitial() throws NamingException {
-  ////////////////////////////////////////////////////////////////////////////////
-    return new InitialContext();
-  }
-
-
-  ////////////////////////////////////////////////////////////////////////////////
-  public static final InitialContext getContestInitial() throws NamingException {
-  ////////////////////////////////////////////////////////////////////////////////
-    Hashtable env = new Hashtable();
-    env.put(javax.naming.Context.INITIAL_CONTEXT_FACTORY, ApplicationServer.JNDI_FACTORY);
-    env.put(javax.naming.Context.PROVIDER_URL, ApplicationServer.CONTEST_HOST_URL);
-    return new InitialContext(env);
-  }
-
-  ////////////////////////////////////////////////////////////////////////////////
-  public static final InitialContext getMPSQASInitial() throws NamingException {
-  ////////////////////////////////////////////////////////////////////////////////
-    Hashtable env = new Hashtable();
-    env.put(javax.naming.Context.INITIAL_CONTEXT_FACTORY, ApplicationServer.JNDI_FACTORY);
-    env.put(javax.naming.Context.PROVIDER_URL, ApplicationServer.MPSQAS_HOST_URL);
-    return new InitialContext(env);
-  }
-
-
+/**
+* Creates an empty property list with no default
+* values.
+*/
+public XProperties() {
+super();
 }
+
+
+/**
+* Creates an empty property list with the
+* specified defaults.
+* @param defaults java.util.Properties
+*/
+public XProperties(Properties defaults) {
+super(defaults);
+}
+
+
+/**
+* Searches for the property with the specified
+* key in this property list. If the key is not
+* found in this property list, the default
+* property list, and its defaults, recursively,
+* are then checked. The method returns
+* <code>null</code> if the property is not found.
+*
+* @param key the property key.
+* @return the value in this property list with
+* the specified key value.
+*/
+public String getProperty(String key) {
+
+
+// Return the property value starting at level 0
+return getProperty(key, 0);
+}
+
+
+/**
+* Searches for the property with the specified
+* key in this property list. If the key is not
+* found in this property list, the default
+* property list, and its defaults, recursively,
+* are then checked. The method returns
+* <code>null</code> if the property is not found.
+*
+* <p>The level parameter specifies the current
+* level of recursive constant substitution. If
+* the requested property value includes a
+* constant, its value is substituted in place
+* through a recursive call to this method,
+* incrementing the level. Once level exceeds
+* MAX_SUBST_DEPTH, no further constant
+* substitutions are performed within the
+* current requested value.
+*
+* @param key the property key.
+* @param level the level of recursion so far
+* @return the value in this property list with
+* the specified key value.
+*/
+private String getProperty(String key, int level) {
+
+
+String value = super.getProperty(key);
+if (value != null) {
+
+
+// Get the index of the first constant, if any
+int beginIndex = 0;
+int startName = value.indexOf(START_CONST, beginIndex);
+
+
+while (startName != -1) {
+if (level+1 > MAX_SUBST_DEPTH) {
+// Exceeded MAX_SUBST_DEPTH
+// Return the value as is
+return value;
+}
+
+
+int endName = value.indexOf(END_CONST, startName);
+if (endName == -1) {
+// Terminating symbol not found
+// Return the value as is
+return value;
+}
+
+
+String constName = value.substring(startName+1, endName);
+String constValue = getProperty(constName, level+1);
+
+
+if (constValue == null) {
+// Property name not found
+// Return the value as is
+return value;
+}
+
+
+// Insert the constant value into the
+// original property value
+String newValue = (startName>0)
+? value.substring(0, startName) : "";
+newValue += constValue;
+
+
+// Start checking for constants at this index
+beginIndex = newValue.length();
+
+
+// Append the remainder of the value
+newValue += value.substring(endName+1);
+
+
+value = newValue;
+
+
+// Look for the next constant
+startName = value.indexOf(START_CONST, beginIndex);
+}
+}
+
+
+// Return the value as is
+return value;
+}
+}
+
