f16ae8fb62d9458ff7ce701ad8d7d1e48109485a
stimpil <stimpil@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-06-15 15:32:05 +0000

    some changes to flow.

--- web_module/branches/tces-branch_05-17-2002/src/main/com/topcoder/web/TCES_old/bean/TCES.java
+++ web_module/branches/tces-branch_05-17-2002/src/main/com/topcoder/web/TCES_old/bean/TCES.java
@@ -68,6 +68,12 @@ public class TCES extends Task implements Serializable {
 		private CoderObject coderObject = new CoderObject();
 		public void setCoderObject(CoderObject objIn) { coderObject = objIn; }
  		public CoderObject getCoderObject() { return coderObject; }
+                private UserObject userObject = new UserObject();
+                public void setUserObject(UserObject o) { userObject = o; }
+                public UserObject getUserObject() {return userObject;}
+                private ProfileExperienceObject = new ProfileExperienceObject();
+                public void setProfileExperienceObject(ProfileExperienceObject o) {profileExperienceObject = o;}
+                public ProfileExperienceObject getProfileExperienceObject() {return profileExperienceObject;}
 
     public void process() throws TaskException {
       Log.msg(" => TCES.process()");
@@ -75,144 +81,219 @@ public class TCES extends Task implements Serializable {
 				throw new TaskException("TCES.process(): No current task");
 			}
 			try {
+                            
+                                if (currentNav.getTaskKey().equals("employment")) {
+                                    /*
+                                        ProfileExperienceBean profExpBean = new ProfileExperienceBean();
+                                        profileExperienceObject.profile_id = new Long((long)currentUser.getUserId());
+                                      */  
+                                }
+                                
 				if (currentNav.getTaskKey().equals("contact")) {
-CoderBean beanCoder = new CoderBean();
-coderObject.coder_id = new Long( (long)currentUser.getUserId() );
-coderObject = beanCoder.request(com.topcoder.web.TCES.ejb.Coder.SELECT, coderObject);
+                                        CoderBean beanCoder = new CoderBean();
+                                        coderObject.coder_id = new Long( (long)currentUser.getUserId() );
+                                        coderObject = beanCoder.request(com.topcoder.web.TCES.ejb.Coder.SELECT, coderObject);
 
-UserBean beanUser = new UserBean();
-UserObject userObject = new UserObject();
-userObject.user_id = coderObject.coder_id;
-beanUser.request(com.topcoder.web.TCES.ejb.User.SELECT, userObject);
-
-
-boolean doUpdate = false;
-
-String[] parameterValues;
-
-isTaskValidated = true;
+                                        UserBean beanUser = new UserBean();
+                                        userObject.user_id = coderObject.coder_id;
+                                        beanUser.request(com.topcoder.web.TCES.ejb.User.SELECT, userObject);
 
 
-parameterValues = (String[])htParams.get("firstName");
-if (this.isValid(parameterValues)) {
-        coderObject.first_name = parameterValues[0];
-} else {
-    //xxx todo - add error redirect
-    isTaskValidated = false;
-}
+                                        boolean doUpdate = false;
+
+                                        String[] parameterValues;
+
+                                        isTaskValidated = true;
 
 
-parameterValues = (String[])htParams.get("lastName");
-if (this.isValid(parameterValues)) {
-        coderObject.last_name = parameterValues[0];
-} else {
-    //xxx todo - add error redirect
-    isTaskValidated = false;
-}
-
-parameterValues = (String[])htParams.get("address1");
-if (this.isValid(parameterValues)) {
-        coderObject.address1 = parameterValues[0];
-} else {
-    //xxx todo - add error redirect
-    isTaskValidated = false;
-}
-
-parameterValues = (String[])htParams.get("address2");
-if (this.isValid(parameterValues)) {
-        coderObject.address2 = parameterValues[0];
-}
-
-parameterValues = (String[])htParams.get("city");
-if (this.isValid(parameterValues)) {
-        coderObject.city = parameterValues[0];
-} else {
-    //xxx todo - add error redirect
-    isTaskValidated = false;
-}
-
-parameterValues = (String[])htParams.get("state");
-if (this.isValid(parameterValues)) {
-        coderObject.state_code = parameterValues[0];
-} else {
-    //xxx todo - add error redirect
-    isTaskValidated = false;
-}
-
-parameterValues = (String[])htParams.get("zip");
-if (this.isValid(parameterValues)) {
-        coderObject.zip = parameterValues[0];
-} else {
-    //xxx todo - add error redirect
-    isTaskValidated = false;
-}
+                                        parameterValues = (String[])htParams.get("firstName");
+                                        if (this.isValid(parameterValues)) {
+                                                coderObject.first_name = parameterValues[0];
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
 
 
-parameterValues = (String[])htParams.get("country");
-if (this.isValid(parameterValues)) {
-        coderObject.country_code = parameterValues[0];
-} else {
-    //xxx todo - add error redirect
-    isTaskValidated = false;
-}
+                                        parameterValues = (String[])htParams.get("lastName");
+                                        if (this.isValid(parameterValues)) {
+                                                coderObject.last_name = parameterValues[0];
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
 
-parameterValues = (String[])htParams.get("phone");
-if (this.isValid(parameterValues)) {
-    coderObject.home_phone = parameterValues[0];
-} else {
-    //xxx todo - add error redirect
-    isTaskValidated = false;
-}
+                                        parameterValues = (String[])htParams.get("address1");
+                                        if (this.isValid(parameterValues)) {
+                                                coderObject.address1 = parameterValues[0];
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
 
-parameterValues = (String[])htParams.get("phone");
-if (this.isValid(parameterValues)) {
-    coderObject.home_phone = parameterValues[0];
-} else {
-    //xxx todo - add error redirect
-    isTaskValidated = false;
-}
+                                        parameterValues = (String[])htParams.get("address2");
+                                        if (this.isValid(parameterValues)) {
+                                                coderObject.address2 = parameterValues[0];
+                                        }
 
-parameterValues = (String[])htParams.get("handle");
-if (this.isValid(parameterValues)) {
-    userObject.handle = parameterValues[0];
-} else {
-    //xxx todo - add error redirect
-    isTaskValidated = false;
-}
+                                        parameterValues = (String[])htParams.get("city");
+                                        if (this.isValid(parameterValues)) {
+                                                coderObject.city = parameterValues[0];
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
+
+                                        parameterValues = (String[])htParams.get("state");
+                                        if (this.isValid(parameterValues)) {
+                                                coderObject.state_code = parameterValues[0];
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
+
+                                        parameterValues = (String[])htParams.get("zip");
+                                        if (this.isValid(parameterValues)) {
+
+                                            /*
+                                             boolean zipOK = true;
+                                             if (parameterValues[0].length < 5) {
+                                                 zipOK = false;
+                                             }
+                                             for (int i = 0; i < parameterValues[0].length(); i++) {
+                                                 if (Character.isDigit(parameterValues[0].charAt(i))) {
+                                                     zipOK = false;
+                                                 }
+                                             }
+                                            //*/
+                                            if (zipOK) {
+                                                coderObject.zip = parameterValues[0];
+                                            } else {
+                                                isTaskValidated = false;
+                                            }
+
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
 
 
-if (isTaskValidated) {
+                                        parameterValues = (String[])htParams.get("country");
+                                        if (this.isValid(parameterValues)) {
+                                                coderObject.country_code = parameterValues[0];
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
 
-    /*
-        coderObject.work_phone=null;
-        coderObject.middle_name=null;
-        coderObject.activation_code=null;
-        coderObject.member_since=null;
-        coderObject.notify=null;
-        coderObject.quote=null;
-        coderObject.employer_search=null;
-        coderObject.relocate=null;
-        coderObject.modify_date=null;
-        coderObject.referral_id=null;
-        coderObject.editor_id=null;
-        coderObject.notify_inquiry=null;
-        coderObject.referral_user_id=null;
-        coderObject.language_id=null;
-        coderObject.coder_type_id=null;
-        coderObject.image=null;
-        coderObject.date_of_birth=null;
-     **/
-        coderObject = beanCoder.request(com.topcoder.web.TCES.ejb.Coder.UPDATE, coderObject);
-        userObject = beanUser.request(com.topcoder.web.TCES.ejb.User.UPDATE, userObject);
 
-        isTaskValidated = true;
-}
-		/*
-					UserBean beanUser = new UserBean();
-					UserObject objUser = new UserObject();
-					objUser.user_id = new Long ((long)currentUser.getUserId());
-					objUser = beanUser.request(com.topcoder.web.TCES.ejb.User.SELECT, objUser);			
-		*/
+                                        parameterValues = (String[])htParams.get("phone");
+                                        if (this.isValid(parameterValues)) {
+                                            coderObject.home_phone = parameterValues[0];
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
+
+                                        parameterValues = (String[])htParams.get("handle");
+                                        if (this.isValid(parameterValues)) {
+                                            userObject.handle = parameterValues[0];
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
+
+                                        String password = "";
+                                        String confirmPassword = "";
+                                        parameterValues = (String[])htParams.get("password");
+                                        if (this.isValid(parameterValues)) {
+                                            password = parameterValues[0];
+                                        }
+                                        parameterValues = (String[])htParams.get("confirmPassword");
+                                        if (this.isValid(parameterValues)) {
+                                            confirmPassword = parameterValues[0];
+                                        }
+                                        if (!(confirmPassword.equals("") && password.equals(""))) {
+                                            if (confirmPassword.equals(password) && password.length() >= 7) {
+                                                userObject.password = password;
+                                            } else {
+                                                //xxx todo - add error redirect
+                                                isTaskValidated = false;
+                                            }
+                                        }
+
+
+                                        String email = "";
+                                        String confirmEmail = "";
+
+                                        parameterValues = (String[])htParams.get("email");
+                                        if (this.isValid(parameterValues)) {
+                                            email = parameterValues[0];
+                                        }
+                                        parameterValues = (String[])htParams.get("confirmEmail");
+                                        if (this.isValid(parameterValues)) {
+                                            confirmEmail = parameterValues[0];
+                                        }
+
+                                        if (email.indexOf("@") == -1 || email.indexOf(".") == -1 || !(email.equals(confirmEmail))) {
+                                            isTaskValidated = false;
+                                        } else {
+                                            userObject.email = parameterValues[0];
+                                        }
+
+                                        parameterValues = (String[])htParams.get("quote");
+                                        if (this.isValid(parameterValues)) {
+                                            coderObject.quote = parameterValues[0];
+                                        } 
+
+                                        parameterValues = (String[])htParams.get("language");
+                                        if (this.isValid(parameterValues)) {
+                                            coderObject.language = parameterValues[0];
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
+
+                                        parameterValues = (String[])htParams.get("editor");
+                                        if (this.isValid(parameterValues)) {
+                                            coderObject.editor = parameterValues[0];
+                                        } else {
+                                            //xxx todo - add error redirect
+                                            isTaskValidated = false;
+                                        }
+
+                                        if (isTaskValidated) {
+
+                                            /*
+                                                coderObject.work_phone=null;
+                                                coderObject.middle_name=null;
+                                                coderObject.activation_code=null;
+                                                coderObject.member_since=null;
+                                                coderObject.notify=null;
+                                                coderObject.quote=null;
+                                                coderObject.employer_search=null;
+                                                coderObject.relocate=null;
+                                                coderObject.modify_date=null;
+                                                coderObject.referral_id=null;
+                                                coderObject.editor_id=null;
+                                                coderObject.notify_inquiry=null;
+                                                coderObject.referral_user_id=null;
+                                                coderObject.language_id=null;
+                                                coderObject.coder_type_id=null;
+                                                coderObject.image=null;
+                                                coderObject.date_of_birth=null;
+                                             **/
+                                                coderObject = beanCoder.request(com.topcoder.web.TCES.ejb.Coder.UPDATE, coderObject);
+                                                userObject = beanUser.request(com.topcoder.web.TCES.ejb.User.UPDATE, userObject);
+
+                                                //isTaskValidated = true;
+                                        }
+                                                        /*
+                                                                                UserBean beanUser = new UserBean();
+                                                                                UserObject objUser = new UserObject();
+                                                                                objUser.user_id = new Long ((long)currentUser.getUserId());
+                                                                                objUser = beanUser.request(com.topcoder.web.TCES.ejb.User.SELECT, objUser);			
+                                                        */
 				}
 			} catch (Exception e) {
 				throw new TaskException("TCES.process(): Exception: " + e.getMessage());			
