208bd5b5a514825fd2a7d548ad089080cedad97d
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-06-24 19:01:11 +0000

    cache now takes arg

--- src/main/com/topcoder/server/distCache/CacheServerMain.java
+++ src/main/com/topcoder/server/distCache/CacheServerMain.java
@@ -9,7 +9,7 @@ public class CacheServerMain
     public static void main(String[] args) {
         String file = null;
         // log.debug("CACHE STARTING");
-        int mode;
+        int mode = CacheServer.MODE_PRIMARY;
         for (int i=0; i<args.length; i++) {
             if (args[i].equals("-primary")) {
                 mode = CacheServer.MODE_PRIMARY;
