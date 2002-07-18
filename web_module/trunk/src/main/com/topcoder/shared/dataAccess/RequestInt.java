79faccdd4687a0e1fca93179fa16f8830bb47524
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-07-18 23:58:14 +0000

    changed toString method and throw exception if setProperties goes wrong

--- web_module/trunk/src/main/com/topcoder/shared/dataAccess/RequestInt.java
+++ web_module/trunk/src/main/com/topcoder/shared/dataAccess/RequestInt.java
@@ -9,11 +9,14 @@ import java.util.Map;
  * @version $Revision$
  * @internal Log of Changes:
  *           $Log$
+ *           Revision 1.1  2002/07/03 00:30:22  gpaul
+ *           moving over here
+ *
  * @author Greg Paul
  */
 public interface RequestInt {
   public Map getProperties();
-  public void setProperties(Map map);
+  public void setProperties(Map map) throws Exception;
   public String getProperty(String sKey);
   public String getProperty(String sKey, String sDefaultValue);
   public void setProperty(String sKey, String sVal); 
