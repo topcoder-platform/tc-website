6c34867f4a68e1ede64eb23da9a6412cdc19f71b
bigjake <bigjake@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-08-15 02:45:09 +0000

    no message

---
+++ web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/web/tces/servlet/TCESCommand.java
@@ -0,0 +1,31 @@
+package com.topcoder.web.tces.servlet;
+
+import com.topcoder.shared.util.ApplicationServer;
+import com.topcoder.shared.util.TCContext;
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.shared.dataAccess.*;
+import com.topcoder.shared.dataAccess.resultSet.*;
+import com.topcoder.web.tces.common.*;
+import com.topcoder.web.tces.ejb.TCESServices.TCESServices;
+import com.topcoder.web.tces.ejb.TCESServices.TCESServicesHome;
+
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * A base interface for
+ * @author bigjake <kitz@mit.edu>
+ *
+ */
+
+public interface TCESCommand {
+
+	void processCommand(HttpServletRequest request, HttpServletResponse response)
+		throws Exception;
+
+}
+
