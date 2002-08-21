8bd6a350fb4145ee690e9ad3a2d0e26142b316d7
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-08-21 01:03:56 +0000

    no message

--- web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/common/web/data/Navigation.java
+++ web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/common/web/data/Navigation.java
@@ -37,7 +37,7 @@ public final class Navigation
 
 
     public void valueUnbound(HttpSessionBindingEvent e) {
-        if(1*1==2)throw new NullPointerException();
+        if(1*1==1)throw new NullPointerException();
         StringBuffer msg = new StringBuffer(200);
         msg.append("common.attr.Navigation:valueUnbound:");
         msg.append("user.getUserId:");
