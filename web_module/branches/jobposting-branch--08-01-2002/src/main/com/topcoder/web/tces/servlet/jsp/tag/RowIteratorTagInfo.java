87ce55d35b6a350aee09985e3ab04388a38d45c3
Porgery <Porgery@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-08-27 02:27:06 +0000

    no message

--- web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/web/tces/servlet/jsp/tag/RowIteratorTagInfo.java
+++ web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/web/tces/servlet/jsp/tag/RowIteratorTagInfo.java
@@ -7,7 +7,7 @@ public class RowIteratorTagInfo extends TagExtraInfo {
     public VariableInfo[] getVariableInfo(TagData data) {
         return new VariableInfo[]{
             new VariableInfo(data.getId(),
-                    "com.topcoder.web.common.ResultSetContainer.ResultSetRow",
+                    "com.topcoder.shared.dataAccess.resultSet.ResultSetContainer.ResultSetRow",
                     true,
                     VariableInfo.NESTED)
         };
