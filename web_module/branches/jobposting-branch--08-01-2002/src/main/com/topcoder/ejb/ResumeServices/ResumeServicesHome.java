4e901b6887bee3128b4000f46e49d161b0f38b06
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-08-29 19:52:21 +0000

    no message

--- web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/ejb/ResumeServices/ResumeServicesHome.java
+++
@@ -1,9 +0,0 @@
-package com.topcoder.ejb.ResumeServices;
-
-import javax.ejb.CreateException;
-import javax.ejb.EJBHome;
-import java.rmi.RemoteException;
-
-public interface ResumeServicesHome extends EJBHome {
-    ResumeServices create() throws CreateException, RemoteException;
-}
