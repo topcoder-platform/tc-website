5ee042fe37fe0c8cc47de62d5640048d9e656b31
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-08-30 15:47:25 +0000

    no message

--- web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/common/web/data/Navigation.java
+++ web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/common/web/data/Navigation.java
@@ -37,9 +37,6 @@ public final class Navigation
 
 
     public void valueUnbound(HttpSessionBindingEvent e) {
-        try{
-            if(1*1==1)throw new NullPointerException();
-        }catch(Exception ex){ex.printStackTrace();}
         StringBuffer msg = new StringBuffer(200);
         msg.append("common.attr.Navigation:valueUnbound:");
         msg.append("user.getUserId:");
