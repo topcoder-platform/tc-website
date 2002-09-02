409074691f512bc9d9e8c390375b32a49e7077e3
bigjake <bigjake@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-09-02 21:22:29 +0000

    placed constants in web.xml

--- web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/web/tces/common/TCESConstants.java
+++ web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/web/tces/common/TCESConstants.java
@@ -11,44 +11,44 @@ public class TCESConstants {
 
     // jsp-related
     public static String JSP_ROOT = null;
-    public static String TCES_PACKAGE = "com.topcoder.web.tces.bean";
-            // com.topcoder.web.tces.bean
+    public static String TCES_PACKAGE = null;
     public static String ERROR_PAGE = null;
-    public static String AUTH_FAILED_PAGE = "/tces?task=LoginTask";
-    public static String LOGIN_PAGE = "/login.jsp";
-    public static String LOGIN_OK_PAGE = "/tces?task=MainTask&st=v";
-    public static String MAIN_PAGE = "/main.jsp";
-    public static String MAIN_GODETAIL_PAGE = "/tces?task=CampaignDetailTask";
-    public static String CAMPAIGN_DETAIL_PAGE = "/campaign_detail.jsp";
-    public static String POSITION_INTEREST_PAGE = "/position_interest.jsp";
-    public static String CAMPAIGN_INTEREST_PAGE = "/campaign_interest.jsp";
-    public static String DEMOGRAPHIC_PAGE = "/demographic_info.jsp";
-    public static String MEMBER_PROFILE_PAGE = "/member_profile.jsp";
-    public static String CODER_DEMOGRAPHICS_PAGE = "/coder_demographics.jsp";
-    public static String COMPETITION_HISTORY_PAGE = "/competition_history.jsp";
-    public static String COMPETITION_STATISTICS_PAGE = "/competition_statistics.jsp";
-    public static String PROBLEM_SUBMISSIONS_PAGE = "/problem_submissions.jsp";
-    public static String PROBLEM_STATISTICS_PAGE = "/problem_statistics.jsp";
-    public static String PROBLEM_STATEMENT_PAGE = "/problem_statement.jsp";
-    public static String MEMBER_INTEREST_PAGE = "/member_interest.jsp";
+    public static String AUTH_FAILED_PAGE = null;
+    public static String LOGIN_PAGE = null;
+    public static String LOGIN_OK_PAGE = null;
+    public static String MAIN_PAGE = null;
+    public static String MAIN_GODETAIL_PAGE = null;
+    public static String CAMPAIGN_DETAIL_PAGE = null;
+    public static String POSITION_INTEREST_PAGE = null;
+    public static String CAMPAIGN_INTEREST_PAGE = null;
+    public static String DEMOGRAPHIC_PAGE = null;
+    public static String MEMBER_PROFILE_PAGE = null;
+    public static String CODER_DEMOGRAPHICS_PAGE = null;
+    public static String COMPETITION_HISTORY_PAGE = null;
+    public static String COMPETITION_STATISTICS_PAGE = null;
+    public static String PROBLEM_SUBMISSIONS_PAGE = null;
+    public static String PROBLEM_STATISTICS_PAGE = null;
+    public static String PROBLEM_STATEMENT_PAGE = null;
+    public static String MEMBER_INTEREST_PAGE = null;
 
-    public static String LOGIN_TASK = "LoginTask";
-    public static String LOGIN_TASK_STEP_VIEW = "v";
-    public static String LOGIN_TASK_STEP_AUTH = "a";
-    public static String MAIN_TASK = "MainTask";
-    public static String CAMPAIGN_DETAIL_TASK = "CampaignDetailTask";
-    public static String CAMPAIGN_INTEREST_TASK = "CampaignInterestTask";
-    public static String POSITION_INTEREST_TASK = "PositionInterestTask";
-    public static String DEMOGRAPHIC_TASK = "DemographicTask";
-    public static String MEMBER_PROFILE_TASK = "MemberProfileTask";
-    public static String CODER_DEMOGRAPHICS_TASK = "CoderDemographicsTask";
-    public static String COMPETITION_HISTORY_TASK = "CompetitionHistoryTask";
-    public static String COMPETITION_STATISTICS_TASK = "CompetitionStatisticsTask";
-    public static String PROBLEM_SUBMISSIONS_TASK = "ProblemSubmissionsTask";
-    public static String PROBLEM_STATISTICS_TASK = "ProblemStatisticsTask";
-    public static String PROBLEM_STATEMENT_TASK = "ProblemStatementTask";
-    public static String MEMBER_INTEREST_TASK = "MemberInterestTask";
+    public static String LOGIN_TASK = null;
+    public static String LOGIN_TASK_STEP_VIEW = null;
+    public static String LOGIN_TASK_STEP_AUTH = null;
+    public static String MAIN_TASK = null;
+    public static String CAMPAIGN_DETAIL_TASK = null;
+    public static String CAMPAIGN_INTEREST_TASK = null;
+    public static String POSITION_INTEREST_TASK = null;
+    public static String DEMOGRAPHIC_TASK = null;
+    public static String MEMBER_PROFILE_TASK = null;
+    public static String CODER_DEMOGRAPHICS_TASK = null;
+    public static String COMPETITION_HISTORY_TASK = null;
+    public static String COMPETITION_STATISTICS_TASK = null;
+    public static String PROBLEM_SUBMISSIONS_TASK = null;
+    public static String PROBLEM_STATISTICS_TASK = null;
+    public static String PROBLEM_STATEMENT_TASK = null;
+    public static String MEMBER_INTEREST_TASK = null;
 
+    // SORT_X constants are used by PositionInterestTask and CampaignInterestTask.
     public static String SORT_HANDLE = "ha";
     public static String SORT_RATING = "ra";
     public static String SORT_STATE = "st";
@@ -56,26 +56,25 @@ public class TCESConstants {
     public static String SORT_SCHOOL = "sc";
     public static String SORT_HITDATE = "hd";
     public static String SORT_COUNTRY = "country";
-    public static String SORT_POSITION = "position";
+    public static String SORT_POSITION = "p";
 
+    public static String SORT_ORDER_ASC = null;
+    public static String SORT_ORDER_DES = null;
 
-    public static String SORT_ORDER_ASC = "a";
-    public static String SORT_ORDER_DES = "d";
-
-    public static String TASK_PARAM = "task";
+    public static String TASK_PARAM = null;
     public static String STEP_PARAM = null;
-    public static String SORT_PARAM = "sort";
-    public static String SORT_ORDER_PARAM = "so";
+    public static String SORT_PARAM = null;
+    public static String SORT_ORDER_PARAM = null;
     public static String USER_ID_PARAM = null;
     public static String HANDLE_PARAM = null;
     public static String PASSWORD_PARAM = null;
-    public static String CAMPAIGN_ID_PARAM = "cid";
+    public static String CAMPAIGN_ID_PARAM = null;
     public static String JOB_ID_PARAM = null;
-    public static String MEMBER_ID_PARAM = "mid";
-    public static String ROUND_ID_PARAM = "rd";
-    public static String PROBLEM_ID_PARAM = "pm";
+    public static String MEMBER_ID_PARAM = null;
+    public static String ROUND_ID_PARAM = null;
+    public static String PROBLEM_ID_PARAM = null;
 
-    public static String MSG_ATTR_KEY = null;
+    public static String MSG_ATTR_KEY = "message";
     public static String COMPANY_NAME_ATTR_KEY = "CompanyName";
     public static String CAMPAIGN_INFO_ATTR_KEY = "CampaignInfoList";
     public static String DEMOGRAPHIC_REFERRAL_KEY = "referral";
@@ -134,27 +133,79 @@ public class TCESConstants {
                                                            "Avg Points for Submissions",
                                                            "Avg Time to Submit" };
 
-    public static int PRO_CODER_TYPE  = 2;
-    public static int STUDENT_CODER_TYPE = 1;
-    
-    public static int PROBLEM_STATUS_OPEN = 120;
-    public static int PROBLEM_STATUS_COMPILED = 121;
+    public static int PRO_CODER_TYPE;
+    public static int STUDENT_CODER_TYPE;
+
+    public static int PROBLEM_STATUS_OPEN;
+    public static int PROBLEM_STATUS_COMPILED;
 
     public static DateFormat DATE_FORMAT = new SimpleDateFormat("MM/dd/yyyy");
     public static NumberFormat NUMBER_FORMAT = new DecimalFormat("####0.00");
 
     public static void init(ServletConfig servletConfig) throws ServletException {
+
         JSP_ROOT = servletConfig.getInitParameter("jsp_root");
+        TCES_PACKAGE = servletConfig.getInitParameter("tces_package");
         ERROR_PAGE = servletConfig.getInitParameter("error_page");
 
+        AUTH_FAILED_PAGE = servletConfig.getInitParameter("auth_failed_page");
+        LOGIN_PAGE = servletConfig.getInitParameter("login_page");
+        LOGIN_OK_PAGE = servletConfig.getInitParameter("login_ok_page");
+        MAIN_PAGE = servletConfig.getInitParameter("main_page");
+        MAIN_GODETAIL_PAGE = servletConfig.getInitParameter("main_godetail_page");
+        CAMPAIGN_DETAIL_PAGE = servletConfig.getInitParameter("campaign_detail_page");
+        POSITION_INTEREST_PAGE = servletConfig.getInitParameter("position_interest_page");
+        CAMPAIGN_INTEREST_PAGE = servletConfig.getInitParameter("campaign_interest_page");
+        DEMOGRAPHIC_PAGE = servletConfig.getInitParameter("demographic_page");
+        MEMBER_PROFILE_PAGE = servletConfig.getInitParameter("member_profile_page");
+        CODER_DEMOGRAPHICS_PAGE = servletConfig.getInitParameter("coder_demographics_page");
+        COMPETITION_HISTORY_PAGE = servletConfig.getInitParameter("competition_history_page");
+        COMPETITION_STATISTICS_PAGE = servletConfig.getInitParameter("competition_statistics_page");
+        PROBLEM_SUBMISSIONS_PAGE = servletConfig.getInitParameter("problem_submissions_page");
+        PROBLEM_STATISTICS_PAGE = servletConfig.getInitParameter("problem_statistics_page");
+        PROBLEM_STATEMENT_PAGE = servletConfig.getInitParameter("problem_statement_page");
+        MEMBER_INTEREST_PAGE = servletConfig.getInitParameter("member_interest_page");
+
+        LOGIN_TASK = servletConfig.getInitParameter("login_task");
+        LOGIN_TASK_STEP_VIEW = servletConfig.getInitParameter("login_task_step_view");
+        LOGIN_TASK_STEP_AUTH = servletConfig.getInitParameter("login_task_step_auth");
+        MAIN_TASK = servletConfig.getInitParameter("main_task");
+        CAMPAIGN_DETAIL_TASK = servletConfig.getInitParameter("campaign_detail_task");
+        CAMPAIGN_INTEREST_TASK = servletConfig.getInitParameter("campaign_interest_task");
+        POSITION_INTEREST_TASK = servletConfig.getInitParameter("position_interest_task");
+        DEMOGRAPHIC_TASK = servletConfig.getInitParameter("demographic_task");
+        MEMBER_PROFILE_TASK = servletConfig.getInitParameter("member_profile_task");
+        CODER_DEMOGRAPHICS_TASK = servletConfig.getInitParameter("coder_demographics_task");
+        COMPETITION_HISTORY_TASK = servletConfig.getInitParameter("competition_history_task");
+        COMPETITION_STATISTICS_TASK = servletConfig.getInitParameter("competition_statistics_task");
+        PROBLEM_SUBMISSIONS_TASK = servletConfig.getInitParameter("problem_submissions_task");
+        PROBLEM_STATISTICS_TASK = servletConfig.getInitParameter("problem_statistics_task");
+        PROBLEM_STATEMENT_TASK = servletConfig.getInitParameter("problem_statement_task");
+        MEMBER_INTEREST_TASK = servletConfig.getInitParameter("member_interest_task");
+
+        SORT_ORDER_ASC = servletConfig.getInitParameter("sort_order_asc");
+        SORT_ORDER_DES = servletConfig.getInitParameter("sort_order_des");
+
+        TASK_PARAM = servletConfig.getInitParameter("task_param");
         STEP_PARAM = servletConfig.getInitParameter("step_param");
-        JOB_ID_PARAM = servletConfig.getInitParameter("job_id_param");
+        SORT_PARAM = servletConfig.getInitParameter("sort_param");
+        SORT_ORDER_PARAM = servletConfig.getInitParameter("sort_order_param");
         USER_ID_PARAM = servletConfig.getInitParameter("user_id_param");
         HANDLE_PARAM = servletConfig.getInitParameter("handle_param");
         PASSWORD_PARAM = servletConfig.getInitParameter("password_param");
+        CAMPAIGN_ID_PARAM = servletConfig.getInitParameter("campaign_id_param");
+        JOB_ID_PARAM = servletConfig.getInitParameter("job_id_param");
+        MEMBER_ID_PARAM = servletConfig.getInitParameter("member_id_param");
+        ROUND_ID_PARAM = servletConfig.getInitParameter("round_id_param");
+        PROBLEM_ID_PARAM = servletConfig.getInitParameter("problem_id_param");
 
-        MSG_ATTR_KEY = servletConfig.getInitParameter("msg_attr_key");
+        PRO_CODER_TYPE = getIntParameter("pro_coder_type");
+        STUDENT_CODER_TYPE = getIntParameter("student_coder_type");
+        PROBLEM_STATUS_OPEN = getIntParameter("problem_status_open");
+        PROBLEM_STATUS_COMPILED = getIntParameter("problem_status_compiled");
 
+        JSP_ROOT = servletConfig.getInitParameter("jsp_root");
+        ERROR_PAGE = servletConfig.getInitParameter("error_page");
     }
 
     private static int getIntParameter(ServletConfig servletConfig, String key)
