d037c7d7b196af510876e2d66050fa8d6268f2df
bigjake <bigjake@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-09-04 03:36:36 +0000

    no message

--- web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/web/tces/servlet/Controller.java
+++ web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/web/tces/servlet/Controller.java
@@ -3,9 +3,9 @@ package com.topcoder.web.tces.servlet;
 import com.topcoder.shared.util.TCContext;
 import com.topcoder.shared.util.logging.Logger;
 import com.topcoder.web.tces.bean.Task;
+import com.topcoder.web.tces.bean.Authentication;
 import com.topcoder.web.tces.common.TCESConstants;
 import com.topcoder.web.tces.common.TCESAuthenticationException;
-import com.topcoder.web.tces.common.Authentication;
 
 import javax.naming.InitialContext;
 import javax.servlet.ServletException;
