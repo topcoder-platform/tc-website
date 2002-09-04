c0d0323902b8066a17094d6dea800903f6d9e2c2
bigjake <bigjake@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-09-04 05:43:20 +0000

    no message

--- web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/web/tces/servlet/Controller.java
+++ web_module/branches/jobposting-branch--08-01-2002/src/main/com/topcoder/web/tces/servlet/Controller.java
@@ -98,7 +98,7 @@ log.debug(getServletContext().getRequestDispatcher(response.encodeURL(task.getNe
             }
         } catch (TCESAuthenticationException authex) {
             log.debug("User not authenticated to access TCES resource.");
-            Authentication.attemptLogin("","",request.getSession(true),"/tces?"+request.getQueryString());
+            Authentication.attemptLogin("","",ctx,request.getSession(true),"/tces?"+request.getQueryString());
             forwardToLoginPage(request, response, authex);
             return;
         } catch (ClassNotFoundException cnfex) {
