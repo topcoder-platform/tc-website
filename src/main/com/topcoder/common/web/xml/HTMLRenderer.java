426e146d207ceeb8e61ab570bb522c6a1f2d2e0c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-09-06 10:40:23 +0000

    no message

--- src/main/com/topcoder/common/web/xml/HTMLRenderer.java
+++ src/main/com/topcoder/common/web/xml/HTMLRenderer.java
@@ -35,7 +35,6 @@ public class HTMLRenderer {
             return baos.toString();
 
         } catch (Exception e) {
-            e.printStackTrace();
             throw new TCException("ejb.HTMLRenderer.HTMLRendererBean:render:ERROR:\n" + e);
         }
     }
