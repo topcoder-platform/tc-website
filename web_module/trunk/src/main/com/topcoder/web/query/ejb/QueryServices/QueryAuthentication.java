e8fefbf2ee3e87ca6441de017a2dd63de7e0f88c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-10-07 21:06:28 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/query/ejb/QueryServices/QueryAuthentication.java
+++ web_module/trunk/src/main/com/topcoder/web/query/ejb/QueryServices/QueryAuthentication.java
@@ -3,10 +3,8 @@ package com.topcoder.web.query.ejb.QueryServices;
 import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 
 import javax.ejb.EJBObject;
-import javax.ejb.EJBException;
 import java.rmi.RemoteException;
-import java.util.Date;
-import java.util.Set;
+import java.rmi.RemoteException;
 
 /**
  *
@@ -19,7 +17,7 @@ public interface QueryAuthentication extends EJBObject {
      * Gets the user_id, handle and password for the specified handle
      * @param handle
      * @return ResultSetContainer containing the user_id, handle and password
-     * @throws EJBException if there was a problem with query execution
+     * @throws RemoteException if there was a problem with query execution
      */
     public ResultSetContainer getLoginInfo(String handle) throws RemoteException;
 
