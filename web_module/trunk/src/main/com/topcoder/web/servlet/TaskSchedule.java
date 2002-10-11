96ebfd1c9f555e5a428341d60c3d4f38b403be9f
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-10-11 00:50:50 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/servlet/TaskSchedule.java
+++ web_module/trunk/src/main/com/topcoder/web/servlet/TaskSchedule.java
@@ -8,9 +8,17 @@ import com.topcoder.common.web.xml.HTMLRenderer;
 import com.topcoder.ejb.DataCache.DataCache;
 import com.topcoder.shared.docGen.xml.*;
 import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.shared.util.TCContext;
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.CachedDataAccess;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.naming.Context;
 import java.util.ArrayList;
+import java.util.Map;
 
 
 public final class TaskSchedule {
@@ -21,9 +29,29 @@ public final class TaskSchedule {
                           HTMLRenderer HTMLmaker, Navigation nav, XMLDocument document)
             throws NavigationException {
         String result = null;
+        Request dataRequest = null;
+        DataAccessInt dai = null;
+        ResultSetContainer rsc = null;
+        Map resultMap = null;
+        Context ctx = null;
+
         try {
+
             RecordTag schedTag = new RecordTag("SCHEDULE");
             DataCache dcHome = Cache.get();
+
+            String roundids = Conversion.checkNull(request.getParameter("rds"));
+            if (!roundids.equals("")) {
+                ctx = TCContext.getInitial();
+                dai = new CachedDataAccess((javax.sql.DataSource)ctx.lookup(DBMS.OLTP_DATASOURCE_NAME));
+                dataRequest = new Request();
+                dataRequest.setContentHandle("tourney_advancers");
+                dataRequest.setProperty("rds", request.getParameter(roundids.trim()));
+                resultMap = dai.getData(dataRequest);
+                rsc = (ResultSetContainer) resultMap.get("Tourney_Advancers");
+                schedTag.addTag(rsc.getTag("Advancers", "Advancer"));
+            }
+   
 
             ArrayList rounds = dcHome.getRounds();
             if (rounds != null && rounds.size() > 0) {
