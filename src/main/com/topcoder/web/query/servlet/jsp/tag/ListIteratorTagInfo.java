c1167ef94cac32193b3b9019f1bb3798366c42c8
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-10-14 14:10:47 +0000

    no message

--- src/main/com/topcoder/web/query/servlet/jsp/tag/ListIteratorTagInfo.java
+++ src/main/com/topcoder/web/query/servlet/jsp/tag/ListIteratorTagInfo.java
@@ -7,7 +7,7 @@ public class ListIteratorTagInfo extends TagExtraInfo {
     public VariableInfo[] getVariableInfo(TagData data) {
         return new VariableInfo[]{
             new VariableInfo(data.getId(),
-                    "java.util.Object",
+                    "java.lang.Object",
                     true,
                     VariableInfo.NESTED)
         };
