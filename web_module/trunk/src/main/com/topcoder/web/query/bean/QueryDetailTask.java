872755b63573c2f67e37008704ae69862247e24d
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-10-14 23:17:37 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/query/bean/QueryDetailTask.java
+++ web_module/trunk/src/main/com/topcoder/web/query/bean/QueryDetailTask.java
@@ -4,6 +4,7 @@ import com.topcoder.shared.util.logging.Logger;
 import com.topcoder.web.query.common.Constants;
 import com.topcoder.web.query.common.AuthenticationException;
 import com.topcoder.web.query.common.Authentication;
+import com.topcoder.web.query.common.Link;
 import com.topcoder.web.query.ejb.QueryServices.*;
 import com.topcoder.shared.util.ApplicationServer;
 import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
@@ -47,6 +48,28 @@ public class QueryDetailTask extends BaseTask implements Task, Serializable {
 	}
 
     public void servletPostAction(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String begin = getServletPath()+"?"+Constants.TASK_PARAM+"=";
+        StringBuffer buf = new StringBuffer();
+        buf.append(begin);
+        buf.append(Constants.DB_SELECTION_TASK);
+        super.getNavLinks().add(new Link(buf.toString(), Constants.DB_SELECTION_NAME));
+        buf.setLength(0);
+        buf.append(begin);
+        buf.append(Constants.MODIFY_COMMAND_TASK);
+        super.getNavLinks().add(new Link(buf.toString(), "New Command"));
+        buf.setLength(0);
+        buf.append(begin);
+        buf.append(Constants.MODIFY_QUERY_TASK);
+        super.getNavLinks().add(new Link(buf.toString(), "New Query"));
+        buf.setLength(0);
+        buf.append(begin);
+        buf.append(Constants.MODIFY_INPUT_TASK);
+        super.getNavLinks().add(new Link(buf.toString(), "New Input"));
+        super.getNavLinks().add(new Link("","")); //spacer
+        buf.setLength(0);
+        buf.append(begin);
+        buf.append(Constants.COMMAND_LIST_TASK);
+        super.getNavLinks().add(new Link(buf.toString(), Constants.COMMAND_LIST_NAME));
     }
 
     public void process(String step) throws Exception {
