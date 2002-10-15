9d6640a0c1908137821e6c5e28146af65444c889
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-10-15 14:31:43 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/query/bean/ModifyQueryTask.java
+++ web_module/trunk/src/main/com/topcoder/web/query/bean/ModifyQueryTask.java
@@ -112,6 +112,7 @@ public class ModifyQueryTask extends BaseTask implements Task, Serializable {
     public void setAttributes(String paramName, String paramValues[]) {
         String value = paramValues[0];
         value = (value == null?"":value.trim());
+        log.debug("setAttributes called...param: " + paramName + " value: " + value);
 
         if (paramName.equalsIgnoreCase(Constants.DB_PARAM)) {
             setDb(value);
