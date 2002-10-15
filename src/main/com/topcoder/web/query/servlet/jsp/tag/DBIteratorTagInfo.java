51a1d358b7ba72830d4d1f550ed1a3c5b4895c9e
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-10-15 01:29:32 +0000

    no message

--- src/main/com/topcoder/web/query/servlet/jsp/tag/DBIteratorTagInfo.java
+++ src/main/com/topcoder/web/query/servlet/jsp/tag/DBIteratorTagInfo.java
@@ -7,7 +7,7 @@ public class DBIteratorTagInfo extends TagExtraInfo {
     public VariableInfo[] getVariableInfo(TagData data) {
         return new VariableInfo[]{
             new VariableInfo(data.getId(),
-                    "com.topcoder.web.query.common.DataBaseBean",
+                    "com.topcoder.web.query.common.DatabaseBean",
                     true,
                     VariableInfo.NESTED)
         };
