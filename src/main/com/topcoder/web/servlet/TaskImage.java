d59d584970fa2c911527a7bf96ca86dea67ef2be
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-12-03 19:14:47 +0000

    no message

--- src/main/com/topcoder/web/servlet/TaskImage.java
+++ src/main/com/topcoder/web/servlet/TaskImage.java
@@ -14,9 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpUtils;
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.Map;
+import java.util.*;
 
 public final class TaskImage {
+
+    private static Map localCache = new HashMap();
 
     static byte[] process(HttpServletRequest request, HttpServletResponse response, Navigation nav)
             throws NavigationException {
@@ -28,7 +30,7 @@ public final class TaskImage {
                 TCContext.getInitial().lookup(DBMS.OLTP_DATASOURCE_NAME));
             Map resultMap = dai.getData(dataRequest);
             ResultSetContainer rsc = (ResultSetContainer) resultMap.get("Sponsor_Image");
-            fis = new FileInputStream(rsc.getItem(0, "file_path").toString());
+            fis = new FileInputStream(rsc.getItem(getNextIndex(dataRequest.toString(), rsc.size()), "file_path").toString());
             result = new byte[fis.available()];
             fis.read(result);
             return result;
@@ -39,7 +41,27 @@ public final class TaskImage {
         }
 
     }
+    
+    private static synchronized int getNextIndex(String key, int size) throws Exception {
+        Integer val = null;
+        int ret = 0;
 
+        if (size < 1) throw new Exception("size must be greater than 0: " + size);
+
+        val = (Integer)localCache.get(key);
+        if (val == null) {
+          val = new Integer(0);
+        }
+        
+        int iCurr = val.intValue();
+        if (iCurr < size) { 
+            iCurr++;
+        } else {
+            iCurr = 0;
+        }
+        localCache.put(key, new Integer(iCurr));
+        return iCurr;
+    }
 
 }
 
