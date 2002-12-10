80ddc6a4556cf5eb4a1d9eaedb4f243075257661
unlord <unlord@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-12-10 08:13:49 +0000

    Implemented actual code for Email EJB .

--- web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/ejb/email/Email.java
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/ejb/email/Email.java
@@ -7,12 +7,12 @@ public interface Email extends EJBObject {
 
   public long createEmail(long _user_id) throws RemoteException;
 
-  public long getEmailTypeId(long _user_id,long _email_id) throws RemoteException;
+  public long getEmailTypeId(long _email_id,long _user_id) throws RemoteException;
 
-  public String getAddress(long _user_id,long _email_id) throws RemoteException;
+  public String getAddress(long _email_id,long _user_id) throws RemoteException;
 
-  public void setEmailTypeId(long _user_id,long _email_id,long _email_type_id) throws RemoteException;
+  public void setEmailTypeId(long _email_id,long _user_id,long _email_type_id) throws RemoteException;
 
-  public void setAddress(long _user_id,long _email_id,String _address) throws RemoteException;
+  public void setAddress(long _email_id,long _user_id,String _address) throws RemoteException;
 
 };
--- web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/ejb/email/EmailBean.java
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/ejb/email/EmailBean.java
@@ -1,9 +1,18 @@
 package com.topcoder.web.ejb.email;
 
+import com.topcoder.util.idgenerator.*;
+
 import java.rmi.RemoteException;
+import java.sql.*;
 import javax.ejb.*;
+import javax.naming.*;
+import javax.sql.*;
 
 public class EmailBean implements SessionBean {
+
+  private final static String HS_APPLICATION_DS="java:comp/env/jdbc/hsAppDS";
+
+  private transient InitialContext init_ctx=null;
 
   private SessionContext ctx;
 
@@ -15,6 +24,15 @@ public class EmailBean implements SessionBean {
     /* do nothing */
   }
 
+  public void ejbCreate() throws CreateException {
+    try {
+      init_ctx=new InitialContext();
+    }
+    catch (NamingException _ne) {
+      _ne.printStackTrace();
+    }
+  }
+
   public void ejbRemove() {
     /* do nothing */
   }
@@ -24,21 +42,209 @@ public class EmailBean implements SessionBean {
   }
 
   public long createEmail(long _user_id) throws RemoteException {
-    return(0); 
+
+    long email_id=0;
+
+    try {
+
+      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
+       */
+      DataSource ds=(DataSource)init_ctx.lookup(HS_APPLICATION_DS);
+
+      if (!IdGenerator.isInitialized()) {
+
+        /* Fix this so that it uses a specific column of a persisted sequences
+         * table with maxLow=1 (this will ensure a unique id for each item of
+         * each table)
+         */
+        IdGenerator.init(ds);
+      }
+
+      /* Fix this so that it asks specifically for the next sequence number for
+       * the 'email' table
+       */
+      email_id=IdGenerator.nextId();
+
+      StringBuffer query=new StringBuffer(1024);
+      query.append("INSERT ");
+      query.append("INTO email (email_id,user_id) ");
+      query.append("VALUES (?,?)");
+
+      Connection con=ds.getConnection();
+      PreparedStatement ps=con.prepareStatement(query.toString());
+      ps.setLong(1,email_id);
+      ps.setLong(2,_user_id);
+
+      int rc=ps.executeUpdate();
+      if (rc!=1) {
+        throw(new RemoteException("Wrong number of rows inserted into "+
+                                  "'email'. Inserted "+rc+", should have "+
+                                  "inserted 1."));
+      }
+    }
+    catch (SQLException _sqle) {
+      _sqle.printStackTrace();
+      throw(new RemoteException(_sqle.getMessage()));
+    }
+    catch (NamingException _ne) {
+      _ne.printStackTrace();
+      throw(new RemoteException(_ne.getMessage()));
+    }
+    return(email_id);
   }
 
-  public long getEmailTypeId(long _user_id,long _email_id) throws RemoteException {
-    return(0);
+  public long getEmailTypeId(long _email_id,long _user_id)
+                                                        throws RemoteException {
+
+    long email_type_id=0;
+
+    try {
+
+      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
+       */
+      DataSource ds=(DataSource)init_ctx.lookup(HS_APPLICATION_DS);
+
+      StringBuffer query=new StringBuffer(1024);
+      query.append("SELECT email_type_id ");
+      query.append("FROM email ");
+      query.append("WHERE email_id=? AND user_id=?");
+
+      Connection con=ds.getConnection();
+      PreparedStatement ps=con.prepareStatement(query.toString());
+      ps.setLong(1,_email_id);
+      ps.setLong(2,_user_id);
+
+      ResultSet rs=ps.executeQuery();
+      if (rs.next()) {
+        email_type_id=rs.getLong(1);
+      }
+      else {
+        throw(new RemoteException("No rows found when selecting from 'email' "+
+                                  "with email_id="+_email_id+" and "+
+                                  "user_id="+_user_id+"."));
+      }
+    }
+    catch (SQLException _sqle) {
+      _sqle.printStackTrace();
+      throw(new RemoteException(_sqle.getMessage()));
+    }
+    catch (NamingException _ne) {
+      _ne.printStackTrace();
+      throw(new RemoteException(_ne.getMessage()));
+    }
+    return(email_type_id);
   }
 
-  public String getAddress(long _user_id,long _email_id) throws RemoteException {
-    return("");
+  public String getAddress(long _email_id,long _user_id)
+                                                        throws RemoteException {
+
+    String address="";
+
+    try {
+
+      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
+       */
+      DataSource ds=(DataSource)init_ctx.lookup(HS_APPLICATION_DS);
+
+      StringBuffer query=new StringBuffer(1024);
+      query.append("SELECT address ");
+      query.append("FROM email ");
+      query.append("WHERE email_id=? AND user_id=?");
+
+      Connection con=ds.getConnection();
+      PreparedStatement ps=con.prepareStatement(query.toString());
+      ps.setLong(1,_email_id);
+      ps.setLong(2,_user_id);
+
+      ResultSet rs=ps.executeQuery();
+      if (rs.next()) {
+        address=rs.getString(1);
+      }
+      else {
+        throw(new RemoteException("No rows found when selecting from 'email' "+
+                                  "with email_id="+_email_id+" and "+
+                                  "user_id="+_user_id+"."));
+      }
+    }
+    catch (SQLException _sqle) {
+      _sqle.printStackTrace();
+      throw(new RemoteException(_sqle.getMessage()));
+    }
+    catch (NamingException _ne) {
+      _ne.printStackTrace();
+      throw(new RemoteException(_ne.getMessage()));
+    }
+    return(address);
   }
 
-  public void setEmailTypeId(long _user_id,long _email_id,long _email_type_id) throws RemoteException {
+  public void setEmailTypeId(long _email_id,long _user_id,long _email_type_id)
+                                                        throws RemoteException {
+    try {
+
+      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
+       */
+      DataSource ds=(DataSource)init_ctx.lookup(HS_APPLICATION_DS);
+		   
+      StringBuffer query=new StringBuffer(1024);
+      query.append("UPDATE email ");
+      query.append("SET email_type_id=? ");
+      query.append("WHERE email_id=? AND user_id=?");
+
+      Connection con=ds.getConnection();
+      PreparedStatement ps=con.prepareStatement(query.toString());
+      ps.setLong(1,_email_type_id);
+      ps.setLong(2,_email_id);
+      ps.setLong(3,_user_id);
+
+      int rc=ps.executeUpdate();
+      if (rc!=1) {
+        throw(new RemoteException("Wrong number of rows updated in 'email'. "+
+                                  "Updated "+rc+", should have updated 1."));
+      }
+    }
+    catch (SQLException _sqle) {
+      _sqle.printStackTrace();
+      throw(new RemoteException(_sqle.getMessage()));
+    }
+    catch (NamingException _ne) {
+      _ne.printStackTrace();
+      throw(new RemoteException(_ne.getMessage()));
+    }
   }
 
-  public void setAddress(long _user_id,long _email_id,String _address) throws RemoteException {
+  public void setAddress(long _email_id,long _user_id,String _address)
+                                                        throws RemoteException {
+    try {
+
+      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
+       */
+      DataSource ds=(DataSource)init_ctx.lookup(HS_APPLICATION_DS);
+		   
+      StringBuffer query=new StringBuffer(1024);
+      query.append("UPDATE email ");
+      query.append("SET address=? ");
+      query.append("WHERE email_id=? AND user_id=?");
+
+      Connection con=ds.getConnection();
+      PreparedStatement ps=con.prepareStatement(query.toString());
+      ps.setString(1,_address);
+      ps.setLong(2,_email_id);
+      ps.setLong(3,_user_id);
+
+      int rc=ps.executeUpdate();
+      if (rc!=1) {
+        throw(new RemoteException("Wrong number of rows updated in 'email'. "+
+                                  "Updated "+rc+", should have updated 1."));
+      }
+    }
+    catch (SQLException _sqle) {
+      _sqle.printStackTrace();
+      throw(new RemoteException(_sqle.getMessage()));
+    }
+    catch (NamingException _ne) {
+      _ne.printStackTrace();
+      throw(new RemoteException(_ne.getMessage()));
+    }
   }
 
 };
