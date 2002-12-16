a3ac26d6100fac7ccf13ebc3680fedec3afbbd78
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-12-16 18:50:20 +0000

    moved from com.topcoder.web.hs.controller.requests

---
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/common/RequestProcessor.java
@@ -0,0 +1,29 @@
+package com.topcoder.web.common;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ * Classes that implement RequestProcessor should contain the
+ * business logic to process a request from a servlet controller.
+ *
+ * Call {@link #setRequest(javax.servlet.ServletRequest)} first,
+ * then {@link #process()} to process the request,
+ * and finally the accessor methods to see the result.
+ *
+ * @author Ambrose Feinstein
+ */
+public interface RequestProcessor {
+
+    public void setRequest(ServletRequest sr);
+
+    public void process() throws Exception;
+
+    /**
+     * if the next page is outside the context of our particular application, it will
+     * likely have to be handled differently.
+     */
+    public boolean isNextPageInContext();
+
+    public String getNextPage();
+}
