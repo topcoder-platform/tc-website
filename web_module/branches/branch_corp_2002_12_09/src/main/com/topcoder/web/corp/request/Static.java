ea25032ad65f3b958aa82c4307f1d46fca3c6f9b
swif0ne <swif0ne@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-12-17 06:20:08 +0000

    Static Processor class called added and called by: com.topcoder.web.corp.controller.MainServletStub

---
+++ web_module/branches/branch_corp_2002_12_09/src/main/com/topcoder/web/corp/request/Static.java
@@ -0,0 +1,142 @@
+package com.topcoder.web.corp.request;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.topcoder.web.common.RequestProcessor;
+
+/**
+* <p>Title: Static </p>
+* <p> Description: Handles "static" pages.  Serve up jsp's in essentially 
+* any directory (below the root), but we need some level of security.  
+* So the current plan is to create a constant STATIC_PREFIX, for this 
+* example we'll assume STATIC_PREFIX="d_".  A request might be: 
+* http://www.topcoder.com/corp/hs?d_1=statistics&d_2=tourney_overview&p=myPage
+* The request processor gets the list of parameters out of the request, 
+* validates them, and then begin to process them.  In the above example, 
+* that would mean the static processor should serve up
+* <document_root>/statistics/tourney_overview/myPage.jsp
+* @version   1.0
+* @author    Daniel Cohn
+*/
+
+public class Static extends AbstractRequestProcessor { 
+    
+    /* Static_Prefix currently "d_" for this working example */
+    private final String STATIC_PREFIX = "d_";
+
+    public String getNextPage() {
+        try { 
+            return requestProcessor(); 
+        }
+        catch (Exception e) {
+            /* TEMPORARY CODE: not sure yet how to handle illegal page 
+             * requests.  Send to error page for now.
+             */
+            return ("/errorPage.jsp?message=" + e); 
+        }
+    }
+
+    void businessProcessing() throws Exception {
+        setContentTag(getNextPage());
+    }
+
+    public boolean hasNextPageSameContext() {
+        return false;
+    }
+    /**
+     * method for processesing a page request and checking to make sure it 
+     * is valid then returning the "served up" page containing a valid URI 
+     * to display.
+     * 
+     * @return String - the actual URI of where to send the browser
+     * @throws Exception 
+     */ 
+    private String requestProcessor() throws Exception {
+
+        String staticRequest = request.getQueryString();
+        int levelsDeep = levelsDeep(staticRequest);  // see method below
+        
+        /* Check to make sure that if d_x (where x = levelsDeep) is
+         * in the request, then d_(x-1), d_(x-2), ... , d_1 must also
+         * exist in the request.  Throws IllegalArgumentException if
+         * this test fails.
+         */
+        StringBuffer paramNums = new StringBuffer(STATIC_PREFIX + 
+                                                  levelsDeep + "=");
+        for (int i=levelsDeep-1; i>0; i--) { 
+            String lookFor = STATIC_PREFIX + i + "=";
+            String requestDirectory = request.getParameter(lookFor);
+            if (requestDirectory == "" || requestDirectory == null) {
+                throw new IllegalArgumentException (
+                       "parameter(s) \"" + paramNums + 
+                       "\" found, but paramater \"" + lookFor + 
+                       "\" was not found in request: \"" + request +
+                       "\".");
+            }
+            paramNums.append(", " + lookFor);
+        }
+
+        String page = request.getParameter("p");
+        if (page == null) {
+            throw new IllegalArgumentException ("no page specified in request.");
+        }
+
+        /* start generating the return string containing the URL.    */
+        StringBuffer ret = new StringBuffer("/");
+        for (int i=1; i<=levelsDeep; i++) {
+            String cur = request.getParameter(STATIC_PREFIX+i);
+            int check = validParameter(cur);  // returns -1 if parameter IS valid.
+            if (check == -1) { 
+                ret.append(cur+"/");
+            }
+            else {
+                char invalidChar = cur.charAt(check);
+                throw new IllegalArgumentException ( 
+                    "parameter #" + i + ": \"" + cur + 
+                    "\" invalid character found: '" + invalidChar + "'.");
+            }
+        }
+        ret.append(page + ".jsp");
+        return ret.toString();
+    }
+
+    /* return the 1-based depth parameter in request                 */
+    private String getParameter(String request, String prefix) {
+        String find = prefix + "=";
+        int startIndex = request.indexOf(find) + find.length();
+        int endIndex;
+        if (prefix == "p") { 
+            endIndex = request.length(); 
+        }
+        else { 
+            endIndex = request.indexOf("&", startIndex); 
+        }
+        return request.substring(startIndex, endIndex);
+    }
+
+
+   /* returns -1 if parameter is valid, otherwise returns the index 
+    *  of the invalid character in the request.                      */
+    private int validParameter(String param) {
+        for (int i=0;i<param.length();i++) {
+            char curChar = param.charAt(i);
+            if ("_-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".indexOf(curChar) == -1)
+                return i;
+        }
+        return -1;
+    }
+
+
+    /* Determine how many directory levels the static address has, 
+     *  ie: /statistics/tourney_overview/myPage.jsp page is 2 
+     *  directory levels deep.                                       */
+    private int levelsDeep(String request) {
+        int lastIndex = request.lastIndexOf(STATIC_PREFIX) 
+                        + STATIC_PREFIX.length();
+        if (lastIndex == 1) { return -1; }
+        int parseTill = request.indexOf("=",lastIndex);
+        String deepest = request.substring(lastIndex, parseTill);
+        return Integer.parseInt(deepest);
+    }
+}
