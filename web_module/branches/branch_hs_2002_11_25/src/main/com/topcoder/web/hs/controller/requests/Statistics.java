8138c33ada5efacaeb5c20fd44e37a65fde03a79
ambrose <ambrose@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-12-17 00:17:14 +0000

    *** empty log message ***

--- web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/hs/controller/requests/Statistics.java
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/hs/controller/requests/Statistics.java
@@ -1,23 +1,12 @@
 package com.topcoder.web.hs.controller.requests;
 
-//import com.topcoder.web.hs.model.*;
-
+import java.util.*;
+import javax.servlet.*;
+import com.topcoder.web.hs.model.*;
 import com.topcoder.shared.dataAccess.*;
 import com.topcoder.shared.util.DBMS;
 import com.topcoder.shared.util.TCContext;
 import com.topcoder.common.web.util.Data;
-//import com.topcoder.common.web.data.Navigation;
-import com.topcoder.shared.util.ApplicationServer;
-//import com.topcoder.shared.util.logging.Logger;
-import com.topcoder.web.stat.bean.CoderRatingStyleBean;
-//import com.topcoder.web.stat.bean.QuickStatListBean;
-import com.topcoder.web.stat.common.StatXMLParser;
-
-import org.w3c.dom.Document;
-
-import javax.servlet.*;
-//import javax.servlet.http.*;
-import java.util.*;
 
 /**
  * A RequestProcessor which handles statistics pages.  It uses the dataAccess
@@ -28,20 +17,16 @@ import java.util.*;
  */
 public class Statistics extends Base {
 
-    private static final String XML_FILE = ApplicationServer.BASE_DIR + "/resources/stat/statServlet.xml";
-
     public void process() throws Exception {
         super.process();
 
-        //@@@ old stats code cached this...
-        Document doc = StatXMLParser.getXMLDocument(XML_FILE);
-        CoderRatingStyleBean crsb = StatXMLParser.buildCoderRatingStyleBean(doc);
-        request.setAttribute(CoderRatingStyleBean.BEAN_TOKEN, crsb);
         Data.initializeDataTypes();
+        request.setAttribute("CODER_COLORS", new CoderRatingStyleBean());
 
         Map map = gpm(request);
         Request dataRequest = new Request(map);
         request.setAttribute("REQUEST_BEAN", dataRequest);
+
         DataAccessInt dai = new CachedDataAccess((javax.sql.DataSource)TCContext.getInitial().lookup(DBMS.DW_DATASOURCE_NAME));
         Map dataMap = dai.getData(dataRequest);
         request.setAttribute("QUERY_RESPONSE", dataMap);
