376080222929e884da0da27c441f8debb5622d7c
slinky <slinky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-12-20 13:24:12 +0000

    no message

--- src/main/com/topcoder/web/ejb/user/User.java
+++ src/main/com/topcoder/web/ejb/user/User.java
@@ -12,10 +12,10 @@ import javax.ejb.EJBException;
 *
 */
 
-public interface User EJBObject {
+public interface User extends EJBObject {
 	long createUser() throws RemoteException, EJBException;
 	void setFirstName(long userId, String firstName) throws RemoteException, EJBException;
-	void setLastName(long userId, string lastName) throws RemoteException, EJBException;
+	void setLastName(long userId, String lastName) throws RemoteException, EJBException;
 	void setUserStatusId(long userId, long userStatusId) throws RemoteException, EJBException;
 	String getFirstName(long userId) throws RemoteException, EJBException;
 	String getLastName(long userId) throws RemoteException, EJBException;
