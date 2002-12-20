4e841b1dba57093d17e72e5522bc60edf45feb03
swif0ne <swif0ne@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2002-12-20 21:42:07 +0000

    put next page in context = true so pages would be forwarded rather then redirected to static pages

--- web_module/branches/branch_corp_2002_12_09/src/main/com/topcoder/web/corp/request/Static.java
+++ web_module/branches/branch_corp_2002_12_09/src/main/com/topcoder/web/corp/request/Static.java
@@ -1,56 +1,39 @@
 package com.topcoder.web.corp.request;
 
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import com.topcoder.web.common.RequestProcessor;
-
 /**
 * <p>Title: Static </p>
 * <p> Description: Handles "static" pages.  Serve up jsp's in essentially 
 * any directory (below the root), constant STATIC_PREFIX="d".  A request 
 * might look like: 
-* http://www.topcoder.com/corp/hs?d1=statistics&d2=tourney_overview&d3=myPage
+* corp/?module=Static&d1=statistics&d2=tourney_overview&d3=myPage
 * The request processor gets the list of parameters out of the request, 
 * validates them, and then begin to process them.  In the above example, 
 * that would mean the static processor should serve up
 * <document_root>/statistics/tourney_overview/myPage.jsp
-* @version   1.1
+* @version   1.2
 * @author    Daniel Cohn
 */
-
 public class Static extends BaseProcessor { 
     
-//    private String url;  // Next page to go to
     public Static() {
-        pageInContext = false; // all pages are in different context
+        pageInContext = true; // Static pages in same contect.
     }
 
     private final String STATIC_PREFIX = "d";  // Prefix for parameters
 
-//    /* Return the next page once process() has been called.  */
-//    public String getNextPage() {
-//        return url; 
-//    }
-
-    /* process() method in BaseProcessor calls method 
-     * businessProcessing()                                         */
+    /** process() method in BaseProcessor calls this businessProcessing() 
+     *  method to define the next Page.  set next page to the Error Page 
+     *  if there is an error when processing the next page request.  */
     void businessProcessing() throws Exception {
         try {
             nextPage = requestProcessor();
         }
         catch (Exception e) {
-            /* TEMPORARY CODE: not sure yet how to handle illegal page 
-             * requests.  Send to error page for now.
-             */
-            nextPage = ("/errorPage.jsp?message=" + e);
+            nextPage = ("/error.jsp?message=" + e);
         }
     }
 
-//    public boolean isNextPageInContext() {
-//        return false;
-//    }
-    /**
+    /*
      * method for processesing a page request and checking to make sure it 
      * is valid then returning the "served up" page containing a valid URI 
      * to display.
@@ -63,8 +46,8 @@ public class Static extends BaseProcessor {
         String staticRequest = request.getQueryString();
         int levelsDeep = levelsDeep(staticRequest);  // see method below
         
-        /* Check to make sure that if d_x (where x = levelsDeep) is
-         * in the request, then d_(x-1), d_(x-2), ... , d_1 must also
+        /* Check to make sure that if dx (where x = levelsDeep) is
+         * in the request, then d(x-1), d(x-2), ... , d1 must also
          * exist in the request.  Throws IllegalArgumentException if
          * this test fails.
          */
@@ -85,14 +68,15 @@ public class Static extends BaseProcessor {
 
         String page = request.getParameter(STATIC_PREFIX+levelsDeep);
         if (page == null) {
-            throw new IllegalArgumentException ("no page specified in request.");
+            throw new IllegalArgumentException 
+                   ("no page specified in request.");
         }
 
         /* start generating the return string containing the URL.    */
         StringBuffer ret = new StringBuffer("/");
         for (int i=1; i<levelsDeep; i++) {
             String cur = request.getParameter(STATIC_PREFIX+i);
-            int check = validParameter(cur);  // returns -1 if parameter IS valid.
+            int check = validParameter(cur);  // returns -1 if valid.
             if (check == -1) { 
                 ret.append(cur+"/");
             }
@@ -113,7 +97,8 @@ public class Static extends BaseProcessor {
     private int validParameter(String param) {
         for (int i=0;i<param.length();i++) {
             char curChar = param.charAt(i);
-            if ("_-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".indexOf(curChar) == -1)
+            if ( ("_-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" + 
+                  "0123456789").indexOf(curChar) == -1)
                 return i;
         }
         return -1;
