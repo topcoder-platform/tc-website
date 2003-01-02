da286fc5a51ad4cd7245e895f0e973e7a6453b3f
unlord <unlord@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-02 17:25:17 +0000

    The initial adding of the Registration RequestProcessor .

---
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/hs/controller/requests/Registration.java
@@ -0,0 +1,24 @@
+package com.topcoder.web.hs.controller.requests;
+
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.shared.util.Transaction;
+
+import java.util.*;
+import javax.servlet.*;
+
+/**
+ * A RequestProcessor which handles the registration of new users.  It contains
+ * the business logic of walking through the different pages and creating new
+ * entries in the database via EJBs.
+ *
+ * @author Nathan Egge
+ * @date 2003/1/2
+ */
+public class Registration extends Base {
+
+  public void process() throws Exception {
+
+    super.process();
+
+  }
+};
