530e896251667b923ef12d750607aa4c84ebe732
ambrose <ambrose@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-03 22:23:44 +0000

    *** empty log message ***

--- web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/common/security/BasicAuthentication.java
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/common/security/BasicAuthentication.java
@@ -53,7 +53,7 @@ public class BasicAuthentication implements WebAuthentication {
             Long uid = new Long(sub.getUserId());
 
             response.addCookie(new Cookie("user_id", uid.toString()));
-            response.addCookie(new Cookie("session_verified", "yes")));
+            response.addCookie(new Cookie("session_verified", "yes"));
 
             persistor.setObject(request.getSession().getId()+"user_id", new Long(sub.getUserId()));
             persistor.setObject(request.getSession().getId()+"session_verified", "yes");
