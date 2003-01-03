bc5bfabc8f35ab9977113558adf17f5469db170c
unlord <unlord@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-03 17:13:35 +0000

    Made Registration processor redirect to default registration page on no command

--- web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/hs/controller/requests/Registration.java
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/hs/controller/requests/Registration.java
@@ -16,9 +16,58 @@ import javax.servlet.*;
  */
 public class Registration extends Base {
 
-  public void process() throws Exception {
+  private final static String HS_REG_BASE="/hs/registration/";
 
+  private final static String REG_PAGE="registration.jsp";
+
+  private final static String REG_STUDENT_PAGE="registration_student.jsp";
+
+  private final static String REG_COACH_PAGE="registration_coach.jsp";
+
+  private final static String CONFIRM_STUDENT_PAGE="confirm_student.jsp";
+  
+  private final static String CONFIRM_COACH_PAGE="confirm_coach.jsp";
+
+  private final static String THANK_YOU_PAGE="thankyou.jsp";
+
+  private final static String REG_CMD="reg";
+
+  private final static String REG_STUDENT_CMD="reg_student";
+
+  private final static String REG_COACH_CMD="reg_coach";
+
+  private final static String CONFIRM_STUDENT_CMD="confirm_student";
+
+  private final static String CONFIRM_COACH_CMD="confirm_student";
+
+  private final static String INVALID_COMMAND="Invalid command passed to "+
+                                              "registration module: ";
+
+  public void process() throws Exception {
     super.process();
 
+    String cmd=request.getParameter("c");
+
+    /* If there is no command, redirect to the main registration page
+     */
+    if (cmd==null||cmd.equals("")) {
+      setNextPage(HS_REG_BASE+REG_PAGE);
+      setIsNextPageInContext(true);
+    }
+    else if (cmd.equals(REG_CMD)) {
+      String coder_type=request.getParameter("coderType");
+    }
+    else if (cmd.equals(REG_STUDENT_CMD)) {
+    }
+    else if (cmd.equals(REG_COACH_CMD)) {
+    }
+    else if (cmd.equals(CONFIRM_STUDENT_CMD)) {
+    }
+    else if (cmd.equals(CONFIRM_COACH_CMD)) {
+    }
+    else { 
+      throw(new IllegalArgumentException(INVALID_COMMAND+cmd));
+    }
   }
+
 };
