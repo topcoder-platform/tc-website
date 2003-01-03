005f305e44d5c6ec04e110881363dcfe8738f00b
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-03 01:33:37 +0000

    no message

--- web_module/branches/branch_screening_2002_12_16/src/main/com/topcoder/web/ejb/email/EmailBean.java
+++ web_module/branches/branch_screening_2002_12_16/src/main/com/topcoder/web/ejb/email/EmailBean.java
@@ -1,5 +1,6 @@
-package com.topcoder.web.ejb.termsofuse;
+package com.topcoder.web.ejb.email;
 
+import com.topcoder.shared.util.DBMS;
 import com.topcoder.util.idgenerator.*;
 import com.topcoder.util.idgenerator.sql.InformixDB;
 
@@ -9,9 +10,7 @@ import javax.ejb.*;
 import javax.naming.*;
 import javax.sql.*;
 
-public class TermsOfUseBean implements SessionBean {
-
-  private final static String HS_APPLICATION_DS="DBMS.OLTP_DATASOURCE_NAME";
+public class EmailBean implements SessionBean {
 
   private transient InitialContext init_ctx=null;
 
@@ -42,36 +41,38 @@ public class TermsOfUseBean implements SessionBean {
     ctx=_ctx;
   }
 
-  public long createTermsOfUse() throws RemoteException {
+  public long createEmail(long _user_id) throws RemoteException {
 
-    long terms_of_use_id=0;
+    long email_id=0;
 
     try {
+
       /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
        */
-      DataSource ds=(DataSource)init_ctx.lookup(HS_APPLICATION_DS);
+      DataSource ds=(DataSource)init_ctx.lookup(DBMS.OLTP_DATASOURCE_NAME);
 
       if (!IdGenerator.isInitialized()) {
         IdGenerator.init(new InformixDB(),ds,"sequence_object","name",
                          "current_value",9999999999L,10,true);
       }
 
-      terms_of_use_id=IdGenerator.nextId("TERMSOFUSE_SEQ");
+      email_id=IdGenerator.nextId("EMAIL_SEQ");
 
       StringBuffer query=new StringBuffer(1024);
       query.append("INSERT ");
-      query.append("INTO terms_of_use (terms_of_use_id) ");
-      query.append("VALUES (?)");
-    
+      query.append("INTO email (email_id,user_id) ");
+      query.append("VALUES (?,?)");
+
       Connection con=ds.getConnection();
       PreparedStatement ps=con.prepareStatement(query.toString());
-      ps.setLong(1,terms_of_use_id);
+      ps.setLong(1,email_id);
+      ps.setLong(2,_user_id);
 
       int rc=ps.executeUpdate();
       if (rc!=1) {
         throw(new RemoteException("Wrong number of rows inserted into "+
-                                  "'terms_of_use'. Inserted "+rc+", should "+
-                                  "have inserted 1."));
+                                  "'email'. Inserted "+rc+", should have "+
+                                  "inserted 1."));
       }
     }
     catch (SQLException _sqle) {
@@ -82,37 +83,38 @@ public class TermsOfUseBean implements SessionBean {
       _ne.printStackTrace();
       throw(new RemoteException(_ne.getMessage()));
     }
-    return(terms_of_use_id);
+    return(email_id);
   }
 
-  public long getTermsOfUseTypeId(long _terms_of_use_id)
+  public long getEmailTypeId(long _email_id,long _user_id)
                                                         throws RemoteException {
 
-    long terms_of_use_type_id=0;
+    long email_type_id=0;
 
     try {
 
       /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
        */
-      DataSource ds=(DataSource)init_ctx.lookup(HS_APPLICATION_DS);
+      DataSource ds=(DataSource)init_ctx.lookup(DBMS.OLTP_DATASOURCE_NAME);
 
       StringBuffer query=new StringBuffer(1024);
-      query.append("SELECT terms_of_use_type_id ");
-      query.append("FROM terms_of_use ");
-      query.append("WHERE terms_of_use_id=?");
+      query.append("SELECT email_type_id ");
+      query.append("FROM email ");
+      query.append("WHERE email_id=? AND user_id=?");
 
       Connection con=ds.getConnection();
       PreparedStatement ps=con.prepareStatement(query.toString());
-      ps.setLong(1,_terms_of_use_id);
+      ps.setLong(1,_email_id);
+      ps.setLong(2,_user_id);
 
       ResultSet rs=ps.executeQuery();
       if (rs.next()) {
-        terms_of_use_type_id=rs.getLong(1);
+        email_type_id=rs.getLong(1);
       }
       else {
-        throw(new RemoteException("No rows found when selecting from "+
-                                  "'terms_of_use' with terms_of_use_id="+
-                                  _terms_of_use_id+"."));
+        throw(new RemoteException("No rows found when selecting from 'email' "+
+                                  "with email_id="+_email_id+" and "+
+                                  "user_id="+_user_id+"."));
       }
     }
     catch (SQLException _sqle) {
@@ -123,72 +125,38 @@ public class TermsOfUseBean implements SessionBean {
       _ne.printStackTrace();
       throw(new RemoteException(_ne.getMessage()));
     }
-    return(terms_of_use_type_id);
+    return(email_type_id);
   }
-  
-  public void setTermsOfUseTypeId(long _terms_of_use_id,
-                                  long _terms_of_use_type_id)
+
+  public String getAddress(long _email_id,long _user_id)
                                                         throws RemoteException {
-    try {
 
-      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
-       */
-      DataSource ds=(DataSource)init_ctx.lookup(HS_APPLICATION_DS);
-
-      StringBuffer query=new StringBuffer(1024);
-      query.append("UPDATE terms_of_use ");
-      query.append("SET terms_of_use_type_id=? ");
-      query.append("WHERE terms_of_use_id=?");
-
-      Connection con=ds.getConnection();
-      PreparedStatement ps=con.prepareStatement(query.toString());
-      ps.setLong(1,_terms_of_use_type_id);
-      ps.setLong(2,_terms_of_use_id);
-
-      int rc=ps.executeUpdate();
-      if (rc!=1) {
-        throw(new RemoteException("Wrong number of rows updated in "+
-                                  "'terms_of_use'. Updated "+rc+", should "+
-                                  "have updated 1."));
-      }
-    }
-    catch (SQLException _sqle) {
-      _sqle.printStackTrace();
-      throw(new RemoteException(_sqle.getMessage()));
-    }
-    catch (NamingException _ne) {
-      _ne.printStackTrace();
-      throw(new RemoteException(_ne.getMessage()));
-    }
-  }
-
-  public String getText(long _terms_of_use_id) throws RemoteException {
-
-    String text=null;
+    String address="";
 
     try {
 
       /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
        */
-      DataSource ds=(DataSource)init_ctx.lookup(HS_APPLICATION_DS);
+      DataSource ds=(DataSource)init_ctx.lookup(DBMS.OLTP_DATASOURCE_NAME);
 
       StringBuffer query=new StringBuffer(1024);
-      query.append("SELECT text ");
-      query.append("FROM terms_of_use ");
-      query.append("WHERE terms_of_use_id=?");
+      query.append("SELECT address ");
+      query.append("FROM email ");
+      query.append("WHERE email_id=? AND user_id=?");
 
       Connection con=ds.getConnection();
       PreparedStatement ps=con.prepareStatement(query.toString());
-      ps.setLong(1,_terms_of_use_id);
+      ps.setLong(1,_email_id);
+      ps.setLong(2,_user_id);
 
       ResultSet rs=ps.executeQuery();
       if (rs.next()) {
-        text=rs.getString(1);
+        address=rs.getString(1);
       }
       else {
-        throw(new RemoteException("No rows found when selecting from "+
-                                  "'terms_of_use' with terms_of_use_id="+
-                                  _terms_of_use_id+"."));
+        throw(new RemoteException("No rows found when selecting from 'email' "+
+                                  "with email_id="+_email_id+" and "+
+                                  "user_id="+_user_id+"."));
       }
     }
     catch (SQLException _sqle) {
@@ -199,32 +167,67 @@ public class TermsOfUseBean implements SessionBean {
       _ne.printStackTrace();
       throw(new RemoteException(_ne.getMessage()));
     }
-    return(text);
+    return(address);
   }
 
-  public void setText(long _terms_of_use_id,String _text)
+  public void setEmailTypeId(long _email_id,long _user_id,long _email_type_id)
                                                         throws RemoteException {
     try {
 
       /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
        */
-      DataSource ds=(DataSource)init_ctx.lookup(HS_APPLICATION_DS);
-
+      DataSource ds=(DataSource)init_ctx.lookup(DBMS.OLTP_DATASOURCE_NAME);
+		   
       StringBuffer query=new StringBuffer(1024);
-      query.append("UPDATE terms_of_use ");
-      query.append("SET text=? ");
-      query.append("WHERE terms_of_use_id=?");
+      query.append("UPDATE email ");
+      query.append("SET email_type_id=? ");
+      query.append("WHERE email_id=? AND user_id=?");
 
       Connection con=ds.getConnection();
       PreparedStatement ps=con.prepareStatement(query.toString());
-      ps.setString(1,_text);
-      ps.setLong(2,_terms_of_use_id);
+      ps.setLong(1,_email_type_id);
+      ps.setLong(2,_email_id);
+      ps.setLong(3,_user_id);
 
       int rc=ps.executeUpdate();
       if (rc!=1) {
-        throw(new RemoteException("Wrong number of rows updated in "+
-                                  "'terms_of_use'. Updated "+rc+", should "+
-                                  "have updated 1."));
+        throw(new RemoteException("Wrong number of rows updated in 'email'. "+
+                                  "Updated "+rc+", should have updated 1."));
+      }
+    }
+    catch (SQLException _sqle) {
+      _sqle.printStackTrace();
+      throw(new RemoteException(_sqle.getMessage()));
+    }
+    catch (NamingException _ne) {
+      _ne.printStackTrace();
+      throw(new RemoteException(_ne.getMessage()));
+    }
+  }
+
+  public void setAddress(long _email_id,long _user_id,String _address)
+                                                        throws RemoteException {
+    try {
+
+      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
+       */
+      DataSource ds=(DataSource)init_ctx.lookup(DBMS.OLTP_DATASOURCE_NAME);
+		   
+      StringBuffer query=new StringBuffer(1024);
+      query.append("UPDATE email ");
+      query.append("SET address=? ");
+      query.append("WHERE email_id=? AND user_id=?");
+
+      Connection con=ds.getConnection();
+      PreparedStatement ps=con.prepareStatement(query.toString());
+      ps.setString(1,_address);
+      ps.setLong(2,_email_id);
+      ps.setLong(3,_user_id);
+
+      int rc=ps.executeUpdate();
+      if (rc!=1) {
+        throw(new RemoteException("Wrong number of rows updated in 'email'. "+
+                                  "Updated "+rc+", should have updated 1."));
       }
     }
     catch (SQLException _sqle) {
