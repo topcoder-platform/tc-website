843757773de2650de7b75571b32697ad0415317b
unlord <unlord@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-05 02:28:48 +0000

    Fixed TermsOfUseBean to throw EJBExceptions and to have a finally block that handles clean up

--- web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/ejb/termsofuse/TermsOfUseBean.java
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/ejb/termsofuse/TermsOfUseBean.java
@@ -41,13 +41,14 @@ public class TermsOfUseBean implements SessionBean {
     ctx=_ctx;
   }
 
-  public long createTermsOfUse() throws RemoteException {
+  public long createTermsOfUse() throws EJBException, RemoteException {
 
     long terms_of_use_id=0;
 
+    Connection con=null;
+    PreparedStatement ps=null;
+
     try {
-      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
-       */
       DataSource ds=(DataSource)init_ctx.lookup(DBMS.OLTP_DATASOURCE_NAME);
 
       if (!IdGenerator.isInitialized()) {
@@ -62,37 +63,56 @@ public class TermsOfUseBean implements SessionBean {
       query.append("INTO terms_of_use (terms_of_use_id) ");
       query.append("VALUES (?)");
     
-      Connection con=ds.getConnection();
-      PreparedStatement ps=con.prepareStatement(query.toString());
+      con=ds.getConnection();
+      ps=con.prepareStatement(query.toString());
       ps.setLong(1,terms_of_use_id);
 
       int rc=ps.executeUpdate();
       if (rc!=1) {
-        throw(new RemoteException("Wrong number of rows inserted into "+
-                                  "'terms_of_use'. Inserted "+rc+", should "+
-                                  "have inserted 1."));
+        throw(new EJBException("Wrong number of rows inserted into "+
+                               "'terms_of_use'. Inserted "+rc+", should "+
+                               "have inserted 1."));
       }
     }
     catch (SQLException _sqle) {
       _sqle.printStackTrace();
-      throw(new RemoteException(_sqle.getMessage()));
+      throw(new EJBException(_sqle.getMessage()));
     }
     catch (NamingException _ne) {
       _ne.printStackTrace();
-      throw(new RemoteException(_ne.getMessage()));
+      throw(new EJBException(_ne.getMessage()));
+    }
+    finally {
+      if (con!=null) {
+        try {
+          con.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+      if (ps!=null) {
+        try {
+          ps.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
     }
     return(terms_of_use_id);
   }
 
   public long getTermsOfUseTypeId(long _terms_of_use_id)
-                                                        throws RemoteException {
+                                          throws EJBException, RemoteException {
 
     long terms_of_use_type_id=0;
 
+    Connection con=null;
+    PreparedStatement ps=null;
+
     try {
 
-      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
-       */
       DataSource ds=(DataSource)init_ctx.lookup(DBMS.OLTP_DATASOURCE_NAME);
 
       StringBuffer query=new StringBuffer(1024);
@@ -100,8 +120,8 @@ public class TermsOfUseBean implements SessionBean {
       query.append("FROM terms_of_use ");
       query.append("WHERE terms_of_use_id=?");
 
-      Connection con=ds.getConnection();
-      PreparedStatement ps=con.prepareStatement(query.toString());
+      con=ds.getConnection();
+      ps=con.prepareStatement(query.toString());
       ps.setLong(1,_terms_of_use_id);
 
       ResultSet rs=ps.executeQuery();
@@ -109,29 +129,49 @@ public class TermsOfUseBean implements SessionBean {
         terms_of_use_type_id=rs.getLong(1);
       }
       else {
-        throw(new RemoteException("No rows found when selecting from "+
-                                  "'terms_of_use' with terms_of_use_id="+
-                                  _terms_of_use_id+"."));
+        throw(new EJBException("No rows found when selecting from "+
+                               "'terms_of_use' with terms_of_use_id="+
+                               _terms_of_use_id+"."));
       }
     }
     catch (SQLException _sqle) {
       _sqle.printStackTrace();
-      throw(new RemoteException(_sqle.getMessage()));
+      throw(new EJBException(_sqle.getMessage()));
     }
     catch (NamingException _ne) {
       _ne.printStackTrace();
-      throw(new RemoteException(_ne.getMessage()));
+      throw(new EJBException(_ne.getMessage()));
+    }
+    finally {
+      if (con!=null) {
+        try {
+          con.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+      if (ps!=null) {
+        try {
+          ps.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
     }
     return(terms_of_use_type_id);
   }
   
   public void setTermsOfUseTypeId(long _terms_of_use_id,
                                   long _terms_of_use_type_id)
-                                                        throws RemoteException {
+                                          throws EJBException, RemoteException {
+
+    Connection con=null;
+    PreparedStatement ps=null;
+
     try {
 
-      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
-       */
       DataSource ds=(DataSource)init_ctx.lookup(DBMS.OLTP_DATASOURCE_NAME);
 
       StringBuffer query=new StringBuffer(1024);
@@ -139,36 +179,56 @@ public class TermsOfUseBean implements SessionBean {
       query.append("SET terms_of_use_type_id=? ");
       query.append("WHERE terms_of_use_id=?");
 
-      Connection con=ds.getConnection();
-      PreparedStatement ps=con.prepareStatement(query.toString());
+      con=ds.getConnection();
+      ps=con.prepareStatement(query.toString());
       ps.setLong(1,_terms_of_use_type_id);
       ps.setLong(2,_terms_of_use_id);
 
       int rc=ps.executeUpdate();
       if (rc!=1) {
-        throw(new RemoteException("Wrong number of rows updated in "+
-                                  "'terms_of_use'. Updated "+rc+", should "+
-                                  "have updated 1."));
+        throw(new EJBException("Wrong number of rows updated in "+
+                               "'terms_of_use'. Updated "+rc+", should "+
+                               "have updated 1."));
       }
     }
     catch (SQLException _sqle) {
       _sqle.printStackTrace();
-      throw(new RemoteException(_sqle.getMessage()));
+      throw(new EJBException(_sqle.getMessage()));
     }
     catch (NamingException _ne) {
       _ne.printStackTrace();
-      throw(new RemoteException(_ne.getMessage()));
+      throw(new EJBException(_ne.getMessage()));
+    }
+    finally {
+      if (con!=null) {
+        try {
+          con.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+      if (ps!=null) {
+        try {
+          ps.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
     }
   }
 
-  public String getText(long _terms_of_use_id) throws RemoteException {
+  public String getText(long _terms_of_use_id)
+                                          throws EJBException, RemoteException {
 
     String text=null;
 
+    Connection con=null;
+    PreparedStatement ps=null;
+
     try {
 
-      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
-       */
       DataSource ds=(DataSource)init_ctx.lookup(DBMS.OLTP_DATASOURCE_NAME);
 
       StringBuffer query=new StringBuffer(1024);
@@ -176,8 +236,8 @@ public class TermsOfUseBean implements SessionBean {
       query.append("FROM terms_of_use ");
       query.append("WHERE terms_of_use_id=?");
 
-      Connection con=ds.getConnection();
-      PreparedStatement ps=con.prepareStatement(query.toString());
+      con=ds.getConnection();
+      ps=con.prepareStatement(query.toString());
       ps.setLong(1,_terms_of_use_id);
 
       ResultSet rs=ps.executeQuery();
@@ -185,28 +245,48 @@ public class TermsOfUseBean implements SessionBean {
         text=rs.getString(1);
       }
       else {
-        throw(new RemoteException("No rows found when selecting from "+
-                                  "'terms_of_use' with terms_of_use_id="+
-                                  _terms_of_use_id+"."));
+        throw(new EJBException("No rows found when selecting from "+
+                               "'terms_of_use' with terms_of_use_id="+
+                               _terms_of_use_id+"."));
       }
     }
     catch (SQLException _sqle) {
       _sqle.printStackTrace();
-      throw(new RemoteException(_sqle.getMessage()));
+      throw(new EJBException(_sqle.getMessage()));
     }
     catch (NamingException _ne) {
       _ne.printStackTrace();
-      throw(new RemoteException(_ne.getMessage()));
+      throw(new EJBException(_ne.getMessage()));
+    }
+    finally {
+      if (con!=null) {
+        try {
+          con.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+      if (ps!=null) {
+        try {
+          ps.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
     }
     return(text);
   }
 
   public void setText(long _terms_of_use_id,String _text)
-                                                        throws RemoteException {
+                                          throws EJBException, RemoteException {
+
+    Connection con=null;
+    PreparedStatement ps=null;
+
     try {
 
-      /* Pull the DataSource object defined as a <resource-ref> in ejb-jar.xml
-       */
       DataSource ds=(DataSource)init_ctx.lookup(DBMS.OLTP_DATASOURCE_NAME);
 
       StringBuffer query=new StringBuffer(1024);
@@ -214,25 +294,43 @@ public class TermsOfUseBean implements SessionBean {
       query.append("SET text=? ");
       query.append("WHERE terms_of_use_id=?");
 
-      Connection con=ds.getConnection();
-      PreparedStatement ps=con.prepareStatement(query.toString());
+      con=ds.getConnection();
+      ps=con.prepareStatement(query.toString());
       ps.setString(1,_text);
       ps.setLong(2,_terms_of_use_id);
 
       int rc=ps.executeUpdate();
       if (rc!=1) {
-        throw(new RemoteException("Wrong number of rows updated in "+
-                                  "'terms_of_use'. Updated "+rc+", should "+
-                                  "have updated 1."));
+        throw(new EJBException("Wrong number of rows updated in "+
+                               "'terms_of_use'. Updated "+rc+", should "+
+                               "have updated 1."));
       }
     }
     catch (SQLException _sqle) {
       _sqle.printStackTrace();
-      throw(new RemoteException(_sqle.getMessage()));
+      throw(new EJBException(_sqle.getMessage()));
     }
     catch (NamingException _ne) {
       _ne.printStackTrace();
-      throw(new RemoteException(_ne.getMessage()));
+      throw(new EJBException(_ne.getMessage()));
+    }
+    finally {
+      if (con!=null) {
+        try {
+          con.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+      if (ps!=null) {
+        try {
+          ps.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
     }
   }
 
