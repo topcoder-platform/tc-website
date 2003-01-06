e3a141a2f01e4482ff48aed10f5046051f441b30
ambrose <ambrose@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-06 04:55:26 +0000

    *** empty log message ***

--- web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/common/security/Constants.java
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/common/security/Constants.java
@@ -7,21 +7,23 @@ import javax.naming.*;
 public class Constants {
 
     /**
-     * Get a remote instance of the named EJB from the context hardcoded below.
-     * We create the context anew each time in case the JNDI provider is restarted.
+     * Get a remote instance of the specified EJB from the context hardcoded below.
+     * Assumes the home class will have the same name plus "Home".
+     *
      * Package scope is deliberate.
      */
     static Object createEJB(Class remoteclass) throws Exception {
 
+       /* create the context anew each time in case the JNDI provider is restarted. */
         Hashtable env = new Hashtable();
         env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
         env.put(Context.PROVIDER_URL, "172.16.20.40:1099");
         InitialContext ctx = new InitialContext(env);
 
         Class remotehomeclass = Class.forName(remoteclass.getName()+"Home");
-        String refname = (String)remotehomeclass.getField("EJB_REF_NAME").get(Null);
+        String refname = (String)remotehomeclass.getField("EJB_REF_NAME").get(null);
         Object remotehome = ctx.lookup(refname);
         Method createmethod = remotehome.getClass().getMethod("create", null);
-        return createmethod.invoke(rh, null);
+        return createmethod.invoke(remotehome, null);
     }
 }
