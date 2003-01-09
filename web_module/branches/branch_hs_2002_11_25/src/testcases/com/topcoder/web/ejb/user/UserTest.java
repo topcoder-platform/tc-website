e30e9e4324002e42eebfead3b71c96fcbe487d79
veredox <veredox@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-09 05:26:13 +0000

    no message

--- web_module/branches/branch_hs_2002_11_25/src/testcases/com/topcoder/web/ejb/user/UserTest.java
+++ web_module/branches/branch_hs_2002_11_25/src/testcases/com/topcoder/web/ejb/user/UserTest.java
@@ -42,21 +42,115 @@ public class UserTest extends EJBTestCase {
 
 
     ////////////////////////////////////////////////////////////////////////////
-    public void testCreateUser() {}
+    public void testCreateUser() throws Exception {
     ////////////////////////////////////////////////////////////////////////////
+
+        User u = getHandle();
+
+        long id = u.createUser();
+
+        assertTrue(id >= 0);
+        assertTrue(null == u.getFirstName());
+        assertTrue(null == u.getLastName());
+        assertEquals(0, u.getUserStatusId());
+    }
 
 
     ////////////////////////////////////////////////////////////////////////////
-    public void testFirstName() {}
+    public void testFirstName() {
     ////////////////////////////////////////////////////////////////////////////
+
+        User u = getHandle();
+
+        long id = u.createUser();
+
+        String name = "testUser";
+
+        u.setFirstName(id, name);
+        assertEquals(name, u.getFirstName(id));
+
+        try {
+            u.setFirstName(id, null);
+            fail("No exception thrown when set to null");
+        } catch (Exception e) {
+            if (e instanceof RuntimeException) {
+                fail (e.toString());
+            }
+        }
+
+        try {
+            u.setFirstName(-1, name);
+            fail("No exception thrown when id < 0");
+        } catch (Exception e) {
+            if (e instanceof RuntimeException) {
+                fail (e.toString());
+            }
+        }
+    }
 
 
     ////////////////////////////////////////////////////////////////////////////
-    public void testLastName() {}
+    public void testLastName() {
     ////////////////////////////////////////////////////////////////////////////
+
+        User u = getHandle();
+
+        long id = u.createUser();
+
+        String name = "testUser";
+
+        u.setLastName(id, name);
+        assertEquals(name, u.getLastName(id));
+
+        try {
+            u.setLastName(id, null);
+            fail("No exception thrown when set to null");
+        } catch (Exception e) {
+            if (e instanceof RuntimeException) {
+                fail (e.toString());
+            }
+        }
+
+        try {
+            u.setLastName(-1, name);
+            fail("No exception thrown when id < 0");
+        } catch (Exception e) {
+            if (e instanceof RuntimeException) {
+                fail (e.toString());
+            }
+        }
+    }
 
 
     ////////////////////////////////////////////////////////////////////////////
-    public void testUserStatusId() {}
+    public void testUserStatusId() {
     ////////////////////////////////////////////////////////////////////////////
+
+        User u = getHandle();
+
+        long id = u.createUser();
+
+        long status = 1;
+
+        u.setUserStatusId(id, status);
+        assertEquals(status, u.getUserStatusId(id));
+
+        try {
+            u.setUserStatusId(id, -1);
+            fail("No exception thrown when status < 0");
+        } catch (Exception e) {
+            if (e instanceof RuntimeException) {
+                fail (e.toString());
+            }
+        }
+
+        try {
+            u.setLastName(-1, status);
+            fail("No exception thrown when id < 0");
+        } catch (Exception e) {
+            if (e instanceof RuntimeException) {
+                fail (e.toString());
+            }
+        }
+    }
 }
