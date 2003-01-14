2b9bf1db8c9c87f7cced931bf6db384e00bc7e57
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-14 01:49:13 +0000

    no message

--- src/main/com/topcoder/web/ejb/password/PasswordLocalHome.java
+++ src/main/com/topcoder/web/ejb/password/PasswordLocalHome.java
@@ -5,7 +5,7 @@ import javax.ejb.CreateException;
 
 public interface PasswordLocalHome extends javax.ejb.EJBLocalHome {
 
-    public static final String EJB_REF_NAME="java:comp/env/ejb/com/topcoder/web/ejb/password/PasswordEJB";
+    public static final String EJB_REF_NAME="java:comp/env/ejb/web/ejb/password/PasswordEJB";
 
     public String encodePassword(String password) throws EJBException, CreateException;
 
