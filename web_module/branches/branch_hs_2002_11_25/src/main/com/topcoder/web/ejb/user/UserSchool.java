c2bd5093f28c93e64da04af50260fc4a3f77936c
unlord <unlord@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-16 17:43:27 +0000

    Added method getCurrentUserSchoolId

--- web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/ejb/user/UserSchool.java
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/ejb/user/UserSchool.java
@@ -9,13 +9,16 @@ public interface UserSchool extends EJBObject {
   public void createUserSchool(long _user_id,long _school_id)
                                            throws EJBException, RemoteException;
 
-  public boolean isCurrent(long _user_id,long _school_id)
-                                           throws EJBException, RemoteException;
-
-  public void setCurrent(long _user_id,long _school_id,boolean _current)
-                                           throws EJBException, RemoteException;
-
   public void removeUserSchool(long _user_id,long _school_id)
+                                           throws EJBException, RemoteException;
+
+  public void setCurrentUserSchoolId(long _user_id,long _school_id)
+                                           throws EJBException, RemoteException;
+
+  public long getCurrentUserSchoolId(long _user_id)
+                                           throws EJBException, RemoteException;
+
+  public boolean isCurrentUserSchoolId(long _user_id,long _school_id)
                                            throws EJBException, RemoteException;
 
 };
--- web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/ejb/user/UserSchoolBean.java
+++ web_module/branches/branch_hs_2002_11_25/src/main/com/topcoder/web/ejb/user/UserSchoolBean.java
@@ -95,10 +95,196 @@ public class UserSchoolBean implements SessionBean {
         }
       }
     }
-    
   }
 
-  public boolean isCurrent(long _user_id,long _school_id)
+  public void removeUserSchool(long _user_id,long _school_id)
+                                          throws EJBException, RemoteException {
+
+    Connection con=null;
+    PreparedStatement ps=null;
+
+    try {
+
+      String ds_name=(String)init_ctx.lookup(DATA_SOURCE);
+      DataSource ds=(DataSource)init_ctx.lookup(ds_name);
+
+      StringBuffer query=new StringBuffer(1024);
+      query.append("DELETE ");
+      query.append("FROM user_school_xref ");
+      query.append("WHERE user_id=? AND school_id=?");
+
+      con=ds.getConnection();
+      ps=con.prepareStatement(query.toString());
+      ps.setLong(1,_user_id);
+      ps.setLong(2,_school_id);
+
+      int rc=ps.executeUpdate();
+      if (rc!=1) {
+        throw(new EJBException("Wrong number of rows deleted from "+
+                               "'user_school_xref'. Deleted "+rc+", should "+
+                               "have deleted 1."));
+      }
+    }
+    catch (SQLException _sqle) {
+      _sqle.printStackTrace();
+      throw(new EJBException(_sqle.getMessage()));
+    }
+    catch (NamingException _ne) {
+      _ne.printStackTrace();
+      throw(new EJBException(_ne.getMessage()));
+    }
+    finally {
+      if (con!=null) {
+        try {
+          con.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+      if (ps!=null) {
+        try {
+          ps.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+    }
+  }
+
+  public void setCurrentUserSchoolId(long _user_id,long _school_id)
+                                          throws EJBException, RemoteException {
+
+    Connection con=null;
+    PreparedStatement ps=null;
+
+    try {
+
+      String ds_name=(String)init_ctx.lookup(DATA_SOURCE);
+      DataSource ds=(DataSource)init_ctx.lookup(ds_name);
+
+      StringBuffer query=new StringBuffer(1024);
+      query.append("UPDATE user_school_xref ");
+      query.append("SET current_ind=0 ");
+      query.append("WHERE user_id=?");
+
+      con=ds.getConnection();
+      ps=con.prepareStatement(query.toString());
+      ps.setLong(1,_user_id);
+
+      int rc=ps.executeUpdate();
+      if (rc<1) {
+        throw(new EJBException("Wrong number of rows updated in "+
+                               "'user_school_xref'. Updated "+rc+", should "+
+                               "have updated at least 1."));
+      }
+
+      query=new StringBuffer(1024);
+      query.append("UPDATE user_school_xref ");
+      query.append("SET current_ind=1 ");
+      query.append("WHERE user_id=? AND school_id=?");
+
+      ps=con.prepareStatement(query.toString());
+      ps.setLong(1,_user_id);
+      ps.setLong(2,_school_id);
+
+      rc=ps.executeUpdate();
+      if (rc!=1) {
+        throw(new EJBException("Wrong number of rows updated in "+
+                               "'user_school_xref'. Updated "+rc+", should "+
+                               "have updated 1."));
+      }
+    }
+    catch (SQLException _sqle) {
+      _sqle.printStackTrace();
+      throw(new EJBException(_sqle.getMessage()));
+    }
+    catch (NamingException _ne) {
+      _ne.printStackTrace();
+      throw(new EJBException(_ne.getMessage()));
+    }
+    finally {
+      if (con!=null) {
+        try {
+          con.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+      if (ps!=null) {
+        try {
+          ps.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+    }
+  }
+
+  public long getCurrentUserSchoolId(long _user_id)
+                                          throws EJBException, RemoteException {
+
+    long school_id=0;
+
+    Connection con=null;
+    PreparedStatement ps=null;
+
+    try {
+
+      String ds_name=(String)init_ctx.lookup(DATA_SOURCE);
+      DataSource ds=(DataSource)init_ctx.lookup(ds_name);
+
+      StringBuffer query=new StringBuffer(1024);
+      query.append("SELECT school_id ");
+      query.append("FROM user_school_xref ");
+      query.append("WHERE user_id=? AND current_ind=1");
+
+      con=ds.getConnection();
+      ps=con.prepareStatement(query.toString());
+      ps.setLong(1,_user_id);
+
+      ResultSet rs=ps.executeQuery();
+      if (rs.next()) {
+        school_id=rs.getLong(1);
+      }
+      else {
+        throw(new EJBException("No rows found when selecting from "+
+                               "'user_school_xref' with user_id="+_user_id+
+                               " AND current_ind=1."));
+      }
+    }
+    catch (SQLException _sqle) {
+      _sqle.printStackTrace();
+      throw(new EJBException(_sqle.getMessage()));
+    }
+    catch (NamingException _ne) {
+      _ne.printStackTrace();
+      throw(new EJBException(_ne.getMessage()));
+    }
+    finally {
+      if (con!=null) {
+        try {
+          con.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+      if (ps!=null) {
+        try {
+          ps.close();
+        }
+        catch (Exception _e) {
+          /* do nothing */
+        }
+      }
+    }
+  }
+
+  public boolean isCurrentUserSchoolId(long _user_id,long _school_id)
                                           throws EJBException, RemoteException {
 
     boolean current=false;
@@ -158,118 +344,6 @@ public class UserSchoolBean implements SessionBean {
       }
     }
     return(current);
-  }
-
-  public void setCurrent(long _user_id,long _school_id,boolean _current)
-                                          throws EJBException, RemoteException {
-
-    Connection con=null;
-    PreparedStatement ps=null;
-
-    try {
-
-      String ds_name=(String)init_ctx.lookup(DATA_SOURCE);
-      DataSource ds=(DataSource)init_ctx.lookup(ds_name);
-
-      StringBuffer query=new StringBuffer(1024);
-      query.append("UPDATE user_school_xref ");
-      query.append("SET current_ind=? ");
-      query.append("WHERE user_id=? AND school_id=?");
-
-      con=ds.getConnection();
-      ps=con.prepareStatement(query.toString());
-      ps.setBoolean(1,_current);
-      ps.setLong(2,_user_id);
-      ps.setLong(3,_school_id);
-
-      int rc=ps.executeUpdate();
-      if (rc!=1) {
-        throw(new EJBException("Wrong number of rows updated in "+
-                               "'user_school_xref'. Updated "+rc+", should "+                                  "have updated 1."));
-      }
-    }
-    catch (SQLException _sqle) {
-      _sqle.printStackTrace();
-      throw(new EJBException(_sqle.getMessage()));
-    }
-    catch (NamingException _ne) {
-      _ne.printStackTrace();
-      throw(new EJBException(_ne.getMessage()));
-    }
-    finally {
-      if (con!=null) {
-        try {
-          con.close();
-        }
-        catch (Exception _e) {
-          /* do nothing */
-        }
-      }
-      if (ps!=null) {
-        try {
-          ps.close();
-        }
-        catch (Exception _e) {
-          /* do nothing */
-        }
-      }
-    }
-  }
-
-  public void removeUserSchool(long _user_id,long _school_id)
-                                          throws EJBException, RemoteException {
-
-    Connection con=null;
-    PreparedStatement ps=null;
-
-    try {
-
-      String ds_name=(String)init_ctx.lookup(DATA_SOURCE);
-      DataSource ds=(DataSource)init_ctx.lookup(ds_name);
-
-      StringBuffer query=new StringBuffer(1024);
-      query.append("DELETE ");
-      query.append("FROM user_school_xref ");
-      query.append("WHERE user_id=? AND school_id=?");
-
-      con=ds.getConnection();
-      ps=con.prepareStatement(query.toString());
-      ps.setLong(1,_user_id);
-      ps.setLong(2,_school_id);
-
-      int rc=ps.executeUpdate();
-      if (rc!=1) {
-        throw(new EJBException("Wrong number of rows deleted from "+
-                               "'user_school_xref'. Deleted "+rc+", should "+
-                               "have deleted 1."));
-      }
-    }
-    catch (SQLException _sqle) {
-      _sqle.printStackTrace();
-      throw(new EJBException(_sqle.getMessage()));
-    }
-    catch (NamingException _ne) {
-      _ne.printStackTrace();
-      throw(new EJBException(_ne.getMessage()));
-    }
-    finally {
-      if (con!=null) {
-        try {
-          con.close();
-        }
-        catch (Exception _e) {
-          /* do nothing */
-        }
-      }
-      if (ps!=null) {
-        try {
-          ps.close();
-        }
-        catch (Exception _e) {
-          /* do nothing */
-        }
-      }
-    }
   }
 
 };
