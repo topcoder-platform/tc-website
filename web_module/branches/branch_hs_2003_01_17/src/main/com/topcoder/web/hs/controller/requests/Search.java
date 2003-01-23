a98299d4acd39999cfd6364371d05e3559241ec9
unlord <unlord@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-23 17:31:07 +0000

    Adding search module

---
+++ web_module/branches/branch_hs_2003_01_17/src/main/com/topcoder/web/hs/controller/requests/Search.java
@@ -0,0 +1,37 @@
+package com.topcoder.web.hs.controller.requests;
+
+import com.topcoder.shared.util.logging.*;
+import com.topcoder.web.hs.model.*;
+
+import java.util.*;
+import javax.servlet.*;
+
+public class Search extends Base {
+
+  private final static String SEARCH_BASE="/search/";
+
+  private final static String ADVANCED_SEARCH_PAGE="advanced_search.jsp";
+
+  private final static String SEARCH_CMD="adv";
+
+  private final static Logger log=Logger.getLogger(Search.class);
+
+  protected void businessProcessing() throws Exception {
+
+    String cmd=request.getParameter("cmd");
+
+    log.info("Search: cmd="+cmd);
+
+    if (cmd==null||cmd.equals("")) {
+      setNextPage(SEARCH_BASE+ADVANCED_SEARCH_PAGE);
+      setIsNextPageInContext(true);
+    }
+    else if (cmd.equals(SEARCH_CMD)) {
+      log.debug("Search processing 'adv' command.");
+
+      
+    }
+    else {
+    }
+  }
+};
