039a2b1f6336154b00dfa43223b12f7e8b4092c6
Porgery <Porgery@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-01-25 07:36:10 +0000

    Implemented back-end functionality

--- web_module/branches/branch_screening_2002_12_16/src/main/com/topcoder/web/screening/request/ProblemList.java
+++ web_module/branches/branch_screening_2002_12_16/src/main/com/topcoder/web/screening/request/ProblemList.java
@@ -1,6 +1,13 @@
 package com.topcoder.web.screening.request;
 
 import com.topcoder.web.screening.common.Constants;
+import com.topcoder.shared.dataAccess.*;
+import com.topcoder.shared.dataAccess.resultSet.*;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.util.Map;
+import java.util.ArrayList;
 
 /**
  * Processor for Problem List page.
@@ -12,6 +19,41 @@ public class ProblemList extends BaseProcessor {
      * @throws Exception
      */
     public void process() throws Exception {
+//        authorize(getSelfRedirect());
+        
+        InitialContext context = new InitialContext();
+        DataAccessInt dAccess = new DataAccess(
+            (DataSource)context.lookup(Constants.DATA_SOURCE));
+        
+        Request dr = new Request();
+        dr.setContentHandle("problemList");
+        dr.setProperty("uid", String.valueOf(getAuthentication().getUser().getId()));
+        
+        Map map = dAccess.getData(dr);
+
+        if(map != null && map.size() == 1)
+        {
+            ResultSetContainer result = 
+                (ResultSetContainer)map.get("problemList");
+            
+            ArrayList list = new ArrayList();
+            if(result != null && result.size() > 0){
+                TCResultItem round;
+                int start=0;
+                while(start < result.size()){
+                    round = result.getItem(start,"session_round_id");
+                    int end = start; 
+                    while(end < result.size() && 
+                        round.equals(result.getItem(end,"session_round_id")))
+                            end++;
+                    list.add(result.subList(start,end));
+                    start = end;
+                }
+            }
+            
+            getRequest().setAttribute("problemList", list);
+        }
+
         setNextPage(Constants.PROBLEM_LIST_PAGE);
         setNextPageInContext(true);
     }
