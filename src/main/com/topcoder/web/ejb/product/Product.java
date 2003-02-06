8acf2b847b1d0ed66853763a8825e0bb82782e17
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-02-06 20:37:32 +0000

    no message

--- src/main/com/topcoder/web/ejb/product/Product.java
+++ src/main/com/topcoder/web/ejb/product/Product.java
@@ -13,38 +13,54 @@ import java.rmi.RemoteException;
  */
 public interface Product extends EJBObject {
     /**
-     *
-     *
      * @see com.topcoder.web.ejb.product.ProductBean#createProduct
      */
     long createProduct(float cost) throws RemoteException, EJBException;
 
     /**
-     *
-     *
      * @see com.topcoder.web.ejb.product.ProductBean#getProductDesc
      */
     String getProductDesc(long productId) throws RemoteException, EJBException;
 
     /**
-     *
-     *
      * @see com.topcoder.web.ejb.product.ProductBean#getCost
      */
     float getCost(long productId) throws RemoteException, EJBException;
 
     /**
-     *
-     *
+     * @see com.topcoder.web.ejb.product.ProductBean#getNumUnits
+     */
+    int getNumUnits(long productId) throws RemoteException, EJBException;
+
+    /**
+     * @see com.topcoder.web.ejb.product.ProductBean#getUnitTypeId
+     */
+    long getUnitTypeId(long productId) throws RemoteException, EJBException;
+
+    /**
+     * @see com.topcoder.web.ejb.product.ProductBean#getUnitTypeDesc
+     */
+    String getUnitTypeDesc(long productId) throws RemoteException, EJBException;
+
+    /**
      * @see com.topcoder.web.ejb.product.ProductBean#setProductDesc
      */
     void setProductDesc(long userId, String productDesc)
             throws RemoteException, EJBException;
 
     /**
-     *
-     *
      * @see com.topcoder.web.ejb.product.ProductBean#setCost
      */
     void setCost(long userId, float cost) throws RemoteException, EJBException;
+
+    /**
+     * @see com.topcoder.web.ejb.product.ProductBean#setNumUnits
+     */
+    void setNumUnits(long userId, int numUnits) throws RemoteException, EJBException;
+
+    /**
+     * @see com.topcoder.web.ejb.product.ProductBean#setUnitTypeId
+     */
+    void setUnitTypeId(long userId, long unitTypeId) throws RemoteException, EJBException;
+
 }
