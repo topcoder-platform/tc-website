d45b9555af795b5254356eeedb77da06cbf57090
Porgery <Porgery@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-02-07 09:38:01 +0000

    Tweaked the error handling

--- web_module/branches/branch_screening_2002_12_16/src/main/com/topcoder/web/screening/request/ProblemList.java
+++ web_module/branches/branch_screening_2002_12_16/src/main/com/topcoder/web/screening/request/ProblemList.java
@@ -1,6 +1,6 @@
 package com.topcoder.web.screening.request;
 
-import com.topcoder.web.screening.common.Constants;
+import com.topcoder.web.screening.common.*;
 import com.topcoder.shared.dataAccess.*;
 import com.topcoder.shared.dataAccess.resultSet.*;
 import com.topcoder.shared.security.User;
@@ -32,33 +32,33 @@ public class ProblemList extends BaseProcessor {
         
         Map map = dAccess.getData(dr);
 
-        if(map != null && map.size() == 1)
-        {
-            ResultSetContainer result = 
-                (ResultSetContainer)map.get("problemList");
+        if(map == null || map.size() != 1)
+            throw new ScreeningException("Data retrieval error");
+
+        ResultSetContainer result = 
+            (ResultSetContainer)map.get("problemList");
             
-            ArrayList list = new ArrayList();
-            if(result != null && result.size() > 0){
-                String round;
-                int item=0;
-                while(item < result.size()){
-                    ArrayList subList = new ArrayList(3);
-                    round = result.getItem(item,"session_round_id").toString();
-                    while(item < result.size() && 
-                        round.equals(result.getItem(item,"session_round_id").toString())){
-                            String problem = result.getItem(item,"problem_id").toString();
-                            subList.add(
-                                ProblemInfo.createProblemInfo(user,
-                                                              Long.parseLong(round),
-                                                              Long.parseLong(problem)));
-                            item++;
-                    }
-                    list.add(subList);
+        ArrayList list = new ArrayList();
+        if(result != null && result.size() > 0){
+            String round;
+            int item=0;
+            while(item < result.size()){
+                ArrayList subList = new ArrayList(3);
+                round = result.getItem(item,"session_round_id").toString();
+                while(item < result.size() && 
+                    round.equals(result.getItem(item,"session_round_id").toString())){
+                        String problem = result.getItem(item,"problem_id").toString();
+                        subList.add(
+                            ProblemInfo.createProblemInfo(user,
+                                                          Long.parseLong(round),
+                                                          Long.parseLong(problem)));
+                        item++;
                 }
+                list.add(subList);
             }
-            
-            getRequest().setAttribute("problemList", list);
         }
+        
+        getRequest().setAttribute("problemList", list);
 
         setNextPage(Constants.PROBLEM_LIST_PAGE);
         setNextPageInContext(true);
