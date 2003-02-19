385b78a86c206e592b7e0bcd2c5d8b368400fcd4
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-02-19 23:06:38 +0000

    added htmlEncode method

--- web_module/trunk/src/main/com/topcoder/web/common/StringUtils.java
+++ web_module/trunk/src/main/com/topcoder/web/common/StringUtils.java
@@ -115,6 +115,46 @@ public final class StringUtils {
         return b.toString();
     }
 
+
+    /**
+     * Takes the given string and converts it so that it will maintain it's
+     * format on an html page.
+     * @return
+     */
+    public static String htmlEncode(String s) {
+        StringBuffer sb = new StringBuffer();
+        char ch = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            if ((ch = s.charAt(i)) == '>') {
+                sb.append("&gt;");
+            } else if (ch == 9) {  //we'll go with 4 spaces for a tab
+                sb.append("&#160;&#160;&#160;&#160;");
+            } else if (ch == '<') {
+                sb.append("&lt;");
+            } else if (ch == 10 || ch == 13) {
+                sb.append("<br />");
+            } else if (((int) ch) < 32) {
+                //anything less than a "space" character is technically unprintable
+                sb.append("[\\u" + (int) ch + "]");
+            } else if ((((int) ch) > 126) && (((int) ch) < 160)) {
+                //anything in this range is unprintable per latin-1
+                sb.append("[\\u" + (int) ch + "]");
+            } else if ((((int) ch) >= 160) && (((int) ch) <= 255)) {
+                //anything in this range is printable per latin-1 with a little massaging
+                sb.append("&#" + (int) ch + ";");
+            } else if ((int) ch > 255) {
+                //anything in this range is unprintable per latin-1
+                //html4.0 has some support but it isn't worth picking out a few cases that
+                //some browsers won't display properly.
+                sb.append("[\\u" + (int) ch + "]");
+            } else
+                sb.append(ch);
+        }
+        return sb.toString();
+    }
+
+
+
     public static void main(String[] args) {
         String blabla = "bla bla";
         System.out.println(consistsOf(blabla, ALPHABET_ALPHA_EN, true));
