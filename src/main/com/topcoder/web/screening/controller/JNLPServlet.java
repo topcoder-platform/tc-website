aeecffafdd9085cf51e7fe241a2ea0a64865e17b
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-02-19 20:21:57 +0000

    no message

--- src/main/com/topcoder/web/screening/controller/JNLPServlet.java
+++ src/main/com/topcoder/web/screening/controller/JNLPServlet.java
@@ -38,54 +38,62 @@ public final class JNLPServlet extends HttpServlet {
         String tunnel = "";
 
         try {
-            host = request.getParameter("host");
-            port = request.getParameter("port");
-            tunnel = request.getParameter("tunnel");
-            companyID = request.getParameter("company");
+             host = request.getParameter("host");
+             if ((host=="") || host.equals("")) host="corporate.topcoder.com";
 
-            /***********************************************************************/
-            out = response.getOutputStream();
-            response.setHeader("Content-Type", "application/x-java-jnlp-file; charset=utf-8");
-            response.setContentType("application/x-java-jnlp-file");
-            /***********************************************************************/
+             port = request.getParameter("port");
+             if ((port=="") || port.equals("")) port="8001";
 
-            out.print("<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-                    "<jnlp spec=\"1.0+\" codebase=\"http://172.16.20.24/contest/screening/\" " +
-                    "href=\"ScreeningApp.jnlp\">\r\n" +
-                    "<information>\r\n" +
-                    "<title>TopCoder Testing Application</title>\r\n" +
-                    "<vendor>TopCoder, Inc.</vendor>\r\n" +
-                    "<homepage href=\"http://www.topcoder.com\"/>\r\n" +
-                    "<description>TopCoder Testing Application</description>\r\n" +
-                    "<icon href=\"http://www.topcoder.com/images/topcoder.jpg\"/>\r\n" +
-                    "</information>\r\n" +
-                    "<security>\r\n" +
-                    "all-permissions/>\r\n" +
-                    "</security>\r\n" +
-                    "<resources>\r\n" +
-                    "<j2se version=\"1.4+\"/>\r\n" +
-                    "<jar href=\"http://172.16.20.20/contest/classes/ScreeningClient.jar\"/>\r\n" +
-                    "</resources>\r\n" +
-                    "<application-desc " +
-                    "main-class=\"com.topcoder.client.screening.tool.view.screeningClient.GenericStarter\">\r\n"+
-                    "<argument>");
-            out.print(host);
-            out.print("</argument>\r\n" +
-                    "<argument>");
-            out.print(port);
-            out.print("</argument>\r\n" +
-                    "<argument>");
-            out.print(companyID);
-            out.print("</argument>\r\n" +
-                    "<argument>");
-            out.print(tunnel);
-            out.print("</argument>\r\n" +
-                    "</application-desc>\r\n" +
-                    "</jnlp>\r\n");
-            //out.write(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+             tunnel = request.getParameter("tunnel");
+             if ((tunnel=="") || tunnel.equals("")) tunnel="http://arena2.topcoder.com/servlet/com.topcoder.utilities.HTTPTunnelling.Tunnel?host=screening+port=8001";
+
+             companyID = request.getParameter("company");
+             if ((companyID=="") || companyID.equals("")) companyID="1";
+
+
+             /***********************************************************************/
+             out = response.getOutputStream();
+             response.setHeader("Content-Type", "application/x-java-jnlp-file; charset=utf-8");
+             response.setContentType("application/x-java-jnlp-file");
+             /***********************************************************************/
+
+             out.print("<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                     "<jnlp spec=\"1.0+\" codebase=\"http://172.16.20.24/contest/screening/\" " +
+                     "href=\"ScreeningApp.jnlp\">\r\n" +
+                     "<information>\r\n" +
+                     "<title>TopCoder Testing Application</title>\r\n" +
+                     "<vendor>TopCoder, Inc.</vendor>\r\n" +
+                     "<homepage href=\"http://www.topcoder.com\"/>\r\n" +
+                     "<description>TopCoder Testing Application</description>\r\n" +
+                     "<icon href=\"http://www.topcoder.com/images/topcoder.jpg\"/>\r\n" +
+                     "</information>\r\n" +
+                     "<security>\r\n" +
+                     "all-permissions/>\r\n" +
+                     "</security>\r\n" +
+                     "<resources>\r\n" +
+                     "<j2se version=\"1.4+\"/>\r\n" +
+                     "<jar href=\"http://172.16.20.20/contest/classes/ScreeningClient.jar\"/>\r\n" +
+                     "</resources>\r\n" +
+                     "<application-desc " +
+                     "main-class=\"com.topcoder.client.screening.tool.view.screeningClient.GenericStarter\">\r\n"+
+                     "<argument>");
+             out.print(host);
+             out.print("</argument>\r\n" +
+                     "<argument>");
+             out.print(port);
+             out.print("</argument>\r\n" +
+                     "<argument>");
+             out.print(companyID);
+             out.print("</argument>\r\n" +
+                     "<argument>");
+             out.print(tunnel);
+             out.print("</argument>\r\n" +
+                     "</application-desc>\r\n" +
+                     "</jnlp>\r\n");
+             //out.write(result);
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
 
     }
 
