f0abdd9d95f4a6f0ce9ff6cec4016de1557046bc
gt494 <gt494@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-02-21 18:44:58 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/screening/controller/JNLPServlet.java
+++ web_module/trunk/src/main/com/topcoder/web/screening/controller/JNLPServlet.java
@@ -61,15 +61,16 @@ public final class JNLPServlet extends HttpServlet {
                      "<jnlp spec=\"1.0+\" " +
                      //"codebase=\"http://corporate.topcoder.com/contest/screening/\" " +
                      //"href=\"ScreeningApp.jnlp\">\r\n" +
+                     ">\r\n" +
                      "<information>\r\n" +
                      "<title>TopCoder Testing Application</title>\r\n" +
                      "<vendor>TopCoder, Inc.</vendor>\r\n" +
-                     "<homepage href=\"http://www.topcoder.com\"/>\r\n" +
+                     "<homepage href=\"http://corporate.topcoder.com\"/>\r\n" +
                      "<description>TopCoder Testing Application</description>\r\n" +
-                     "<icon href=\"http://www.topcoder.com/images/topcoder.jpg\"/>\r\n" +
+                     "<icon href=\"http://corporate.topcoder.com/images/topcoder.jpg\"/>\r\n" +
                      "</information>\r\n" +
                      "<security>\r\n" +
-                     "all-permissions/>\r\n" +
+                     "<all-permissions/>\r\n" +
                      "</security>\r\n" +
                      "<resources>\r\n" +
                      "<j2se version=\"1.4+\"/>\r\n" +
