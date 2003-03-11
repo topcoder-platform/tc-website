1ba73e1b8f688efe3015ffd0e4015bcb72369eaa
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-03-11 22:33:22 +0000

    create requestInfo object and populate it

--- src/main/com/topcoder/web/screening/controller/MainServlet.java
+++ src/main/com/topcoder/web/screening/controller/MainServlet.java
@@ -1,6 +1,7 @@
 package com.topcoder.web.screening.controller;
 
 import java.io.IOException;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -33,7 +34,8 @@ import com.topcoder.web.screening.model.RequestInfo;
  */
 
 public class MainServlet extends HttpServlet {
-     private final static Logger log = Logger.getLogger(MainServlet.class);
+    private final static Logger log = Logger.getLogger(MainServlet.class);
+
     /**
      * Init() is run the moment that the servlet is loaded into the web server.
      * Initializes the static variables.
@@ -43,14 +45,13 @@ public class MainServlet extends HttpServlet {
     public void init() throws ServletException {
         Constants.initialize(getServletConfig());
 
-        if(!Constants.isInitialized())
+        if (!Constants.isInitialized())
             throw new ServletException("Constants did not initialize properly");
 
         //initialize data types for problem component factory
         try {
             Data.initializeDataTypes();
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             throw new ServletException(e.getMessage());
         }
     }
@@ -65,7 +66,7 @@ public class MainServlet extends HttpServlet {
      * @throws IOException
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-               throws ServletException, IOException {
+            throws ServletException, IOException {
         doPost(request, response);
     }
 
@@ -82,23 +83,24 @@ public class MainServlet extends HttpServlet {
      * @throws IOException
      */
     public void doPost(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
+            throws ServletException, IOException {
         try {
             Persistor p = new SessionPersistor(request.getSession());
             WebAuthentication authen = new BasicAuthentication(p, request, response);
             long userId = authen.getActiveUser().getId();
+            PrincipalMgr pm = new PrincipalMgr();
+            TCSubject sub = pm.getUserSubject(userId);
+            Authorization author = new TCSAuthorization(sub);
 
             RequestInfo rInfo = new RequestInfo();
-            rInfo.setIsNew(false);
-            rInfo.setControllerUrl(request.getServletPath());
-            rInfo.setUser(authen.getActiveUser());
+            Set groups = ((TCSAuthorization) author).getGroups();
+            rInfo.setAll(authen.getActiveUser(), groups);
             request.setAttribute(Constants.REQUEST_INFO, rInfo);
 
-            String procParam =
-                request.getParameter(Constants.REQUEST_PROCESSOR);
+            String procParam = request.getParameter(Constants.REQUEST_PROCESSOR);
 
             //redirect to some default page
-            if (procParam == null || procParam.length() == 0){
+            if (procParam == null || procParam.length() == 0) {
                 sendToPage(request, response, Constants.DEFAULT_PAGE, true);
                 return;
             }
@@ -107,13 +109,10 @@ public class MainServlet extends HttpServlet {
                 throw new Exception("Request Processor in illegal format.");
 
             String className = null;
-            if(Constants.PROCESSORS_PACKAGE.endsWith("."))
-            {
-                className = Constants.PROCESSORS_PACKAGE+procParam;
-            }
-            else
-            {
-                className = Constants.PROCESSORS_PACKAGE + "."  + procParam;
+            if (Constants.PROCESSORS_PACKAGE.endsWith(".")) {
+                className = Constants.PROCESSORS_PACKAGE + procParam;
+            } else {
+                className = Constants.PROCESSORS_PACKAGE + "." + procParam;
             }
             log.info(new StringBuffer(100).append("[**** ").append(procParam).append(" **** ").
                     append(authen.getActiveUser().getUserName()).append(" **** ").
@@ -122,28 +121,24 @@ public class MainServlet extends HttpServlet {
             Class procClass = Class.forName(className);
             Resource r = new ClassResource(procClass);
 
-            PrincipalMgr pm = new PrincipalMgr();
-            TCSubject sub = pm.getUserSubject(userId);
-            Authorization author = new TCSAuthorization(sub);
-
-            if(!author.hasPermission(r)){
+            if (!author.hasPermission(r)) {
                 String redirect;
-                if(request.getMethod().equals("POST")){
+                if (request.getMethod().equals("POST")) {
                     redirect = request.getServletPath();
-                }else{
+                } else {
                     redirect = request.getServletPath() + '?' + request.getQueryString();
                 }
-                if(authen.getActiveUser().isAnonymous()){
-                    request.setAttribute(Constants.REDIRECT,redirect);
+                if (authen.getActiveUser().isAnonymous()) {
+                    request.setAttribute(Constants.REDIRECT, redirect);
                     request.setAttribute(Constants.MESSAGE_PARAMETER,
-                        "You must be logged in to access that resource.");
-                    throw new AnonymousUserException("Login required for "+r.getName());
-                }else{
+                            "You must be logged in to access that resource.");
+                    throw new AnonymousUserException("Login required for " + r.getName());
+                } else {
                     throw new PermissionDeniedException(authen.getActiveUser(), r);
                 }
             }
 
-            RequestProcessor rp = (RequestProcessor)procClass.newInstance();
+            RequestProcessor rp = (RequestProcessor) procClass.newInstance();
 
             rp.setRequest(request);
             rp.setAuthentication(authen);
@@ -177,25 +172,23 @@ public class MainServlet extends HttpServlet {
                             HttpServletResponse response,
                             String page,
                             boolean forward)
-                     throws ServletException, IOException {
+            throws ServletException, IOException {
         if (forward) {
             javax.servlet.RequestDispatcher disp =
-                getServletContext().getRequestDispatcher(response.encodeURL(page));
-            if(disp == null){
+                    getServletContext().getRequestDispatcher(response.encodeURL(page));
+            if (disp == null) {
                 disp = getServletContext().getRequestDispatcher(Constants.ERROR_PAGE);
                 request.setAttribute("Exception",
-                    new ScreeningException("Resource '" + page + "' not found."));
+                        new ScreeningException("Resource '" + page + "' not found."));
             }
             disp.forward(request, response);
-        }
-        else {
+        } else {
             String redirectPage = null;
             //do this so people can redirect to internal pages as well
             //as external
             if (page.startsWith("http")) {
                 redirectPage = page;
-            }
-            else {
+            } else {
                 redirectPage = request.getContextPath() + page;
             }
             response.sendRedirect(response.encodeRedirectURL(redirectPage));
@@ -214,7 +207,7 @@ public class MainServlet extends HttpServlet {
     private void sendToErrorPage(HttpServletRequest request,
                                  HttpServletResponse response,
                                  Throwable exception)
-                          throws ServletException, IOException {
+            throws ServletException, IOException {
         request.setAttribute("Exception", exception);
         sendToPage(request, response, Constants.ERROR_PAGE, true);
     }
@@ -229,9 +222,9 @@ public class MainServlet extends HttpServlet {
      * @throws IOException
      */
     private void sendToPermErrorPage(HttpServletRequest request,
-                                 HttpServletResponse response,
-                                 Throwable exception)
-                          throws ServletException, IOException {
+                                     HttpServletResponse response,
+                                     Throwable exception)
+            throws ServletException, IOException {
         request.setAttribute("Exception", exception);
         sendToPage(request, response, Constants.PERM_ERROR_PAGE, true);
     }
