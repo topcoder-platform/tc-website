357691db9b6861e2e2bfdada5b7260cf3dcb5bc7
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-03-28 19:40:24 +0000

    no message

---
+++ web_module/trunk/src/main/com/topcoder/web/security/request/Login.java
@@ -0,0 +1,10 @@
+package com.topcoder.web.security.request;
+
+import com.topcoder.web.security.Constants;
+import com.topcoder.web.common.BaseProcessor;
+
+public class Login extends BaseProcessor {
+    public void businessProcessing() {
+        setNextPage(Constants.jsp_root + Constants.login_page);
+    }
+}
