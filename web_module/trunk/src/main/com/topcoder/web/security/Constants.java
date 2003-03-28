0df4b79fb47e024ceb930346f591297a44434403
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-03-28 19:59:39 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/security/Constants.java
+++ web_module/trunk/src/main/com/topcoder/web/security/Constants.java
@@ -1,5 +1,7 @@
 package com.topcoder.web.security;
 
+import com.topcoder.shared.util.logging.Logger;
+
 import javax.servlet.ServletConfig;
 import java.lang.reflect.Field;
 
@@ -8,6 +10,7 @@ public class Constants {
     public static String index_page = null;
     public static String jsp_root = null;
     public static String login_page = null;
+    private static Logger log = Logger.getLogger(Constants.class);
 
     /**
      * Initializes all public static Strings in this class which also appear as
@@ -18,6 +21,9 @@ public class Constants {
         for (int i = 0; i < f.length; i++)
             try {
                 f[i].set(null, sc.getInitParameter(f[i].getName()));
+                if (f[i].get(null)==null)
+                    log.debug("did not load " + f[i].getName() + " constant");
+                else log.debug("loaded " + f[i].getName() + " constant with value " + f[i].get(null));
             } catch (Exception e) {
                 /* probably harmless, could just be a type or modifier mismatch */
                 e.printStackTrace();
