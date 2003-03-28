929bdb1fa5a8a0bc102048a18a5430bd0c57b69d
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-03-28 20:05:45 +0000

    no message

--- src/main/com/topcoder/web/security/controller/MainServlet.java
+++ src/main/com/topcoder/web/security/controller/MainServlet.java
@@ -1,6 +1,19 @@
 package com.topcoder.web.security.controller;
 
 import com.topcoder.web.common.BaseServlet;
+import com.topcoder.web.security.Constants;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 
 
-public class MainServlet extends BaseServlet { }
+public class MainServlet extends BaseServlet {
+    /**
+     * Initializes the servlet.
+     * @throws javax.servlet.ServletException
+     */
+    public synchronized void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        Constants.init(config);
+    }
+}
