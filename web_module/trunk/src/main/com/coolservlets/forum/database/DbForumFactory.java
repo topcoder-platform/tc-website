defaa9ba2fe8384bfb08415d2fd7a68e5e796f51
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-04-14 18:06:27 +0000

    caching on

--- web_module/trunk/src/main/com/coolservlets/forum/database/DbForumFactory.java
+++ web_module/trunk/src/main/com/coolservlets/forum/database/DbForumFactory.java
@@ -78,7 +78,7 @@ public class DbForumFactory extends ForumFactory {
          "SELECT DISTINCT permission FROM jiveUserPerm WHERE forumID=? " +
          "AND userID=?";
     //By default, cache is enabled.
-    private boolean cacheEnabled = false;
+    private boolean cacheEnabled = true;
     private boolean userPermCacheEnabled = true;
     //Cache forum objects for faster access.
     protected Cache forumCache;
