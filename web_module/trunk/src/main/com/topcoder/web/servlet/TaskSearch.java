56cf5a3254bb925a163dd76f45ba18530526ea90
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-06-04 16:21:17 +0000

    get referral info using query tool

--- web_module/trunk/src/main/com/topcoder/web/servlet/TaskSearch.java
+++ web_module/trunk/src/main/com/topcoder/web/servlet/TaskSearch.java
@@ -353,7 +353,7 @@ public final class TaskSearch {
                         rsc = (ResultSetContainer) resultMap.get("Coder_Data");
                         if (!rsc.isEmpty()) {
                             stat = new com.topcoder.common.web.data.stat.coder.Coder();
-                            stat.setMemberSince((java.sql.Date)rsc.getItem(0, "member_since").getResultData());
+                            stat.setMemberSince(new java.sql.Date(((Date)rsc.getItem(0, "member_since").getResultData()).getTime()));
                             stat.setHandle(rsc.getItem(0, "handle").toString());
                             stat.setRating(((Integer)rsc.getItem(0, "rating").getResultData()).intValue());
                             stat.setTotalEarnings(((Float)rsc.getItem(0, "total_earnings").getResultData()).floatValue());
