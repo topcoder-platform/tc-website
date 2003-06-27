b8959b07bbe8e6aacbfb5e60653c014fae2982d0
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-06-27 17:01:31 +0000

    no message

--- src/main/com/topcoder/web/privatelabel/model/Base.java
+++ src/main/com/topcoder/web/privatelabel/model/Base.java
@@ -1,5 +1,7 @@
 package com.topcoder.web.privatelabel.model;
 
+import java.lang.reflect.Field;
+
 /**
  *
  * @author gpaul 06.26.2003
@@ -17,5 +19,19 @@ public class Base {
 
     public void setNew(boolean isNew) {
         this.isNew = isNew;
+    }
+
+    public String toString() {
+        Field[] f = this.getClass().getFields();
+        StringBuffer buf = new StringBuffer(200);
+        for (int i = 0; i < f.length; i++) {
+            try {
+                buf.append(f[i].getName() + " = " + f[i].get(null));
+            } catch (Exception e) {
+                buf.append(f[i].getName() + " is not accessible");
+            }
+            if (i<f.length-1) buf.append(", ");
+        }
+        return buf.toString();
     }
 }
