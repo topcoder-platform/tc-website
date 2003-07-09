3a6b2cb47ac482646389f5ce2382f917e7bd2db5
apps <apps@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-09 20:13:07 +0000

    *** empty log message ***

--- scripts/runcache.sh
+++ scripts/runcache.sh
@@ -1,11 +1,11 @@
 #!/bin/ksh
 
-JAVACMD=java
+MAXMEM=128m
+JAVACMD=/usr/j2sdk1.4.2/bin/java
 BASE=..
 MAIN=com.topcoder.shared.distCache.CacheServerMain
 PROCESSOR=DefaultProcessor
 CMD=usage
-MAXMEM=1024m
 LOGFILE=cache-`date +%Y-%m-%d-%H-%M-%S`.log
 
 CP=$BASE/build/classes
@@ -13,15 +13,35 @@ CP=$CP:$BASE/resources
 CP=$CP:$BASE/lib/jars/nbio.jar
 CP=$CP:$CLASSPATH
 
+OPTS="-cp $CP -XX:MaxTenuringThreshold=0 -XX:SurvivorRatio=20000 -Xms$MAXMEM -Xmx$MAXMEM -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
+#OPTS="-cp $CP -XX:MaxTenuringThreshold=0 -XX:SurvivorRatio=20000 -Xms$MAXMEM -Xmx$MAXMEM -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC"
+
+
+##############################################
+#1. -XX:MaxTenuringThreshold=0 
+#2. -XX:SurvivorRatio=20000 
+#3. -Xms128m
+#4. -Xmx128m
+#5. -XX:+ UseConcMarkSweepGC 
+#6. -XX:+UseParNewGC 
+#
+#1.  simulate promote all
+#2.  make eden large
+#3.  force a large permananent area
+#4.  force a large permananent area
+#5.  use the concurrent old generation gc
+#6.  use the parallel new generation gc
+##############################################
+
 if [[ $1 != "" ]] ; then
         CMD=$1
         shift
 fi
 
 if [ "$CMD" = "start" ] ; then
-    nohup $JAVACMD -cp $CP -Xmx$MAXMEM $MAIN -primary $@ >$LOGFILE 2>&1 &
+    nohup $JAVACMD $OPTS $MAIN -primary $@ >$LOGFILE 2>&1 &
     echo $! > primary.pid
-    nohup $JAVACMD -cp $CP -Xmx$MAXMEM $MAIN -secondary $@ >$LOGFILE 2>&1 &
+    nohup $JAVACMD $OPTS $MAIN -secondary $@ >$LOGFILE 2>&1 &
     echo $! > secondary.pid
         echo "start maxmem=$MAXMEM"
 elif [ "$CMD" = "stop" ] ; then
