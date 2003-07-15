fbb783040db669568193f7397167b85b7b844ff9
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-15 19:26:03 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/survey/Results.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/survey/Results.java
@@ -1,12 +1,16 @@
 package com.topcoder.web.tc.controller.request.survey;
 
 import com.topcoder.web.tc.Constants;
+import com.topcoder.web.tc.model.Question;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.shared.dataAccess.Request;
 import com.topcoder.shared.dataAccess.DataAccessInt;
 import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 
 import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Results extends SurveyData {
     protected void surveyProcessing() throws TCWebException {
@@ -21,6 +25,34 @@ public class Results extends SurveyData {
         responseRequest.setProperty("sid", String.valueOf(surveyId));
         responseRequest.setProperty("qid", String.valueOf(questionId));
         return (ResultSetContainer)dataAccess.getData(responseRequest).get("response_info");
+    }
+
+    protected final List getQuestionInfo(long surveyId) throws Exception {
+        Request r = new Request();
+        r.setContentHandle("survey_questions");
+        r.setProperty("sid", String.valueOf(surveyId));
+        //response data has to be live, no cache
+        DataAccessInt dataAccess = getDataAccess();
+        Map qMap = dataAccess.getData(r);
+        ResultSetContainer questions = (ResultSetContainer) qMap.get("question_list");
+
+        Question q = null;
+        ResultSetContainer.ResultSetRow question = null;
+        List questionList = new ArrayList(questions.size());
+        for (Iterator it = questions.iterator(); it.hasNext();) {
+            question = (ResultSetContainer.ResultSetRow) it.next();
+            int temp = question.getIntItem("question_style_id");
+            if (!isFreeForm(temp)) {
+                q = new Question();
+                q.setId(question.getLongItem("question_id"));
+                q.setStyleId(temp);
+                q.setTypeId(question.getIntItem("question_type_id"));
+                q.setText(question.getStringItem("question_text"));
+                q.setAnswerInfo(makeAnswerInfo(surveyId, q.getId()));
+                questionList.add(q);
+            }
+        }
+        return questionList;
     }
 
 
