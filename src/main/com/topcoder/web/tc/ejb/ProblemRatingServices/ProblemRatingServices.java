88d46ea9524e8418bcada0c3f20baf6b6a091b8d
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-17 16:09:52 +0000

    adding problem rating stuff

---
+++ src/main/com/topcoder/web/tc/ejb/ProblemRatingServices/ProblemRatingServices.java
@@ -0,0 +1,10 @@
+package com.topcoder.web.tc.ejb.ProblemRatingServices;
+
+
+import javax.ejb.EJBObject;
+import java.rmi.RemoteException;
+import java.util.Map;
+
+public interface ProblemRatingServices extends EJBObject {
+    public void submitAnswers(int[] questions, int[] ratings, long userID, int problemID) throws RemoteException, Exception;
+}
