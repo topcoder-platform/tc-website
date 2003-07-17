08bf3e4701e87592d7f39235929948d73b83e180
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-17 19:06:47 +0000

    adding problem rating stuff

--- src/main/com/topcoder/web/tc/controller/request/problemRating/Results.java
+++ src/main/com/topcoder/web/tc/controller/request/problemRating/Results.java
@@ -16,32 +16,19 @@ import java.util.Map;
 public class Results extends Base {
     protected void businessProcessing() throws TCWebException {
         try{
-            getRequest().setAttribute("problemRatingResults",getProblemResults(Integer.parseInt(getRequest().getParameter(Constants.PROBLEM_ID))));
+            Request r = new Request();
+            r.setContentHandle("Problem Rating Results");
+            r.setProperty("pm", getRequest().getParameter(Constants.PROBLEM_ID));
+            //response data has to be live, no cache
+            DataAccessInt dataAccess = getDataAccess();
+            Map qMap = dataAccess.getData(r);
+            ResultSetContainer questions = (ResultSetContainer) qMap.get("problem rating results");
+            getRequest().setAttribute("problemRatingResults",questions);
             setNextPage(Constants.PROBLEM_RATING_RESULTS);
             setIsNextPageInContext(true);
         }catch(Exception e){
             e.printStackTrace();
         }
     }
-
-    protected final List getProblemResults(int problemID)throws Exception {
-        Request r = new Request();
-        r.setContentHandle("Problem Rating Results");
-        r.setProperty("pm", String.valueOf(problemID));
-        //response data has to be live, no cache
-        DataAccessInt dataAccess = getDataAccess();
-        Map qMap = dataAccess.getData(r);
-        ResultSetContainer questions = (ResultSetContainer) qMap.get("Problem Rating Results");
-
-        ResultSetContainer.ResultSetRow question = null;
-        List prList = new ArrayList(questions.size());
-        for (Iterator it = questions.iterator(); it.hasNext();) {
-            question = (ResultSetContainer.ResultSetRow) it.next();
-            prList.add(new ProblemRatingResult(question.getStringItem("question"),Double.parseDouble(question.getItem("average").toString())));
-        }
-        return prList;
-    }
-
-
 }
 
