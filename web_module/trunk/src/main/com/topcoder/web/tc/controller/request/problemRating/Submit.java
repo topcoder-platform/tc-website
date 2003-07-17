cdd2019d3a5a703aca92df56f5c4986905ecbd50
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-17 16:09:52 +0000

    adding problem rating stuff

---
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/problemRating/Submit.java
@@ -0,0 +1,60 @@
+package com.topcoder.web.tc.controller.request.problemRating;
+
+import com.topcoder.web.common.BaseProcessor;
+import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.common.security.SessionPersistor;
+import com.topcoder.shared.util.TCContext;
+import com.topcoder.shared.util.ApplicationServer;
+import com.topcoder.security.TCSubject;
+import com.topcoder.web.tc.ejb.ProblemRatingServices.ProblemRatingServices;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import javax.rmi.PortableRemoteObject;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Iterator;
+import java.util.Enumeration;
+import java.util.ArrayList;
+
+/**
+ * This puts the results of a problem rating into the DB
+ * @author lbackstrom 07.08.2003
+ */
+public class Submit extends Base {
+
+    protected static final TCSubject CREATE_USER = new TCSubject(100000);
+
+    protected void businessProcessing() throws TCWebException{
+        long userID = getUser().getId();
+        InitialContext ctx = null;
+        try {
+            ctx = new InitialContext();
+            ProblemRatingServices prs = (ProblemRatingServices)createEJB(ctx, ProblemRatingServices.class, "main:");
+            HttpServletRequest request = getRequest();
+            Enumeration en = request.getParameterNames();
+            ArrayList al = new ArrayList(10);
+            while(en.hasMoreElements()){
+                String next = en.nextElement().toString();
+                if(next.charAt(0) == 'q'){
+                    try{
+                        Integer question_id = new Integer(next.substring(1));
+                        Integer rating = new Integer(request.getParameterValues(next)[0]);
+                        al.add(question_id);
+                        al.add(rating);
+                    }catch(Exception e){
+                        //this parameter wasn't really a question, don't worry about it
+                    }
+                }
+            }
+            int[] qs = new int[al.size()/2];
+            int[] rs = new int[al.size()/2];
+            for(int i = 0, ptr = 0; i<qs.length; i++){
+                qs[i] = Integer.parseInt(al.get(ptr++).toString());
+                rs[i] = Integer.parseInt(al.get(ptr++).toString());
+            }
+            prs.submitAnswers(qs,rs,userID,Integer.parseInt(request.getParameterValues("pm")[0]));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
