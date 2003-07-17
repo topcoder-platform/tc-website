49aac976f3695d1a14c2d1e1fe398f0f1b4d683f
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-17 18:09:21 +0000

    adding problem rating stuff

---
+++ src/main/com/topcoder/web/tc/controller/request/problemRating/Results.java
@@ -0,0 +1,42 @@
+package com.topcoder.web.tc.controller.request.problemRating;
+
+import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.tc.Constants;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.web.tc.model.ProblemRatingResult;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class Results extends Base {
+    protected void businessProcessing() throws TCWebException {
+        setNextPage(Constants.PROBLEM_RATING_RESULTS);
+        setIsNextPageInContext(true);
+    }
+
+    protected final List getProblemResults(int problemID) throws Exception {
+        Request r = new Request();
+        r.setContentHandle("Problem Rating Results");
+        r.setProperty("pm", String.valueOf(problemID));
+        //response data has to be live, no cache
+        DataAccessInt dataAccess = getDataAccess();
+        Map qMap = dataAccess.getData(r);
+        ResultSetContainer questions = (ResultSetContainer) qMap.get("Problem Rating Results");
+
+        ResultSetContainer.ResultSetRow question = null;
+        List prList = new ArrayList(questions.size());
+        for (Iterator it = questions.iterator(); it.hasNext();) {
+            question = (ResultSetContainer.ResultSetRow) it.next();
+            prList.add(new ProblemRatingResult(question.getStringItem("question"),Double.parseDouble(question.getItem("average").toString())));
+        }
+        return prList;
+    }
+
+
+}
+
---
+++ src/main/com/topcoder/web/tc/view/problem_rating/results.jsp
@@ -0,0 +1,23 @@
+<%@  page language="java"  %>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<%@ taglib uri="/rsc-taglib.tld" prefix="rsc" %>
+<%@ taglib uri="/tc.tld" prefix="tc" %>
+<html>
+<head>
+<title>TopCoder Problem Rating Results</title>
+</head>
+<body>
+    <table>
+        <rsc:iterator list="<%=problemRatingResults%>" id="result">
+        <tr>
+            <td>
+                <rsc:item row="<%=result%>" name="question"/>
+            </td>
+            <td>
+                <rsc:item row="<%=result%>" name="avg" format="#.##"/>%
+            </td>
+        </tr>
+        </rsc:iterator>
+    </table>
+</body>
+</html>
