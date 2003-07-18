59780b5bacc83e05a896bc66b6eb58bddd6ac89d
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-18 14:46:30 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/crpf/Report.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/crpf/Report.java
@@ -2,10 +2,12 @@ package com.topcoder.web.tc.controller.request.crpf;
 
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.common.model.SortInfo;
 import com.topcoder.web.tc.Constants;
 import com.topcoder.shared.dataAccess.Request;
 import com.topcoder.shared.dataAccess.DataAccessConstants;
 import com.topcoder.shared.dataAccess.CachedDataAccess;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 
 import java.util.Map;
 
@@ -29,6 +31,13 @@ public class Report extends Base {
             CachedDataAccess da = (CachedDataAccess)getDataAccess(true);
             da.setExpireTime(30*60*1000); //30 minutes should be sufficient
             Map m = da.getData(r);
+            ResultSetContainer rsc = (ResultSetContainer)m.get("crpf_donation_list");
+            SortInfo s = new SortInfo();
+            s.addDefault(rsc.getColumnIndex("rank"), "asc");
+            s.addDefault(rsc.getColumnIndex("handle"), "asc");
+            s.addDefault(rsc.getColumnIndex("rating"), "desc");
+            s.addDefault(rsc.getColumnIndex("amount"), "desc");
+            getRequest().setAttribute(SortInfo.REQUEST_KEY, s);
             getRequest().setAttribute("donationInfo", m.get("crpf_donation_list"));
         } catch (Exception e) {
             throw new TCWebException(e);
@@ -37,4 +46,5 @@ public class Report extends Base {
         setIsNextPageInContext(true);
 
     }
+
 }
