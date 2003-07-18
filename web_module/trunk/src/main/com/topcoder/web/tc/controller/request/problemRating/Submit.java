b59d64bf5d0710bdcba2b0f18e98a2220b741d23
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-18 04:08:31 +0000

    adding problem rating stuff

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/problemRating/Submit.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/problemRating/Submit.java
@@ -58,6 +58,7 @@ public class Submit extends Base {
                 setNextPage("?" + Constants.MODULE_KEY + "=ProblemRatingResults&" + Constants.PROBLEM_ID + "=" + request.getParameter("pid"));
                 setIsNextPageInContext(true);
             }else{
+                request.setAttribute("error","Please answer all of the questions.");
                 setNextPage("?" + Constants.MODULE_KEY + "=ProblemRatingQuestions");
                 setIsNextPageInContext(true);
             }
