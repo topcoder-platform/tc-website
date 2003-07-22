73ff9e59f4f1049f7cf19daf41538026f8d8cbd4
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-22 17:24:27 +0000

    adding problem rating stuff

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/problemRating/Base.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/problemRating/Base.java
@@ -5,10 +5,18 @@ import com.topcoder.shared.dataAccess.CachedDataAccess;
 import com.topcoder.shared.dataAccess.DataAccess;
 import com.topcoder.shared.util.DBMS;
 import com.topcoder.web.common.BaseProcessor;
+import com.topcoder.web.tc.model.ProblemRatingQuestion;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.web.tc.Constants;
 
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 import javax.rmi.PortableRemoteObject;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Iterator;
 
 abstract public class Base extends BaseProcessor {
 
@@ -34,5 +42,48 @@ abstract public class Base extends BaseProcessor {
         else
             dAccess = new DataAccess(ds);
         return dAccess;
+    }
+    protected void processResults() throws Exception{
+        Request r = new Request();
+        r.setContentHandle("Problem Rating Results");
+        r.setProperty("pm", getRequest().getParameter(Constants.PROBLEM_ID));
+        //response data has to be live, no cache
+        DataAccessInt dataAccess = getDataAccess();
+        Map qMap = dataAccess.getData(r);
+        ResultSetContainer questions = (ResultSetContainer) qMap.get("problem rating results");
+        ResultSetContainer problemName = (ResultSetContainer) qMap.get("problem name");
+        getRequest().setAttribute("problemRatingResults",questions);
+        getRequest().setAttribute("problemName",problemName.getRow(0).getStringItem("name"));
+    }
+
+    protected void processQuestions() throws Exception{
+        long userID = getUser().getId();
+        Request r = new Request();
+        r.setContentHandle("Problem Rating Questions");
+        r.setProperty("pm", getRequest().getParameter(Constants.PROBLEM_ID));
+        r.setProperty("cr", String.valueOf(userID));
+        //response data has to be live, no cache
+        DataAccessInt dataAccess = getDataAccess();
+        Map qMap = dataAccess.getData(r);
+        ResultSetContainer ratings = (ResultSetContainer) qMap.get("problem rated");
+        if(ratings.getRow(0).getIntItem("count")!=0){
+            processResults();
+            setNextPage(Constants.PROBLEM_RATING_RESULTS);
+            setIsNextPageInContext(true);
+            return;
+        }
+        ResultSetContainer questionRSC = (ResultSetContainer) qMap.get("problem rating questions");
+        ResultSetContainer problemName = (ResultSetContainer) qMap.get("problem name");
+        Iterator it = questionRSC.iterator();
+        List questions = new ArrayList(10);
+        while(it.hasNext()){
+            ResultSetContainer.ResultSetRow row = (ResultSetContainer.ResultSetRow)it.next();
+            ProblemRatingQuestion prq = new ProblemRatingQuestion();
+            prq.setQuestion(row.getStringItem("question"));
+            prq.setID(row.getIntItem("question_id"));
+            questions.add(prq);
+        }
+        getRequest().setAttribute("problemRatingQuestions",questions);
+        getRequest().setAttribute("problemName",problemName.getRow(0).getStringItem("name"));
     }
 }
--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/problemRating/Results.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/problemRating/Results.java
@@ -15,16 +15,7 @@ import java.util.Map;
 public class Results extends Base {
     protected void businessProcessing() throws TCWebException {
         try{
-            Request r = new Request();
-            r.setContentHandle("Problem Rating Results");
-            r.setProperty("pm", getRequest().getParameter(Constants.PROBLEM_ID));
-            //response data has to be live, no cache
-            DataAccessInt dataAccess = getDataAccess();
-            Map qMap = dataAccess.getData(r);
-            ResultSetContainer questions = (ResultSetContainer) qMap.get("problem rating results");
-            ResultSetContainer problemName = (ResultSetContainer) qMap.get("problem name");
-            getRequest().setAttribute("problemRatingResults",questions);
-            getRequest().setAttribute("problemName",problemName.getRow(0).getStringItem("name"));
+            processResults();
             setNextPage(Constants.PROBLEM_RATING_RESULTS);
             setIsNextPageInContext(true);
         }catch(Exception e){
