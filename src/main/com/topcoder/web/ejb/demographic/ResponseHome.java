29fc4a15b4b53d7c3c69e6e78826708f871ef57c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-23 23:26:17 +0000

    no message

--- src/main/com/topcoder/web/ejb/demographic/ResponseHome.java
+++ src/main/com/topcoder/web/ejb/demographic/ResponseHome.java
@@ -1,7 +1,5 @@
 package com.topcoder.web.ejb.demographic;
 
-import com.topcoder.web.ejb.survey.Response;
-
 import javax.ejb.EJBHome;
 import javax.ejb.CreateException;
 import java.rmi.RemoteException;
