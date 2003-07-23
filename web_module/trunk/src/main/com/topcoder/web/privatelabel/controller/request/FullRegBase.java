abd7a6c236f59b09054ceffce2f8ad3a1ccfa8ee
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-23 14:47:38 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/FullRegBase.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/FullRegBase.java
@@ -37,10 +37,10 @@ abstract class FullRegBase extends SimpleRegBase {
             for (Iterator it = questions.values().iterator(); it.hasNext();) {
                 StringBuffer buf = new StringBuffer(100);
                 DemographicQuestion q = (DemographicQuestion)it.next();
-                buf.append("question: " + q.getText() + "  ");
+                buf.append("question: " + q.getId() + " " + q.getText() + "  ");
                 for (Iterator it1 = q.getAnswers().iterator(); it1.hasNext();) {
                     DemographicAnswer a = (DemographicAnswer)it1.next();
-                    buf.append(a.getText() + " ");
+                    buf.append(a.getAnswerId() + " " + a.getText() + " ");
                 }
                 log.debug(buf.toString());
             }
--- web_module/trunk/src/main/com/topcoder/web/privatelabel/view/tag/DemographicAnswerText.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/view/tag/DemographicAnswerText.java
@@ -2,13 +2,17 @@ package com.topcoder.web.privatelabel.view.tag;
 
 import com.topcoder.web.privatelabel.model.DemographicResponse;
 import com.topcoder.web.privatelabel.model.DemographicQuestion;
+import com.topcoder.web.privatelabel.model.DemographicAnswer;
+import com.topcoder.shared.util.logging.Logger;
 
 import javax.servlet.jsp.tagext.TagSupport;
 import javax.servlet.jsp.JspException;
 import java.util.Map;
+import java.util.Iterator;
 import java.io.IOException;
 
 public class DemographicAnswerText  extends TagSupport {
+    protected static Logger log = Logger.getLogger(DemographicAnswerText.class);
     private DemographicResponse response = null;
     private Map questions = null;
 
@@ -24,6 +28,20 @@ public class DemographicAnswerText  extends TagSupport {
         try {
             DemographicQuestion question = (DemographicQuestion)questions.get(new Long(response.getQuestionId()));
             String ret = "";
+
+
+            for (Iterator it = questions.values().iterator(); it.hasNext();) {
+                StringBuffer buf = new StringBuffer(100);
+                DemographicQuestion q = (DemographicQuestion)it.next();
+                buf.append("question: " + q.getText() + "  ");
+                for (Iterator it1 = q.getAnswers().iterator(); it1.hasNext();) {
+                    DemographicAnswer a = (DemographicAnswer)it1.next();
+                    buf.append(a.getText() + " ");
+                }
+                log.debug(buf.toString());
+            }
+
+
             if (question.getAnswerType()==DemographicQuestion.FREE_FORM) {
                 ret = response.getText();
             } else if (question.getAnswerType()==DemographicQuestion.SINGLE_SELECT) {
