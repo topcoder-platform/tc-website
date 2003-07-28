a8bcdf4844c9f39db235c057ce434e0862a5a180
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-07-28 18:49:01 +0000

    no message

---
+++ src/main/com/topcoder/web/ejb/currentschool/CurrentSchoolBean.java
@@ -0,0 +1,52 @@
+package com.topcoder.web.ejb.currentschool;
+
+import com.topcoder.web.ejb.BaseEJB;
+import com.topcoder.shared.util.DBMS;
+
+import javax.ejb.EJBException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CurrentSchoolBean extends BaseEJB {
+    public void createCurrentSchool(long coderId, String schoolName, String dataSource) throws EJBException {
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        InitialContext ctx = null;
+
+        try {
+            StringBuffer query = new StringBuffer(1024);
+            query.append("INSERT ");
+            query.append(  "INTO current_school (coder_id, school_name) ");
+            query.append("VALUES (?, ?)");
+
+            ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup(dataSource);
+            conn = ds.getConnection();
+            ps = conn.prepareStatement(query.toString());
+            ps.setLong(1, coderId);
+            ps.setString(2, schoolName);
+
+            int rc = ps.executeUpdate();
+            if (rc != 1) {
+                throw(new EJBException("Wrong number of rows inserted into " +
+                        "'current_school'. Inserted " + rc + ", should have " +
+                        "inserted 1."));
+            }
+        } catch (SQLException sqle) {
+            DBMS.printSqlException(true, sqle);
+            throw(new EJBException(sqle.getMessage()));
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } finally {
+            close(ps);
+            close(conn);
+            close(ctx);
+        }
+
+    }
+}
