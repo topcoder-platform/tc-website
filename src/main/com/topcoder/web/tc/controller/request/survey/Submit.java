7c1366203ad683aece9c2796e50c976f12b88659
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-09 21:40:55 +0000

    use new methods in response bean

--- src/main/com/topcoder/web/tc/controller/request/survey/Submit.java
+++ src/main/com/topcoder/web/tc/controller/request/survey/Submit.java
@@ -50,11 +50,10 @@ public class Submit extends View {
                     for (Iterator it = responses.iterator(); it.hasNext();) {
                         resp = (SurveyResponse) it.next();
                         hasAllFreeForm &= resp.isFreeForm();
-                        response.createResponse(resp.getUserId(), resp.getQuestionId());
                         if (resp.isFreeForm()) {
-                            response.setResponseText(resp.getUserId(), resp.getQuestionId(), resp.getText());
+                            response.createResponse(resp.getUserId(), resp.getQuestionId(), resp.getText());
                         } else {
-                            response.setAnswerId(resp.getUserId(), resp.getQuestionId(), resp.getAnswerId());
+                            response.createResponse(resp.getUserId(), resp.getQuestionId(), resp.getAnswerId());
                         }
                     }
                     Transaction.commit(tx);
