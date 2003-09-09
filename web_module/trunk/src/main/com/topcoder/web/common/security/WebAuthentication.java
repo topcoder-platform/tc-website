9fde3d56ec2cbb725c97925be98525f983e7f1ba
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-09 03:34:52 +0000

    add cookie path stuff

--- web_module/trunk/src/main/com/topcoder/web/common/security/WebAuthentication.java
+++ web_module/trunk/src/main/com/topcoder/web/common/security/WebAuthentication.java
@@ -2,6 +2,8 @@ package com.topcoder.web.common.security;
 
 import com.topcoder.shared.security.Authentication;
 import com.topcoder.shared.security.User;
+import com.topcoder.shared.security.Resource;
+import com.topcoder.shared.security.LoginException;
 
 /**
  * interface for classes that are doing web related authentication and would like to use cookies.
@@ -10,6 +12,28 @@ import com.topcoder.shared.security.User;
  * @author Greg Paul
  */
 public interface WebAuthentication extends Authentication {
-    /** this looks at the cookie first, whereas inherited getUser() looks at the session only */
-    public User getActiveUser();
+    /** this looks at the cookie first, whereas inherited getUser() looks at the session only
+     * implementing classes should provide some method of setting a default cookie path
+     */
+    User getActiveUser();
+
+    /** this looks at the cookie first, whereas inherited getUser() looks at the session only
+     * use the specified resource to determine the path for the cookie
+     */
+    User getActiveUser(Resource r);
+
+    /**
+     * log in with the specified resource for a path in the cookie
+     * @param r
+     * @param u
+     * @throws LoginException
+     */
+    void login(Resource r, User u) throws LoginException;
+
+    /** log out with the specified resource for a path in the cookie
+     *
+     * @param r
+     */
+    void logout(Resource r);
+
 }
