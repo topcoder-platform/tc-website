9db6511ac55ccd74db9f01a8c3fd5ad85389dcda
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-09 21:41:18 +0000

    no pkey, so must have complete create methods

--- web_module/trunk/src/main/com/topcoder/web/ejb/survey/ResponseBean.java
+++ web_module/trunk/src/main/com/topcoder/web/ejb/survey/ResponseBean.java
@@ -20,8 +20,8 @@ public class ResponseBean extends BaseEJB {
     private final static String DATA_SOURCE = "java:comp/env/datasource_name";
     private final static String JTS_DATA_SOURCE = "java:comp/env/jts_datasource_name";
 
-    public void createResponse(long userId, long questionId) {
-        log.debug("createResponse called... user_id="+userId+" questionId="+questionId);
+    public void createResponse(long userId, long questionId, long answerId) {
+        log.debug("createResponse called... user_id="+userId+" questionId="+questionId+" answerId="+answerId);
 
         Context ctx = null;
         PreparedStatement ps = null;
@@ -34,9 +34,10 @@ public class ResponseBean extends BaseEJB {
             ds = (DataSource) ctx.lookup(JTS_DATA_SOURCE);
             conn = ds.getConnection();
 
-            ps = conn.prepareStatement("INSERT INTO response (user_id, question_id) VALUES (?,?)");
+            ps = conn.prepareStatement("INSERT INTO response (user_id, question_id, answer_id) VALUES (?,?,?)");
             ps.setLong(1, userId);
             ps.setLong(2, questionId);
+            ps.setLong(3, answerId);
 
             int rows = ps.executeUpdate();
 
@@ -44,12 +45,12 @@ public class ResponseBean extends BaseEJB {
                 throw new EJBException("Wrong number of rows in insert: " + rows);
         } catch (SQLException sqe) {
             DBMS.printSqlException(true, sqe);
-            throw new EJBException("SQLException creating response user_id="+userId+" questionId="+questionId);
+            throw new EJBException("SQLException creating response user_id="+userId+" questionId="+questionId+" answerId="+answerId);
         } catch (NamingException e) {
-            throw new EJBException("NamingException creating response user_id="+userId+" questionId="+questionId);
+            throw new EJBException("NamingException creating response user_id="+userId+" questionId="+questionId+" answerId="+answerId);
         } catch (Exception e) {
-            throw new EJBException("Exception creating response user_id="+userId+" questionId="+
-                    questionId+":\n" + e.getMessage());
+            throw new EJBException("Exception creating response user_id="+userId+" questionId="+questionId+" answerId="+answerId+
+                    ":\n" + e.getMessage());
         } finally {
             close(ps);
             close(conn);
@@ -57,10 +58,8 @@ public class ResponseBean extends BaseEJB {
         }
     }
 
-
-
-    public void setAnswerId(long userId, long questionId, long answerId) {
-        log.debug("setAnswerId called... user_id="+userId+" questionId="+questionId+" answerId="+answerId);
+    public void createResponse(long userId, long questionId, String text) {
+        log.debug("createResponse called... user_id="+userId+" questionId="+questionId+" text="+text);
 
         Context ctx = null;
         PreparedStatement ps = null;
@@ -73,10 +72,10 @@ public class ResponseBean extends BaseEJB {
             ds = (DataSource) ctx.lookup(JTS_DATA_SOURCE);
             conn = ds.getConnection();
 
-            ps = conn.prepareStatement("UPDATE response SET answer_id = ? WHERE user_id = ? AND question_id = ?");
-            ps.setLong(1, answerId);
-            ps.setLong(2, userId);
-            ps.setLong(3, questionId);
+            ps = conn.prepareStatement("INSERT INTO response (user_id, question_id, text) VALUES (?,?,?)");
+            ps.setLong(1, userId);
+            ps.setLong(2, questionId);
+            ps.setBytes(3, DBMS.serializeTextString(text));
 
             int rows = ps.executeUpdate();
 
@@ -84,12 +83,12 @@ public class ResponseBean extends BaseEJB {
                 throw new EJBException("Wrong number of rows in insert: " + rows);
         } catch (SQLException sqe) {
             DBMS.printSqlException(true, sqe);
-            throw new EJBException("SQLException updating user_id="+userId+" questionId="+questionId+" answerId="+answerId);
+            throw new EJBException("SQLException creating response user_id="+userId+" questionId="+questionId+" text="+text);
         } catch (NamingException e) {
-            throw new EJBException("NamingException updating user_id="+userId+" questionId="+questionId+" answerId="+answerId);
+            throw new EJBException("NamingException creating response user_id="+userId+" questionId="+questionId+" text="+text);
         } catch (Exception e) {
-            throw new EJBException("Exception updating user_id="+userId+" questionId="+questionId+" answerId="+
-                    answerId+":\n" + e.getMessage());
+            throw new EJBException("Exception creating response user_id="+userId+" questionId="+questionId+" text="+text +
+                    ":\n" + e.getMessage());
         } finally {
             close(ps);
             close(conn);
@@ -97,42 +96,6 @@ public class ResponseBean extends BaseEJB {
         }
     }
 
-    public void setResponseText(long userId, long questionId, String text) {
-        log.debug("createResponse called... user_id="+userId+" questionId="+questionId);
-
-        Context ctx = null;
-        PreparedStatement ps = null;
-        Connection conn = null;
-        DataSource ds = null;
-
-        try {
-            ctx = new InitialContext();
-
-            ds = (DataSource) ctx.lookup(JTS_DATA_SOURCE);
-            conn = ds.getConnection();
-
-            ps = conn.prepareStatement("UPDATE response set response = ? WHERE user_id = ? AND question_id = ?");
-            ps.setBytes(1, DBMS.serializeTextString(text));
-            ps.setLong(2, userId);
-            ps.setLong(3, questionId);
-
-            int rows = ps.executeUpdate();
-
-            if (rows != 1)
-                throw new EJBException("Wrong number of rows in insert: " + rows);
-        } catch (SQLException sqe) {
-            DBMS.printSqlException(true, sqe);
-            throw new EJBException("SQLException creating user_id="+userId+" questionId="+questionId);
-        } catch (NamingException e) {
-            throw new EJBException("NamingException creating user_id="+userId+" questionId="+questionId);
-        } catch (Exception e) {
-            throw new EJBException("Exception creating user_id="+userId+" questionId="+questionId+":\n" + e.getMessage());
-        } finally {
-            close(ps);
-            close(conn);
-            close(ctx);
-        }
-    }
 
 
     public boolean exists(long userId, long questionId) {
