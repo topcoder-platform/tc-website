fbf36226cf910bef5758a1daebc0ef36058cb307
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-11 16:39:05 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/GoogleRegSubmit.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/GoogleRegSubmit.java
@@ -24,40 +24,60 @@ public class GoogleRegSubmit extends FullRegSubmit {
 
     protected void handleActivation(SimpleRegInfo info, UserPrincipal newUser) throws TCWebException {
         try {
-            User user = (User)createEJB(getInitialContext(), User.class);
             StringBuffer buf = new StringBuffer(1000);
+            User user = (User) createEJB(getInitialContext(), User.class);
+            String code = user.getActivationCode(newUser.getId(), db);
 
             TCSEmailMessage mail = new TCSEmailMessage();
             mail.setSubject("IMPORTANT - Google Event Activation Email");
 
-            buf.append("wanna activate your account? click here:\n\n");
-            buf.append("http://");
-            buf.append(ApplicationServer.SERVER_NAME);
-            SessionInfo sInfo = (SessionInfo)getRequest().getAttribute(BaseServlet.SESSION_INFO_KEY);
-            buf.append(sInfo.getServletPath());
-            buf.append("?");
-            buf.append(Constants.MODULE_KEY);
-            buf.append("=");
-            buf.append(Constants.GOOGLE_ACTIVATION);
-            buf.append("&");
-            buf.append(Activate.ACTIVATION_CODE);
-            buf.append("=");
-            buf.append(user.getActivationCode(newUser.getId(), db));
-            buf.append("&");
-            buf.append(Constants.COMPANY_ID);
-            buf.append("=");
-            buf.append(regInfo.getCompanyId());
-
+            buf.append("GOOGLE EVENT ACTIVATION INFORMATION\n\n");
+            buf.append("Your Google activation code is ");
+            buf.append(code);
+            buf.append("\n\n");
+            buf.append("To activate your account, navigate to the following WWW URL:\n\n");
+            buf.append(getUrl(code));
+            buf.append("If you cannot click on the web address above, please copy the address into your web browser to continue.  If the address spans two lines, please make sure you copy and paste both sections without any spaces between them.\n\n");
+            buf.append("Your handle and password will provide you with access to the Google Competition Arena, where you can practice and chat, and where you'll compete in the Google Extreme Coding Challenge.\n\n");
+            buf.append("PRACTICING FOR THE EVENT\n");
+            buf.append("There is a practice room available to you that will allow you to become acclimated with the competition environment before you participate in the Google Extreme Coding Challenge.  The practice room contains a problem set that will be very similar in nature to the problems you will encounter during competition.  The practice room will be available from 8:00AM EDT on October 1, 2003 through 5:00PM EDT on October 15, 2003.\n\n");
+            buf.append("You can access the practice rooms by navigating to http://www.topcoder.com/google and clicking on the Practice Now tab.\n\n");
+            buf.append("LAUNCHING THE ARENA\n");
+            buf.append("You can launch and login to the competition arena at the appropriate time by navigating to http://www.topcoder.com/google and clicking on the Compete Now tab.\n\n");
+            buf.append("Windows, Linux and Unix users need to have the Java 1.4.x runtime installed in order to launch the arena.\n\n");
+            buf.append("Mac OS X  users need to have the Java 1.4.x runtime installed, which requires OS X version 10.2.x.\n\n");
+            buf.append("We also suggest that you read up on the competition process by navigating to http://www.topcoder.com/google and downloading the competition manual.\n\n");
+            buf.append("If you have any questions about how to participate, please email them to service@topcoder.com\n\n");
+            buf.append("Thank you for registering for the Google Extreme Coding Challenge.  We look forward to seeing you in the arena!\n\n");
 
             mail.setBody(buf.toString());
             mail.addToAddress(info.getEmail(), TCSEmailMessage.TO);
             mail.setFromAddress("service@topcoder.com");
             log.info("sent registration email to " + info.getEmail());
             EmailEngine.send(mail);
-
-
         } catch (Exception e) {
             throw new TCWebException(e);
         }
+    }
+
+    private String getUrl(String code) {
+        StringBuffer buf = new StringBuffer(200);
+        buf.append("http://");
+        buf.append(ApplicationServer.SERVER_NAME);
+        SessionInfo sInfo = (SessionInfo) getRequest().getAttribute(BaseServlet.SESSION_INFO_KEY);
+        buf.append(sInfo.getServletPath());
+        buf.append("?");
+        buf.append(Constants.MODULE_KEY);
+        buf.append("=");
+        buf.append(Constants.GOOGLE_ACTIVATION);
+        buf.append("&");
+        buf.append(Activate.ACTIVATION_CODE);
+        buf.append("=");
+        buf.append(code);
+        buf.append("&");
+        buf.append(Constants.COMPANY_ID);
+        buf.append("=");
+        buf.append(regInfo.getCompanyId());
+        return buf.toString();
     }
 }
