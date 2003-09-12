82c977c291a948a12efa56f47253a629c420ad44
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-12 20:27:36 +0000

    implement the login method to go to the main tc web app

--- src/main/com/topcoder/web/query/servlet/Controller.java
+++ src/main/com/topcoder/web/query/servlet/Controller.java
@@ -1,10 +1,14 @@
 package com.topcoder.web.query.servlet;
 
 import com.topcoder.web.common.BaseServlet;
+import com.topcoder.web.common.SessionInfo;
 import com.topcoder.web.query.common.Constants;
+import com.topcoder.shared.util.logging.Logger;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 /**
@@ -15,9 +19,28 @@ import javax.servlet.ServletException;
  */
 
 public class Controller extends BaseServlet {
+
+    private static Logger log = Logger.getLogger(BaseServlet.class);
+
     public synchronized void init(ServletConfig config) throws ServletException {
         super.init(config);
         Constants.init(config);
+    }
+
+
+
+    protected void handleLogin(HttpServletRequest request, HttpServletResponse response, SessionInfo info) throws Exception {
+        /* forward to the login page, with a message and a way back */
+        request.setAttribute(MESSAGE_KEY, "In order to continue, you must provide your user name " +
+                "and password.");
+        request.setAttribute(NEXT_PAGE_KEY, info.getRequestString());
+
+        request.setAttribute(MODULE, LOGIN_PROCESSOR);
+
+        String dest = LOGIN_SERVLET == null ? info.getServletPath() : LOGIN_SERVLET;
+
+        log.debug("forwarding to " + dest);
+        getServletContext().getContext("/").getRequestDispatcher(response.encodeURL(dest)).forward(request, response);
     }
 
 }
