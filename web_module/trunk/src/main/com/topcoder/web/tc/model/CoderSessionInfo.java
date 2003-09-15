d431c65b64fb88155c8793b6842ee882e9b8023d
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-15 19:48:26 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/model/CoderSessionInfo.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/model/CoderSessionInfo.java
@@ -34,28 +34,19 @@ public class CoderSessionInfo extends SessionInfo {
 
     public CoderSessionInfo(HttpServletRequest request, WebAuthentication authentication, Set groups) throws Exception {
         super(request, authentication, groups);
-        log.debug("in codersessioninfo constructor");
         rating = 0;
         rank = 0;
         if (!authentication.getActiveUser().isAnonymous()) {
-            log.debug("1");
             ResultSetContainer info = getInfo(authentication.getActiveUser().getId());
-            log.debug("2");
             if (!info.isEmpty()) {
-                log.debug("3a1");
                 rating = info.getIntItem(0, "rating");
-                log.debug("3a2");
                 hasImage = info.getIntItem(0, "has_image")>0;
-                log.debug("3a3");
                 activationCode = info.getStringItem(0, "activation_code");
-                log.debug("3a4");
             } else {
                 log.warn("couldn't find session info for: " + authentication.getActiveUser().getId());
             }
-            log.debug("4");
             ResultSetContainer rsc = getDwInfo(authentication.getActiveUser().getId());
             if (!rsc.isEmpty()) {
-                log.debug("5");
                 rank = rsc.getIntItem(0, "rank");
             } else {
                 log.debug("couldn't find rank info for: " + authentication.getActiveUser().getId());
