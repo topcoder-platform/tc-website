77481d4bc516043a5c2642ce98c32724a07b110f
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-17 15:17:16 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/jsp/tcscript.js
+++ web_module/trunk/src/main/com/topcoder/web/jsp/tcscript.js
@@ -50,29 +50,6 @@ function infoWindow(url) {
 
 
 
-var months=new Array(13);
-months[1]="January";
-months[2]="February";
-months[3]="March";
-months[4]="April";
-months[5]="May";
-months[6]="June";
-months[7]="July";
-months[8]="August";
-months[9]="September";
-months[10]="October";
-months[11]="November";
-months[12]="December";
-var time=new Date();
-var lmonth=months[time.getMonth() + 1];
-var date=time.getDate();
-var year=time.getYear();
-if (year < 2000)
-year = year + 1900;
-document.write(" " + lmonth + " ");
-document.write(date + ", " + year + " ");
-
-
 function goTo(selection) {
   sel = selection.options[selection.selectedIndex].value;
   if (sel == "arena")
