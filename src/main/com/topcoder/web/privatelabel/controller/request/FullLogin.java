96ca3fb8436d4e7caa349d0a3d9f1dd87ae3e4b9
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-18 20:59:07 +0000

    *** empty log message ***

---
+++ src/main/com/topcoder/web/privatelabel/controller/request/FullLogin.java
@@ -0,0 +1,74 @@
+package com.topcoder.web.privatelabel.controller.request;
+
+import com.topcoder.web.privatelabel.Constants;
+import com.topcoder.shared.security.SimpleUser;
+import com.topcoder.shared.security.LoginException;
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+
+import java.util.Arrays;
+
+abstract class FullLogin extends FullReg {
+
+    protected static final char[] INACTIVE_STATI = {'I', '0', '9', '6', '5', '4'};
+    protected static final char[] UNACTIVE_STATI = {'U', '2'};
+    protected static final char[] ACTIVE_STATI = {'1', 'A'};
+
+    static {
+        //sort them so that one can use Arrays.binarySearch to figure out if a particular
+        //status is in the list
+        Arrays.sort(INACTIVE_STATI);
+        Arrays.sort(UNACTIVE_STATI);
+        Arrays.sort(ACTIVE_STATI);
+    }
+
+    protected boolean hasTopCoderAccount() throws Exception {
+        String handle = getRequestParameter(Constants.HANDLE);
+        String password = getRequestParameter(Constants.PASSWORD);
+
+        boolean ret = false;
+        long userId = getUserId(handle);
+        char status = getStatus(userId);
+        if (Arrays.binarySearch(ACTIVE_STATI, status)>0) {
+            try {
+                getAuthentication().login(new SimpleUser(0, handle, password));
+                ret = true;
+            } catch(LoginException l) {
+                addError(Constants.HANDLE, l.getMessage());
+            }
+        } else {
+            addError(Constants.HANDLE, "Account status not active.");
+        }
+
+        return ret;
+    }
+
+    /**
+     * shouldn't use ejb slooooooooow
+     * @param userId
+     * @return
+     * @throws Exception if user doesn't exist or some other ejb problem
+     */
+    private char getStatus(long userId) throws Exception {
+        char result;
+        com.topcoder.web.ejb.user.User user = (com.topcoder.web.ejb.user.User)createEJB(getInitialContext(), com.topcoder.web.ejb.user.User.class);
+        result = user.getStatus(userId, DBMS.COMMON_OLTP_DATASOURCE_NAME);
+        return result;
+
+    }
+
+    protected long getUserId(String handle) throws Exception {
+        Request r = new Request();
+        r.setContentHandle("user_id_using_handle");
+        r.setProperty("ha", handle);
+        ResultSetContainer rsc = (ResultSetContainer)getDataAccess(DBMS.OLTP_DATASOURCE_NAME).getData(r).get("user_id");
+        if (rsc.isEmpty())
+            return -1;
+        else return rsc.getLongItem(0, "user_id");
+    }
+
+
+    abstract protected boolean hasCompanyAccount() throws Exception;
+
+}
