e495009ecc7b103df1f0f87cb797c059950b6fa2
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-19 21:19:15 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/ejb/school/CurrentSchool.java
+++ src/main/com/topcoder/web/ejb/school/CurrentSchool.java
@@ -5,6 +5,12 @@ import javax.ejb.EJBException;
 import java.rmi.RemoteException;
 
 public interface CurrentSchool extends EJBObject {
-    void createCurrentSchool(long coderId, String schoolName, long schoolId, String dataSource)
+
+    void createCurrentSchool(long coderId, String dataSource)
             throws EJBException, RemoteException;
+
+    void setSchoolName(long coderId, String schoolName, String dataSource) throws EJBException, RemoteException;
+
+    void setSchoolId(long coderId, long schoolId, String dataSource) throws EJBException, RemoteException;
+
 }
