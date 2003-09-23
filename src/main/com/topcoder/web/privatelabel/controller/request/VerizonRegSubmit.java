76be4919814c34612bd32fb1a59626e18c5f5064
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-23 13:40:28 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/privatelabel/controller/request/VerizonRegSubmit.java
+++ src/main/com/topcoder/web/privatelabel/controller/request/VerizonRegSubmit.java
@@ -34,39 +34,41 @@ public class VerizonRegSubmit extends FullRegSubmit {
 
     protected void handleActivation(SimpleRegInfo info, UserPrincipal newUser) throws TCWebException {
         try {
-            StringBuffer buf = new StringBuffer(1000);
-            User user = (User) createEJB(getInitialContext(), User.class);
-            String code = user.getActivationCode(newUser.getId(), db);
+            if (isEligible()) {
+                StringBuffer buf = new StringBuffer(1000);
+                User user = (User) createEJB(getInitialContext(), User.class);
+                String code = user.getActivationCode(newUser.getId(), db);
 
-            TCSEmailMessage mail = new TCSEmailMessage();
-            mail.setSubject("IMPORTANT - Google Code Jam 2003 Activation Email");
+                TCSEmailMessage mail = new TCSEmailMessage();
+                mail.setSubject("IMPORTANT - Google Code Jam 2003 Activation Email");
 
-            buf.append("You're invited to take part in the Google Code Jam 2003, a programming competition that will be worthy of your time, your skills and your interest.\n\n");
-            buf.append("GOOGLE CODE JAM 2003 ACTIVATION INFORMATION\n\n");
-            buf.append("Your Google activation code is ");
-            buf.append(code);
-            buf.append("\n\n");
-            buf.append("To activate your account, navigate to the following WWW URL:\n\n");
-            buf.append(getUrl(code));
-            buf.append("\n\n");
-            buf.append("If you cannot click on the web address above, please copy the address into your web browser to continue.  If the address spans two lines, please make sure you copy and paste both sections without any spaces between them.\n\n");
-            buf.append("Your handle and password will provide you with access to the Google Competition Arena, where you can practice and chat, and where you'll compete in the Google Code Jam 2003.\n\n");
-            buf.append("PRACTICING FOR THE EVENT\n");
-            buf.append("There is a practice room available to you that will allow you to become acclimated with the competition environment before you participate in the Google Code Jam 2003.  The practice room contains a problem set that will be very similar in nature to the problems you will encounter during competition.  The practice room will be available from 8:00AM EDT on October 1, 2003 through 5:00PM EDT on November 14, 2003.\n\n");
-            buf.append("You can access the practice rooms by navigating to http://www.topcoder.com/google and clicking on the Practice Arena tab.\n\n");
-            buf.append("LAUNCHING THE ARENA\n");
-            buf.append("You can launch and login to the competition arena at the appropriate time by navigating to http://www.topcoder.com/google and clicking on the Compete Now tab.\n\n");
-            buf.append("Windows, Linux and Unix users need to have the Java 1.4.x runtime installed in order to launch the arena.\n\n");
-            buf.append("Mac OS X  users need to have the Java 1.4.x runtime installed, which requires OS X version 10.2.x.\n\n");
-            buf.append("We also suggest that you read up on the competition process by navigating to http://www.topcoder.com/google and downloading the competition manual.\n\n");
-            buf.append("If you have any questions about how to participate, please email them to service@topcoder.com\n\n");
-            buf.append("Thank you for registering for the Google Code Jam 2003.  We look forward to seeing you in the Arena!\n\n");
+                buf.append("You're invited to take part in the Google Code Jam 2003, a programming competition that will be worthy of your time, your skills and your interest.\n\n");
+                buf.append("GOOGLE CODE JAM 2003 ACTIVATION INFORMATION\n\n");
+                buf.append("Your Google activation code is ");
+                buf.append(code);
+                buf.append("\n\n");
+                buf.append("To activate your account, navigate to the following WWW URL:\n\n");
+                buf.append(getUrl(code));
+                buf.append("\n\n");
+                buf.append("If you cannot click on the web address above, please copy the address into your web browser to continue.  If the address spans two lines, please make sure you copy and paste both sections without any spaces between them.\n\n");
+                buf.append("Your handle and password will provide you with access to the Google Competition Arena, where you can practice and chat, and where you'll compete in the Google Code Jam 2003.\n\n");
+                buf.append("PRACTICING FOR THE EVENT\n");
+                buf.append("There is a practice room available to you that will allow you to become acclimated with the competition environment before you participate in the Google Code Jam 2003.  The practice room contains a problem set that will be very similar in nature to the problems you will encounter during competition.  The practice room will be available from 8:00AM EDT on October 1, 2003 through 5:00PM EDT on November 14, 2003.\n\n");
+                buf.append("You can access the practice rooms by navigating to http://www.topcoder.com/google and clicking on the Practice Arena tab.\n\n");
+                buf.append("LAUNCHING THE ARENA\n");
+                buf.append("You can launch and login to the competition arena at the appropriate time by navigating to http://www.topcoder.com/google and clicking on the Compete Now tab.\n\n");
+                buf.append("Windows, Linux and Unix users need to have the Java 1.4.x runtime installed in order to launch the arena.\n\n");
+                buf.append("Mac OS X  users need to have the Java 1.4.x runtime installed, which requires OS X version 10.2.x.\n\n");
+                buf.append("We also suggest that you read up on the competition process by navigating to http://www.topcoder.com/google and downloading the competition manual.\n\n");
+                buf.append("If you have any questions about how to participate, please email them to service@topcoder.com\n\n");
+                buf.append("Thank you for registering for the Google Code Jam 2003.  We look forward to seeing you in the Arena!\n\n");
 
-            mail.setBody(buf.toString());
-            mail.addToAddress(info.getEmail(), TCSEmailMessage.TO);
-            mail.setFromAddress("service@topcoder.com");
-            log.info("sent registration email to " + info.getEmail());
-            EmailEngine.send(mail);
+                mail.setBody(buf.toString());
+                mail.addToAddress(info.getEmail(), TCSEmailMessage.TO);
+                mail.setFromAddress("service@topcoder.com");
+                log.info("sent registration email to " + info.getEmail());
+                EmailEngine.send(mail);
+            }
         } catch (Exception e) {
             throw new TCWebException(e);
         }
@@ -92,12 +94,6 @@ public class VerizonRegSubmit extends FullRegSubmit {
         buf.append(regInfo.getCompanyId());
         return buf.toString();
     }
-
-
-
-
-
-
 
     /**
      *
