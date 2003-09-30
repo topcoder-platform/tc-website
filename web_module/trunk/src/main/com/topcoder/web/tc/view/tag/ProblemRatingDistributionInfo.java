d78565e7dd1357b981daa2175e51c8c9ec57c3b6
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-09-30 13:51:45 +0000

    *** empty log message ***

---
+++ web_module/trunk/src/main/com/topcoder/web/tc/view/tag/ProblemRatingDistributionInfo.java
@@ -0,0 +1,22 @@
+package com.topcoder.web.tc.view.tag;
+
+import javax.servlet.jsp.tagext.TagExtraInfo;
+import javax.servlet.jsp.tagext.VariableInfo;
+import javax.servlet.jsp.tagext.TagData;
+
+public class ProblemRatingDistributionInfo extends TagExtraInfo {
+
+        public VariableInfo[] getVariableInfo(TagData data) {
+            return new VariableInfo[]{
+                new VariableInfo(data.getId(),
+                        "com.topcoder.web.tc.model.ProblemRatingDistribution",
+                        true,
+                        VariableInfo.NESTED)
+            };
+        }
+
+    }
+
+
+
+}
