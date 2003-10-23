e47c41e403997199922a6e406cb6d3d4b93f15f1
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-10-23 18:53:45 +0000

    sort

--- src/main/com/topcoder/web/tc/controller/request/statistics/Simple.java
+++ src/main/com/topcoder/web/tc/controller/request/statistics/Simple.java
@@ -7,6 +7,8 @@ import com.topcoder.shared.util.TCResourceBundle;
 import com.topcoder.shared.util.DBMS;
 import com.topcoder.shared.dataAccess.Request;
 import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.DataAccessConstants;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 
 import javax.servlet.http.HttpUtils;
 import java.util.Map;
@@ -21,10 +23,20 @@ public class Simple extends Base {
         Request dataRequest = new Request();
         Map map = HttpUtils.parseQueryString(getRequest().getQueryString());
         map.remove(Constants.MODULE_KEY); //no need to include this one
+        map.remove(DataAccessConstants.SORT_COLUMN);
+        map.remove(DataAccessConstants.SORT_DIRECTION);
+
         try {
             dataRequest.setProperties(map);
             DataAccessInt dai = getDataAccess(getDb(), true);
             Map result = dai.getData(dataRequest);
+
+            ResultSetContainer rsc = (ResultSetContainer)result.get(dataRequest.getContentHandle());
+            String sortCol = getRequest().getParameter(DataAccessConstants.SORT_COLUMN);
+            String sortDir = getRequest().getParameter(DataAccessConstants.SORT_DIRECTION);
+            if (sortCol != null && sortDir != null && rsc != null)
+                rsc.sortByColumn(sortCol, sortDir.trim().toLowerCase().equals("asc"));
+
             getRequest().setAttribute("resultMap", result);
 
             String nextPage = bundle.getProperty(dataRequest.getContentHandle());
