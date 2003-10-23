0535c3b27331cdc5dd8260df8e4db61220b9095b
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-10-23 15:32:25 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/statistics/Simple.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/statistics/Simple.java
@@ -1,7 +1,9 @@
 package com.topcoder.web.tc.controller.request.statistics;
 
 import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.common.StringUtils;
 import com.topcoder.web.tc.Constants;
+import com.topcoder.shared.util.TCResourceBundle;
 import com.topcoder.shared.util.DBMS;
 import com.topcoder.shared.dataAccess.Request;
 import com.topcoder.shared.dataAccess.DataAccessInt;
@@ -11,16 +13,26 @@ import java.util.Map;
 
 public class Simple extends Base {
 
+    private static TCResourceBundle bundle = new TCResourceBundle("TC");
+
+    public static final String TRANS_FLAG = "trans";
+
     protected void businessProcessing() throws TCWebException {
         Request dataRequest = new Request();
         Map map = HttpUtils.parseQueryString(getRequest().getQueryString());
         map.remove(Constants.MODULE_KEY); //no need to include this one
         try {
             dataRequest.setProperties(map);
-            DataAccessInt dwdai = getDataAccess(DBMS.DW_DATASOURCE_NAME, true);
-            Map result = dwdai.getData(dataRequest);
+            DataAccessInt dai = getDataAccess(getDb(), true);
+            Map result = dai.getData(dataRequest);
             getRequest().setAttribute("resultMap", result);
-            setNextPage(Constants.STATISTICS_PATH+dataRequest.getContentHandle()+".jsp");
+
+            String nextPage = bundle.getProperty(dataRequest.getContentHandle());
+            if (nextPage==null) {
+                setNextPage(Constants.STATISTICS_PATH+dataRequest.getContentHandle()+".jsp");
+            } else {
+                setNextPage(nextPage);
+            }
             setIsNextPageInContext(true);
 
         } catch (TCWebException e) {
@@ -30,5 +42,15 @@ public class Simple extends Base {
         }
 
     }
+
+    protected String getDb() {
+        String ret = DBMS.DW_DATASOURCE_NAME;
+        String flag = StringUtils.checkNull(getRequest().getParameter(TRANS_FLAG));
+        if (flag.toLowerCase().trim().equals("true")) {
+            ret = DBMS.OLTP_DATASOURCE_NAME;
+        }
+        return ret;
+    }
+
 
 }
