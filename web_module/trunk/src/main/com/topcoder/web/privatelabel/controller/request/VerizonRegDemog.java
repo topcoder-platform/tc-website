840ae461b06bb73f0248aa9b6a4e89ca5bb8fb98
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-10-28 14:53:55 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/VerizonRegDemog.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/VerizonRegDemog.java
@@ -1,6 +1,8 @@
 package com.topcoder.web.privatelabel.controller.request;
 
 import com.topcoder.web.privatelabel.Constants;
+import com.topcoder.web.privatelabel.model.SimpleRegInfo;
+import com.topcoder.web.privatelabel.model.VerizonRegInfo;
 
 public class VerizonRegDemog extends FullRegDemog {
     protected void setNextPage() {
@@ -11,4 +13,13 @@ public class VerizonRegDemog extends FullRegDemog {
         }
         setIsNextPageInContext(true);
     }
+
+    protected SimpleRegInfo makeRegInfo() throws Exception {
+        SimpleRegInfo ret = super.makeRegInfo();
+        if (!(ret instanceof VerizonRegInfo)) {
+            ret = new VerizonRegInfo(ret);
+        }
+        return ret;
+   }
+
 }
