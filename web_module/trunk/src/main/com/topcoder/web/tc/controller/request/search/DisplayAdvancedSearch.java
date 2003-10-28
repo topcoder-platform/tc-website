73e3529388907c8d7437dcc2e143d2cca9157534
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-10-28 17:58:24 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/search/DisplayAdvancedSearch.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/search/DisplayAdvancedSearch.java
@@ -5,11 +5,12 @@ import com.topcoder.web.tc.Constants;
 import com.topcoder.web.tc.model.MemberSearch;
 import com.topcoder.web.common.TCWebException;
 
-public class DisplayAdvancedSearch extends SimpleSearch {
+public class DisplayAdvancedSearch extends AdvancedSearch {
     protected void businessProcessing() throws TCWebException {
         MemberSearch m = new MemberSearch();
         m.setStateList(getStateList());
         m.setCountryList(getCountryList());
+        super.setDefaults(m);
         getRequest().setAttribute("memberSearch", m);
         setNextPage(Constants.ADVANCED_SEARCH);
         setIsNextPageInContext(true);
