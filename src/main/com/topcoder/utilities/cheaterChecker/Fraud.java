43cdad75ce3a6844ed5f25d5b8d312357420fa78
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-10-31 15:39:11 +0000

    *** empty log message ***

--- src/main/com/topcoder/utilities/cheaterChecker/Fraud.java
+++ src/main/com/topcoder/utilities/cheaterChecker/Fraud.java
@@ -1,7 +1,7 @@
 package com.topcoder.utilities.cheaterChecker;
 
 public interface Fraud {
-    final static int MAX_REPORT = 20;
+    final static int MAX_REPORT = 100;
     String getReport();
     void execute() throws Exception;
 }
