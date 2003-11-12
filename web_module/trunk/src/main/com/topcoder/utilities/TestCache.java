39d0ed5f03406b51d1551f93bb306e5d13b8d34f
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-11-12 21:36:50 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/utilities/TestCache.java
+++ web_module/trunk/src/main/com/topcoder/utilities/TestCache.java
@@ -6,6 +6,7 @@ import com.topcoder.shared.util.logging.Logger;
 
 public class TestCache {
     private static Logger log = Logger.getLogger(TestCache.class);
+
     public static void main(String[] args) {
         CacheClient cc = null;
         try {
@@ -15,33 +16,36 @@ public class TestCache {
         }
         String s = new String("af;lkjadf;lkjasdlkfjasl;kfjaslkfjas;lkdjfas;lkfjas;lkdjflaksdjf;laksdjf;lkadsjfl;kasjf;lkasjfl;kasjfl;kasjf;lkasdjflkasjflk;sdjal;kfjasd;lkfjasdlk;fjas;lkfjasdk;lfjlkasjf;laksjf;klasdjf;lkasjf;lkasdjfl;kasjf;lkasjf;lkasjf;klasjdf;klasjdf;lkajs;lfkjsa;lkfjaslk;fjaslkfjalskfja;klsdjfa;klsjf;alskdjf;lkasjfl;kadsfja;lksjf;lkasdjf;laksjf;lkasjf;lkasdjf;lkadsjf;lkasdjfl;kasdjf;lkasdjf;l");
         StringBuffer big = new StringBuffer(10000);
-        for (int i=0; i<100000; i++) {
+        for (int i = 0; i < 100000; i++) {
             big.append(String.valueOf(i));
         }
         try {
             int max = 1000;
-            /*
+
             log.debug("add " + max + " items to the cache, cache for 50 hours");
-            for (int i=0; i<max; i++) {
-                cc.set("mykey"+i, s, 1000*60*60*50);
+            for (int i = 0; i < max; i++) {
+                cc.set("mykey" + i, s, 1000 * 60 * 60 * 50);
                 log.debug("added item " + i);
             }
-            log.debug("add " + 10*max + " items to the cache, and get every 5th, cache for a minute");
-            for (int i=0; i<10*max; i++) {
-                cc.set("mynewkey"+i, s, 1000*60);
-                if (i%5==0) {
+            log.debug("add " + 10 * max + " items to the cache, and get every 5th, cache for a minute");
+            for (int i = 0; i < 10 * max; i++) {
+                cc.set("mynewkey" + i, s, 1000 * 60);
+                if (i % 5 == 0) {
                     log.debug("get key " + i);
-                    cc.get("mynewkey"+i);
+                    cc.get("mynewkey" + i);
                 }
-            } */
-            log.debug("add " + 10*max + " size " + big.toString().getBytes().length + " items to the cache, and get every 5th, cache for 50 hours");
-            for (int i=0; i<10*max; i++) {
-                cc.set("anothernewkey"+i, big.toString(), 1000*60*60*50);
-                if (i%5==0) {
+            }
+            /*
+            log.debug("add " + 10 * max + " size " + big.toString().getBytes().length + " items to the cache, and get every 5th, cache for 50 hours");
+            for (int i = 0; i < 10 * max; i++) {
+                cc.set("anothernewkey" + i, big.toString(), 1000 * 60 * 60 * 50);
+                if (i % 5 == 0) {
                     log.debug("get key " + i);
-                    cc.get("mynewkey"+i);
+                    cc.get("mynewkey" + i);
                 }
-            }        } catch (Exception e) {
+            }
+            */
+        } catch (Exception e) {
             log.error("error", e);
         }
     }
