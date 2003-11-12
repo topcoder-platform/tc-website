5f58e51b572bd49fc1723d8cbca4bcc792ce7aa3
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-11-12 03:26:31 +0000

    *** empty log message ***

---
+++ web_module/trunk/src/main/com/topcoder/utilities/TestCache.java
@@ -0,0 +1,29 @@
+package com.topcoder.utilities;
+
+import com.topcoder.shared.distCache.CacheClient;
+import com.topcoder.shared.distCache.CacheClientFactory;
+import com.topcoder.shared.util.logging.Logger;
+
+public class TestCache {
+    private static Logger log = Logger.getLogger(TestCache.class);
+    public static void main(String[] args) {
+        CacheClient cc = null;
+        try {
+            cc = CacheClientFactory.createCacheClient();
+        } catch (Exception e) {
+            log.error("error", e);
+        }
+        String s = new String("af;lkjadf;lkjasdlkfjasl;kfjaslkfjas;lkdjfas;lkfjas;lkdjflaksdjf;laksdjf;lkadsjfl;kasjf;lkasjfl;kasjfl;kasjf;lkasdjflkasjflk;sdjal;kfjasd;lkfjasdlk;fjas;lkfjasdk;lfjlkasjf;laksjf;klasdjf;lkasjf;lkasdjfl;kasjf;lkasjf;lkasjf;klasjdf;klasjdf;lkajs;lfkjsa;lkfjaslk;fjaslkfjalskfja;klsdjfa;klsjf;alskdjf;lkasjfl;kadsfja;lksjf;lkasdjf;laksjf;lkasjf;lkasdjf;lkadsjf;lkasdjfl;kasdjf;lkasdjf;l");
+        try {
+            int max = 1000;
+            log.debug("add " + max + " items to the cache");
+            for (int i=0; i<max; i++) {
+                cc.set("mykey"+i, s, 1000*60*30);
+                log.debug("added item " + i);
+            }
+        } catch (Exception e) {
+            log.error("error", e);
+        }
+    }
+
+}
