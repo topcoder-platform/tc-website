88832a2c371362b3d5db76e6904baa09f6c20961
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-11-13 18:31:13 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/legacy/TaskAffidavit.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/legacy/TaskAffidavit.java
@@ -53,7 +53,9 @@ public final class TaskAffidavit {
                 requestTask = Conversion.checkNull(request.getParameter("t"));
                 requestCommand = Conversion.checkNull(request.getParameter("c"));
 
-                if (requestCommand.equals("2003_open_submit_info") || requestCommand.equals("2003_comp_open_submit_info")) {
+                if (requestCommand.equals("2003_open_submit_info")
+                        || requestCommand.equals("2003_comp_open_submit_info")
+                        || requestCommand.equals("2003_crpf_submit_info")) {
 
                     TreeMap questions = new TreeMap();
                     TreeMap answers = new TreeMap();
@@ -89,6 +91,8 @@ public final class TaskAffidavit {
                     TCSEmailMessage mail = new TCSEmailMessage();
                     if (requestCommand.equals("2003_open_submit_info"))
                         mail.setSubject("Travel Info Response - " + nav.getUser().getHandle());
+                    else if (requestCommand.equals("2003_crpf_submit_info"))
+                        mail.setSubject("CRPF Travel Info Response - " + nav.getUser().getHandle());
                     else mail.setSubject("Component Travel Info Response - " + nav.getUser().getHandle());
 
                     mail.setBody(buf.toString());
