02c9ffa8f5a56d617668c74335537bd697872791
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-11-24 22:12:27 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/common/SessionInfo.java
+++ web_module/trunk/src/main/com/topcoder/web/common/SessionInfo.java
@@ -44,7 +44,7 @@ public class SessionInfo implements Serializable {
         String query = request.getQueryString();
         queryString = (query==null) ? ("") : ("?"+query);
         StringBuffer buf = new StringBuffer(200);
-        buf.append("http://");        //any better way than a hardcode?
+        buf.append("http://");        //todo any better way than a hardcode?
         buf.append(request.getServerName());
         buf.append(queryString);
         requestString = buf.toString();
