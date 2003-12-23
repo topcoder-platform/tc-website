ab6e43543366610963ace1c42dfa3cf3d40f473d
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-12-23 15:38:18 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/jsp/tcscript.js
+++ web_module/trunk/src/main/com/topcoder/web/jsp/tcscript.js
@@ -61,7 +61,7 @@ function infoWindow(url) {
   var left = (screen.availWidth - width) / 2;
   var top = (screen.availHeight-height)/2;
   OpenWin=this.open(url,"Info",
-            "toolbar=no,menubar=no,location=no,scrollbars=auto,resizable=yes"
+            "toolbar=no,menubar=no,location=no,scrollbars=yes,resizable=yes"
             + ",width=" + width + ",height=" + height
             + ",left=" + left + ",top=" + top);
 }
