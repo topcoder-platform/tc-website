2ad2b9314a298ed8a6f2d80fabfc94b492b12820
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2003-12-30 17:01:29 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/common/tag/ResultSetItemTag.java
+++ web_module/trunk/src/main/com/topcoder/web/common/tag/ResultSetItemTag.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 public class ResultSetItemTag extends FormatTag {
 
     private ResultSetContainer.ResultSetRow row;
+    private ResultSetContainer set;
     private String name;
+    private int rowIndex=0;   //default to the first row
 
     public void setRow(ResultSetContainer.ResultSetRow row) {
         this.row = row;
@@ -19,8 +21,20 @@ public class ResultSetItemTag extends FormatTag {
         this.name = name;
     }
 
+    public void setResultSet(ResultSetContainer set) {
+        this.set = set;
+    }
+
+    public void setRowIndex(int rowIndex) {
+        this.rowIndex = rowIndex;
+    }
+
     public int doStartTag() throws JspException {
-        setObject(row.getItem(name).getResultData());
+        if (row==null) {
+            setObject(set.getItem(rowIndex, name).getResultData());
+        } else {
+            setObject(row.getItem(name).getResultData());
+        }
         return super.doStartTag();
     }
 
