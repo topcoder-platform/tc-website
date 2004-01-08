63e33164b38494194cbf358b56881ea7d0b9faa2
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-01-08 14:17:18 +0000

    *** empty log message ***

--- src/main/com/topcoder/utilities/cheaterChecker/SimilarSourceSubsequences.java
+++ src/main/com/topcoder/utilities/cheaterChecker/SimilarSourceSubsequences.java
@@ -28,7 +28,7 @@ public class SimilarSourceSubsequences implements Fraud {
             for (int j = i + 1; j < tokens.size(); j++) {
                 s1 = (Submission) submissions.get(i);
                 s2 = (Submission) submissions.get(j);
-                if (s1.isIncluded() || s2.isIncluded() && s1.getLanguageId()==s2.getLanguageId()) {
+                if ((s1.isIncluded() || s2.isIncluded()) && s1.getLanguageId()==s2.getLanguageId()) {
                     int val = Util.getLongestCommonSubsequence(((TokenizedSource) tokens.get(i)).getTokens(),
                             ((TokenizedSource) tokens.get(j)).getTokens(), false);
                     results.add(new ComparisonResult(i, j, val));
