a36d19840315416ba0a6477d8d13bae3aabcbd86
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-01-13 23:24:45 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/MainServlet.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/MainServlet.java
@@ -2,17 +2,19 @@ package com.topcoder.web.privatelabel.controller;
 
 import com.topcoder.web.common.BaseServlet;
 import com.topcoder.web.common.TCRequest;
+import com.topcoder.web.common.SessionInfo;
 import com.topcoder.web.common.security.WebAuthentication;
 import com.topcoder.web.common.security.BasicAuthentication;
 import com.topcoder.web.common.security.SessionPersistor;
 import com.topcoder.web.privatelabel.Constants;
+import com.topcoder.web.tc.model.CoderSessionInfo;
 import com.topcoder.shared.util.logging.Logger;
 import com.topcoder.shared.security.Resource;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
 
 /**
  *
@@ -37,4 +39,12 @@ public class MainServlet extends BaseServlet {
                                                      HttpServletResponse response) throws Exception {
         return new BasicAuthentication(new SessionPersistor(request.getSession()), request, response, BasicAuthentication.PRIVATE_LABEL_SITE);
     }
+
+        protected SessionInfo createSessionInfo(TCRequest request,
+                                            WebAuthentication auth, Set groups) throws Exception {
+        CoderSessionInfo ret = new CoderSessionInfo(request, auth, groups);
+        return ret;
+    }
+
+
 }
