63ce2492b8629c60ff59464e056cf1b385c2c522
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-01-19 20:31:50 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/card/Data.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/card/Data.java
@@ -34,8 +34,8 @@ public class Data extends Base {
 
             TransformerHandler hd = tf.newTransformerHandler();
             Transformer serializer = hd.getTransformer();
-            serializer.setOutputProperty(OutputKeys.ENCODING,"ISO-8859-1");
-            serializer.setOutputProperty(OutputKeys.INDENT,"yes");
+//            serializer.setOutputProperty(OutputKeys.ENCODING,"ISO-8859-1");
+//            serializer.setOutputProperty(OutputKeys.INDENT,"yes");
             hd.setResult(streamResult);
             hd.startDocument();
 
