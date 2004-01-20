94d1b8790e9ae9154bf2b17380327b4ce7a151b6
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-01-20 22:43:20 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/ejb/BaseEJB.java
+++ web_module/trunk/src/main/com/topcoder/web/ejb/BaseEJB.java
@@ -1,13 +1,18 @@
 package com.topcoder.web.ejb;
 
 import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.web.common.RowNotFoundException;
 
 import javax.ejb.SessionContext;
 import javax.ejb.SessionBean;
+import javax.ejb.EJBException;
 import javax.naming.Context;
+import javax.naming.InitialContext;
 import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public abstract class BaseEJB implements SessionBean {
     private SessionContext ctx;
@@ -43,6 +48,198 @@ public abstract class BaseEJB implements SessionBean {
     public void setSessionContext(SessionContext ctx) {
         this.ctx = ctx;
     }
+
+    protected int insert(String tableName, String[] colNames, String[] colValues, String dataSource) {
+        if (colNames.length!=colValues.length)
+            throw new IllegalArgumentException("name and value arrays don't have the same number of elements.");
+        else {
+            StringBuffer query = new StringBuffer(200);
+            query.append("insert into ").append(tableName);
+            for (int i=0; i<colNames.length; i++) {
+                query.append(colNames[i]);
+                if (colNames.length>1 && i!=colNames.length-1)
+                    query.append(", ");
+            }
+            query.append(") values (");
+            for (int i=0; i<colNames.length; i++) {
+                query.append(colNames[i]);
+                if (colNames.length>1 && i!=colNames.length-1)
+                    query.append(", ");
+            }
+            query.append(")");
+            log.debug(query);
+
+            Connection conn = null;
+            PreparedStatement ps = null;
+            InitialContext ctx = null;
+            try {
+
+                conn = DBMS.getConnection(dataSource);
+                ps = conn.prepareStatement(query.toString());
+                for (int i=0; i<colNames.length; i++) {
+                    ps.setString(i+1, colValues[i]);
+                }
+
+                int rc = ps.executeUpdate();
+                return rc;
+            } catch (SQLException e) {
+                DBMS.printSqlException(true, e);
+                throw(new EJBException(e.getMessage()));
+            } finally {
+                close(ps);
+                close(conn);
+                close(ctx);
+            }
+
+        }
+    }
+
+
+    protected int delete(String tableName, String[] colNames, String[] colValues, String dataSource) {
+        if (colNames.length!=colValues.length)
+            throw new IllegalArgumentException("name and value arrays don't have the same number of elements.");
+        else {
+            StringBuffer query = new StringBuffer(200);
+            query.append("delete from ").append(tableName).append(" where ");
+            for (int i=0; i<colNames.length; i++) {
+                query.append(colNames[i]).append(" = ?");
+                if (colNames.length>1 && i!=colNames.length-1)
+                    query.append(" and ");
+            }
+
+            log.debug(query);
+
+            Connection conn = null;
+            PreparedStatement ps = null;
+            InitialContext ctx = null;
+            try {
+
+                conn = DBMS.getConnection(dataSource);
+                ps = conn.prepareStatement(query.toString());
+                for (int i=0; i<colNames.length; i++) {
+                    ps.setString(i+1, colValues[i]);
+                }
+
+                int rc = ps.executeUpdate();
+                return rc;
+            } catch (SQLException e) {
+                DBMS.printSqlException(true, e);
+                throw(new EJBException(e.getMessage()));
+            } finally {
+                close(ps);
+                close(conn);
+                close(ctx);
+            }
+
+        }
+    }
+
+    protected int update(String tableName, String[] colNames, String[] colValues, String dataSource) {
+        if (colNames.length!=colValues.length)
+            throw new IllegalArgumentException("name and value arrays don't have the same number of elements.");
+        else {
+            StringBuffer query = new StringBuffer(200);
+            query.append("update ").append(tableName).append(" set ");
+            for (int i=0; i<colNames.length; i++) {
+                query.append(colNames[i]).append(" = ?");
+                if (colNames.length>1 && i!=colNames.length-1)
+                    query.append(", ");
+            }
+
+            log.debug(query);
+
+            Connection conn = null;
+            PreparedStatement ps = null;
+            InitialContext ctx = null;
+            try {
+
+                conn = DBMS.getConnection(dataSource);
+                ps = conn.prepareStatement(query.toString());
+                for (int i=0; i<colNames.length; i++) {
+                    ps.setString(i+1, colValues[i]);
+                }
+
+                int rc = ps.executeUpdate();
+                return rc;
+            } catch (SQLException e) {
+                DBMS.printSqlException(true, e);
+                throw(new EJBException(e.getMessage()));
+            } finally {
+                close(ps);
+                close(conn);
+                close(ctx);
+            }
+
+        }
+    }
+
+    protected Integer selectInt(String tableName, String colName, String[] colNames, String[] colValues, String dataSource) {
+        String sRet = selectString(tableName, colName, colNames, colValues, dataSource);
+        Integer ret = null;
+        if (!(sRet==null||sRet.equals(""))) {
+            ret = new Integer(sRet);
+        }
+        return ret;
+    }
+
+    protected Long selectLong(String tableName, String colName, String[] colNames, String[] colValues, String dataSource) {
+        String sRet = selectString(tableName, colName, colNames, colValues, dataSource);
+        Long ret = null;
+        if (!(sRet==null||sRet.equals(""))) {
+            ret = new Long(sRet);
+        }
+        return ret;
+    }
+
+    protected String selectString(String tableName, String colName, String[] colNames, String[] colValues, String dataSource) {
+        if (colNames.length!=colValues.length)
+            throw new IllegalArgumentException("name and value arrays don't have the same number of elements.");
+        else {
+            StringBuffer query = new StringBuffer(200);
+            query.append("select ").append(colName).append(" from ").append(tableName).append(" where ");
+            for (int i=0; i<colNames.length; i++) {
+                query.append(colNames[i]).append(" = ?");
+                if (colNames.length>1 && i!=colNames.length-1)
+                    query.append(" and ");
+            }
+
+            log.debug(query);
+
+            Connection conn = null;
+            PreparedStatement ps = null;
+            InitialContext ctx = null;
+            ResultSet rs = null;
+            try {
+
+                conn = DBMS.getConnection(dataSource);
+                ps = conn.prepareStatement(query.toString());
+                for (int i=0; i<colNames.length; i++) {
+                    ps.setString(i+1, colValues[i]);
+                }
+
+                rs = ps.executeQuery();
+                String ret = null;
+                if (rs.next()) {
+                    ret = rs.getString(colName);
+                } else {
+                    throw new RowNotFoundException("no row found for " + query.toString());
+                }
+                return ret;
+            } catch (SQLException e) {
+                DBMS.printSqlException(true, e);
+                throw(new EJBException(e.getMessage()));
+            } finally {
+                close(rs);
+                close(ps);
+                close(conn);
+                close(ctx);
+            }
+
+        }
+    }
+
+
+
 
     protected void close(ResultSet rs) {
         if (rs != null) {
---
+++ web_module/trunk/src/main/com/topcoder/web/ejb/user/UserPreferenceBean.java
@@ -0,0 +1,91 @@
+package com.topcoder.web.ejb.user;
+
+import com.topcoder.web.ejb.BaseEJB;
+import com.topcoder.shared.util.logging.Logger;
+
+import javax.ejb.EJBException;
+
+/**
+ * @author dok
+ * Date: Jan 20, 2004
+ */
+public class UserPreferenceBean extends BaseEJB {
+
+    private final static Logger log = Logger.getLogger(UserPreferenceBean.class);
+
+    public void createUserPreference(long userId, int preferenceId, String dataSource)
+            throws EJBException {
+        int ret = insert("user_preference",
+                new String[] {"user_id", "preference_id"},
+                new String[] {String.valueOf(userId), String.valueOf(preferenceId)},
+                dataSource);
+        if (ret != 1) {
+            throw(new EJBException("Wrong number of rows inserted into " +
+                    "'user_preference'. Inserted " + ret + ", " +
+                    "should have inserted 1."));
+        }
+
+/*
+
+        log.debug("createUserPreference called. user_id=" + userId + " " +
+                "preference_id=" + preferenceId);
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        InitialContext ctx = null;
+        try {
+
+            StringBuffer query = new StringBuffer(1024);
+            query.append("INSERT ");
+            query.append("INTO user_preference (user_id,preference_id) ");
+            query.append("VALUES (?,?)");
+
+            conn = DBMS.getConnection(dataSource);
+            ps = conn.prepareStatement(query.toString());
+            ps.setLong(1, userId);
+            ps.setLong(2, preferenceId);
+
+            int rc = ps.executeUpdate();
+            if (rc != 1) {
+                throw(new EJBException("Wrong number of rows inserted into " +
+                        "'user_preference'. Inserted " + rc + ", " +
+                        "should have inserted 1."));
+            }
+        } catch (SQLException e) {
+            DBMS.printSqlException(true, e);
+            throw(new EJBException(e.getMessage()));
+        } finally {
+            close(ps);
+            close(conn);
+            close(ctx);
+        }
+*/
+    }
+
+    public void removeUserPreference(long userId, int preferenceId, String dataSource) {
+        int ret = delete("user_preference",
+                new String[] {"user_id", "preference_id"},
+                new String[] {String.valueOf(userId), String.valueOf(preferenceId)},
+                dataSource);
+        log.debug("removed " + ret + " rows");
+
+    }
+
+    public void setValue(long userId, int preferenceId, String value, String dataSource) {
+        int ret = update("user_preference",
+                new String[] {"value"},
+                new String[] {value},
+                dataSource);
+        log.debug("updated " + ret + " rows");
+
+    }
+
+    public String getValue(long userId, int preferenceId, String dataSource) {
+        return selectString("user_preference",
+                "value",
+                new String[] {"user_id", "preference_id"},
+                new String[] {String.valueOf(userId), String.valueOf(preferenceId)},
+                dataSource);
+    }
+
+}
