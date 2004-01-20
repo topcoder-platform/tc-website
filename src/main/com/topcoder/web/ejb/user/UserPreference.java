d0c9b8ab8ac343206beb18a47e0a980b2413ce28
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-01-20 22:46:25 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/ejb/user/UserPreference.java
+++ src/main/com/topcoder/web/ejb/user/UserPreference.java
@@ -1,6 +1,7 @@
 package com.topcoder.web.ejb.user;
 
 import javax.ejb.EJBObject;
+import javax.ejb.EJBException;
 import java.rmi.RemoteException;
 
 /**
@@ -8,8 +9,8 @@ import java.rmi.RemoteException;
  * Date: Jan 20, 2004
  */
 public interface UserPreference extends EJBObject  {
-    void createUserPreference(long userId, int preferenceId, String dataSource) throws RemoteException;
-    void removeUserPreference(long userId, int preferenceId, String dataSource) throws RemoteException;
-    void setValue(long userId, int preferenceId, String value, String dataSource) throws RemoteException;
-    String getValue(long userId, int preferenceId, String dataSource) throws RemoteException;
+    void createUserPreference(long userId, int preferenceId, String dataSource) throws EJBException,RemoteException;
+    void removeUserPreference(long userId, int preferenceId, String dataSource) throws EJBException,RemoteException;
+    void setValue(long userId, int preferenceId, String value, String dataSource) throws EJBException,RemoteException;
+    String getValue(long userId, int preferenceId, String dataSource) throws EJBException,RemoteException;
 }
