981de171bf11d515a3e406bce995bcafd097fc1d
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-01-27 18:20:02 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/util/TCCC04TermsAgree.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/util/TCCC04TermsAgree.java
@@ -4,6 +4,7 @@ import com.topcoder.web.tc.controller.request.Base;
 import com.topcoder.web.tc.Constants;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.PermissionException;
+import com.topcoder.web.common.NavigationException;
 import com.topcoder.web.ejb.user.UserTermsOfUse;
 import com.topcoder.shared.util.DBMS;
 import com.topcoder.shared.util.TCContext;
@@ -13,6 +14,8 @@ import com.topcoder.shared.dataAccess.Request;
 import com.topcoder.shared.dataAccess.DataAccess;
 
 import javax.naming.InitialContext;
+import java.util.Calendar;
+import java.util.Date;
 
 public class TCCC04TermsAgree extends Base {
 
@@ -21,12 +24,24 @@ public class TCCC04TermsAgree extends Base {
             if (getUser().isAnonymous()) {
                 throw new PermissionException(getUser(), new SimpleResource("TCCC04Terms"));
             } else {
-                UserTermsOfUse userTerms = (UserTermsOfUse)createEJB(getInitialContext(), UserTermsOfUse.class);
-                if (!isRegistered(getUser().getId()) && isEligible(getUser().getId())) {
-                    log.debug("user has not previously aggreed to these terms");
-                    userTerms.createUserTermsOfUse(getUser().getId(), Constants.TCCC04_TERMS_OF_USE_ID, DBMS.OLTP_DATASOURCE_NAME);
+                Calendar now = Calendar.getInstance();
+                now.setTime(new Date());
+                Calendar end = Calendar.getInstance();
+                end.set(2004, 1, 23, 9, 0);
+                Calendar beginning = Calendar.getInstance();
+                end.set(2004, 1, 1, 0, 0);
+                if (now.before(end)) {
+                    throw new NavigationException("The registration period for the TCCC is over.");
+                } else if (!now.after(beginning)) {
+                    throw new NavigationException("The registration period for the TCCC has not yet begun.");
                 } else {
-                    log.debug("user has previously aggreed to these terms");
+                    UserTermsOfUse userTerms = (UserTermsOfUse)createEJB(getInitialContext(), UserTermsOfUse.class);
+                    if (!isRegistered(getUser().getId()) && isEligible(getUser().getId())) {
+                        log.debug("user has not previously aggreed to these terms");
+                        userTerms.createUserTermsOfUse(getUser().getId(), Constants.TCCC04_TERMS_OF_USE_ID, DBMS.OLTP_DATASOURCE_NAME);
+                    } else {
+                        log.debug("user has previously aggreed to these terms");
+                    }
                 }
                 //this should go to the default page
                 setIsNextPageInContext(false);
--- web_module/trunk/src/main/com/topcoder/web/tc/view/home/message.jsp
+++ web_module/trunk/src/main/com/topcoder/web/tc/view/home/message.jsp
@@ -1,23 +1,32 @@
+<%@ page import="java.util.Date,
+                 java.util.Calendar"%>
 <%
     boolean isEligible = new Boolean((String)request.getAttribute("isEligible")).booleanValue();
     boolean isRegistered = new Boolean((String)request.getAttribute("isRegistered")).booleanValue();
+    Calendar now = Calendar.getInstance();
+    now.setTime(new Date());
+    Calendar end = Calendar.getInstance();
+    end.set(2004, 1, 23, 9, 0);
 
 %>
 
-<table border="0" cellspacing="0" cellpadding="0" width="180">
-    <tr><td><img src="/i/messageBoxTop.gif" width="180" height="11" border="0"/></td></tr>
-    <tr><td><img src="/i/important_message.gif" width="180" height="20" border="0"/></td></tr>
-</table>
-<table cellspacing="0" cellpadding="5" class="messageBoxFrame">
-    <tr>
-        <td class="messageBoxText">
-            <% if (!isEligible) { %>
-                you're not eligible
-            <% } else if (isRegistered) { %>
-                you're registered already
-            <% } else { %>
-                    <img src="/i/tournament/tccc04/message_not_reg.gif" width="180" height="69" border="0"/>
-            <% } %>
-        </td>
-    </tr>
-</table>
+
+<% if (now.before(end)) { %>
+    <table border="0" cellspacing="0" cellpadding="0" width="180">
+        <tr><td><img src="/i/messageBoxTop.gif" width="180" height="11" border="0"/></td></tr>
+        <tr><td><img src="/i/important_message.gif" width="180" height="20" border="0"/></td></tr>
+    </table>
+    <table cellspacing="0" cellpadding="5" class="messageBoxFrame">
+        <tr>
+            <td class="messageBoxText">
+                <% if (!isEligible) { %>
+                    you're not eligible
+                <% } else if (isRegistered) { %>
+                    you're registered already
+                <% } else { %>
+                        <img src="/i/tournament/tccc04/message_not_reg.gif" width="180" height="69" border="0"/>
+                <% } %>
+            </td>
+        </tr>
+    </table>
+<% } %>
