a079c8dcf8ccf3be0234b2fe26b2e94e08cb9691
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-02-11 14:31:33 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/tc/controller/request/development/ViewReviewProjects.java
+++ src/main/com/topcoder/web/tc/controller/request/development/ViewReviewProjects.java
@@ -1,6 +1,8 @@
 package com.topcoder.web.tc.controller.request.development;
 
 import com.topcoder.web.common.TCWebException;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 
 /**
  * @author dok
@@ -9,6 +11,15 @@ import com.topcoder.web.common.TCWebException;
 public class ViewReviewProjects extends Base {
 
     protected void developmentProcessing() throws TCWebException {
+        Request r = new Request();
+        r.setContentHandle("review_projects");
+        try {
+            getRequest().setAttribute("projectList", (ResultSetContainer)getDataAccess().getData(r).get("review_projects"));
+        } catch (TCWebException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new TCWebException(e);
+        }
         setNextPage("/dev/reviewProjects.jsp");
         setIsNextPageInContext(true);
     }
