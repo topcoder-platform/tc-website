2cbdcde2c4a8a26c5b390328795eca74b094d57a
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-02-12 05:58:20 +0000

    *** empty log message ***

---
+++ src/main/com/topcoder/web/ejb/rboardapplication/RBoardApplication.java
@@ -0,0 +1,28 @@
+package com.topcoder.web.ejb.rboardapplication;
+
+import javax.ejb.EJBObject;
+import java.rmi.RemoteException;
+
+/**
+ * @author dok
+ * Date: Feb 12, 2004
+ */
+public interface RBoardApplication extends EJBObject {
+
+    /**
+     *
+     *
+     * @see com.topcoder.web.ejb.rboardapplication.RBoardApplicationBean#createRBoardApplication
+     */
+    void createRBoardApplication(String dataSource, long userId, long projectId, int phaseId) throws RemoteException;
+
+    void setReviewRespId(String dataSource, long userId, long projectId, int phaseId, int reviewRespId) throws RemoteException;
+    void setPrimary(String dataSource, long userId, long projectId, int phaseId, boolean isPrimary) throws RemoteException;
+
+    int getReviewRespId(String dataSource, long userId, long projectId, int phaseId) throws RemoteException;
+    boolean isPrimary(String dataSource, long userId, long projectId, int phaseId) throws RemoteException;
+
+    boolean exists(String dataSource, long userId, long projectId, int phaseId) throws RemoteException;
+
+}
+
---
+++ src/main/com/topcoder/web/ejb/rboardapplication/RBoardApplicationHome.java
@@ -0,0 +1,12 @@
+package com.topcoder.web.ejb.rboardapplication;
+
+import javax.ejb.CreateException;
+import java.rmi.RemoteException;
+
+/**
+ * @author dok
+ * Date: Feb 12, 2004
+ */
+public interface RBoardApplicationHome extends javax.ejb.EJBHome {
+    public RBoardApplication create() throws CreateException, RemoteException;
+}
