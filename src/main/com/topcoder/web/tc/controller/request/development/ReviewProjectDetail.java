85268b2493c3124c14bb311f5f9322125a65bc08
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-02-12 02:34:04 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/tc/controller/request/development/ReviewProjectDetail.java
+++ src/main/com/topcoder/web/tc/controller/request/development/ReviewProjectDetail.java
@@ -2,7 +2,15 @@ package com.topcoder.web.tc.controller.request.development;
 
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.tc.model.SoftwareComponent;
+import com.topcoder.web.tc.model.ReviewBoardApplication;
 import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Iterator;
 
 /**
  * @author dok
@@ -16,7 +24,70 @@ public class ReviewProjectDetail extends Base {
         r.setProperty("pj", StringUtils.checkNull(getRequest().getParameter("pj")));
         r.setProperty("ph", StringUtils.checkNull(getRequest().getParameter("ph")));
         try {
-            getRequest().setAttribute("projectDetail", getDataAccess().getData(r).get("review_project_detail"));
+            Map results = getDataAccess().getData(r);
+            ResultSetContainer detail = (ResultSetContainer)results.get("review_project_detail");
+            getRequest().setAttribute("projectDetail", detail);
+
+            ArrayList reviewerList = new ArrayList(3);
+
+            if (detail.isEmpty()) {
+                throw new NavigationException("Could not find information on the project selected.");
+            } else {
+                if (detail.getLongItem(0, "phase_id")==SoftwareComponent.DEV_PHASE) {
+                    ResultSetContainer reviewers = (ResultSetContainer)results.get("development_reviewers");
+                    ResultSetContainer.ResultSetRow row = null;
+                    //add all the positions
+                    for (Iterator it = reviewers.iterator(); it.hasNext();) {
+                        row = (ResultSetContainer.ResultSetRow)it.next();
+                        //this one has not been assigned yet
+                        if (row.getStringItem("handle")==null) {
+                            reviewerList.add(makeApp(row.getStringItem("reviewer_type"),
+                                    detail.getIntItem(0, "submission_count"),
+                                    detail.getIntItem(0, "phase_id"),
+                                    detail.getIntItem(0, "level_id"),
+                                    detail.getLongItem(0, "project_id")));
+                        } else{
+                            //this one has been assigned
+                            reviewerList.add(makeApp(row.getStringItem("reviewer_type"),
+                                    detail.getIntItem(0, "submission_count"),
+                                    detail.getIntItem(0, "phase_id"),
+                                    detail.getIntItem(0, "level_id"),
+                                    row.getLongItem("user_id"),
+                                    row.getStringItem("handle"),
+                                    row.getIntItem("primary")==1,
+                                    detail.getLongItem(0, "project_id")));
+                        }
+                    }
+                } else {
+                    ResultSetContainer reviewers = (ResultSetContainer)results.get("design_reviewers");
+                    ResultSetContainer.ResultSetRow row = null;
+                    int count = 0;
+                    //add all the assigned positions
+                    for (Iterator it = reviewers.iterator(); it.hasNext();) {
+                        count++;
+                        row = (ResultSetContainer.ResultSetRow)it.next();
+                        reviewerList.add(makeApp("Reviewer",
+                                detail.getIntItem(0, "submission_count"),
+                                detail.getIntItem(0, "phase_id"),
+                                detail.getIntItem(0, "level_id"),
+                                row.getLongItem("user_id"),
+                                row.getStringItem("handle"),
+                                row.getIntItem("primary")==1,
+                                detail.getLongItem(0, "project_id")));
+                    }
+                    for (int i = count; i<3; i++) {
+                        //add empty positions until the list is 3 long
+                        reviewerList.add(makeApp("Reviewer",
+                                detail.getIntItem(0, "submission_count"),
+                                detail.getIntItem(0, "phase_id"),
+                                detail.getIntItem(0, "level_id"),
+                                detail.getLongItem(0, "project_id")));
+                    }
+                }
+
+                getRequest().setAttribute("reviewerList", reviewerList);
+            }
+
         } catch (TCWebException e) {
             throw e;
         } catch (Exception e) {
@@ -26,4 +97,25 @@ public class ReviewProjectDetail extends Base {
         setIsNextPageInContext(true);
 
     }
+
+    private ReviewBoardApplication makeApp(String reviewerType, int numSubmissions, int phaseId,
+                                           int levelId, long userId, String handle, boolean primary,
+                                           long projectId) {
+        ReviewBoardApplication ret = makeApp(reviewerType, numSubmissions, phaseId, levelId, projectId);
+        ret.setHandle(handle);
+        ret.setPrimary(primary);
+        ret.setUserId(userId);
+        return ret;
+    }
+
+    private ReviewBoardApplication makeApp(String reviewerType, int numSubmissions, int phaseId,
+                                           int levelId, long projectId) {
+        //we'll always assume 1 submission gets through screening
+        ReviewBoardApplication ret = new ReviewBoardApplication(phaseId, levelId, numSubmissions, 1);
+        ret.setProjectId(projectId);
+        ret.setReviewerType(reviewerType);
+        return ret;
+    }
+
+
 }
--- src/main/com/topcoder/web/tc/model/SoftwareComponent.java
+++ src/main/com/topcoder/web/tc/model/SoftwareComponent.java
@@ -7,44 +7,44 @@ package com.topcoder.web.tc.model;
  */
 public class SoftwareComponent {
 
-    private static float[] DESIGN_HOUR_LOOKUP = {0f, 4f, 8f, 11f};
-    private static float[] DESIGN_EFFORT_LOOKUP = {0f, .10f, .15f, .20f};
-    private static float[] DESIGN_PRICE_LOOKUP = {0f, 105f, 210f};
+    private final static float[] DESIGN_HOUR_LOOKUP = {0f, 4f, 8f, 11f};
+    private final static float[] DESIGN_EFFORT_LOOKUP = {0f, .10f, .15f, .20f};
+    private final static float[] DESIGN_PRICE_LOOKUP = {0f, 105f, 210f};
 
-    private static float[] DEV_HOUR_LOOKUP = {0f, 4f, 8f, 11f};
-    private static float[] DEV_EFFORT_LOOKUP = {0f, .20f, .25f, .30f};
-    private static float[] DEV_PRICE_LOOKUP = {0f, 96f, 144f};
+    private final static float[] DEV_HOUR_LOOKUP = {0f, 4f, 8f, 11f};
+    private final static float[] DEV_EFFORT_LOOKUP = {0f, .20f, .25f, .30f};
+    private final static float[] DEV_PRICE_LOOKUP = {0f, 96f, 144f};
 
-//    private static float DEV_RATE = 20f;
-    private static float DEV_REVIEW_RATE = 20f;
-//    private static float DESIGN_RATE = 25f;
-    private static float DESIGN_REVIEW_RATE = 25f;
+//    private final static float DEV_RATE = 20f;
+    private final static float DEV_REVIEW_RATE = 20f;
+//    private final static float DESIGN_RATE = 25f;
+    private final static float DESIGN_REVIEW_RATE = 25f;
 
-    private static int DEV_PHASE = 113;
-    private static int DESIGN_PHASE = 112;
+    public final static int DEV_PHASE = 113;
+    public final static int DESIGN_PHASE = 112;
 
-    private int phase;
-    private int level;
+    private int phaseId;
+    private int levelId;
     private int submissionCount;
     private int submissionsPassedScreening;
 
     private SoftwareComponent() {}
 
-    public SoftwareComponent(int level, int submissionCount, int submissionsPassedScreening, int phase) {
-        this.phase = phase;
-        this.level = level;
+    public SoftwareComponent(int level, int submissionCount, int submissionsPassedScreening, int phaseId) {
+        this.phaseId = phaseId;
+        this.levelId = level;
         this.submissionCount = submissionCount;
         this.submissionsPassedScreening = submissionsPassedScreening;
     }
 
     public float getCost() {
         float ret = 0.0f;
-        if (phase == DEV_PHASE) {
+        if (phaseId == DEV_PHASE) {
             ret = getDevCost();
-        } else if (phase == DESIGN_PHASE) {
+        } else if (phaseId == DESIGN_PHASE) {
             ret = getDesignCost();
         } else {
-            throw new RuntimeException("invalid phase " + phase);
+            throw new RuntimeException("invalid phaseId " + phaseId);
         }
         return Math.round(ret);
     }
@@ -52,24 +52,24 @@ public class SoftwareComponent {
 
     public float getPrimaryReviewCost() {
         float ret = 0.0f;
-        if (phase == DEV_PHASE) {
+        if (phaseId == DEV_PHASE) {
             ret = getPrimaryDevReviewCost();
-        } else if (phase == DESIGN_PHASE) {
+        } else if (phaseId == DESIGN_PHASE) {
             ret = getPrimaryDesignReviewCost();
         } else {
-            throw new RuntimeException("invalid phase " + phase);
+            throw new RuntimeException("invalid phaseId " + phaseId);
         }
         return Math.round(ret);
     }
 
     public float getReviewCost() {
         float ret = 0.0f;
-        if (phase == DEV_PHASE) {
+        if (phaseId == DEV_PHASE) {
             ret = getDevReviewCost();
-        } else if (phase == DESIGN_PHASE) {
+        } else if (phaseId == DESIGN_PHASE) {
             ret = getDesignReviewCost();
         } else {
-            throw new RuntimeException("invalid phase " + phase);
+            throw new RuntimeException("invalid phaseId " + phaseId);
         }
         return Math.round(ret);
     }
@@ -79,7 +79,7 @@ public class SoftwareComponent {
      * @return
      */
     private float getDevCost() {
-        return DEV_PRICE_LOOKUP[level];
+        return DEV_PRICE_LOOKUP[levelId];
     }
 
     /**
@@ -87,7 +87,7 @@ public class SoftwareComponent {
      * @return
      */
     private float getDesignCost() {
-        return DESIGN_PRICE_LOOKUP[level];
+        return DESIGN_PRICE_LOOKUP[levelId];
     }
 
     /**
@@ -110,7 +110,7 @@ public class SoftwareComponent {
      * @return
      */
     private float getCoreDevReviewCost() {
-        float reviewTime = DEV_EFFORT_LOOKUP[level] * DEV_HOUR_LOOKUP[level] * (float)submissionsPassedScreening;
+        float reviewTime = DEV_EFFORT_LOOKUP[levelId] * DEV_HOUR_LOOKUP[levelId] * (float)submissionsPassedScreening;
         if (reviewTime < 2) reviewTime = 2f;  //give them a minimum of 2 hours to review
         float reviewCost = reviewTime*DEV_REVIEW_RATE;
         float startupCost = DEV_REVIEW_RATE / 2; //30 minutes to "start up"
@@ -163,7 +163,7 @@ public class SoftwareComponent {
      * @return
      */
     private float getCoreDesignReviewCost() {
-        float reviewTime = DESIGN_EFFORT_LOOKUP[level] * DESIGN_HOUR_LOOKUP[level] * (float)submissionsPassedScreening;
+        float reviewTime = DESIGN_EFFORT_LOOKUP[levelId] * DESIGN_HOUR_LOOKUP[levelId] * (float)submissionsPassedScreening;
         if (reviewTime < 1) reviewTime = 1f;  //give them a minimum of 1 hours to review
         float reviewCost = reviewTime*DESIGN_REVIEW_RATE;
         float startupCost = (1f/2f) * DESIGN_REVIEW_RATE; //30 minutes to "start up"
@@ -176,20 +176,20 @@ public class SoftwareComponent {
     public static void main(String[] args) {
         if (args.length!=4) {
             System.out.println("usage: java " + SoftwareComponent.class.toString() + " " +
-                    "<level> <numSubmissions> <numSubmissionPassScreening> <phase>");
-            System.out.println("dev phase " + DEV_PHASE);
-            System.out.println("design phase " + DESIGN_PHASE);
+                    "<levelId> <numSubmissions> <numSubmissionPassScreening> <phaseId>");
+            System.out.println("dev phaseId " + DEV_PHASE);
+            System.out.println("design phaseId " + DESIGN_PHASE);
         } else {
             SoftwareComponent sc = new SoftwareComponent(Integer.parseInt(args[0]), Integer.parseInt(args[1]),
                     Integer.parseInt(args[2]), Integer.parseInt(args[3]));
             System.out.println("-------------------------------------------------------------");
-            if (sc.phase==DEV_PHASE) {
+            if (sc.phaseId==DEV_PHASE) {
                 System.out.println("        Dev Cost:            |      " + sc.getCost());
                 System.out.println("-----------------------------+-------------------------------");
                 System.out.println("     Dev Review Cost:        |      " + sc.getReviewCost());
                 System.out.println("-----------------------------+-------------------------------");
                 System.out.println("  Dev Primary Review Cost    |      " + sc.getPrimaryReviewCost());
-            } else if (sc.phase==DESIGN_PHASE) {
+            } else if (sc.phaseId==DESIGN_PHASE) {
                 System.out.println("      Design Cost:           |      " + sc.getCost());
                 System.out.println("-----------------------------+-------------------------------");
                 System.out.println("   Design Review Cost:       |      " + sc.getReviewCost());
