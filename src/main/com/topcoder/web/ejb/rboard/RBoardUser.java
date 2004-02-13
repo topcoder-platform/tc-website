4b63fc9b4d723fe7aced5b732f30d51d79f611bf
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-02-13 19:01:06 +0000

    *** empty log message ***

---
+++ src/main/com/topcoder/web/ejb/rboard/RBoardUser.java
@@ -0,0 +1,32 @@
+package com.topcoder.web.ejb.rboard;
+
+import java.rmi.RemoteException;
+
+/**
+ * @author dok
+ * Date: Feb 13, 2004
+ */
+public interface RBoardUser {
+
+
+    void createRBoardUser(String dataSource, long userId, int phaseId, int statusId,
+                          boolean hasContract, boolean canReviewJava,
+                          boolean canReviewDotNet, boolean canReviewFlash) throws RemoteException;
+
+    void setStatus(String dataSource, long userId, int phaseId, int statusId) throws RemoteException;
+    void setHasContract(String dataSource, long userId, int phaseId, boolean hasConstract) throws RemoteException;
+    void setCanReviewJava(String dataSource, long userId, int phaseId, boolean canReviewJava) throws RemoteException;
+    void setCanReviewDotNet(String dataSource, long userId, int phaseId, boolean canReviewDotNet) throws RemoteException;
+    void setCanReviewFlash(String dataSource, long userId, int phaseId, boolean canReviewFlash) throws RemoteException;
+
+    int getStatus(String dataSource, long userId, int phaseId) throws RemoteException;
+    boolean hasContract(String dataSource, long userId, int phaseId) throws RemoteException;
+    boolean canReviewJava(String dataSource, long userId, int phaseId) throws RemoteException;
+    boolean canReviewDotNet(String dataSource, long userId, int phaseId) throws RemoteException;
+    boolean canReviewFlash(String dataSource, long userId, int phaseId) throws RemoteException;
+
+
+
+
+
+}
