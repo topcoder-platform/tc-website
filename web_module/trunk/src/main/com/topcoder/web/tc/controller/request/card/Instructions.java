6c87194d8c4af537b810a06cd285ad02fe84c7f2
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-03-11 02:56:10 +0000

    *** empty log message ***

---
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/card/Instructions.java
@@ -0,0 +1,49 @@
+package com.topcoder.web.tc.controller.request.card;
+
+import com.topcoder.web.tc.controller.request.Base;
+import com.topcoder.web.tc.Constants;
+import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.common.PermissionException;
+import com.topcoder.web.common.RowNotFoundException;
+import com.topcoder.web.ejb.user.UserPreference;
+import com.topcoder.shared.security.ClassResource;
+import com.topcoder.shared.util.DBMS;
+
+import java.rmi.RemoteException;
+
+/**
+ * @author dok
+ * Date: Mar 10, 2004
+ */
+public class Instructions extends Base {
+
+    protected void businessProcessing() throws TCWebException {
+
+        if (getUser().isAnonymous()) {
+            throw new PermissionException(getUser(), new ClassResource(this.getClass()));
+        } else {
+            //instructions page should indicate card is unlocked or not
+
+            try {
+                UserPreference up = (UserPreference)createEJB(getInitialContext(), UserPreference.class);
+                boolean cardUnlocked = false;
+                try {
+                    up.getValue(getUser().getId(), Constants.UNLOCK_CARD_PREFERENCE_ID, DBMS.COMMON_OLTP_DATASOURCE_NAME);
+                    cardUnlocked = true;
+                } catch (RemoteException e) {
+                    if (!(e.detail instanceof RowNotFoundException))
+                        throw e;
+                }
+                getRequest().setAttribute("cardUnlocked", new Boolean(cardUnlocked));
+            } catch (TCWebException e) {
+                throw e;
+            } catch (Exception e) {
+                throw new TCWebException(e);
+            }
+
+            setNextPage(Constants.CARD_INSTRUCTIONS);
+            setIsNextPageInContext(true);
+        }
+    }
+
+}
