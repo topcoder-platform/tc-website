8afc9ca9d06fa9b9e44ce3dcd91002f15421e766
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-04-02 21:21:03 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/ejb/BaseEJB.java
+++ src/main/com/topcoder/web/ejb/BaseEJB.java
@@ -2,6 +2,7 @@ package com.topcoder.web.ejb;
 
 import com.topcoder.shared.util.logging.Logger;
 import com.topcoder.shared.util.DBMS;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 import com.topcoder.web.common.RowNotFoundException;
 
 import javax.ejb.SessionContext;
@@ -9,10 +10,7 @@ import javax.ejb.SessionBean;
 import javax.ejb.EJBException;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import java.sql.ResultSet;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public abstract class BaseEJB implements SessionBean {
     private SessionContext ctx;
@@ -185,10 +183,55 @@ public abstract class BaseEJB implements SessionBean {
         }
     }
 
+    protected int updateDate(String tableName, String colName, Date colValue,
+                         String[] constraintNames, String[] constraintValues, String dataSource) {
+        if (constraintNames.length!=constraintValues.length)
+            throw new IllegalArgumentException("contraint name and value arrays don't have the same number of elements.");
+        else {
+            StringBuffer query = new StringBuffer(200);
+            query.append("update ").append(tableName).append(" set ");
+            query.append(colName);
+            query.append( " = ?");
+            query.append(" where ");
+            for (int i=0; i<constraintNames.length; i++) {
+                query.append(constraintNames[i]).append(" = ?");
+                if (constraintNames.length>1 && i!=constraintNames.length-1)
+                    query.append(" and ");
+            }
+
+            log.debug(query);
+
+            Connection conn = null;
+            PreparedStatement ps = null;
+            InitialContext ctx = null;
+            try {
+
+                conn = DBMS.getConnection(dataSource);
+                ps = conn.prepareStatement(query.toString());
+                ps.setDate(1, colValue);
+                for (int j=0; j<constraintNames.length; j++) {
+                    ps.setString(j+2, constraintValues[j]);
+                }
+                int rc = ps.executeUpdate();
+                return rc;
+            } catch (SQLException e) {
+                DBMS.printSqlException(true, e);
+                throw(new EJBException(e.getMessage()));
+            } finally {
+                close(ps);
+                close(conn);
+                close(ctx);
+            }
+
+        }
+    }
+
+
+
     protected Integer selectInt(String tableName, String colName, String[] colNames, String[] colValues, String dataSource) {
         String sRet = selectString(tableName, colName, colNames, colValues, dataSource);
         Integer ret = null;
-        if (!(sRet==null||sRet.equals(""))) {
+        if (!(sRet==null||sRet.trim().equals(""))) {
             ret = new Integer(sRet);
         }
         return ret;
@@ -197,7 +240,7 @@ public abstract class BaseEJB implements SessionBean {
     protected Long selectLong(String tableName, String colName, String[] colNames, String[] colValues, String dataSource) {
         String sRet = selectString(tableName, colName, colNames, colValues, dataSource);
         Long ret = null;
-        if (!(sRet==null||sRet.equals(""))) {
+        if (!(sRet==null||sRet.trim().equals(""))) {
             ret = new Long(sRet);
         }
         return ret;
@@ -251,6 +294,103 @@ public abstract class BaseEJB implements SessionBean {
     }
 
 
+    protected Date selectDate(String tableName, String colName, String[] colNames, String[] colValues, String dataSource) {
+        if (colNames.length!=colValues.length)
+            throw new IllegalArgumentException("name and value arrays don't have the same number of elements.");
+        else {
+            StringBuffer query = new StringBuffer(200);
+            query.append("select ").append(colName).append(" from ").append(tableName).append(" where ");
+            for (int i=0; i<colNames.length; i++) {
+                query.append(colNames[i]).append(" = ?");
+                if (colNames.length>1 && i!=colNames.length-1)
+                    query.append(" and ");
+            }
+
+            log.debug(query);
+
+            Connection conn = null;
+            PreparedStatement ps = null;
+            InitialContext ctx = null;
+            ResultSet rs = null;
+            try {
+
+                conn = DBMS.getConnection(dataSource);
+                ps = conn.prepareStatement(query.toString());
+                for (int i=0; i<colNames.length; i++) {
+                    ps.setString(i+1, colValues[i]);
+                }
+
+                rs = ps.executeQuery();
+                Date ret = null;
+                if (rs.next()) {
+                    ret = rs.getDate(colName);
+                } else {
+                    throw new RowNotFoundException("no row found for " + query.toString());
+                }
+                return ret;
+            } catch (SQLException e) {
+                DBMS.printSqlException(true, e);
+                throw new EJBException(e.getMessage());
+            } finally {
+                close(rs);
+                close(ps);
+                close(conn);
+                close(ctx);
+            }
+
+        }
+    }
+
+    protected ResultSetContainer selectSet(String tableName, String colNames[], String[] constraintColNames,
+                                           String[] constraintColValues, String dataSource) {
+        if (constraintColNames.length!=constraintColValues.length)
+            throw new IllegalArgumentException("name and value arrays don't have the same number of elements.");
+        else {
+            StringBuffer query = new StringBuffer(200);
+            query.append("select ");
+            for (int i=0; i<colNames.length; i++) {
+                query.append(colNames[i]);
+                if (colNames.length>1 && i!=colNames.length-1)
+                    query.append(", ");
+            }
+
+            query.append(" from ").append(tableName).append(" where ");
+            for (int i=0; i<constraintColNames.length; i++) {
+                query.append(constraintColNames[i]).append(" = ?");
+                if (constraintColNames.length>1 && i!=constraintColNames.length-1)
+                    query.append(" and ");
+            }
+
+            log.debug(query);
+
+            Connection conn = null;
+            PreparedStatement ps = null;
+            InitialContext ctx = null;
+            ResultSet rs = null;
+            try {
+
+                conn = DBMS.getConnection(dataSource);
+                ps = conn.prepareStatement(query.toString());
+                for (int i=0; i<constraintColNames.length; i++) {
+                    ps.setString(i+1, constraintColValues[i]);
+                }
+                rs = ps.executeQuery();
+                return new ResultSetContainer(rs);
+
+            } catch (SQLException e) {
+                DBMS.printSqlException(true, e);
+                throw new EJBException(e.getMessage());
+            } catch (Exception e) {
+                throw new EJBException(e.getMessage());
+            } finally {
+                close(rs);
+                close(ps);
+                close(conn);
+                close(ctx);
+            }
+
+        }
+    }
 
 
     protected void close(ResultSet rs) {
