e0a4aceef38cca0359fc10bb97286596727935c0
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-04-29 20:06:52 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/corp/controller/request/ViewCard.java
+++ web_module/trunk/src/main/com/topcoder/web/corp/controller/request/ViewCard.java
@@ -1,4 +1,4 @@
-package com.topcoder.web.tc.controller.request.card;
+package com.topcoder.web.corp.controller.request;
 
 import com.topcoder.web.common.BaseProcessor;
 import com.topcoder.web.common.TCWebException;
@@ -6,19 +6,24 @@ import com.topcoder.web.common.StringUtils;
 
 /**
  * @author dok
- * Date: Jan 19, 2004
+ * Date: Apr 29, 2004
  */
-public class Profile extends BaseProcessor {
+public class ViewCard extends BaseProcessor {
 
     protected void businessProcessing() throws TCWebException {
 
         String coderId = StringUtils.checkNull(getRequest().getParameter("cr"));
         if (coderId.equals("")) {
             throw new TCWebException("Coder id parameter not included in request");
+        } else {
+            //pretty much just pass it along.  the data retriever will check
+            //to be sure it should be serving up the data
+            getRequest().setAttribute("cr", coderId);
+            setNextPage("/corp/recruiting/card_lp.jsp");
+            setIsNextPageInContext(true);
         }
 
-        setNextPage("/corp/?module=ViewCard&cr="+coderId);
-        setIsNextPageInContext(false);
+
     }
 
 }
