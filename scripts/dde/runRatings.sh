e5abc138da6f27b4eae7e5d37da44f59fca978e1
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-06-02 20:20:55 +0000

    ratings

--- scripts/dde/runRatings.sh
+++ scripts/dde/runRatings.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-CP=../conf:../build/classes:../jars/configmanager_xerces-1.4.4.jar:../jars/tcsUtil.jar:$CLASSPATH
+nohup java -cp "/usr/web/resources:/usr/web/lib/jars/log4j.jar:/usr/web/lib/jars/ifxjdbc.jar:/
+usr/web/lib/jars/jdbc7.0-1.2.jar:/usr/web/lib/jars/xml/2.2.D14/xerces.jar:/usr/web/build/class
+es" com.topcoder.dde.util.DWLoad.RatingQubits >> /usr/web/scripts/dde/ratings.log 2>&1 &
 
-java -cp $CP com.topcoder.dde.util.DWLoad.RatingQubits
