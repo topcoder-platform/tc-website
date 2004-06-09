29615c726049ad7109f9f680b6a689860015186b
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-06-09 16:26:17 +0000

    usdc results addition

---
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/development/USDCContestDetails.java
@@ -0,0 +1,32 @@
+/*
+ * USDCResults.java
+ *
+ * Created on June 9, 2004, 11:22 AM
+ */
+
+package com.topcoder.web.tc.controller.request.development;
+
+import com.topcoder.shared.util.DBMS;
+
+/**
+ *
+ * @author  rfairfax
+ */
+public class USDCContestDetails extends StatBase {
+    
+    String getCommandName() {
+        return "usdc_contest_details";
+    }
+    
+    String getDataSourceName() {
+        return DBMS.DW_DATASOURCE_NAME;
+    }
+    
+    String getPageName() {
+        return "/dev/usdc_contest_det.jsp";
+    }
+    
+    void statProcessing() throws com.topcoder.web.common.TCWebException {
+    }
+    
+}
