c54ebd5833e9a2462c8ff61bf0ed9b1a2c347ffc
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-06-15 15:36:53 +0000

    usdc page changes

--- src/main/com/topcoder/web/tc/controller/request/development/USDCContestDetails.java
+++ src/main/com/topcoder/web/tc/controller/request/development/USDCContestDetails.java
@@ -6,8 +6,19 @@
 
 package com.topcoder.web.tc.controller.request.development;
 
-import com.topcoder.shared.util.DBMS;
 import java.util.Map;
+import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.tc.Constants;
+import com.topcoder.web.tc.controller.legacy.TaskDevelopment;
+import com.topcoder.web.tc.model.SoftwareComponent;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.DataAccessConstants;
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.util.DBMS;
+import javax.servlet.http.HttpUtils;
 
 /**
  *
@@ -16,59 +27,148 @@ import java.util.Map;
 public class USDCContestDetails extends StatBase {
     
     String getCommandName() {
-        if(getRequest().getParameter("type").equals("1"))
-        {
-            //first winning submission
-            return "usdc_contest_details_first_winner";
+        Request dataRequest = new Request();
+        Map map = HttpUtils.parseQueryString(getRequest().getQueryString()); 
+        map.remove(Constants.MODULE_KEY); 
+        map.remove(DataAccessConstants.SORT_COLUMN);
+        map.remove(DataAccessConstants.SORT_DIRECTION);
+
+        try {
+            dataRequest.setProperties(map);
+            dataRequest.setContentHandle("usdc_contest_info");
+            DataAccessInt dai = getDataAccess(DBMS.TCS_OLTP_DATASOURCE_NAME, true);
+            Map result = dai.getData(dataRequest);
+            
+            ResultSetContainer rsc = (ResultSetContainer)result.get("contest_info");
+            
+            if(rsc.getIntItem(0, "prize_type_id") == 1)
+            {
+                return "usdc_contest_details_first_winner";
+            }
+            else if(rsc.getIntItem(0, "prize_type_id") == 2 || rsc.getIntItem(0, "prize_type_id") == 3)
+            {
+                return "usdc_contest_details";
+            }
+            else if(rsc.getIntItem(0, "prize_type_id") == 4)
+            {
+                return "usdc_contest_details_most_submissions";
+            }
+            else
+            {
+                return "usdc_contest_details_monthly";
+            }
+        } catch (TCWebException e) {
+            throw new TCWebException(e);
+        } catch (Exception e) {
+            throw new TCWebException(e);
         }
-        else if(getRequest().getParameter("type").equals("3"))
-        {
-            //most submissions (min 2, passing score)
-            return "usdc_contest_details_most_submissions";
-        }
-        else if(getRequest().getParameter("type").equals("4"))
-        {
-            //most submissions (min 2, passing score)
-            return "usdc_contest_details_monthly";
-        }
-        else
-            return "usdc_contest_details";
+            
+        return "usdc_contest_details";
     }
     
     String getDataSourceName() {
-        if(getRequest().getParameter("type").equals("4"))
-        {
-            return DBMS.TCS_OLTP_DATASOURCE_NAME;
+        Request dataRequest = new Request();
+        Map map = HttpUtils.parseQueryString(getRequest().getQueryString()); 
+        map.remove(Constants.MODULE_KEY); 
+        map.remove(DataAccessConstants.SORT_COLUMN);
+        map.remove(DataAccessConstants.SORT_DIRECTION);
+
+        try {
+            dataRequest.setProperties(map);
+            dataRequest.setContentHandle("usdc_contest_info");
+            DataAccessInt dai = getDataAccess(DBMS.TCS_OLTP_DATASOURCE_NAME, true);
+            Map result = dai.getData(dataRequest);
+            
+            ResultSetContainer rsc = (ResultSetContainer)result.get("contest_info");
+            
+            if(rsc.getIntItem(0, "prize_type_id") == 5 || rsc.getIntItem(0, "prize_type_id") == 6)
+            {
+                return DBMS.TCS_OLTP_DATASOURCE_NAME;
+            }
+            else
+            {
+                return DBMS.DW_DATASOURCE_NAME;
+            }
+        } catch (TCWebException e) {
+            throw new TCWebException(e);
+        } catch (Exception e) {
+            throw new TCWebException(e);
         }
-        else
-            return DBMS.DW_DATASOURCE_NAME;
+            
+        return DBMS.DW_DATASOURCE_NAME;
+        
     }
     
     String getPageName() {
-        if(getRequest().getParameter("type").equals("3"))
-        {
-            return "/dev/usdc_contest_det_most_submissions.jsp";
+                Request dataRequest = new Request();
+        Map map = HttpUtils.parseQueryString(getRequest().getQueryString()); 
+        map.remove(Constants.MODULE_KEY); 
+        map.remove(DataAccessConstants.SORT_COLUMN);
+        map.remove(DataAccessConstants.SORT_DIRECTION);
+
+        try {
+            dataRequest.setProperties(map);
+            dataRequest.setContentHandle("usdc_contest_info");
+            DataAccessInt dai = getDataAccess(DBMS.TCS_OLTP_DATASOURCE_NAME, true);
+            Map result = dai.getData(dataRequest);
+            
+            ResultSetContainer rsc = (ResultSetContainer)result.get("contest_info");
+            
+            if(rsc.getIntItem(0, "prize_type_id") == 4)
+            {
+                return "/dev/usdc_contest_det_most_submissions.jsp";
+            }
+            else if(rsc.getIntItem(0, "prize_type_id") == 5 || rsc.getIntItem(0, "prize_type_id") == 6)
+            {
+                return "/dev/usdc_contest_det_monthly.jsp";
+            }
+            else
+            {
+                return "/dev/usdc_contest_det.jsp";
+            }
+        } catch (TCWebException e) {
+            throw new TCWebException(e);
+        } catch (Exception e) {
+            throw new TCWebException(e);
         }
-        else if(getRequest().getParameter("type").equals("4"))
-        {
-            return "/dev/usdc_contest_det_monthly.jsp";
-        }
-        else
-            return "/dev/usdc_contest_det.jsp";
+            
+        return "/dev/usdc_contest_det.jsp";
+
     }
     
     void statProcessing() throws com.topcoder.web.common.TCWebException {
-        if(getRequest().getParameter("type").equals("1"))
-        {
-            //reposition result->resultMap->contest_results_only_winners to contest_results so that the page doesn't have to know what
-            //type of contest this is
-            Map result =  (Map)getRequest().getAttribute("resultMap");
+        
+        Request dataRequest = new Request();
+        Map map = HttpUtils.parseQueryString(getRequest().getQueryString()); 
+        map.remove(Constants.MODULE_KEY); 
+        map.remove(DataAccessConstants.SORT_COLUMN);
+        map.remove(DataAccessConstants.SORT_DIRECTION);
+
+        try {
+            dataRequest.setProperties(map);
+            dataRequest.setContentHandle("usdc_contest_info");
+            DataAccessInt dai = getDataAccess(DBMS.TCS_OLTP_DATASOURCE_NAME, true);
+            Map result = dai.getData(dataRequest);
             
-            result.put("contest_results", result.get("contest_results_only_winners"));
-            //result.remove("contest_results_only_winners");
+            ResultSetContainer rsc = (ResultSetContainer)result.get("contest_info");
             
-            getRequest().setAttribute("resultMap", result);
+            if(rsc.getIntItem(0, "prize_type_id") == 1 )
+            {
+                //reposition result->resultMap->contest_results_only_winners to contest_results so that the page doesn't have to know what
+                //type of contest this is
+                Map result =  (Map)getRequest().getAttribute("resultMap");
+
+                result.put("contest_results", result.get("contest_results_only_winners"));
+                //result.remove("contest_results_only_winners");
+
+                getRequest().setAttribute("resultMap", result);
+            }
+        } catch (TCWebException e) {
+            throw new TCWebException(e);
+        } catch (Exception e) {
+            throw new TCWebException(e);
         }
+            
     }
     
 }
