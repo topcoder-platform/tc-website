af539e238042821e3924430edd2252934027bea7
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-07-14 14:51:36 +0000

    screening changes

--- web_module/branches/screening_tool_6-15-04/src/main/com/topcoder/web/corp/controller/request/screening/Demographics.java
+++ web_module/branches/screening_tool_6-15-04/src/main/com/topcoder/web/corp/controller/request/screening/Demographics.java
@@ -10,6 +10,7 @@ import com.topcoder.web.corp.common.Constants;
 import com.topcoder.web.common.StringUtils;
 import com.topcoder.web.corp.controller.request.screening.BaseSessionProcessor;
 import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.corp.common.ScreeningException;
 /**
  *
  * @author  rfairfax
@@ -17,6 +18,12 @@ import com.topcoder.web.common.TCWebException;
 public class Demographics extends BaseScreeningProcessor {
     
     protected void screeningProcessing() throws com.topcoder.web.common.TCWebException {
+        String campaignId = getRequest().getParameter(Constants.CAMPAIGN_ID);
+        if (campaignId == null) {
+            // notify the user about the error
+            log.error("Campaign ID is not specified.");
+            throw new ScreeningException("No campaign ID had been specified.");
+        }
         
         setNextPage(Constants.DEMOGRAPHICS_PAGE);
         setIsNextPageInContext(true); 
