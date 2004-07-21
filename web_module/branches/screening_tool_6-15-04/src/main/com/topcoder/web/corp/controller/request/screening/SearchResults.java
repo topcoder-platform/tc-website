e29f0dd37802602c357fa6beb999a18fb0547ec1
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-07-21 18:39:59 +0000

    screening changes

--- web_module/branches/screening_tool_6-15-04/src/main/com/topcoder/web/corp/controller/request/screening/SearchResults.java
+++ web_module/branches/screening_tool_6-15-04/src/main/com/topcoder/web/corp/controller/request/screening/SearchResults.java
@@ -141,9 +141,8 @@ public class SearchResults extends BaseScreeningProcessor {
         r.setProperty("search"+DataAccessConstants.END_RANK, String.valueOf(ret.getEnd()));
 
         
-        DataAccessInt cda = Util.getDataAccess();
-        //CachedQueryDataAccess cda = new CachedQueryDataAccess(Constants.DATA_SOURCE);
-        //cda.setExpireTime(15 * 60 * 1000); //cache for 15 minutes
+        CachedQueryDataAccess cda = new CachedQueryDataAccess(Constants.DATA_SOURCE);
+        cda.setExpireTime(1); //cache for 15 minutes
         Map res = cda.getData(r);
         ResultSetContainer rsc = (ResultSetContainer) res.get("search");
         ResultSetContainer rscCount = (ResultSetContainer) res.get("count");
