4d78be49906791fa9518a7a8f19c0646e134a159
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-07-22 20:08:22 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/Constants.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/Constants.java
@@ -22,6 +22,8 @@ public class Constants {
 
     /* request parameters */
     public static String PROBLEM_ID;
+    public static String ROUND_ID;
+    public static String DIVISION_ID;
     public static String SURVEY_ID;
     public static String MODULE_KEY;
     public static String FIRST_NAME;
