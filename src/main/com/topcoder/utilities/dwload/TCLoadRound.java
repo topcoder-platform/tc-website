3f58bc84800cf9d2f373767b8558f7ace8391652
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-07-30 17:40:31 +0000

    *** empty log message ***

--- src/main/com/topcoder/utilities/dwload/TCLoadRound.java
+++ src/main/com/topcoder/utilities/dwload/TCLoadRound.java
@@ -1234,6 +1234,7 @@ public class TCLoadRound extends TCLoad {
             query.append("       ,(SELECT rtlu.round_type_desc ");       // 10
             query.append("           FROM round_type_lu rtlu ");
             query.append("          WHERE rtlu.round_type_id = r.round_type_id) ");
+            query.append("       ,r.short_name");
             query.append("  FROM round r ");
             query.append("       ,round_segment rs ");
             query.append(" WHERE r.round_id = ? ");
@@ -1257,10 +1258,11 @@ public class TCLoadRound extends TCLoad {
             query.append("       ,round_type_id ");    // 8
             query.append("       ,invitational  ");    // 9
             query.append("       ,notes         ");    // 10
-            query.append("       ,round_type_desc) "); // 11
+            query.append("       ,round_type_desc, "); //11
+            query.append("       ,short_name)"); // 12
             query.append("VALUES (");
             query.append("?,?,?,?,?,?,?,?,?,?,");
-            query.append("?)");
+            query.append("?,?)");
 
             psIns = prepareStatement(query.toString(), TARGET_DB);
 
@@ -1276,7 +1278,8 @@ public class TCLoadRound extends TCLoad {
             query.append("       ,invitational  = ? ");   // 8
             query.append("       ,notes = ?         ");   // 9
             query.append("       ,round_type_desc = ? "); // 10
-            query.append(" WHERE round_id = ? ");         // 11
+            query.append("       ,short_name = ? ");      // 11
+            query.append(" WHERE round_id = ? ");         // 12
             psUpd = prepareStatement(query.toString(), TARGET_DB);
 
             query = new StringBuffer(100);
@@ -1310,7 +1313,8 @@ public class TCLoadRound extends TCLoad {
                     psUpd.setInt(8, rs.getInt(8));        // invitational
                     psUpd.setString(9, rs.getString(9));     // notes
                     psUpd.setString(10, rs.getString(10));    // round_type_desc
-                    psUpd.setInt(11, rs.getInt(1));  // round_id
+                    psUpd.setString(11, rs.getString(11));   //shortname
+                    psUpd.setInt(12, rs.getInt(1));  // round_id
 
                     retVal = psUpd.executeUpdate();
                     count += retVal;
@@ -1332,6 +1336,7 @@ public class TCLoadRound extends TCLoad {
                     psIns.setInt(9, rs.getInt(8));        // invitational
                     psIns.setString(10, rs.getString(9));     // notes
                     psIns.setString(11, rs.getString(10));    // round_type_desc
+                    psIns.setString(12, rs.getString(11));  //short name
 
                     retVal = psIns.executeUpdate();
                     count += retVal;
