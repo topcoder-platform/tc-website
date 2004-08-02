30173fbd327a77c92b954249a7f3c417602ba3be
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-08-02 20:43:04 +0000

    software reg changes for tco

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/legacy/TaskDevelopment.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/legacy/TaskDevelopment.java
@@ -665,7 +665,7 @@ else if (command.equals("send")) {
     public static boolean tcoTermsCheck(long userId) throws Exception {
         Calendar c = Calendar.getInstance();
         
-        if(c.before(new GregorianCalendar(104, 7, 1)) || c.after(GregorianCalendar(104, 9, 23)))
+        if(c.before(new GregorianCalendar(104, 7, 1)) || c.after(new GregorianCalendar(104, 9, 23)))
         {
             return true;
         } 
