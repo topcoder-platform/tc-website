5b3dd3e1f27da0766a86cff5de6b2ba2438558c0
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-08-04 21:27:22 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/util/TCO04AlgorithmTermsAgree.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/util/TCO04AlgorithmTermsAgree.java
@@ -2,6 +2,8 @@ package com.topcoder.web.tc.controller.request.util;
 
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.TCRequest;
+import com.topcoder.web.common.TCResponse;
+import com.topcoder.web.common.security.WebAuthentication;
 
 import java.util.Calendar;
 
@@ -26,6 +28,34 @@ public class TCO04AlgorithmTermsAgree extends TermsAgreeBase {
     public void setRequest(TCRequest request) {
         super.setRequest(request);
         helper.setRequest(request);
+    }
+
+    /**
+     * We need this method so that we can set the response on our
+     * little helper processor.  We can't have multiple inheritance
+     * so we need this processor so that we can access some
+     * of the convenience methods in <cdoe>TermsBase</code>
+     * but we want to use the business logic
+     * contained in <code>TermsAgreeBase</code>
+     * @param response
+     */
+    public void setResponse(TCResponse response) {
+        super.setResponse(response);
+        helper.setResponse(response);
+    }
+
+    /**
+     * We need this method so that we can set the authentication on our
+     * little helper processor.  We can't have multiple inheritance
+     * so we need this processor so that we can access some
+     * of the convenience methods in <cdoe>TermsBase</code>
+     * but we want to use the business logic
+     * contained in <code>TermsAgreeBase</code>
+     * @param auth
+     */
+    public void setAuthentication(WebAuthentication auth) {
+        super.setAuthentication(auth);
+        helper.setAuthentication(auth);
     }
 
     protected Calendar getEnd() {
