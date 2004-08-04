cb2d8d041f5ee94c64c8be7e1f0a476e10c2bf97
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-08-04 21:27:22 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/tc/controller/request/util/TCO04ComponentTermsAgree.java
+++ src/main/com/topcoder/web/tc/controller/request/util/TCO04ComponentTermsAgree.java
@@ -1,6 +1,8 @@
 package com.topcoder.web.tc.controller.request.util;
 
 import com.topcoder.web.common.TCRequest;
+import com.topcoder.web.common.TCResponse;
+import com.topcoder.web.common.security.WebAuthentication;
 
 import java.util.Calendar;
 
@@ -26,6 +28,35 @@ public class TCO04ComponentTermsAgree extends TermsAgreeBase {
         super.setRequest(request);
         helper.setRequest(request);
     }
+
+    /**
+     * We need this method so that we can set the response on our
+     * little helper processor.  We can't have multiple inheritance
+     * so we need this processor so that we can access some
+     * of the convenience methods in <cdoe>TermsBase</code>
+     * but we want to use the business logic
+     * contained in <code>TermsAgreeBase</code>
+     * @param response
+     */
+    public void setResponse(TCResponse response) {
+        super.setResponse(response);
+        helper.setResponse(response);
+    }
+
+    /**
+     * We need this method so that we can set the authentication on our
+     * little helper processor.  We can't have multiple inheritance
+     * so we need this processor so that we can access some
+     * of the convenience methods in <cdoe>TermsBase</code>
+     * but we want to use the business logic
+     * contained in <code>TermsAgreeBase</code>
+     * @param auth
+     */
+    public void setAuthentication(WebAuthentication auth) {
+        super.setAuthentication(auth);
+        helper.setAuthentication(auth);
+    }
+
 
     protected Calendar getEnd() {
         return helper.getEnd();
