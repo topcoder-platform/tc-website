2260c85bec7472cc6959c9f34a48281bba5028a0
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-08-06 15:40:18 +0000

    search change

--- web_module/trunk/src/main/com/topcoder/web/corp/controller/request/screening/Search.java
+++ web_module/trunk/src/main/com/topcoder/web/corp/controller/request/screening/Search.java
@@ -57,8 +57,6 @@ public class Search extends BaseScreeningProcessor {
 
             List l = getQuestionList();
             Collections.sort(l);
-            
-            log.error("RYAN VAL IS " + l.size());
 
             ret.setQuestions(l);
         
