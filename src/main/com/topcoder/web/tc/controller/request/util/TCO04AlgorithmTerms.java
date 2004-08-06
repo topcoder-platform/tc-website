2b8055ea153db914d2d4b58a2a6a801b2e4cb04e
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-08-06 17:41:27 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/tc/controller/request/util/TCO04AlgorithmTerms.java
+++ src/main/com/topcoder/web/tc/controller/request/util/TCO04AlgorithmTerms.java
@@ -21,8 +21,8 @@ public class TCO04AlgorithmTerms extends TermsBase {
 
     public Calendar getBeginning() {
         Calendar beginning = Calendar.getInstance();
-        beginning.set(2004, Calendar.AUGUST, 9, 9, 0);
-        //beginning.set(2004, Calendar.JULY, 9, 9, 0);
+        //beginning.set(2004, Calendar.AUGUST, 9, 9, 0);
+        beginning.set(2004, Calendar.JULY, 9, 9, 0);
         return beginning;
     }
 
