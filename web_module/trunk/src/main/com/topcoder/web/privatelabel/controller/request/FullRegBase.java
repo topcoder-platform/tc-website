9ec7276bb25a061e1ec0a816e3b855b508cd82b3
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-08-13 16:27:23 +0000

    google reg

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/FullRegBase.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/FullRegBase.java
@@ -148,7 +148,6 @@ public abstract class FullRegBase extends SimpleRegBase {
 
     protected Map getQuestions() {
         try {
-            log.error(((FullRegInfo)regInfo));
             if (questions==null)
                 questions=getQuestions(transDb, ((FullRegInfo)regInfo).getCoderType(), Integer.parseInt(getRequestParameter(Constants.COMPANY_ID)));
         } catch (Exception e) {
