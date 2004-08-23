ca2a910a8c214231372a3b3684ce2505c08e34a9
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-08-23 20:56:32 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/tc/controller/MainServlet.java
+++ src/main/com/topcoder/web/tc/controller/MainServlet.java
@@ -44,9 +44,10 @@ public class MainServlet extends BaseServlet {
         if (request.getServerName().startsWith(ApplicationServer.SERVER_NAME))
             super.process(request, response);
         else {
+            log.error("Invalid Request - Wrong Server Name " + request.getServerName());
             response.setStatus(400);
             PrintWriter out = response.getWriter();
-            out.println("<html><head><title>Invalid Request</title></head>");
+            out.println("<html><head><title>IInvalid Request - Wrong Server Name</title></head>");
             out.println("<body><h4>Your request was invalid for this server.</h4>");
             out.println("</body></html>");
             out.flush();
--- src/main/com/topcoder/web/tc/controller/legacy/MainServlet.java
+++ src/main/com/topcoder/web/tc/controller/legacy/MainServlet.java
@@ -114,8 +114,10 @@ public final class MainServlet extends HttpServlet {
             //just trying to protect against invalid requests getting to the
             //app server.  currently, we don't have a way to stop this ad the web
             //server layer.
-            if (!request.getServerName().startsWith(ApplicationServer.SERVER_NAME))
+            if (!request.getServerName().startsWith(ApplicationServer.SERVER_NAME)) {
+                log.error(request.getRemoteHost() + " Made an Invalid Request - Wrong Server Name " + request.getServerName());
                 throw new NavigationException("Invalid Request - Wrong Server Name", TCServlet.NAVIGATION_ERROR_PAGE);
+            }
 
             // CHECK FOR SESSION TIMEOUT
             if (request.isRequestedSessionIdValid() == false && request.getRequestedSessionId() != null) {
