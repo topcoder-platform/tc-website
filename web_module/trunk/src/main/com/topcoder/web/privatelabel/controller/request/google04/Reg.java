c103de684c683bb594d2be78694134dec5e7f20f
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-08-26 20:08:03 +0000

    google student / pro fix

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/google04/Reg.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/google04/Reg.java
@@ -20,4 +20,17 @@ public class Reg extends FullReg {
         setIsNextPageInContext(true);
     }
     
+    protected ResultSetContainer getCountryList() throws TCWebException {
+        try {
+            Request request = new Request();
+            request.setContentHandle("country_list_google");
+            Map map = getDataAccess(transDb, true).getData(request);
+            if (map == null)
+                throw new Exception("error getting country list from db");
+            else
+                return (ResultSetContainer) map.get("country_list_google");
+        } catch (Exception e) {
+            throw new TCWebException(e);
+        }
+    }
 }
