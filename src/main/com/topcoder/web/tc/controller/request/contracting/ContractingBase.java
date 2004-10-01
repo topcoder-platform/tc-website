7fe57be1c005d716e8d2c99e021d9874d0647909
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-10-01 18:11:34 +0000

    Contracting changes

--- src/main/com/topcoder/web/tc/controller/request/contracting/ContractingBase.java
+++ src/main/com/topcoder/web/tc/controller/request/contracting/ContractingBase.java
@@ -11,6 +11,8 @@ import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.security.SessionPersistor;
 import com.topcoder.shared.security.Persistor;
 import com.topcoder.web.tc.Constants;
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.web.common.PermissionException;
 
 import com.topcoder.web.tc.model.ContractingInfo;
 /**
@@ -21,9 +23,13 @@ abstract public class ContractingBase extends BaseProcessor {
      
     protected Persistor p; 
     protected ContractingInfo info;
-    
+    protected static Logger log = Logger.getLogger(ContractingBase.class);
+
     protected void businessProcessing() throws TCWebException {
         try {
+            if (!userIdentified())
+                throw new PermissionException(getUser(), new ClassResource(this.getClass())); 
+            
             p = new SessionPersistor(getRequest().getSession(true));
 
             info = getInfoFromPersistor();
@@ -58,12 +64,17 @@ abstract public class ContractingBase extends BaseProcessor {
 
     protected ContractingInfo getInfoFromPersistor() {
         ContractingInfo info = null;
+        
+        log.debug("LOADING DATA FROM PERSISTOR");
+        
         info = (ContractingInfo)p.getObject(Constants.CONTRACTING_INFO);
         return info;
     }
     
     protected ContractingInfo getInfoFromDB() {
         ContractingInfo info = new ContractingInfo();
+        
+        log.debug("LOADING DATA FROM DB");
         
         info.setUserID(getUser().getId());
         
