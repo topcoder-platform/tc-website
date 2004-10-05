c72b44f37c40d3f9c21ecdb8d2afb1dfeaf21763
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-10-05 20:27:27 +0000

    contracting changes

--- web_module/trunk/src/main/com/topcoder/web/tc/view/tag/PreferenceInput.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/view/tag/PreferenceInput.java
@@ -75,6 +75,10 @@ public class PreferenceInput extends BaseTag {
         if(getDefaultValue() != null && getDefaultValue().equals(getTrueValue())) {
             s.append("checked ");
         }
+        else {
+            s.append("/>");
+            s.append(getDefaultValue());
+        }
         
         s.append("/>");
         return s.toString();
