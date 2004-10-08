23caa27cce65a547b01f8a3d9fbd7c07c4ce0b1d
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-10-08 18:58:25 +0000

    contracting changes

--- src/main/com/topcoder/web/tc/controller/request/contracting/Submit.java
+++ src/main/com/topcoder/web/tc/controller/request/contracting/Submit.java
@@ -19,6 +19,8 @@ import javax.transaction.UserTransaction;
 import com.topcoder.shared.util.*;
 
 import com.topcoder.web.ejb.user.UserPreference;
+import com.topcoder.web.ejb.resume.ResumeServices;
+import com.topcoder.web.ejb.coderskill.CoderSkill;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -39,37 +41,121 @@ public class Submit  extends ContractingBase {
                 InitialContext ctx = TCContext.getInitial();
                 UserPreference prefbean = (UserPreference)createEJB(ctx, UserPreference.class);
                 
-                //load pref group list, iterate through each one, deleting, updating
-                Request r = new Request();
-                r.setContentHandle("preference_groups");
+                if(info.isEdit()) {
+                    //load pref group list, iterate through each one, deleting, updating
+                    Request r = new Request();
+                    r.setContentHandle("preference_groups");
 
-                ResultSetContainer rsc = (ResultSetContainer)getDataAccess().getData(r).get("preference_groups");
-                for(int i = 0; i < rsc.size(); i++) {
-                    ResultSetContainer rscPrefs = prefbean.getPreferencesByGroup(info.getUserID(), rsc.getIntItem(i, "preference_group_id"),DBMS.COMMON_OLTP_DATASOURCE_NAME);
-                    for(int j = 0; j < rscPrefs.size(); j++) {
-                        //check if info object has this preference
-                        if(info.getPreference(rscPrefs.getStringItem(j, "preference_id")) == null) {
-                            //delete
-                            prefbean.removeUserPreference(info.getUserID(), rscPrefs.getIntItem(j, "preference_id"), DBMS.COMMON_OLTP_DATASOURCE_NAME);
-                        } else {
-                            //update
-                            prefbean.setPreferenceValueID(info.getUserID(), rscPrefs.getIntItem(j, "preference_id"),
-                                Integer.parseInt(info.getPreference(rscPrefs.getStringItem(j, "preference_id"))), DBMS.COMMON_OLTP_DATASOURCE_NAME);
-                            info.removePref(rscPrefs.getStringItem(j, "preference_id"));
+                    ResultSetContainer rsc = (ResultSetContainer)getDataAccess().getData(r).get("preference_groups");
+                    for(int i = 0; i < rsc.size(); i++) {
+                        ResultSetContainer rscPrefs = prefbean.getPreferencesByGroup(info.getUserID(), rsc.getIntItem(i, "preference_group_id"),DBMS.COMMON_OLTP_DATASOURCE_NAME);
+                        for(int j = 0; j < rscPrefs.size(); j++) {
+                            //check if info object has this preference
+                            if(info.getPreference(rscPrefs.getStringItem(j, "preference_id")) == null) {
+                                //delete
+                                prefbean.removeUserPreference(info.getUserID(), rscPrefs.getIntItem(j, "preference_id"), DBMS.COMMON_OLTP_DATASOURCE_NAME);
+                            } else {
+                                //update
+                                prefbean.setPreferenceValueID(info.getUserID(), rscPrefs.getIntItem(j, "preference_id"),
+                                    Integer.parseInt(info.getPreference(rscPrefs.getStringItem(j, "preference_id"))), DBMS.COMMON_OLTP_DATASOURCE_NAME);
+                                info.removePref(rscPrefs.getStringItem(j, "preference_id"));
+                            }
                         }
                     }
                 }
                 //insert
-                Iterator i = info.getPreferenceNames();
-                while(i.hasNext()) {
-                    String s = (String)i.next();
+                Iterator it = info.getPreferenceNames();
+                while(it.hasNext()) {
+                    String s = (String)it.next();
                     prefbean.createUserPreference(info.getUserID(), Integer.parseInt(s), DBMS.COMMON_OLTP_DATASOURCE_NAME);
                     prefbean.setPreferenceValueID(info.getUserID(), Integer.parseInt(s), Integer.parseInt(info.getPreference(s)), DBMS.COMMON_OLTP_DATASOURCE_NAME);
                 }
                 
                 //resume
+                if(info.getResume() != null)
+                {
+                    byte[] fileBytes = null;
+                    String fileName = "";
+                    int fileType = -1;
+
+                    fileBytes = new byte[(int) info.getResume().getSize()];
+                    info.getResume().getInputStream().read(fileBytes);
+                    if (fileBytes == null || fileBytes.length == 0) {
+                        //ignore this
+                    }
+                    else {
+                        //fileType = Integer.parseInt(file.getParameter("fileType"));
+                        Map types = getFileTypes();
+                        if(types.containsKey(info.getResume().getContentType()) )
+                        {
+                            log.debug("FOUND TYPE");
+                            fileType = ((Long) types.get(info.getResume().getContentType())).intValue();
+                        }
+                        else
+                        {
+                            log.debug("DID NOT FIND TYPE " + info.getResume().getContentType());
+                        }
+                        fileName = info.getResume().getRemoteFileName();
+                        ResumeServices resumeServices = (ResumeServices) createEJB(getInitialContext(), ResumeServices.class);
+                        resumeServices.putResume(info.getUserID(), fileType, fileName, fileBytes, DBMS.OLTP_DATASOURCE_NAME);
+                    }
+                }
                 
                 //skills
+                CoderSkill skillbean = (CoderSkill)createEJB(ctx, CoderSkill.class);
+                
+                if(info.isEdit()) {
+                    Hashtable edits = new Hashtable();
+                    ArrayList deletes = new ArrayList();
+                    
+                    //load skill type list, iterate through each one, deleting, updating
+                    Request r = new Request();
+                    r.setContentHandle("skill_types");
+
+                    ResultSetContainer rsc = (ResultSetContainer)getDataAccess().getData(r).get("skill_types");
+                    for(int i = 0; i < rsc.size(); i++) {
+                        ResultSetContainer rscSkills = skillbean.getSkillsByType(info.getUserID(), rsc.getIntItem(i, "skill_type_id"),DBMS.OLTP_DATASOURCE_NAME);
+                        for(int j = 0; j < rscSkills.size(); j++) {
+                            //check if info object has this skill
+                            if(info.getSkill(rscSkills.getStringItem(j, "skill_id")) == null) {
+                                //delete
+                                deletes.add(rscSkills.getStringItem(j, "skill_id"));
+                            } else {
+                                //update
+                                edits.put(rscSkills.getStringItem(j, "skill_id"), info.getSkill(rscSkills.getStringItem(j, "skill_id")));
+                                info.removeSkill(rscSkills.getStringItem(j, "skill_id"));
+                            }
+                        }
+                    }
+                    
+                    //process bulk deletes / updates
+                    int[] deleteArray = new int[deletes.size()];
+                    for(int i = 0; i < deletes.size(); i++) {
+                        deleteArray[i] = Integer.parseInt((String)deletes.get(i));
+                    }
+                    skillbean.bulkRemoveCoderSkill(info.getUserID(), deleteArray, DBMS.OLTP_DATASOURCE_NAME);
+                    
+                    int[] updateSkillIdArray = new int[edits.keySet().size()];
+                    int[] updateRankingArray = new int[edits.keySet().size()];
+                    Iterator t = edits.keySet().iterator();
+                    for(int i = 0; i < edits.keySet().size(); i++) {
+                        String s = (String)t.next();
+                        updateSkillIdArray[i] = Integer.parseInt(s);
+                        updateRankingArray[i] = Integer.parseInt((String)edits.get(s));
+                    }
+                    skillbean.bulkSetRanking(info.getUserID(), updateSkillIdArray, updateRankingArray, DBMS.OLTP_DATASOURCE_NAME);
+                }
+                
+                //inserts
+                int[] insertSkillIdArray = new int[info.getSkillCount()];
+                int[] insertRankingArray = new int[info.getSkillCount()];
+                it = info.getSkillNames();
+                for(int i = 0; i < info.getSkillCount(); i++) {
+                    String s = (String)it.next();
+                    insertSkillIdArray[i] = Integer.parseInt(s);
+                    insertRankingArray[i] = Integer.parseInt(info.getSkill(s));
+                }
+                skillbean.bulkCreateCoderSkill(info.getUserID(), insertSkillIdArray, insertRankingArray, DBMS.OLTP_DATASOURCE_NAME);
                 
                 //notes
                 
@@ -84,6 +170,21 @@ public class Submit  extends ContractingBase {
         } catch(Exception e) {
             throw new TCWebException(e);
         }
+    }
+    
+    protected Map getFileTypes() throws Exception {
+        Request r = new Request();
+        r.setContentHandle("file_types");
+        Map qMap = getDataAccess().getData(r);
+        ResultSetContainer questions = (ResultSetContainer) qMap.get("file_types");
+        ResultSetContainer.ResultSetRow row = null;
+
+        Map ret = new HashMap();
+        for (Iterator it = questions.iterator(); it.hasNext();) {
+            row = (ResultSetContainer.ResultSetRow) it.next();
+            ret.put(row.getStringItem("mime_type"), new Long( row.getLongItem("file_type_id")) );
+        }
+        return ret;
     }
 
     protected void setNextPage() {
