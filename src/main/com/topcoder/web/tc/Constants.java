2bfae8e4274a457fdfc720beef58cea9d4b4ae8a
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-11-18 14:20:42 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/tc/Constants.java
+++ src/main/com/topcoder/web/tc/Constants.java
@@ -125,6 +125,7 @@ public class Constants {
     public static int FLASH_CATALOG_ID;
     public static int APPLICATIONS_CATALOG_ID;
     public static int INTERNAL_NOTE_TYPE_ID;
+    public static int NOTE_TEXT;
     public static int MAX_RATED_INQUIRIES;
     public static int MAX_UNRATED_INQUIRIES;
     public static int REVIEWER_TERMS_ID;
