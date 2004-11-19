9baf541dd1a56dbbf9ecc71210c1cd71480704bb
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-11-19 14:41:36 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/contracting/Confirm.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/contracting/Confirm.java
@@ -6,7 +6,6 @@
 
 package com.topcoder.web.tc.controller.request.contracting;
 
-import com.topcoder.web.tc.model.ContractingInfo;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.tc.Constants;
 
@@ -24,7 +23,7 @@ import com.topcoder.shared.util.DBMS;
  * @author  rfairfax
  */
 public class Confirm  extends ContractingBase {
-    
+
     protected void contractingProcessing() throws TCWebException {
         try {
             //load preference groups
@@ -44,11 +43,11 @@ public class Confirm  extends ContractingBase {
                 rpref.setProperty("prid", String.valueOf(rsc.getIntItem(i, "preference_group_id")));
 
                 ContractingResponseGroup g = new ContractingResponseGroup();
-                
+
                 g.setName(name);
-                
+
                 ResultSetContainer rscPref = (ResultSetContainer)getDataAccess().getData(rpref).get("preferences_by_group");
-                for(int j = 0; j < rscPref.size(); j++) {                   
+                for(int j = 0; j < rscPref.size(); j++) {
 
                     String text = rscPref.getStringItem(j, "preference_desc");
                     int type =rscPref.getIntItem(j, "preference_type_id");
@@ -57,7 +56,7 @@ public class Confirm  extends ContractingBase {
                     if(type != Constants.PREFERENCE_SINGLE_ANSWER && info.getPreference(String.valueOf(id)) != null) {
                         //look up answer
                         String answer = "";
-                        
+
                         Request rval = new Request();
                         rval.setContentHandle("preference_values");
                         rval.setProperty("prid", String.valueOf(rscPref.getIntItem(j, "preference_id")));
@@ -71,21 +70,21 @@ public class Confirm  extends ContractingBase {
                             }
 
                         }
-                        
+
                         ContractingResponse rsp = new ContractingResponse();
                         rsp.setName(text);
                         rsp.setVal(answer);
-                        
+
                         g.addResponse(rsp);
                     }
-                    
+
                 }
-                
+
                 if(g.getResponses().size() != 0) {
                     groups.add(g);
                 }
             }
-            
+
             getRequest().setAttribute("prefs", groups);
 
             //resume status
@@ -103,7 +102,7 @@ public class Confirm  extends ContractingBase {
 
             //tech skill
             ArrayList techSkills = new ArrayList();
-            
+
             //load skill list from db
             r = new Request();
             r.setContentHandle("skill_list");
@@ -118,16 +117,17 @@ public class Confirm  extends ContractingBase {
                     ContractingResponse resp = new ContractingResponse();
                     resp.setName(text);
                     resp.setVal(info.getSkill(String.valueOf(id)));
-                    
-                    techSkills.add(resp);                
+
+                    techSkills.add(resp);
                 }
             }
-            
+
             getRequest().setAttribute("techSkills", techSkills);
-            
+            getRequest().setAttribute("techNote", getNote(Constants.TECH_NOTE_TYPE_ID, getUser().getId()));
+
             //database skill
             ArrayList dbSkills = new ArrayList();
-            
+
             //load skill list from db
             r = new Request();
             r.setContentHandle("skill_list");
@@ -142,16 +142,17 @@ public class Confirm  extends ContractingBase {
                     ContractingResponse resp = new ContractingResponse();
                     resp.setName(text);
                     resp.setVal(info.getSkill(String.valueOf(id)));
-                    
-                    dbSkills.add(resp);                
+
+                    dbSkills.add(resp);
                 }
             }
-            
+
             getRequest().setAttribute("dbSkills", dbSkills);
-        
+            getRequest().setAttribute("dbNote", getNote(Constants.DB_NOTE_TYPE_ID, getUser().getId()));
+
             //languages skill
             ArrayList langSkills = new ArrayList();
-            
+
             //load skill list from db
             r = new Request();
             r.setContentHandle("skill_list");
@@ -166,16 +167,17 @@ public class Confirm  extends ContractingBase {
                     ContractingResponse resp = new ContractingResponse();
                     resp.setName(text);
                     resp.setVal(info.getSkill(String.valueOf(id)));
-                    
-                    langSkills.add(resp);                
+
+                    langSkills.add(resp);
                 }
             }
-            
+
             getRequest().setAttribute("langSkills", langSkills);
-            
+            getRequest().setAttribute("langNote", getNote(Constants.LANGUAGE_NOTE_TYPE_ID, getUser().getId()));
+
             //os skill
             ArrayList osSkills = new ArrayList();
-            
+
             //load skill list from db
             r = new Request();
             r.setContentHandle("skill_list");
@@ -190,16 +192,17 @@ public class Confirm  extends ContractingBase {
                     ContractingResponse resp = new ContractingResponse();
                     resp.setName(text);
                     resp.setVal(info.getSkill(String.valueOf(id)));
-                    
-                    osSkills.add(resp);                
+
+                    osSkills.add(resp);
                 }
             }
-            
+
             getRequest().setAttribute("osSkills", osSkills);
-            
+            getRequest().setAttribute("osNote", getNote(Constants.OS_NOTE_TYPE_ID, getUser().getId()));
+
             //industries skill
             ArrayList industrySkills = new ArrayList();
-            
+
             //load skill list from db
             r = new Request();
             r.setContentHandle("skill_list");
@@ -214,13 +217,14 @@ public class Confirm  extends ContractingBase {
                     ContractingResponse resp = new ContractingResponse();
                     resp.setName(text);
                     resp.setVal(info.getSkill(String.valueOf(id)));
-                    
-                    industrySkills.add(resp);                
+
+                    industrySkills.add(resp);
                 }
             }
-            
+
             getRequest().setAttribute("industrySkills", industrySkills);
-            
+            getRequest().setAttribute("industryNote", getNote(Constants.INDUSTRY_NOTE_TYPE_ID, getUser().getId()));
+
         } catch(TCWebException tce) {
             throw tce;
         } catch(Exception e) {
@@ -228,10 +232,20 @@ public class Confirm  extends ContractingBase {
         }
     }
 
+
+    protected String getNote(int noteTypeId, long userId) throws Exception {
+        Request r = new Request();
+        r.setContentHandle("skill_note");
+        r.setProperty(Constants.USER_ID, String.valueOf(userId));
+        ResultSetContainer skillNote = (ResultSetContainer)getDataAccess().getData(r).get("skill_note");
+        return skillNote.getStringItem(0, "text")==null?"":skillNote.getStringItem(0, "text");
+
+    }
+
     protected void setNextPage() {
-        
-        setNextPage(Constants.CONTRACTING_CONFIRM_PAGE); 
+
+        setNextPage(Constants.CONTRACTING_CONFIRM_PAGE);
         setIsNextPageInContext(true);
     }
-    
+
 }
