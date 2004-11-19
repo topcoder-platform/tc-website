6daa5e3272d911067bfd3ab62b33c12d5ae69130
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-11-19 22:19:12 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/ejb/requestservices/RequestServicesBean.java
+++ web_module/trunk/src/main/com/topcoder/web/ejb/requestservices/RequestServicesBean.java
@@ -1,9 +1,15 @@
 package com.topcoder.web.ejb.requestservices;
 
 import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.shared.util.DBMS;
 import com.topcoder.web.ejb.BaseEJB;
 
 import javax.ejb.EJBException;
+import javax.naming.InitialContext;
+import java.sql.Timestamp;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * User: dok
@@ -14,30 +20,58 @@ public class RequestServicesBean extends BaseEJB {
 
     private final static Logger log = Logger.getLogger(RequestServicesBean.class);
 
-    public void createRequest(long userId, String url, String dataSource) {
+    public void createRequest(long userId, String url, Timestamp time, String dataSource) {
         log.debug("createRequest called. url: " + url
-                + " userId: " + userId);
-        int ret = insert("request",
-                new String[] {"user_id", "url"},
-                new String[] {String.valueOf(userId), String.valueOf(url)},
-                dataSource);
-        if (ret != 1) {
-            throw(new EJBException("Wrong number of rows inserted into " +
-                    "'request'. Inserted " + ret + ", " +
-                    "should have inserted 1."));
+                + " userId: " + userId + " time: " + time);
+
+        StringBuffer query = new StringBuffer(200);
+        query.append("insert into request (user_id, url, create_date) ");
+        query.append(" values (?, ?, ?)");
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        InitialContext ctx = null;
+        try {
+            conn = DBMS.getConnection(dataSource);
+            ps = conn.prepareStatement(query.toString());
+            ps.setLong(1, userId);
+            ps.setString(2, url);
+            ps.setTimestamp(3, time);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            DBMS.printSqlException(true, e);
+            throw(new EJBException(e.getMessage()));
+        } finally {
+            close(ps);
+            close(conn);
+            close(ctx);
         }
     }
 
-    public void createRequest(String url, String dataSource) {
-        log.debug("createRequest called. url: " + url);
-        int ret = insert("request",
-                new String[] {"url"},
-                new String[] {String.valueOf(url)},
-                dataSource);
-        if (ret != 1) {
-            throw(new EJBException("Wrong number of rows inserted into " +
-                    "'request'. Inserted " + ret + ", " +
-                    "should have inserted 1."));
+    public void createRequest(String url, Timestamp time, String dataSource) {
+        log.debug("createRequest called. url: " + url + " time: " + time);
+        StringBuffer query = new StringBuffer(200);
+        query.append("insert into request (url, create_date) ");
+        query.append(" values (?, ?)");
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        InitialContext ctx = null;
+        try {
+            conn = DBMS.getConnection(dataSource);
+            ps = conn.prepareStatement(query.toString());
+            ps.setString(1, url);
+            ps.setTimestamp(2, time);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            DBMS.printSqlException(true, e);
+            throw(new EJBException(e.getMessage()));
+        } finally {
+            close(ps);
+            close(conn);
+            close(ctx);
         }
     }
 
