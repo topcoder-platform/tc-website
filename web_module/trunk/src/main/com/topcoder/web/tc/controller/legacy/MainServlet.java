05a410f5f654ccee4d83aabbfa5dfd35c54d9f32
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-11-23 20:04:06 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/legacy/MainServlet.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/legacy/MainServlet.java
@@ -142,7 +142,7 @@ public final class MainServlet extends HttpServlet {
             requestTask = request.getParameter("t");
             requestCommand = request.getParameter("c");
 
-            if (!"sponsor_image".equals(requestCommand.toLowerCase())) {
+            if (!"sponsor_image".equals(requestCommand)) {
                 WebAuthentication authentication = new BasicAuthentication(
                         new SessionPersistor(session),
                         tcRequest,
