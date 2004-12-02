aa9d43f3eb8ee7ef5f95358a172eecaba9ee1fe7
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-12-02 20:40:54 +0000

    *** empty log message ***

--- web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/common/BaseServlet.java
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/common/BaseServlet.java
@@ -104,7 +104,7 @@ public abstract class BaseServlet extends HttpServlet {
             TCSubject user = getUser(authentication.getActiveUser().getId());
             info = createSessionInfo(tcRequest, authentication, user.getPrincipals());
             tcRequest.setAttribute(SESSION_INFO_KEY, info);
-
+            RequestTracker.trackRequest(authentication.getActiveUser(), tcRequest);
 
             StringBuffer loginfo = new StringBuffer(100);
             loginfo.append("[**** ");
@@ -195,9 +195,6 @@ public abstract class BaseServlet extends HttpServlet {
 
     protected RequestProcessor callProcess(String processorName, TCRequest request, TCResponse response,
                                            WebAuthentication authentication) throws Exception {
-
-        RequestTracker.trackRequest(authentication.getActiveUser(), request);
-
         RequestProcessor rp = null;
 
         rp = (RequestProcessor) Class.forName(processorName).newInstance();
