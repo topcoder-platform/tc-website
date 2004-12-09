5eaa858216240ab40c600e72ba4c47a184ef8ea8
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-12-09 16:51:43 +0000

    *** empty log message ***

--- web_module/branches/pdf_profiles/src/main/com/topcoder/web/tc/controller/request/profile/PlacementProfile.java
+++ web_module/branches/pdf_profiles/src/main/com/topcoder/web/tc/controller/request/profile/PlacementProfile.java
@@ -8,8 +8,16 @@ package com.topcoder.web.tc.controller.request.profile;
 
 import com.topcoder.web.common.BaseProcessor;
 import com.topcoder.web.common.TCWebException;
-
+import com.topcoder.web.common.StringUtils;
 import com.topcoder.web.tc.Constants;
+import com.topcoder.web.tc.model.PlacementProfileSearchResult;
+
+import com.topcoder.shared.dataAccess.*;
+import com.topcoder.shared.dataAccess.resultSet.*;
+import com.topcoder.shared.util.DBMS;
+
+import java.util.ArrayList;
+import java.util.Map;
 /**
  *
  * @author rfairfax
@@ -18,7 +26,64 @@ public class PlacementProfile extends BaseProcessor {
     
 
     protected void businessProcessing() throws TCWebException {
+        //look for search values
+        String handle = "";
+        String firstname = "";
+        String lastname = "";
+        
+        handle = StringUtils.checkNull(getRequest().getParameter("handle"));
+        firstname = StringUtils.checkNull(getRequest().getParameter("firstname"));
+        lastname = StringUtils.checkNull(getRequest().getParameter("lastname"));
+        
+        setDefault("handle", handle);
+        setDefault("firstname", firstname);
+        setDefault("lastname", lastname);
+        
+        if(!handle.equals("") || !firstname.equals("") || !lastname.equals("")) {
+            //search
+            StringBuilder sb = new StringBuilder();
+            sb.append("SELECT u.user_id, u.handle, c.first_name, c.last_name ");
+            sb.append("FROM user u, coder c, common_oltp:user_preference p ");
+            sb.append("WHERE c.coder_id = u.user_id ");
+            sb.append("and p.user_id = u.user_id ");
+            sb.append("and p.preference_id in (2,7) ");
+            if(!handle.equals("")) {
+                sb.append("and lower(u.handle) like '" + handle + "' ");
+            }
+            if(!firstname.equals("")) {
+                sb.append("and lower(c.first_name) like '" + firstname + "' ");
+            }
+            if(!lastname.equals("")) {
+                sb.append("and lower(c.last_name) like '" + lastname + "' ");
+            }
+            sb.append("GROUP BY 1,2,3,4 ");
+            
+            QueryRequest r = new QueryRequest();
+            r.addQuery("search", sb.toString());
+            
+            Map m = getDataAccess().getData(r);
+            ResultSetContainer rsc = (ResultSetContainer)m.get("search");
+            
+            ArrayList results = new ArrayList();
+            
+            for(int i = 0; i < rsc.size(); i++) {
+                results.add(new PlacementProfileSearchResult(rsc.getIntItem(i, "user_id"), rsc.getStringItem(i, "handle"),
+                                            rsc.getStringItem(i, "first_name"), rsc.getStringItem(i, "last_name")));
+            }
+            
+            getRequest().setAttribute("results", results);
+            
+        }
+        
+        //TODO: load recent profiles list
+        
         setNextPage(Constants.PROFILE_MAIN_PAGE); 
         setIsNextPageInContext(true);
+    }
+    
+    protected static DataAccessInt getDataAccess() throws Exception { 
+       DataAccessInt dAccess = null;
+       dAccess = new DataAccess(DBMS.OLTP_DATASOURCE_NAME);
+       return dAccess;
     }
 }
