cc160a566257bcfae95dc5028d5a6c411e3cea2f
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-12-09 20:08:12 +0000

    *** empty log message ***

--- web_module/branches/pdf_profiles/src/main/com/topcoder/web/tc/controller/request/profile/ProfileConfig.java
+++ web_module/branches/pdf_profiles/src/main/com/topcoder/web/tc/controller/request/profile/ProfileConfig.java
@@ -12,20 +12,24 @@ import com.topcoder.web.common.StringUtils;
 import com.topcoder.web.tc.Constants;
 import com.topcoder.web.tc.model.PlacementConfigInfo;
 
+import com.topcoder.web.ejb.user.User;
+
 import com.topcoder.shared.dataAccess.*;
 import com.topcoder.shared.dataAccess.resultSet.*;
 import com.topcoder.shared.util.DBMS;
+import com.topcoder.shared.util.TCContext;
 
 import java.lang.StringBuffer;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import java.util.ArrayList;
 import java.util.Map;
 /**
  *
  * @author rfairfax
  */
-public class ProfileConfig extends BaseProcessor {
-    
+public class ProfileConfig extends BaseProcessor { 
 
     protected void businessProcessing() throws TCWebException {
         try {
@@ -35,6 +39,12 @@ public class ProfileConfig extends BaseProcessor {
             PlacementConfigInfo info = new PlacementConfigInfo();
             
             info.setUserID(uid);
+            
+            InitialContext ctx = TCContext.getInitial();
+            User userbean = (User)createEJB(ctx, User.class);
+            
+            info.setHandle(userbean.getHandle(uid, DBMS.OLTP_DATASOURCE_NAME));
+            info.setName(userbean.getFirstName(uid, DBMS.OLTP_DATASOURCE_NAME) + " " + userbean.getLastName(uid, DBMS.OLTP_DATASOURCE_NAME));
             
             getRequest().setAttribute("info", info);
 
@@ -46,9 +56,9 @@ public class ProfileConfig extends BaseProcessor {
         
     }
     
-    protected static DataAccessInt getDataAccess() throws Exception { 
+    protected static DataAccessInt getDWDataAccess() throws Exception { 
        DataAccessInt dAccess = null;
-       dAccess = new QueryDataAccess(DBMS.OLTP_DATASOURCE_NAME);
+       dAccess = new DataAccess(DBMS.DW_DATASOURCE_NAME);
        return dAccess;
     }
 }
