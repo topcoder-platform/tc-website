55864a0e9caa3b607c39e22b3c03bf49866652ef
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-12-10 20:54:59 +0000

    *** empty log message ***

--- web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/common/TCWebException.java
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/common/TCWebException.java
@@ -1,31 +1,14 @@
 package com.topcoder.web.common;
 
-import java.io.PrintWriter;
-import java.io.PrintStream;
+import com.topcoder.shared.util.TCException;
 
 /**
  * BaseProcessor exception for TCWeb Tool
- * 
+ *
  * @author Grimicus
  * @version 1.0
  */
-public class TCWebException extends Exception {
-    /** 
-     * Line separator for the local OS
-     */
-    private static final String SEPARATOR =
-            System.getProperty("line.separator");
-
-    /** 
-     * String constant for delimiting this exception and its nested one. 
-     */
-    private static final String NESTED_STRING =
-            SEPARATOR + SEPARATOR + " NESTED WITHIN: ";
-
-    /**
-     * The nested exception.  The exception that this class is wrapped around.
-     */
-    private Throwable nestedException;
+public class TCWebException extends TCException {
 
     /**
      * Default Constructor
@@ -53,8 +36,7 @@ public class TCWebException extends Exception {
      * @param nestedException the nested exception
      */
     public TCWebException(Throwable nestedException) {
-        super();
-        this.nestedException = nestedException;
+        super(nestedException);
     }
 
     /**
@@ -66,100 +48,7 @@ public class TCWebException extends Exception {
      * @param nestedException the nested exception
      */
     public TCWebException(String message, Throwable nestedException) {
-        super(message);
-        this.nestedException = nestedException;
+        super(message, nestedException);
     }
 
-    /** 
-     *  Set to an exception that should be nested
-     *  
-     * @param val The throwable to be nested
-     */
-    protected void setNestedException(Throwable val) {
-        nestedException = val;
-    }
-
-    /**
-     * <p>
-     * Gets the nested exception
-     * </p>
-     *
-     * @return Throwable the nested exception
-     */
-    public Throwable getNestedException() {
-        return nestedException;
-    }
-
-    /**
-     * <p>
-     * Prints the message for this exception including any nested exceptions.
-     * Nested Exception messages are printed at the top to show the deepest
-     * error first.
-     * </p>
-     * 
-     * @return The string holding the messages of all nested exceptions and 
-     *         this one.
-     */
-    public String getNestedMessage() {
-        String output = null;
-        String msg = null;
-        String msg2 = super.getMessage();
-
-        if (nestedException != null) {
-            msg = nestedException.getMessage();
-        }
-
-        if (msg != null) {
-            if (msg2 != null) {
-                output = msg + NESTED_STRING + msg2;
-            } else {
-                output = msg;
-            }
-        } else {
-            if (msg2 != null) {
-                output = msg2;
-            } else {
-                output = null;
-            }
-        }
-
-        return output;
-    }
-
-    /**
-     * Prints the stack trace to standard error
-     */
-    public void printStackTrace() {
-        printStackTrace(System.err);
-    }
-
-    /**
-     * Prints the given stack trace to a given PrintWriter object.
-     * Nested Exception's stack trace is printed first so that the
-     * deepest exception (i.e. the actual error), is shown first.
-     *
-     * @param writer PrintWriter containing stack trace
-     */
-    public void printStackTrace(PrintWriter writer) {
-        if (nestedException != null) {
-            nestedException.printStackTrace(writer);
-            writer.println(NESTED_STRING);
-        }
-        super.printStackTrace(writer);
-    }
-
-    /**
-     * Prints the given stack trace to a PrintStream object.
-     * Nested Exception's stack trace is printed first so that the
-     * deepest exception (i.e. the actual error), is shown first.
-     *
-     * @param stream PrintStream containing stack trace
-     */
-    public void printStackTrace(PrintStream stream) {
-        if (nestedException != null) {
-            nestedException.printStackTrace(stream);
-            stream.println(NESTED_STRING);
-        }
-        super.printStackTrace(stream);
-    }
 }
