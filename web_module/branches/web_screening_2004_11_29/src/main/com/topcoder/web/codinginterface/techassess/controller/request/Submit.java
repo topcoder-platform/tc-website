32a5c1de1c356c34ab64a512ef1bbc2e184a39f9
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-12-13 21:02:21 +0000

    *** empty log message ***

---
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/Submit.java
@@ -0,0 +1,50 @@
+package com.topcoder.web.codinginterface.techassess.controller.request;
+
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.codinginterface.techassess.Constants;
+import com.topcoder.shared.util.TCException;
+import com.topcoder.shared.netCommon.screening.request.ScreeningSubmitRequest;
+
+import javax.jms.ObjectMessage;
+
+/**
+ * User: dok
+ * Date: Dec 6, 2004
+ * Time: 11:17:58 AM
+ */
+public class Submit extends Base {
+
+
+    protected void businessProcessing() throws TCException {
+
+        //todo figure out resubmits and build appropriate request
+        long componentId = 0;
+        int problemTypeId = 0;
+
+        if (hasParameter(Constants.COMPONENT_ID)) {
+            componentId = Long.parseLong(getRequest().getParameter(Constants.COMPONENT_ID).trim());
+        } else {
+            throw new NavigationException("Invalid Request, missing parameter");
+        }
+
+        if (hasParameter(Constants.PROBLEM_TYPE_ID)) {
+            problemTypeId = Integer.parseInt(getRequest().getParameter(Constants.PROBLEM_TYPE_ID).trim());
+        } else {
+            throw new NavigationException("Invalid Request, missing parameter");
+        }
+
+        ScreeningSubmitRequest request = new ScreeningSubmitRequest(componentId, problemTypeId);
+        request.setServerID(Constants.SERVER_ID);
+
+        String messageId = send(request);
+
+        ObjectMessage response = receive(2000, messageId);
+
+
+
+
+        setNextPage(Constants.PAGE_INDEX);
+        setIsNextPageInContext(true);
+    }
+
+}
