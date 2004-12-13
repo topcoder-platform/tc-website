c5b8aabef6578c65a041d2910b73c5ade87e20c2
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-12-13 21:15:33 +0000

    *** empty log message ***

---
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/Test.java
@@ -0,0 +1,57 @@
+package com.topcoder.web.codinginterface.techassess.controller.request;
+
+import com.topcoder.web.common.BaseProcessor;
+import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.codinginterface.techassess.Constants;
+import com.topcoder.shared.netCommon.screening.request.ScreeningSubmitRequest;
+import com.topcoder.shared.netCommon.screening.request.ScreeningTestRequest;
+import com.topcoder.shared.util.TCException;
+
+import javax.jms.ObjectMessage;
+import java.util.ArrayList;
+
+/**
+ * User: dok
+ * Date: Dec 6, 2004
+ * Time: 11:17:41 AM
+ */
+public class Test extends Base {
+
+
+    protected void businessProcessing() throws TCException {
+
+
+        long componentId = 0;
+        int problemTypeId = 0;
+        ArrayList args = null;
+
+        if (hasParameter(Constants.COMPONENT_ID)) {
+            componentId = Long.parseLong(getRequest().getParameter(Constants.COMPONENT_ID).trim());
+        } else {
+            throw new NavigationException("Invalid Request, missing parameter");
+        }
+
+        if (hasParameter(Constants.PROBLEM_TYPE_ID)) {
+            problemTypeId = Integer.parseInt(getRequest().getParameter(Constants.PROBLEM_TYPE_ID).trim());
+        } else {
+            throw new NavigationException("Invalid Request, missing parameter");
+        }
+
+        //todo get the args
+
+
+
+        ScreeningTestRequest request = new ScreeningTestRequest(args, componentId, problemTypeId);
+        request.setServerID(Constants.SERVER_ID);
+
+        String messageId = send(request);
+
+        ObjectMessage response = receive(2000, messageId);
+
+
+        setNextPage(Constants.PAGE_INDEX);
+        setIsNextPageInContext(true);
+    }
+
+}
