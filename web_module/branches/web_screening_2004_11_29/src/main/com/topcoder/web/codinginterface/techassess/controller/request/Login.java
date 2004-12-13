232026f0f617ee1379386ad3e128708dae548412
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-12-13 22:53:09 +0000

    *** empty log message ***

--- web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/Login.java
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/Login.java
@@ -3,8 +3,16 @@ package com.topcoder.web.codinginterface.techassess.controller.request;
 import com.topcoder.web.common.NavigationException;
 import com.topcoder.web.codinginterface.techassess.Constants;
 import com.topcoder.shared.netCommon.screening.request.ScreeningLoginRequest;
+import com.topcoder.shared.messaging.QueueMessageSender;
+import com.topcoder.shared.messaging.QueueMessageReceiver;
+import com.topcoder.shared.screening.common.ScreeningApplicationServer;
+import com.topcoder.shared.screening.common.ScreeningContext;
+import com.topcoder.shared.util.DBMS;
 
 import javax.jms.ObjectMessage;
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
 
 /**
  * User: dok
@@ -46,6 +54,28 @@ public class Login extends Base {
         log.debug(Thread.currentThread().toString());
         ObjectMessage response = receive(2000, messageId);
         log.debug("response " + response);
+
+
+        Context context = null;
+        try {
+            context = ScreeningContext.getJMSContext();
+        } catch (NamingException e) {
+            throw new ServletException(e);
+        }
+
+
+        QueueMessageSender sender = new QueueMessageSender(ScreeningApplicationServer.JMS_FACTORY,
+                DBMS.REQUEST_QUEUE, context);
+        sender.setPersistent(true);
+        sender.setDBPersistent(false);
+        sender.setFaultTolerant(false);
+
+        QueueMessageReceiver receiver = new QueueMessageReceiver(ScreeningApplicationServer.JMS_FACTORY,
+                DBMS.RESPONSE_QUEUE, context, "server="+Constants.SERVER_ID);
+        ObjectMessage resp = receiver.getMessage(100);
+        log.debug("got " + resp);
+
+
 
 
         setNextPage(Constants.PAGE_INDEX);
