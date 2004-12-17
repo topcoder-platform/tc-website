1bad837ad5da538726fff39b7af8f7547807c745
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-12-17 21:21:23 +0000

    *** empty log message ***

--- web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/Constants.java
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/Constants.java
@@ -35,6 +35,7 @@ public class Constants {
     public static String CODE;
     public static String MODULE;
     public static String STATIC_PREFIX;
+    public static String MESSAGE_ID;
 
     //request processors
     public static String RP_LOGIN;
