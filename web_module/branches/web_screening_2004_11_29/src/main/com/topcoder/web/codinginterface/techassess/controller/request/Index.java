fe17aedb803e94be7a2f72e3c58445cf40fc5e95
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2004-12-22 16:17:00 +0000

    *** empty log message ***

--- web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/Index.java
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/Index.java
@@ -1,9 +1,17 @@
 package com.topcoder.web.codinginterface.techassess.controller.request;
 
-import com.topcoder.web.common.TCWebException;
-import com.topcoder.web.common.BaseServlet;
-import com.topcoder.web.common.SessionInfo;
 import com.topcoder.web.codinginterface.techassess.Constants;
+import com.topcoder.shared.screening.common.ScreeningApplicationServer;
+import com.topcoder.shared.netCommon.screening.request.ScreeningGetProblemSetsRequest;
+import com.topcoder.shared.netCommon.screening.response.ScreeningGetProblemSetsResponse;
+import com.topcoder.shared.netCommon.screening.response.data.ScreeningProblemSet;
+import com.topcoder.shared.language.JavaLanguage;
+import com.topcoder.shared.language.CPPLanguage;
+import com.topcoder.shared.language.CSharpLanguage;
+import com.topcoder.shared.language.VBLanguage;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * User: dok
@@ -12,19 +20,71 @@ import com.topcoder.web.codinginterface.techassess.Constants;
 public class Index extends Base {
 
 
-    protected void businessProcessing() throws TCWebException {
+    protected void businessProcessing() throws Exception {
 
         if (getUser().isAnonymous()) {
             setNextPage(buildProcessorRequestString(Constants.RP_LOGIN,
                     new String[] {Constants.COMPANY_ID}, new String[]{String.valueOf(getCompanyId())}));
             setIsNextPageInContext(false);
         } else {
-            setNextPage(Constants.PAGE_INDEX);
-            setIsNextPageInContext(true);
+
+            ScreeningGetProblemSetsRequest request = new ScreeningGetProblemSetsRequest();
+            request.setServerID(ScreeningApplicationServer.WEB_SERVER_ID);
+            request.setSessionID(getSessionId());
+
+            send(request);
+
+            showProcessingPage(buildProcessorRequestString(Constants.RP_INDEX_RESPONSE,
+                    new String[] {Constants.MESSAGE_ID}, new String[]{String.valueOf(getMessageId())}));
+
+            ScreeningGetProblemSetsResponse response = (ScreeningGetProblemSetsResponse)receive(5000);
+
+            ScreeningProblemSet[] problemSets = response.getProblemSets();
+            ArrayList sets = new ArrayList();
+            for (int i=0; i<problemSets.length; i++) {
+                sets.add(problemSets[i]);
+            }
+            log.debug("there are " + sets.size() + " problem sets");
+
+            getRequest().setAttribute(Constants.PROBLEM_SETS, sets);
+            getRequest().setAttribute(Constants.LANGUAGES, getLanguages(response));
+
+            closeProcessingPage();
 
         }
 
-
     }
+
+
+    private ArrayList getLanguages(ScreeningGetProblemSetsResponse response) {
+        ArrayList languageIds= response.getAllowedLanguages();
+        ArrayList languages = new ArrayList(languageIds.size());
+        for (Iterator it = languageIds.iterator(); it.hasNext();) {
+            switch (((Integer)it.next()).intValue()) {
+                case JavaLanguage.ID : {
+                    languages.add(JavaLanguage.JAVA_LANGUAGE);
+                    break;
+                }
+
+                case CPPLanguage.ID : {
+                    languages.add(CPPLanguage.CPP_LANGUAGE);
+                    break;
+                }
+
+                case CSharpLanguage.ID : {
+                    languages.add(CSharpLanguage.CSHARP_LANGUAGE);
+                    break;
+                }
+
+                case VBLanguage.ID : {
+                    languages.add(VBLanguage.VB_LANGUAGE);
+                    break;
+                }
+            }
+        }
+        return languages;
+    }
+
+
 
 }
--- web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/Login.java
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/Login.java
@@ -1,22 +1,12 @@
 package com.topcoder.web.codinginterface.techassess.controller.request;
 
 import com.topcoder.web.common.NavigationException;
-import com.topcoder.web.common.SessionInfo;
-import com.topcoder.web.common.BaseServlet;
 import com.topcoder.web.codinginterface.techassess.Constants;
 import com.topcoder.shared.netCommon.screening.request.ScreeningLoginRequest;
 import com.topcoder.shared.netCommon.screening.response.ScreeningLoginResponse;
-import com.topcoder.shared.netCommon.screening.response.data.ScreeningProblemSet;
 import com.topcoder.shared.util.logging.Logger;
 import com.topcoder.shared.security.SimpleUser;
-import com.topcoder.shared.language.JavaLanguage;
-import com.topcoder.shared.language.CPPLanguage;
-import com.topcoder.shared.language.CSharpLanguage;
-import com.topcoder.shared.language.VBLanguage;
 import com.topcoder.shared.screening.common.ScreeningApplicationServer;
-
-import java.util.ArrayList;
-import java.util.Iterator;
 
 /**
  * User: dok
@@ -58,27 +48,16 @@ public class Login extends Base {
 
                 send(request);
 
-                SessionInfo info = (SessionInfo)getRequest().getAttribute(BaseServlet.SESSION_INFO_KEY);
-                showProcessingPage(info.getServletPath()+"?"+Constants.MODULE+"="+Constants.RP_LOGIN_RESPONSE+
-                        "&"+Constants.MESSAGE_ID+"="+getMessageId());
+                showProcessingPage(buildProcessorRequestString(Constants.RP_LOGIN_RESPONSE,
+                    new String[] {Constants.MESSAGE_ID}, new String[]{String.valueOf(getMessageId())}));
 
                 ScreeningLoginResponse response = (ScreeningLoginResponse)receive(5000);
 
 
                 if (response.isSuccess()) {
                     getAuthentication().login(new SimpleUser(response.getUserID(), "", ""));
-                    ScreeningProblemSet[] problemSets = response.getProblemSets();
-                    ArrayList sets = new ArrayList();
-                    for (int i=0; i<problemSets.length; i++) {
-                        sets.add(problemSets[i]);
-                    }
-                    log.debug("there are " + sets.size() + " problem sets");
-
-                    getRequest().getSession().setAttribute(Constants.PROBLEM_SETS, sets);
-                    getRequest().getSession().setAttribute(Constants.LANGUAGES, getLanguages(response));
                     setSessionId(response.getSessionID());
                     setCompanyId(companyId);
-
                 } else {
                     addError(Constants.HANDLE, response.getMessage());
                 }
@@ -93,33 +72,5 @@ public class Login extends Base {
 
     }
 
-    private ArrayList getLanguages(ScreeningLoginResponse response) {
-        ArrayList languageIds= response.getAllowedLanguages();
-        ArrayList languages = new ArrayList(languageIds.size());
-        for (Iterator it = languageIds.iterator(); it.hasNext();) {
-            switch (((Integer)it.next()).intValue()) {
-                case JavaLanguage.ID : {
-                    languages.add(JavaLanguage.JAVA_LANGUAGE);
-                    break;
-                }
-
-                case CPPLanguage.ID : {
-                    languages.add(CPPLanguage.CPP_LANGUAGE);
-                    break;
-                }
-
-                case CSharpLanguage.ID : {
-                    languages.add(CSharpLanguage.CSHARP_LANGUAGE);
-                    break;
-                }
-
-                case VBLanguage.ID : {
-                    languages.add(VBLanguage.VB_LANGUAGE);
-                    break;
-                }
-            }
-        }
-        return languages;
-    }
 
 }
