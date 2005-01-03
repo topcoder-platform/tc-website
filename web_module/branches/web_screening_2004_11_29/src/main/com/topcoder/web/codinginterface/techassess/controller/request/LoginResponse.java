a7b4d8ad63bf9c52674838701f626885bcd79509
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-01-03 14:56:42 +0000

    *** empty log message ***

--- web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/LoginResponse.java
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/LoginResponse.java
@@ -12,23 +12,16 @@ public class LoginResponse extends Base {
     protected static final Logger log = Logger.getLogger(LoginResponse.class);
 
     protected void businessProcessing() throws Exception {
-        if (getUser().isAnonymous()) {
-            setNextPage(buildProcessorRequestString(Constants.RP_LOGIN,
-                    new String[] {Constants.COMPANY_ID}, new String[]{String.valueOf(getCompanyId())}));
-            setIsNextPageInContext(false);
-        } else {
+        setNextPage(buildProcessorRequestString(Constants.RP_INDEX, null, null));
+        setIsNextPageInContext(false);
 
-            setNextPage(buildProcessorRequestString(Constants.RP_INDEX, null, null));
-            setIsNextPageInContext(false);
-
-            if (hasParameter(Constants.MESSAGE_ID)) {
-                String messageId = getRequest().getParameter(Constants.MESSAGE_ID);
-                loadSessionErrorsIntoRequest(messageId);
-                loadSessionDefaultsIntoRequest(messageId);
-                if (hasErrors()) {
-                    setNextPage(Constants.PAGE_LOGIN);
-                    setIsNextPageInContext(true);
-                }
+        if (hasParameter(Constants.MESSAGE_ID)) {
+            String messageId = getRequest().getParameter(Constants.MESSAGE_ID);
+            loadSessionErrorsIntoRequest(messageId);
+            loadSessionDefaultsIntoRequest(messageId);
+            if (hasErrors()) {
+                setNextPage(Constants.PAGE_LOGIN);
+                setIsNextPageInContext(true);
             }
         }
     }
