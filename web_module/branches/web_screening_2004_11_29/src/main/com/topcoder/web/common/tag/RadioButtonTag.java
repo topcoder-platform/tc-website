7535650ed5127a00fd80c2998c3d577227104667
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-01-03 23:02:34 +0000

    *** empty log message ***

---
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/common/tag/RadioButtonTag.java
@@ -0,0 +1,55 @@
+package com.topcoder.web.common.tag;
+
+/**
+ * User: dok
+ * Date: Jan 3, 2005
+ * Time: 5:53:26 PM
+ */
+public class RadioButtonTag extends BaseTag {
+    private String value=null;
+    private String selected=null;
+
+    public int doStartTag() {
+        StringBuffer ret = new StringBuffer();
+        try {
+            ret.append("<input type=\"radio\" name=\"").append(name).append("\" ");
+
+            if (selected==null) {
+                selected = getDefaultValue()==null?null:getDefaultValue().toString();
+            }
+            if (value != null) {
+                boolean checked = value.equalsIgnoreCase(selected)||"true".equalsIgnoreCase(selected);
+                if (checked) {
+                    ret.append("checked=\"").append(value).append("\" ");
+                }
+            }
+            ret.append("/>");
+            pageContext.getOut().print(ret.toString());
+        } catch (java.io.IOException ioe) {
+        }
+        return SKIP_BODY;
+    }
+
+    /**
+     * Sets the value.
+     * @param value The value to set
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setValue(boolean value) {
+        this.value = new Boolean(value).toString();
+    }
+
+    public void setSelected(String selected) {
+        this.selected=selected;
+    }
+
+    protected void init() {
+        this.value =null;
+        this.selected=null;
+    }
+
+}
+
