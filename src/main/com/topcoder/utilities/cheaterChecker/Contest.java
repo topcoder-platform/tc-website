e3e27c97e1868bff7fc13efcbf4b8a541b1e13e1
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-01-12 21:22:41 +0000

    *** empty log message ***

--- src/main/com/topcoder/utilities/cheaterChecker/Contest.java
+++ src/main/com/topcoder/utilities/cheaterChecker/Contest.java
@@ -143,7 +143,7 @@ public class Contest {
             query.append(" AND cc.round_id = r.round_id ");
             query.append(" AND r.room_type_id = 2 ");
             query.append(" AND cc.component_id = ? ");
-            query.append(" AND s.submission_points > 0 ");
+            query.append(" AND s.submission_points >= 0 ");//
             query.append(" AND s.component_state_id = cc.component_state_id ");
             query.append(" AND rr.round_id = r.round_id ");
             query.append(" AND rr.room_id = r.room_id ");
