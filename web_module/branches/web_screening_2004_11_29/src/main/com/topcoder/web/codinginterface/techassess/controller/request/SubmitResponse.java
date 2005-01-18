bb612fa7e4bef9cf54effbb36803529e8b153866
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-01-18 16:09:57 +0000

    *** empty log message ***

---
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/techassess/controller/request/SubmitResponse.java
@@ -0,0 +1,25 @@
+package com.topcoder.web.codinginterface.techassess.controller.request;
+
+import com.topcoder.web.codinginterface.techassess.Constants;
+
+/**
+ * @author  dok
+ * @version  $Revision$ $Date$
+ * Create Date: Jan 18, 2005
+ */
+public class SubmitResponse extends Base {
+
+    protected void techAssessProcessing() throws Exception {
+        if (getUser().isAnonymous()) {
+            setNextPage(buildProcessorRequestString(Constants.RP_LOGIN,
+                    new String[]{Constants.COMPANY_ID}, new String[]{String.valueOf(getCompanyId())}));
+            setIsNextPageInContext(false);
+        } else {
+            getRequest().setAttribute(Constants.PROBLEM_TYPE_ID, getRequest().getParameter(Constants.PROBLEM_TYPE_ID));
+            getRequest().setAttribute(Constants.COMPONENT_ID, getRequest().getParameter(Constants.COMPONENT_ID));
+            setNextPage(Constants.PAGE_SUBMIT_CONFIRM);
+            setIsNextPageInContext(true);
+        }
+    }
+
+}
