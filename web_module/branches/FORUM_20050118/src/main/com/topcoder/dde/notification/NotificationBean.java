ed809d9a75140ff6ca0098443a5e6ebeb26d01ec
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-01-20 20:19:42 +0000

    *** empty log message ***

--- web_module/branches/FORUM_20050118/src/main/com/topcoder/dde/notification/NotificationBean.java
+++ web_module/branches/FORUM_20050118/src/main/com/topcoder/dde/notification/NotificationBean.java
@@ -51,6 +51,16 @@ import java.util.List;
 
 import com.topcoder.apps.review.persistence.Common;
 
+import com.topcoder.apps.review.EJBHelper;
+import com.topcoder.apps.review.ConfigHelper;
+
+import com.topcoder.file.render.RecordTag;
+import com.topcoder.file.render.ValueTag;
+import com.topcoder.file.render.XMLDocument;
+import com.topcoder.file.render.xsl.XSLTransformerWrapper;
+import com.topcoder.file.render.xsl.XSLTransformerWrapperException;
+import com.topcoder.message.email.EmailEngine;
+import com.topcoder.message.email.TCSEmailMessage;
 
 import com.topcoder.util.TCException;
 import com.topcoder.util.idgenerator.bean.IdGen;
@@ -75,6 +85,7 @@ import javax.sql.DataSource;
 
 
 import java.util.Properties;
+import java.util.Map;
 
 /**
  * This is the concrete implementation of the ProjectTracker interface.
@@ -150,8 +161,81 @@ public class NotificationBean implements SessionBean {
 
     public void notifyEvent(String event, Properties prop) {
         info("Notification.notifyEvent " + event + " with properties " + prop);
+
+        XMLDocument xmlDocument = new XMLDocument("MAILDATA");
+
+        for (Iterator it = prop.entrySet().iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry) it.next();
+            xmlDocument.addTag(new ValueTag(entry.getKey(), entry.getValue()));
+            info(entry.getKey()+"="+entry.getValue());
+        }
+
+
+        String filenameXSL = ConfigHelper.getMailTemplatePath() + "forum_post_notification.xsl"; //qq
+        info ("filenameXSL=" +filenameXSL);
+
+        String bodyText = formatBody(xmlDocument, filenameXSL);
+        info("body text: " + bodyText);
+        sendMail("notification@gmail.com", "amarcu@gmail.com", "subject", bodyText);
     }
 
+    /**
+     * Send a mail from a user to another user.
+     *
+     * @param from the sender
+     * @param to the destination user
+     * @param subject the subject
+     * @param messageText the text of the message
+     *
+     * @throws Exception propagate any exceptions
+     */
+    static void sendMail(String from, String to, String subject, String messageText) throws Exception {
+        if (!EJBHelper.isTestMode()) {
+            TCSEmailMessage message = new TCSEmailMessage();
+            message.setFromAddress(from);
+            message.setToAddress(to, TCSEmailMessage.TO);
+            message.setSubject(subject);
+            message.setBody(messageText);
+            LogHelper.log("Sending e-mail to address: " + formatAddress(to));
+            EmailEngine.send(message);
+        } else {
+            LogHelper.log("--mail--");
+            LogHelper.log("From: " + from);
+            LogHelper.log("To: " + to);
+            LogHelper.log("Subject: " + subject);
+            LogHelper.log(messageText);
+            LogHelper.log("---end--");
+        }
+    }
+
+    /**
+     * Formats the body of the email by appling an XSL transformation to an XML file which is dinamically
+     * generated (containing info such as the names of the sender and receiver, the project name and id and the reason.
+     *
+     * @param from the sender user
+     * @param to the receiver user
+     * @param proj the project
+     * @param reason the reason
+     * @param changeType the type of the project change (a combination of the constants defined in this class)
+     * @param bodyXSL the name of the XSL file (template of the body) to be applied
+     *
+     * @return the formatted body
+     *
+     * @exception XSLTransformerWrapperException propagated from the document generator
+     * @exception FileNotFoundException if the XSL file is not found
+     */
+    private static String formatBody(XMLDocument xmlDocument, String bodyXSL)
+                throws XSLTransformerWrapperException, FileNotFoundException {
+        String xmlData = xmlDocument.createXML();
+        if (EJBHelper.isTestMode()) {
+            LogHelper.log(xmlData);
+        }
+        XSLTransformerWrapper xslt = new XSLTransformerWrapper(new FileInputStream(bodyXSL));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        xslt.transform(new ByteArrayInputStream(xmlData.getBytes()), out);
+        String result = out.toString();
+        return result;
+    }
 
     /**
      * This method is required by the EJB Specification. Used to get the context ... for dynamic connection pools.
