ba0f237078dec4f2015c2c1ecbb92a00867fed3a
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-01-21 19:06:12 +0000

    *** empty log message ***

---
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/CodingInterfaceConstants.java
@@ -0,0 +1,22 @@
+package com.topcoder.web.codinginterface;
+
+/**
+ * @author  dok
+ * @version  $Revision$ $Date$
+ * Create Date: Jan 21, 2005
+ */
+public class CodingInterfaceConstants {
+
+    public static String HANDLE = "ha";
+    public static String PASSWORD = "ps";
+    public static String COMPONENT_ID = "cid";
+    public static String PROBLEM_TYPE_ID = "ptid";
+    public static String LANGUAGE_ID = "lid";
+    public static String CODE = "code";
+    public static String MODULE = "module";
+    public static String STATIC_PREFIX = "d";
+    public static String MESSAGE_ID = "messid";
+    public static String SUBMIT_FLAG = "subf";
+    public static String TEST_ARGUMENT_PREFIX = "arg";
+
+}
