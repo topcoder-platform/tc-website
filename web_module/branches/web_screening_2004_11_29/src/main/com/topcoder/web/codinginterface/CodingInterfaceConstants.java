115e106582a33a9e12c5950535f0ac958b66e67f
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-01-21 19:06:56 +0000

    *** empty log message ***

--- web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/CodingInterfaceConstants.java
+++ web_module/branches/web_screening_2004_11_29/src/main/com/topcoder/web/codinginterface/CodingInterfaceConstants.java
@@ -7,16 +7,16 @@ package com.topcoder.web.codinginterface;
  */
 public class CodingInterfaceConstants {
 
-    public static String HANDLE = "ha";
-    public static String PASSWORD = "ps";
-    public static String COMPONENT_ID = "cid";
-    public static String PROBLEM_TYPE_ID = "ptid";
-    public static String LANGUAGE_ID = "lid";
-    public static String CODE = "code";
-    public static String MODULE = "module";
-    public static String STATIC_PREFIX = "d";
-    public static String MESSAGE_ID = "messid";
-    public static String SUBMIT_FLAG = "subf";
-    public static String TEST_ARGUMENT_PREFIX = "arg";
+    public static final String HANDLE = "ha";
+    public static final String PASSWORD = "ps";
+    public static final String COMPONENT_ID = "cid";
+    public static final String PROBLEM_TYPE_ID = "ptid";
+    public static final String LANGUAGE_ID = "lid";
+    public static final String CODE = "code";
+    public static final String MODULE = "module";
+    public static final String STATIC_PREFIX = "d";
+    public static final String MESSAGE_ID = "messid";
+    public static final String SUBMIT_FLAG = "subf";
+    public static final String TEST_ARGUMENT_PREFIX = "arg";
 
 }
