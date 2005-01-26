606dc2ca00fea686f4fb782a7997e97ccd53533c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-01-26 16:59:23 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/util/TourneyInfo.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/util/TourneyInfo.java
@@ -3,9 +3,11 @@ package com.topcoder.web.tc.controller.request.util;
 import com.topcoder.shared.util.DBMS;
 import com.topcoder.shared.dataAccess.DataAccess;
 import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.security.SimpleResource;
 import com.topcoder.web.tc.controller.request.Static;
 import com.topcoder.web.tc.Constants;
 import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.common.PermissionException;
 
 import java.util.Map;
 
@@ -20,13 +22,17 @@ public class TourneyInfo extends Static {
     protected void businessProcessing() throws TCWebException {
 
         try {
-            DataAccess dai = new DataAccess(DBMS.OLTP_DATASOURCE_NAME);
-            Request dataRequest = new Request();
+            if (getUser().isAnonymous()) {
+                throw new PermissionException(getUser(), new SimpleResource("TCCC04Terms"));
+            } else {
+                DataAccess dai = new DataAccess(DBMS.OLTP_DATASOURCE_NAME);
+                Request dataRequest = new Request();
 
-            dataRequest.setContentHandle("affidavit_info");
-            dataRequest.setProperty("cr", "" + getUser().getId());
-            dataRequest.setProperty("mid", "" + getUser().getId());
-            getRequest().setAttribute("info", dai.getData(dataRequest));
+                dataRequest.setContentHandle("affidavit_info");
+                dataRequest.setProperty("cr", "" + getUser().getId());
+                dataRequest.setProperty("mid", "" + getUser().getId());
+                getRequest().setAttribute("info", dai.getData(dataRequest));
+            }
 
         } catch (TCWebException e) {
             throw e;
