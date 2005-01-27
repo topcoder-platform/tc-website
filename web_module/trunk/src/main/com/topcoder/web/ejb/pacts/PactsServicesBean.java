f3c4fe0c0537cddb3e7250c19a2cd04a73687bee
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-01-27 19:10:00 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/ejb/pacts/PactsServicesBean.java
+++ web_module/trunk/src/main/com/topcoder/web/ejb/pacts/PactsServicesBean.java
@@ -1176,8 +1176,8 @@ public class PactsServicesBean extends BaseEJB implements PactsConstants {
         HashMap hm = new HashMap();
         hm.put(USER_DEMOGRAPHICS, rsc);
 
-        sb.replace(0, sb.length(), "SELECT cs.school_id, cs.coder_id, cs.school_name from  ")
-                .append(" current_school cs where coder_id = " + userId);
+        sb.replace(0, sb.length(), "SELECT cs.school_id, cs.coder_id, s.ame as school_name from  ")
+                .append(" current_school cs, school s where cs.school_id = s.school_id and coder_id = " + userId);
 
         rsc = null;
         rsc = runSelectQuery(sb.toString(), true);
