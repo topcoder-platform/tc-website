6f5203098092468482b57a3522fb8ce1f48b4a24
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-02-07 16:22:39 +0000

    *** empty log message ***

---
+++ web_module/trunk/src/main/com/topcoder/dde/util/ConstantsInt.java
@@ -0,0 +1,11 @@
+package com.topcoder.dde.util;
+
+/**
+ * @author  dok
+ * @version  $Revision$ $Date$
+ * Create Date: Feb 4, 2005
+ */
+public interface ConstantsInt {
+
+    public static final String STATIC_PREFIX = "d";
+}
