3243c63af7c030386931719a4b2bfa7e9db3086c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-02-08 16:41:35 +0000

    *** empty log message ***

--- src/main/com/topcoder/dde/request/ComponentTermsAgree.java
+++ src/main/com/topcoder/dde/request/ComponentTermsAgree.java
@@ -3,9 +3,8 @@ package com.topcoder.dde.request;
 import com.topcoder.web.common.BaseProcessor;
 import com.topcoder.web.common.PermissionException;
 import com.topcoder.shared.security.ClassResource;
-import com.topcoder.dde.user.UserManagerLocalHome;
-import com.topcoder.dde.user.UserManagerLocal;
-import com.topcoder.dde.util.Constants;
+import com.topcoder.dde.user.UserManagerRemoteHome;
+import com.topcoder.dde.user.UserManagerRemote;
 
 import javax.rmi.PortableRemoteObject;
 
@@ -21,9 +20,10 @@ public class ComponentTermsAgree extends BaseProcessor {
         if (getUser().isAnonymous()) {
             throw new PermissionException(getUser(), new ClassResource(this.getClass()));
         } else {
-            UserManagerLocalHome userManagerHome = (UserManagerLocalHome)
-                    getInitialContext().lookup("dde/UserManager");
-            UserManagerLocal userManager = userManagerHome.create();
+            Object objUserManager = getInitialContext().lookup("dde/UserManager");
+            UserManagerRemoteHome userManagerHome = (UserManagerRemoteHome)
+                    PortableRemoteObject.narrow(objUserManager, UserManagerRemoteHome.class);
+            UserManagerRemote userManager = userManagerHome.create();
 
             userManager.agreeToComponentTerms(getUser().getId());
             setNextPage("/catalog/index.jsp");
