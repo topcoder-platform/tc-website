9dd998b73628d36595685df3536f81d3de65ee46
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-02-14 18:46:15 +0000

    doubleclick reg

--- src/main/com/topcoder/web/privatelabel/controller/request/dc05/Confirm.java
+++ src/main/com/topcoder/web/privatelabel/controller/request/dc05/Confirm.java
@@ -10,10 +10,12 @@ import com.topcoder.web.privatelabel.controller.request.FullRegConfirm;
 import com.topcoder.web.privatelabel.model.FullRegInfo;
 import com.topcoder.web.privatelabel.model.ResumeRegInfo;
 import com.topcoder.web.privatelabel.model.SimpleRegInfo;
+import com.topcoder.web.common.StringUtils;
 
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Confirm extends FullRegConfirm {
     protected void setNextPage() {
@@ -32,7 +34,59 @@ public class Confirm extends FullRegConfirm {
 
         try {
             //validate front fields here
+            
+            //check handle
+            if (info.getHandle().length() > Constants.MAX_HANDLE_LENGTH ||
+                    info.getHandle().length() < Constants.MIN_HANDLE_LENGTH) {
+                addError(Constants.HANDLE, "Your handle must contain between " +
+                        Constants.MIN_HANDLE_LENGTH + " and " + Constants.MAX_HANDLE_LENGTH + " characters.");
+            }
+           
+            if (!StringUtils.containsOnly(info.getHandle(), Constants.HANDLE_ALPHABET, false)) {
+                addError(Constants.HANDLE,
+                        "Please limit the characters in your handle to letter, numbers and common punctuation symbols.");
+            }
+            
+            try {
+                if (info.isNew() && userExists(info.getHandle())) {
+                    addError(Constants.HANDLE, "Please choose another handle.");
+                }
+            } catch (Exception e) {
+                throw new TCWebException(e);
+            }
 
+            //check password
+            if (info.getPassword().length() > Constants.MAX_PASSWORD_LENGTH ||
+                    info.getPassword().length() < Constants.MIN_PASSWORD_LENGTH) {
+                addError(Constants.PASSWORD, "Your password must contain between " +
+                        Constants.MIN_PASSWORD_LENGTH + " and " + Constants.MAX_PASSWORD_LENGTH + " characters.");
+            }
+            if (!info.getPassword().equals(info.getPasswordConfirm())) {
+                addError(Constants.PASSWORD_CONFIRM, "Please be sure password and password confirmation match.");
+            }
+
+            //check email
+            if (info.getEmail().length() > Constants.MAX_EMAIL_LENGTH) {
+                addError(Constants.EMAIL, "Your email must be between at most " +
+                        Constants.MAX_EMAIL_LENGTH + " characters.");
+            }
+            
+            StringTokenizer st = new StringTokenizer(info.getEmail(), "@.");
+            if (st.countTokens() < 3
+                    || !StringUtils.contains(info.getEmail(), '@')
+                    || !StringUtils.contains(info.getEmail(), '.')) {
+                addError(Constants.EMAIL, "Please enter a valid email address.");
+            }
+
+            //check first name
+            if (info.getFirstName().length() < 1) {
+                addError(Constants.FIRST_NAME, "Please enter your first name.");
+            }
+
+            //check last name
+            if (info.getLastName().length() < 1) {
+                addError(Constants.LAST_NAME, "Please enter your last name.");
+            }
 
         } catch (Exception e) {
             throw new TCWebException(e);
