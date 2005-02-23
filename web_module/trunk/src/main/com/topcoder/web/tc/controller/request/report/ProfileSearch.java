6ac6e34a2006ae19cfa990dddcd31b4345b837d8
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-02-23 01:38:47 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/report/ProfileSearch.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/report/ProfileSearch.java
@@ -282,7 +282,7 @@ public class ProfileSearch extends Base {
                 query.append(value[i]);
                 query.append(x);
                 if(i < 4){
-                    query.append(" 00:00:00.0'\'\n");
+                    query.append(" 00:00:00.0'\n");
                 }else{
                     query.append('\n');
                 }
