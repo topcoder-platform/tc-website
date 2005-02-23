053cbd4963d9aec209f2d3ee4b975b55f0793a92
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-02-23 18:18:52 +0000

    *** empty log message ***

--- src/main/com/topcoder/common/web/render/DataValueRenderer.java
+++ src/main/com/topcoder/common/web/render/DataValueRenderer.java
@@ -30,6 +30,39 @@ public class DataValueRenderer implements ElementRenderer {
     }
 
     public String toPlainText(Language language) {
-        return dataValue.encode();
+        return breakIt(dataValue.encode());
+    }
+    
+    private String breakIt(String s) {
+        int breakLen = 80;
+        StringBuffer out = new StringBuffer(s);
+        char ch;
+        if (s.length() > breakLen) {
+            out = new StringBuffer(s.length());
+            out.append("\n");
+            if(s.charAt(0)=='{' && s.indexOf('"')==-1){
+                //must be an int[]
+                int last = 0;
+                for(int i = 0; i<s.length(); i++){
+                    ch = s.charAt(i);
+                    if(ch==',' && out.length()-last > breakLen-12){
+                        out.append(",\n ");
+                        last = out.length();
+                    }else{
+                        out.append(ch);
+                    }
+                }
+            }else{
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == '\"' && s.length() > i+1 && s.charAt(i + 1) == ',') {
+                        out.append("\",\n ");
+                        i += 2;
+                    } else {
+                        out.append(s.charAt(i));
+                    }
+                }
+            }
+        }
+        return out.toString();
     }
 }
