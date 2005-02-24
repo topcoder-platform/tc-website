4d718324e3fc707da3fbab91347cbc92af707d86
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-02-24 21:56:18 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/render/ejb/WebMenuHome.java
+++
@@ -1,8 +0,0 @@
-package com.topcoder.web.render.ejb;
-
-import javax.ejb.CreateException;
-import java.rmi.RemoteException;
-
-public interface WebMenuHome extends javax.ejb.EJBHome {
-    public WebMenu create() throws RemoteException, CreateException;
-}
--- web_module/trunk/src/main/com/topcoder/web/render/ejb/WebStatusObject.java
+++
@@ -1,9 +0,0 @@
-package com.topcoder.web.render.ejb;
-
-import java.io.Serializable;
-
-public class WebStatusObject
-        implements Serializable {
-    public Integer status_id;
-    public String status_desc;
-}
