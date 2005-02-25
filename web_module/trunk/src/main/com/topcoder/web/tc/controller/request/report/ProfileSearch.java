fef41d95111544de352d363e31402a9e2dce270e
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-02-25 20:11:41 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/report/ProfileSearch.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/report/ProfileSearch.java
@@ -114,7 +114,7 @@ public class ProfileSearch extends Base {
         query.append("    AND u.user_id = c.coder_id\n");
         query.append("    AND u.status = 'A'\n");
         query.append("    AND cry.country_code = c.country_code\n");
-        query.append("    AND st.state_code = c.state_code\n");
+        query.append("    AND st.state_code = NVL(c.state_code,'')\n");
         if(comp != null && comp.length() > 0){
             query.append("    AND src.coder_id = c.coder_id\n");
             query.append("    AND drc.demographic_question_id = 15\n");
