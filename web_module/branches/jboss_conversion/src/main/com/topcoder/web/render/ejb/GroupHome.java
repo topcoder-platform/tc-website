5fe23ebcc961a35f7cb5e3d764b7cd655d860c33
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-03-16 20:16:16 +0000

    no message

--- web_module/branches/jboss_conversion/src/main/com/topcoder/web/render/ejb/GroupHome.java
+++
@@ -1,8 +0,0 @@
-package com.topcoder.web.render.ejb;
-
-import javax.ejb.CreateException;
-import java.rmi.RemoteException;
-
-public interface GroupHome extends javax.ejb.EJBHome {
-    public Group create() throws RemoteException, CreateException;
-}
--- web_module/branches/jboss_conversion/src/main/com/topcoder/web/render/ejb/WebContentGroupHome.java
+++
@@ -1,8 +0,0 @@
-package com.topcoder.web.render.ejb;
-
-import javax.ejb.CreateException;
-import java.rmi.RemoteException;
-
-public interface WebContentGroupHome extends javax.ejb.EJBHome {
-    public WebContentGroup create() throws RemoteException, CreateException;
-}
