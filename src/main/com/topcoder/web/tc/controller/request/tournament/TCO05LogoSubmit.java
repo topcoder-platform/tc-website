32950df1fea2eaa9e59df400b4606e78e0d0f0b1
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-04-06 17:51:38 +0000

    *** empty log message ***

---
+++ src/main/com/topcoder/web/tc/controller/request/tournament/TCO05LogoSubmit.java
@@ -0,0 +1,38 @@
+package com.topcoder.web.tc.controller.request.tournament;
+
+import com.topcoder.web.tc.controller.request.Base;
+import com.topcoder.web.common.PermissionException;
+import com.topcoder.web.common.MultipartRequest;
+import com.topcoder.shared.security.SimpleResource;
+import com.topcoder.shared.util.ApplicationServer;
+import com.topcoder.servlet.request.UploadedFile;
+
+import java.io.FileOutputStream;
+
+/**
+ * @author  dok
+ * @version  $Revision$ $Date$
+ * Create Date: Apr 6, 2005
+ */
+public class TCO05LogoSubmit extends Base {
+    private static final String IMAGE_PATH = ApplicationServer.BASE_DIR + "images/tco05logo/";
+    protected void businessProcessing() throws Exception {
+
+        if (getUser().isAnonymous()) {
+            throw new PermissionException(getUser(), new SimpleResource(this.getClass().getName()));
+        } else {
+            MultipartRequest request = (MultipartRequest)getRequest();
+            UploadedFile[] files = request.getAllUploadedFiles();
+            for (int i=0; i<files.length; i++) {
+                log.debug("got file " + files[i].getFile().toString());
+                FileOutputStream fos = new FileOutputStream(IMAGE_PATH + files[i].getFile().getName());
+                log.debug("write that file to " + IMAGE_PATH + files[i].getFile().getName());
+                byte[] bytes = new byte[files[i].getSize()];
+                files[i].getInputStream().read(bytes);
+                fos.write(bytes);
+                //create record in image table
+                //create record in coder image xref table
+            }
+        }
+    }
+}
