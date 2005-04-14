290b243f3c333a6513c43c305f784ef81d610c78
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-04-14 20:12:01 +0000

    *** empty log message ***

--- web_module/branches/jboss_conversion/src/main/com/topcoder/ejb/ContestAdminServices/ContestAdminServices.java
+++ web_module/branches/jboss_conversion/src/main/com/topcoder/ejb/ContestAdminServices/ContestAdminServices.java
@@ -9,44 +9,50 @@ import java.util.ArrayList;
 public interface ContestAdminServices extends EJBObject {
 
 
-    public void insertBlobObject(String tableName, String fieldName, Object obj, String whereClause) throws RemoteException;
+/*
+    void insertBlobObject(String tableName, String fieldName, Object obj, String whereClause) throws RemoteException;
 
-    public Object getBlobObject(String tableName, String fieldName, String whereClause) throws RemoteException;
+    Object getBlobObject(String tableName, String fieldName, String whereClause) throws RemoteException;
 
-    public ArrayList getContestList() throws RemoteException;
+    ArrayList getContestList() throws RemoteException;
 
-    public ArrayList getRoundProblems(int round_id, int contest_id) throws RemoteException;
+    ArrayList getRoundProblems(int round_id, int contest_id) throws RemoteException;
 
-    public ArrayList getLanguages() throws RemoteException;
+    ArrayList getLanguages() throws RemoteException;
 
-    public ArrayList getRoundList(int contest_id) throws RemoteException;
+    ArrayList getRoundList(int contest_id) throws RemoteException;
 
-    public ArrayList getSystemTestCaseReportList(int roundId, int problemId, int coderId, int filter) throws RemoteException;
+    ArrayList getSystemTestCaseReportList(int roundId, int problemId, int coderId, int filter) throws RemoteException;
 
-    public ArrayList getCoderList(int roundId, int problemId) throws RemoteException;
+    ArrayList getCoderList(int roundId, int problemId) throws RemoteException;
+*/
 
-    public ArrayList getCoderList(int roundId) throws RemoteException;
+    ArrayList getCoderList(int roundId) throws RemoteException;
 
-    public ArrayList getProblemList(int round_id) throws RemoteException;
+    ArrayList getProblemList(int round_id) throws RemoteException;
 
-    public ArrayList getSystemTestCaseReportList(int round_id, int filter) throws RemoteException;
+/*
+    ArrayList getSystemTestCaseReportList(int round_id, int filter) throws RemoteException;
+*/
 
-    public ArrayList getRoundList() throws RemoteException;
+    ArrayList getRoundList() throws RemoteException;
 
-    public void nullifyChallenge(int challengeId) throws RemoteException;
+    void nullifyChallenge(int challengeId) throws RemoteException;
 
-    public void removeSystemTestResult(int roundId, int coderId, int problemId, int testCaseId) throws RemoteException;
+/*
+    void removeSystemTestResult(int roundId, int coderId, int problemId, int testCaseId) throws RemoteException;
+*/
 
-    public ArrayList getRoomList(int round_id) throws RemoteException;
+    ArrayList getRoomList(int round_id) throws RemoteException;
 
-    public ArrayList getChallengeList(int roundId, int roomId, int filter, int constraintType) throws RemoteException;
+    ArrayList getChallengeList(int roundId, int roomId, int filter, int constraintType) throws RemoteException;
 
-    public void overturnChallenge(int challenge_id) throws RemoteException;
+    void overturnChallenge(int challenge_id) throws RemoteException;
 
-    public ArrayList getCodersByRound(int roundId) throws RemoteException;
+    ArrayList getCodersByRound(int roundId) throws RemoteException;
 
-    public ArrayList getCoderCompilations(int roundId, int coderId) throws RemoteException;
+    ArrayList getCoderCompilations(int roundId, int coderId) throws RemoteException;
 
-    public ArrayList getRounds() throws RemoteException;
+    ArrayList getRounds() throws RemoteException;
 }
 
