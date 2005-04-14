efdc82028fbc2c04d1f16540641c6eae64a016f7
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-04-14 20:12:04 +0000

    *** empty log message ***

--- web_module/branches/jboss_conversion/src/main/com/topcoder/ejb/ContestAdminServices/ContestAdminServices.java
+++ web_module/branches/jboss_conversion/src/main/com/topcoder/ejb/ContestAdminServices/ContestAdminServices.java
@@ -9,39 +9,16 @@ import java.util.ArrayList;
 public interface ContestAdminServices extends EJBObject {
 
 
-/*
-    void insertBlobObject(String tableName, String fieldName, Object obj, String whereClause) throws RemoteException;
-
-    Object getBlobObject(String tableName, String fieldName, String whereClause) throws RemoteException;
-
-    ArrayList getContestList() throws RemoteException;
-
-    ArrayList getRoundProblems(int round_id, int contest_id) throws RemoteException;
-
-    ArrayList getLanguages() throws RemoteException;
 
     ArrayList getRoundList(int contest_id) throws RemoteException;
-
-    ArrayList getSystemTestCaseReportList(int roundId, int problemId, int coderId, int filter) throws RemoteException;
-
-    ArrayList getCoderList(int roundId, int problemId) throws RemoteException;
-*/
 
     ArrayList getCoderList(int roundId) throws RemoteException;
 
     ArrayList getProblemList(int round_id) throws RemoteException;
 
-/*
-    ArrayList getSystemTestCaseReportList(int round_id, int filter) throws RemoteException;
-*/
-
     ArrayList getRoundList() throws RemoteException;
 
     void nullifyChallenge(int challengeId) throws RemoteException;
-
-/*
-    void removeSystemTestResult(int roundId, int coderId, int problemId, int testCaseId) throws RemoteException;
-*/
 
     ArrayList getRoomList(int round_id) throws RemoteException;
 
