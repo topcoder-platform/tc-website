b1abe36ea9a84a38fe4298a41c5fa3a35b0071a2
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-04-27 20:44:47 +0000

    *** empty log message ***

--- web_module/branches/screening_2005_04_25/src/main/com/topcoder/web/admin/controller/MainServlet.java
+++ web_module/branches/screening_2005_04_25/src/main/com/topcoder/web/admin/controller/MainServlet.java
@@ -2,6 +2,10 @@ package com.topcoder.web.admin.controller;
 
 import com.topcoder.web.admin.Constants;
 import com.topcoder.web.common.BaseServlet;
+import com.topcoder.web.common.SessionInfo;
+import com.topcoder.web.common.security.WebAuthentication;
+import com.topcoder.shared.security.Resource;
+import com.topcoder.shared.security.ClassResource;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -16,4 +20,13 @@ public class MainServlet extends BaseServlet {
         super.init(config);
         Constants.init(config);
     }
+
+
+    protected boolean hasPermission(WebAuthentication auth, Resource r) throws Exception {
+        return createAuthorization(auth.getActiveUser()).hasPermission(new ClassResource(this.getClass()));
+    }
+
+
+
+
 }
