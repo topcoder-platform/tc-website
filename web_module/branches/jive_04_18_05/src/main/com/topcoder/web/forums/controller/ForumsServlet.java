9ba97d5a7d33072080aea3a73f6a84b57fbe6748
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-04-29 20:54:38 +0000

    no message

--- web_module/branches/jive_04_18_05/src/main/com/topcoder/web/forums/controller/ForumsServlet.java
+++ web_module/branches/jive_04_18_05/src/main/com/topcoder/web/forums/controller/ForumsServlet.java
@@ -35,71 +35,9 @@ import java.util.Set;
  * Window - Preferences - Java - Code Style - Code Templates
  */
 public class ForumsServlet extends BaseServlet {
-	private final static Logger log = Logger.getLogger(MainServlet.class);
-
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        Constants.initialize();
-
-        if (!Constants.isInitialized())
-            throw new ServletException("Constants did not initialize properly");
-    }
+	private final static Logger log = Logger.getLogger(ForumsServlet.class);
 
     protected boolean hasPermission(WebAuthentication auth, Resource r) throws Exception {
         return true;
-    }
-
-    protected void process(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        //just trying to protect against invalid requests getting to the
-        //app server.  currently, we don't have a way to stop this ad the web
-        //server layer.
-        if ((ApplicationServer.PROD == ApplicationServer.ENVIRONMENT &&
-                request.getServerName().startsWith(ApplicationServer.SERVER_NAME)) ||
-                ApplicationServer.ENVIRONMENT != ApplicationServer.PROD) {
-            super.process(request, response);
-        } else {
-            log.error(request.getRemoteHost() + " Made an Invalid Request - Wrong Server Name " + request.getServerName());
-            response.setStatus(400);
-            PrintWriter out = response.getWriter();
-            out.println("<html><head><title>Invalid Request - Wrong Server Name</title></head>");
-            out.println("<body><h4>Your request was invalid for this server.</h4>");
-            out.println("</body></html>");
-            out.flush();
-        }
-
-    }
-
-
-    protected SessionInfo createSessionInfo(TCRequest request,
-                                            WebAuthentication auth, Set groups) throws Exception {
-        //todo get rid of this junk, we end up doing all the same stuff anyway, when the navigation object
-        //goes, so does this crap
-        Navigation nav = (Navigation) request.getSession(true).getAttribute("navigation");
-        CoderSessionInfo ret = null;
-        ret = new CoderSessionInfo(request, auth, groups);
-        if (nav == null) {
-            nav = new Navigation(request, ret);
-            nav.setCoderSessionInfo(ret);
-            request.getSession(true).setAttribute("navigation", nav);
-        } else {
-            nav.setCoderSessionInfo(ret);
-            request.setAttribute("navigation", nav);
-        }
-        return ret;
-    }
-
-    protected String getProcessor(String key) {
-        String ret = super.getProcessor(key);
-        if (ret.equals(key)) {
-            //yuck, gonna throw errors all over the place
-            TCResourceBundle bundle = new TCResourceBundle("TC");
-            try {
-                ret = bundle.getProperty(key);
-            } catch (MissingResourceException ignore) {
-                //just return what we got
-            }
-        }
-        return ret;
     }
 }
