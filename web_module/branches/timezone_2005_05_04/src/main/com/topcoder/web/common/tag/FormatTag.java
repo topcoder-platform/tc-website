09dc9b3b90dd36424a81339f4460c78e852bffbc
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-05-10 14:34:44 +0000

    *** empty log message ***

--- web_module/branches/timezone_2005_05_04/src/main/com/topcoder/web/common/tag/FormatTag.java
+++ web_module/branches/timezone_2005_05_04/src/main/com/topcoder/web/common/tag/FormatTag.java
@@ -53,8 +53,8 @@ public class FormatTag extends TagSupport {
                 ret.append(formatter.format(object));
                 if (isDate) {
                     String tz1 = new SimpleDateFormat("z").format(object);
-                    if (ret.indexOf(tz1)>-1) {
-                        ret.replace(ret.indexOf(tz1), tz1.length(),
+                    if (ret.toString().indexOf(tz1)>-1) {
+                        ret.replace(ret.toString().indexOf(tz1), tz1.length(),
                                 DateUtils.getUTCOffsetString(((Date)object), getTimeZone()));
                     }
                 }
