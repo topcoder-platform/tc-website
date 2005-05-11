8d6ed0f224e36d56b5f0d372162ba5ef9cac7523
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-05-11 22:08:07 +0000

    member profile

--- src/main/com/topcoder/utilities/ServerMonitorBot.java
+++ src/main/com/topcoder/utilities/ServerMonitorBot.java
@@ -196,6 +196,7 @@ public class ServerMonitorBot {
                 TCSEmailMessage em = new TCSEmailMessage();
                 em.addToAddress("8609182841@mmode.com", TCSEmailMessage.TO);
                 em.addToAddress("8604626228@vtext.com", TCSEmailMessage.TO);
+                em.addToAddress("6508045266@vtext.com", TCSEmailMessage.TO);
                 em.addToAddress("8604656205@mobile.mycingular.com", TCSEmailMessage.TO);
                 em.addToAddress("8606144043@vtext.com", TCSEmailMessage.TO);
 
@@ -211,6 +212,7 @@ public class ServerMonitorBot {
                 em.addToAddress("gpaul@topcoder.com", TCSEmailMessage.TO);
                 em.addToAddress("thaas@topcoder.com", TCSEmailMessage.TO);
                 em.addToAddress("ivern@topcoder.com", TCSEmailMessage.TO);
+                em.addToAddress("mktong@topcoder.com", TCSEmailMessage.TO);
                 em.addToAddress("javier-topcoder-alarm@ivern.org", TCSEmailMessage.TO);
                 em.addToAddress("8602686127@messaging.sprintpcs.com", TCSEmailMessage.TO);
 
