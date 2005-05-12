26525b73d58fa272a3c3b76fa94001d20adbbf2b
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-05-12 15:58:05 +0000

    rating utils

--- src/main/com/topcoder/dde/util/DWLoad/RatingQubits.java
+++ src/main/com/topcoder/dde/util/DWLoad/RatingQubits.java
@@ -626,8 +626,7 @@ public class RatingQubits {
                     people3++;
                 }
                 double oldVolatility = ((Double) volatilities.elementAt(i)).doubleValue();
-                newVolatility.addElement(new Double(Math.sqrt((oldVolatility * oldVolatility
-                        + weight * (diff * matchStdDevEquals) * (diff * matchStdDevEquals) / (1 + weight)) / (1 + weight))));
+                newVolatility.addElement(new Double(Math.sqrt((oldVolatility*oldVolatility) / (1+weight) + ((newrating-oldrating)*(newrating-oldrating))/ weight)));
             } else {
                 sqiv += (performedAs - oldrating) * (performedAs - oldrating);
                 people++;
@@ -717,30 +716,111 @@ public class RatingQubits {
         return j * j;
     }
 
-    private double normsinv(double p) {
-        /* Calculates approximation to the inverse of the cumulative
-             standard normal distribution function
+private double normsinv(double p) {
+        return normsinvnew(p);
+    }
+    
+    private double normsinvnew(double p) {
+    /* ********************************************
+     * Original algorythm and Perl implementation can
+     * be found at:
+     * http://www.math.uio.no/~jacklam/notes/invnorm/index.html
+     * Author:
+     *  Peter J. Acklam
+     *  jacklam@math.uio.no
+     * ****************************************** */
+        
+        // Define break-points.
+        // variable for result
+        if(p <= 0) return Double.NEGATIVE_INFINITY;
+        else if(p >= 1) return Double.POSITIVE_INFINITY;
+        
+        double z = 0;
 
-             http://www.pitt.edu/~wpilib/statfaq/gaussfaq.html */
-
-        if (p <= 0) return Double.NEGATIVE_INFINITY;
-        if (p >= 1) return Double.POSITIVE_INFINITY;
-
-        double t;
-
-        if (p <= .5) {
-            t = Math.sqrt(-2.0 * Math.log(p));
-            return -t + (2.515517 + t * (0.802853 + t * 0.010328)) / (1 + t * (1.432788 + t * (0.189269 + t * 0.001308)));
-        } else {
-            t = Math.sqrt(-2.0 * Math.log(1 - p));
-            return t - (2.515517 + t * (0.802853 + t * 0.010328)) / (1 + t * (1.432788 + t * (0.189269 + t * 0.001308)));
+        // Rational approximation for lower region:
+        if( p < P_LOW )
+        {
+          double q  = Math.sqrt(-2*Math.log(p));
+          z = (((((NORMINV_C[0]*q+NORMINV_C[1])*q+NORMINV_C[2])*q+NORMINV_C[3])*q+NORMINV_C[4])*q+NORMINV_C[5]) / ((((NORMINV_D[0]*q+NORMINV_D[1])*q+NORMINV_D[2])*q+NORMINV_D[3])*q+1);
         }
+        // Rational approximation for upper region:
+        else if ( P_HIGH < p )
+        {
+          double q  = Math.sqrt(-2*Math.log(1-p));
+          z = -(((((NORMINV_C[0]*q+NORMINV_C[1])*q+NORMINV_C[2])*q+NORMINV_C[3])*q+NORMINV_C[4])*q+NORMINV_C[5]) / ((((NORMINV_D[0]*q+NORMINV_D[1])*q+NORMINV_D[2])*q+NORMINV_D[3])*q+1);
+        }
+        // Rational approximation for central region:
+        else
+        {
+          double q = p - 0.5D;
+          double r = q * q;
+          z = (((((NORMINV_A[0]*r+NORMINV_A[1])*r+NORMINV_A[2])*r+NORMINV_A[3])*r+NORMINV_A[4])*r+NORMINV_A[5])*q / (((((NORMINV_B[0]*r+NORMINV_B[1])*r+NORMINV_B[2])*r+NORMINV_B[3])*r+NORMINV_B[4])*r+1);
+        }
+        
+        z = refine(z, p);
+        return z;
+    }
+    
+
+    private static final double P_LOW  = 0.02425D;
+    private static final double P_HIGH = 1.0D - P_LOW;
+
+    // Coefficients in rational approximations.
+    private static final double NORMINV_A[] =
+    { -3.969683028665376e+01,  2.209460984245205e+02,
+    -2.759285104469687e+02,  1.383577518672690e+02,
+    -3.066479806614716e+01,  2.506628277459239e+00 };
+
+    private static final double NORMINV_B[] =
+    { -5.447609879822406e+01,  1.615858368580409e+02,
+    -1.556989798598866e+02,  6.680131188771972e+01,
+    -1.328068155288572e+01 };
+
+    private static final double NORMINV_C[] =
+    { -7.784894002430293e-03, -3.223964580411365e-01,
+    -2.400758277161838e+00, -2.549732539343734e+00,
+    4.374664141464968e+00,  2.938163982698783e+00 };
+
+    private static final double NORMINV_D[] =
+    { 7.784695709041462e-03,  3.224671290700398e-01,
+    2.445134137142996e+00,  3.754408661907416e+00 };
+
+    public static double erf(double z) {
+        double t = 1.0 / (1.0 + 0.5 * Math.abs(z));
+
+        // use Horner's method
+        double ans = 1 - t * Math.exp( -z*z   -   1.26551223 +
+                                            t * ( 1.00002368 +
+                                            t * ( 0.37409196 + 
+                                            t * ( 0.09678418 + 
+                                            t * (-0.18628806 + 
+                                            t * ( 0.27886807 + 
+                                            t * (-1.13520398 + 
+                                            t * ( 1.48851587 + 
+                                            t * (-0.82215223 + 
+                                            t * ( 0.17087277))))))))));
+        if (z >= 0) return  ans;
+        else        return -ans;
+    }
+    
+    public static double erfc(double z) {
+        return 1.0 - erf(z);
+    }
+
+    public static double refine(double x, double d)
+    {
+        if( d > 0 && d < 1)
+        {
+          double e = 0.5D * erfc(-x/Math.sqrt(2.0D)) - d;
+          double u = e * Math.sqrt(2.0D*Math.PI) * Math.exp((x*x)/2.0D);
+          x = x - u/(1.0D + x*u/2.0D);
+        }
+        return x;
     }
 
     private double norminv(double p, double mean, double stddev) {
         return mean + normsinv(p) * stddev;
     }
-
     private double snormdens(double v) {
         return Math.exp(-v * v / 2) / Math.sqrt(2 * Math.PI);
     }
