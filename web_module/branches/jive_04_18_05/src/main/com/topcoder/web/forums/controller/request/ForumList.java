d9746a510bbbe3c4e83c6c1289ae9f3c6005ae4e
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-05-20 14:57:46 +0000

    no message

--- web_module/branches/jive_04_18_05/src/main/com/topcoder/web/forums/controller/request/ForumList.java
+++ web_module/branches/jive_04_18_05/src/main/com/topcoder/web/forums/controller/request/ForumList.java
@@ -5,6 +5,8 @@ package com.topcoder.web.forums.controller.request;
 
 import java.util.Iterator;
 
+import com.jivesoftware.forum.ResultFilter;
+import com.jivesoftware.base.JiveConstants;
 
 /**
  * @author mtong
@@ -13,7 +15,11 @@ public class ForumList extends ForumsProcessor {
 	protected void businessProcessing() throws Exception {
 		super.businessProcessing();
 		
-		Iterator itForums = forumFactory.getForums();
+        ResultFilter resultFilter = new ResultFilter();
+        resultFilter.setSortField(JiveConstants.MODIFICATION_DATE);
+        resultFilter.setSortOrder(ResultFilter.DESCENDING);
+        
+		Iterator itForums = forumFactory.getForums(resultFilter);
 		getRequest().setAttribute("forums", itForums);
 		
 		setNextPage("/main.jsp");
