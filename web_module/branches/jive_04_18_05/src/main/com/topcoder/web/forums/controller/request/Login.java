c6f7f71fb6ece7cb066937f08e9843f9d0748ac8
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-05-26 21:04:31 +0000

    no message

--- web_module/branches/jive_04_18_05/src/main/com/topcoder/web/forums/controller/request/Login.java
+++ web_module/branches/jive_04_18_05/src/main/com/topcoder/web/forums/controller/request/Login.java
@@ -13,6 +13,7 @@ import com.topcoder.web.ejb.email.Email;
 import com.topcoder.web.ejb.user.User;
 import com.topcoder.web.tc.Constants;
 import com.topcoder.web.tc.controller.request.Base;
+import com.topcoder.web.tc.controller.request.authentication.*;
 import com.topcoder.web.common.model.CoderSessionInfo;
 
 import java.util.Arrays;
