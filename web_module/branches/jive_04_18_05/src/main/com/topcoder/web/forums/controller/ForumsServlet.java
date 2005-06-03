9e9d35887f17d18e033a7c965f51015654fa0bdf
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-06-03 19:23:17 +0000

    no message

--- web_module/branches/jive_04_18_05/src/main/com/topcoder/web/forums/controller/ForumsServlet.java
+++ web_module/branches/jive_04_18_05/src/main/com/topcoder/web/forums/controller/ForumsServlet.java
@@ -122,7 +122,6 @@ public class ForumsServlet extends BaseServlet {
 		            }
 		        }
 		        if (!response.isCommitted()) {
-                    log.debug("###@@@ fetchRegularPage: " + rp.getNextPage()); 
 		            fetchRegularPage(request, response, rp.getNextPage(), rp.isNextPageInContext());
 		            return;
 		        }
@@ -158,8 +157,8 @@ public class ForumsServlet extends BaseServlet {
         request.setAttribute(NEXT_PAGE_KEY, info.getRequestString());
 
         request.setAttribute(MODULE, LOGIN_PROCESSOR);
-        log.debug("###@@@ handleLogin: " + info.getServletPath());        
-        fetchRegularPage(request, response, LOGIN_SERVLET == null ? info.getServletPath() : LOGIN_SERVLET, true);
+        fetchRegularPage(request, response, "/login.jsp", true);
+        //fetchRegularPage(request, response, LOGIN_SERVLET == null ? info.getServletPath() : LOGIN_SERVLET, true);
     }
 
     protected RequestProcessor callProcess(String processorName, TCRequest request, TCResponse response,
