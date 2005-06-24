276e0943495a83c1701540741150449ee6f53f21
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-06-24 14:50:29 +0000

    tco05 component results

---
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/development/TCO05ContestDetailsOverall.java
@@ -0,0 +1,19 @@
+/*
+ * TCCC05ContestDetailsOverall.java
+ *
+ * Created on January 6, 2005, 11:24 AM
+ */
+
+package com.topcoder.web.tc.controller.request.development;
+
+/**
+ *
+ * @author rfairfax
+ */
+public class TCO05ContestDetailsOverall extends TCCC05ContestDetails {
+
+    int getMax() {
+        return 3;
+    }
+
+}
