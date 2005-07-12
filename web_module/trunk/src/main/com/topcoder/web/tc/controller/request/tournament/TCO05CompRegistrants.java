9a3e392255dacb16ae07193b47013f052e6aa2d5
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-07-12 14:30:31 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/tournament/TCO05CompRegistrants.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/tournament/TCO05CompRegistrants.java
@@ -11,9 +11,12 @@
 package com.topcoder.web.tc.controller.request.tournament;
 
 import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.DataAccessConstants;
 import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 import com.topcoder.shared.util.DBMS;
 import com.topcoder.web.tc.controller.request.Base;
+import com.topcoder.web.common.StringUtils;
+
 import java.util.Map;
 
 /**
@@ -21,21 +24,28 @@ import java.util.Map;
  * @author rfairfax
  */
 public class TCO05CompRegistrants extends Base {
-    
+
     public void businessProcessing() {
         try {
             Request r = new Request();
             r.setContentHandle("tco05_comp_registrants");
-        
+
             Map m = getDataAccess(DBMS.OLTP_DATASOURCE_NAME, false).getData(r);
-        
+
             ResultSetContainer rsc = (ResultSetContainer)m.get("tco05_comp_registrants");
-            
+
+            String sortDir = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.SORT_DIRECTION));
+            String sortCol = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.SORT_COLUMN));
+
+            if (!(sortCol.equals("") || sortDir.equals(""))) {
+                rsc.sortByColumn(sortCol, sortDir.equals("asc"));
+            }
+
             getRequest().setAttribute("list", rsc);
-            
+
             setNextPage("/tournaments/tco05/comp_registrants.jsp");
             setIsNextPageInContext(true);
-            
+
         } catch (Exception e) {
         }
     }
