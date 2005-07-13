0fe21195ce442bb99536a3468e1c15da76cc84af
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-07-13 22:44:34 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/forums/controller/request/ForumList.java
+++ src/main/com/topcoder/web/forums/controller/request/ForumList.java
@@ -3,30 +3,30 @@
  */
 package com.topcoder.web.forums.controller.request;
 
-import java.util.Iterator;
-
-import com.jivesoftware.forum.ResultFilter;
 import com.jivesoftware.base.JiveConstants;
+import com.jivesoftware.forum.ResultFilter;
+
+import java.util.Iterator;
 
 /**
  * @author mtong
- * 
+ *
  * Provides the root list of forums.
  */
 public class ForumList extends ForumsProcessor {
 	protected void businessProcessing() throws Exception {
 		super.businessProcessing();
-		
+
         ResultFilter resultFilter = new ResultFilter();
         resultFilter.setSortField(JiveConstants.MODIFICATION_DATE);
         resultFilter.setSortOrder(ResultFilter.DESCENDING);
-       
+
         Iterator itCategories = forumFactory.getRootForumCategory().getCategories();
 		Iterator itForums = forumFactory.getRootForumCategory().getForums(resultFilter);
-        
+
 		getRequest().setAttribute("forums", itForums);
         getRequest().setAttribute("categories", itCategories);
-		
+
 		setNextPage("/main.jsp");
 		setIsNextPageInContext(true);
 	}
