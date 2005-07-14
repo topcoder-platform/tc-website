a9591b1664a94725d46974a9ff8c2da2c2df2a5e
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-07-14 02:22:41 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/forums/controller/request/ForumsProcessor.java
+++ src/main/com/topcoder/web/forums/controller/request/ForumsProcessor.java
@@ -7,6 +7,10 @@ import com.jivesoftware.base.AuthToken;
 import com.jivesoftware.base.User;
 import com.jivesoftware.forum.ForumFactory;
 import com.topcoder.web.common.BaseProcessor;
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.CachedDataAccess;
+import com.topcoder.shared.dataAccess.DataAccess;
+import com.topcoder.shared.util.DBMS;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -62,4 +66,24 @@ public abstract class ForumsProcessor extends BaseProcessor {
     public boolean isGuest() {
     	return authToken.isAnonymous();
     }
+
+    public DataAccessInt getDataAccess() throws Exception {
+        return getDataAccess(DBMS.OLTP_DATASOURCE_NAME, false);
+    }
+
+    public DataAccessInt getDataAccess(boolean cached) throws Exception {
+        return getDataAccess(DBMS.OLTP_DATASOURCE_NAME, cached);
+    }
+
+    public DataAccessInt getDataAccess(String datasource, boolean cached) throws Exception {
+        if (datasource == null) return null;
+        DataAccessInt dAccess = null;
+        if (cached)
+            dAccess = new CachedDataAccess(datasource);
+        else
+            dAccess = new DataAccess(datasource);
+        return dAccess;
+    }
+
+
 }
