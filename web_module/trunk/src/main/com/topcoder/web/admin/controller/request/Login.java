3139fc91be389d6692a195e173e7118e40452882
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-07-22 14:16:40 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/admin/controller/request/Login.java
+++ web_module/trunk/src/main/com/topcoder/web/admin/controller/request/Login.java
@@ -1,7 +1,11 @@
 package com.topcoder.web.admin.controller.request;
 
 import com.topcoder.web.common.BaseProcessor;
-import com.topcoder.shared.util.ApplicationServer;
+import com.topcoder.web.common.TCWebException;
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.shared.security.User;
+import com.topcoder.shared.security.SimpleUser;
+import com.topcoder.shared.security.LoginException;
 
 /**
  * @author  dok
@@ -10,15 +14,34 @@ import com.topcoder.shared.util.ApplicationServer;
  */
 public class Login extends BaseProcessor {
 
+    private static final Logger log = Logger.getLogger(Login.class);
 
-    protected void businessProcessing() throws Exception {
+    public static final String KEY_USER_HANDLE = "handle";
+    public static final String KEY_USER_PASS = "pass";
+    private static final String LOGIN_PAGE = "/login.jsp";
 
-        setNextPage("/tc?module=Login&nextpage=http://"+ApplicationServer.SERVER_NAME+"/admin");
-        setIsNextPageInContext(false);
+    public void businessProcessing() throws Exception {
 
-
+        String handle = getRequest().getParameter(KEY_USER_HANDLE);
+        String passw = getRequest().getParameter(KEY_USER_PASS);
+        if (handle == null || handle.trim().length() == 0) {
+            setNextPage(LOGIN_PAGE);
+        } else {
+            log.debug("login attempt[login/passw]: " + handle + "/" + passw);
+            User possibleUser = new SimpleUser(0, handle, passw);
+            try {
+                getAuthentication().login(possibleUser);
+                log.debug("user " + possibleUser.getUserName() + " has logged in");
+                setNextPage(getRequest().getContextPath());
+                setIsNextPageInContext(false);
+            } catch (LoginException e) {
+                addError(KEY_USER_HANDLE, "Login Unsuccessful");
+                setNextPage(LOGIN_PAGE);
+                setIsNextPageInContext(true);
+            } catch (Exception ae) {
+                throw new TCWebException(ae);
+            }
+        }
     }
-
-
 
 }
