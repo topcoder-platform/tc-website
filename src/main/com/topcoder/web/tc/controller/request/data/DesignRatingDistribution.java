64d6fc4c7eaf17808057aadecea07d8543b1d15c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-07-27 20:30:30 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/tc/controller/request/data/DesignRatingDistribution.java
+++ src/main/com/topcoder/web/tc/controller/request/data/DesignRatingDistribution.java
@@ -2,23 +2,24 @@ package com.topcoder.web.tc.controller.request.data;
 
 import com.topcoder.shared.dataAccess.Request;
 import com.topcoder.shared.util.DBMS;
-
+import com.topcoder.web.tc.model.SoftwareComponent;
 
 /**
  * @author  dok
  * @version  $Revision$ $Date$
- * Create Date: Jul 26, 2005
+ * Create Date: Jul 27, 2005
  */
-public class AlgRatingDistribution extends RatingDistribution {
+public class DesignRatingDistribution extends RatingDistribution {
 
     protected Request getDistributionRequest() {
         Request r = new Request();
-        r.setContentHandle("dd_algorithm_rating_distribution");
+        r.setContentHandle("dd_component_rating_distribution");
+        r.setProperty("pi", String.valueOf(SoftwareComponent.DEV_PHASE));
         return r;
     }
 
     protected String getDb() {
-        return DBMS.DW_DATASOURCE_NAME;
+        return DBMS.TCS_DW_DATASOURCE_NAME;
     }
 
 }
