3200f4950b4d2c8a09d2b5bee21dcb3ff87a2687
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-08-02 14:49:22 +0000

    no message

--- src/main/com/topcoder/web/tc/controller/request/authentication/Login.java
+++ src/main/com/topcoder/web/tc/controller/request/authentication/Login.java
@@ -66,8 +66,7 @@ public class Login extends Base {
                                 return;
                             } else {
                                 log.debug("user active");
-                                //String dest = StringUtils.replace(StringUtils.checkNull(getRequest().getParameter(BaseServlet.NEXT_PAGE_KEY)),"&","%26");
-                                String dest = StringUtils.checkNull(getRequest().getParameter(BaseServlet.NEXT_PAGE_KEY));
+                                String dest = StringUtils.replace(StringUtils.checkNull(getRequest().getParameter(BaseServlet.NEXT_PAGE_KEY)),"&","%26");
                                 String forumsURL = "http://"+ApplicationServer.FORUMS_SERVER_NAME;
 
                                 //todo make this https
