996a373ac632a39021626a373cb671856aa31d99
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-08-03 16:13:51 +0000

    *** empty log message ***

---
+++ src/main/com/topcoder/web/tc/controller/request/tournament/TCO05TravelInfoSubmit.java
@@ -0,0 +1,27 @@
+package com.topcoder.web.tc.controller.request.tournament;
+
+import com.topcoder.web.common.SessionInfo;
+import com.topcoder.web.common.BaseServlet;
+import com.topcoder.web.tc.Constants;
+
+/**
+ * @author  dok
+ * @version  $Revision$ $Date$
+ * Create Date: Aug 3, 2005
+ */
+public class TCO05TravelInfoSubmit  extends BaseSubmitTravelInfo {
+    protected String getSuccessPage() {
+        StringBuffer next = new StringBuffer(100);
+        next.append(((SessionInfo) getRequest().getAttribute(BaseServlet.SESSION_INFO_KEY)).getServletPath());
+        next.append("?").append(Constants.MODULE_KEY).append("=Static&");
+        next.append(Constants.STATIC_PREFIX).append("1=tournaments&");
+        next.append(Constants.STATIC_PREFIX).append("2=tco05&");
+        next.append(Constants.STATIC_PREFIX).append("3=docs&");
+        next.append(Constants.STATIC_PREFIX).append("4=travelInfoSent");
+        return next.toString();
+    }
+
+    protected String[] getRecipients() {
+        return new String[]{"mluce@topcoder.com"};
+    }
+}
