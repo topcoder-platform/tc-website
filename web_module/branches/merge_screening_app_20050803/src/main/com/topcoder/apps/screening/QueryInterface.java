a14b1efc3ec3c36ed2064451c8bd69906b4c4e71
ivern <ivern@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-08-05 15:52:33 +0000

    added alternate methods that take a connection

--- web_module/branches/merge_screening_app_20050803/src/main/com/topcoder/apps/screening/QueryInterface.java
+++ web_module/branches/merge_screening_app_20050803/src/main/com/topcoder/apps/screening/QueryInterface.java
@@ -53,6 +53,28 @@ public interface QueryInterface {
      * @throws DatabaseException if an error occured at the database level while attempting to retrieve the results.
      */
     ScreeningRecord[] getSubmissionStatus(long projectId, long submitterId);
+    
+    /**
+     * <strong>Purpose</strong>:
+     * Obtains details about the screening processes run for all submission by a given submitter to a given project.
+     * Each response code and dynamic response text will be returned. This version of the method uses a specified connection.
+     *
+     * <strong>Valid Args</strong>:
+     * An integer.
+     *
+     * <strong>Valid Return Values</strong>:
+     * A String.
+     *
+     * @param projectId The id of the project to investigate.
+     * @param submitterId the identifier for a submitter.
+     * @param conn The connection to use.
+     * @return An XML String containing the results of the query.
+     * @throws InvalidProjectNameException if the 'project' parameter does not correspond to an existing project.
+     * @throws InvalidSubmitterIdException if the 'submitterId' parameter does not correspond to an exisiting
+     *                                     submitter.
+     * @throws DatabaseException if an error occured at the database level while attempting to retrieve the results.
+     */
+    ScreeningRecord[] getSubmissionStatus(long projectId, long submitterId, Connection conn);
 
     /**
      * <strong>Purpose</strong>:
@@ -72,4 +94,22 @@ public interface QueryInterface {
      */
     ScreeningRecord[] getAllSubmissions(long projectId);
 
+    /**
+     * <strong>Purpose</strong>:
+     * Obtains details about the screening processes run for all submissions to a particular project. Each response
+     * code and dynamic response text will be returned. This version of the method uses a specified connection.
+     *
+     * <strong>Valid Args</strong>:
+     * A non-null string.
+     *
+     * <strong>Valid Return Values</strong>:
+     * A String.
+     *
+     * @param project The id of the project to investigate.
+     * @param conn The connection to use.
+     * @return An XML String containing the results of the query.
+     * @throws InvalidProjectNameException if the 'project' parameter does not correspond to an existing project.
+     * @throws DatabaseException if an error occured at the database level while attempting to retrieve the results.
+     */
+    ScreeningRecord[] getAllSubmissions(long projectId, Connection conn);
 }
