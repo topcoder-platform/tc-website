1253c88ccb0eba437fa63d232f64df4bae9a8769
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-08-18 19:13:38 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/forums/controller/request/PreviewMessage.java
+++ web_module/trunk/src/main/com/topcoder/web/forums/controller/request/PreviewMessage.java
@@ -37,7 +37,7 @@ public class PreviewMessage extends ForumsProcessor {
         String subject = com.jivesoftware.util.StringUtils.escapeHTMLTags(
                 getRequest().getParameter(ForumConstants.MESSAGE_SUBJECT).trim());
         String body = getRequest().getParameter(ForumConstants.MESSAGE_BODY).trim();
-        String previewBody = com.jivesoftware.util.StringUtils.escapeHTMLTags(body).replaceAll("&","&amp;amp;");
+        String previewBody = com.jivesoftware.util.StringUtils.escapeHTMLTags(body).replaceAll("&","&amp;");
         log.debug("##############previewBody: "+previewBody);
         log.debug("##############previewBody: "+previewBody);
         
