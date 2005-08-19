43423b5df6dfa7553ddd82790fefa1100e6f2331
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-08-19 21:39:25 +0000

    *** empty log message ***

---
+++ src/main/com/topcoder/utilities/HandleChars.java
@@ -0,0 +1,46 @@
+package com.topcoder.utilities;
+
+import com.topcoder.shared.dataAccess.QueryRequest;
+import com.topcoder.shared.dataAccess.QueryDataAccess;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.util.DBMS;
+
+import java.util.Map;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+/**
+ * @author  dok
+ * @version  $Revision$ $Date$
+ * Create Date: Aug 19, 2005
+ */
+public class HandleChars {
+    public static void main(String[] args) {
+        try {
+            String query = "select handle from user";
+            QueryRequest qr = new QueryRequest();
+            qr.addQuery("handles", query);
+            QueryDataAccess da = new QueryDataAccess(DBMS.OLTP_DATASOURCE_NAME);
+            Map m = da.getData(qr);
+            ResultSetContainer rsc = (ResultSetContainer)m.get("handles");
+            TreeSet set = new TreeSet();
+            String handle = null;
+            ResultSetContainer.ResultSetRow row = null;
+            for (Iterator it = rsc.iterator(); it.hasNext();) {
+                row = (ResultSetContainer.ResultSetRow)it.next();
+                handle = row.getStringItem("handle");
+                for (int i=0; i<handle.length(); i++) {
+                    set.add(new Character(handle.charAt(i)));
+                }
+            }
+
+            for (Iterator it = set.iterator(); it.hasNext();) {
+                System.out.print(it.next().toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+}
