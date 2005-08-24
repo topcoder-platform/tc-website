15291d1a79d8252e3de0023a7f19194ee1814a07
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-08-24 18:44:36 +0000

    *** empty log message ***

--- web_module/branches/dw_scorecards_20050817/src/main/com/topcoder/web/query/servlet/jsp/tag/ResultSetItemTag.java
+++ web_module/branches/dw_scorecards_20050817/src/main/com/topcoder/web/query/servlet/jsp/tag/ResultSetItemTag.java
@@ -2,6 +2,7 @@ package com.topcoder.web.query.servlet.jsp.tag;
 
 import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.web.common.StringUtil;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
@@ -12,6 +13,7 @@ public class ResultSetItemTag extends TagSupport {
     private static Logger log = Logger.getLogger(ResultSetItemTag.class);
     private ResultSetContainer.ResultSetRow row;
     private String name;
+    private boolean escapeHTML;
 
     public void setRow(ResultSetContainer.ResultSetRow row) {
         this.row = row;
@@ -21,9 +23,14 @@ public class ResultSetItemTag extends TagSupport {
         this.name = name;
     }
 
+    public void setEscapeHTML(boolean escapeHTML) {
+        this.escapeHTML = escapeHTML;
+    }
+
     public int doStartTag() throws JspException {
         try {
-            pageContext.getOut().print(row.getItem(name).toString());
+            String text = escapeHTML? StringUtil.htmlEncode(row.getItem(name).toString()) : row.getItem(name).toString();
+            pageContext.getOut().print(text);
         } catch (IOException e) {
             throw new JspException(e.getMessage());
         }
