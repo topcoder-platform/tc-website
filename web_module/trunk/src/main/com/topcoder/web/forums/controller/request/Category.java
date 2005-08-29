456af23ce8f3cc2cb3a4951bcdafc8a084f13ba9
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-08-29 19:13:17 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/forums/controller/request/Category.java
+++ web_module/trunk/src/main/com/topcoder/web/forums/controller/request/Category.java
@@ -7,8 +7,10 @@ import com.jivesoftware.base.JiveConstants;
 import com.jivesoftware.forum.ResultFilter;
 import com.jivesoftware.forum.ForumCategory;
 import com.jivesoftware.forum.Forum;
+import com.jivesoftware.forum.action.util.Paginator;
+import com.topcoder.web.common.StringUtils;
 import com.topcoder.web.forums.ForumConstants;
-import com.topcoder.web.forums.controller.ForumsUtil;
+import com.topcoder.web.forums.model.Paging;
 
 import java.util.Iterator;
 import java.util.ArrayList;
@@ -23,12 +25,28 @@ public class Category extends ForumsProcessor {
 		super.businessProcessing();
 
         long categoryID = Long.parseLong(getRequest().getParameter(ForumConstants.CATEGORY_ID));
+        ForumCategory forumCategory = forumFactory.getForumCategory(categoryID);
+        
+        int startIdx = 0;
+        if ((!StringUtils.checkNull(getRequest().getParameter(ForumConstants.START_IDX)).equals(""))) {
+            startIdx = Integer.parseInt(getRequest().getParameter(ForumConstants.START_IDX));
+        }
+        int forumRange = ForumConstants.DEFAULT_FORUM_RANGE;
+        if (user != null) {
+            try {
+                forumRange = Integer.parseInt(user.getProperty("jiveForumRange"));
+            } catch (Exception ignored) {}
+        }
         
         ResultFilter resultFilter = new ResultFilter();
         resultFilter.setSortField(JiveConstants.MODIFICATION_DATE);
         resultFilter.setSortOrder(ResultFilter.DESCENDING);
+        resultFilter.setStartIndex(startIdx);
+        resultFilter.setNumResults(forumRange);
+        int totalItemCount = forumCategory.getForumCount(resultFilter);
 
-        ForumCategory forumCategory = forumFactory.getForumCategory(categoryID);
+        Paging paging = new Paging(resultFilter, totalItemCount);
+        Paginator paginator = new Paginator(paging);
         Iterator itCategories = forumCategory.getCategories();
 		Iterator itForums = forumCategory.getForums(resultFilter);
         
@@ -48,6 +66,7 @@ public class Category extends ForumsProcessor {
 		getRequest().setAttribute("forums", itForums);
         getRequest().setAttribute("categories", itCategories);
         getRequest().setAttribute("resultFilter", resultFilter);
+        getRequest().setAttribute("paginator", paginator);
 
 		setNextPage("/viewCategory.jsp");
 		setIsNextPageInContext(true);
