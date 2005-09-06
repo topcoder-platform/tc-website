d9dff3eb43adc9ab8943710bb02fe1f4feed4457
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-09-06 22:02:13 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/query/common/Constants.java
+++ src/main/com/topcoder/web/query/common/Constants.java
@@ -176,10 +176,10 @@ public class Constants {
         DB_LIST.add(new DatabaseBean(DBMS.TCS_DW_DATASOURCE_NAME, "TCS Data Warehouse"));
         DB_LIST.add(new DatabaseBean(DBMS.CORP_OLTP_DATASOURCE_NAME, "Corporate"));
         DB_LIST.add(new DatabaseBean(DBMS.SCREENING_OLTP_DATASOURCE_NAME, "Screening"));
-        DB_LIST.add(new DatabaseBean("GOOGLE_OLTP", "Google"));
-        DB_LIST.add(new DatabaseBean("GOOGLE_DW", "Google DW"));
-        DB_LIST.add(new DatabaseBean("GOOGLE_INDIA_OLTP", "Google India"));
-        DB_LIST.add(new DatabaseBean("GOOGLE_INDIA_DW", "Google India DW"));
+        DB_LIST.add(new DatabaseBean("java:/GOOGLE_OLTP", "Google"));
+        DB_LIST.add(new DatabaseBean("java:/GOOGLE_DW", "Google DW"));
+        DB_LIST.add(new DatabaseBean("java:/GOOGLE_INDIA_OLTP", "Google India"));
+        DB_LIST.add(new DatabaseBean("java:/GOOGLE_INDIA_DW", "Google India DW"));
     }
 
 }
