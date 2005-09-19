151db135fdf079c026b2813574fed84617112779
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-09-19 19:23:15 +0000

    *** empty log message ***

---
+++ src/main/com/topcoder/web/tc/controller/request/tournament/TCO06LogoTermsAgree.java
@@ -0,0 +1,44 @@
+package com.topcoder.web.tc.controller.request.tournament;
+
+import com.topcoder.web.tc.controller.request.Base;
+import com.topcoder.web.tc.Constants;
+import com.topcoder.web.common.PermissionException;
+import com.topcoder.web.ejb.user.UserTermsOfUse;
+import com.topcoder.shared.security.SimpleResource;
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.shared.util.ApplicationServer;
+
+import javax.transaction.TransactionManager;
+import javax.transaction.Status;
+
+/**
+ * @author  dok
+ * @version  $Revision$ $Date$
+ * Create Date: Sept 19, 2005
+ */
+public class TCO06LogoTermsAgree extends Base {
+
+    protected void businessProcessing() throws Exception {
+        if (getUser().isAnonymous()) {
+            throw new PermissionException(getUser(), new SimpleResource(this.getClass().getName()));
+        } else {
+            UserTermsOfUse ut = (UserTermsOfUse)createEJB(getInitialContext(), UserTermsOfUse.class);
+
+            TransactionManager tm = (TransactionManager) getInitialContext().lookup(ApplicationServer.TRANS_MANAGER);
+            try {
+                tm.begin();
+                if (!ut.hasTermsOfUse(getUser().getId(), Constants.TCO06_LOGO_TERMS_ID, DBMS.JTS_OLTP_DATASOURCE_NAME)) {
+                    ut.createUserTermsOfUse(getUser().getId(), Constants.TCO06_LOGO_TERMS_ID, DBMS.JTS_OLTP_DATASOURCE_NAME);
+                }
+                tm.commit();
+            } catch (Exception e) {
+                if (tm!=null && tm.getStatus()==Status.STATUS_ACTIVE)
+                    tm.rollback();
+                throw e;
+            }
+            setNextPage("/tournaments/tco06/logo_submit.jsp");
+            setIsNextPageInContext(true);
+        }
+    }
+
+}
