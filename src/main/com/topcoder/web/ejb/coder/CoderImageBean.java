0053317e637872fe524952265619bd7e4663d104
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-09-19 18:54:26 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/ejb/coder/CoderImageBean.java
+++ src/main/com/topcoder/web/ejb/coder/CoderImageBean.java
@@ -2,6 +2,15 @@ package com.topcoder.web.ejb.coder;
 
 import com.topcoder.web.ejb.BaseEJB;
 import com.topcoder.web.common.RowNotFoundException;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.util.DBMS;
+
+import javax.naming.InitialContext;
+import javax.ejb.EJBException;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
 
 
 /**
@@ -35,4 +44,46 @@ public class CoderImageBean extends BaseEJB {
             return false;
         }
     }
+
+    private final String IMAGES = "select i.image_id " +
+                                       ", i.file_name " +
+                                       ", i.image_type_id " +
+                                       ", i.link " +
+                                       ", i.height " +
+                                       ", i.width " +
+                                       ", i.modify_date " +
+                                    "from coder_image_xref x " +
+                                       ", image i " +
+                                   "where i.image_id = x.image_id " +
+                                     "and x.coder_id = ? " +
+                                     "and i.image_type_id = ?";
+
+    public ResultSetContainer getImages(long coderId, int imageTypeId, String dataSource) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        InitialContext ctx = null;
+        ResultSet rs = null;
+        try {
+
+            conn = DBMS.getConnection(dataSource);
+            ps = conn.prepareStatement(IMAGES);
+            ps.setLong(1, coderId);
+            ps.setInt(2, imageTypeId);
+            rs = ps.executeQuery();
+            return new ResultSetContainer(rs);
+
+        } catch (SQLException e) {
+            DBMS.printSqlException(true, e);
+            throw new EJBException(e.getMessage());
+        } catch (Exception e) {
+            throw new EJBException(e.getMessage());
+        } finally {
+            close(rs);
+            close(ps);
+            close(conn);
+            close(ctx);
+        }
+
+    }
+
 }
