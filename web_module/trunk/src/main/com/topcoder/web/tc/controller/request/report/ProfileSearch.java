a4fb157aa8b7ca7ceab7a2eaab06dd4b2892ce88
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-10-05 17:04:39 +0000

    *** empty log message ***

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/report/ProfileSearch.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/report/ProfileSearch.java
@@ -75,7 +75,7 @@ public class ProfileSearch extends Base {
         (constraints = demo[1]).addAll(skills[1]);
         String comp = request.getParameter("company");
         String sch = request.getParameter("school");
-        
+
         StringBuffer query = new StringBuffer(5000);
         if("on".equals(request.getParameter("count"))){
             headers.add("Count");
@@ -136,11 +136,12 @@ public class ProfileSearch extends Base {
             query.append("  , (select ur1.rating from tcs_catalog:user_rating ur1 where ur1.user_id = c.coder_id AND ur1.phase_id = 112) as Design_Rating\n");
             query.append("  , (select ur2.rating from tcs_catalog:user_rating ur2 where ur2.user_id = c.coder_id AND ur2.phase_id = 113) as Development_Rating\n");
             query.append("  , (select '<a href=/tc?module=DownloadResume&uid=' || res2.coder_id || '>Resume</a>' from resume res2 where res2.coder_id = c.coder_id)\n");
-            query.append("  , (select '<a href=/tc?module=ViewNotes&uid=' || c.coder_id || '>' || max(n.modify_date) || '</a>' from user_note_xref unx, note n where unx.user_id = c.coder_id and unx.note_id = n.note_id)\n");
+            query.append("  , (select max(n.modify_date) from user_note_xref unx, note n where unx.user_id = c.coder_id and unx.note_id = n.note_id)\n");
             query.append("  , (select unique '<a href=/tc?module=PlacementInfoDetail&uid=' || upi.user_id || '>Placement Info</a>' from user_preference upi where upi.user_id = c.coder_id AND upi.preference_id in (2,7))\n");
+            query.append("  , (select '<a href=/tc?module=ViewNotes&uid=' || c.coder_id || '>Notes</a>' from user_note_xref unx, note n where unx.user_id = c.coder_id and unx.note_id = n.note_id)\n");
             query.append("  , '<a href=/tc?module=LegacyReport&t=profile&ha=' || u.handle || '>General Info</a>'\n");
             query.append("  , '<a href=/tc?module=MemberProfile&cr=' || c.coder_id || '>Public Profile</a>'\n");
-            headers.addAll(Arrays.asList(new String[]{ "Algorithm Rating", "Design Rating", "Development Rating","","Notes","","",""}));
+            headers.addAll(Arrays.asList(new String[]{ "Algorithm Rating", "Design Rating", "Development Rating","","Notes","","","",""}));
             for(int i = 0; i<skills[2].size(); i++){
                 query.append("  , ");
                 query.append((String)skills[2].get(i));
