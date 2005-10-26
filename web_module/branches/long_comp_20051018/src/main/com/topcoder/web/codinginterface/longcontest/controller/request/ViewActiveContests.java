b703fbd0233dc7bc3aa915dfabbe3018f6dd9f9a
farsight <farsight@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-10-26 08:41:21 +0000

    Initial Creation

---
+++ web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/controller/request/ViewActiveContests.java
@@ -0,0 +1,28 @@
+package com.topcoder.web.codinginterface.longcontest.controller.request;
+
+import java.util.Map;
+
+import com.topcoder.web.codinginterface.longcontest.Constants;
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.web.common.TCRequest;
+import com.topcoder.web.common.TCWebException;
+import com.topcoder.shared.util.logging.Logger;
+
+
+public class ViewActiveContests extends Base{
+	
+    protected static final Logger log = Logger.getLogger(ViewActiveContests.class);
+
+    protected void businessProcessing() throws TCWebException {
+//        try{
+//            
+//        }catch(TCWebException e){
+//            throw e;
+//        }catch(Exception e){
+//            e.printStackTrace();
+//            throw new TCWebException("Error retrieving page.");
+//        }
+    }
+}
