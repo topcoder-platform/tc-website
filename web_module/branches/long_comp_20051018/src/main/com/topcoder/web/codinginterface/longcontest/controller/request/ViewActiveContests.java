8688039f203b333e353be04ce8f9230af5c4ba7b
farsight <farsight@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-10-27 23:39:54 +0000

    *** empty log message ***

--- web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/controller/request/ViewActiveContests.java
+++ web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/controller/request/ViewActiveContests.java
@@ -2,14 +2,15 @@ package com.topcoder.web.codinginterface.longcontest.controller.request;
 
 import java.util.Map;
 
-import com.topcoder.web.codinginterface.longcontest.Constants;
+import com.topcoder.shared.dataAccess.CachedDataAccess;
 import com.topcoder.shared.dataAccess.DataAccessInt;
 import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultColumn;
 import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
-import com.topcoder.web.common.TCRequest;
-import com.topcoder.web.common.TCWebException;
+import com.topcoder.shared.util.DBMS;
 import com.topcoder.shared.util.logging.Logger;
-
+import com.topcoder.web.codinginterface.longcontest.Constants;
+import com.topcoder.web.common.TCWebException;
 
 public class ViewActiveContests extends Base{
 	
@@ -25,6 +26,30 @@ public class ViewActiveContests extends Base{
 //            e.printStackTrace();
 //            throw new TCWebException("Error retrieving page.");
 //        }
+    	
+    	try {
+	    	DataAccessInt dai = new CachedDataAccess(DBMS.OLTP_DATASOURCE_NAME);
+	    	
+	    	Request r = new Request();
+	    	r.setContentHandle("long_contest_active_contests");
+	    	
+	    	Map m = dai.getData(r);
+	    	ResultSetContainer rsc = (ResultSetContainer)m.get("long_contest_active_contests");
+	    	
+	    	for(int i = 0; i < rsc.getRowCount(); i++) {
+	    		for(int j = 0; j < rsc.getColumnCount(); j++) {
+	    			//ResultColumn col = rsc.getColumnInfo(j);
+	    			System.out.print(rsc.getStringItem(i,j));
+	    		}
+	    		System.out.println();
+	    	}
+	    	
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    		throw new TCWebException("Error retrieving page.");
+    	}
+    	
+    	
     	setNextPage(Constants.PAGE_ACTIVE_CONTESTS);
     	setIsNextPageInContext(true);
     }
