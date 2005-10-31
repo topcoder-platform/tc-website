2da8e85092dca62ef94b6308af7d745f05bf891d
farsight <farsight@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-10-31 06:38:55 +0000

    added PAGE_STANDINGS

--- web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/Constants.java
+++ web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/Constants.java
@@ -67,6 +67,7 @@ public class Constants implements CodingInterfaceConstants {
     public static String RP_PROBLEM_STATEMENT;
     public static String PAGE_PROBLEM_STATEMENT;
     public static String PAGE_PROBLEM_STATEMENT_CONTENT;
+    public static String PAGE_STANDINGS;
     public static String CONTEST_LIST_KEY;
     public static String ROUND_STANDINGS_LIST_KEY;
     
