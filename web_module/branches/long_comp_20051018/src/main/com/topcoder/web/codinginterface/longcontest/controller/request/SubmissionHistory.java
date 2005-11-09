bd8373f4a95762116269d7658f200d561abb2c4b
porgery <porgery@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-11-09 20:33:49 +0000

    Added sorting/paging code

--- web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/controller/request/SubmissionHistory.java
+++ web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/controller/request/SubmissionHistory.java
@@ -3,11 +3,13 @@ package com.topcoder.web.codinginterface.longcontest.controller.request;
 import java.util.Map;
 
 import com.topcoder.web.codinginterface.longcontest.Constants;
+import com.topcoder.shared.dataAccess.DataAccessConstants;
 import com.topcoder.shared.dataAccess.DataAccessInt;
 import com.topcoder.shared.dataAccess.Request;
 import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 import com.topcoder.web.common.TCRequest;
 import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.common.PermissionException;
 import com.topcoder.shared.util.logging.Logger;
 
 
@@ -15,17 +17,57 @@ public class SubmissionHistory extends Base{
     protected static final Logger log = Logger.getLogger(SubmissionHistory.class);
 
     protected void businessProcessing() throws TCWebException {
+	if (getUser().isAnonymous()) {
+            throw new PermissionException(getUser(), new ClassResource(this.getClass()));
+	}
         try{
             TCRequest request = getRequest();
             Request r = new Request();
-            r.setContentHandle("long_coder_submissions");
+            
+            String startRankStr = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.START_RANK));
+            String numRecordsStr = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.NUMBER_RECORDS));
+            String sortDir = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.SORT_DIRECTION));
+            String sortColStr = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.SORT_COLUMN));
+            
+            int numRecords = Integer.parseInt(Constants.DEFAULT_ROW_COUNT);
+            int startRank = 1, sortCol = 3;
+            if (!"".equals(numRecordsStr)) {
+                numRecords = Integer.parseInt(numRecordsStr);
+            }
+            if (!startRankStr.equals("")){
+                startRank = Integer.parseInt(startRankStr);
+            }
+            if (!sortColStr.equals("")) {
+                sortCol = Integer.parseInt(sortColStr);
+            }
+
+            int endRank = startRank + numRecords - 1;
+            
+            String component = request.getParameter(Constants.COMPONENT_ID);
+            if(component == null){
+                // TODO: translate problem id into component id
+            }
+            
+            r.setContentHandle("long_contest_submission_history");
             r.setProperty(Constants.CODER_ID,request.getParameter(Constants.CODER_ID));
-            r.setProperty(Constants.COMPONENT_ID,request.getParameter(Constants.COMPONENT_ID));
+            r.setProperty(Constants.COMPONENT_ID,component);
             r.setProperty(Constants.ROUND_ID,request.getParameter(Constants.ROUND_ID));
-            DataAccessInt dataAccess = getDataAccess(false);
-            Map m = dataAccess.getData(r);
-            request.setAttribute(Constants.LONG_CONTEST_CODER_SUBMISSIONS_KEY, m);
-            setNextPage(Constants.CODER_SUBMISSIONS_JSP);
+            Map result = getDataAccess(false).getData(r);
+            ResultSetContainer rsc = (ResultSetContainer) result.get("long_coder_submissions");
+            rsc.sortByColumn(sortCol, !"desc".equals(sortDir));
+
+            rsc = new ResultSetContainer(rsc, startRank, endRank);
+
+            result.put("long_coder_submissions", rsc);
+
+//            SortInfo s = new SortInfo();
+//            getRequest().setAttribute(SortInfo.REQUEST_KEY, s);
+
+            setDefault(DataAccessConstants.NUMBER_RECORDS, ""+numRecords);
+            setDefault(DataAccessConstants.START_RANK, ""+startRank);
+
+            request.setAttribute("resultMap", result);
+            setNextPage(Constants.PAGE_SUBMISSION_HISTORY);
             setIsNextPageInContext(true);
         }catch(Exception e){
             e.printStackTrace();
--- web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/controller/request/ViewSubmissionHistory.java
+++ web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/controller/request/ViewSubmissionHistory.java
@@ -15,16 +15,56 @@ public class ViewSubmissionHistory extends Base{
     protected static final Logger log = Logger.getLogger(ViewSubmissionHistory.class);
 
     protected void businessProcessing() throws TCWebException {
+	if (getUser().isAnonymous()) {
+            throw new PermissionException(getUser(), new ClassResource(this.getClass()));
+	}
         try{
             TCRequest request = getRequest();
             Request r = new Request();
-            r.setContentHandle("long_coder_submissions");
+            
+            String startRankStr = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.START_RANK));
+            String numRecordsStr = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.NUMBER_RECORDS));
+            String sortDir = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.SORT_DIRECTION));
+            String sortColStr = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.SORT_COLUMN));
+            
+            int numRecords = Integer.parseInt(Constants.DEFAULT_ROW_COUNT);
+            int startRank = 1, sortCol = 3;
+            if (!"".equals(numRecordsStr)) {
+                numRecords = Integer.parseInt(numRecordsStr);
+            }
+            if (!startRankStr.equals("")){
+                startRank = Integer.parseInt(startRankStr);
+            }
+            if (!sortColStr.equals("")) {
+                sortCol = Integer.parseInt(sortColStr);
+            }
+
+            int endRank = startRank + numRecords - 1;
+            
+            String component = request.getParameter(Constants.COMPONENT_ID);
+            if(component == null){
+                // TODO: translate problem id into component id
+            }
+            
+            r.setContentHandle("long_contest_submission_history");
             r.setProperty(Constants.CODER_ID,request.getParameter(Constants.CODER_ID));
-            r.setProperty(Constants.COMPONENT_ID,request.getParameter(Constants.COMPONENT_ID));
+            r.setProperty(Constants.COMPONENT_ID,component);
             r.setProperty(Constants.ROUND_ID,request.getParameter(Constants.ROUND_ID));
-            DataAccessInt dataAccess = getDataAccess(false);
-            Map m = dataAccess.getData(r);
-            request.setAttribute(Constants.LONG_CONTEST_CODER_SUBMISSIONS_KEY, m);
+            Map result = getDataAccess(false).getData(r);
+            ResultSetContainer rsc = (ResultSetContainer) result.get("long_coder_submissions");
+            rsc.sortByColumn(sortCol, !"desc".equals(sortDir));
+
+            rsc = new ResultSetContainer(rsc, startRank, endRank);
+
+            result.put("long_coder_submissions", rsc);
+
+//            SortInfo s = new SortInfo();
+//            getRequest().setAttribute(SortInfo.REQUEST_KEY, s);
+
+            setDefault(DataAccessConstants.NUMBER_RECORDS, ""+numRecords);
+            setDefault(DataAccessConstants.START_RANK, ""+startRank);
+
+            request.setAttribute("resultMap", result);
             setNextPage(Constants.PAGE_SUBMISSION_HISTORY);
             setIsNextPageInContext(true);
         }catch(Exception e){
