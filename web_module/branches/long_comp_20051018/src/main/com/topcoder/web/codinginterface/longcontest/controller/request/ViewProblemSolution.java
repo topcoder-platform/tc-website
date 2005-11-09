24300b26b41dd00008dc61c4b76be051a8917cc8
porgery <porgery@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-11-09 03:50:51 +0000

    Converted from transactional db to warehouse db

--- web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/controller/request/ViewProblemSolution.java
+++ web_module/branches/long_comp_20051018/src/main/com/topcoder/web/codinginterface/longcontest/controller/request/ViewProblemSolution.java
@@ -18,19 +18,19 @@ public class ViewProblemSolution extends Base{
         try{
             TCRequest request = getRequest();
             Request r = new Request();
-            r.setContentHandle("long_code");
+            r.setContentHandle("long_contest_submission");
             r.setProperty(Constants.CODER_ID,request.getParameter(Constants.CODER_ID));
-            r.setProperty(Constants.COMPONENT_ID,request.getParameter(Constants.COMPONENT_ID));
+            r.setProperty(Constants.PROBLEM_ID,request.getParameter(Constants.PROBLEM_ID));
             r.setProperty(Constants.ROUND_ID,request.getParameter(Constants.ROUND_ID));
             r.setProperty(Constants.SUBMISSION_NUMBER,request.getParameter(Constants.SUBMISSION_NUMBER));
-            DataAccessInt dataAccess = getDataAccess(false);
+            DataAccessInt dataAccess = getDataAccess(DBMS.DW_DATASOURCE_NAME, true);
             Map m = dataAccess.getData(r);
-            ResultSetContainer rsc = (ResultSetContainer)m.get("long_contest_over");
-            boolean over = rsc.getBooleanItem(0,0);
-            if(!over){
-                throw new TCWebException("Long contest is not over yet.");
-            }
-            request.setAttribute(Constants.LONG_CODE_KEY, m);
+ //           ResultSetContainer rsc = (ResultSetContainer)m.get("long_contest_over");
+ //           boolean over = rsc.getBooleanItem(0,0);
+ //           if(!over){
+ //               throw new TCWebException("Long contest is not over yet.");
+ //           }
+            request.setAttribute("resultMap", m);
             setNextPage(Constants.PAGE_PROBLEM_SOLUTION);
             setIsNextPageInContext(true);
         }catch(TCWebException e){
