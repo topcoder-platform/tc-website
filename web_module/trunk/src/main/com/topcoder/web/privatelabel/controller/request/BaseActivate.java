71bb1b0917d83c3df37c3ff726e379ebb0ecf4d7
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-11-10 18:17:29 +0000

    create a reginfo object that can be used to determine what page to go to...ie it includes a locale

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/BaseActivate.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/BaseActivate.java
@@ -5,8 +5,10 @@ import com.topcoder.web.common.StringUtils;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.ejb.user.User;
 import com.topcoder.web.privatelabel.model.SimpleRegInfo;
+import com.topcoder.web.privatelabel.Constants;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 abstract public class BaseActivate extends RegistrationBase {
 
@@ -66,7 +68,13 @@ abstract public class BaseActivate extends RegistrationBase {
      * @throws Exception
      */
     protected SimpleRegInfo makeRegInfo() throws Exception {
-        return null;
+        SimpleRegInfo ret = new SimpleRegInfo();
+        if (hasRequestParameter(Constants.LOCALE)) {
+            ret.setLocale(new Locale(StringUtils.checkNull(getRequestParameter(Constants.LOCALE))));
+        } else {
+            ret.setLocale(Locale.US);
+        }
+        return ret;
     }
 
 }
