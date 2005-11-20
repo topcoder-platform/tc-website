c652d8e8448a86f244bfa0ec69cff372c4581936
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-11-20 21:40:14 +0000

    adjust location of resume bean

--- src/main/com/topcoder/web/ejb/resume/ResumeServices.java
+++ src/main/com/topcoder/web/ejb/resume/ResumeServices.java
@@ -1,7 +1,7 @@
 package com.topcoder.web.ejb.resume;
 
 import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
-import com.topcoder.web.tc.controller.legacy.resume.bean.Resume;
+import com.topcoder.web.tc.controller.request.resume.Resume;
 
 import javax.ejb.EJBException;
 import javax.ejb.EJBObject;
