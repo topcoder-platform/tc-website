1f4cbec905add5f9ea72cf4b79f74f4f6b1f7acb
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-11-21 21:29:53 +0000

    add survey admin site

--- web_module/trunk/src/main/com/topcoder/web/admin/AdminConstants.java
+++ web_module/trunk/src/main/com/topcoder/web/admin/AdminConstants.java
@@ -9,4 +9,26 @@ public interface AdminConstants {
     public static final String TERMS_OF_USE_ID = "tid";
     public static final String TERMS = "terms";
     public static final String TERMS_OF_USE_TYPE_ID = "ttid";
+    public static final String SURVEY_ID = "sid";
+    public static final String QUESTION_ID = "qid";
+    public static final String ANSWER_ID = "aid";
+    public static final String SURVEY_NAME = "sname";
+    public static final String SURVEY_START = "sstart";
+    public static final String SURVEY_END = "send";
+    public static final String SURVEY_STATUS = "sstatus";
+    public static final String SURVEY_TEXT = "stext";
+    public static final String RESULTS_VIEWABLE = "rv";
+    public static final String KEYWORD = "kw";
+    public static final String QUESTION_STYLE = "qs";
+    public static final String QUESTION_TYPE = "qt";
+    public static final String QUESTION_STATUS = "qstat";
+    public static final String QUESTION_TEXT = "qtext";
+    public static final String REQUIRED = "req";
+    public static final String ANSWER_TEXT ="atext";
+    public static final String SORT = "sort";
+
+    public static final int[] ACTIVE_STATUSES = {1,83};
+    public static final int[] INACTIVE_STATUSES = {0,82};
+
+
 }
