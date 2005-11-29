f3927e1fca7126dda38c238413476344f125368b
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-11-29 00:19:42 +0000

    have textinput take a format param so that the default value can be displayed as intended

--- web_module/trunk/src/main/com/topcoder/web/common/tag/TextInputTag.java
+++ web_module/trunk/src/main/com/topcoder/web/common/tag/TextInputTag.java
@@ -1,16 +1,26 @@
 package com.topcoder.web.common.tag;
 
 import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.util.format.ObjectFormatter;
+import com.topcoder.util.format.ObjectFormatterFactory;
+import com.topcoder.util.format.FormatMethodFactory;
+import com.topcoder.web.common.DateUtils;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class TextInputTag extends BaseTag {
     protected static Logger log = Logger.getLogger(TextInputTag.class);
-    private String value;
+    private Object value;
     private int size = -1;
     private int maxlength = -1;
     private boolean passw = false;
     private String styleClass = null;
     private String onKeyPress = null;
     private boolean editable = true;
+    private String format = null;
+    private String timeZone = null;
 
     public int doStartTag() {
         StringBuffer ret = new StringBuffer();
@@ -32,30 +42,52 @@ public class TextInputTag extends BaseTag {
             }
 
             if (value == null) {
-                value = getDefaultValue() == null ? null : getDefaultValue().toString();
+                value = getDefaultValue() == null ? null : getDefaultValue();
             }
             if (value != null) {
-                ret.append("value=\"").append(value).append("\" ");
+                ret.append("value=\"").append(format(value)).append("\" ");
             }
             ret.append("/>");
         } else {
             if (value == null) {
-                value = getDefaultValue() == null ? null : getDefaultValue().toString();
+                value = getDefaultValue() == null ? null : getDefaultValue();
             }
             if (value != null) {
-                ret.append(value);
+                ret.append(format(value));
             }
         }
-        log.debug("outputing: " + ret.toString());
         try {
-            //pageContext.getOut().print(new String(ret.toString().getBytes(), "UTF-8"));
             pageContext.getOut().print(ret.toString());
-            //<%@ page pageEncoding="ISO-8859-1" %>
         } catch (java.io.IOException ioe) {
         }
         return SKIP_BODY;
     }
 
+    public String format(final Object value) {
+        ObjectFormatter formatter = ObjectFormatterFactory.getEmptyFormatter();
+        Object object = null;
+        if (format != null) {
+            if (value instanceof Number) {
+                formatter.setFormatMethodForClass(Number.class,
+                        new NumberFormatMethod(format), true);
+            } else if (value instanceof Date) {
+                Calendar cal = Calendar.getInstance();
+                if (timeZone!=null) {
+                    object = DateUtils.getConvertedDate((Date)value, timeZone);
+                    cal.setTimeZone(TimeZone.getTimeZone(timeZone));
+                } else {
+                    cal.setTimeZone(TimeZone.getDefault());
+                }
+                cal.setTime((Date)object);
+                object = cal;
+                formatter.setFormatMethodForClass(Calendar.class,
+                        new CalendarDateFormatMethod(format), true);
+            }
+        }
+        formatter.setFormatMethodForClass(new Object().getClass(),
+                    FormatMethodFactory.getDefaultObjectFormatMethod(), true);
+        return formatter.format(object);
+    }
 
     /**
      * Sets the value.
@@ -121,6 +153,14 @@ public class TextInputTag extends BaseTag {
         editable = edit;
     }
 
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
     protected void init() {
         this.value = null;
         this.size = -1;
@@ -129,6 +169,7 @@ public class TextInputTag extends BaseTag {
         this.styleClass = null;
         this.onKeyPress = null;
         this.editable = true;
+        this.format =null;
     }
 
 
