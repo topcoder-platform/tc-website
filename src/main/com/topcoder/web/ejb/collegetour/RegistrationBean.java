76fc0b7dcfbefec6d1301e7ff81b9dc4e7509799
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-12-05 23:54:53 +0000

    create college tour registration ejb

---
+++ src/main/com/topcoder/web/ejb/collegetour/RegistrationBean.java
@@ -0,0 +1,17 @@
+package com.topcoder.web.ejb.collegetour;
+
+import com.topcoder.web.ejb.BaseEJB;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Dec 5, 2005
+ */
+public class RegistrationBean extends BaseEJB {
+    public void create(long eventId, long coderId, String dataSource) {
+        insert("college_tour_registration", new String[] {"event_id", "coder_id"},
+                new String[] {String.valueOf(eventId), String.valueOf(coderId)}, dataSource);
+    }
+
+
+}
