b53ef8d671d213098ca69709bcc124619567441c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-12-05 23:30:38 +0000

    remove college tour constants

--- src/main/com/topcoder/web/common/WebConstants.java
+++ src/main/com/topcoder/web/common/WebConstants.java
@@ -7,5 +7,5 @@ package com.topcoder.web.common;
  */
 public interface WebConstants {
     public static final String HANDLE = "ha";
-    public static final String COLLEGE_TOUR_EVENT_ID = "cteid";
+    public static final String ROUND_ID = "rd";
 }
