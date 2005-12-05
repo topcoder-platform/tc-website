4787f646ebbe0a0bdeb62af2e1de6584cf163125
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-12-05 23:48:08 +0000

    add create local ejb method

--- web_module/trunk/src/main/com/topcoder/web/common/BaseProcessor.java
+++ web_module/trunk/src/main/com/topcoder/web/common/BaseProcessor.java
@@ -10,6 +10,7 @@ import com.topcoder.web.common.security.WebAuthentication;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.rmi.PortableRemoteObject;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -243,8 +244,27 @@ public abstract class BaseProcessor implements RequestProcessor {
      */
     public static Object createEJB(InitialContext ctx, Class remoteclass) throws NamingException, Exception {
         Object remotehome = ctx.lookup(remoteclass.getName() + "Home");
-        Method createmethod = remotehome.getClass().getMethod("create", null);
+        Method createmethod = PortableRemoteObject.narrow(remotehome,
+                remotehome.getClass()).getClass().getMethod("create", null);
         return createmethod.invoke(remotehome, null);
+    }
+
+    /**
+     * Get a local instance of the specified EJB.
+     * Assumes the home class will have the same name plus "Home".
+     *
+     * @param ctx the IntialContext to use on the lookup
+     * @param c The class of the interface which should be returned.
+     * @throws NamingException if we can't find the get context
+     * @throws Exception if something goes wrong when creating or calling
+     * the method on the ejb.
+     *
+     */
+    public static Object createLocalEJB(InitialContext ctx, Class c) throws NamingException, Exception {
+        Object home = ctx.lookup(c.getName() + "LocalHome");
+        Method createmethod = PortableRemoteObject.narrow(home,
+                home.getClass()).getClass().getMethod("create", null);
+        return createmethod.invoke(home, null);
     }
 
     public static void close(Context ctx) {
