b4927a6270a3bd0dd0371e2aa59b48e425cb77d0
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-12-06 21:26:58 +0000

    add

---
+++ src/main/com/topcoder/utilities/EmailSender.java
@@ -0,0 +1,98 @@
+package com.topcoder.utilities;
+
+import com.topcoder.shared.dataAccess.DataAccess;
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.util.EmailEngine;
+import com.topcoder.shared.util.TCSEmailMessage;
+import com.topcoder.shared.util.sql.InformixSimpleDataSource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Iterator;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Dec 6, 2005
+ */
+public class EmailSender {
+    private static final String GCCJ05_PROD = "jdbc:informix-sqli://192.168.14.51:2020/gcj_china_05_oltp:INFORMIXSERVER=informixol" +
+            "tp_tcp;user=coder;password=altec";
+    private static final String DEV_TRANS =
+            "jdbc:informix-sqli://65.112.118.221:1526/informixoltp:INFORMIXSERVER=devinformix10_shm;user=coder;password=altec";
+    private static final String GCCJ05_TRANS =
+            "jdbc:informix-sqli://63.144.135.202:1526/gcj_china_05_oltp:INFORMIXSERVER=tc_memeber_dev_tcp;user=coder;password=altec";
+
+    private static final String senderEmail = "gcjc@topcoder.com";
+    private static final String senderName = "Google Code Jam China";
+    public static void main(String[] args) {
+        if (args.length!=2) {
+            log("usage: " + EmailSender.class + " <filename> <command> <subject>");
+            System.exit(1);
+        } else {
+            try {
+                long start = System.currentTimeMillis();
+                EmailSender e = new EmailSender();
+                String content = e.getFile(args[0]);
+                ResultSetContainer recipients = e.getRecipients(args[1]);
+                String subject = args[2];
+                int successCount = 0;
+                int failCount = 0;
+                for (Iterator it = recipients.iterator(); it.hasNext();) {
+                    if (e.send((ResultSetContainer.ResultSetRow)it.next(), content, subject)) {
+                        successCount++;
+                    } else {
+                        failCount++;
+                    }
+                }
+                long end = System.currentTimeMillis();
+                log("sent " + successCount + " emails, " + failCount + " failed.  it took " + (float)(end-start)/(float)1000 + " seconds");
+                System.exit(0);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+
+    private boolean send(ResultSetContainer.ResultSetRow recipient, String text, String subject) {
+        try {
+            TCSEmailMessage message = new TCSEmailMessage();
+            message.setFromAddress(senderEmail, senderName);
+            message.setSubject(subject);
+            message.addToAddress(recipient.getStringItem("address"), TCSEmailMessage.TO);
+            StringBuffer buf = new StringBuffer(text.length()+50);
+            buf.append("Hello ").append(recipient.getStringItem("handle")).append(",\n\n");
+            buf.append(text);
+            message.setBody(buf.toString());
+            EmailEngine.send(message);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private String getFile(String fileName) throws IOException {
+        FileInputStream fis = new FileInputStream(fileName);
+        FileChannel chan = fis.getChannel();
+        ByteBuffer buf = ByteBuffer.allocate((int)chan.size());
+        chan.read(buf);
+        return buf.asCharBuffer().toString();
+
+    }
+
+    private ResultSetContainer getRecipients(String command) throws Exception {
+        Request dataRequest = new Request();
+        dataRequest.setContentHandle(command);
+        DataAccessInt dai = new DataAccess(new InformixSimpleDataSource(GCCJ05_TRANS));
+        return (ResultSetContainer)dai.getData(dataRequest).get(command);
+    }
+
+    private static void log(Object o) {
+        System.out.println(o.toString());
+    }
+}
