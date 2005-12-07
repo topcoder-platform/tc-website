104aabbb9a922365d72bc92d2c5e00af4c11607c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-12-07 22:17:05 +0000

    implement store rather than commit

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/ResumeRegSubmit.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/ResumeRegSubmit.java
@@ -15,92 +15,29 @@ import java.util.Map;
  */
 public abstract class ResumeRegSubmit extends FullRegSubmit {
 
+    protected long store(SimpleRegInfo regInfo) throws Exception {
+        long userId = super.store(regInfo);
+        ResumeRegInfo info = (ResumeRegInfo) regInfo;
+        if (info.hasResume()) {
+            int fileType = -1;
 
-    //todo wack this crap when we have fixed the regular site to not use the transactional db for contact info
-    //don't need to reimplement commit here at that point
-    protected long commit(SimpleRegInfo regInfo) throws TCWebException {
-
-        long userId = super.commit(regInfo);
-        try {
-
-            ResumeRegInfo info = (ResumeRegInfo) regInfo;
-            if (info.hasResume()) {
-                int fileType = -1;
-
-                if (info.getResume().length == 0)
-                    addError(Constants.FILE, getBundle().getProperty("error_empty_file"));
-                else {
-                    //fileType = Integer.parseInt(file.getParameter("fileType"));
-                    Map types = getFileTypes(transDb);
-                    if (types.containsKey(info.getResumeContentType())) {
-                        log.debug("FOUND TYPE");
-                        fileType = ((Long) types.get(info.getResumeContentType())).intValue();
-                    } else {
-                        log.debug("DID NOT FIND TYPE " + info.getResumeContentType());
-                    }
-                    ResumeServices resumeServices = (ResumeServices) createEJB(getInitialContext(), ResumeServices.class);
-                    resumeServices.putResume(userId, fileType, info.getResumeFileName(), info.getResume(), transDb);
+            if (info.getResume().length == 0)
+                addError(Constants.FILE, getBundle().getProperty("error_empty_file"));
+            else {
+                //fileType = Integer.parseInt(file.getParameter("fileType"));
+                Map types = getFileTypes(transDb);
+                if (types.containsKey(info.getResumeContentType())) {
+                    log.debug("FOUND TYPE");
+                    fileType = ((Long) types.get(info.getResumeContentType())).intValue();
+                } else {
+                    log.debug("DID NOT FIND TYPE " + info.getResumeContentType());
                 }
+                ResumeServices resumeServices = (ResumeServices) createEJB(getInitialContext(), ResumeServices.class);
+                resumeServices.putResume(userId, fileType, info.getResumeFileName(), info.getResume(), transDb);
             }
-
-
-/*
-            no need to update the topcoder information, this data is store completely separately
-            if (((Coder) createEJB(getInitialContext(), Coder.class)).exists(userId, DBMS.OLTP_DATASOURCE_NAME)) {
-                TransactionManager tm = null;
-                try {
-                    UserServicesHome userHome = (UserServicesHome) PortableRemoteObject.narrow(getInitialContext().lookup(
-                            UserServicesHome.class.getName()),
-                            UserServicesHome.class);
-
-                    UserServices userEJB = userHome.findByPrimaryKey(new Long(userId));
-                    com.topcoder.common.web.data.User u = userEJB.getUser();
-
-                    u.setPassword(regInfo.getPassword());
-                    CoderRegistration c = (CoderRegistration) u.getUserTypeDetails().get("Coder");
-                    c.setFirstName(regInfo.getFirstName());
-                    c.setLastName(regInfo.getLastName());
-                    c.setMiddleName(regInfo.getMiddleName());
-                    u.setEmail(regInfo.getEmail());
-                    c.setHomeAddress1(regInfo.getAddress1());
-                    c.setHomeAddress2(regInfo.getAddress2());
-                    c.setHomeCity(regInfo.getCity());
-                    State s = new State();
-                    s.setStateCode(regInfo.getStateCode());
-                    c.setHomeState(s);
-                    c.setHomeZip(regInfo.getZip());
-                    Country co = new Country();
-                    co.setCountryCode(regInfo.getCountryCode());
-                    c.setHomeCountry(co);
-                    c.setModified("U");
-
-                    u.getUserTypeDetails().put("Coder", c);
-
-                    u.setModified("U");
-
-                    tm = (TransactionManager) getInitialContext().lookup(ApplicationServer.TRANS_MANAGER);
-                    tm.begin();
-                    userEJB.setUser(u);
-                    tm.commit();
-                } catch (Exception e) {
-                    try {
-                        if (tm != null && tm.getStatus() == Status.STATUS_ACTIVE) {
-                            tm.rollback();
-                        }
-                    } catch (Exception te) {
-                        throw new TCWebException(e);
-                    }
-                    throw new TCWebException(e);
-                }
-            }
-*/
-        } catch (TCWebException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new TCWebException(e);
         }
-
         return userId;
+
     }
 
     protected abstract void handleActivation(SimpleRegInfo info, long userId) throws TCWebException;
