c2e12f7b7efc1ae6f4f8a8e3c941317b7d386105
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-12-07 17:27:26 +0000

    fix setting of datasource

--- web_module/trunk/src/main/com/topcoder/utilities/EmailSender.java
+++ web_module/trunk/src/main/com/topcoder/utilities/EmailSender.java
@@ -82,6 +82,7 @@ public class EmailSender {
     }
 
     public void setDataSource(String dataSource) {
+        log("set datasource " + dataSource);
         this.dataSource = dataSource;
     }
 
