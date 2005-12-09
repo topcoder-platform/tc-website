09e1d34c05ee06161e653e91c005b2e7e2dce22e
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-12-09 17:50:11 +0000

    use reg start and end

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/collegetour/Register.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/collegetour/Register.java
@@ -3,12 +3,14 @@ package com.topcoder.web.tc.controller.request.collegetour;
 import com.topcoder.shared.security.ClassResource;
 import com.topcoder.shared.util.ApplicationServer;
 import com.topcoder.shared.util.DBMS;
+import com.topcoder.web.common.NavigationException;
 import com.topcoder.web.common.PermissionException;
 import com.topcoder.web.ejb.collegetour.Registration;
 import com.topcoder.web.ejb.collegetour.RegistrationLocal;
 
 import javax.transaction.Status;
 import javax.transaction.TransactionManager;
+import java.sql.Timestamp;
 
 /**
  * @author dok
@@ -20,19 +22,23 @@ public class Register extends Base {
         if (userIdentified()) {
             TransactionManager tm = null;
             try {
-                tm = (TransactionManager) getInitialContext().lookup(ApplicationServer.TRANS_MANAGER);
-                tm.begin();
-                RegistrationLocal reg = (RegistrationLocal)createLocalEJB(getInitialContext(), Registration.class);
-                if (!reg.exists(getEventId(), getUser().getId(), DBMS.JTS_OLTP_DATASOURCE_NAME)) {
-                    reg.create(getEventId(), getUser().getId(), DBMS.JTS_OLTP_DATASOURCE_NAME);
+                Timestamp now = new Timestamp(System.currentTimeMillis());
+                if (now.before(getRegEnd()) && now.after(getRegStart())) {
+                    tm = (TransactionManager) getInitialContext().lookup(ApplicationServer.TRANS_MANAGER);
+                    tm.begin();
+                    RegistrationLocal reg = (RegistrationLocal)createLocalEJB(getInitialContext(), Registration.class);
+                    if (!reg.exists(getEventId(), getUser().getId(), DBMS.JTS_OLTP_DATASOURCE_NAME)) {
+                        reg.create(getEventId(), getUser().getId(), DBMS.JTS_OLTP_DATASOURCE_NAME);
+                    }
+                    tm.commit();
+                } else {
+                    throw new NavigationException("Sorry, registration is not currently open");
                 }
-                tm.commit();
             } catch (Exception e) {
                 if (tm != null && tm.getStatus() == Status.STATUS_ACTIVE) {
                     tm.rollback();
                 }
             }
-
             setNextPage("/success.jsp");
             setIsNextPageInContext(true);
         } else {
