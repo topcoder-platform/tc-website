bcf501116da31d2212a2e6531dc7ae1fc9d21531
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-12-27 22:43:28 +0000

    fix dates

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/util/TCO06ComponentTerms.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/util/TCO06ComponentTerms.java
@@ -13,8 +13,8 @@ public class TCO06ComponentTerms extends TCO06AlgorithmTerms {
 
     public Calendar getBeginning() {
         Calendar start = Calendar.getInstance();
-//        start.set(2006, Calendar.JANUARY, 2,9,0);
-        start.set(2005, Calendar.JANUARY, 2,9,0);
+        start.set(2006, Calendar.JANUARY, 2,9,0);
+//        start.set(2005, Calendar.JANUARY, 2,9,0);
         return start;
     }
 
