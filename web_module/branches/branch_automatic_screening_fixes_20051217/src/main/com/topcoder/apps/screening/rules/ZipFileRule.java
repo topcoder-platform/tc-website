564c4edbacb7d5c22ed2e750a6ef1120c07d6481
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-12-28 22:26:14 +0000

    *** empty log message ***

--- web_module/branches/branch_automatic_screening_fixes_20051217/src/main/com/topcoder/apps/screening/rules/ZipFileRule.java
+++ web_module/branches/branch_automatic_screening_fixes_20051217/src/main/com/topcoder/apps/screening/rules/ZipFileRule.java
@@ -71,15 +71,13 @@ public class ZipFileRule implements ScreeningRule {
 
             //root.mkdir();
 
-            String command = "/usr/bin/unzip " + file.getAbsolutePath() + " -d " + root.getAbsolutePath();
+            String command = "/usr/bin/unzip -poqq " + file.getAbsolutePath() + " -d " + root.getAbsolutePath();
             log.info("command: " + command); // plk
             ExecutionResult er = Exec.execute(new String[] {command});
             log.info("ok"); // plk
 
 /*            ArchiveUtility au = new ArchiveUtility(file, new ZipArchiver());
             au.extractContents(root);*/
-
-/*/usr/bin/unzip /home/tcsdev1/submissions/Submitter_10608475_2005-12-28-17-09-02-104.zip*/
 
             return true;
         } catch (DatabaseException dbe) {
