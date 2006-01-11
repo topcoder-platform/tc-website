d1d6f928c3c7723d72a37dc8e0c62f5fc7d7e9c5
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-01-11 20:22:43 +0000

    add

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/development/Register.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/development/Register.java
@@ -1,7 +1,33 @@
 package com.topcoder.web.tc.controller.request.development;
 
+import com.topcoder.dde.catalog.ComponentManager;
+import com.topcoder.dde.catalog.ComponentManagerHome;
+import com.topcoder.dde.catalog.ComponentVersionInfo;
+import com.topcoder.dde.catalog.Forum;
+import com.topcoder.dde.user.UserManagerRemote;
+import com.topcoder.dde.user.UserManagerRemoteHome;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.util.*;
+import com.topcoder.util.format.ObjectFormatter;
+import com.topcoder.util.format.ObjectFormatterFactory;
 import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.common.tag.CalendarDateFormatMethod;
+import com.topcoder.web.ejb.email.Email;
+import com.topcoder.web.ejb.project.Project;
+import com.topcoder.web.ejb.project.ProjectLocal;
 import com.topcoder.web.tc.Constants;
+
+import javax.ejb.CreateException;
+import javax.naming.InitialContext;
+import javax.rmi.PortableRemoteObject;
+import javax.transaction.TransactionManager;
+import javax.transaction.Status;
+import java.rmi.RemoteException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.TimeZone;
 
 /**
  * @author dok
@@ -15,9 +41,9 @@ public class Register extends ViewRegistration {
             //check if user can do the project (there's like 10 things to check.
             //if they can't, give them error message
             //if they can, check if they are registered for the tco
-              //if they are not registered,
-                //if they don't have final confirm flag
-                  //add a flag to the request and send them to a confirm page
+            //if they are not registered,
+            //if they don't have final confirm flag
+            //add a flag to the request and send them to a confirm page
             //register and go to success page
             validation();
 
@@ -26,10 +52,11 @@ public class Register extends ViewRegistration {
                 boolean isEligible = getRequest().getAttribute(Constants.MESSAGE) == null;
                 if (isEligible) {
                     if (isTournamentTime()) {
-                        boolean isRegisteredForTournament = getRequest().getAttribute("notRegistered")==null;
-                        boolean isConfirmed = getRequest().getParameter("confirm")!=null;
-                        if (isRegisteredForTournament || (!isRegisteredForTournament && isConfirmed)) {
+                        boolean isRegisteredForTournament = getRequest().getAttribute("notRegistered") == null;
+                        boolean isConfirmed = getRequest().getParameter("confirm") != null;
+                        if (isRegisteredForTournament || isConfirmed) {
                             register();
+                            //send email
                             setNextPage("/dev/regSuccess.jsp");
                             setIsNextPageInContext(true);
                         } else {
@@ -58,7 +85,88 @@ public class Register extends ViewRegistration {
         }
     }
 
-    private void register() {
+    private void register() throws Exception, RemoteException, CreateException {
+        InitialContext ctx = null;
+        try {
+            ctx = TCContext.getContext(ApplicationServer.SECURITY_CONTEXT_FACTORY, ApplicationServer.TCS_APP_SERVER_URL);
+            log.debug("creating user");
+            Object objUserManager = ctx.lookup(UserManagerRemoteHome.EJB_REF_NAME);
+            UserManagerRemoteHome userManagerHome = (UserManagerRemoteHome) PortableRemoteObject.narrow(objUserManager, UserManagerRemoteHome.class);
+            UserManagerRemote userManager = userManagerHome.create();
+
+            //we can assume it's valid, we've done the validation already
+            long projectId = Long.parseLong(getRequest().getParameter(Constants.PROJECT_ID));
+
+            ProjectLocal pl = (ProjectLocal) createLocalEJB(getInitialContext(), Project.class);
+            long componentId = pl.getComponentId(projectId, DBMS.TCS_OLTP_DATASOURCE_NAME);
+            int phase = 111 + pl.getProjectTypeId(projectId, DBMS.TCS_OLTP_DATASOURCE_NAME);
+
+            Object objComponentManager = ctx.lookup(ComponentManagerHome.EJB_REF_NAME);
+            ComponentManagerHome componentManagerHome =
+                    (ComponentManagerHome) PortableRemoteObject.narrow(objComponentManager, ComponentManagerHome.class);
+            ComponentManager componentManager = componentManagerHome.create(componentId);
+            String project = componentManager.getComponentInfo().getName() +
+                    (phase==ComponentVersionInfo.SPECIFICATION?"Design":"Development");
+            long activeForumId = componentManager.getActiveForum(Forum.SPECIFICATION).getId();
+
+
+            TransactionManager tm = (TransactionManager) getInitialContext().lookup(ApplicationServer.TRANS_MANAGER);
+
+            try {
+                tm.begin();
+                userManager.registerForProject(getUser().getId(), getRequest().getParameter("Comment"), projectId);
+                tm.commit();
+            } catch (Exception e) {
+                if (tm != null && tm.getStatus() == Status.STATUS_ACTIVE) {
+                    tm.rollback();
+                }
+                throw e;
+            }
+
+            TCSEmailMessage mail = new TCSEmailMessage();
+            Email e = (Email)createEJB(getInitialContext(), Email.class);
+            mail.addToAddress(e.getAddress(e.getPrimaryEmailId(getUser().getId(), DBMS.OLTP_DATASOURCE_NAME),
+                    DBMS.OLTP_DATASOURCE_NAME), TCSEmailMessage.TO);
+            mail.setFromAddress("service@topcodersoftware.com");
+            mail.setSubject(project);
+
+            Request r = new Request();
+            r.setContentHandle("project_detail");
+            r.setProperty(Constants.PROJECT_ID, String.valueOf(projectId));
+            Map resultMap = getDataAccess().getData(r);
+            ResultSetContainer details = (ResultSetContainer) resultMap.get("project_detail");
+            Timestamp submitDeadline = (Timestamp)details.getItem(0, "initial_submission_date").getResultData();
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(submitDeadline);
+            cal.setTimeZone(TimeZone.getDefault());
+
+            ObjectFormatter formatter = ObjectFormatterFactory.getEmptyFormatter();
+            formatter.setFormatMethodForClass(Calendar.class,
+                                new CalendarDateFormatMethod("MM.dd.yyyy HH:mm a z"), true);
+            String date = formatter.format(cal);
+
+            if (phase == ComponentVersionInfo.SPECIFICATION) {
+                mail.setBody("Registration Complete. \n\n" +
+                        "Thank you, " + getUser().getUserName() + ", for your interest in the " + project + " component. You now have access to the Developer Forum ( http://" + ApplicationServer.SOFTWARE_SERVER_NAME + "/forum/c_forum.jsp?f=" + activeForumId + " ) which can be used to obtain design documentation (see the Design Phase Documents thread), as well as to ask questions regarding the component design. Please post your questions at any time and a product manager will respond within 24 hours. Any questions asked within 6 hours of the submission due date/time may not be answered in time, so get your questions in early!\n\n" +
+                        "The deadline for submitting a solution is " + date + ". Please upload your design using the project page found here: http://" + ApplicationServer.SOFTWARE_SERVER_NAME + "/review.  If you encounter any problems, please contact us at service@topcodersoftware.com.  All late submissions will be ignored.\n\n" +
+                        "If you have any questions please contact service@topcodersoftware.com\n\n" +
+                        "TopCoder Software Team");
+
+            } else {
+
+                mail.setBody("Registration Complete\n\n" +
+                        "Thank you, " + getUser().getUserName() + ", for your interest in the " + project + " component. You now have access to the Developer Forum ( http://" + ApplicationServer.SOFTWARE_SERVER_NAME + "/forum/c_forum.jsp?f=" + activeForumId + " ) which can be used to obtain the component design (See \"Development Phase Documents\" thread), as well as to ask questions regarding the development process or the component design. Please post your questions at any time and the component designer will respond within 24 hours. Any questions asked within 6 hours of the submission due date/time may not be answered, so get your questions in early!\n\n" +
+                        "The deadline for submitting a solution is " + date + ". Please upload your solution using the project page found here: http://" + ApplicationServer.SOFTWARE_SERVER_NAME + "/review. If you encounter any problems, please contact us at service@topcodersoftware.com.  Any late submissions will be ignored. \n\n" +
+                        "If you have any questions please contact service@topcodersoftware.com\n\n" +
+                        "TopCoder Software Team");
+            }
+            EmailEngine.send(mail);
+
+
+        } finally {
+            close(ctx);
+        }
+
 
     }
 }
