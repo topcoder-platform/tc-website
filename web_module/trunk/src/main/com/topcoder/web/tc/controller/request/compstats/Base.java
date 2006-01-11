1f13f7fa42ecbedf81a3c89d497535534f22fb2e
budka <budka@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-01-11 19:34:28 +0000

    added utility method to lookup database info

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/compstats/Base.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/compstats/Base.java
@@ -3,11 +3,13 @@ package com.topcoder.web.tc.controller.request.compstats;
 import com.topcoder.shared.dataAccess.CachedDataAccess;
 import com.topcoder.shared.dataAccess.DataAccess;
 import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 import com.topcoder.shared.util.DBMS;
 import com.topcoder.web.common.BaseProcessor;
 
 import com.topcoder.shared.util.logging.Logger;
-
+import java.util.*;
 
 abstract public class Base extends BaseProcessor {
 
@@ -31,5 +33,21 @@ protected static final Logger log = Logger.getLogger(Base.class);
             dAccess = new DataAccess(datasource);
         return dAccess;
     }
+    
+    // returns a rsc from the database using the given command.  It is assumed that
+    // the command contains a query with the same name.
+    protected ResultSetContainer queryDB(String commandName) throws Exception {
+    	return queryDB(commandName, commandName);
+    }
+    
+    // returns a rsc from the database using the given command and given query name.
+	protected ResultSetContainer queryDB(String commandName, String queryName) throws Exception {
+		Request r = new Request();
+		r.setContentHandle(commandName);
+		DataAccessInt dai = getDataAccess(true);
+		Map result = dai.getData(r);
+		return (ResultSetContainer) result.get(queryName);
+	}
+	
 
 }
