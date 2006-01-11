548693cd650db6c1f2d1d0d18bccce393ebd2568
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-01-11 17:32:34 +0000

    add get component id method

--- web_module/trunk/src/main/com/topcoder/web/ejb/project/ProjectBean.java
+++ web_module/trunk/src/main/com/topcoder/web/ejb/project/ProjectBean.java
@@ -1,14 +1,15 @@
 package com.topcoder.web.ejb.project;
 
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.shared.util.logging.Logger;
 import com.topcoder.web.ejb.BaseEJB;
 import com.topcoder.web.ejb.roomresult.RoomResultBean;
-import com.topcoder.shared.util.logging.Logger;
-import com.topcoder.shared.util.DBMS;
 
-import javax.naming.InitialContext;
 import javax.ejb.EJBException;
+import javax.naming.InitialContext;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -41,11 +42,51 @@ public class ProjectBean extends BaseEJB {
         }
 
     }
-    
+
     public int getProjectTypeId(long projectId, String dataSource) {
         log.debug("get project id called for project " + projectId);
         return selectInt("project", "project_type_id", new String[]{"project_id", "cur_version"},
                 new String[]{String.valueOf(projectId), "1"}, dataSource).intValue();
+    }
+
+    private static final String componentInfo =
+            "select cv.component_id " +
+            "  from project p " +
+              "   , comp_versions cv " +
+           "  where p.project_id = ? " +
+             "  and p.cur_version = 1 " +
+             "  and p.comp_vers_id = cv.comp_vers_id";
+
+    public long getComponentId(long projectId, String dataSource)  {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = DBMS.getConnection(dataSource);
+
+            ps = conn.prepareStatement(componentInfo);
+            ps.setLong(1, projectId);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getLong("component_id");
+            } else {
+                throw new EJBException("Invalid project id specified: " + projectId);
+            }
+
+
+        } catch (SQLException e) {
+            DBMS.printSqlException(true, e);
+            throw new EJBException(e.getMessage());
+        } catch (Exception e) {
+            throw new EJBException(e.getMessage());
+        } finally {
+            close(rs);
+            close(ps);
+            close(conn);
+        }
+
+
     }
 
 }
