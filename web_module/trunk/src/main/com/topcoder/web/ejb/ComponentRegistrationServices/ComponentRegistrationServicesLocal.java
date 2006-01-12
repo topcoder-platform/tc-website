c9f04b6c7b49fb2a8281df39cd5e4bb80207bd59
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-01-12 16:57:18 +0000

    create local version

--- web_module/trunk/src/main/com/topcoder/web/ejb/ComponentRegistrationServices/ComponentRegistrationServicesLocal.java
+++ web_module/trunk/src/main/com/topcoder/web/ejb/ComponentRegistrationServices/ComponentRegistrationServicesLocal.java
@@ -1,9 +1,47 @@
 package com.topcoder.web.ejb.ComponentRegistrationServices;
 
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+
+import javax.ejb.EJBLocalObject;
+import javax.ejb.EJBException;
+import java.rmi.RemoteException;
+
 /**
  * @author dok
  * @version $Revision$ Date: 2005/01/01 00:00:00
  *          Create Date: Jan 12, 2006
  */
-public interface ComponentRegistrationServicesLocal {
+public interface ComponentRegistrationServicesLocal extends EJBLocalObject {
+
+    boolean isUserRegistered(long projectId, long userId, String dataSource);
+
+    boolean isRegClosed(long projectId, String dataSource);
+
+    boolean hasUserReviewedProject(long projectId, long userId, String dataSource);
+
+    boolean isUserWinningDesigner(long projectId, long userId, String dataSource);
+
+    boolean isUserReliableEnough(long phaseId, long userId, String dataSource);
+
+    int getMaxUnratedRegistrants(long projectId, String dataSource);
+
+    int getMaxRatedRegistrants(long projectId, String dataSource);
+
+    int getUnratedRegistrantCount(long projectId, String dataSource);
+
+    int getRatedRegistrantCount(long projectId, String dataSource);
+
+    // Survey methods
+    ResultSetContainer getActiveQuestions();
+
+    ResultSetContainer getActiveAnswers();
+
+    ResultSetContainer getAnswers(long questionId);
+
+    void createResponse(long projectId, long userId, long questionId, long answerId);
+
+    void createResponse(long projectId, long userId, long questionId, String text);
+
+    boolean responseExists(long projectId, long userId, long questionId);
+}
 }
--- web_module/trunk/src/main/com/topcoder/web/ejb/ComponentRegistrationServices/ComponentRegistrationServicesLocalHome.java
+++ web_module/trunk/src/main/com/topcoder/web/ejb/ComponentRegistrationServices/ComponentRegistrationServicesLocalHome.java
@@ -1,9 +1,18 @@
 package com.topcoder.web.ejb.ComponentRegistrationServices;
 
+import com.topcoder.web.ejb.termsofuse.TermsOfUseLocal;
+
+import javax.ejb.EJBLocalHome;
+import javax.ejb.CreateException;
+
 /**
  * @author dok
  * @version $Revision$ Date: 2005/01/01 00:00:00
  *          Create Date: Jan 12, 2006
  */
-public interface ComponentRegistrationServicesLocalHome {
+public interface ComponentRegistrationServicesLocalHome extends EJBLocalHome {
+
+    public ComponentRegistrationServicesLocal create() throws CreateException;
+}
+{
 }
