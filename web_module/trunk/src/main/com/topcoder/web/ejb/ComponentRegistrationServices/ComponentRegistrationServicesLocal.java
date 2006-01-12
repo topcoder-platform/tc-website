c1f2098e0ca87827b2548a4242ba7a4e04670fcc
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-01-12 16:55:50 +0000

    create local version

---
+++ web_module/trunk/src/main/com/topcoder/web/ejb/ComponentRegistrationServices/ComponentRegistrationServicesLocal.java
@@ -0,0 +1,9 @@
+package com.topcoder.web.ejb.ComponentRegistrationServices;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Jan 12, 2006
+ */
+public interface ComponentRegistrationServicesLocal {
+}
