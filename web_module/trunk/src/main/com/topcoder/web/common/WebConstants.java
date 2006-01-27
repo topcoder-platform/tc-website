fae925d129bd645bf3077c22f9e1cacda9c5940e
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-01-27 19:23:27 +0000

    add some placement stuff

--- web_module/trunk/src/main/com/topcoder/web/common/WebConstants.java
+++ web_module/trunk/src/main/com/topcoder/web/common/WebConstants.java
@@ -1,9 +1,9 @@
 package com.topcoder.web.common;
 
 /**
- * @author  dok
- * @version  $Revision$ $Date$
- * Create Date: May 2, 2005
+ * @author dok
+ * @version $Revision$ $Date$
+ *          Create Date: May 2, 2005
  */
 public interface WebConstants {
     public static final String HANDLE = "ha";
@@ -11,5 +11,31 @@ public interface WebConstants {
     public static final String FORUM_ID = "forumID";
     public static final String STATIC_PREFIX = "d";
     public static final String TIMEZONE = "tz";
+    public static final String USER_ID = "uid";
+
+
+    public static final int PREFERENCE_TEXT_ANSWER = 3;
+    public static final int PREFERENCE_MULTIPLE_ANSWER = 2;
+    public static final int PREFERENCE_SINGLE_ANSWER = 1;
+
+    public static final int SKILL_TYPE_TECHNOLOGIES = 4;
+    public static final int SKILL_TYPE_DATABASES = 3;
+    public static final int SKILL_TYPE_LANGUAGES = 2;
+    public static final int SKILL_TYPE_OS = 1;
+    public static final int SKILL_TYPE_INDUSTRIES = 5;
+
+    public static final int TECH_NOTE_TYPE_ID = 11;
+    public static final int DB_NOTE_TYPE_ID = 13;
+    public static final int LANGUAGE_NOTE_TYPE_ID = 10;
+    public static final int OS_NOTE_TYPE_ID = 12;
+    public static final int INDUSTRY_NOTE_TYPE_ID = 14;
+
+    public static final int PREFERENCE_CONTRACTING = 2;
+    public static final int PREFERENCE_CONTRACTING_TRUE = 32;
+    public static final int PREFERENCE_CONTRACTING_FALSE = 33;
+    public static final int PREFERENCE_PERMANENT = 7;
+    public static final int PREFERENCE_PERMANENT_TRUE = 34;
+    public static final int PREFERENCE_PERMANENT_FALSE = 35;
+
 
 }
