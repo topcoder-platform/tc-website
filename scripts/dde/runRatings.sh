42d9e08a20103bccd60103444103ce2750345b5e
apps <apps@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-01-30 17:10:49 +0000

    *** empty log message ***

--- scripts/dde/runRatings.sh
+++ scripts/dde/runRatings.sh
@@ -1,2 +1,15 @@
-java -cp "../../resources:../../lib/jars/log4j.jar:../../lib/jars/ifxjdbc.jar:../../lib/jars/jdbc7.0-1.2.jar:../../build/classes:../../lib/jars/tcsUtil.jar:../../lib/jars/configmanager2.jar:../../conf:$CLASSPATH" com.topcoder.dde.util.DWLoad.RatingQubits 
+BASE=../..
+CP=""
+CP=$CP:$BASE/conf
+CP=$CP:$BASE/resources
+CP=$CP:$BASE/lib/jars/log4j.jar
+CP=$CP:$BASE/lib/jars/ifxjdbc.jar
+CP=$CP:$BASE/lib/jars/jdbc7.0-1.2.jar
+CP=$CP:$BASE/lib/jars/xml/xerces/xercesImpl.jar
+CP=$CP:$BASE/build/classes
+CP=$CP:$BASE/lib/jars/tcsUtil.jar
+CP=$CP:$BASE/lib/jars/configmanager2.jar
+CP=$CP:$BASE/lib/jars/jboss/xercesImpl.jar
+CP=$CP:$BASE/lib/jars/jboss/xml-apis.jar
 
+nohup java -cp $CP com.topcoder.dde.util.DWLoad.RatingQubits >> ./ratings.log 2>&1 &
