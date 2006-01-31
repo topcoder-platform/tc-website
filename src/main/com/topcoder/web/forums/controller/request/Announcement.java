51bf3f5d2d858bb5a7e4c03b3f9a56e1b18a7ad1
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-01-31 22:35:27 +0000

    no message

--- src/main/com/topcoder/web/forums/controller/request/Announcement.java
+++ src/main/com/topcoder/web/forums/controller/request/Announcement.java
@@ -3,11 +3,15 @@
  */
 package com.topcoder.web.forums.controller.request;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import com.topcoder.web.common.StringUtils;
 import com.topcoder.web.forums.ForumConstants;
 import com.jivesoftware.base.JiveConstants;
+import com.jivesoftware.forum.AnnouncementManager;
 import com.jivesoftware.forum.ForumCategory;
 import com.jivesoftware.forum.Forum;
-
 
 
 /**
@@ -19,9 +23,11 @@ public class Announcement extends ForumsProcessor {
 	protected void businessProcessing() throws Exception {
 		super.businessProcessing();
 
+        AnnouncementManager announcementManager = forumFactory.getAnnouncementManager();
+        
         long announcementID = Long.parseLong(getRequest().getParameter(ForumConstants.ANNOUNCEMENT_ID));
         com.jivesoftware.forum.Announcement announcement = 
-            forumFactory.getAnnouncementManager().getAnnouncement(announcementID);
+            announcementManager.getAnnouncement(announcementID);
         ForumCategory forumCategory = null;
         Forum forum = null;
         if (announcement.getContainerObjectType() == JiveConstants.FORUM) {
@@ -31,6 +37,25 @@ public class Announcement extends ForumsProcessor {
             forumCategory = forumFactory.getForumCategory(announcement.getContainerObjectID());
         } else if (announcement.getContainerObjectType() == JiveConstants.SYSTEM) {
             forumCategory = forumFactory.getRootForumCategory();
+        }
+        
+        String annCmd = StringUtils.checkNull(getRequest().getParameter(ForumConstants.ANNOUNCEMENT_COMMAND));
+        if (annCmd.equals("Expire")) {
+            Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("EST"));
+            announcement.setEndDate(cal.getTime());
+        } else if (annCmd.equals("Activate")) {
+            announcement.setEndDate(null);
+        } else if (annCmd.equals("Delete")) {
+            announcementManager.deleteAnnouncement(announcement);
+            if (forum != null) {
+                setNextPage("?module=ThreadList&forumID=" + forum.getID() + "&mc=" + forum.getMessageCount());
+            } else if (forumCategory != null) {
+                setNextPage("?module=Category&categoryID=" + forumCategory.getID());
+            } else {
+                setNextPage("?module=ForumList");
+            }            
+            setIsNextPageInContext(false);
+            return;
         }
         
         getRequest().setAttribute("forumFactory", forumFactory);
