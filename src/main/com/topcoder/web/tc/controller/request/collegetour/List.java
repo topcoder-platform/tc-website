03c5cc2f02e3b1c97b9b68c78479ecf20fbe7d5a
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-02-07 19:06:26 +0000

    add

---
+++ src/main/com/topcoder/web/tc/controller/request/collegetour/List.java
@@ -0,0 +1,26 @@
+package com.topcoder.web.tc.controller.request.collegetour;
+
+import com.topcoder.web.common.BaseProcessor;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.CachedDataAccess;
+import com.topcoder.shared.util.DBMS;
+
+import java.util.Map;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Feb 7, 2006
+ */
+public class List extends BaseProcessor {
+
+    protected void businessProcessing() throws Exception {
+        Request r = new Request();
+        r.setContentHandle("college_tour_school_list");
+        Map m = new CachedDataAccess(1000*60*60, DBMS.OLTP_DATASOURCE_NAME).getData(r);
+        getRequest().setAttribute("list", m.get("college_tour_school_list"));
+        setNextPage("/collegetour/list.jsp");
+        setIsNextPageInContext(true);
+    }
+
+}
