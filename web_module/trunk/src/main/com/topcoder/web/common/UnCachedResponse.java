e29fbd017fd45e7ad6961a2c2e738529fc4b47f7
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-02-13 22:20:16 +0000

    remove unused instance of principal manager

--- web_module/trunk/src/main/com/topcoder/web/common/UnCachedResponse.java
+++ web_module/trunk/src/main/com/topcoder/web/common/UnCachedResponse.java
@@ -8,11 +8,44 @@ import javax.servlet.http.HttpServletResponse;
  *          Create Date: Feb 13, 2006
  */
 public class UnCachedResponse extends SimpleResponse {
+
+    /**
+     * build the response object and set headers such that
+     * browsers/proxies will not cache this response. 
+     *
+     * @param response
+     */
     public UnCachedResponse(HttpServletResponse response) {
         super(response);
-        this.response.setHeader("Cache-Control","no-store");
-        this.response.setHeader("Pragma","no-cache");
-        this.response.setDateHeader ("Expires", 0);
+        init();
+    }
+
+    /**
+     * accept a flag indicating whether or not to overwrite the headers we're setting to
+     * make proxies/browsers not cache the response.  this is an all or nothing operation.
+     * if any of the headers that we're concerned with are set, then we' don't overwrite any.
+     *
+     * @param response
+     * @param overwrite
+     */
+    public UnCachedResponse(HttpServletResponse response, boolean overwrite) {
+        super(response);
+        if (overwrite) {
+            init();
+        } else {
+            if (!this.response.containsHeader("Cache-Control") &&
+                    !this.response.containsHeader("Pragma") &&
+                    !this.response.containsHeader("Expires")) {
+                init();
+            }
+        }
+
+    }
+
+    private void init() {
+        this.response.setHeader("Cache-Control", "no-store");
+        this.response.setHeader("Pragma", "no-cache");
+        this.response.setDateHeader("Expires", 0);
     }
 
 }
