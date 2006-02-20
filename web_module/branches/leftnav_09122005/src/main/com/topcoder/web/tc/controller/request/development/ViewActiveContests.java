0d14c83ae4c179683c3f9bd217918abee74cfeb8
budka <budka@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-02-20 20:00:12 +0000

    Pulls phase id from url

--- web_module/branches/leftnav_09122005/src/main/com/topcoder/web/tc/controller/request/development/ViewActiveContests.java
+++ web_module/branches/leftnav_09122005/src/main/com/topcoder/web/tc/controller/request/development/ViewActiveContests.java
@@ -2,6 +2,8 @@ package com.topcoder.web.tc.controller.request.development;
 
 import com.topcoder.shared.dataAccess.Request;
 import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.tc.model.SoftwareComponent;
 
 /**
  * @author dok
@@ -9,9 +11,15 @@ import com.topcoder.web.common.TCWebException;
  *          Create Date: Jan 4, 2006
  */
 public class ViewActiveContests extends Base {
-
+    private final String PHASE_ID_KEY = "pi";   
     protected void developmentProcessing() throws TCWebException {
         try {
+            int pi = Integer.parseInt(StringUtils.checkNull(getRequest().getParameter(PHASE_ID_KEY)));
+            if (!(pi == SoftwareComponent.DESIGN_PHASE || pi == SoftwareComponent.DEV_PHASE)) {
+                throw new TCWebException("Missing or invalid phase_id parameter (pi expected)");
+            }
+            getRequest().setAttribute("phase_id_is_design", new Boolean(ph == SoftwareComponent.DESIGN_PHASE));
+            
             Request r = new Request();
             r.setContentHandle("active_contests");
             getRequest().setAttribute("resultMap", getDataAccess().getData(r));
