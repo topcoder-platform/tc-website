424e6158d2d61ef463c6822d213d0d956d975542
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-02-20 22:47:40 +0000

    fix queries to work on long_comp_result not room_result

--- web_module/trunk/src/main/com/topcoder/web/ejb/longcompresult/LongCompResultBean.java
+++ web_module/trunk/src/main/com/topcoder/web/ejb/longcompresult/LongCompResultBean.java
@@ -20,33 +20,33 @@ public class LongCompResultBean extends BaseEJB {
     private static Logger log = Logger.getLogger(LongCompResultBean.class);
 
     public void createLongCompResult(long roundId, long coderId, String dataSource) {
-        log.debug("createRoomResult called... " + roundId + " " + coderId);
-        insert("room_result", new String[] {"round_id", "coder_id"},
+        log.debug("createLongCompResult called... " + roundId + " " + coderId);
+        insert("long_comp_result", new String[] {"round_id", "coder_id"},
                 new String[] {String.valueOf(roundId), String.valueOf(coderId)}, dataSource);
     }
 
     public void setPointTotal(long roundId, long coderId, double pointTotal, String dataSource)  {
         log.debug("setPointTotal called... " + roundId + " " + coderId + " " + pointTotal);
-        update("room_result", new String[] {"point_total"}, new String[] {String.valueOf(pointTotal)},
+        update("long_comp_result", new String[] {"point_total"}, new String[] {String.valueOf(pointTotal)},
                 new String[] {"round_id", "coder_id"},
                 new String[] {String.valueOf(roundId), String.valueOf(coderId)}, dataSource);
     }
     public void setAttended(long roundId, long coderId, boolean attended, String dataSource)  {
         log.debug("setAttended called... " + roundId + " " + coderId + " " + attended);
-        update("room_result", new String[] {"attended"}, new String[] {attended?"Y":"N"},
+        update("long_comp_result", new String[] {"attended"}, new String[] {attended?"Y":"N"},
                 new String[] {"round_id", "coder_id"},
                 new String[] {String.valueOf(roundId), String.valueOf(coderId)}, dataSource);
     }
 
     public double getPointTotal(long roundId, long coderId, String dataSource)  {
         log.debug("getPointTotal called... " + roundId + " " + coderId);
-        return selectDouble("room_result", "point_total", new String[] {"round_id", "coder_id"},
+        return selectDouble("long_comp_result", "point_total", new String[] {"round_id", "coder_id"},
             new String[] {String.valueOf(roundId),String.valueOf(coderId)}, dataSource).doubleValue();
     }
 
     public boolean getAttended(long roundId, long coderId, String dataSource)  {
         log.debug("getAttended called... " + roundId + " " + coderId);
-        return "Y".equals(selectString("room_result", "attended", new String[] {"round_id", "coder_id"},
+        return "Y".equals(selectString("long_comp_result", "attended", new String[] {"round_id", "coder_id"},
             new String[] {String.valueOf(roundId), String.valueOf(coderId)}, dataSource));
 
     }
@@ -61,7 +61,7 @@ public class LongCompResultBean extends BaseEJB {
             try {
                 conn = DBMS.getConnection(DBMS.JTS_OLTP_DATASOURCE_NAME);
 
-                ps = conn.prepareStatement("SELECT '1' FROM room_result WHERE coder_id = ? AND round_id = ?");
+                ps = conn.prepareStatement("SELECT '1' FROM long_comp_result WHERE coder_id = ? AND round_id = ?");
                 ps.setLong(1, coderId);
                 ps.setLong(2, roundId);
 
