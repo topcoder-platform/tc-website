8f25c684e3fc78d0f3842640d8c08af62de5fb7f
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-02-23 22:35:08 +0000

    add text before code

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/googleindia06/Submit.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/googleindia06/Submit.java
@@ -83,6 +83,7 @@ public class Submit extends ResumeRegSubmit {
 
                 buf.append("GOOGLE CODE JAM INDIA ACTIVATION INFORMATION\n\n");
 
+                buf.append("Your Google activation code is ");
                 buf.append(code);
                 buf.append("\n\n");
                 buf.append("To activate your account, navigate to the following WWW URL:\n\n");
