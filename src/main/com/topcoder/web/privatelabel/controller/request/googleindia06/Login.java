e140b7dfb5546db802457c7e76fd8df8279d486b
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-02-23 20:45:03 +0000

    handle the case where theyr'e logging in with an inactive account

--- src/main/com/topcoder/web/privatelabel/controller/request/googleindia06/Login.java
+++ src/main/com/topcoder/web/privatelabel/controller/request/googleindia06/Login.java
@@ -28,7 +28,7 @@ public class Login extends FullLogin {
         now.setTime(new Date());
 
         //log.debug("new: " + regInfo.isNew() + " now " + now + " beginning " + beginning + " end " + end);
-        if (regInfo.isNew() && (now.before(beginning) || now.after(end))) {
+        if (regInfo!=null && regInfo.isNew() && (now.before(beginning) || now.after(end))) {
             setNextPage(Constants.GOOGLE_INDIA_06_REG_CLOSED_PAGE);
             setIsNextPageInContext(true);
         } else {
