fae614880742604f9c2bbe14e33bb8ebd2173685
budka <budka@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-02-23 15:19:57 +0000

    no message

---
+++ web_module/branches/leftnav_09122005/src/main/com/topcoder/web/tc/controller/request/calendar/MatchDetails.java
@@ -0,0 +1,114 @@
+/*
+ * MatchDetails.java
+ *
+ * Created on February 22, 2006, 12:20 PM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package com.topcoder.web.tc.controller.request.calendar;
+
+import com.topcoder.web.tc.controller.request.Base;
+import com.topcoder.common.web.data.Navigation;
+import com.topcoder.common.web.util.Cache;
+//import com.topcoder.common.web.util.Conversion;
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.common.web.util.Data;
+import com.topcoder.common.web.xml.HTMLRenderer;
+import com.topcoder.shared.dataAccess.CachedDataAccess;
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.docGen.xml.RecordTag;
+import com.topcoder.shared.docGen.xml.ValueTag;
+import com.topcoder.shared.docGen.xml.XMLDocument;
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.shared.util.TCContext;
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.common.TCWebException;
+
+import javax.naming.Context;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Map;
+import com.topcoder.shared.dataAccess.DataAccessConstants;
+/**
+ *
+ * @author cstjohn
+ */
+public class MatchDetails extends Base {
+    private static Logger log = Logger.getLogger(MatchDetails.class);
+
+    /*    static String process(HttpServletRequest request,
+                          HTMLRenderer HTMLmaker, Navigation nav, XMLDocument document)
+            throws NavigationException {  */
+    
+    protected void businessProcessing() throws TCWebException {
+        String result = null;
+        Request dataRequest = null;
+        DataAccessInt dai = null;
+        ResultSetContainer rsc = null;
+        Map resultMap = null;
+        Context ctx = null;
+
+        try {
+
+            //RecordTag schedTag = new RecordTag("SCHEDULE");
+            //DataCache dcHome = Cache.get();
+
+            String sortDir = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.SORT_DIRECTION));
+            String sortCol = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.SORT_COLUMN));
+            String roundId = StringUtils.checkNull(getRequest().getParameter("RoundId"));
+            getRequest().setAttribute("round_id", roundId);
+            
+            //schedTag.addTag(new ValueTag("RoundId", roundId));
+
+            //String command = Conversion.checkNull(request.getParameter("c"));
+            //if (command.equals("srm") || command.equals("srm_rules")) {
+            
+            ctx = TCContext.getInitial();
+            dai = new CachedDataAccess(DBMS.OLTP_DATASOURCE_NAME);
+            dataRequest = new Request();
+            dataRequest.setContentHandle("schedule_srms");
+            dataRequest.setProperty("rd", roundId.trim());
+            resultMap = dai.getData(dataRequest);
+            ResultSetContainer.ResultSetRow rsr = (ResultSetContainer.ResultSetRow)((ResultSetContainer) resultMap.get("Schedule_SRMS")).get(0);
+            //schedTag.addTag(rsc.getTag("Round", "Details"));
+
+            getRequest().setAttribute("contest_name", rsr.getStringItem("contest_name"));
+            getRequest().setAttribute("round_id", new Integer(rsr.getIntItem("round_id")));
+            getRequest().setAttribute("path",  rsr.getStringItem("path"));
+            getRequest().setAttribute("link",  rsr.getStringItem("link"));
+            getRequest().setAttribute("width", new Integer(rsr.getIntItem("width")));
+            getRequest().setAttribute("height", new Integer(rsr.getIntItem("height")));
+            String date = "";
+            date += "&day=" + rsr.getStringItem("day");
+            date += "&month=" + rsr.getStringItem("month");
+            date += "&year=" + rsr.getStringItem("year");
+            date += "&hour=" + rsr.getStringItem("hour");
+            date += "&min=" + rsr.getStringItem("minute");
+            date += "&sec=0";
+            getRequest().setAttribute("date", date);
+            getRequest().setAttribute("reg_begin", rsr.getStringItem("reg_begin"));
+            getRequest().setAttribute("coding_begin", rsr.getStringItem("coding_begin"));
+            getRequest().setAttribute("forum_id", new Integer(rsr.getIntItem("forum_id")));
+            
+            /*ArrayList contests = dcHome.getAdContests();
+            if (contests != null && contests.size() > 0) {
+                schedTag.addTag(Data.getDynamicContestInfo(contests));
+            }*/
+
+            //document.addTag(schedTag);
+            //log.debug(document.getXML(2));
+            setNextPage("/calendar/srm.jsp");
+            setIsNextPageInContext(true);
+        } catch (TCWebException we) {
+            throw we;
+        } catch (Exception e) {
+            throw new TCWebException(e);
+        }
+    }    
+}
