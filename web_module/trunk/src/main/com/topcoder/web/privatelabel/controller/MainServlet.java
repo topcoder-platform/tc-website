d3b7ff53683fbcc5634e0602e2fd9dc219564ba1
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-02-23 23:37:41 +0000

    add logic to create an auth that has the db in it

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/MainServlet.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/MainServlet.java
@@ -7,14 +7,13 @@ import com.topcoder.web.common.BaseServlet;
 import com.topcoder.web.common.SessionInfo;
 import com.topcoder.web.common.TCRequest;
 import com.topcoder.web.common.TCResponse;
+import com.topcoder.web.common.model.CoderSessionInfo;
 import com.topcoder.web.common.security.BasicAuthentication;
 import com.topcoder.web.common.security.SessionPersistor;
 import com.topcoder.web.common.security.WebAuthentication;
 import com.topcoder.web.privatelabel.Constants;
-import com.topcoder.web.common.model.CoderSessionInfo;
+import com.topcoder.web.privatelabel.controller.request.RegistrationBase;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
 import java.util.MissingResourceException;
 import java.util.Set;
 
@@ -31,8 +30,20 @@ public class MainServlet extends BaseServlet {
 
     protected WebAuthentication createAuthentication(TCRequest request,
                                                      TCResponse response) throws Exception {
-        return new BasicAuthentication(new SessionPersistor(request.getSession()),
-                request, response, BasicAuthentication.PRIVATE_LABEL_SITE);
+        long companyId = Long.parseLong(request.getParameter(Constants.COMPANY_ID));
+        String db=null;
+        try {
+            db = RegistrationBase.getCompanyDb(companyId, Constants.TRANSACTIONAL);
+        } catch (Exception e) {
+            log.warn("no db found for company " + companyId);
+        }
+        if (db==null) {
+            return new BasicAuthentication(new SessionPersistor(request.getSession()),
+                    request, response, BasicAuthentication.PRIVATE_LABEL_SITE);
+        } else {
+            return new BasicAuthentication(new SessionPersistor(request.getSession()),
+                    request, response, BasicAuthentication.PRIVATE_LABEL_SITE, db);
+        }
     }
 
     protected SessionInfo createSessionInfo(TCRequest request,
