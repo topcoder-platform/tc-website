069892b1956e3b6ff2b19a661fb7b25265b6334e
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-03-01 07:31:59 +0000

    fix

--- web_module/branches/leftnav_09122005/src/main/com/topcoder/utilities/ProjectResultPlacedSetter.java
+++ web_module/branches/leftnav_09122005/src/main/com/topcoder/utilities/ProjectResultPlacedSetter.java
@@ -36,7 +36,7 @@ public class ProjectResultPlacedSetter {
             conn = dev.getConnection();
 
             ps1 = conn.prepareStatement(select);
-            ps1 = conn.prepareStatement(update);
+            ps2 = conn.prepareStatement(update);
             rs = ps1.executeQuery();
 
             long lastProjectId = -1;
