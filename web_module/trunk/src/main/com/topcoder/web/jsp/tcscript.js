a09860302729b2603a510b24a15edcc634b2f06c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-03-13 15:28:15 +0000

    put the full path into the tctime request

--- web_module/trunk/src/main/com/topcoder/web/jsp/tcscript.js
+++ web_module/trunk/src/main/com/topcoder/web/jsp/tcscript.js
@@ -17,7 +17,7 @@ document.write('<link type="image/x-icon" rel="shortcut icon" href="/i/favicon.i
   }
 
 function tcTime() {
-  w=window.open("http://wwww.topcoder.com/tc?module=Static&d1=calendar&d2=time","Time","top=2,left=2,width=250,height=50,resizable=yes,status=1");
+  w=window.open("http://www.topcoder.com/tc?module=Static&d1=calendar&d2=time","Time","top=2,left=2,width=250,height=50,resizable=yes,status=1");
   return;
 }
 
