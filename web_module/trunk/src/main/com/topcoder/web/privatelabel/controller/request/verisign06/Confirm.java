fbdb05d5a4622f8c4e0ee846bd275fac5cfb8b42
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-03-24 22:05:33 +0000

    implement makeRegInfo

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/verisign06/Confirm.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/verisign06/Confirm.java
@@ -1,8 +1,9 @@
 package com.topcoder.web.privatelabel.controller.request.verisign06;
 
 import com.topcoder.shared.util.logging.Logger;
-import com.topcoder.web.common.StringUtils;
-import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.common.*;
+import com.topcoder.web.common.model.DemographicQuestion;
+import com.topcoder.web.common.model.DemographicResponse;
 import com.topcoder.web.privatelabel.Constants;
 import com.topcoder.web.privatelabel.controller.request.FullRegConfirm;
 import com.topcoder.web.privatelabel.model.FullRegInfo;
@@ -11,6 +12,7 @@ import com.topcoder.web.privatelabel.model.SimpleRegInfo;
 import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.Iterator;
 
 /**
  * @author dok
@@ -42,12 +44,12 @@ public class Confirm extends FullRegConfirm {
     }
 
     protected void setNextPage() {
-            if (hasErrors()) {
-                setNextPage(Constants.VERISIGN_06_REG_PAGE);
-            } else {
-                setNextPage(Constants.VERISIGN_06_REG_CONFIRM_PAGE);
-            }
-            setIsNextPageInContext(true);
+        if (hasErrors()) {
+            setNextPage(Constants.VERISIGN_06_REG_PAGE);
+        } else {
+            setNextPage(Constants.VERISIGN_06_REG_CONFIRM_PAGE);
+        }
+        setIsNextPageInContext(true);
     }
 
 
@@ -109,7 +111,80 @@ public class Confirm extends FullRegConfirm {
         if (info.getLastName().length() < 1) {
             addError(Constants.LAST_NAME, getBundle().getProperty("error_enter_last_name"));
         }
-  
+
+    }
+
+
+    /**
+     * the difference between this and the parents makeRegInfo() is that
+     * this one does not require there to be reg info in the sesssion already.
+     * @return
+     * @throws Exception
+     */
+    protected SimpleRegInfo makeRegInfo() throws Exception {
+        FullRegInfo info = (FullRegInfo) super.makeRegInfo();
+        log.debug("name " + info.getFirstName() + " " + info.getLastName());
+
+        //get the rest from the request
+        if (hasRequestParameter(Constants.CODER_TYPE)) {
+            String sCoderType = getRequestParameter(Constants.CODER_TYPE);
+            info.setCoderType(Integer.parseInt(sCoderType == null ? "-1" : sCoderType));
+        }
+
+        //get the demographic responses
+        DemographicQuestion q = null;
+        String[] values = null;
+        DemographicResponse r = null;
+        String key = null;
+        List questionList = getQuestionList(info.getCoderType(), getLocale());
+        //loop through all the questions
+        info.clearResponses();
+        for (Iterator it = questionList.iterator(); it.hasNext();) {
+            q = (DemographicQuestion) it.next();
+            key = Constants.DEMOG_PREFIX + q.getId();
+            values = getRequest().getParameterValues(key);
+            if (q.isRequired() && !hasRequestParameter(key) && !info.hasResponse(q.getId())) {
+                //this is cheating, cuz really it should be done in the data checking method.
+                addError(key, getBundle().getProperty("error_required_answer"));
+            } else if (values != null) {
+                //if they've responded in this request, replace whatever was there in persitor with this new stuff
+                if (values.length > 0) {
+                    info.removeResponses(q.getId());
+                }
+                String value = null;
+                //loop through all the responses in the request
+                for (int i = 0; i < values.length; i++) {
+                    value = StringUtils.checkNull(values[i]).trim();
+                    if (value.length() > 0) {
+                        r = new DemographicResponse();
+                        r.setQuestionId(q.getId());
+                        if (q.getAnswerType() == DemographicQuestion.FREE_FORM) {
+                            r.setText(values[i]);
+                            r.setSort(q.getSort());
+                            info.addResponse(r);
+                        } else if (q.getAnswerType() == DemographicQuestion.SINGLE_SELECT ||
+                                q.getAnswerType() == DemographicQuestion.MULTIPLE_SELECT) {
+                            try {
+                                r.setAnswerId(Long.parseLong(values[i]));
+                                r.setSort(q.getSort());
+                                info.addResponse(r);
+                            } catch (NumberFormatException e) {
+                                //skip it, it's invalid, checking will have to pick it up later
+                            }
+                        } else {
+                            throw new Exception("invalid answer type found: " + q.getAnswerType() + " for question " + q.getId());
+                        }
+                    } else if (q.isRequired()) {
+                        /* at this point, we know that the parameter was included in the request, but the value
+                        * was empty.  we'll complain to them and ask them to fill in the question
+                        */
+                        addError(key, getBundle().getProperty("error_required_answer"));
+                    }
+                }
+            }
+
+        }
+        return info;
     }
 
 
