7e464e4300010c203493b44e595b27840a12183f
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-03-27 16:17:58 +0000

    *** empty log message ***

---
+++ web_module/branches/tcs_onsite_2006_03_14/src/main/com/topcoder/web/onsite/controller/request/ComponentRoundInfoProcessor.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2006 TopCoder, Inc. All rights reserved.
+ */
+
+package com.topcoder.web.onsite.controller.request;
+
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.web.onsite.Constants;
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.shared.security.ClassResource;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.dataAccess.DataAccess;
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.web.common.BaseProcessor;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.ArrayList;
+import com.topcoder.shared.netCommon.messages.spectator.DefineComponentContest;
+import com.topcoder.shared.netCommon.messages.spectator.RequestComponentRoundInfo;
+import com.topcoder.shared.netCommon.messages.MessageUtil;
+import com.topcoder.shared.netCommon.messages.spectator.ComponentData;
+import com.topcoder.shared.netCommon.messages.spectator.ComponentCoder;
+import com.topcoder.shared.netCommon.messages.spectator.CoderData;
+import com.topcoder.shared.netCommon.messages.spectator.ComponentScoreUpdate;
+import com.topcoder.shared.netCommon.messages.MessagePacket;
+
+/**
+ * <strong>Purpose</strong>:
+ * A processor to retrieve competitions to wager on.
+ * 
+ * @author pulky
+ * @version 1.0
+ */
+public class ComponentRoundInfoProcessor extends BaseProcessor {
+
+    /**
+     * The logger to log to.
+     */
+    private static final Logger log = Logger.getLogger(ComponentRoundInfoProcessor.class);
+
+    /**
+     * Retrieves data from the DB to the requestor.
+     *
+     * @param contestId the contest id of the required data.
+     *
+     * @return a Map with the retrieved ResultSetContainers.
+     */
+    private Map getComponentRoundInfoData(long contestId) throws Exception {
+        Request request = new Request();
+        request.setContentHandle(Constants.COMPONENT_ROUND_INFO_COMMAND);
+        request.setProperty(Constants.USER_ID, String.valueOf(contestId));
+        DataAccessInt dai = new DataAccess(DBMS.TCS_OLTP_DATASOURCE_NAME);
+
+        return dai.getData(request);
+    }
+
+    /**
+     * Process the ComponentRoundInfo request.
+     * 
+     */
+    protected void businessProcessing() throws Exception {
+        // decodes request
+        RequestComponentRoundInfo requestComponentRoundInfo = 
+            (RequestComponentRoundInfo) MessageUtil.decodeQueryStringMessage(getRequest().getQueryString());
+
+        // retrieves data
+        Map m = getComponentRoundInfoData(requestComponentRoundInfo.getContestID());
+
+        ResultSetContainer rscComponentData = (ResultSetContainer)m.get(Constants.COMPONENT_DATA_QUERY);
+        ResultSetContainer rscComponentCoder = (ResultSetContainer)m.get(Constants.COMPONENT_CODER_QUERY);
+        ResultSetContainer rscReviewerData = (ResultSetContainer)m.get(Constants.REVIEWER_DATA_QUERY);
+        ResultSetContainer rscComponentScore = (ResultSetContainer)m.get(Constants.COMPONENT_SCORE_QUERY);
+        
+        log.debug("Got " +  rscComponentData.size() + " rows for: " + Constants.COMPONENT_DATA_QUERY);
+        log.debug("Got " +  rscComponentCoder.size() + " rows for: " + Constants.COMPONENT_CODER_QUERY);
+        log.debug("Got " +  rscReviewerData.size() + " rows for: " + Constants.REVIEWER_DATA_QUERY);
+        log.debug("Got " +  rscComponentScore.size() + " rows for: " + Constants.COMPONENT_SCORE_QUERY);
+        
+        // builds the objects to be returned
+        ComponentData componentData = null;
+        if (rscComponentData.size() > 0) {
+            componentData  = new ComponentData(
+                requestComponentRoundInfo.getComponentID(), 
+                rscComponentData.getStringItem(0, "component_name"),
+                rscComponentData.getStringItem(0, "category_name"));            
+        }
+
+        ArrayList componentCoderList = null;
+        if (rscComponentCoder.size() > 0) {
+            componentCoderList = new ArrayList(rscComponentCoder.size());
+            
+            Iterator it = rscComponentCoder.iterator();
+            ResultSetContainer.ResultSetRow rsr;
+            while (it.hasNext()) {
+                 rsr = (ResultSetContainer.ResultSetRow) it.next();
+                componentCoderList.add(new ComponentCoder(
+                    rsr.getIntItem("user_id"), 
+                    rsr.getStringItem("handle"), 
+                    rsr.getIntItem("new_rating"), 
+                    rsr.getIntItem("wager_amount")));
+            }
+        }
+        
+        ArrayList reviewerDataList = null;
+        if (rscComponentCoder.size() > 0) {
+            reviewerDataList = new ArrayList(rscReviewerData.size());
+            
+            Iterator it = rscReviewerData.iterator();
+            ResultSetContainer.ResultSetRow rsr;
+            while (it.hasNext()) {
+                rsr = (ResultSetContainer.ResultSetRow) it.next();
+                reviewerDataList.add(new CoderData(
+                    rsr.getIntItem("user_id"), 
+                    rsr.getStringItem("handle"), 
+                    rsr.getIntItem("new_rating")));
+            }
+        }
+        
+        // creates DefineComponentContest instance
+        DefineComponentContest defineComponentContest = new DefineComponentContest(
+            requestComponentRoundInfo.getContestID(), 
+            requestComponentRoundInfo.getRoundID(),
+            componentData,
+            componentCoderList, 
+            reviewerDataList);
+        
+        // adds first message for DefineComponentContest.
+        MessagePacket mp = new MessagePacket();
+        //TODO mp.add(defineComponentContest);                            ******* DefineComponentContest does not implement Message??
+
+        // adds all ComponentScoreUpdate messages.
+        if (rscComponentScore.size() > 0) {
+            Iterator it = rscComponentScore.iterator();
+            ResultSetContainer.ResultSetRow rsr;
+            while (it.hasNext()) {
+                rsr = (ResultSetContainer.ResultSetRow) it.next();
+                
+                mp.add(new ComponentScoreUpdate(
+                    requestComponentRoundInfo.getContestID(),
+                    requestComponentRoundInfo.getRoundID(),
+                    requestComponentRoundInfo.getComponentID(), 
+                    rsr.getIntItem("submitter_id"), 
+                    rsr.getIntItem("author_id"),
+                    rsr.getIntItem("score")));
+            }
+        }
+        
+        // encodes and retrieves the messages packet
+        //TODO String xmlString = MessageUtil.encodeXMLMessage(mp);        ******* cannot encode MessagePackets??
+        //TODO how retrieve the xml?
+    }
+}
