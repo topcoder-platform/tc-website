d6f2509522034a5ce9fecf6ffbd195788be5b97f
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-04-17 21:47:16 +0000

    add

---
+++ src/main/com/topcoder/web/privatelabel/controller/request/googleeurope06/CredentialReminder.java
@@ -0,0 +1,21 @@
+package com.topcoder.web.privatelabel.controller.request.googleeurope06;
+
+import com.topcoder.web.privatelabel.controller.request.BaseCredentialReminder;
+import com.topcoder.web.privatelabel.Constants;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Apr 17, 2006
+ */
+public class CredentialReminder extends BaseCredentialReminder {
+
+    protected String getStartPage() {
+        return Constants.GOOGLE_EUROPE_06_CREDENTIALS_PAGE;
+    }
+
+    protected String getSuccessPage() {
+        return Constants.GOOGLE_EUROPE_06_CREDENTIALS_SENT_PAGE;
+    }
+}
+
