381ac8cde3fca48b57b7fbee3296918999d021bc
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-04-18 22:00:57 +0000

    don't reimplement the country list method

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/googleeurope06/Confirm.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/googleeurope06/Confirm.java
@@ -1,14 +1,10 @@
 package com.topcoder.web.privatelabel.controller.request.googleeurope06;
 
-import com.topcoder.web.privatelabel.controller.request.ResumeRegConfirm;
 import com.topcoder.web.privatelabel.Constants;
-import com.topcoder.web.common.TCWebException;
-import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
-import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.web.privatelabel.controller.request.ResumeRegConfirm;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Map;
 
 /**
  * @author dok
@@ -38,19 +34,6 @@ public class Confirm extends ResumeRegConfirm {
             setIsNextPageInContext(true);
         }
 
-    }
-    protected ResultSetContainer getCountryList() throws TCWebException {
-        try {
-            Request request = new Request();
-            request.setContentHandle("country_list_google");
-            Map map = getDataAccess(transDb, true).getData(request);
-            if (map == null)
-                throw new Exception("error getting country list from db");
-            else
-                return (ResultSetContainer) map.get("country_list_google");
-        } catch (Exception e) {
-            throw new TCWebException(e);
-        }
     }
 
 }
