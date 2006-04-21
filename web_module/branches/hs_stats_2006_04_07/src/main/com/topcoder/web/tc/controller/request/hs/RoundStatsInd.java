bfd8d6b3ad7c1ee5c8be60e76e18ddf908124a03
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-04-21 14:53:55 +0000

    *** empty log message ***

---
+++ web_module/branches/hs_stats_2006_04_07/src/main/com/topcoder/web/tc/controller/request/hs/RoundStatsInd.java
@@ -0,0 +1,54 @@
+package com.topcoder.web.tc.controller.request.hs;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer.ResultSetRow;
+import com.topcoder.web.common.TCRequest;
+import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.tc.Constants;
+
+/**
+ *
+ * @author cucu
+ */
+public class RoundStatsInd extends Base { 
+
+    
+    protected void businessProcessing() throws TCWebException {
+        try {
+            RoundInfo round = getRoundAndSeasonIds(getRequest());
+            //ListInfo li = new ListInfo(getRequest(), 1, 50, 2, "ASC");
+            
+            Request r = new Request();
+            r.setContentHandle("hs_round_stats_ind");
+            r.setProperty("rd", round.getRoundId() + "");
+            r.setProperty("snid", round.getSeasonId() + "");
+            r.setProperty("sntid", HS_SNTID + "");
+            
+            DataAccessInt dai = getDataAccess(true);
+            Map result = dai.getData(r);
+            
+            fillRoundAndSeasonNames (round, result);
+            
+                       
+//            sortAndCrop(result, "team_result", li);
+                        
+            getRequest().setAttribute("resultMap", result);
+            getRequest().setAttribute("roundInfo", round);
+  //          getRequest().setAttribute("listInfo", li);
+            
+            setNextPage(Constants.HS_ROUND_STATS_TEAM);
+            setIsNextPageInContext(true);
+        } catch (TCWebException we) {
+            throw we;
+        } catch (Exception e) {
+            throw new TCWebException(e);
+        }
+    }
+
+
+}
