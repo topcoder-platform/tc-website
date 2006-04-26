bf83f4d3fc854b4e1aec7418835162ae1a7fb099
nobody <nobody@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-04-26 22:20:29 +0000

    This commit was manufactured by cvs2svn to create tag 'tc_site_release_5_0_11'.

---
+++ web_module/tags/tc_site_release_5_0_11/src/main/com/topcoder/web/codinginterface/techassess/controller/request/Compile.java
@@ -0,0 +1,89 @@
+package com.topcoder.web.codinginterface.techassess.controller.request;
+
+import com.topcoder.shared.netCommon.screening.request.ScreeningCompileRequest;
+import com.topcoder.shared.netCommon.screening.response.ScreeningCompileResponse;
+import com.topcoder.shared.problem.Problem;
+import com.topcoder.shared.problem.ProblemComponent;
+import com.topcoder.shared.screening.common.ScreeningApplicationServer;
+import com.topcoder.web.codinginterface.ServerBusyException;
+import com.topcoder.web.codinginterface.techassess.Constants;
+import com.topcoder.web.codinginterface.techassess.model.ProblemInfo;
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.common.StringUtils;
+
+/**
+ * User: dok
+ * Date: Jan 3, 2005
+ * Time: 1:06:13 PM
+ */
+public class Compile extends Base {
+
+    protected void techAssessProcessing() throws Exception {
+
+        if (getUser().isAnonymous()) {
+            setNextPage(buildProcessorRequestString(Constants.RP_LOGIN,
+                    new String[]{Constants.COMPANY_ID}, new String[]{String.valueOf(getCompanyId())}));
+            setIsNextPageInContext(false);
+        } else {
+
+
+            long componentId = 0;
+            int problemTypeId = 0;
+            int languageId = 0;
+            String code = null;
+
+            if (hasParameter(Constants.COMPONENT_ID)) {
+                componentId = Long.parseLong(getRequest().getParameter(Constants.COMPONENT_ID).trim());
+            } else {
+                throw new NavigationException("Invalid Request, missing parameter");
+            }
+
+            if (hasParameter(Constants.PROBLEM_TYPE_ID)) {
+                problemTypeId = Integer.parseInt(getRequest().getParameter(Constants.PROBLEM_TYPE_ID).trim());
+            } else {
+                throw new NavigationException("Invalid Request, missing parameter");
+            }
+
+            if (hasParameter(Constants.LANGUAGE_ID)) {
+                languageId = Integer.parseInt(getRequest().getParameter(Constants.LANGUAGE_ID).trim());
+            } else {
+                throw new NavigationException("Invalid Request, missing parameter");
+            }
+
+            code = StringUtils.checkNull(getRequest().getParameter(Constants.CODE));
+
+            ScreeningCompileRequest request = new ScreeningCompileRequest(componentId, problemTypeId, languageId, code);
+            request.setServerID(ScreeningApplicationServer.WEB_SERVER_ID);
+            request.setSessionID(getSessionId());
+
+            try {
+                send(request);
+            } catch (ServerBusyException e) {
+                throw new NavigationException("Sorry, the server is busy with a previous request.  " +
+                        "When using this tool, please wait for a response before you attempt to proceed.");
+            }
+
+            showProcessingPage();
+
+            ScreeningCompileResponse response = (ScreeningCompileResponse) receive(30*1000);
+
+            setUserMessage(response.getMessage());
+            Problem p = new Problem();
+            p.setProblemComponents(new ProblemComponent[]{response.getProblemComponent()});
+            ProblemInfo problem = new ProblemInfo(code, componentId, languageId, p, problemTypeId);
+            problem.setStartTime(response.getOpenTime());
+            problem.setTime(response.getLength());
+            log.debug("compile start: " + problem.getStartTime() + " length: " + problem.getTime());
+
+            setDefault(Constants.PROBLEM, problem);
+
+            closeProcessingPage(buildProcessorRequestString(Constants.RP_VIEW_PROBLEM_RESPONSE,
+                    new String[]{Constants.MESSAGE_ID, Constants.COMPONENT_ID, Constants.PROBLEM_TYPE_ID},
+                    new String[]{String.valueOf(getMessageId()), String.valueOf(componentId), String.valueOf(problemTypeId)}));
+
+        }
+
+
+    }
+
+}
---
+++ web_module/tags/tc_site_release_5_0_11/src/main/com/topcoder/web/jsp/calendar.jsp
@@ -0,0 +1,116 @@
+<%@ page import="com.topcoder.shared.util.ApplicationServer"%>
+<style type="text/css">
+#calendarTCO06Mini
+{
+color: #FFFFFF;
+font-size: 11px;
+font-weight: bold;
+text-align: center;
+vertical-align: middle;
+background-color: #9C268F;;
+}
+#calendarTCO06Date
+{
+color: #FFFFFF;
+font-weight: bold;
+text-decoration: none;
+}
+#calendarLongMini
+{
+color: #FFFFFF;
+font-size: 11px;
+font-weight: bold;
+text-align: center;
+vertical-align: middle;
+background-color: #007DC5;;
+}
+#calendarLongDate
+{
+color: #FFFFFF;
+font-weight: bold;
+text-decoration: none;
+}
+
+</style>
+
+<table width="170" bgcolor="#4A4A4A" cellspacing="0" cellpadding="0" border="0">
+<tr>
+<td valign="top">
+<table width="100%" border="0" cellspacing="1" cellpadding="1">
+<tr><td colspan="8" class="statTextBig" nowrap="nowrap" height="18">&#160;Match Dates: April</td></tr>
+
+<tr>
+<td id="calendarDays" width="15%">S</td>
+<td id="calendarDays" width="14%">M</td>
+<td id="calendarDays" width="14%">T</td>
+<td id="calendarDays" width="14%">W</td>
+<td id="calendarDays" width="14%">T</td>
+<td id="calendarDays" width="14%">F</td>
+<td id="calendarDays" width="15%">S</td>
+</tr>
+
+<tr>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarMini">1</td>
+</tr>
+
+<tr>
+<td id="calendarMini">2</td>
+<td id="calendarSRMMini"><A id="miniSRMDate" href="/tc?module=MatchDetails&rd=9817">3</A></td>
+<td id="calendarMini">4</td>
+<td id="calendarMini">5</td>
+<td id="calendarSRMMini" style="background: url(/i/tournament/tco06/calendar_srm_tco.gif) top center repeat-y;;"><A id="miniSRMDate" href="/tc?module=MatchDetails&rd=9818">6</A></td>
+<td id="calendarMini">7</td>
+<td id="calendarMini">8</td>
+</tr>
+
+<tr>
+<td id="calendarMini">9</td>
+<td id="calendarMini">10</td>
+<td id="calendarSRMMini"><A id="miniSRMDate" href="/tc?module=MatchDetails&rd=9819">11</A></td>
+<td id="calendarMini">12</td>
+<td id="calendarTCO06Mini"><A id="calendarTCO06Date" href="/tc?module=Static&d1=tournaments&d2=tco06&d3=comp_schedule">13</A></td>
+<td id="calendarMini">14</td>
+<td id="calendarMini">15</td>
+</tr>
+
+<tr>
+<td id="calendarMini">16</td>
+<td id="calendarMini">17</td>
+<td id="calendarMini">18</td>
+<td id="calendarLongMini" align="left"><A id="calendarLongDate" href="/longcontest/?module=Static&d1=intel_rules">19</A></td>
+<td id="calendarTCO06Mini"><A id="calendarTCO06Date" href="/tc?module=Static&d1=tournaments&d2=tco06&d3=comp_schedule">20</A></td>
+<td id="calendarMini">21</td>
+<td id="calendarSRMMini"><A id="miniSRMDate" href="/tc?module=MatchDetails&rd=9820">22</A></td>
+</tr>
+
+<tr>
+<td id="calendarMini">23</td>
+<td id="calendarMini">24</td>
+<td id="calendarMini">25</td>
+<td id="calendarMini">26</td>
+<td id="calendarSRMMini"><A id="miniSRMDate" href="/tc?module=MatchDetails&rd=9821">27</A></td>
+<td id="calendarMini">28</td>
+<td id="calendarMini">29</td>
+</tr>
+
+<tr>
+<td id="calendarMini">30</td>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarBlankMini">&#160;</td>
+<td id="calendarBlankMini">&#160;</td>
+</tr>
+
+<tr><td colspan="8" class="statText" nowrap="nowrap" align="right"><A class="statText" HREF="/tc?module=Static&d1=calendar&d2=thisMonth">More Match Dates&#160;</A></td></tr>
+</table>
+</td>
+</tr>
+</table>
