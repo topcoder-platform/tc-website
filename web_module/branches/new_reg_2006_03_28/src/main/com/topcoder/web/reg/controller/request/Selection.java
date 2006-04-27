74836a018851bf0e53d2fbbda41ae90622391784
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-04-27 16:32:34 +0000

    add logic to populate reg types

--- web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/request/Selection.java
+++ web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/request/Selection.java
@@ -1,6 +1,13 @@
 package com.topcoder.web.reg.controller.request;
 
 import com.topcoder.web.reg.dao.RegistrationTypeDAO;
+import com.topcoder.web.reg.dao.UserDAO;
+import com.topcoder.web.reg.Constants;
+import com.topcoder.web.reg.model.RegistrationType;
+import com.topcoder.web.common.PermissionException;
+import com.topcoder.shared.security.ClassResource;
+
+import java.util.Iterator;
 
 /**
  * @author dok
@@ -14,10 +21,19 @@ public class Selection extends Base {
         //if the user is logged in, figure out what registration types they have
         //so that the check boxes can be populated.
 
-        getRequest().setAttribute("registrationTypeList", new RegistrationTypeDAO().getRegistrationTypes());
+        if (userLoggedIn()) {
+            getRequest().setAttribute("registrationTypeList", new RegistrationTypeDAO().getRegistrationTypes());
+            RegistrationType rt;
+            for (Iterator it = new UserDAO().find(new Long(getUser().getId())).getRegistrationTypes().iterator(); it.hasNext();) {
+                rt = (RegistrationType)it.next();
+                setDefault(Constants.REGISTRATION_TYPE+rt.getId(), "on");
+            }
 
-        setNextPage("/selection.jsp");
-        setIsNextPageInContext(true);
+            setNextPage("/selection.jsp");
+            setIsNextPageInContext(true);
+        } else {
+            throw new PermissionException(getUser(), new ClassResource(this.getClass()));
+        }
     }
 
 }
