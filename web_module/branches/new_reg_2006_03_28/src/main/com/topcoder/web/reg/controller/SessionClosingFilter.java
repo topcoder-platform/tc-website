71cb9317e858360d68c92a45f3266d878020e2ca
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-05-04 19:41:11 +0000

    add

--- web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/SessionClosingFilter.java
+++ web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/SessionClosingFilter.java
@@ -1,6 +1,7 @@
 package com.topcoder.web.reg.controller;
 
 import com.topcoder.web.reg.HibernateUtils;
+import com.topcoder.shared.util.logging.Logger;
 
 import javax.servlet.*;
 import java.io.IOException;
@@ -11,20 +12,25 @@ import java.io.IOException;
  *          Create Date: May 4, 2006
  */
 public class SessionClosingFilter implements Filter {
-   private FilterConfig filterConfig = null;
-   public void init(FilterConfig filterConfig)
-      throws ServletException {
-      this.filterConfig = filterConfig;
-   }
-   public void destroy() {
-      this.filterConfig = null;
-   }
-   public void doFilter(ServletRequest request,
-                        ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
+    protected static final Logger log = Logger.getLogger(SessionClosingFilter.class);
+    private FilterConfig filterConfig = null;
 
+    public void init(FilterConfig filterConfig)
+            throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    public void destroy() {
+        this.filterConfig = null;
+    }
+
+    public void doFilter(ServletRequest request,
+                         ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        log.debug("oh yeah, filtering!!!");
         HibernateUtils.closeSession();
 
 
-   }
+    }
 }
