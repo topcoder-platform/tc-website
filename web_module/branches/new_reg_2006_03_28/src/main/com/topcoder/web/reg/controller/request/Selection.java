520c20bb7c6b9f93e3e965dcfb5491533ce4d055
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-05-09 00:59:43 +0000

    add a flag for new registrations

--- web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/request/Selection.java
+++ web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/request/Selection.java
@@ -1,8 +1,8 @@
 package com.topcoder.web.reg.controller.request;
 
+import com.topcoder.web.reg.Constants;
 import com.topcoder.web.reg.dao.RegistrationTypeDAO;
 import com.topcoder.web.reg.dao.UserDAO;
-import com.topcoder.web.reg.Constants;
 import com.topcoder.web.reg.model.RegistrationType;
 import com.topcoder.web.common.PermissionException;
 import com.topcoder.shared.security.ClassResource;
@@ -18,28 +18,28 @@ public class Selection extends Base {
 
     protected void registrationProcessing() throws Exception {
 
-        //if the user is logged in, figure out what registration types they have
-        //so that the check boxes can be populated.
+        boolean newReg = String.valueOf(true).equalsIgnoreCase(getRequest().getParameter(Constants.NEW_REG));
 
-        if (userIdentified()) {
-            //they must be attempting to update their profile, but are not logged in
-            throw new PermissionException(getUser(), new ClassResource(this.getClass()));
-        } else if (userLoggedIn()) {
+        if (userLoggedIn()) {
             //they're updating their info, and they're logged in, so here we go
             getRequest().setAttribute("registrationTypeList", new RegistrationTypeDAO().getRegistrationTypes());
             RegistrationType rt;
             for (Iterator it = new UserDAO().find(new Long(getUser().getId())).getRegistrationTypes().iterator(); it.hasNext();) {
-                rt = (RegistrationType)it.next();
-                setDefault(Constants.REGISTRATION_TYPE+rt.getId(), String.valueOf(true));
+                rt = (RegistrationType) it.next();
+                setDefault(Constants.REGISTRATION_TYPE + rt.getId(), String.valueOf(true));
             }
-
             setNextPage("/selection.jsp");
             setIsNextPageInContext(true);
         } else {
-            //they must be attempting to register
-            getRequest().setAttribute("registrationTypeList", new RegistrationTypeDAO().getRegistrationTypes());
-            setNextPage("/selection.jsp");
-            setIsNextPageInContext(true);
+            if (newReg) {
+                //they must be attempting to register
+                getRequest().setAttribute("registrationTypeList", new RegistrationTypeDAO().getRegistrationTypes());
+                setNextPage("/selection.jsp");
+                setIsNextPageInContext(true);
+            } else {
+                throw new PermissionException(getUser(), new ClassResource(this.getClass()));
+
+            }
         }
     }
 
