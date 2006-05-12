2b8eb436eef6291266734957d9cd005fa55df815
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-05-12 14:01:52 +0000

    go back to the way it was built

--- web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/ExtendedThreadLocalSessionContext.java
+++ web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/ExtendedThreadLocalSessionContext.java
@@ -1,43 +1,42 @@
 package com.topcoder.web.reg.controller;
 
-import org.hibernate.engine.SessionFactoryImplementor;
-import org.hibernate.context.ThreadLocalSessionContext;
-import org.hibernate.SessionFactory;
-import org.hibernate.FlushMode;
-import org.hibernate.classic.Session;
 import com.topcoder.shared.util.logging.Logger;
+import org.hibernate.FlushMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.hibernate.context.ThreadLocalSessionContext;
+import org.hibernate.engine.SessionFactoryImplementor;
 
 /**
  * Trivial implementation of a session-per-conversation context.
- * <p>
+ * <p/>
  * This is how you can activate it programmatically:
  * <pre>
  *         HibernateUtil.getConfiguration().setProperty(
  *              Environment.CURRENT_SESSION_CONTEXT_CLASS,
  *              ExtendedThreadLocalSessionContext.class.getName()
  *        );
- *
+ * <p/>
  * </pre>
- * <p>
+ * <p/>
  * Or simply set the <tt>hibernate.current_session_context_class</tt>
  * configuration option to this fully qualified class name.
- * <p>
+ * <p/>
  * You have to bind/unbind your disconnected long-running Session on each requst:
- * <p>
+ * <p/>
  * <pre>
  *  ExtendedThreadLocalSessionContext.bind(hibernateSession); // Begin request
  *  // ... service executes
  *  hibernateSession = ExtendedThreadLocalSessionContext.unbind(sf); // End request
  *  // ... store hibernateSession until next request
  * </pre>
- * <p>
+ * <p/>
  * Finally, don't forget to end your long-running conversation by
  * calling <tt>flush()</tt> on your Session, and make sure you close
  * it afterwards.
  *
- * @see org.hibernate.ce.auction.web.filter.HibernateThreadExtendedFilter
- *
  * @author christian@hibernate.org
+ * @see org.hibernate.ce.auction.web.filter.HibernateThreadExtendedFilter
  */
 public class ExtendedThreadLocalSessionContext extends ThreadLocalSessionContext {
 
@@ -47,33 +46,42 @@ public class ExtendedThreadLocalSessionContext extends ThreadLocalSessionContext
         super(factory);
     }
 
-    ThreadLocal session = new ThreadLocal();
 
     // No automatic flushing of the Session at transaction commit, client calls flush()
-    protected boolean isAutoFlushEnabled() { return false; }
+    protected boolean isAutoFlushEnabled() {
+        return false;
+    }
+
     protected Session buildOrObtainSession() {
-        Session s = (Session)session.get();
-        if (s==null) {
-            log.debug("Opening a new Session");
-            s = super.buildOrObtainSession();
-            log.debug("Disabling automatic flushing of the Session");
-            s.setFlushMode(FlushMode.NEVER);
-        }
+        log.debug("Opening a new Session");
+        Thread.dumpStack();
+        Session s = super.buildOrObtainSession();
+        log.debug("Disabling automatic flushing of the Session");
+        s.setFlushMode(FlushMode.NEVER);
         return s;
     }
 
     // No automatic closing of the Session at transaction commit, client calls close()
-    protected boolean isAutoCloseEnabled() { return false; }
+    protected boolean isAutoCloseEnabled() {
+        return false;
+    }
 
     // Don't unbind after transaction completion, we expect the client to do this
     // so it can flush() and close() if needed (or continue the conversation).
     protected CleanupSynch buildCleanupSynch() {
         return new NoCleanupSynch(factory);
     }
+
     private static class NoCleanupSynch extends ThreadLocalSessionContext.CleanupSynch {
-        public NoCleanupSynch(SessionFactory factory) { super(factory); }
-        public void beforeCompletion() {}
-        public void afterCompletion(int i) {}
+        public NoCleanupSynch(SessionFactory factory) {
+            super(factory);
+        }
+
+        public void beforeCompletion() {
+        }
+
+        public void afterCompletion(int i) {
+        }
     }
 }
 
