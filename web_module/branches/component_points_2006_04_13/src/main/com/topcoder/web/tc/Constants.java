4a8a0011035394fdb2bceabd6889c49f0919a3c8
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-05-13 00:15:39 +0000

    no message

--- web_module/branches/component_points_2006_04_13/src/main/com/topcoder/web/tc/Constants.java
+++ web_module/branches/component_points_2006_04_13/src/main/com/topcoder/web/tc/Constants.java
@@ -18,15 +18,15 @@ public class Constants implements WebConstants {
     public static String HOME_PAGE;
     public static String VIEW_LEADER_BOARD_PAGE;
     public static String VIEW_ROOKIE_BOARD_PAGE;
-    public static String VIEW_POINTS_HISTORY_PAGE;
+    public static String VIEW_COMPETITION_HISTORY_PAGE;
     
     public static String LEADER_BOARD_COMMAND;
     public static String LEADER_BOARD_QUERY;
     public static String ROOKIE_BOARD_COMMAND;
     public static String ROOKIE_BOARD_QUERY;
     public static String CODER_LIST_KEY;
-    public static String POINTS_HISTORY_COMMAND;
-    public static String POINTS_HISTORY_QUERY;
+    public static String COMPETITION_HISTORY_COMMAND;
+    public static String COMPETITION_HISTORY_QUERY;
     public static String CURRENT_PERIOD_COMMAND;
     public static String CURRENT_PERIOD_QUERY;
     public static String DR_SEASON_COMMAND;
