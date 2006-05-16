b9c20eecf1bcb3109c315fc178edf5d09d3097c2
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-05-16 00:01:48 +0000

    add a bunch of defaults

--- web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/request/ViewSchoolSearch.java
+++ web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/request/ViewSchoolSearch.java
@@ -1,5 +1,9 @@
 package com.topcoder.web.reg.controller.request;
 
+import com.topcoder.web.reg.model.User;
+import com.topcoder.web.reg.model.SchoolType;
+import com.topcoder.web.reg.Constants;
+
 /**
  * @author dok
  * @version $Revision$ Date: 2005/01/01 00:00:00
@@ -7,6 +11,26 @@ package com.topcoder.web.reg.controller.request;
  */
 public class ViewSchoolSearch extends Base  {
     protected void registrationProcessing() throws Exception {
+
+        User u = getRegUser();
+
+        if (u.getHomeAddress()!=null) {
+            if (u.getHomeAddress().getCountry()!=null) {
+                setDefault(Constants.COUNTRY_CODE, u.getHomeAddress().getCountry().getCode());
+            }
+            if (u.getHomeAddress().getState()!=null) {
+                setDefault(Constants.STATE_CODE, u.getHomeAddress().getState().getCode());
+            }
+            if (u.getHomeAddress().getProvince()!=null) {
+                setDefault(Constants.PROVINCE, u.getHomeAddress().getProvince());
+            }
+            if (getRequestedTypes().contains(getFactory().getRegistrationTypeDAO().getHighSchoolType())) {
+                setDefault(Constants.SCHOOL_TYPE, SchoolType.HIGH_SCHOOL.toString());
+            }
+        }
+
+        getRequest().setAttribute("schoolTypes", getFactory().getSchoolTypeDAO().getSchoolTypes());
+        getRequest().setAttribute("countries", getFactory().getCountryDAO().getCountries());
         setNextPage("/school.jsp");
         setIsNextPageInContext(true);
     }
