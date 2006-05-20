d0fff78f270c260d5495e8d5a2af462480ed2dfc
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-05-20 21:14:39 +0000

    supply datasource

--- web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/RegServlet.java
+++ web_module/branches/new_reg_2006_03_28/src/main/com/topcoder/web/reg/controller/RegServlet.java
@@ -1,14 +1,19 @@
 package com.topcoder.web.reg.controller;
 
 import com.topcoder.shared.security.Resource;
+import com.topcoder.shared.security.Authorization;
+import com.topcoder.shared.security.User;
 import com.topcoder.shared.util.logging.Logger;
 import com.topcoder.shared.util.DBMS;
 import com.topcoder.web.common.BaseServlet;
 import com.topcoder.web.common.TCRequest;
 import com.topcoder.web.common.TCResponse;
+import com.topcoder.web.common.SecurityHelper;
 import com.topcoder.web.common.security.WebAuthentication;
 import com.topcoder.web.common.security.BasicAuthentication;
 import com.topcoder.web.common.security.SessionPersistor;
+import com.topcoder.web.common.security.TCSAuthorization;
+import com.topcoder.security.TCSubject;
 
 /**
  * @author dok
@@ -22,9 +27,17 @@ public class RegServlet extends BaseServlet {
         return true;
     }
 
-        protected WebAuthentication createAuthentication(TCRequest request,
-                                                         TCResponse response) throws Exception {
+    protected WebAuthentication createAuthentication(TCRequest request,
+                                                     TCResponse response) throws Exception {
         return new BasicAuthentication(new SessionPersistor(request.getSession()), request, response,
                 BasicAuthentication.REG_SITE, DBMS.JTS_OLTP_DATASOURCE_NAME);
+    }
+
+    protected Authorization createAuthorization(User user) throws Exception {
+        return new TCSAuthorization(SecurityHelper.getUserSubject(user.getId(), DBMS.JTS_OLTP_DATASOURCE_NAME));
+    }
+
+        protected TCSubject getUser(long id) throws Exception {
+        return SecurityHelper.getUserSubject(id, DBMS.JTS_OLTP_DATASOURCE_NAME);
     }
 }
