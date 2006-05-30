e50e45d6a7c4e101c8e379d3fd4b6c4650a507b0
nobody <nobody@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-05-30 20:42:06 +0000

    This commit was manufactured by cvs2svn to create tag 'before_merge_component_points_2006_04_13'.

---
+++ web_module/tags/before_merge_component_points_2006_04_13/src/testcases/com/topcoder/web/reg/DetachedTestCase.java
@@ -0,0 +1,23 @@
+package com.topcoder.web.reg;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: May 11, 2006
+ */
+public class DetachedTestCase extends TCHibernateTestCase {
+
+
+
+
+
+    public void testRollback() {
+        HibernateUtils.begin();
+        HibernateUtils.rollback();
+        assertTrue("got here without exception", true);
+    }
+
+
+
+
+}
