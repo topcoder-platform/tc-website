94a5db59cd7a4d053d4a9745925baa0925eef0db
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-05-31 02:47:17 +0000

    fix validation

--- src/main/com/topcoder/web/reg/validation/ReferralOtherValidator.java
+++ src/main/com/topcoder/web/reg/validation/ReferralOtherValidator.java
@@ -2,6 +2,7 @@ package com.topcoder.web.reg.validation;
 
 import com.topcoder.web.common.validation.*;
 import com.topcoder.web.reg.Constants;
+import com.topcoder.shared.util.logging.Logger;
 
 /**
  * @author dok
@@ -9,14 +10,14 @@ import com.topcoder.web.reg.Constants;
  *          Create Date: May 30, 2006
  */
 public class ReferralOtherValidator implements Validator {
+    private static final Logger log = Logger.getLogger(ReferralOtherValidator.class);
 
     public ValidationResult validate(ValidationInput input) {
         ValidationResult ret = new NonEmptyValidator("Please enter your referral details.").validate(input);
         if (ret.isValid()) {
             return new SizeValidator(1, Constants.MAX_REFERRAL_OTHER_LENGTH, "referral details").validate(input);
         } else {
-            //if they didn't enter a quote, that's ok, it's not required.
-            return ValidationResult.SUCCESS;
+            return ret; 
         }
 
     }
