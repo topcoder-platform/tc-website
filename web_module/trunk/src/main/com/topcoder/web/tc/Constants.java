b8afeac079a10c87754a21bd2788a8912047f72e
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-06-02 14:18:17 +0000

    add tccc06 logo terms id

--- web_module/trunk/src/main/com/topcoder/web/tc/Constants.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/Constants.java
@@ -73,6 +73,7 @@ public class Constants implements WebConstants {
     public static String SCHOOL_NAME;
     public static int TCO05_LOGO_TERMS_ID;
     public static int TCO06_LOGO_TERMS_ID;
+    public static int TCCC06_LOGO_TERMS_ID;
     public static String LOGO;
     public static int TCO05_SPONSOR_TERMS_ID;
 
