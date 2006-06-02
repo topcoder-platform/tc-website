d33861e17a382d01715fe6ea91a5d9f61a9d58d6
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-06-02 19:35:55 +0000

    *** empty log message ***

--- web_module/branches/or_changes_2006_05_15/src/main/com/topcoder/web/tc/Constants.java
+++ web_module/branches/or_changes_2006_05_15/src/main/com/topcoder/web/tc/Constants.java
@@ -149,12 +149,12 @@ public class Constants implements WebConstants {
     public static final int TCO06_SPONSOR_COMPANY_QUESTION_ID = 31;
     public static final int TCO06_SPONSOR_TERMS_ID = 20388;
     public static int UNLOCK_CARD_PREFERENCE_ID;
-/*    public static int JAVA_CATALOG_ID;
+    public static int JAVA_CATALOG_ID;
     public static int DOT_NET_CATALOG_ID;
     public static int CUSTOM_JAVA_CATALOG_ID;
     public static int CUSTOM_DOT_NET_CATALOG_ID;
     public static int FLASH_CATALOG_ID;
-    public static int APPLICATIONS_CATALOG_ID;*/
+    public static int APPLICATIONS_CATALOG_ID;
     public static int INTERNAL_NOTE_TYPE_ID;
     //public static int LANGUAGE_NOTE_TYPE_ID;
     //public static int TECH_NOTE_TYPE_ID;
