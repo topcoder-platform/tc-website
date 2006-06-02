dd8c8a97ddcdf9de03c5a147b48da4e042f6d466
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-06-02 17:22:23 +0000

    dont' include millis in the file name

--- src/main/com/topcoder/web/tc/controller/request/tournament/BaseLogoSubmit.java
+++ src/main/com/topcoder/web/tc/controller/request/tournament/BaseLogoSubmit.java
@@ -61,8 +61,8 @@ abstract class BaseLogoSubmit extends Base {
                     fileName.append(cal.get(Calendar.HOUR_OF_DAY)).append("_");
                     fileName.append(cal.get(Calendar.MINUTE)).append("_");
                     fileName.append(cal.get(Calendar.SECOND)).append("_");
-                    fileName.append(cal.get(Calendar.MILLISECOND)).append("_s");
-                    fileName.append(submissionCount+1);
+                    //fileName.append(cal.get(Calendar.MILLISECOND)).append("_");
+                    fileName.append("s").append(submissionCount+1);
                     int idx = file.getRemoteFileName().lastIndexOf('.');
                     if (idx >= 0) {
                         fileName.append(file.getRemoteFileName().substring(file.getRemoteFileName().lastIndexOf('.')));
