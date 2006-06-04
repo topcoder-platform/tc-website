acb5d54973548676a84df55ba206d1a5f2daac02
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-06-04 14:01:40 +0000

    *** empty log message ***

--- web_module/branches/or_changes_2006_05_15/src/main/com/topcoder/apps/review/rboard/RBoardApplicationBean.java
+++ web_module/branches/or_changes_2006_05_15/src/main/com/topcoder/apps/review/rboard/RBoardApplicationBean.java
@@ -23,15 +23,23 @@ import com.topcoder.util.idgenerator.bean.IdGenHome;
  */
 public class RBoardApplicationBean extends BaseEJB {
 
+    private static final int FINAL_REVIEWER_ROLE_ID = 5;
+
+    private static final int AGGREGATOR_ROLE_ID = 4;
+
+    private static final int PRIMARY_SCREENER_ROLE_ID = 2;
+
+    private static final int REVIEWER_ROLE_ID = 3;
+
     private static final int INTERNAL_ADMIN_USER = 100129;
-    
+
     public static final int JAVA_CATALOG_ID = 5801776;
     public static final int DOT_NET_CATALOG_ID = 5801777;
     public static final int CUSTOM_JAVA_CATALOG_ID = 5801778;
     public static final int CUSTOM_DOT_NET_CATALOG_ID = 5801779;
     public static final int FLASH_CATALOG_ID = 8459260;
     public static final int APPLICATIONS_CATALOG_ID = 9926572;
-    public static final int ACTIVE_REVIEWER = 100;    
+    public static final int ACTIVE_REVIEWER = 100;
 
     private IdGen createIDGen(String dataSource) throws CreateException {
         try {
@@ -62,10 +70,19 @@ public class RBoardApplicationBean extends BaseEJB {
                     + prefix + permissionName));
         }
         close(ps);
-        insert(conn, "user_role_xref",
-            new String[]{"user_role_id", "login_id", "role_id"},
-            new String[]{String.valueOf(idGen.nextId()), String.valueOf(userId), 
-            String.valueOf(roleId)});
+        long userRoleId = idGen.nextId();
+        try {
+            selectLong(conn, "user_role_xref",
+                    "user_role_id",
+                    new String[]{"user_role_id", "login_id", "role_id"},
+                    new String[]{String.valueOf(userRoleId), String.valueOf(userId),
+                    String.valueOf(roleId)});
+        } catch (RowNotFoundException e) {
+            insert(conn, "user_role_xref",
+                new String[]{"user_role_id", "login_id", "role_id"},
+                new String[]{String.valueOf(userRoleId), String.valueOf(userId),
+                String.valueOf(roleId)});
+        }
     }
 
     /**
@@ -126,15 +143,18 @@ public class RBoardApplicationBean extends BaseEJB {
      * @throws SQLException
      * @throws RemoteException
      */
-    private void insertUserRole(Connection conn, IdGen idGen, long userId,
+    private void insertUserRole(Connection conn, IdGen idGen, long rUserRoleVId, long userId,
         long projectId, int reviewRespId, long rUserRoleId, long rRoleId,
         long paymentInfoId) throws SQLException, RemoteException {
+        // Resets current version
+        resetCurrentVersion(conn, rUserRoleVId);
+
         insert(conn, "r_user_role",
             new String[]{"r_user_role_v_id", "r_user_role_id", "r_role_id", "project_id",
             "login_id", "payment_info_id", "r_resp_id", "modify_user", "cur_version"},
-            new String[]{String.valueOf(idGen.nextId()), String.valueOf(rUserRoleId), 
-            String.valueOf(rRoleId), String.valueOf(projectId), String.valueOf(userId), 
-            String.valueOf(paymentInfoId), String.valueOf(reviewRespId), 
+            new String[]{String.valueOf(idGen.nextId()), String.valueOf(rUserRoleId),
+            String.valueOf(rRoleId), String.valueOf(projectId), String.valueOf(userId),
+            String.valueOf(paymentInfoId), String.valueOf(reviewRespId),
             String.valueOf(INTERNAL_ADMIN_USER), "1"});
     }
 
@@ -146,9 +166,9 @@ public class RBoardApplicationBean extends BaseEJB {
      */
     private void resetCurrentVersion(Connection conn, long rUserRoleVId) {
         try {
-            
+
             System.out.println("update r_user_role : " + rUserRoleVId);
-            
+
             update(conn, "r_user_role",
                 new String[]{"cur_version"}, new String[]{"0"},
                 new String[]{"r_user_role_v_id"},
@@ -158,9 +178,9 @@ public class RBoardApplicationBean extends BaseEJB {
     }
 
     public void createRBoardApplication(String dataSource, long userId,
-            long projectId, int reviewRespId, int phaseId, Timestamp opensOn, 
+            long projectId, int reviewRespId, int phaseId, Timestamp opensOn,
             int reviewTypeId, boolean primary) throws RemoteException{
-        
+
         IdGen idGen = null;
         try {
             idGen = createIDGen(dataSource);
@@ -173,11 +193,12 @@ public class RBoardApplicationBean extends BaseEJB {
 
         try {
             conn = DBMS.getConnection(dataSource);
-            
+
             // gets project info
             Map projectInfo = getProjectInfo(projectId, conn);
+            String prefix = buildPrefix(projectInfo);
 
-            // gets UserRole info
+            // gets UserRole info (First reviewer)
             ps = conn.prepareStatement("SELECT * FROM r_user_role "
                     + "WHERE project_id = ? and login_id is null "
                     + "and cur_version = 1");
@@ -192,32 +213,38 @@ public class RBoardApplicationBean extends BaseEJB {
 
             insert(conn, "rboard_application",
                     new String[]{"user_id", "project_id", "phase_id", "review_resp_id", "primary_ind"},
-                    new String[]{String.valueOf(userId), String.valueOf(projectId), 
-                        String.valueOf(phaseId), String.valueOf(reviewRespId), 
+                    new String[]{String.valueOf(userId), String.valueOf(projectId),
+                        String.valueOf(phaseId), String.valueOf(reviewRespId),
                         String.valueOf(primary ? 1 : 0)});
 
-            if (rs.next()) {
+            boolean reviewerInserted = false;
+            while (rs.next()) {
                 long rUserRoleVId = rs.getLong("r_user_role_v_id");
                 long rUserRoleId = rs.getLong("r_user_role_id");
                 long rRoleId = rs.getLong("r_role_id");
                 long paymentInfoId = rs.getLong("payment_info_id");
 
-                // Resets current version
-                resetCurrentVersion(conn, rUserRoleVId);
+                if (rRoleId == REVIEWER_ROLE_ID && !reviewerInserted) {
+                    // insert new UserRole
+                    insertUserRole(conn, idGen, rUserRoleVId, userId, projectId, reviewRespId,
+                            rUserRoleId, rRoleId, paymentInfoId);
 
-                // insert new UserRole
-                insertUserRole(conn, idGen, userId, projectId, reviewRespId,
+                    // create permissions.
+                    createPermission(conn, idGen, "Review " + projectId, prefix, userId);
+                    createPermission(conn, idGen, "View Project " + projectId, prefix, userId);
+                    createPermission(conn, idGen, "ForumUser "
+                            + String.valueOf(projectInfo.get("forumId")), "", userId);
+
+                    reviewerInserted = true;
+                } else if (primary && (rRoleId == PRIMARY_SCREENER_ROLE_ID ||
+                        rRoleId == AGGREGATOR_ROLE_ID || rRoleId == FINAL_REVIEWER_ROLE_ID)) {
+                    // insert new UserRole
+                    insertUserRole(conn, idGen, rUserRoleVId, userId, projectId, reviewRespId,
                         rUserRoleId, rRoleId, paymentInfoId);
-
-                // create permissions.
-                String prefix = buildPrefix(projectInfo);
-                createPermission(conn, idGen, "Review " + projectId, prefix, userId);
-                createPermission(conn, idGen, "View Project " + projectId, prefix, userId);
-                createPermission(conn, idGen, "ForumUser "
-                        + String.valueOf(projectInfo.get("forumId")), "", userId);
-            } else {
-                throw (new EJBException("Couldn't find UserRole rows for pid:"
-                        + projectId));
+                }
+            }
+            if (!reviewerInserted) {
+                throw (new EJBException("Couldn't find UserRole rows for pid:" + projectId));
             }
             System.out.println("Commit Transaction");
             conn.commit();
@@ -260,7 +287,7 @@ public class RBoardApplicationBean extends BaseEJB {
                 new String[]{String.valueOf(projectId)},
                 dataSource);
     }
-    
+
     public Timestamp getLatestReviewApplicationTimestamp(Connection conn, long userId) {
         StringBuffer query = new StringBuffer(200);
         query.append("select create_date from rboard_application where user_id = ?");
@@ -307,13 +334,13 @@ public class RBoardApplicationBean extends BaseEJB {
      * @param reviewTypeId
      * @throws RemoteException
      */
-    public void validateUser(String dataSource, int catalog, int reviewTypeId, long userId, 
+    public void validateUser(String dataSource, int catalog, int reviewTypeId, long userId,
             int phaseId) throws RemoteException {
         Connection conn = null;
 
         try {
             conn = DBMS.getConnection(dataSource);
-        
+
             Map reviewRespMap = null;
             try {
                 reviewRespMap = getReviewRespInfo(dataSource);
@@ -321,23 +348,23 @@ public class RBoardApplicationBean extends BaseEJB {
                 DBMS.printSqlException(true, e);
                 throw new EJBException(e.getMessage());
             }
-    
+
             int status = 0;
             try {
                 status = getStatus(conn, userId, phaseId);
             } catch (RowNotFoundException rnfe) {
                 throw new RemoteException("Sorry, you are not a reviewer.  Please contact TopCoder if you would like to become one.");
             }
-    
+
             if (status != ACTIVE_REVIEWER) {
                 throw new RemoteException("Sorry, you are not authorized to perform reviews at this time.");
             }
-    
+
             if (!reviewRespMap.containsKey(new Integer(reviewTypeId)) ||
                     !reviewRespMap.get(new Integer(reviewTypeId)).equals(new Integer(phaseId))) {
                 throw new RemoteException("Invalid request, incorrect review position specified.");
             }
-    
+
             try {
                 if (catalog == JAVA_CATALOG_ID || catalog == CUSTOM_JAVA_CATALOG_ID) {
                     if (!canReviewJava(conn, userId, phaseId)) {
@@ -403,14 +430,14 @@ public class RBoardApplicationBean extends BaseEJB {
             close(conn);
         }
     }
-    
+
     /**
      * @param opensOn
      * @param reviewTypeId
      * @throws RemoteException
      */
     private void validateUserTrans(Connection conn, long projectId, int phaseId, long userId, Timestamp opensOn, int reviewTypeId, boolean primary) throws RemoteException {
-        
+
         if (exists(conn, userId, projectId, phaseId)) {
             throw new RemoteException("You have already applied to review this project.");
         }
@@ -484,7 +511,7 @@ public class RBoardApplicationBean extends BaseEJB {
             }
         }
     }
-    
+
     /**
      * @param projectId
      * @param conn
@@ -502,9 +529,9 @@ public class RBoardApplicationBean extends BaseEJB {
         ps = conn.prepareStatement("select review_resp_id, review_resp_name, phase_id " +
                 "from review_resp");
         rs = ps.executeQuery();
-        
+
         while (rs.next()) {
-            returnMap.put(new Integer(rs.getInt("review_resp_id")), 
+            returnMap.put(new Integer(rs.getInt("review_resp_id")),
                 new Integer(rs.getInt("phase_id")));
         }
         close(rs);
@@ -513,8 +540,8 @@ public class RBoardApplicationBean extends BaseEJB {
 
         return returnMap;
     }
-    
-    
+
+
     private int getStatus(Connection conn, long userId, int phaseId) {
         return selectInt(conn,
                 "rboard_user",
