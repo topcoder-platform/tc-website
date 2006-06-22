c7ce485bb5ad091584f2013ab11f5f494a6dc4a5
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-06-22 20:54:03 +0000

    implement the new stuff for known users

--- web_module/trunk/src/main/com/topcoder/dde/util/TCSAuthentication.java
+++ web_module/trunk/src/main/com/topcoder/dde/util/TCSAuthentication.java
@@ -10,21 +10,23 @@ import com.topcoder.web.common.security.WebAuthentication;
 
 import javax.naming.InitialContext;
 import javax.rmi.PortableRemoteObject;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * An authentication implementation for the TCS site.  It doesn't provide actual login facilities
  * but makes it possible to emulate the regular MVC architecture on the TCS site.
- * @author  dok
- * @version  $Revision$ $Date$
- * Create Date: Feb 4, 2005
+ *
+ * @author dok
+ * @version $Revision$ $Date$
+ *          Create Date: Feb 4, 2005
  */
 public class TCSAuthentication implements WebAuthentication {
 
     private HttpServletRequest request;
     private HttpServletResponse response;
-    private static final User GUEST= SimpleUser.createGuest();
+    private static final User GUEST = SimpleUser.createGuest();
 
 
     public TCSAuthentication(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -43,7 +45,7 @@ public class TCSAuthentication implements WebAuthentication {
                 UserManagerRemoteHome userManagerHome = (UserManagerRemoteHome) PortableRemoteObject.narrow(objUserManager, UserManagerRemoteHome.class);
                 UserManagerRemote userManager = userManagerHome.create();
                 TCSubject sub = Cookies.getUserFromLoginCookies(request, response, userManager);
-                if (sub!=null) {
+                if (sub != null) {
                     tcsUser = userManager.getUser(sub.getUserId());
                     request.getSession().setAttribute("TCUSER", tcsUser);
                 }
@@ -55,20 +57,24 @@ public class TCSAuthentication implements WebAuthentication {
         if (tcsUser == null) {
             ret = GUEST;
         } else {
+            markKnownUser();
             ret = new SimpleUser(tcsUser.getId(), tcsUser.getRegInfo().getUsername(),
                     tcsUser.getRegInfo().getPassword());
         }
         return ret;
     }
 
-    /** Get the details of the logged in user if any; otherwise return an anonymous user. */
+    /**
+     * Get the details of the logged in user if any; otherwise return an anonymous user.
+     */
     public User getUser() {
-        User ret= null;
+        User ret = null;
         com.topcoder.dde.user.User tcsUser =
                 (com.topcoder.dde.user.User) request.getSession().getAttribute("TCUSER");
         if (tcsUser == null) {
             ret = GUEST;
         } else {
+            markKnownUser();
             ret = new SimpleUser(tcsUser.getId(), tcsUser.getRegInfo().getUsername(),
                     tcsUser.getRegInfo().getPassword());
         }
@@ -77,10 +83,12 @@ public class TCSAuthentication implements WebAuthentication {
 
     /**
      * Attempt to log in with the given user.
+     *
      * @param u
      * @param rememberUser whether or not the user
-     * should be recognized the next time they log in (getActiveUser())
+     *                     should be recognized the next time they log in (getActiveUser())
      * @throws com.topcoder.shared.security.LoginException
+     *
      */
     public void login(User u, boolean rememberUser) throws LoginException {
         throw new RuntimeException("Not implemented");
@@ -89,6 +97,7 @@ public class TCSAuthentication implements WebAuthentication {
     public void login(User u) throws LoginException {
         throw new RuntimeException("Not implemented");
     }
+
     public void login(User u, boolean rememberUser, String dataSource) throws LoginException {
         throw new RuntimeException("Not implemented");
     }
@@ -97,10 +106,26 @@ public class TCSAuthentication implements WebAuthentication {
         throw new RuntimeException("Not implemented");
     }
 
-    /** Log out, if currently considered logged in. */
+    /**
+     * Log out, if currently considered logged in.
+     */
     public void logout() {
         throw new RuntimeException("Not implemented");
     }
 
+    private void markKnownUser() {
+        Cookie c = new Cookie(KNOWN_USER, String.valueOf(true));
+        c.setMaxAge(Integer.MAX_VALUE);
+        response.addCookie(c);
+    }
+
+    public boolean isKnownUser() {
+        Cookie[] ca = request.getCookies();
+        boolean found = false;
+        for (int i = 0; ca != null && i < ca.length && !found; i++) {
+            found = KNOWN_USER.equals(ca[i].getName());
+        }
+        return found;
+    }
 
 }
