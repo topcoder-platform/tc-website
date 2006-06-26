50aa5a0e25352bfc8f991759ba9406f989e57868
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-06-26 18:53:31 +0000

    no message

--- web_module/branches/pacts_merge_06_26_2006/src/main/com/topcoder/web/tc/controller/legacy/pacts/servlet/PactsMemberServlet.java
+++ web_module/branches/pacts_merge_06_26_2006/src/main/com/topcoder/web/tc/controller/legacy/pacts/servlet/PactsMemberServlet.java
@@ -30,7 +30,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 public class PactsMemberServlet extends BaseServlet implements PactsConstants {
