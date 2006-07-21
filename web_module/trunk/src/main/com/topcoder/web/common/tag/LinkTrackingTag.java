5d76a0a2729e3f91524cc4f6f756fda9565df504
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-07-21 19:07:26 +0000

    add

---
+++ web_module/trunk/src/main/com/topcoder/web/common/tag/LinkTrackingTag.java
@@ -0,0 +1,57 @@
+package com.topcoder.web.common.tag;
+
+import com.topcoder.shared.util.ApplicationServer;
+import com.topcoder.web.common.StringUtils;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+import java.io.IOException;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Jul 21, 2006
+ */
+public class LinkTrackingTag extends TagSupport {
+    private String link = "";
+    private String refer = "";
+    private String cssclass = "";
+
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setCSSClass(String cssclass) {
+        this.cssclass = cssclass;
+    }
+
+    public void setRefer(String refer) {
+        this.refer = refer;
+    }
+
+    public int doStartTag() throws JspException {
+        StringBuffer buf = new StringBuffer(100);
+        buf.append(ApplicationServer.SERVER_NAME);
+        buf.append("/tc?module=LinkTracking&link=");
+        buf.append(StringUtils.replace(link, "&", "%26"));
+        buf.append("&refer=").append(refer);
+        try {
+            pageContext.getOut().print(buf.toString());
+        } catch (IOException e) {
+            throw new JspException(e);
+        }
+        return SKIP_BODY;
+    }
+
+
+    public int doEndTag() throws JspException {
+        this.link = "";
+        this.refer = "";
+        this.cssclass = "";
+        return super.doEndTag();
+    }
+
+
+}
+
