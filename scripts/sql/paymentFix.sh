405e7caee7ad2d154956b7a470d12bcf6dda75dc
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-07-24 19:16:53 +0000

    *** empty log message ***

--- scripts/sql/paymentFix.sh
+++ scripts/sql/paymentFix.sh
@@ -1,4 +1,4 @@
 CP=../../resources:../../build/classes:$CLASSPATH:../../lib/jars/log4j-1.2.7.jar:../../lib/jars/log4j-boot.jar:../../lib/jars/ifxjdbc.jar
 
-nohup java  -cp $CP com.topcoder.shared.util.sql.DBUtilityLauncher com.topcoder.shared.util.sql.PaymentFixUtility -xmlfile paymentFix.xml $@ > ./nohup.out 2>&1 &
+nohup java  -cp $CP com.topcoder.shared.util.sql.DBUtilityLauncher com.topcoder.utilities.sql.PaymentFixUtility -xmlfile paymentFix.xml $@ > ./nohup.out 2>&1 &
 tail -f nohup.out
