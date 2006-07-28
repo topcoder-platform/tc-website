b801903bc603edf05a0fc3e9a075c5975a9e7e40
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-07-28 13:57:01 +0000

    *** empty log message ***

--- web_module/branches/public_forums_2006_07_14/src/main/com/topcoder/web/common/model/FixedPriceComponent.java
+++ web_module/branches/public_forums_2006_07_14/src/main/com/topcoder/web/common/model/FixedPriceComponent.java
@@ -7,30 +7,42 @@ import com.topcoder.shared.util.logging.Logger;
 
 /**
  * <strong>Purpose</strong>:
- * Calculates and resizes Reviewers tasks costs
+ * Calculates and resizes Reviewers tasks costs based on default values
+ * adjusted with fixed values.
  *
  * @author pulky
  * @version 1.0.0
  */
 public class FixedPriceComponent extends DefaultPriceComponent {
+    /**
+     * The logger to log to.
+     */
     private static Logger log = Logger.getLogger(FixedPriceComponent.class);
 
+    /**
+     * Stores the primary fixed payment used to resize primary payments.
+     */
     private float primaryFixedPayment = 0;
 
+    /**
+     * Stores the secondary fixed payment used to resize secondary payments.
+     */
     private float secondaryFixedPayment = 0;
 
+    /**
+     * Stores the adjustment factor for primary payments.
+     */
     private float primaryAdjustmentFactor = 1;
 
+    /**
+     * Default constructor
+     */
     protected FixedPriceComponent() {
     }
 
-    public FixedPriceComponent(int levelId, int submissionCount, int submissionsPassedScreening, int phaseId)  {
-        super(levelId, submissionCount, submissionsPassedScreening, phaseId);
-
-        log.debug("level: " + levelId + " submissionCount: " + submissionCount + " submissionPassedScreening: " +
-                submissionsPassedScreening + " phaseId: " + phaseId);
-    }
-
+    /**
+     * Constructor compatible with DefaultPriceComponent
+     */
     public FixedPriceComponent(int levelId, int submissionCount, int submissionsPassedScreening, int phaseId,
            float primaryFixedPayment, float secondaryFixedPayment)  {
         super(levelId, submissionCount, submissionsPassedScreening, phaseId);
@@ -42,30 +54,83 @@ public class FixedPriceComponent extends DefaultPriceComponent {
                 submissionsPassedScreening + " phaseId: " + phaseId + " primaryFixedPayment: " + primaryFixedPayment + " secondaryFixedPayment: " + secondaryFixedPayment);
     }
 
+    /**
+     * Gets Review Price
+     *
+     * @return The fixed value if exists or delegates to super.
+     * @see DefaultPriceComponent
+     */
     public float getReviewPrice() {
         return Math.round(secondaryFixedPayment > 0 ? secondaryFixedPayment : super.getReviewPrice());
     }
 
+    /**
+     * Gets Aggregation Cost
+     *
+     * Delegates to super and adjusts with the primary adjustment factor calculated using the fixed amounts.
+     *
+     * @return resized Aggregation cost.
+     * @see DefaultPriceComponent
+     */
     public float getAggregationCost() {
         return Math.round(super.getAggregationCost() * primaryAdjustmentFactor);
     }
 
+    /**
+     * Gets Screening Cost
+     *
+     * Delegates to super and adjusts with the primary adjustment factor calculated using the fixed amounts.
+     *
+     * @return resized Screening cost.
+     * @see DefaultPriceComponent
+     */
     public float getScreeningCost() {
         return Math.round(super.getScreeningCost() * primaryAdjustmentFactor);
     }
 
+    /**
+     * Gets Final Review Cost
+     *
+     * Delegates to super and adjusts with the primary adjustment factor calculated using the fixed amounts.
+     *
+     * @return resized Final Review cost.
+     * @see DefaultPriceComponent
+     */
     public float getFinalReviewCost() {
         return Math.round(super.getFinalReviewCost() * primaryAdjustmentFactor);
     }
 
+    /**
+     * Gets Core Review Cost
+     *
+     * Delegates to super and adjusts with the primary adjustment factor calculated using the fixed amounts.
+     *
+     * @return resized Core Review cost.
+     * @see DefaultPriceComponent
+     */
     public float getCoreReviewCost() {
         return Math.round(super.getCoreReviewCost() * primaryAdjustmentFactor);
     }
 
+    /**
+     * Gets Primary Review Cost
+     *
+     * Delegates to super and adjusts with the primary adjustment factor calculated using the fixed amounts.
+     *
+     * @return resized Primary Review cost.
+     * @see DefaultPriceComponent
+     */
     public float getPrimaryReviewPrice() {
         return Math.round(primaryFixedPayment > 0 ? primaryFixedPayment : super.getPrimaryReviewPrice());
     }
 
+    /**
+     * Setter for Primary payment fixed amount
+     *
+     * Also calculates the adjustmente factor.
+     *
+     * @param amount The primary payment fixed amount
+     */
     private void setPrimaryFixedPayment(float amount) {
         this.primaryFixedPayment = amount;
 
@@ -77,10 +142,18 @@ public class FixedPriceComponent extends DefaultPriceComponent {
         this.primaryAdjustmentFactor = amount > 0 ? (amount / oldTotal) : 1;
     }
 
+    /**
+     * Setter for Secondary payment fixed amount
+     *
+     * @param amount The Secondary payment fixed amount
+     */
     private void setSecondaryFixedPayment(float amount) {
         this.secondaryFixedPayment = amount;
     }
 
+    /**
+     * Command line utility
+     */
     private static void showPayments(int submissionCount, int submissionsPassedScreening, int phaseId,
             float primaryFixedPayment, float secondaryFixedPayment) {
 
@@ -106,6 +179,9 @@ public class FixedPriceComponent extends DefaultPriceComponent {
         System.out.println("------------------------------------------------------------");
     }
 
+    /**
+     * Command line utility
+     */
     public static void main(String[] args) {
         if (args.length != 5) {
             System.out.println("usage: java " + FixedPriceComponent.class.toString() + " " +
