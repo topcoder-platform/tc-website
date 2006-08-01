a2f733ff5a800bc1cf4737e66675d17a8891cc14
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-01 19:46:13 +0000

    add update test

--- web_module/branches/creative_2006_06_19/src/testcases/com/topcoder/web/creative/dao/ContestDAOTestCase.java
+++ web_module/branches/creative_2006_06_19/src/testcases/com/topcoder/web/creative/dao/ContestDAOTestCase.java
@@ -32,6 +32,62 @@ public class ContestDAOTestCase extends TCHibernateTestCase {
         assertTrue("did not create contst", c1 != null);
     }
 
+
+    public void testUpdate() {
+        Contest c = new Contest();
+        c.setName("gp contest " + System.currentTimeMillis());
+        c.setStartTime(new Timestamp(new Date().getTime()));
+        c.setEndTime(new Timestamp(c.getStartTime().getTime() + 1000 * 60 * 60 * 24 * 20));
+
+        ContestConfig conf1 = new ContestConfig();
+        conf1.setContest(c);
+        conf1.setProperty(CreativeDAOUtil.getFactory().getContestPropertyDAO().find(ContestProperty.CONTEST_OVERVIEW_TEXT));
+        conf1.setValue("this is my contest description");
+        conf1.getId().setContest(c);
+        conf1.getId().setProperty(conf1.getProperty());
+
+        ContestConfig conf2 = new ContestConfig();
+        conf2.setContest(c);
+        conf2.setProperty(CreativeDAOUtil.getFactory().getContestPropertyDAO().find(ContestProperty.PRIZE_DESCRIPTION));
+        conf2.setValue("this is my prize description");
+        conf2.getId().setContest(c);
+        conf2.getId().setProperty(conf2.getProperty());
+
+        c.addConfig(conf1);
+        c.addConfig(conf2);
+
+        ContestPrize cp1 = new ContestPrize();
+        cp1.setAmount(new Float("100"));
+        cp1.setPlace(new Integer(1));
+        cp1.setContest(c);
+        cp1.getId().setContest(c);
+        cp1.getId().setPlace(new Integer(1));
+
+        ContestPrize cp2 = new ContestPrize();
+        cp2.setAmount(new Float("50"));
+        cp2.setPlace(new Integer(2));
+        cp2.setContest(c);
+        cp2.getId().setContest(c);
+        cp2.getId().setPlace(new Integer(2));
+
+        c.addPrize(cp1);
+        c.addPrize(cp2);
+
+
+        CreativeDAOUtil.getFactory().getContestDAO().saveOrUpdate(c);
+        tearDown();
+        setUp();
+        Contest c1 = CreativeDAOUtil.getFactory().getContestDAO().find(c.getId());
+
+        ContestConfig pConfig = c1.getPrizeDescription();
+        pConfig.setValue("new desc");
+        CreativeDAOUtil.getFactory().getContestDAO().saveOrUpdate(c1);
+        tearDown();
+        setUp();
+        Contest c2 = CreativeDAOUtil.getFactory().getContestDAO().find(c.getId());
+        assertTrue("update failed", "new desc".equals(c2.getPrizeDescription().getValue()));
+    }
+
     public void testSaveWithDetails() {
         Contest c = new Contest();
         c.setName("gp contest " + System.currentTimeMillis());
