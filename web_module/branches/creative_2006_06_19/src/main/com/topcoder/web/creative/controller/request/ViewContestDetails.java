97b9395b6dd3b1941918ade3a8532a2ffce55a78
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-01 22:19:28 +0000

    putthe contest in the request and change the dest page

--- web_module/branches/creative_2006_06_19/src/main/com/topcoder/web/creative/controller/request/ViewContestDetails.java
+++ web_module/branches/creative_2006_06_19/src/main/com/topcoder/web/creative/controller/request/ViewContestDetails.java
@@ -1,6 +1,11 @@
 package com.topcoder.web.creative.controller.request;
 
 import com.topcoder.web.common.BaseProcessor;
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.creative.Constants;
+import com.topcoder.web.creative.dao.CreativeDAOUtil;
+import com.topcoder.web.creative.model.Contest;
 
 /**
  * @author dok
@@ -9,7 +14,25 @@ import com.topcoder.web.common.BaseProcessor;
  */
 public class ViewContestDetails extends BaseProcessor {
     protected void businessProcessing() throws Exception {
-        setNextPage("/projectDetails.jsp");
-        setIsNextPageInContext(true);
+        //todo only allow the viewing of active contests..contests with appropriate status for normal users
+        //todo admins can view projects with other statii
+
+        String contestId = getRequest().getParameter(Constants.CONTEST_ID);
+        if ("".equals(StringUtils.checkNull(contestId))) {
+            throw new NavigationException("No contest specified");
+        } else {
+            Long cid;
+            try {
+                cid = new Long(contestId);
+            } catch (NumberFormatException e) {
+                throw new NavigationException("Invalid contest specified");
+            }
+            Contest contest = CreativeDAOUtil.getFactory().getContestDAO().find(cid);
+            getRequest().setAttribute("contest", contest);
+
+            setNextPage("/contestDetails.jsp");
+            setIsNextPageInContext(true);
+        }
+
     }
 }
