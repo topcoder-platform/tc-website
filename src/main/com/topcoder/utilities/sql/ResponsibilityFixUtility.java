e97a47077321c057d126357f055f362d42139572
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-03 15:27:56 +0000

    *** empty log message ***

--- src/main/com/topcoder/utilities/sql/ResponsibilityFixUtility.java
+++ src/main/com/topcoder/utilities/sql/ResponsibilityFixUtility.java
@@ -48,7 +48,7 @@ public class ResponsibilityFixUtility extends DBUtility {
             query = new StringBuffer(200);
             query.append("select project_id, r_user_role_v_id, r_resp_id, login_id from r_user_role where  ");
             query.append("r_role_id = 3 and cur_version = 1 and (r_resp_id is null or r_resp_id in (4, 5, 6)) ");
-            query.append("where modify_date > ? ");
+            query.append("and modify_date > ? ");
             query.append("order by project_id");
             psSel = prepareStatement("tcs_catalog", query.toString());
             psSel.setString(1, startDate);
