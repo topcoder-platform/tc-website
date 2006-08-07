abac74c80ce15bcc1adf386f508707a611b25f34
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-07 19:03:20 +0000

    some email changes

--- web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/google06/Submit.java
+++ web_module/trunk/src/main/com/topcoder/web/privatelabel/controller/request/google06/Submit.java
@@ -93,12 +93,12 @@ public class Submit extends ResumeRegSubmit {
                 buf.append("Your handle and password will provide you with access to the Competition Arena, where you can practice and chat, and where you'll compete in Google Code Jam.\n\n");
 
                 buf.append("PRACTICING FOR THE EVENT\n");
-                buf.append("There is a practice room available to you that will allow you to become acclimated with the competition environment before you participate in Google Code Jam.  The practice room contains a problem set that will be very similar in nature to the problems you will encounter during competition.  The practice room will be available from 8:00AM GMT on May 1, 2006 through 4:00PM GMT on June 29, 2006.\n\n");
+                buf.append("There is a practice room available to you that will allow you to become acclimated with the competition environment before you participate in Google Code Jam.  The practice room contains a problem set that will be very similar in nature to the problems you will encounter during competition.  The practice room will be available from 9:00AM EDT (GMT/UTC 04) on August 14, 2006 through 4:00PM EDT (GMT/UTC -4) on October 27, 2006.\n\n");
 
                 buf.append("You can access the practice rooms by navigating to http://").append(getSessionInfo().getServerName()).append("/googlecodejam and clicking on the Competition Arena tab.\n\n");
 
                 buf.append("COMPETING IN THE QUALIFICATION ROUND\n");
-                buf.append("You may participate in the Qualification Round at any time between Tuesday, May 23 at 11:00AM GMT and Wednesday, May 24 at 11:00AM GMT.  You will have up to one hour to solve two problems.  It is recommended that you utilize the practice area prior to participating in the qualification.\n\n");
+                buf.append("You may participate in the Qualification Round at any time between Tuesday, September 5 at 12 noon EDT (GMT/UTC -4) and Wednesday, September 6 at 12 noon EDT (GMT/UTC -4).  You will have up to one hour to solve two problems.  It is recommended that you utilize the practice area prior to participating in the qualification.\n\n");
 
                 buf.append("LAUNCHING THE ARENA\n");
                 buf.append("You can launch and login to the competition arena at the appropriate time by navigating to http://").append(getSessionInfo().getServerName()).append("/googlecodejam and clicking on the Competition Arena tab.\n\n");
@@ -109,7 +109,7 @@ public class Submit extends ResumeRegSubmit {
 
                 buf.append("If you have any questions about how to participate, please email them to gcj@topcoder.com.\n\n");
 
-                buf.append("Thank you for registering for Google Code Jam.  We look forward to seeing you in the Arena!");
+                buf.append("Thank you for registering for Google Code Jam 2006.  We look forward to seeing you in the Arena!");
 
                 mail.setBody(buf.toString());
                 mail.addToAddress(info.getEmail(), TCSEmailMessage.TO);
