06a75226c3f3fffaf36e8b80d7b41a103fde1013
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-07 15:32:59 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/tc/controller/request/report/ProfileSearch.java
+++ src/main/com/topcoder/web/tc/controller/request/report/ProfileSearch.java
@@ -535,9 +535,10 @@ public class ProfileSearch extends Base {
             query.append("  and phase_id = 112 ");
             query.append("  and u.user_id = c.coder_id) > current - " + maxDaysDes + " units day ");
             */
+            query.append("  and ur_des.user_id = u.user_id");
             query.append("  and ur_des.last_rated_project_id = p_des.project_id");
             query.append("  and p_des.cur_version=1 ");
-            query.append("  and ur_des.phase_id = 112 ");
+            query.append("  and p_des.phase_id = 112 ");
             query.append(" AND ur_des.rating_date > (current - " + maxDaysDes + " units day) ");
 
         }
@@ -553,9 +554,10 @@ public class ProfileSearch extends Base {
             query.append("  and phase_id = 113 ");
             query.append("  and u.user_id = c.coder_id) > current - " + maxDaysDev + " units day ");*/
 
+            query.append("  and ur_dev.user_id = u.user_id");
             query.append("  and ur_dev.last_rated_project_id = p_dev.project_id");
             query.append("  and p_dev.cur_version=1 ");
-            query.append("  and ur_dev.phase_id = 113 ");
+            query.append("  and p_dev.phase_id = 113 ");
             query.append(" AND ur_dev.rating_date > (current - " + maxDaysDev + " units day) ");
 
         }
