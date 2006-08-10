441f5d1af91f1c4c4f1cc49260af87bf80933246
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-10 23:38:04 +0000

    add some more security

--- src/main/com/topcoder/web/studio/controller/request/Register.java
+++ src/main/com/topcoder/web/studio/controller/request/Register.java
@@ -1,6 +1,8 @@
 package com.topcoder.web.studio.controller.request;
 
+import com.topcoder.shared.security.ClassResource;
 import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.common.PermissionException;
 import com.topcoder.web.common.ShortHibernateProcessor;
 import com.topcoder.web.common.dao.DAOFactory;
 import com.topcoder.web.common.dao.DAOUtil;
@@ -18,52 +20,57 @@ import com.topcoder.web.studio.model.ContestRegistration;
  */
 public class Register extends ShortHibernateProcessor {
     protected void dbProcessing() throws Exception {
-        if ("POST".equals(getRequest().getMethod())) {
-            Long contestId;
-            try {
-                contestId = new Long(getRequest().getParameter(Constants.CONTEST_ID));
-            } catch (NumberFormatException e) {
-                throw new NavigationException("Invalid Contest Specified");
-            }
-
-            StudioDAOFactory cFactory = StudioDAOUtil.getFactory();
-            DAOFactory factory = DAOUtil.getFactory();
-
-            Contest c = cFactory.getContestDAO().find(contestId);
-            User u = factory.getUserDAO().find(new Long(getUser().getId()));
-
-            if (cFactory.getContestRegistrationDAO().find(c, u) == null) {
-                if ("on".equals(getRequest().getParameter(Constants.TERMS_AGREE))) {
-
-                    ContestRegistration cr = new ContestRegistration();
-                    cr.setContest(c);
-                    cr.setUser(u);
-                    cr.setTerms(DAOUtil.getFactory().getTermsOfUse().find(new Integer(Constants.CONTEST_TERMS_OF_USE_ID)));
-                    cr.getId().setContest(c);
-                    cr.getId().setUser(u);
-
-                    StudioDAOUtil.getFactory().getContestRegistrationDAO().saveOrUpdate(cr);
-
-                    markForCommit();
-                } else {
-                    addError(Constants.TERMS_AGREE, "You must agree to the terms in order to continue.");
+        if (userLoggedIn()) {
+            if ("POST".equals(getRequest().getMethod())) {
+                Long contestId;
+                try {
+                    contestId = new Long(getRequest().getParameter(Constants.CONTEST_ID));
+                } catch (NumberFormatException e) {
+                    throw new NavigationException("Invalid Contest Specified");
                 }
-            }
 
-            if (hasErrors()) {
-                setDefault(Constants.CONTEST_ID, contestId.toString());
-                getRequest().setAttribute("contest", c);
-                setNextPage("/contestReg.jsp");
-                setIsNextPageInContext(true);
+                StudioDAOFactory cFactory = StudioDAOUtil.getFactory();
+                DAOFactory factory = DAOUtil.getFactory();
+
+                Contest c = cFactory.getContestDAO().find(contestId);
+                User u = factory.getUserDAO().find(new Long(getUser().getId()));
+
+                if (cFactory.getContestRegistrationDAO().find(c, u) == null) {
+                    if ("on".equals(getRequest().getParameter(Constants.TERMS_AGREE))) {
+
+                        ContestRegistration cr = new ContestRegistration();
+                        cr.setContest(c);
+                        cr.setUser(u);
+                        cr.setTerms(DAOUtil.getFactory().getTermsOfUse().find(new Integer(Constants.CONTEST_TERMS_OF_USE_ID)));
+                        cr.getId().setContest(c);
+                        cr.getId().setUser(u);
+
+                        StudioDAOUtil.getFactory().getContestRegistrationDAO().saveOrUpdate(cr);
+
+                        markForCommit();
+                    } else {
+                        addError(Constants.TERMS_AGREE, "You must agree to the terms in order to continue.");
+                    }
+                }
+
+                if (hasErrors()) {
+                    setDefault(Constants.CONTEST_ID, contestId.toString());
+                    getRequest().setAttribute("contest", c);
+                    setNextPage("/contestReg.jsp");
+                    setIsNextPageInContext(true);
+                } else {
+                    setDefault(Constants.CONTEST_ID, contestId.toString());
+                    getRequest().setAttribute("contest", c);
+                    setNextPage("/submit.jsp");
+                    setIsNextPageInContext(true);
+                }
+
             } else {
-                setDefault(Constants.CONTEST_ID, contestId.toString());
-                getRequest().setAttribute("contest", c);
-                setNextPage("/submit.jsp");
-                setIsNextPageInContext(true);
+                throw new NavigationException("Invalid request type.");
             }
 
         } else {
-            throw new NavigationException("Invalid request type.");
+            throw new PermissionException(getUser(), new ClassResource(this.getClass()));
         }
 
     }
