a652a5b5fa707b21f1b1ca61a3718d433298eac1
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-15 19:55:40 +0000

    *** empty log message ***

--- web_module/branches/automatic_application_screening_2006_08_14/src/main/com/topcoder/dde/util/ConstantsInt.java
+++ web_module/branches/automatic_application_screening_2006_08_14/src/main/com/topcoder/dde/util/ConstantsInt.java
@@ -9,4 +9,7 @@ public interface ConstantsInt {
 
     public static final String STATIC_PREFIX = "d";
     public static final String TERMS = "terms";
+
+    public static final String USER_ID = "uid";
+    public static final String MODULE_KEY = "module";
 }
