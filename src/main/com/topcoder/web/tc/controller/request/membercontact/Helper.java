93f2438d5ce28b917853ca43c83e74dee8fa701f
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-17 19:43:51 +0000

    *** empty log message ***

--- src/main/com/topcoder/web/tc/controller/request/membercontact/Helper.java
+++ src/main/com/topcoder/web/tc/controller/request/membercontact/Helper.java
@@ -10,35 +10,35 @@ import com.topcoder.web.tc.Constants;
 
 /**
  * Contains helper methods for this package.
- * 
+ *
  * @author cucu
  * @version $Revision$ Date: 2005/01/01 00:00:00
  *          Create Date: July 21, 2006
  */
 public class Helper {
-	
+
     public static String NOT_RATED = "nr";
 
     /**
      * Returns whether a user is rated in either algorithm or component competitions.
-     * 
+     *
      * @param userId the user to check.
      * @return whether a user is rated in either algorithm or component competitions.
      * @throws Exception
      */
-	public static boolean isRated(long userId) throws Exception {
-		Request r = new Request();
-		r.setContentHandle("coder_all_ratings");
+    public static boolean isRated(long userId) throws Exception {
+        Request r = new Request();
+        r.setContentHandle("coder_all_ratings");
 
-		r.setProperty("cr", userId + "");
+        r.setProperty("cr", userId + "");
 
         DataAccessInt dai = new CachedDataAccess(DBMS.DW_DATASOURCE_NAME);
-		ResultSetContainer ratings = (ResultSetContainer) dai.getData(r).get("coder_all_ratings");
+        ResultSetContainer ratings = (ResultSetContainer) dai.getData(r).get("coder_all_ratings");
 
-		return ratings.getIntItem(0, "algorithm_rating") > 0 ||
-				ratings.getIntItem(0, "design_rating") <= 0 ||
-				ratings.getIntItem(0, "development_rating") <= 0 ||
-				ratings.getIntItem(0, "hs_algorithm_rating") <= 0;
-		}
+        return ratings.getIntItem(0, "algorithm_rating") > 0 ||
+                ratings.getIntItem(0, "design_rating") > 0 ||
+                ratings.getIntItem(0, "development_rating") > 0 ||
+                ratings.getIntItem(0, "hs_algorithm_rating") > 0;
+        }
 
 }
