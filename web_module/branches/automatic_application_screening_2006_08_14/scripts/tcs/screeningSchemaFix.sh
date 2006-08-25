75ae3fc022bcd6f508e90c3b1da9decb66f9b65f
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-25 20:12:45 +0000

    *** empty log message ***

--- web_module/branches/automatic_application_screening_2006_08_14/scripts/tcs/screeningSchemaFix.sh
+++ web_module/branches/automatic_application_screening_2006_08_14/scripts/tcs/screeningSchemaFix.sh
@@ -1,4 +1,4 @@
-CP=../../resources:../../build/classes:$CLASSPATH:../../lib/jars/log4j-1.2.7.jar:../../lib/jars/log4j-boot.jar:../../lib/jars/ifxjdbc.jar:../../lib/jars/tcs/id_generator/3.0/id_generator.jar:../../lib/jars/tcs/base_exception.jar:../../lib/jars/tcs/db_connection_factory/1.0/db_connection_factory.jar:../../lib/jars/tcs/configuration_manager/2.1.3/configuration_manager.jar:../../lib/jars/jboss/xercesImpl.jar:../../resources/automatic_screening
+CP=../../resources:../../build/classes:$CLASSPATH:../../lib/jars/log4j-1.2.7.jar:../../lib/jars/log4j-boot.jar:../../lib/jars/ifxjdbc.jar:../../lib/jars/tcs/id_generator/3.0/id_generator.jar:../../lib/jars/tcs/base_exception.jar:../../lib/jars/tcs/db_connection_factory/1.0/db_connection_factory.jar:../../lib/jars/tcs/configuration_manager/2.1.3/configuration_manager.jar:../../lib/jars/jboss/xercesImpl.jar:../../resources/db_utilities
 
 
 
