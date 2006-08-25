6056a052d06777faedeeb11a512490d071cb877d
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-25 19:00:35 +0000

    *** empty log message ***

--- web_module/branches/automatic_application_screening_2006_08_14/scripts/tcs/screeningSchemaFix.sh
+++ web_module/branches/automatic_application_screening_2006_08_14/scripts/tcs/screeningSchemaFix.sh
@@ -1,4 +1,4 @@
-CP=../../resources:../../build/classes:$CLASSPATH:../../lib/jars/log4j-1.2.7.jar:../../lib/jars/log4j-boot.jar:../../lib/jars/ifxjdbc.jar:../../lib/jars/tcs/id_generator/3.0/id_generator.jar
+CP=../../resources:../../build/classes:$CLASSPATH:../../lib/jars/log4j-1.2.7.jar:../../lib/jars/log4j-boot.jar:../../lib/jars/ifxjdbc.jar:../../lib/jars/tcs/id_generator/3.0/id_generator.jar:../../lib/jars/tcs/base_exception.jar
 
 nohup java  -cp $CP com.topcoder.shared.util.sql.DBUtilityLauncher com.topcoder.utilities.tcs.ScreeningSchemaFixUtility -xmlfile screeningSchemaFix.xml $@ > ./nohup.out 2>&1 &
 tail -f nohup.out
