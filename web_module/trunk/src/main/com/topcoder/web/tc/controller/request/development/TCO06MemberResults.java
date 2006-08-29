ac4913376d09971abc1c52619e5d8ed0be9947fe
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-29 21:07:01 +0000

    fix index

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/development/TCO06MemberResults.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/development/TCO06MemberResults.java
@@ -97,7 +97,7 @@ public class TCO06MemberResults extends StatBase {
                     arr.add(new UserContestResult(rsc.getStringItem(i, "component_name"), pts,
                             place, score,
                             rsc.getIntItem(i, "component_id"), prz, sub, bComplete,
-                            rscDetails.getLongItem(j, "project_id"), rscDetails.getIntItem(i, "viewable_category_ind") == 1));
+                            rscDetails.getLongItem(j, "project_id"), rscDetails.getIntItem(j, "viewable_category_ind") == 1));
                 }
             }
         }
