5c5bfecd2db1d0f430f6f1e84df31b8a71d4bc05
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-30 18:11:25 +0000

    pkey with submission

--- web_module/branches/studio_changes_2006_08_18/src/main/com/topcoder/web/studio/model/ContestResult.java
+++ web_module/branches/studio_changes_2006_08_18/src/main/com/topcoder/web/studio/model/ContestResult.java
@@ -1,9 +1,6 @@
 package com.topcoder.web.studio.model;
 
-import com.topcoder.web.common.model.AlgoRating;
 import com.topcoder.web.common.model.Base;
-
-import java.io.Serializable;
 
 /**
  * @author dok
@@ -11,16 +8,17 @@ import java.io.Serializable;
  *          Create Date: Aug 29, 2006
  */
 public class ContestResult extends Base implements Comparable {
-    private Identifier id = new Identifier();
+    private Long id;
     private Contest contest;
     private Submission submission;
     private Prize prize;
 
-    public Identifier getId() {
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(Identifier id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -54,11 +52,11 @@ public class ContestResult extends Base implements Comparable {
         } else {
             try {
                 ContestResult oa = (ContestResult) o;
-                boolean sameContest = (oa.getContest().getId() == null && getContest().getId() == null) ||
-                        (oa.getContest().getId() != null && getContest().getId() != null && oa.getContest().getId().equals(getContest().getId()));
                 boolean sameSubmission = (oa.getSubmission().getId() == null && getSubmission().getId() == null) ||
                         (oa.getSubmission().getId() != null && getSubmission().getId() != null && oa.getSubmission().getId().equals(getSubmission().getId()));
-                return sameContest && sameSubmission;
+                boolean samePrize = (oa.getPrize().getId() == null && getPrize().getId() == null) ||
+                        (oa.getPrize().getId() != null && getPrize().getId() != null && oa.getPrize().getId().equals(getPrize().getId()));
+                return sameSubmission && samePrize;
             } catch (ClassCastException e) {
                 return false;
             }
@@ -68,12 +66,12 @@ public class ContestResult extends Base implements Comparable {
     public int hashCode() {
         StringBuffer buf = new StringBuffer(30);
 
-        if (getContest() == null) {
+        if (getPrize() == null) {
             buf.append("");
-        } else if (getContest().getId() == null) {
-            buf.append(getContest().hashCode());
+        } else if (getPrize().getId() == null) {
+            buf.append(getPrize().hashCode());
         } else {
-            buf.append(getContest().getId());
+            buf.append(getPrize().getId());
         }
         buf.append(" ");
         if (getSubmission() == null) {
@@ -88,61 +86,6 @@ public class ContestResult extends Base implements Comparable {
 
     public int compareTo(Object o) {
         return getPrize().compareTo(((ContestResult) o).getPrize());
-    }
-
-
-    public static class Identifier implements Serializable {
-
-        private Contest contest;
-        private Submission submission;
-
-
-        public Identifier() {
-
-        }
-
-        public Identifier(Contest contest, Submission submission) {
-            this.contest = contest;
-            this.submission = submission;
-        }
-
-        public Contest getContest() {
-            return contest;
-        }
-
-        public void setContest(Contest contest) {
-            this.contest = contest;
-        }
-
-        public Submission getSubmission() {
-            return submission;
-        }
-
-        public void setSubmission(Submission submission) {
-            this.submission = submission;
-        }
-
-        public boolean equals(Object o) {
-            if (o == null) {
-                return false;
-            } else {
-                try {
-                    AlgoRating.Identifier oa = (AlgoRating.Identifier) o;
-                    return (oa.getCoder().equals(getContest()) &&
-                            oa.getType().equals(getSubmission()));
-                } catch (ClassCastException e) {
-                    return false;
-                }
-            }
-        }
-
-        public int hashCode() {
-            StringBuffer buf = new StringBuffer(100);
-            buf.append(getContest().getId());
-            buf.append(" ");
-            buf.append(getSubmission().getId());
-            return buf.toString().hashCode();
-        }
     }
 
 
