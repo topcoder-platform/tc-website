e39a3a6e49cec503c722448f542dd4f7b2361354
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-09-06 20:38:44 +0000

    *** empty log message ***

--- web_module/branches/payments_2006_08_30/scripts/ddl/PACTS/informixoltp.sql
+++ web_module/branches/payments_2006_08_30/scripts/ddl/PACTS/informixoltp.sql
@@ -1,28 +1,36 @@
 create table payment_reference_lu (
     payment_reference_id DECIMAL(3,0),
-    database_name VARCHAR(30) NOT NULL,
-    table_name VARCHAR(30) NOT NULL,
-    field_name VARCHAR(30) NOT NULL
+    reference_field_name VARCHAR(30) NOT NULL,
 );
 
 
-alter table payment_reference_lu add constraint primary_key
+alter table payment_reference_lu add constraint primary key
     (payment_reference_id) constraint payment_reference_lu_pk;
     
-insert into payment_reference_lu(payment_reference_id, database_name, table_name, field_name)
-values(1, 'informix_oltp', 'round', 'round_id');
+insert into payment_reference_lu(payment_reference_id, reference_field_name)
+values(1, 'algorithm_round_id');
 
-insert into payment_reference_lu(payment_reference_id, database_name, table_name, field_name)
-values(2, 'informix_oltp', 'project', 'project_id');
+insert into payment_reference_lu(payment_reference_id, reference_field_name)
+values(2, 'component_project_id');
 
-insert into payment_reference_lu(payment_reference_id, database_name, table_name, field_name)
-values(3, 'informix_oltp', 'problem', 'problem_id');
+insert into payment_reference_lu(payment_reference_id, reference_field_name)
+values(3, 'algorithm_problem_id');
 
-insert into payment_reference_lu(payment_reference_id, database_name, table_name, field_name)
-values(4, 'informix_oltp', 'contest', 'contest_id');
+insert into payment_reference_lu(payment_reference_id, reference_field_name)
+values(4, 'studio_contest_id');
+
+insert into payment_reference_lu(payment_reference_id, reference_field_name)
+values(5, 'component_contest_id');
+
+insert into payment_reference_lu(payment_reference_id, reference_field_name)
+values(6, 'digital_run_stage_id');
+
+insert into payment_reference_lu(payment_reference_id, reference_field_name)
+values(7, 'digital_run_season_id');
 
 
 alter table payment_type_lu add payment_reference_id DECIMAL(3,0);
+alter table payment_type_lu add show_in_profile_ind DECIMAL(1,0) default 1 not null;
 
 
 alter table payment_type_lu add constraint foreign key 
@@ -54,17 +62,21 @@ values(15, 'Problem Writing Payment', 3);
 insert into payment_type_lu(payment_type_id, payment_type_desc)
 values(16, 'CCIP Payment');
 
-insert into payment_type_lu(payment_type_id, payment_type_desc)
-values(17, 'Digital Run Payment');
+insert into payment_type_lu(payment_type_id, payment_type_desc, payment_reference_id)
+values(17, 'Digital Run Payment', 6);
+
+insert into payment_type_lu(payment_type_id, payment_type_desc, payment_reference_id)
+values(18, 'Digital Run Rockie Payment', 7);
+
+insert into payment_type_lu(payment_type_id, payment_type_desc, payment_reference_id)
+values(19, 'Component Tournament Bouns Payment', 5);
 
 insert into payment_type_lu(payment_type_id, payment_type_desc)
-values(18, 'Tournament Bouns Payment');
-
-insert into payment_type_lu(payment_type_id, payment_type_desc)
-values(19, 'Royalty Payment');
+values(20, 'Royalty Payment');
 
 update payment_type_lu set payment_type_desc='Algorithm Contest Payment', payment_reference_id = 1
 where payment_type_id = 1;
 
 update payment_type_lu set payment_reference_id = 2
 where payment_type_id in (6,7);
+
