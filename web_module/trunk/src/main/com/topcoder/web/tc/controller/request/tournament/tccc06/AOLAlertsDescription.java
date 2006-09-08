00ce1708f1001cccb4b6c6d373925518ae93a718
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-09-08 21:00:10 +0000

    add

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/tournament/tccc06/AOLAlertsDescription.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/tournament/tccc06/AOLAlertsDescription.java
@@ -28,7 +28,7 @@ public class AOLAlertsDescription extends BaseProcessor {
             String val = Util.encodePassword(String.valueOf(getUser().getId()), AOL_ALIAS);
             long after = System.currentTimeMillis();
             if (log.isDebugEnabled()) {
-                log.debug("encrtyped id is: " + val + " it took " + (after - before) + " ms");
+                log.debug("encrypted id is: " + val + " it took " + (after - before) + " ms");
             }
             Cookie c = new Cookie(AOL_COOKIE, val);
             c.setMaxAge(60 * 60);//1 hour
