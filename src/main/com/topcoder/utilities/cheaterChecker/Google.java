42fb8a946c25ee98aeddb6a2439da5f107c127bb
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-09-08 01:42:12 +0000

    more debug

--- src/main/com/topcoder/utilities/cheaterChecker/Google.java
+++ src/main/com/topcoder/utilities/cheaterChecker/Google.java
@@ -10,8 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Google {
     private static Logger log = Logger.getLogger(Contest.class);
