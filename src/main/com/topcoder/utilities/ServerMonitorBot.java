50da55e3aab19b20a5ca466cf6476bae781b4076
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-09-12 15:15:00 +0000

    change to new servers

--- src/main/com/topcoder/utilities/ServerMonitorBot.java
+++ src/main/com/topcoder/utilities/ServerMonitorBot.java
@@ -13,12 +13,13 @@ import java.io.File;
 import java.io.InputStream;
 
 /**
- *
- * @author  rfairfax
+ * @author rfairfax
  */
 public class ServerMonitorBot {
 
-    /** Creates a new instance of ServerMonitorBot */
+    /**
+     * Creates a new instance of ServerMonitorBot
+     */
     public ServerMonitorBot() {
     }
 
@@ -41,11 +42,11 @@ public class ServerMonitorBot {
 
                 System.out.println("STARTING");
                 String[] callAndArgs = {"wget",
-                                        "http://www.topcoder.com",
-                                        "--header=Host: www.topcoder.com",
-                                        "--timeout=30",
-                                        "-t1",
-                                        ""};
+                        "http://www.topcoder.com",
+                        "--header=Host: www.topcoder.com",
+                        "--timeout=30",
+                        "-t1",
+                        ""};
 
                 Process p = r.exec(callAndArgs);
                 p.waitFor();
@@ -61,14 +62,14 @@ public class ServerMonitorBot {
                     if (fiveone) {
                         fiveone = false;
                         System.out.println("FAILED, SENDING MAIL");
-                        addError("connection to 12.51 failed");
+                        addError("connection to wwww.topcoder.com failed");
                         sendError();
                     }
                 } else if (ret.indexOf("200 OK") == -1) {
                     if (fiveone) {
                         fiveone = false;
                         System.out.println("FAILED, SENDING MAIL");
-                        addError("response from 12.51 failed");
+                        addError("response from wwww.topcoder.com failed");
                         addErrorLarge(ret);
                         sendError();
                     }
@@ -83,19 +84,19 @@ public class ServerMonitorBot {
                 } catch (Exception e) {
 
                 }
-                
+
                 String[] callAndArgs2 = {"wget",
-                                         "http://192.168.12.151:8080/index.jsp",
-                                         "--timeout=30",
-                                         "-t1",
-                                         ""};
+                        "http://192.168.12.151:8080/index.jsp",
+                        "--timeout=30",
+                        "-t1",
+                        ""};
 
                 p = r.exec(callAndArgs2);
                 p.waitFor();
 
                 ret = getData(p.getErrorStream());
                 p.destroy();
-                
+
                 System.out.println("2:" + ret);
                 System.out.println(p.exitValue());
 
@@ -117,26 +118,26 @@ public class ServerMonitorBot {
                 } else {
                     software = true;
                 }
-                
+
                 try {
                     File f = new File("index.jsp");
                     f.delete();
                 } catch (Exception e) {
-                    
+
                 }
 
                 String[] callAndArgs3 = {"wget",
-                                         "http://192.168.12.61:8080",
-                                         "--timeout=30",
-                                         "-t1",
-                                         ""};
+                        "http://192.168.10.91",
+                        "--timeout=30",
+                        "-t1",
+                        ""};
 
                 p = r.exec(callAndArgs3);
                 p.waitFor();
 
                 ret = getData(p.getErrorStream());
                 p.destroy();
-                
+
                 System.out.println("2:" + ret);
                 System.out.println(p.exitValue());
 
@@ -158,26 +159,26 @@ public class ServerMonitorBot {
                 } else {
                     forums = true;
                 }
-                
+
                 try {
                     File f = new File("index.html?module=Main");
                     f.delete();
                 } catch (Exception e) {
-                    
+
                 }
-                
+
                 String[] callAndArgs4 = {"wget",
-                                         "http://192.168.10.93",
-                                         "--timeout=30",
-                                         "-t1",
-                                         ""};
+                        "http://192.168.10.93",
+                        "--timeout=30",
+                        "-t1",
+                        ""};
 
                 p = r.exec(callAndArgs4);
                 p.waitFor();
 
                 ret = getData(p.getErrorStream());
                 p.destroy();
-                
+
                 System.out.println("2:" + ret);
                 System.out.println(p.exitValue());
 
@@ -199,15 +200,14 @@ public class ServerMonitorBot {
                 } else {
                     studio = true;
                 }
-                
+
                 try {
                     File f = new File("index.html");
                     f.delete();
                 } catch (Exception e) {
-                    
+
                 }
 
-                
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -245,7 +245,7 @@ public class ServerMonitorBot {
     }
 
     public void sendError() {
-       if (!errorText.equals("")) {
+        if (!errorText.equals("")) {
             System.out.println("SENDING ERROR LOG");
             try {
                 TCSEmailMessage em = new TCSEmailMessage();
@@ -276,8 +276,8 @@ public class ServerMonitorBot {
                 em.setSubject("Server Error");
                 em.setBody(errorTextLarge);
                 em.setFromAddress("rfairfax@topcoder.com");
-		
-		EmailEngine.send(em);
+
+                EmailEngine.send(em);
             } catch (Exception e) {
                 System.out.println("HERE" + e.getClass());
                 e.printStackTrace();
