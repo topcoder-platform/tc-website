6dfbf6924d4bc9b40bd1d0efe617dfe149428e98
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-09-27 23:28:36 +0000

    add rating type constants

--- web_module/trunk/src/main/com/topcoder/web/common/WebConstants.java
+++ web_module/trunk/src/main/com/topcoder/web/common/WebConstants.java
@@ -19,40 +19,40 @@ public interface WebConstants {
     public static final String PROJECT_ID_KEY = "project_id";
     public static final String USER_NAME = "username";
     public static final String PASSWORD = "password";
-    
-    public static final String PASSWORD_CONFIRM="pac";
-    public static final String EMAIL="em";
-    public static final String EMAIL_CONFIRM="emc";
-    public static final String GIVEN_NAME ="fn";
-    public static final String MIDDLE_NAME="mn";
-    public static final String SURNAME ="ln";
-    public static final String ADDRESS1="a1";
-    public static final String ADDRESS2="a2";
-    public static final String ADDRESS3="a3";
-    public static final String COUNTRY_CODE="cc";
-    public static final String STATE_CODE="sc";
-    public static final String PROVINCE="prov";
-    public static final String CITY="city";
-    public static final String POSTAL_CODE ="zip";
-    public static final String PHONE_NUMBER="ph";
 
-    public static final String TERMS="terms";
-    public static final String TERMS_AGREE="terms_agree";
-    public static final String TERMS_OF_USE_ID="tuid";
+    public static final String PASSWORD_CONFIRM = "pac";
+    public static final String EMAIL = "em";
+    public static final String EMAIL_CONFIRM = "emc";
+    public static final String GIVEN_NAME = "fn";
+    public static final String MIDDLE_NAME = "mn";
+    public static final String SURNAME = "ln";
+    public static final String ADDRESS1 = "a1";
+    public static final String ADDRESS2 = "a2";
+    public static final String ADDRESS3 = "a3";
+    public static final String COUNTRY_CODE = "cc";
+    public static final String STATE_CODE = "sc";
+    public static final String PROVINCE = "prov";
+    public static final String CITY = "city";
+    public static final String POSTAL_CODE = "zip";
+    public static final String PHONE_NUMBER = "ph";
+
+    public static final String TERMS = "terms";
+    public static final String TERMS_AGREE = "terms_agree";
+    public static final String TERMS_OF_USE_ID = "tuid";
 
     public static final String CODER_ID = "cr";
 
 
     //secondary reg parameters
-    public static final String CODER_TYPE="cty";
-    public static final String DEMOG_PREFIX="demog_";
+    public static final String CODER_TYPE = "cty";
+    public static final String DEMOG_PREFIX = "demog_";
 
-    public static String ACTIVATION_CODE="code";
+    public static String ACTIVATION_CODE = "code";
 
-    public static String SCHOOL_ID="scid";
+    public static String SCHOOL_ID = "scid";
 
     //resume parameters
-    public static final String RESUME="resume";
+    public static final String RESUME = "resume";
 
     public static final int PREFERENCE_TEXT_ANSWER = 3;
     public static final int PREFERENCE_MULTIPLE_ANSWER = 2;
@@ -78,6 +78,9 @@ public interface WebConstants {
     public static final int PREFERENCE_PERMANENT_FALSE = 35;
 
     public static final String LEFT_NAV_KEY = "node";
+
+    public static final int HS_ALGO_RATING_TYPE_ID = 2;
+    public static final int TC_ALGO_RATING_TYPE_ID = 1;
 
 
     public static final char[] INACTIVE_STATI = {'0', '9', '6', '5', '4', 'I'};
