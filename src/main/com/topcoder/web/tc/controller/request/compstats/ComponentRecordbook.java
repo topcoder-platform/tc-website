0833c9172a733f7433583e4ef4f053a4be16a6d4
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-09-27 15:35:08 +0000

    add appeal percentage

--- src/main/com/topcoder/web/tc/controller/request/compstats/ComponentRecordbook.java
+++ src/main/com/topcoder/web/tc/controller/request/compstats/ComponentRecordbook.java
@@ -1,16 +1,14 @@
 package com.topcoder.web.tc.controller.request.compstats;
 
-import com.topcoder.shared.dataAccess.DataAccessConstants;
 import com.topcoder.shared.dataAccess.DataAccessInt;
 import com.topcoder.shared.dataAccess.Request;
 import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 import com.topcoder.shared.util.DBMS;
-import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.model.SoftwareComponent;
 import com.topcoder.web.common.tag.HandleTag;
-import com.topcoder.web.common.TCWebException;
-import com.topcoder.web.tc.Constants;
 import com.topcoder.web.tc.controller.request.Static;
+
 import java.util.Map;
 
 public class ComponentRecordbook extends Static {
@@ -23,44 +21,45 @@ public class ComponentRecordbook extends Static {
         // Ensure that the "c" parameter is valid
         String handle = dataRequest.getContentHandle().trim().toLowerCase();
         if (
-            !handle.equals("avg_placement") &&
-            !handle.equals("component_money") &&
-            !handle.equals("high_scores") &&
-            !handle.equals("largest_comp_prize") &&
-            !handle.equals("most_reviews") &&
-            !handle.equals("most_submissions") &&
-            !handle.equals("most_wins") &&
-            !handle.equals("overall_money") &&
-            !handle.equals("royalties") &&
-            !handle.equals("tournament_money") &&
-            !handle.equals("win_percentage")
-        ) throw new TCWebException("Invalid c parameter: " + handle);
+                !handle.equals("appeal_percentage") &&
+                        !handle.equals("avg_placement") &&
+                        !handle.equals("component_money") &&
+                        !handle.equals("high_scores") &&
+                        !handle.equals("largest_comp_prize") &&
+                        !handle.equals("most_reviews") &&
+                        !handle.equals("most_submissions") &&
+                        !handle.equals("most_wins") &&
+                        !handle.equals("overall_money") &&
+                        !handle.equals("royalties") &&
+                        !handle.equals("tournament_money") &&
+                        !handle.equals("win_percentage")
+                ) throw new TCWebException("Invalid c parameter: " + handle);
         getRequest().setAttribute("handle", handle);
-        
+
         // Ensure that the "type" parameter is valid
         // Set up the inClause ("112", "113", or "112,113") along the way
         String type = dataRequest.getProperty("type");
         if (type == null) type = HandleTag.COMPONENT;
 
         String inClause = null;
-        if (type.trim().toLowerCase().equals(HandleTag.COMPONENT)) 
+        if (type.trim().toLowerCase().equals(HandleTag.COMPONENT))
             inClause = SoftwareComponent.DESIGN_PHASE + "," + SoftwareComponent.DEV_PHASE;
         else if (type.trim().toLowerCase().equals(HandleTag.DESIGN))
             inClause = "" + SoftwareComponent.DESIGN_PHASE;
-        else if (type.trim().toLowerCase().equals(HandleTag.DEVELOPMENT)) 
+        else if (type.trim().toLowerCase().equals(HandleTag.DEVELOPMENT))
             inClause = "" + SoftwareComponent.DEV_PHASE;
         else
-            throw new TCWebException ("Invalid type parameter: " + type);
-        
+            throw new TCWebException("Invalid type parameter: " + type);
+
         dataRequest.setProperty("pis", inClause); // query input parameter
-        
+
         DataAccessInt dai = getDataAccess(DBMS.TCS_DW_DATASOURCE_NAME, true);
         Map result = dai.getData(dataRequest);
         ResultSetContainer rsc = (ResultSetContainer) result.get(dataRequest.getContentHandle());
         getRequest().setAttribute("result", rsc);
 
-        String includeJsp 
-            = "/compstats/" + dataRequest.getContentHandle() + ".jsp"; 
+        String includeJsp
+                = "/compstats/" + dataRequest.getContentHandle() + ".jsp";
         getRequest().setAttribute("includeJsp", includeJsp);
         setNextPage("/compstats/ComponentRecordbook.jsp");
         setIsNextPageInContext(true);
