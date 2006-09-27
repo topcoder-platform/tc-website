e335050240b683c1cb8db3d1bb8491c21a3221e8
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-09-27 15:20:14 +0000

    *** empty log message ***

--- web_module/branches/payments_2006_08_30/src/main/com/topcoder/web/tc/controller/legacy/pacts/controller/request/internal/SelectPaymentTypeReference.java
+++ web_module/branches/payments_2006_08_30/src/main/com/topcoder/web/tc/controller/legacy/pacts/controller/request/internal/SelectPaymentTypeReference.java
@@ -14,26 +14,31 @@ public class SelectPaymentTypeReference extends BaseProcessor implements PactsCo
 
     protected void businessProcessing() throws TCWebException {
         try {
-            int refId = Integer.parseInt(getRequest().getParameter("reference_id"));
+            log.debug("reference_type_id=" + getRequest().getParameter("reference_type_id"));
+            int refId = Integer.parseInt(getRequest().getParameter("reference_type_id"));
             int type = Integer.parseInt(getRequest().getParameter("payment_type_id"));
             String search = getRequest().getParameter("search_text");
 
             DataInterfaceBean dib = new DataInterfaceBean();
 
             if (search != null) {
+                // If a search is being done...
                 Map map = null;
                 switch (refId) {
                     case REFERENCE_ALGORITHM_PROBLEM_ID:
                         map = dib.findProblems("%" + search + "%");
                         getRequest().setAttribute(ALGORITHM_PROBLEM_LIST, map.get(ALGORITHM_PROBLEM_LIST));
                         break;
+
                     case REFERENCE_COMPONENT_PROJECT_ID:
                         map = dib.findProjects("%" + search + "%");
                         getRequest().setAttribute(COMPONENT_PROJECT_LIST, map.get(COMPONENT_PROJECT_LIST));
                         break;
+
                     case REFERENCE_ALGORITHM_ROUND_ID:
                         if (type == ALGORITHM_CONTEST_PAYMENT) {
                             map = dib.findRounds("%" + search + "%", new int[] {1,2,3,17,18}); // fix values
+
                         } else if (type == MARATHON_MATCH_PAYMENT) {
                             map = dib.findRounds("%" + search + "%", new int[] {10,11,12}); // fix values
                         }
@@ -41,12 +46,14 @@ public class SelectPaymentTypeReference extends BaseProcessor implements PactsCo
                         break;
                 }
             } else {
+                // no search, for the references that have few fields.
                 Map map = null;
                 switch (refId) {
                     case REFERENCE_DIGITAL_RUN_SEASON_ID:
                         map = dib.getDigitalRunSeasonList();
                         getRequest().setAttribute(DIGITAL_RUN_SEASON_LIST, map.get(DIGITAL_RUN_SEASON_LIST));
                         break;
+
                     case REFERENCE_DIGITAL_RUN_STAGE_ID:
                         map = dib.getDigitalRunStageList();
                         getRequest().setAttribute(DIGITAL_RUN_STAGE_LIST, map.get(DIGITAL_RUN_STAGE_LIST));
@@ -54,7 +61,7 @@ public class SelectPaymentTypeReference extends BaseProcessor implements PactsCo
                 }
 
             }
-            getRequest().setAttribute("reference_id", refId + "");
+            getRequest().setAttribute("reference_type_id", refId + "");
             getRequest().setAttribute("search", search);
             setNextPage(INTERNAL_SELECT_PAYMENT_TYPE_REFERENCE);
             setIsNextPageInContext(true);
