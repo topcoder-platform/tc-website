d6791d05d34f473ac308815b6a8ec5cf8b397698
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-09-28 14:16:52 +0000

    *** empty log message ***

--- web_module/branches/payments_2006_08_30/scripts/ddl/PACTS/informixoltp.sql
+++ web_module/branches/payments_2006_08_30/scripts/ddl/PACTS/informixoltp.sql
@@ -104,8 +104,10 @@ update payment_type_lu set due_date_interval = 60
 where payment_type_id in (19,22);
 
 insert into security_perms (role_id, permission, create_user_id, security_status_id)
-values(2003, 'com.topcoder.web.tc.controller.legacy.pacts.controller.request.internal.SelectPaymentTypeReference', 132456,1)
+values(2003, 'com.topcoder.web.tc.controller.legacy.pacts.controller.request.internal.ajax.SelectPaymentTypeReference', 132456,1);
 
+insert into security_perms (role_id, permission, create_user_id, security_status_id)
+values(2003, 'com.topcoder.web.tc.controller.legacy.pacts.controller.request.internal.ajax.FillPaymentData', 132456,1);
 
 
 alter table payment_detail add column algorithm_round_id DECIMAL(10,0);
---
+++ web_module/branches/payments_2006_08_30/src/main/com/topcoder/web/tc/view/pacts/internal/ajax/fillPaymentData.jsp
@@ -0,0 +1,18 @@
+<%@page contentType="text/xml"%>
+<%@ taglib uri="tc-webtags.tld" prefix="tc-webtag" %>
+<%@ taglib uri="common-functions" prefix="cf" %>
+<%@ taglib uri="http://taconite.sf.net/tags" prefix="tac" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<c:set var="payment" value="${requestScope.payment}"/>
+
+<taconite-root xml:space="preserve">
+
+ <taconite-replace-children contextNodeID="runJS" parseInBrowser="true">     
+   <script type="text/javascript">
+        
+        document.f.payment_desc.value = '<c:out value="${payment.description}" />';
+    </script>
+</taconite-replace-children>      
+ </taconite-root>
+ 
