b4c887a125d9dccc21b3353be7e47d07d46e476c
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-10-10 17:11:05 +0000

    fixed a reference to project_id in payment_detail table (now is component_project_id)

--- web_module/trunk/src/main/com/topcoder/web/ejb/pacts/PactsServicesBean.java
+++ web_module/trunk/src/main/com/topcoder/web/ejb/pacts/PactsServicesBean.java
@@ -605,7 +605,7 @@ public class PactsServicesBean extends BaseEJB implements PactsConstants {
         selectPaymentHeader.append("SELECT p.payment_id, pd.payment_desc, pd.payment_type_id, ");
         selectPaymentHeader.append("pt.payment_type_desc, pd.payment_method_id, pm.payment_method_desc, ");
         selectPaymentHeader.append("pd.net_amount, pd.status_id, s.status_desc, ");
-        selectPaymentHeader.append("p.user_id, u.handle, pd.date_modified, pd.gross_amount, pd.project_id, pd.client, p.review, ");
+        selectPaymentHeader.append("p.user_id, u.handle, pd.date_modified, pd.gross_amount, pd.client, p.review, ");
         selectPaymentHeader.append("pd.algorithm_round_id, pd.component_project_id, pd.algorithm_problem_id, ");
         selectPaymentHeader.append("pd.studio_contest_id, pd.component_contest_id, pd.digital_run_stage_id, ");
         selectPaymentHeader.append("pd.digital_run_season_id, pd.parent_payment_id ");
