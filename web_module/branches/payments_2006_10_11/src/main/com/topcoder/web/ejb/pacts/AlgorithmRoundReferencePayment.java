45907ae8d8da2cad9957eaed1666530b6d6b4517
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-10-20 23:00:26 +0000

    *** empty log message ***

--- web_module/branches/payments_2006_10_11/src/main/com/topcoder/web/ejb/pacts/AlgorithmRoundReferencePayment.java
+++ web_module/branches/payments_2006_10_11/src/main/com/topcoder/web/ejb/pacts/AlgorithmRoundReferencePayment.java
@@ -154,7 +154,7 @@ public abstract class AlgorithmRoundReferencePayment extends BasePayment {
          */
         private void lookupData(long roundId) throws SQLException {
             StringBuffer query = new StringBuffer(100);
-            query.append(" select c.name || ' ' || r.name as round_name,  c.end_date");
+            query.append(" select NVL(r.short_name, c.name || ' ' || r.name) as round_name,  c.end_date");
             query.append(" from round r, ");
             query.append(" contest c ");
             query.append(" where r.contest_id = c.contest_id ");
