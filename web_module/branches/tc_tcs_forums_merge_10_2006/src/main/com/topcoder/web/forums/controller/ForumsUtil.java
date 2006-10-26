02246a9eb349b0b93adc15164110a30f5d3a6109
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-10-26 21:59:01 +0000

    no message

--- web_module/branches/tc_tcs_forums_merge_10_2006/src/main/com/topcoder/web/forums/controller/ForumsUtil.java
+++ web_module/branches/tc_tcs_forums_merge_10_2006/src/main/com/topcoder/web/forums/controller/ForumsUtil.java
@@ -21,7 +21,6 @@ import com.jivesoftware.forum.RatingManager;
 import com.jivesoftware.forum.database.DbForumFactory;
 import com.jivesoftware.forum.database.DbForumMessage;
 import com.jivesoftware.util.StringUtils;
-import com.opensymphony.oscache.util.StringUtil;
 import com.topcoder.shared.util.ApplicationServer;
 import com.topcoder.shared.util.logging.Logger;
 import com.topcoder.web.common.BaseProcessor;
