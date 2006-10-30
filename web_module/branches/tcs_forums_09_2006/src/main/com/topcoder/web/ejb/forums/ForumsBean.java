e1c296a70bb58adb4b9b0f4479a06c2eaeb50be4
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-10-30 18:33:13 +0000

    no message

--- web_module/branches/tcs_forums_09_2006/src/main/com/topcoder/web/ejb/forums/ForumsBean.java
+++ web_module/branches/tcs_forums_09_2006/src/main/com/topcoder/web/ejb/forums/ForumsBean.java
@@ -24,6 +24,7 @@ import java.util.Iterator;
 public class ForumsBean extends BaseEJB {
 
     private static Logger log = Logger.getLogger(ForumsBean.class);
+    private static ForumFactory forumFactory = ForumFactory.getInstance(new TCAuthToken(100129));
 
     // Creates a new forum in the "Algorithm Matches" category of the forums for the given round.
     // Also sets the forum_id field of the corresponding row in DW.round.
@@ -42,7 +43,6 @@ public class ForumsBean extends BaseEJB {
                 DBMS.OLTP_DATASOURCE_NAME);
         
         try {
-            ForumFactory forumFactory = ForumFactory.getInstance(new TCAuthToken(100129));
             Forum forum = forumFactory.createForum(roundName, "", forumFactory.getForumCategory(14));
             
             this.update("round",
@@ -59,7 +59,6 @@ public class ForumsBean extends BaseEJB {
     }
     
     public String[] getCategoryNames() {
-        ForumFactory forumFactory = ForumFactory.getInstance(AuthFactory.getAnonymousAuthToken());
         Iterator it = forumFactory.getRootForumCategory().getCategories();
         ArrayList listNames = new ArrayList();
         while (it.hasNext()) {
@@ -75,4 +74,20 @@ public class ForumsBean extends BaseEJB {
     			new String[]{String.valueOf(threadID)}, 
     			DBMS.FORUMS_DATASOURCE_NAME).intValue();
     }
+    
+    // Software Forums
+    //public Forum getComponentCategory(long compVersionID, int forumType) throws ForumCategoryNotFoundException {
+    	// find a way to efficiently map a component to its category - retrieve from DB
+    	//forumFactory.getForumCategory(378).getCategories()
+    //}
+    
+    // test
+    public int numSoftwareCategories() throws ForumCategoryNotFoundException {
+    	return forumFactory.getForumCategory(378).getCategoryCount();
+    }
+    
+    public void addSoftwareCategory() throws ForumCategoryNotFoundException, UnauthorizedException {
+    	forumFactory.getForumCategory(378).createCategory("Test Category", "This is a test.");
+    }
+    // Software Forums - End
 }
