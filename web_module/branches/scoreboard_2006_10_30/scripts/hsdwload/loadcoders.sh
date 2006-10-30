1339bd8d679cd25b78f608157175ddd2d4e3dae9
nobody <nobody@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-10-30 18:19:27 +0000

    This commit was manufactured by cvs2svn to create branch 'scoreboard_2006_10_30'.

---
+++ web_module/branches/scoreboard_2006_10_30/scripts/hsdwload/loadcoders.sh
@@ -0,0 +1,1 @@
+nohup java -cp "/usr/web/resources:/usr/web/lib/jars/log4j.jar:/usr/web/lib/jars/ifxjdbc.jar:/usr/web/lib/jars/jdbc7.0-1.2.jar:/usr/web/lib/jars/xml/2.2.D14/xerces.jar:/usr/web/build/classes" com.topcoder.utilities.hsdwload.TCLoadUtility -xmlfile /usr/web/scripts/hsdwload/loadcoders.xml >> /usr/web/scripts/hsdwload/coder_load.log 2>&1 &
---
+++ web_module/branches/scoreboard_2006_10_30/src/main/com/topcoder/web/ejb/forums/Forums.java
@@ -0,0 +1,14 @@
+package com.topcoder.web.ejb.forums;
+
+import javax.ejb.EJBObject;
+import java.rmi.RemoteException;
+import javax.ejb.EJBException;
+
+public interface Forums extends EJBObject {
+    
+    public void createMatchForum(int roundID) throws EJBException, RemoteException;
+
+    public String[] getCategoryNames() throws EJBException, RemoteException;
+    
+    public int getThreadMessageCount(int threadID) throws EJBException, RemoteException;
+}
