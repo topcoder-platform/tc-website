410c9fbac615e322547f22abbe14df09f3f1abbe
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-11-02 18:10:14 +0000

    *** empty log message ***

--- web_module/branches/payments_2006_10_31/src/main/com/topcoder/web/ejb/pacts/PactsServicesBean.java
+++ web_module/branches/payments_2006_10_31/src/main/com/topcoder/web/ejb/pacts/PactsServicesBean.java
@@ -5769,7 +5769,7 @@ public class PactsServicesBean extends BaseEJB implements PactsConstants {
         query.append("WHEN a.status_id = 58 THEN '' ");
         query.append("ELSE 'Expired' END as time_left_order, ");
         query.append("pd.net_amount, a.notarized, s.status_desc, pd.date_paid, a.date_affirmed, ");
-        query.append("a.affidavit_id, a.affirmed, a.date_created, p.payment_id, ");
+        query.append("a.affidavit_id, a.affirmed, a.date_created, p.payment_id, status_id, ");
         query.append(PactsConstants.AFFIDAVIT_EXPIRE_TIME + " units day -(today - a.date_created) as time_left, a.date_created, a.date_affirmed ");
         query.append("FROM affidavit a, status_lu s, ");
         query.append("OUTER (payment p, payment_detail pd) ");
