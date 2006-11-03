64666af3a24f3b13a2a2553105c719e23fb664f2
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-11-03 19:16:48 +0000

    *** empty log message ***

--- web_module/branches/payments_2006_10_31/src/main/com/topcoder/utilities/pacts/ExpireOldAffidavitsMBean.java
+++ web_module/branches/payments_2006_10_31/src/main/com/topcoder/utilities/pacts/ExpireOldAffidavitsMBean.java
@@ -9,10 +9,8 @@ public interface ExpireOldAffidavitsMBean extends ServiceMBean {
      * @return init status for this MBean
      *******************************************************/
     public String getInitStatus();
-    
-    
+        
     public String getRunningTime();
 
 	public void setRunningTime(String runningTime);
-
 }
