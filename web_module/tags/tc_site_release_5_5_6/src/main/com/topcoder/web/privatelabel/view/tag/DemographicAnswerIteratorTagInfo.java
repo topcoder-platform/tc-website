47cec25f3e0f683fb363295a250e38bad4cb9d80
nobody <nobody@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-11-03 06:51:25 +0000

    This commit was manufactured by cvs2svn to create tag 'tc_site_release_5_5_6'.

---
+++ web_module/tags/tc_site_release_5_5_6/src/main/com/topcoder/web/privatelabel/view/tag/DemographicAnswerIteratorTagInfo.java
@@ -0,0 +1,19 @@
+package com.topcoder.web.privatelabel.view.tag;
+
+import javax.servlet.jsp.tagext.TagData;
+import javax.servlet.jsp.tagext.TagExtraInfo;
+import javax.servlet.jsp.tagext.VariableInfo;
+
+public class DemographicAnswerIteratorTagInfo extends TagExtraInfo {
+
+    public VariableInfo[] getVariableInfo(TagData data) {
+        return new VariableInfo[]{
+                new VariableInfo(data.getId(),
+                        "com.topcoder.web.privatelabel.model.DemographicAnswer",
+                        true,
+                        VariableInfo.NESTED)
+        };
+    }
+
+}
+
