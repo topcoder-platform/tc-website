7ccc95dd678090e3b43132c6d0e78e0e68a70a87
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-11-27 19:25:32 +0000

    add

---
+++ web_module/branches/studio_enhancement_2006_11_10/src/main/com/topcoder/web/studio/controller/request/UpdateSubmissionRank.java
@@ -0,0 +1,30 @@
+package com.topcoder.web.studio.controller.request;
+
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.common.ShortHibernateProcessor;
+import com.topcoder.web.studio.Constants;
+import com.topcoder.web.studio.dao.StudioDAOUtil;
+import com.topcoder.web.studio.dao.SubmissionDAO;
+import com.topcoder.web.studio.model.Submission;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Nov 20, 2006
+ */
+public class UpdateSubmissionRank extends ShortHibernateProcessor {
+    protected void dbProcessing() throws Exception {
+
+        String submissionId = getRequest().getParameter(Constants.SUBMISSION_ID);
+        SubmissionDAO dao = StudioDAOUtil.getFactory().getSubmissionDAO();
+        Submission s = dao.find(new Long(submissionId));
+        if (s.getSubmitter().getId().longValue() == getUser().getId()) {
+            dao.changeRank(new Integer(getRequest().getParameter(Constants.SUBMISSION_RANK)), s);
+
+        } else {
+            throw new NavigationException("Illegal operation attempted, submission doesn't belong to current user.");
+        }
+
+
+    }
+}
---
+++ web_module/branches/studio_enhancement_2006_11_10/src/main/com/topcoder/web/studio/view/rankChangeResponse.jsp
@@ -0,0 +1,20 @@
+<%@page contentType="text/xml"%>
+
+<taconite-root xml:space="preserve">
+    <taconite-replace contextNodeID="tr${oldRank}" parseInBrowser="true">
+
+    </taconite-replace>
+    <taconite-replace contextNodeID="tr${newRank}" parseInBrowser="true">
+
+    </taconite-replace>
+ </taconite-root>
+
+
+<%--
+rank
+submission file name
+                        view link
+                        submit date
+                        passed/failed
+                        
+                        --%>
