d31599707321359a98d89a4d8a1c565e698b40ae
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-11-30 02:49:21 +0000

    try the new improved batch update like netflix works

--- web_module/branches/studio_enhancement_2006_11_10/src/main/com/topcoder/web/studio/controller/request/BatchUpdateRank.java
+++ web_module/branches/studio_enhancement_2006_11_10/src/main/com/topcoder/web/studio/controller/request/BatchUpdateRank.java
@@ -30,6 +30,84 @@ public class BatchUpdateRank extends BaseSubmissionDataProcessor {
         Date now = new Date();
         List userSubmissions = dao.getSubmissions(contestId, new Long(getUser().getId()));
         Integer maxRank = null;
+        TreeMap changes = new TreeMap();
+        for (Enumeration paramNames = getRequest().getParameterNames(); paramNames.hasMoreElements();) {
+            paramName = (String) paramNames.nextElement();
+            if (paramName.startsWith(Constants.SUBMISSION_ID)) {
+                newRank = getRequest().getParameter(paramName);
+                ValidationResult r = new IntegerValidator("Please enter a valid number.").validate(new StringInput(newRank));
+                if (!r.isValid()) {
+                    addError(paramName, r.getMessage());
+                } else {
+                    currSubmission = findSubmission(userSubmissions,
+                            new Long(paramName.substring(Constants.SUBMISSION_ID.length())));
+                    if (now.before(currSubmission.getContest().getStartTime()) ||
+                            now.after(currSubmission.getContest().getEndTime()) ||
+                            !ContestStatus.ACTIVE.equals(currSubmission.getContest().getStatus().getId())) {
+                        addError(paramName, "Sorry, you can not make a change to a submission for a contest that is not active.");
+                    } else {
+                        if (maxRank==null) {
+                            maxRank = dao.getMaxRank(currSubmission.getContest(), currSubmission.getSubmitter());
+                        }
+                        newRankInt = new Integer(newRank);
+                        if (newRankInt.intValue()<1) {
+                            newRankInt = new Integer(1);
+                        } else if (newRankInt.intValue() > maxRank.intValue()) {
+                            newRankInt = maxRank;
+                        }
+                        if (!newRankInt.equals(currSubmission.getRank())) {
+                            changes.put(newRankInt, currSubmission);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        Submission s = (Submission)userSubmissions.get(0);
+        if (!hasErrors()) {
+            ArrayList a = new ArrayList(changes.keySet());
+            Collections.reverse(a);
+
+            for (int i=0; i<a.size(); i++) {
+                dao.changeRank((Integer)a.get(i) ,(Submission)changes.get(a.get(i)));
+            }
+
+            markForCommit();
+            closeConversation();
+
+            beginCommunication();
+
+            HibernateUtils.getSession().refresh(s);
+            dao = StudioDAOUtil.getFactory().getSubmissionDAO();
+        }
+
+        loadSubmissionData(s.getSubmitter(), s.getContest(), dao, maxRank);
+        //override so that the user gets their data back to them
+        for (Enumeration paramNames = getRequest().getParameterNames(); paramNames.hasMoreElements();) {
+            paramName = (String) paramNames.nextElement();
+            setDefault(paramName, getRequest().getParameter(paramName));
+        }
+        
+        setIsNextPageInContext(true);
+        setNextPage("submitTableBody.jsp");
+
+
+    }
+
+/*
+    protected void dbProcessing() throws Exception {
+
+        Long contestId = new Long(getRequest().getParameter(Constants.CONTEST_ID));
+
+        SubmissionDAO dao = StudioDAOUtil.getFactory().getSubmissionDAO();
+        String paramName;
+        String newRank;
+        Integer newRankInt;
+        Submission currSubmission = null;
+        Date now = new Date();
+        List userSubmissions = dao.getSubmissions(contestId, new Long(getUser().getId()));
+        Integer maxRank = null;
         for (Enumeration paramNames = getRequest().getParameterNames(); paramNames.hasMoreElements();) {
             paramName = (String) paramNames.nextElement();
             if (paramName.startsWith(Constants.SUBMISSION_ID)) {
@@ -92,12 +170,21 @@ public class BatchUpdateRank extends BaseSubmissionDataProcessor {
             paramName = (String) paramNames.nextElement();
             setDefault(paramName, getRequest().getParameter(paramName));
         }
-        
+
         setIsNextPageInContext(true);
         setNextPage("submitTableBody.jsp");
 
 
     }
+*/
+
+
+
+
+
+
+
+
 
     private Submission findSubmission(List submissions, Long id) {
         Submission curr = null;
