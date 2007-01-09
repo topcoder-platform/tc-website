cfccb9d4ad3b5d6674d73f0eb042d451c24b6c92
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-01-09 16:51:02 +0000

    no message

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/membercontact/MemberContact.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/membercontact/MemberContact.java
@@ -9,6 +9,7 @@ import com.topcoder.web.common.WebConstants;
 import com.topcoder.web.common.dao.DAOUtil;
 import com.topcoder.web.common.model.User;
 import com.topcoder.web.tc.Constants;
+import com.topcoder.web.common.SecurityHelper;
 
 /**
  * Processor for Member Contact page.
@@ -30,11 +31,13 @@ public class MemberContact extends ShortHibernateProcessor {
 
         User sender  = DAOUtil.getFactory().getUserDAO().find(new Long(getUser().getId()));
 
-        if (!Helper.isRated(getUser().getId())) {
+        // users who have MemberContact Permission skips the rated validation.
+        if (!Helper.isRated(getUser().getId()) && 
+                !SecurityHelper.hasPermission(getLoggedInUser(), new ClassResource(MemberContact.class))) {
             getRequest().setAttribute(Helper.NOT_RATED, String.valueOf(true));
         } else if (Helper.isBanned(getUser().getId()) 
-            	|| (Arrays.binarySearch(WebConstants.ACTIVE_STATI, sender.getStatus().charValue()) < 0)) {
-            getRequest().setAttribute(Helper.BANNED, String.valueOf(true));        	
+                || (Arrays.binarySearch(WebConstants.ACTIVE_STATI, sender.getStatus().charValue()) < 0)) {
+            getRequest().setAttribute(Helper.BANNED, String.valueOf(true));         
         } else {
             if (!sender.isMemberContactEnabled()) {
                 getRequest().setAttribute(CAN_RECEIVE, String.valueOf(true));
--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/membercontact/ValidateHandle.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/membercontact/ValidateHandle.java
@@ -3,7 +3,6 @@ package com.topcoder.web.tc.controller.request.membercontact;
 import com.topcoder.web.common.ShortHibernateProcessor;
 import com.topcoder.web.common.dao.DAOUtil;
 import com.topcoder.web.common.model.User;
-import com.topcoder.web.common.validation.NonEmptyValidator;
 import com.topcoder.web.common.validation.StringInput;
 import com.topcoder.web.common.validation.ValidationResult;
 import com.topcoder.web.tc.Constants;
@@ -20,20 +19,13 @@ public class ValidateHandle extends ShortHibernateProcessor {
 
     protected void dbProcessing() throws Exception {
         String handle = getRequest().getParameter(SendMail.TO_HANDLE);
-        String text = getRequest().getParameter(SendMail.HAS_TEXT);
-
         User user = DAOUtil.getFactory().getUserDAO().find(new Long(getUser().getId()));
 
         ValidationResult handleValidation = new HandleValidator(user).validate(new StringInput(handle));
-        ValidationResult textValidation = new NonEmptyValidator("Please enter the message text.")
-            .validate(new StringInput(text));
 
         getRequest().setAttribute("handleValidation", handleValidation);
-        getRequest().setAttribute("textValidation", textValidation);
 
         setNextPage(Constants.VALIDATE_HANDLE);
         setIsNextPageInContext(true);
     }
-
-
 }
