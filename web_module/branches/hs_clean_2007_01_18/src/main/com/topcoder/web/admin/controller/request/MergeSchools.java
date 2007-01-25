321f835e9ec241bdc0ee42a2a983228516d08409
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-01-25 20:30:00 +0000

    *** empty log message ***

--- web_module/branches/hs_clean_2007_01_18/src/main/com/topcoder/web/admin/controller/request/MergeSchools.java
+++ web_module/branches/hs_clean_2007_01_18/src/main/com/topcoder/web/admin/controller/request/MergeSchools.java
@@ -1,5 +1,6 @@
 package com.topcoder.web.admin.controller.request;
 
+import com.topcoder.shared.util.DBMS;
 import com.topcoder.web.ejb.school.School;
 
 /**
@@ -13,6 +14,7 @@ public class MergeSchools extends Base {
     	
     	School s = (School) createEJB(getInitialContext(), School.class);
 
+    	s.createSchool(DBMS.OLTP_DATASOURCE_NAME, "", "A", "Bs As", "Argentina", 7545675,"Test");
     	s.mergeSchools(s1, s2);
     	
     	setNextPage("/admin/?" + getRequest().getQueryString().replaceFirst("MergeSchools", "CleanSchools"));
