355427a1355d5b2276de348c97492e9f0081f3a9
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-01-25 13:43:44 +0000

    *** empty log message ***

---
+++ web_module/branches/registration_2007_01_23/src/main/com/topcoder/utilities/UpdateResponse.java
@@ -0,0 +1,107 @@
+package com.topcoder.utilities;
+
+import com.topcoder.shared.util.TCContext;
+import com.topcoder.shared.util.TCResourceBundle;
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.shared.util.sql.InformixSimpleDataSource;
+import com.topcoder.web.common.IdGeneratorClient;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UpdateResponse {
+    private static Logger log = Logger.getLogger(UpdateResponse.class);
+
+    public static void main(String[] args) {
+        UpdateResponse r = new UpdateResponse();
+        try {
+            r.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void run() throws Exception {
+        
+        InitialContext ctx = TCContext.getInitial();
+        
+        Connection conOLTP  = new InformixSimpleDataSource(new TCResourceBundle("DBMS").getProperty("INFORMIX_CONNECT_STRING")).getConnection();
+        
+        PreparedStatement ps = conOLTP.prepareStatement("select response_id, question_id from response where response_id is null order by question_id asc");
+
+        ResultSet rs = null;
+
+        try {
+            rs = ps.executeQuery();
+            long responseId; 
+            int count = 0;
+            while (rs.next()) {
+                
+                responseId = IdGeneratorClient.getSeqId("RESPONSE_SEQ");
+
+                rs.updateLong(1, responseId);
+                rs.updateRow();
+
+                if (count % 10 == 0) System.out.println(count + " rows inserted");
+            }
+            System.out.println(count + " rows updated");
+        } finally {
+            close(rs);
+            close(ps);
+            close(conOLTP);
+            close(ctx);
+        }
+    }
+
+    protected void close(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (Exception ignore) {
+                log.error("FAILED to close ResultSet.");
+                ignore.printStackTrace();
+            }
+        }
+    }
+
+    protected void close(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (Exception ignore) {
+                log.error("FAILED to close Connection.");
+                ignore.printStackTrace();
+            }
+        }
+
+    }
+
+    protected void close(Context ctx) {
+        if (ctx != null) {
+            try {
+                ctx.close();
+            } catch (Exception ignore) {
+                log.error("FAILED to close Context.");
+                ignore.printStackTrace();
+            }
+        }
+
+    }
+
+    protected void close(PreparedStatement ps) {
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (Exception ignore) {
+                log.error("FAILED to close PreparedStatement.");
+                ignore.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
