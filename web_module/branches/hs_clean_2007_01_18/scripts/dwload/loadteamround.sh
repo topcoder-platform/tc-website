374b7afef5c141fcc29cef1ffff46adb62c9709e
apps <apps@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-01-30 14:18:00 +0000

    *** empty log message ***

--- web_module/branches/hs_clean_2007_01_18/scripts/dwload/loadteamround.sh
+++ web_module/branches/hs_clean_2007_01_18/scripts/dwload/loadteamround.sh
@@ -4,5 +4,5 @@ CP=$CP:/home/branch3/web/lib/jars/log4j-1.2.7.jar
 CP=$CP:/home/branch3/web/lib/jars/ifxjdbc.jar
 CP=$CP:/home/branch3/web/build/classes
 
-nohup java -cp $CP com.topcoder.shared.util.dwload.TCLoadUtility -xmlfile loadrank.xml > ./nohup.out 2>&1 &
+nohup java -cp $CP com.topcoder.shared.util.dwload.TCLoadUtility -xmlfile loadteamround.xml > ./nohup.out 2>&1 &
 tail -f nohup.out
