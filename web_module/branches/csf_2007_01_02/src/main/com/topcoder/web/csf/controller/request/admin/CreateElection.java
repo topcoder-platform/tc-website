9be0c7b64dbad71ac30bf73fd868cdbae3a8b6ed
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-02-01 23:30:32 +0000

    include submissions

--- web_module/branches/csf_2007_01_02/src/main/com/topcoder/web/csf/controller/request/admin/CreateElection.java
+++ web_module/branches/csf_2007_01_02/src/main/com/topcoder/web/csf/controller/request/admin/CreateElection.java
@@ -30,11 +30,12 @@ public class CreateElection extends Base {
         String contestId = getRequest().getParameter(Constants.CONTEST_ID);
 
         String[] submissionIds = getRequest().getParameterValues(Constants.SUBMISSION_IDS);
+        Submission[] submissions = null;
         if (submissionIds == null || submissionIds.length == 0) {
             addError(Constants.SUBMISSION_IDS, "No submissions selected");
         } else {
-            Submission[] submissions = new Submission[submissionIds.length];
             SubmissionDAO sDao = CSFDAOUtil.getFactory().getSubmissionDAO();
+            submissions = new Submission[submissionIds.length];
             try {
                 for (int i = 0; i < submissionIds.length; i++) {
                     submissions[i] = sDao.find(new Long(submissionIds[i]));
@@ -42,45 +43,44 @@ public class CreateElection extends Base {
             } catch (NumberFormatException ex) {
                 addError(Constants.SUBMISSION_IDS, "Please choose valid submissions");
             }
+        }
+        if (hasErrors()) {
+            setDefault(Constants.START_TIME, startTime);
+            setDefault(Constants.END_TIME, endTime);
+            setDefault(Constants.SUBMISSION_IDS, submissions);
 
-            if (hasErrors()) {
-                setDefault(Constants.START_TIME, startTime);
-                setDefault(Constants.END_TIME, endTime);
-                setDefault(Constants.SUBMISSION_IDS, submissions);
-
-                Contest contest = CSFDAOUtil.getFactory().getContestDAO().find(new Long(contestId));
-                ArrayList a = new ArrayList(contest.getSubmissions());
-                Collections.sort(a, new Comparator() {
-                    public int compare(Object o1, Object o2) {
-                        return ((Submission) o1).getId().compareTo(((Submission) o2).getId());
-                    }
-                });
-                getRequest().setAttribute("submissions", a);
-
-                setNextPage("/admin/electionCreation.jsp");
-                setIsNextPageInContext(true);
-            } else {
-                SimpleDateFormat sdf = new SimpleDateFormat(Constants.JAVA_DATE_FORMAT);
-
-                CondorcetSchulzeElection election = new CondorcetSchulzeElection();
-                election.setStartTime(new Timestamp(sdf.parse(startTime).getTime()));
-                election.setEndTime(new Timestamp(sdf.parse(endTime).getTime()));
-                election.setName(submissions[0].getContest().getName());
-
-                Candidate c;
-                for (int i = 0; i < submissions.length; i++) {
-                    c = new Candidate(submissions[i].getId().toString());
-                    c.setElection(election);
-                    election.getCandidates().add(c);
+            Contest contest = CSFDAOUtil.getFactory().getContestDAO().find(new Long(contestId));
+            ArrayList a = new ArrayList(contest.getSubmissions());
+            Collections.sort(a, new Comparator() {
+                public int compare(Object o1, Object o2) {
+                    return ((Submission) o1).getId().compareTo(((Submission) o2).getId());
                 }
+            });
+            getRequest().setAttribute("submissions", a);
 
-                VotingDAOUtil.getFactory().getCondorcetSchulzeElectionDAO().saveOrUpdate(election);
+            setNextPage("/admin/electionCreation.jsp");
+            setIsNextPageInContext(true);
+        } else {
+            SimpleDateFormat sdf = new SimpleDateFormat(Constants.JAVA_DATE_FORMAT);
 
-                setNextPage(getSessionInfo().getServletPath() + "?" + Constants.MODULE_KEY +
-                        "=Static&d1=admin&d2=electionCreationSuccess.jsp");
-                setIsNextPageInContext(false);
+            CondorcetSchulzeElection election = new CondorcetSchulzeElection();
+            election.setStartTime(new Timestamp(sdf.parse(startTime).getTime()));
+            election.setEndTime(new Timestamp(sdf.parse(endTime).getTime()));
+            election.setName(submissions[0].getContest().getName());
+
+            Candidate c;
+            for (int i = 0; i < submissions.length; i++) {
+                c = new Candidate(submissions[i].getId().toString());
+                c.setElection(election);
+                election.getCandidates().add(c);
             }
 
+            VotingDAOUtil.getFactory().getCondorcetSchulzeElectionDAO().saveOrUpdate(election);
+
+            setNextPage(getSessionInfo().getServletPath() + "?" + Constants.MODULE_KEY +
+                    "=Static&d1=admin&d2=electionCreationSuccess.jsp");
+            setIsNextPageInContext(false);
         }
+
     }
 }
