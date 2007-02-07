82668389006fe6be85519ed334782a341b8fe0a8
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-02-07 20:08:45 +0000

    add

---
+++ web_module/branches/csf_2007_01_02/src/main/com/topcoder/web/csf/controller/request/SubmitBallot.java
@@ -0,0 +1,16 @@
+package com.topcoder.web.csf.controller.request;
+
+import com.topcoder.web.common.ShortHibernateProcessor;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Jan 31, 2007
+ */
+public class SubmitBallot extends ShortHibernateProcessor {
+    protected void dbProcessing() throws Exception {
+        //get their selections
+        //validate
+        //save to db
+    }
+}
