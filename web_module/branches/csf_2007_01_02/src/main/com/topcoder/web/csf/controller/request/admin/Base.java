839949a65067134ce2ae83814e71ee309b58cdb4
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-02-12 22:00:23 +0000

    add project id config

--- web_module/branches/csf_2007_01_02/src/main/com/topcoder/web/csf/controller/request/admin/Base.java
+++ web_module/branches/csf_2007_01_02/src/main/com/topcoder/web/csf/controller/request/admin/Base.java
@@ -78,6 +78,10 @@ public abstract class Base extends ShortHibernateProcessor {
                 setDefault(Constants.CONTEST_PROPERTY + CONTEST_PROPS[i], temp.getValue());
             }
         }
+        temp = contest.getConfig(dao.find(new Integer(9)));
+        if (temp!=null) {
+            setDefault(Constants.PROJECT_ID_KEY, temp.getValue());//project id config property
+        }
 
         Set a = contest.getFileTypes();
         ArrayList fileTypes = new ArrayList(a.size());
@@ -91,7 +95,6 @@ public abstract class Base extends ShortHibernateProcessor {
         setDefault(Constants.CONTEST_NAME, contest.getName());
         setDefault(Constants.START_TIME, sdf.format(contest.getStartTime()));
         setDefault(Constants.END_TIME, sdf.format(contest.getEndTime()));
-        setDefault(Constants.PROJECT_ID_KEY, contest.getConfig(dao.find(new Integer(9))));//project id config property
 
 
     }
