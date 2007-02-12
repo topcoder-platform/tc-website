c4e96f4e829ad8b25711de515a943f0032a43fe1
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-02-12 21:37:36 +0000

    set default for the project id

--- web_module/branches/csf_2007_01_02/src/main/com/topcoder/web/csf/controller/request/admin/Base.java
+++ web_module/branches/csf_2007_01_02/src/main/com/topcoder/web/csf/controller/request/admin/Base.java
@@ -91,6 +91,7 @@ public abstract class Base extends ShortHibernateProcessor {
         setDefault(Constants.CONTEST_NAME, contest.getName());
         setDefault(Constants.START_TIME, sdf.format(contest.getStartTime()));
         setDefault(Constants.END_TIME, sdf.format(contest.getEndTime()));
+        setDefault(Constants.PROJECT_ID_KEY, contest.getConfig(dao.find(new Integer(9))));//project id config property
 
 
     }
