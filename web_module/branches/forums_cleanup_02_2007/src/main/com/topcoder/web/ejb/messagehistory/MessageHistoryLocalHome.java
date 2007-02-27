0ef46d7b9e18849dc907410517501d506e656b30
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-02-27 21:59:23 +0000

    no message

--- web_module/branches/forums_cleanup_02_2007/src/main/com/topcoder/web/ejb/messagehistory/MessageHistoryLocalHome.java
+++ web_module/branches/forums_cleanup_02_2007/src/main/com/topcoder/web/ejb/messagehistory/MessageHistoryLocalHome.java
@@ -1,13 +1,13 @@
 package com.topcoder.web.ejb.messagehistory;
 
 import javax.ejb.CreateException;
-import javax.ejb.EJBHome;
+import javax.ejb.EJBLocalHome;
 
 /**
  * @author mtong
  */
 
-public interface MessageHistoryLocalHome extends EJBHome {
+public interface MessageHistoryLocalHome extends EJBLocalHome {
     
     public static final String EJB_REF_NAME = "java:/com.topcoder.web.ejb.messagehistory.MessageHistoryLocalHome";
     
