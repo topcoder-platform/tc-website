2a3ac10048a9ddd09a5c3a48a9cea814f841503b
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-02-27 15:08:16 +0000

    changes for new version of file upload

--- web_module/trunk/src/main/com/topcoder/web/forums/controller/request/Attach.java
+++ web_module/trunk/src/main/com/topcoder/web/forums/controller/request/Attach.java
@@ -9,6 +9,8 @@ import com.jivesoftware.forum.AttachmentException;
 import com.jivesoftware.forum.Forum;
 import com.jivesoftware.forum.ForumMessage;
 import com.jivesoftware.forum.ForumPermissions;
+import com.topcoder.servlet.request.FileDoesNotExistException;
+import com.topcoder.servlet.request.PersistenceException;
 import com.topcoder.servlet.request.UploadedFile;
 import com.topcoder.shared.security.ClassResource;
 import com.topcoder.web.common.BaseServlet;
@@ -159,10 +161,22 @@ public class Attach extends ForumsProcessor {
         } catch (UnauthorizedException e) {
             e.printStackTrace();
             addError(ForumConstants.ATTACHMENT_ERROR, "Sorry, you do not have permission to attach files.");
+        } catch (FileDoesNotExistException e) {
+            e.printStackTrace();
+            addError(ForumConstants.ATTACHMENT_ERROR, e.getMessage());
+        } catch (PersistenceException e) {
+            e.printStackTrace();
+            addError(ForumConstants.ATTACHMENT_ERROR, e.getMessage());
         } finally {
             if (errors.size() > 0) {
-                log.info("Errors encountered when attaching file: " + fileName +
-                        " (" + uploadedFile.getSize() + " bytes)");
+                try {
+                    log.info("Errors encountered when attaching file: " + fileName +
+                            " (" + uploadedFile.getSize() + " bytes)");
+                } catch (PersistenceException e) {
+                    e.printStackTrace();
+                } catch (FileDoesNotExistException e) {
+                    e.printStackTrace();
+                }
                 ArrayList errorsList = (ArrayList) errors.get(ForumConstants.ATTACHMENT_ERROR);
                 for (int i = 0; i < errorsList.size(); i++) {
                     log.info("\t" + (String) errorsList.get(i));
