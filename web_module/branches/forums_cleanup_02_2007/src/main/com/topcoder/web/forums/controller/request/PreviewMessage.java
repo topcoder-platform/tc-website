84edce6680e790f9fbf291138dd03c56e736faba
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-01 16:43:50 +0000

    no message

--- web_module/branches/forums_cleanup_02_2007/src/main/com/topcoder/web/forums/controller/request/PreviewMessage.java
+++ web_module/branches/forums_cleanup_02_2007/src/main/com/topcoder/web/forums/controller/request/PreviewMessage.java
@@ -66,7 +66,7 @@ public class PreviewMessage extends ForumsProcessor {
         setDefault(ForumConstants.TEMP_MESSAGE_ID, getRequest().getParameter(ForumConstants.TEMP_MESSAGE_ID));
         setDefault(ForumConstants.POST_MODE, postMode);
         setDefault(ForumConstants.MESSAGE_SUBJECT, subject);
-        setDefault(ForumConstants.MESSAGE_BODY, textareaBody);
+        setDefault(ForumConstants.MESSAGE_BODY, body);
         
         if (message != null && thread != null) {
             getRequest().setAttribute("thread", thread);
