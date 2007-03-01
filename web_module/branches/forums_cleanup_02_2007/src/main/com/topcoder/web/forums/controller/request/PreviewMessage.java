72750c706376f936feb3bb0a8e3670370c45e073
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-01 01:33:38 +0000

    no message

--- web_module/branches/forums_cleanup_02_2007/src/main/com/topcoder/web/forums/controller/request/PreviewMessage.java
+++ web_module/branches/forums_cleanup_02_2007/src/main/com/topcoder/web/forums/controller/request/PreviewMessage.java
@@ -35,6 +35,8 @@ public class PreviewMessage extends ForumsProcessor {
         ForumMessage message = null;
         ForumThread thread = null;
         
+        getHttpRequest().setCharacterEncoding("ISO-8859-1");
+        
         String forumIDStr = StringUtils.checkNull(getRequest().getParameter(ForumConstants.FORUM_ID));
         String messageIDStr = StringUtils.checkNull(getRequest().getParameter(ForumConstants.MESSAGE_ID));
         String tempMessageIDStr = StringUtils.checkNull(getRequest().getParameter(ForumConstants.TEMP_MESSAGE_ID));
