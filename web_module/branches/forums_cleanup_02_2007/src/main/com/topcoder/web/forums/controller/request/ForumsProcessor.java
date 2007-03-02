13f3c75c74300bf31005a178e98b61b01529382f
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-02 01:02:38 +0000

    no message

--- web_module/branches/forums_cleanup_02_2007/src/main/com/topcoder/web/forums/controller/request/ForumsProcessor.java
+++ web_module/branches/forums_cleanup_02_2007/src/main/com/topcoder/web/forums/controller/request/ForumsProcessor.java
@@ -37,8 +37,6 @@ public abstract class ForumsProcessor extends BaseProcessor {
      * Subclasses should do their work by implementing this method.
      */
     protected void businessProcessing() throws Exception {
-        log.info("character encoding: " + getHttpRequest().getCharacterEncoding());
-        getHttpRequest().setCharacterEncoding("ISO-8859-1");
         getResponse().setContentType("text/html; charset=ISO-8859-1");
         getHttpResponse().setContentType("text/html; charset=ISO-8859-1");
         
