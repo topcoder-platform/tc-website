c9c424eb90ff65ed12d623522a321f4591d1b08f
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-09 14:34:13 +0000

    fixed tabs

--- web_module/trunk/src/main/com/topcoder/web/admin/controller/request/MergeSchools.java
+++ web_module/trunk/src/main/com/topcoder/web/admin/controller/request/MergeSchools.java
@@ -8,14 +8,14 @@ import com.topcoder.web.ejb.school.School;
 public class MergeSchools extends Base {
 
     protected void businessProcessing() throws Exception {
-    	long s1 = Long.parseLong(getRequest().getParameter("s1"));
-    	long s2 = Long.parseLong(getRequest().getParameter("s2"));
-    	
-    	School s = (School) createEJB(getInitialContext(), School.class);
+        long s1 = Long.parseLong(getRequest().getParameter("s1"));
+        long s2 = Long.parseLong(getRequest().getParameter("s2"));
 
-    	s.mergeSchools(s1, s2, "on".equals(getRequest().getParameter("ua")));
-    	
-    	setNextPage("/admin/?" + getRequest().getQueryString().replaceFirst("MergeSchools", "SearchSchools"));
+        School s = (School) createEJB(getInitialContext(), School.class);
+
+        s.mergeSchools(s1, s2, "on".equals(getRequest().getParameter("ua")));
+
+        setNextPage("/admin/?" + getRequest().getQueryString().replaceFirst("MergeSchools", "SearchSchools"));
         setIsNextPageInContext(false);
     }
 
