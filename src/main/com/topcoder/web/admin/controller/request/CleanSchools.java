74bdbc96e83cf03d676d24afde398135f75cfc2e
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-09 14:34:13 +0000

    fixed tabs

--- src/main/com/topcoder/web/admin/controller/request/CleanSchools.java
+++ src/main/com/topcoder/web/admin/controller/request/CleanSchools.java
@@ -10,15 +10,15 @@ import com.topcoder.web.common.dao.DAOUtil;
 public class CleanSchools extends ShortHibernateProcessor {
 
     protected void dbProcessing() throws Exception {
-    	setDefault("cc", "");
-    	setDefault("type", new Integer(-1));
-    	setDefault("order", "c");
-    	setDefault("date", "");
-    	setDefault("ua", "true");
-    	
+        setDefault("cc", "");
+        setDefault("type", new Integer(-1));
+        setDefault("order", "c");
+        setDefault("date", "");
+        setDefault("ua", "true");
+
         getRequest().setAttribute("countries",  DAOUtil.getFactory().getCountryDAO().getCountries());
-    	
-    	setNextPage("/cleanSchools.jsp");
+
+        setNextPage("/cleanSchools.jsp");
         setIsNextPageInContext(true);
     }
 
--- src/main/com/topcoder/web/admin/controller/request/SearchSchools.java
+++ src/main/com/topcoder/web/admin/controller/request/SearchSchools.java
@@ -17,23 +17,23 @@ import com.topcoder.web.common.dao.SchoolDAO;
 public class SearchSchools extends ShortHibernateProcessor {
 
     protected void dbProcessing() throws Exception {
-    	try {
-    	DAOFactory dao = DAOUtil.getFactory();
-    	SchoolDAO s = dao.getSchoolDAO();
-    	String cc = getRequest().getParameter("cc");
-    	String name = getRequest().getParameter("name");
-    	Integer type = new Integer(getRequest().getParameter("type"));
-    	Date creationAfter = StringUtils.checkNull(getRequest().getParameter("date")).trim().length()==0 ? null :
-    		new SimpleDateFormat("MM/dd/yy").parse(getRequest().getParameter("date"));
-    	String order = getRequest().getParameter("order");
+        try {
+        DAOFactory dao = DAOUtil.getFactory();
+        SchoolDAO s = dao.getSchoolDAO();
+        String cc = getRequest().getParameter("cc");
+        String name = getRequest().getParameter("name");
+        Integer type = new Integer(getRequest().getParameter("type"));
+        Date creationAfter = StringUtils.checkNull(getRequest().getParameter("date")).trim().length()==0 ? null :
+            new SimpleDateFormat("MM/dd/yy").parse(getRequest().getParameter("date"));
+        String order = getRequest().getParameter("order");
 
-    	
-    	List schools = s.search(type.intValue() <0? null:dao.getSchoolTypeDAO().find(type), name, creationAfter, cc, "c".equals(order));
-    	getRequest().setAttribute("schools", schools);
-    	} catch (Exception e) {
-    		addError("error", e.getMessage());
-    	}
-    	setNextPage("/searchSchools.jsp");
+
+        List schools = s.search(type.intValue() <0? null:dao.getSchoolTypeDAO().find(type), name, creationAfter, cc, "c".equals(order));
+        getRequest().setAttribute("schools", schools);
+        } catch (Exception e) {
+            addError("error", e.getMessage());
+        }
+        setNextPage("/searchSchools.jsp");
         setIsNextPageInContext(true);
     }
 
