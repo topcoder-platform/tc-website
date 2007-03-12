e1e91074289eeef43c0e81aa34651b186d6aa5e1
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-12 22:09:05 +0000

    no message

--- src/main/com/topcoder/web/oracle/view/temp/org/tool-man/cookies.js
+++ src/main/com/topcoder/web/oracle/view/temp/org/tool-man/cookies.js
@@ -5,30 +5,30 @@ based on http://www.quirksmode.org/js/cookies.html
 
 ToolMan._cookieOven = {
 
-	set : function(name, value, expirationInDays) {
-		if (expirationInDays) {
-			var date = new Date()
-			date.setTime(date.getTime() + (expirationInDays * 24 * 60 * 60 * 1000))
-			var expires = "; expires=" + date.toGMTString()
-		} else {
-			var expires = ""
-		}
-		document.cookie = name + "=" + value + expires + "; path=/"
-	},
+    set : function(name, value, expirationInDays) {
+        if (expirationInDays) {
+            var date = new Date()
+            date.setTime(date.getTime() + (expirationInDays * 24 * 60 * 60 * 1000))
+            var expires = "; expires=" + date.toGMTString()
+        } else {
+            var expires = ""
+        }
+        document.cookie = name + "=" + value + expires + "; path=/"
+    },
 
-	get : function(name) {
-		var namePattern = name + "="
-		var cookies = document.cookie.split(';')
-		for(var i = 0, n = cookies.length; i < n; i++) {
-			var c = cookies[i]
-			while (c.charAt(0) == ' ') c = c.substring(1, c.length)
-			if (c.indexOf(namePattern) == 0)
-				return c.substring(namePattern.length, c.length)
-		}
-		return null
-	},
+    get : function(name) {
+        var namePattern = name + "="
+        var cookies = document.cookie.split(';')
+        for (var i = 0, n = cookies.length; i < n; i++) {
+            var c = cookies[i]
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length)
+            if (c.indexOf(namePattern) == 0)
+                return c.substring(namePattern.length, c.length)
+        }
+        return null
+    },
 
-	eraseCookie : function(name) {
-		createCookie(name, "", -1)
-	}
+    eraseCookie : function(name) {
+        createCookie(name, "", -1)
+    }
 }
