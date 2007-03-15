bc78f3f7d683cb4a5750c9a7bf4726882c273a13
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-15 16:01:55 +0000

    fix tco07 constant

--- src/main/com/topcoder/web/common/model/Event.java
+++ src/main/com/topcoder/web/common/model/Event.java
@@ -11,7 +11,7 @@ import java.util.Set;
  *          Create Date: Jan 18, 2007
  */
 public class Event extends Base {
-    public static final Long TCO07_COMPONENT_ID = new Long(2020);
+    public static final Long TCO07_COMPONENT_ID = new Long(7);
 
     private Long id;
     private EventType type;
