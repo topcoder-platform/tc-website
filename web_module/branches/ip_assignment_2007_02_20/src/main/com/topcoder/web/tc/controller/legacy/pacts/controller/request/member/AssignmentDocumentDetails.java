aa88e21aab90bd52451086d20321b29c71c8e7ed
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-20 14:51:16 +0000

    *** empty log message ***

--- web_module/branches/ip_assignment_2007_02_20/src/main/com/topcoder/web/tc/controller/legacy/pacts/controller/request/member/AssignmentDocumentDetails.java
+++ web_module/branches/ip_assignment_2007_02_20/src/main/com/topcoder/web/tc/controller/legacy/pacts/controller/request/member/AssignmentDocumentDetails.java
@@ -1,6 +1,8 @@
 package com.topcoder.web.tc.controller.legacy.pacts.controller.request.member;
 
+import com.topcoder.shared.security.ClassResource;
 import com.topcoder.web.common.BaseProcessor;
+import com.topcoder.web.common.PermissionException;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.model.AssignmentDocument;
 import com.topcoder.web.tc.controller.legacy.pacts.bean.DataInterfaceBean;
@@ -13,32 +15,36 @@ import com.topcoder.web.tc.controller.legacy.pacts.common.PactsConstants;
 public class AssignmentDocumentDetails extends BaseProcessor implements PactsConstants {
 
     protected void businessProcessing() throws TCWebException {
-        long assignment_document_id = Long.parseLong(getRequest().getParameter(ASSIGNMENT_DOCUMENT_ID));
-
-        try {
-            DataInterfaceBean bean = new DataInterfaceBean();
-
-            AssignmentDocument ad = bean.getAssignmentDocument(assignment_document_id);
-            getRequest().setAttribute(PACTS_INTERNAL_RESULT, ad);
-
-            // check that the assignment document belongs to the logged user.
-            if (ad == null) {
-                throw new IllegalArgumentException("Assignment Document not found");  
+        if (userLoggedIn()) {
+            long assignment_document_id = Long.parseLong(getRequest().getParameter(ASSIGNMENT_DOCUMENT_ID));
+    
+            try {
+                DataInterfaceBean bean = new DataInterfaceBean();
+    
+                AssignmentDocument ad = bean.getAssignmentDocument(assignment_document_id);
+                getRequest().setAttribute(PACTS_INTERNAL_RESULT, ad);
+    
+                // check that the assignment document belongs to the logged user.
+                if (ad == null) {
+                    throw new IllegalArgumentException("Assignment Document not found");  
+                }
+                
+                if (ad.getUser().getId().longValue() != getUser().getId()) {
+                    throw new IllegalArgumentException("This AD doesn't belong to the logged user");  
+                }
+                
+                Boolean hasHardCopy = bean.hasHardCopyAssignmentDocumentByUserId(ad.getUser().getId().longValue(), 
+                        ad.getType().getId().longValue());
+    
+                getRequest().setAttribute("has_hard_copy", hasHardCopy);
+                
+                setNextPage(ASSIGNMENT_DOCUMENT_DETAILS_JSP);
+                setIsNextPageInContext(true);
+            } catch (Exception e) {
+                throw new TCWebException(e);
             }
-            
-            if (ad.getUser().getId().longValue() != getUser().getId()) {
-                throw new IllegalArgumentException("This AD doesn't belong to the logged user");  
-            }
-            
-            Boolean hasHardCopy = bean.hasHardCopyAssignmentDocumentByUserId(ad.getUser().getId().longValue(), 
-                    ad.getType().getId().longValue());
-
-            getRequest().setAttribute("has_hard_copy", hasHardCopy);
-            
-            setNextPage(ASSIGNMENT_DOCUMENT_DETAILS_JSP);
-            setIsNextPageInContext(true);
-        } catch (Exception e) {
-            throw new TCWebException(e);
+        } else {
+            throw new PermissionException(getUser(), new ClassResource(this.getClass()));
         }
     }
 }
