27080a12ac87ceda8c977c83b7539429908e31d0
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-20 18:28:04 +0000

    no message

--- web_module/branches/forums_cleanup_02_2007/src/main/com/topcoder/dde/catalog/CatalogSearchEngine.java
+++ web_module/branches/forums_cleanup_02_2007/src/main/com/topcoder/dde/catalog/CatalogSearchEngine.java
@@ -19,6 +19,9 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
