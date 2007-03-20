70dbfefaa911afe5b4e83e409df3b6614fdd05ee
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-20 14:38:34 +0000

    *** empty log message ***

--- web_module/branches/studio_ip_assignment_2007_03_13/src/main/com/topcoder/web/studio/controller/request/legal/AffirmAssignmentDocument.java
+++ web_module/branches/studio_ip_assignment_2007_03_13/src/main/com/topcoder/web/studio/controller/request/legal/AffirmAssignmentDocument.java
@@ -1,6 +1,8 @@
 package com.topcoder.web.studio.controller.request.legal;
 
+import com.topcoder.shared.security.ClassResource;
 import com.topcoder.web.common.BaseProcessor;
+import com.topcoder.web.common.PermissionException;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.model.AssignmentDocument;
 import com.topcoder.web.common.model.AssignmentDocumentStatus;
@@ -14,35 +16,39 @@ import com.topcoder.web.studio.controller.request.PactsServicesLocator;
 public class AffirmAssignmentDocument extends BaseProcessor  {
 
     protected void businessProcessing() throws TCWebException {
-        long assignment_document_id = Long.parseLong(getRequest().getParameter(Constants.ASSIGNMENT_DOCUMENT_ID));
-
-        try {
-            AssignmentDocument ad = PactsServicesLocator.getService().getAssignmentDocument(assignment_document_id);
-            getRequest().setAttribute(Constants.RESULT_KEY, ad);
-
-            // check that the assignment document belongs to the logged user.
-            if (ad == null || ad.getUser().getId().longValue() != getUser().getId()) {
-                throw new IllegalArgumentException("Assignment Document not found");  
+        if (userLoggedIn()) {
+            long assignment_document_id = Long.parseLong(getRequest().getParameter(Constants.ASSIGNMENT_DOCUMENT_ID));
+    
+            try {
+                AssignmentDocument ad = PactsServicesLocator.getService().getAssignmentDocument(assignment_document_id);
+                getRequest().setAttribute(Constants.RESULT_KEY, ad);
+    
+                // check that the assignment document belongs to the logged user.
+                if (ad == null || ad.getUser().getId().longValue() != getUser().getId()) {
+                    throw new IllegalArgumentException("Assignment Document not found");  
+                }
+    
+                if (!ad.getStatus().getId().equals(AssignmentDocumentStatus.PENDING_STATUS_ID)) {
+                    throw new IllegalArgumentException("Assignment Document should be pending");  
+                }
+    
+                Boolean hasHardCopy = PactsServicesLocator.getService()
+                    .hasHardCopyAssignmentDocumentByUserId(ad.getUser().getId().longValue(), 
+                        ad.getType().getId().longValue());
+    
+                if (!hasHardCopy.booleanValue()) {
+                    throw new IllegalArgumentException("You must send a hard copy of the Assignment Document before you can use this system to affirm");  
+                }
+                
+                PactsServicesLocator.getService().affirmAssignmentDocument(ad);
+                
+                setNextPage("/legal/?module=AssignmentDocumentHistory");
+                setIsNextPageInContext(true);
+            } catch (Exception e) {
+                throw new TCWebException(e);
             }
-
-            if (!ad.getStatus().getId().equals(AssignmentDocumentStatus.PENDING_STATUS_ID)) {
-                throw new IllegalArgumentException("Assignment Document should be pending");  
-            }
-
-            Boolean hasHardCopy = PactsServicesLocator.getService()
-                .hasHardCopyAssignmentDocumentByUserId(ad.getUser().getId().longValue(), 
-                    ad.getType().getId().longValue());
-
-            if (!hasHardCopy.booleanValue()) {
-                throw new IllegalArgumentException("You must send a hard copy of the Assignment Document before you can use this system to affirm");  
-            }
-            
-            PactsServicesLocator.getService().affirmAssignmentDocument(ad);
-            
-            setNextPage("/legal/?module=AssignmentDocumentHistory");
-            setIsNextPageInContext(true);
-        } catch (Exception e) {
-            throw new TCWebException(e);
+        } else {
+            throw new PermissionException(getUser(), new ClassResource(this.getClass()));
         }
     }
 }
--- web_module/branches/studio_ip_assignment_2007_03_13/src/main/com/topcoder/web/studio/controller/request/legal/AssignmentDocumentDetails.java
+++ web_module/branches/studio_ip_assignment_2007_03_13/src/main/com/topcoder/web/studio/controller/request/legal/AssignmentDocumentDetails.java
@@ -1,6 +1,8 @@
 package com.topcoder.web.studio.controller.request.legal;
 
+import com.topcoder.shared.security.ClassResource;
 import com.topcoder.web.common.BaseProcessor;
+import com.topcoder.web.common.PermissionException;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.model.AssignmentDocument;
 import com.topcoder.web.studio.Constants;
@@ -13,33 +15,37 @@ import com.topcoder.web.studio.controller.request.PactsServicesLocator;
 public class AssignmentDocumentDetails extends BaseProcessor {
     
     protected void businessProcessing() throws TCWebException {
-        long assignment_document_id = Long.parseLong(getRequest().getParameter(Constants.ASSIGNMENT_DOCUMENT_ID));
-
-        try {
-            AssignmentDocument ad = PactsServicesLocator.getService()
-                .getAssignmentDocument(assignment_document_id);
-            
-            getRequest().setAttribute(Constants.RESULT_KEY, ad);
-
-            // check that the assignment document belongs to the logged user.
-            if (ad == null) {
-                throw new IllegalArgumentException("Assignment Document not found");  
+        if (userLoggedIn()) {
+            long assignment_document_id = Long.parseLong(getRequest().getParameter(Constants.ASSIGNMENT_DOCUMENT_ID));
+    
+            try {
+                AssignmentDocument ad = PactsServicesLocator.getService()
+                    .getAssignmentDocument(assignment_document_id);
+                
+                getRequest().setAttribute(Constants.RESULT_KEY, ad);
+    
+                // check that the assignment document belongs to the logged user.
+                if (ad == null) {
+                    throw new IllegalArgumentException("Assignment Document not found");  
+                }
+                
+                if (ad.getUser().getId().longValue() != getUser().getId()) {
+                    throw new IllegalArgumentException("This AD doesn't belong to the logged user");  
+                }
+                
+                Boolean hasHardCopy = PactsServicesLocator.getService()
+                    .hasHardCopyAssignmentDocumentByUserId(ad.getUser().getId().longValue(), 
+                        ad.getType().getId().longValue());
+    
+                getRequest().setAttribute("has_hard_copy", hasHardCopy);
+                
+                setNextPage("/legal/assignmentDocumentDetails.jsp");
+                setIsNextPageInContext(true);
+            } catch (Exception e) {
+                throw new TCWebException(e);
             }
-            
-            if (ad.getUser().getId().longValue() != getUser().getId()) {
-                throw new IllegalArgumentException("This AD doesn't belong to the logged user");  
-            }
-            
-            Boolean hasHardCopy = PactsServicesLocator.getService()
-                .hasHardCopyAssignmentDocumentByUserId(ad.getUser().getId().longValue(), 
-                    ad.getType().getId().longValue());
-
-            getRequest().setAttribute("has_hard_copy", hasHardCopy);
-            
-            setNextPage("/legal/assignmentDocumentDetails.jsp");
-            setIsNextPageInContext(true);
-        } catch (Exception e) {
-            throw new TCWebException(e);
+        } else {
+            throw new PermissionException(getUser(), new ClassResource(this.getClass()));
         }
     }
 }
