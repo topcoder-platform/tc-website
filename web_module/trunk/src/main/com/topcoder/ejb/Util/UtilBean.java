32be956683f5822bc3c8121a84d5f9ccb0c7f832
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-22 17:14:58 +0000

    use id generator

--- web_module/trunk/src/main/com/topcoder/ejb/Util/UtilBean.java
+++ web_module/trunk/src/main/com/topcoder/ejb/Util/UtilBean.java
@@ -1,10 +1,9 @@
 package com.topcoder.ejb.Util;
 
 import com.topcoder.shared.ejb.BaseEJB;
-import com.topcoder.shared.util.ApplicationServer;
 import com.topcoder.shared.util.DBMS;
-import com.topcoder.shared.util.TCContext;
 import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.web.common.IdGeneratorClient;
 
 import java.rmi.RemoteException;
 import java.sql.PreparedStatement;
@@ -20,42 +19,25 @@ public class UtilBean extends BaseEJB {
         java.sql.Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        javax.naming.Context ctx = null;
         try {
             conn = DBMS.getConnection();
             StringBuffer query = new StringBuffer(150);
-            ctx = TCContext.getInitial();
-            javax.naming.Context env = (javax.naming.Context) ctx.lookup("java:comp/env");
-            int seq = ((java.lang.Integer) env.lookup("LINK_SEQ")).intValue();
-            /*********************************************************/
-            query.append(" EXECUTE PROCEDURE nextval(?)");
-            /*********************************************************/
+            long newLinkId = IdGeneratorClient.getSeqId("LINK_SEQ");
+            log.debug("NEW_LINK_ID=" + newLinkId);
+            query.append(" INSERT");
+            query.append(" INTO");
+            query.append(" link_hit (");
+            query.append(" link_id");
+            query.append(" ,link");
+            query.append(" ,refer");
+            query.append(" ,timestamp");
+            query.append(" )");
+            query.append(" VALUES (?,?,?,CURRENT)");
             ps = conn.prepareStatement(query.toString());
-            query.delete(0, 150);
-            ps.setInt(1, seq);
-            rs = ps.executeQuery();
-            ps.clearParameters();
-            if (rs.next()) {
-                int newLinkId = rs.getInt(1);
-                log.debug("NEW_LINK_ID=" + newLinkId);
-                /*********************************************************/
-                query.append(" INSERT");
-                query.append(" INTO");
-                query.append(" link_hit (");
-                query.append(" link_id");
-                query.append(" ,link");
-                query.append(" ,refer");
-                query.append(" ,timestamp");
-                query.append(" )");
-                query.append(" VALUES (?,?,?,CURRENT)");
-                /*********************************************************/
-                DBMS.close(ps);
-                ps = conn.prepareStatement(query.toString());
-                ps.setInt(1, newLinkId);
-                ps.setString(2, link);
-                ps.setString(3, refer);
-                ps.executeUpdate();
-            }
+            ps.setLong(1, newLinkId);
+            ps.setString(2, link);
+            ps.setString(3, refer);
+            ps.executeUpdate();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -64,7 +46,6 @@ public class UtilBean extends BaseEJB {
             DBMS.close(ps);
             DBMS.close(rs);
             DBMS.close(conn);
-            ApplicationServer.close(ctx);
         }
     }
 
