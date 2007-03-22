90826026e3216bccc099ea7020edab5f016dfaf1
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-22 17:05:29 +0000

    trim out unused stuff

--- src/main/com/topcoder/common/web/util/DateTime.java
+++ src/main/com/topcoder/common/web/util/DateTime.java
@@ -1,10 +1,5 @@
 package com.topcoder.common.web.util;
 
-import com.topcoder.ejb.Util.Util;
-import com.topcoder.shared.util.TCContext;
-import com.topcoder.web.common.BaseProcessor;
-
-import javax.naming.InitialContext;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.FieldPosition;
@@ -20,83 +15,11 @@ public final class DateTime {
     private static final char CORRECT_DELIMETER = '/';
     private static final char[] INCORRECT_DELIMETER = {'-', '.'};
     private static final String[] VALID_TIMESTAMP = {
-        "MM/dd/yy hh:mm a"
-        , "MM/dd/yyyy hh:mm a"
-        , "MM/dd/yy"
-        , "MM/dd/yyyy"
+            "MM/dd/yy hh:mm a"
+            , "MM/dd/yyyy hh:mm a"
+            , "MM/dd/yy"
+            , "MM/dd/yyyy"
     };
-
-
-    public static final java.sql.Date getCurrentDate() throws Exception {
-        java.sql.Date result = null;
-        InitialContext ctx = null;
-        try {
-            ctx = TCContext.getInitial();
-            Util util = (Util) BaseProcessor.createEJB(ctx, Util.class);
-            result = util.getCurrentDate();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new Exception("common.DateTime:getCurrentDate: Error " + e);
-        } finally {
-            if (ctx != null) {
-                try {
-                    ctx.close();
-                } catch (Exception ignore) {
-                }
-            }
-        }
-        return result;
-    }
-
-
-    public static final java.sql.Date getCurrentDate(java.sql.Connection conn) throws Exception {
-        java.sql.Date result = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            ps = conn.prepareStatement("SELECT CURRENT FROM dual");
-            rs = ps.executeQuery();
-            if (rs.next()) result = rs.getDate(1);
-        } catch (Exception e) {
-            throw new Exception("common.DateTime:getCurrentDate: Error " + e);
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (Exception ignore) {
-                }
-            }
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (Exception ignore) {
-                }
-            }
-        }
-        return result;
-    }
-
-
-    public static final java.sql.Time getCurrentTime() throws Exception {
-        java.sql.Time result = null;
-        InitialContext ctx = null;
-        try {
-            ctx = TCContext.getInitial();
-            Util util = (Util) BaseProcessor.createEJB(ctx, Util.class);
-            result = util.getCurrentTime();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new Exception("common.DateTime:getCurrentTime:ERROR:" + e);
-        } finally {
-            if (ctx != null) {
-                try {
-                    ctx.close();
-                } catch (Exception ignore) {
-                }
-            }
-        }
-        return result;
-    }
 
 
     public static final java.sql.Time getCurrentTime(java.sql.Connection conn) throws Exception {
@@ -107,55 +30,6 @@ public final class DateTime {
             ps = conn.prepareStatement("SELECT CURRENT FROM dual");
             rs = ps.executeQuery();
             if (rs.next()) result = rs.getTime(1);
-        } catch (Exception e) {
-            throw new Exception("common.DateTime:getCurrentDate: Error " + e);
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (Exception ignore) {
-                }
-            }
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (Exception ignore) {
-                }
-            }
-        }
-        return result;
-    }
-
-
-    public static final java.sql.Timestamp getCurrentTimestamp() throws Exception {
-        java.sql.Timestamp result = null;
-        InitialContext ctx = null;
-        try {
-            ctx = TCContext.getInitial();
-            Util util = (Util) BaseProcessor.createEJB(ctx, Util.class);
-            result = util.getCurrentTimestamp();
-        } catch (Exception e) {
-            throw new Exception("common.DateTime:getCurrentTimestamp: Error " + e);
-        } finally {
-            if (ctx != null) {
-                try {
-                    ctx.close();
-                } catch (Exception ignore) {
-                }
-            }
-        }
-        return result;
-    }
-
-    public static final java.sql.Timestamp getCurrentTimestamp(java.sql.Connection conn)
-            throws Exception {
-        java.sql.Timestamp result = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            ps = conn.prepareStatement("SELECT CURRENT FROM dual");
-            rs = ps.executeQuery();
-            if (rs.next()) result = rs.getTimestamp(1);
         } catch (Exception e) {
             throw new Exception("common.DateTime:getCurrentDate: Error " + e);
         } finally {
