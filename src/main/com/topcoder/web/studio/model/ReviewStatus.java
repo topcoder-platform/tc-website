6f279741f79697da598dae41f7955305216c37d8
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-03-28 17:44:04 +0000

    add

--- src/main/com/topcoder/web/studio/model/ReviewStatus.java
+++ src/main/com/topcoder/web/studio/model/ReviewStatus.java
@@ -8,8 +8,9 @@ import com.topcoder.web.common.model.Base;
  *          Create Date: Jun 27, 2006
  */
 public class ReviewStatus extends Base {
-    public static final Integer PASSED = new Integer(1);
-    public static final Integer FAILED = new Integer(2);
+    public static final Integer PASSED = 1;
+    public static final Integer FAILED = 2;
+    public static final Integer CHEATED = 3;
     private Integer id;
     private String description;
 
