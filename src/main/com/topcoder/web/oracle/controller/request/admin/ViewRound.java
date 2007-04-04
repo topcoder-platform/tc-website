afae13d6d13387fe94ed5c3e34e72029717db42a
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-04-04 21:49:04 +0000

    add contst id stuff

--- src/main/com/topcoder/web/oracle/controller/request/admin/ViewRound.java
+++ src/main/com/topcoder/web/oracle/controller/request/admin/ViewRound.java
@@ -1,8 +1,10 @@
 package com.topcoder.web.oracle.controller.request.admin;
 
+import com.topcoder.web.common.NavigationException;
 import com.topcoder.web.common.StringUtils;
 import com.topcoder.web.oracle.Constants;
 import com.topcoder.web.oracle.dao.OracleDAOUtil;
+import com.topcoder.web.oracle.model.Contest;
 import com.topcoder.web.oracle.model.Round;
 import com.topcoder.web.oracle.model.RoundStatus;
 
@@ -15,17 +17,29 @@ public class ViewRound extends Base {
 
     protected void dbProcessing() throws Exception {
         String roundId = getRequest().getParameter(Constants.ROUND_ID);
+        String contestId = getRequest().getParameter(Constants.CONTEST_ID);
 
-        if (!"".equals(StringUtils.checkNull(roundId))) {
-            //load
-            Round round = OracleDAOUtil.getFactory().getRoundDAO().find(new Integer(roundId));
-            loadEditRoundData(round);
+        if (!"".equals(StringUtils.checkNull(contestId))) {
+            Contest c = OracleDAOUtil.getFactory().getContestDAO().find(new Integer(contestId));
+            if (c==null) {
+                throw new NavigationException("Invalid Contest specified");
+            } else {
+                if (!"".equals(StringUtils.checkNull(roundId))) {
+                    //load
+                    Round round = OracleDAOUtil.getFactory().getRoundDAO().find(new Integer(roundId));
+                    loadEditRoundData(round);
+                } else {
+                    setDefault(Constants.ROUND_STATUS_ID, RoundStatus.UNACTIVE);
+                    loadGeneralEditRoundData();
+                }
+                setDefault(Constants.CONTEST_ID, c.getId());
+                setNextPage("/admin/editRound.jsp");
+                setIsNextPageInContext(true);
+            }
         } else {
-            setDefault(Constants.ROUND_STATUS_ID, RoundStatus.UNACTIVE);
-            loadGeneralEditRoundData();
+            throw new NavigationException("No Contest specified");
         }
-        setNextPage("/admin/editRound.jsp");
-        setIsNextPageInContext(true);
+
 
     }
 }
