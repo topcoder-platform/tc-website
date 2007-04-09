0f7abb1865d6e573fe45f64504d751ab62b36e7f
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-04-09 17:37:08 +0000

    don't inerit from Base

--- src/main/com/topcoder/web/reg/controller/request/Logout.java
+++ src/main/com/topcoder/web/reg/controller/request/Logout.java
@@ -1,26 +1,24 @@
 package com.topcoder.web.reg.controller.request;
 
 import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.web.common.BaseProcessor;
 import com.topcoder.web.common.BaseServlet;
-import com.topcoder.web.common.TCWebException;
 
 /**
  * @author dok
  * @version $Revision$ Date: 2005/01/01 00:00:00
  *          Create Date: Apr 27, 2006
  */
-public class Logout extends Base {
+public class Logout extends BaseProcessor {
 
     protected static final Logger log = Logger.getLogger(Logout.class);
 
-    protected void registrationProcessing() throws TCWebException {
-
+    protected void businessProcessing() throws Exception {
         // Logs the user out
         getAuthentication().logout();
 
         // Invalidates the session
         getRequest().getSession().invalidate();
-        getRequest().getSession(true);
 
         // Go to active contests page
         String nextpage = (String)getRequest().getAttribute(BaseServlet.NEXT_PAGE_KEY);
@@ -30,5 +28,4 @@ public class Logout extends Base {
         setNextPage(nextpage);
         setIsNextPageInContext(false);
     }
-
 }
