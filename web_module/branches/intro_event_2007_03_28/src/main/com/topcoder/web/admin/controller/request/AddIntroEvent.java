abd96e1888afce4eb04a3e86f00f855e805971e1
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-04-16 12:55:48 +0000

    *** empty log message ***

---
+++ web_module/branches/intro_event_2007_03_28/src/main/com/topcoder/web/admin/controller/request/AddIntroEvent.java
@@ -0,0 +1,24 @@
+package com.topcoder.web.admin.controller.request;
+
+import java.util.List;
+
+import com.topcoder.web.common.ShortHibernateProcessor;
+import com.topcoder.web.common.dao.DAOUtil;
+
+/**
+ * @author cucu
+ */
+public class AddIntroEvent extends ShortHibernateProcessor {
+
+    @Override
+    protected void dbProcessing() throws Exception {
+
+        List tz = DAOUtil.getFactory().getTimeZoneDAO().getTimeZones();
+        getRequest().setAttribute("timezones", tz)
+        
+        setNextPage("admin/editIntroEvent.jsp");
+        setIsNextPageInContext(true);
+    }
+
+
+}
