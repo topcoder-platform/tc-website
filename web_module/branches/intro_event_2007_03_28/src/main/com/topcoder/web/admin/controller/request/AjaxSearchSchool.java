8fa4f95f73b126d6aee086839c300d8f4bfbbb56
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-04-20 13:45:30 +0000

    *** empty log message ***

--- web_module/branches/intro_event_2007_03_28/src/main/com/topcoder/web/admin/controller/request/AjaxSearchSchool.java
+++ web_module/branches/intro_event_2007_03_28/src/main/com/topcoder/web/admin/controller/request/AjaxSearchSchool.java
@@ -1,15 +1,18 @@
 package com.topcoder.web.admin.controller.request;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.topcoder.web.common.ShortHibernateProcessor;
 import com.topcoder.web.common.dao.DAOUtil;
+import com.topcoder.web.common.model.School;
 
 /**
  * @author cucu
  */
 public class AjaxSearchSchool extends ShortHibernateProcessor {
 
+    @SuppressWarnings("unchecked")
     @Override
     protected void dbProcessing() throws Exception {
         String search = getRequest().getParameter("school_search");
@@ -17,7 +20,16 @@ public class AjaxSearchSchool extends ShortHibernateProcessor {
         boolean searchAgain = getRequest().getParameter("search_again") != null;
         
         if (!searchAgain) {
-            List schools = DAOUtil.getFactory().getSchoolDAO().searchByName(search, 100);
+            List<Object[]> list = DAOUtil.getFactory().getSchoolDAO().searchByName(search, 100);
+            
+            List<School> schools = new ArrayList<School>();
+            for(Object[] o : list) {
+                Integer count = (Integer) o[0];
+                School school = (School) o[1];
+                school.setName(school.getName() + " (" + count + ")");
+                
+                schools.add(school);
+            }
             getRequest().setAttribute("schools", schools);
         }
         
