9c5d4b64dc0cfc311eccc3862ea6cc6cd14b973b
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-02 16:53:52 +0000

    *** empty log message ***

--- web_module/branches/intro_event_2007_03_28/src/main/com/topcoder/web/admin/controller/request/EditIntroEvent.java
+++ web_module/branches/intro_event_2007_03_28/src/main/com/topcoder/web/admin/controller/request/EditIntroEvent.java
@@ -1,9 +1,7 @@
 package com.topcoder.web.admin.controller.request;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import com.topcoder.web.common.DateUtils;
 import com.topcoder.web.common.NavigationException;
