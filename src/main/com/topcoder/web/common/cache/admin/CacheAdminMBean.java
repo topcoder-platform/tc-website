a43d88325f5ff8b967bc0661f90bd6e99ec6ed64
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-02 14:11:27 +0000

    add

---
+++ src/main/com/topcoder/web/common/cache/admin/CacheAdminMBean.java
@@ -0,0 +1,14 @@
+package com.topcoder.web.common.cache.admin;
+
+import org.jboss.system.ServiceMBean;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: May 2, 2007
+ */
+public interface CacheAdminMBean extends ServiceMBean {
+    public void clear();
+
+    public void removelike(String s);
+}
