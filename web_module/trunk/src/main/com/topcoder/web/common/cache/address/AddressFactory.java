9c33989d0e70bbc4508e352ae8b63135b41f11d2
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-08 13:57:35 +0000

    add some support for dist cache

--- web_module/trunk/src/main/com/topcoder/web/common/cache/address/AddressFactory.java
+++ web_module/trunk/src/main/com/topcoder/web/common/cache/address/AddressFactory.java
@@ -2,9 +2,9 @@ package com.topcoder.web.common.cache.address;
 
 import com.topcoder.security.TCSubject;
 import com.topcoder.shared.dataAccess.RequestInt;
+import com.topcoder.shared.util.TCResourceBundle;
+import com.topcoder.web.common.cache.JbossCacheClient;
 import com.topcoder.web.common.cache.MaxAge;
-import com.topcoder.web.common.cache.address.jboss.RequestAddress;
-import com.topcoder.web.common.cache.address.jboss.TCSubjectAddress;
 
 /**
  * @author dok
@@ -15,20 +15,39 @@ public class AddressFactory {
     //add config so that we can swap out implementations easily.
     // between the different cache implementations
 
+    private static final TCResourceBundle bundle = new TCResourceBundle("cache");
+    private static final boolean isJbossCache = JbossCacheClient.class.getName().equals(bundle.getProperty("cache_client_class"));
+
     public static CacheAddress create(RequestInt request) {
-        return new RequestAddress(request);
+        if (isJbossCache) {
+            return new com.topcoder.web.common.cache.address.jboss.RequestAddress(request);
+        } else {
+            return new com.topcoder.web.common.cache.address.dist.RequestAddress(request);
+        }
     }
 
     public static CacheAddress create(RequestInt request, MaxAge maxAge) {
-        return new RequestAddress(request, maxAge);
+        if (isJbossCache) {
+            return new com.topcoder.web.common.cache.address.jboss.RequestAddress(request, maxAge);
+        } else {
+            return new com.topcoder.web.common.cache.address.dist.RequestAddress(request);
+        }
     }
 
     public static CacheAddress create(TCSubject subject, String dataSource) {
-        return new TCSubjectAddress(subject, dataSource);
+        if (isJbossCache) {
+            return new com.topcoder.web.common.cache.address.jboss.TCSubjectAddress(subject, dataSource);
+        } else {
+            return new com.topcoder.web.common.cache.address.dist.TCSubjectAddress(subject, dataSource);
+        }
     }
 
     public static CacheAddress create(TCSubject subject, String dataSource, MaxAge maxAge) {
-        return new TCSubjectAddress(subject, dataSource, maxAge);
+        if (isJbossCache) {
+            return new com.topcoder.web.common.cache.address.jboss.TCSubjectAddress(subject, dataSource, maxAge);
+        } else {
+            return new com.topcoder.web.common.cache.address.dist.TCSubjectAddress(subject, dataSource);
+        }
     }
 
 }
