496c4fd46ed982069a82ef7e5f18cd75ba7403e3
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-11 20:28:56 +0000

    add

---
+++ web_module/trunk/src/main/com/topcoder/web/oracle/controller/request/admin/ViewCandidates.java
@@ -0,0 +1,40 @@
+package com.topcoder.web.oracle.controller.request.admin;
+
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.oracle.Constants;
+import com.topcoder.web.oracle.dao.OracleDAOFactory;
+import com.topcoder.web.oracle.dao.OracleDAOUtil;
+import com.topcoder.web.oracle.model.Round;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: May 11, 2007
+ */
+public class ViewCandidates extends Base {
+    protected void dbProcessing() throws Exception {
+        //load up all the candidates in the rounds
+
+
+        String roundId = getRequest().getParameter(Constants.ROUND_ID);
+
+        OracleDAOFactory f = OracleDAOUtil.getFactory();
+
+        Round round = null;
+
+        if ("".equals(StringUtils.checkNull(roundId))) {
+            throw new NavigationException("Invalid round specified.");
+        } else {
+            round = f.getRoundDAO().find(new Integer(roundId));
+            if (round == null) {
+                throw new NavigationException("Invalid round specified.");
+            } else {
+                getRequest().setAttribute("candidates", f.getCandidateDAO().getCandidates(round));
+            }
+        }
+
+        setNextPage("/admin/viewCandidates.jsp");
+        setIsNextPageInContext(true);
+    }
+}
