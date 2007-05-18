64680210787f496d3e08a8e84c7fef03b9323c9f
nobody <nobody@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-18 13:42:11 +0000

    This commit was manufactured by cvs2svn to create branch 'queue_wackaroo_2007_05_18'.

---
+++ web_module/branches/queue_wackaroo_2007_05_18/src/main/com/topcoder/web/common/model/algo/Contest.java
@@ -0,0 +1,50 @@
+package com.topcoder.web.common.model.algo;
+
+import java.sql.Timestamp;
+
+import com.topcoder.web.common.model.Base;
+
+/**
+ * @author cucu
+= */
+@SuppressWarnings("serial")
+public class Contest extends Base {
+
+    protected Long id = null;    
+    protected String name = null;
+    protected Timestamp startDate = null;    
+    protected Timestamp endDate = null;    
+    protected String status = null;
+    
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(Timestamp endDate) {
+        this.endDate = endDate;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(Timestamp startDate) {
+        this.startDate = startDate;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
