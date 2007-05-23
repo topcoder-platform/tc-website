899d838fbadb4401d371e5e7ffad7a8ad0eeae37
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-23 12:36:13 +0000

    extend httpservletrequest

--- web_module/trunk/src/main/com/topcoder/web/common/TCRequest.java
+++ web_module/trunk/src/main/com/topcoder/web/common/TCRequest.java
@@ -1,46 +1,6 @@
 package com.topcoder.web.common;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpSession;
-import java.util.Enumeration;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 
-public interface TCRequest {
-    String getParameter(String name);
-
-    String getServletPath();
-
-    String getContextPath();
-
-    void setAttribute(String name, Object o);
-
-    Object getAttribute(String name);
-
-    String getMethod();
-
-    Enumeration getParameterNames();
-
-    String getQueryString();
-
-    String[] getParameterValues(String name);
-
-    HttpSession getSession();
-
-    HttpSession getSession(boolean create);
-
-    String getServerName();
-
-    Cookie[] getCookies();
-
-    String getProtocol();
-
-    String getRemoteAddr();
-
-    String getRemoteHost();
-
-    void removeAttribute(java.lang.String name);
-
-    Map getParameterMap();
-
-    String getHeader(String s);
+public interface TCRequest extends HttpServletRequest {
 }
