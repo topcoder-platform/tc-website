88354df14596dee136ce73bb8fc6921131981ef8
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-23 13:43:24 +0000

    implement httpservletrequestwrapper

--- web_module/trunk/src/main/com/topcoder/web/common/TCResponse.java
+++ web_module/trunk/src/main/com/topcoder/web/common/TCResponse.java
@@ -1,35 +1,11 @@
 package com.topcoder.web.common;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.Cookie;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author dok
- * Date: Jan 17, 2004
+ *         Date: Jan 17, 2004
  */
-public interface TCResponse {
+public interface TCResponse extends HttpServletResponse {
 
-    void addCookie(Cookie cookie);
-
-    String encodeRedirectURL(String url);
-
-    String encodeURL(String url);
-
-    void sendRedirect(String location) throws IOException;
-
-    ServletOutputStream getOutputStream() throws IOException;
-
-    PrintWriter getWriter() throws IOException;
-
-    boolean isCommitted();
-
-    void setContentType(String type);
-
-    void flushBuffer() throws IOException;
-
-    void addHeader(String header, String val);
-
-    void setStatus(int i);
 }
