7f904897c417b603d58900a46c4f975dec1b3b1e
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-28 19:46:12 +0000

    Pacts status reengeneering pre-merge

--- web_module/branches/pacts_status_2007_04_13_merge/src/main/com/topcoder/web/ejb/pacts/PactsServicesBean.java
+++ web_module/branches/pacts_status_2007_04_13_merge/src/main/com/topcoder/web/ejb/pacts/PactsServicesBean.java
@@ -2673,7 +2673,7 @@ public class PactsServicesBean extends BaseEJB implements PactsConstants {
         selectHeaders.append("SELECT p.payment_id, pd.payment_desc, pd.payment_type_id, pd.payment_method_id, p.create_date, pd.create_date as modify_date,  ");
         selectHeaders.append("pt.payment_type_desc, pm.payment_method_desc, pd.net_amount, pd.payment_status_id, s.payment_status_desc, ");
         selectHeaders.append("p.user_id, u.handle, u.first_name, u.middle_name, u.last_name, ");
-        selectHeaders.append("pd.date_modified, pd.gross_amount, pd.client, nvl(pdsrx.payment_status_reason_id, 0), ");
+        selectHeaders.append("pd.date_modified, pd.gross_amount, pd.client, nvl(pdsrx.payment_status_reason_id, 0) as payment_status_reason_id, ");
         selectHeaders.append("pd.algorithm_round_id, pd.component_project_id, pd.algorithm_problem_id, ");
         selectHeaders.append("pd.studio_contest_id, pd.component_contest_id, pd.digital_run_stage_id, ");
         selectHeaders.append("pd.digital_run_season_id, pd.parent_payment_id, pd.total_amount, pd.installment_number ");
