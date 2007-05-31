ae1e679b6211be25a9492383c57d751326a30c7a
nobody <nobody@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-31 14:18:32 +0000

    This commit was manufactured by cvs2svn to create tag 'before_merge_web_pacts_status_2007_04_13'.

---
+++ web_module/tags/before_merge_web_pacts_status_2007_04_13/src/main/com/topcoder/web/common/model/IdGenerator.java
@@ -0,0 +1,62 @@
+package com.topcoder.web.common.model;
+
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.web.common.IdGeneratorClient;
+import com.topcoder.web.common.StringUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.SessionImplementor;
+import org.hibernate.id.Configurable;
+import org.hibernate.id.IdentifierGenerator;
+import org.hibernate.type.Type;
+
+import java.io.Serializable;
+import java.util.Properties;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Mar 31, 2006
+ */
+public class IdGenerator implements IdentifierGenerator, Configurable {
+    protected static final Logger log = Logger.getLogger(IdGenerator.class);
+    public String SEQUENCE_NAME = "sequence_name";
+    public String RETURN_CLASS = "return_class";
+
+    private String seqName = null;
+    private Class returnClass = null;
+
+    public void configure(Type type, Properties params, Dialect d) {
+        seqName = (String) params.get(SEQUENCE_NAME);
+        String className = (String) params.get(RETURN_CLASS);
+        if ("".equals(StringUtils.checkNull(className))) {
+            returnClass = Long.class;
+        } else {
+            try {
+                returnClass = Class.forName(className);
+            } catch (ClassNotFoundException e) {
+                //if the user screwed up the config, just use Long
+                log.warn("ClassNotFound: " + params.get(RETURN_CLASS) + ", using Long instead");
+                returnClass = Long.class;
+            }
+        }
+    }
+
+    public Serializable generate(SessionImplementor session, Object object) throws HibernateException {
+        if (seqName == null) {
+            throw new HibernateException("Sequence Name not specified in configuration.");
+        }
+        try {
+            if (log.isDebugEnabled()) {
+                log.debug("our return class will be " + returnClass.getName());
+            }
+            long id = IdGeneratorClient.getSeqId(seqName);
+            return (Number)returnClass.getConstructor(new Class[]{String.class}).newInstance(new String[]{String.valueOf(id)});
+        } catch (Exception e) {
+            throw new HibernateException(e);
+        }
+
+
+    }
+}
+
---
+++ web_module/tags/before_merge_web_pacts_status_2007_04_13/src/main/com/topcoder/web/tc/view/dev/support/diag_javaUseCase.jsp
@@ -0,0 +1,41 @@
+<%@  page language="java"  %>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<head>
+<title>Development FAQ</title>
+
+<jsp:include page="../../script.jsp" />
+
+</head>
+
+<body>
+
+<table align=left cellpadding=0 cellspacing=10 class=bodyText>
+	<tr>
+		<td colspan=2>
+		<p class=bodyText>
+		The following is the standard color scheme found in this diagram.  The coordinates, given in<br/>
+		(row, column) format, refer to the standard Poseidon color palette grid.  All coordinates are<br/>
+		1-based. For example, (1,1) refers to the color selection in the upper left corner of the palette.
+		</p>
+		</td>
+	</tr>
+	</tr>
+		<td valign=top nowrap=nowrap>
+		<strong>Use Case</strong> (3, 9)<br/>
+		<strong>Aggregate Use Case</strong> (3, 1)<br/>
+		</td>
+		<td valign=top nowrap=nowrap align=left width="100%">
+		<strong>External Component Use Case</strong> (2, 6)<br/>
+		<strong>Note</strong> (2, 5)
+		</td>
+	</tr>
+	<tr>
+		<td colspan=2>
+		<img src="/i/development/UML_Standards_Java_Use_Case_Diagram.gif" border=0/>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
