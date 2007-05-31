839ea2e8bee777ac6a20fd476dc652f25df02874
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-31 17:14:51 +0000

    fix bad merge

--- src/main/com/topcoder/web/ejb/pacts/PactsServicesBean.java
+++ src/main/com/topcoder/web/ejb/pacts/PactsServicesBean.java
@@ -5830,6 +5830,9 @@ public class PactsServicesBean extends BaseEJB implements PactsConstants {
                           query.append("  pd.digital_run_stage_id = ? OR ");
                           query.append("  pd.digital_run_season_id = ? OR ");
                           query.append("  pd.parent_payment_id = ?)");
+                          for (int j = 0; j < 8; j++) {
+                              objects.add(value);
+                          }                          
                       } else if (key.equals(PAYMENT_STATUS_ID)) {
                           query.append("AND pd.payment_status_id = ? ");
                           objects.add(value);
