6975a656b9808cb6b38415997336199079ef8dbb
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-07-17 21:49:04 +0000

    less debug

--- web_module/branches/studio_or_changes_2007_07_11/src/main/com/topcoder/web/studio/model/Prize.java
+++ web_module/branches/studio_or_changes_2007_07_11/src/main/com/topcoder/web/studio/model/Prize.java
@@ -78,9 +78,6 @@ public class Prize extends Base implements Comparable {
         if (this == o) return true;
         if (!(o instanceof Prize)) return false;
         final Prize that = (Prize) o;
-        if (log.isDebugEnabled()) {
-            log.debug("XXX equals called : " + this.toString() + "    " + that.toString());
-        }
         if (this.getType().equals(that.getType())) {
             if (this.getPlace() != null && that.getPlace() != null) {
                 return this.getPlace().equals(that.getPlace()) && this.getAmount().equals(that.getAmount());
@@ -102,9 +99,7 @@ public class Prize extends Base implements Comparable {
         buf.append(getType().hashCode()).append(" ");
         buf.append(getPlace()).append(" ");
         buf.append(getAmount());
-        if (log.isDebugEnabled()) {
-            log.debug("XXX hashcode called " + buf + " " + buf.toString().hashCode() + " " + buf.hashCode());
-        }
+        //have to use the string's hashcode here because the string builder's will change
         return buf.toString().hashCode();
     }
 
