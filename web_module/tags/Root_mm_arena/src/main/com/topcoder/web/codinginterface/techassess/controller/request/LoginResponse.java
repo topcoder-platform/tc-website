cac04cff3f6063e02a57a529568bcf19c9c9af74
nobody <nobody@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-07-19 17:06:11 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_mm_arena'.

---
+++ web_module/tags/Root_mm_arena/src/main/com/topcoder/web/codinginterface/techassess/controller/request/LoginResponse.java
@@ -0,0 +1,41 @@
+package com.topcoder.web.codinginterface.techassess.controller.request;
+
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.web.codinginterface.techassess.Constants;
+
+/**
+ * User: dok
+ * Date: Dec 17, 2004
+ */
+public class LoginResponse extends Base {
+
+    protected static final Logger log = Logger.getLogger(LoginResponse.class);
+
+    protected void techAssessProcessing() throws Exception {
+        setNextPage(buildProcessorRequestString(Constants.RP_INDEX, null, null));
+        setIsNextPageInContext(false);
+
+        if (hasParameter(Constants.MESSAGE_ID)) {
+            String messageId = getRequest().getParameter(Constants.MESSAGE_ID);
+            loadSessionErrorsIntoRequest(messageId);
+            loadUserMessageIntoRequest(messageId);
+            loadSessionDefaultsIntoRequest(messageId);
+            //no handle default means that they are just loading up the login page to login
+            //the login processor will put the company id in the url for all of its redirects
+            //if it's not there, it is a likely signal of a page refresh
+            log.debug("fresh: " + getDefault(Constants.FRESH_REQUEST));
+            if (String.valueOf(true).equals(getDefault(Constants.FRESH_REQUEST))) {
+                if (hasErrors()||!hasDefault(Constants.HANDLE)) {
+                    setNextPage(Constants.PAGE_LOGIN);
+                    setIsNextPageInContext(true);
+                }
+            } else {
+                //this most likely means they hit refresh on the response page
+                setNextPage(buildProcessorRequestString(Constants.RP_LOGIN,
+                        new String[]{Constants.COMPANY_ID}, new String[]{String.valueOf(getCompanyId())}));
+                setIsNextPageInContext(false);
+
+            }
+        }
+    }
+}
