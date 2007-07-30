82c46d5452b375ff683e5a9a71b5d531a052f419
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-07-30 20:06:28 +0000

    add professor nomination stuff

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/tournament/tccc07/SubmitProfessorNomination.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/tournament/tccc07/SubmitProfessorNomination.java
@@ -1,31 +1,94 @@
 package com.topcoder.web.tc.controller.request.tournament.tccc07;
 
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.common.dao.DAOUtil;
+import com.topcoder.web.common.model.Event;
+import com.topcoder.web.common.validation.NonEmptyValidator;
+import com.topcoder.web.common.validation.StringInput;
+import com.topcoder.web.common.validation.ValidationResult;
+import com.topcoder.web.tc.Constants;
+import com.topcoder.web.tc.dao.TCDAOUtil;
+import com.topcoder.web.tc.model.Nomination;
+import com.topcoder.web.tc.validation.NominationValidator;
+
 /**
  * @author dok
  * @version $Revision$ Date: 2005/01/01 00:00:00
  *          Create Date: Jul 26, 2007
  */
-public class SubmitProfessorNomination {
-    //take professor name
-    //professor university
-    //university website
-    //field of instruction
-    //professor email address
-    //description of why professor deserves this award
-    //send email to professor letting them know they've been nominated
+public class SubmitProfessorNomination extends ViewProfessorNomination {
+    protected void profProcessing(Event e) throws Exception {
 
-    /*
-   nomination
-   ---------
-   nomination_id
-   nominator_id
-   name
-   email_address
-   school_name
-   field
-   school_url
-   nomination_text
+        String name = getRequest().getParameter(Constants.FULL_NAME);
+        String school = getRequest().getParameter(Constants.SCHOOL_NAME);
+        String url = getRequest().getParameter(Constants.URL);
+        String field = getRequest().getParameter(Constants.FIELD_OF_INSTRUCTION);
+        String otherField = getRequest().getParameter(Constants.OTHER_FIELD_OF_INSTRUCTION);
+        String email = getRequest().getParameter(Constants.EMAIL);
+        String text = getRequest().getParameter(Constants.NOMINATION_TEXT);
+
+        ValidationResult nameResult = new NonEmptyValidator("Please enter your professor's name.").validate(new StringInput(name));
+        if (!nameResult.isValid()) {
+            addError(Constants.FULL_NAME, nameResult.getMessage());
+        }
+
+        ValidationResult schoolResult = new NonEmptyValidator("Please enter your professor's school.").validate(new StringInput(school));
+        if (!schoolResult.isValid()) {
+            addError(Constants.SCHOOL_NAME, schoolResult.getMessage());
+        }
+
+        ValidationResult fieldResult = new NonEmptyValidator("Please choose your professor's field of instruction.").validate(new StringInput(field));
+        if (!fieldResult.isValid()) {
+            addError(Constants.FIELD_OF_INSTRUCTION, fieldResult.getMessage());
+        }
+
+        if ("Other".equals(field)) {
+            ValidationResult otherFieldResult = new NonEmptyValidator("Please enter your professor's field of instruction.").validate(new StringInput(otherField));
+            if (!otherFieldResult.isValid()) {
+                addError(Constants.FIELD_OF_INSTRUCTION, otherFieldResult.getMessage());
+            }
+        }
+
+        ValidationResult emailResult = new NonEmptyValidator("Please enter your professor's email address.").validate(new StringInput(email));
+        if (!emailResult.isValid()) {
+            addError(Constants.EMAIL, emailResult.getMessage());
+        }
+
+        ValidationResult textResult = new NominationValidator().validate(new StringInput(text));
+        if (!textResult.isValid()) {
+            addError(Constants.NOMINATION_TEXT, textResult.getMessage());
+        }
+
+        if (hasErrors()) {
+            setDefault(Constants.FULL_NAME, name);
+            setDefault(Constants.SCHOOL_NAME, school);
+            setDefault(Constants.URL, url);
+            setDefault(Constants.FIELD_OF_INSTRUCTION, field);
+            setDefault(Constants.OTHER_FIELD_OF_INSTRUCTION, otherField);
+            setDefault(Constants.EMAIL, email);
+            setDefault(Constants.NOMINATION_TEXT, text);
+        } else {
+            Nomination n = new Nomination();
+            n.setEvent(e);
+            n.setEmailAddress(email);
+            if ("Other".equals(field)) {
+                n.setFieldOfInstruction(otherField);
+            } else {
+                n.setFieldOfInstruction(field);
+            }
+            n.setName(name);
+            n.setNominator(DAOUtil.getFactory().getUserDAO().find(getUser().getId()));
+            n.setSchoolName(school);
+            if ("".equals(StringUtils.checkNull(url).trim())) {
+                n.setSchoolURL(url);
+            }
+            n.setText(text);
+            TCDAOUtil.getFactory().getNominationDAO().saveOrUpdate(n);
+
+            setNextPage("/tc?module=Static&d1=tournaments&d2=tccc07&d3=professor&d4=nominationSuccess");
+            setIsNextPageInContext(false);
+        }
 
 
-    */
+    }
 }
