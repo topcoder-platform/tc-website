032795dc92bc701a669b17e307ec3798225efcdd
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-08-03 21:47:03 +0000

    tccc07 results pages

--- web_module/branches/tccc07site_results_2007_07_27_updated/src/main/com/topcoder/web/tc/controller/request/tournament/tccc07/ContestDetails.java
+++ web_module/branches/tccc07site_results_2007_07_27_updated/src/main/com/topcoder/web/tc/controller/request/tournament/tccc07/ContestDetails.java
@@ -1,187 +1,32 @@
 package com.topcoder.web.tc.controller.request.tournament.tccc07;
 
-import com.topcoder.shared.dataAccess.DataAccessInt;
-import com.topcoder.shared.dataAccess.Request;
-import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
-import com.topcoder.shared.util.DBMS;
-import com.topcoder.web.common.TCWebException;
-import com.topcoder.web.tc.controller.request.development.StatBase;
-import com.topcoder.web.tc.model.UserContestDetail;
-
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Map;
+import com.topcoder.web.tc.controller.request.tournament.ContestDetailsBase;
 
 /**
  * @author dok, pulky
  * @version $Revision$ Date: 2005/01/01 00:00:00
  *          Create Date: Mar 1, 2007
  */
-public class ContestDetails extends StatBase {
-
+public class ContestDetails extends ContestDetailsBase {
     final String CONTEST_PREFIX = "tccc07";
-    
-    protected String getCommandName() {
-        return "contest_projects";
+
+    final int[] placementPoints = new int[]{10, 7, 5, 4, 0};
+
+    final int MAX_NUMBER_PROJECTS = 4;
+
+    @Override
+    public int[] getPlacementPoints() {
+        return placementPoints;
     }
 
-    protected String getDataSourceName() {
-        return DBMS.TCS_OLTP_DATASOURCE_NAME;
+    @Override
+    public int getMax() {
+        return MAX_NUMBER_PROJECTS;
     }
 
-    protected String getPageName() {
-        return "/tournaments/" + CONTEST_PREFIX + "/component/contestDetails.jsp";
-    }
-
-    private ArrayList arr = new ArrayList();
-
-    int getMax() {
-        return 4;
-    }
-
-    protected void statProcessing() throws com.topcoder.web.common.TCWebException {
-        Map result2 = (Map) getRequest().getAttribute("resultMap");
-
-        ResultSetContainer rsc = (ResultSetContainer) result2.get("contest_projects");
-
-        DecimalFormat dfmt = new DecimalFormat("$#,##0.00");
-
-        int[] placementPoints = new int[]{10, 7, 5, 4, 0};
-
-        log.debug("rscDetails.size(): (1) " + rsc.size());
-        for (int i = 0; i < rsc.size(); i++) {
-            //for each contest, get details and build array
-            Request dataRequest = new Request();
-            Map result;
-            try {
-                dataRequest.setContentHandle("project_results_all");
-//                dataRequest.setContentHandle(CONTEST_PREFIX + "_project_results_all");
-                dataRequest.setProperty("ct", getRequest().getParameter("ct"));
-                dataRequest.setProperty("pj", String.valueOf(rsc.getIntItem(i, "project_id")));
-                dataRequest.setProperty("eid", getRequest().getParameter("eid"));
-
-                DataAccessInt dai = getDataAccess(DBMS.TCS_DW_DATASOURCE_NAME, true);
-                result = dai.getData(dataRequest);
-
-            } catch (Exception e) {
-                throw new TCWebException(e);
-            }
-
-//            ResultSetContainer rscDetails = (ResultSetContainer) result.get(CONTEST_PREFIX + "_project_results_all");
-            ResultSetContainer rscDetails = (ResultSetContainer) result.get("project_results_all");
-            ResultSetContainer rscComplete = (ResultSetContainer) result.get("project_details");
-            boolean isComplete = false;
-            if (!rscComplete.isEmpty() && rscComplete.getIntItem(0, "complete_status") == 1) {
-                isComplete = true;
-            }
-
-            log.debug("rscDetails.size(): " + rscDetails.size());
-            for (int j = 0; j < rscDetails.size(); j++) {
-                int pts = 0;
-
-                if (rscDetails.getItem(j, "final_score").getResultData() != null) {
-                    if (rscDetails.getDoubleItem(j, "final_score") >= 75) {
-                        if (j < placementPoints.length) {
-                            pts = placementPoints[j];
-                        }
-                    }
-                }
-
-                addPoints(rscDetails.getStringItem(j, "handle"), rscDetails.getIntItem(j, "user_id"), pts,
-                        isComplete, rscDetails.getIntItem(j, "submit_ind") == 1);
-            }
-        }
-
-        //add prizes
-        Request dataRequest = new Request();
-        Map result;
-        try {
-//            dataRequest.setContentHandle(CONTEST_PREFIX + "_contest_prizes");
-            dataRequest.setContentHandle("contest_prizes");
-            dataRequest.setProperty("ct", getRequest().getParameter("ct"));
-
-            DataAccessInt dai = getDataAccess(DBMS.TCS_DW_DATASOURCE_NAME, true);
-            result = dai.getData(dataRequest);
-
-        } catch (Exception e) {
-            throw new TCWebException(e);
-        }
-
-//        ResultSetContainer rscPrizes = (ResultSetContainer) result.get(CONTEST_PREFIX + "_contest_prizes");
-        ResultSetContainer rscPrizes = (ResultSetContainer) result.get("contest_prizes");
-
-        for (int i = 0; i < rscPrizes.size(); i++) {
-            addPrize(rscPrizes.getIntItem(i, "user_id"), dfmt.format(rscPrizes.getDoubleItem(i, "prize_payment")));
-        }
-
-        Collections.sort(arr, new myComparator());
-
-        getRequest().setAttribute("results", arr);
-
-    }
-
-    public void addPrize(int userId, String prz) {
-        log.debug("addPrize called : " + userId + " - " + prz);
-
-        UserContestDetail user = null;
-        for (int i = 0; i < arr.size(); i++) {
-            UserContestDetail item = (UserContestDetail) arr.get(i);
-            if (item.getUserID() == userId) {
-                user = item;
-                break;
-            }
-        }
-        
-        if (user != null) {
-            user.setPayment(prz);
-        } else {
-            log.debug("Null user!!");
-        }
-    }
-
-    public void addPoints(String handle, int userId, int pts, boolean completed, boolean isSubmitted) {
-        log.debug("addPoints called : " + handle + " - " + userId + " - " + pts + " - " + completed + " - " + isSubmitted);
-        UserContestDetail user = null;
-        for (int i = 0; i < arr.size(); i++) {
-            UserContestDetail item = (UserContestDetail) arr.get(i);
-            if (item.getUserID() == userId) {
-                user = item;
-                break;
-            }
-        }
-        if (user == null) {
-            user = new UserContestDetail(handle, userId);
-            log.debug("adding user: " + handle + " - " + userId);
-            arr.add(user);
-        }
-
-        user.addPoints(pts, getMax());
-        if (completed) {
-            user.setComplete(user.getComplete() + 1);
-        } else {
-            user.setIncomplete(user.getIncomplete() + 1);
-        }
-        if (isSubmitted) {
-            user.setSubmissionCount(user.getSubmissionCount() + 1);
-        }
-
-    }
-
-    public class myComparator implements Comparator {
-
-        public int compare(Object o1, Object o2) {
-            UserContestDetail a1 = (UserContestDetail) o1;
-            UserContestDetail a2 = (UserContestDetail) o2;
-
-            if (a1.getPoints() == a2.getPoints()) {
-                return a1.getHandle().compareToIgnoreCase(a2.getHandle());
-            }
-
-            return a2.getPoints() - a1.getPoints();
-        }
-
+    @Override
+    protected String getContestPrefix() {
+        return CONTEST_PREFIX;
     }
 
 }
--- web_module/branches/tccc07site_results_2007_07_27_updated/src/main/com/topcoder/web/tc/controller/request/tournament/tccc07/ContestProjects.java
+++ web_module/branches/tccc07site_results_2007_07_27_updated/src/main/com/topcoder/web/tc/controller/request/tournament/tccc07/ContestProjects.java
@@ -1,30 +1,18 @@
 package com.topcoder.web.tc.controller.request.tournament.tccc07;
 
-import com.topcoder.shared.util.DBMS;
-import com.topcoder.web.tc.controller.request.development.StatBase;
+import com.topcoder.web.tc.controller.request.tournament.ContestProjectsBase;
 
 /**
  * @author dok
  * @version $Revision$ Date: 2005/01/01 00:00:00
  *          Create Date: Mar 1, 2007
  */
-public class ContestProjects extends StatBase {
+public class ContestProjects extends ContestProjectsBase {
 
     final String CONTEST_PREFIX = "tccc07";
-    
-    protected String getCommandName() {
-        return "contest_projects";
-    }
 
-    protected String getDataSourceName() {
-        return DBMS.TCS_OLTP_DATASOURCE_NAME;
+    @Override
+    protected String getContestPrefix() {
+        return CONTEST_PREFIX;
     }
-
-    protected String getPageName() {
-        return "/tournaments/" + CONTEST_PREFIX + "/component/contestProjects.jsp";
-    }
-
-    protected void statProcessing() throws com.topcoder.web.common.TCWebException {
-    }
-
 }
