de073279c4d74caa7f3393da9d59f33d68938ce0
nobody <nobody@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-08-29 18:48:31 +0000

    This commit was manufactured by cvs2svn to create branch 'wiki_2007_08_29'.

---
+++ web_module/branches/wiki_2007_08_29/src/main/com/topcoder/web/common/tag/ListSelectTag.java
@@ -0,0 +1,60 @@
+package com.topcoder.web.common.tag;
+
+import javax.servlet.jsp.JspException;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author lars
+ */
+public class ListSelectTag extends SelectTag {
+    static public class Option {
+        private String v, t;
+        private boolean s;
+
+        public Option(String val, String text) {
+            this(val, text, false);
+        }
+
+        public Option(String val, String text, boolean selected) {
+            v = val;
+            t = text;
+            s = selected;
+        }
+    }
+
+    private List list = null;
+
+    public void setList(List l) {
+        list = l;
+    }
+
+    /**
+     * @see SelectTag#getOptionText(Object)
+     */
+    protected String getOptionText(Object o) {
+        Option t = (Option) o;
+        if (t.s) {
+            setSelectedText(t.t);
+        }
+        return t.t;
+    }
+
+    /**
+     * @see com.topcoder.web.common.tag.SelectTag#getOptionValue(java.lang.Object)
+     */
+    protected String getOptionValue(Object o) {
+        Option t = (Option) o;
+        if (t.s) {
+            setSelectedValue(t.v);
+        }
+        return t.v;
+    }
+
+    /**
+     * @see com.topcoder.web.common.tag.SelectTag#getSelectOptions()
+     */
+    protected Collection getSelectOptions() throws JspException {
+        return list;
+    }
+}
---
+++ web_module/branches/wiki_2007_08_29/src/main/com/topcoder/web/privatelabel/controller/request/SimpleReg.java
@@ -0,0 +1,24 @@
+package com.topcoder.web.privatelabel.controller.request;
+
+import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.privatelabel.Constants;
+
+
+/**
+ *
+ * @author gpaul 06.26.2003
+ */
+public class SimpleReg extends SimpleRegBase {
+    protected void registrationProcessing() throws TCWebException {
+        getRequest().setAttribute("countryList", getCountryList());
+        getRequest().setAttribute("fullCountryList", getFullCountryList());
+        getRequest().setAttribute("stateList", getStateList());
+        setNextPage();
+    }
+
+    protected void setNextPage() {
+        setNextPage(Constants.SIMPLE_REG_PAGE);
+        setIsNextPageInContext(true);
+    }
+
+}
