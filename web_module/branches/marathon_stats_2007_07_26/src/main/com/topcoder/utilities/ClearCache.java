d13e9931bdfdc51cc181d64d9fd29d5d7a3604b5
cucu <cucu@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-08-30 13:27:59 +0000

    *** empty log message ***

--- web_module/branches/marathon_stats_2007_07_26/src/main/com/topcoder/utilities/ClearCache.java
+++ web_module/branches/marathon_stats_2007_07_26/src/main/com/topcoder/utilities/ClearCache.java
@@ -1,5 +1,13 @@
 package com.topcoder.utilities;
 
+import java.lang.reflect.InvocationTargetException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.topcoder.shared.util.TCContext;
+import com.topcoder.shared.util.TCResourceBundle;
+import com.topcoder.web.common.cache.CacheClient;
 import com.topcoder.web.common.cache.JbossCacheClient;
 import com.topcoder.web.common.cache.TCCacheException;
 
@@ -9,6 +17,23 @@ public class ClearCache {
      * @param args
      */
     public static void main(String[] args) {
+        InitialContext ctx = null;        
+        TCResourceBundle b = new TCResourceBundle("cache");
+        try {
+            ctx = TCContext.getInitial(b.getProperty("host_url"));
+            //using reflection so that we don't a lot of nasty dependencies when using the class.
+            CacheClient cache = (CacheClient) ctx.lookup(b.getProperty("jndi_name"));
+
+            cache.clearCache();
+            
+            System.out.println("1. cleared");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            TCContext.close(ctx);
+        }
+
         try {
             JbossCacheClient cache = new JbossCacheClient();
             cache.set("test", "hi");
