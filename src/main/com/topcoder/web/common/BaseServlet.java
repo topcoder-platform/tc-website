35acc6eb63c4a27f5e3013cb649b022a09aac249
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-09-05 13:33:12 +0000

    flush cookies

--- src/main/com/topcoder/web/common/BaseServlet.java
+++ src/main/com/topcoder/web/common/BaseServlet.java
@@ -224,7 +224,7 @@ public abstract class BaseServlet extends HttpServlet {
                 //if there is an exception in post processing, and we've already started writting the response
                 //we're not going to be able to forward to the error page.
                 rp.postProcessing();
-
+                authentication.flushCookies();
             } catch (Throwable e) {
                 handleException(request, response, e);
             }
