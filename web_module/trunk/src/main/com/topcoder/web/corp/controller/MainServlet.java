a2370a3738cd3020963104b8e7005c6db9c45a7e
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-09-05 20:15:13 +0000

    use tc cookie

--- web_module/trunk/src/main/com/topcoder/web/corp/controller/MainServlet.java
+++ web_module/trunk/src/main/com/topcoder/web/corp/controller/MainServlet.java
@@ -2,11 +2,6 @@ package com.topcoder.web.corp.controller;
 
 import com.topcoder.shared.util.logging.Logger;
 import com.topcoder.web.common.BaseServlet;
-import com.topcoder.web.common.TCRequest;
-import com.topcoder.web.common.TCResponse;
-import com.topcoder.web.common.security.BasicAuthentication;
-import com.topcoder.web.common.security.SessionPersistor;
-import com.topcoder.web.common.security.WebAuthentication;
 import com.topcoder.web.corp.Constants;
 
 import javax.servlet.ServletConfig;
@@ -26,11 +21,6 @@ public class MainServlet extends BaseServlet {
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         Constants.init(config);
-    }
-
-    protected WebAuthentication createAuthentication(TCRequest request,
-                                                     TCResponse response) throws Exception {
-        return new BasicAuthentication(new SessionPersistor(request.getSession()), request, response, BasicAuthentication.CORP_SITE);
     }
 
 }
