fd5dfccd7eee0f8eb3b3301d0b655c226dd17140
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-09-12 18:33:17 +0000

    add session info

--- src/main/com/topcoder/web/distui/controller/request/Top.java
+++ src/main/com/topcoder/web/distui/controller/request/Top.java
@@ -1,9 +1,14 @@
 package com.topcoder.web.distui.controller.request;
 
+import com.topcoder.shared.security.SimpleUser;
+import com.topcoder.shared.security.User;
 import com.topcoder.web.common.BaseServlet;
 import com.topcoder.web.common.SessionInfo;
-import com.topcoder.web.common.security.BasicAuthentication;
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.common.WebConstants;
+import com.topcoder.web.common.security.LightAuthentication;
 import com.topcoder.web.common.security.SessionPersistor;
+import com.topcoder.web.common.security.WebAuthentication;
 
 import java.util.Collections;
 
@@ -15,9 +20,19 @@ import java.util.Collections;
 public class Top extends Base {
     public void process() throws Exception {
 
-        SessionInfo info = new SessionInfo(getRequest(),
-                new BasicAuthentication(new SessionPersistor(getRequest().getSession()),
-                        getRequest(), getResponse(), BasicAuthentication.MAIN_SITE), Collections.EMPTY_SET);
+        String userId = StringUtils.checkNull(getRequest().getParameter(WebConstants.USER_ID));
+        WebAuthentication auth = new LightAuthentication(new SessionPersistor(getRequest().getSession(true)),
+            getRequest(), getResponse(), LightAuthentication.MAIN_SITE);
+        if (!"".equals(userId.trim())) {
+            try {
+                User u = new SimpleUser(new Long(userId),"", "");
+                auth.login(u,false);
+            } catch (NumberFormatException e) {
+                //ignore
+            }
+        }
+
+        SessionInfo info = new SessionInfo(getRequest(), auth, Collections.EMPTY_SET);
 
         getRequest().setAttribute(BaseServlet.SESSION_INFO_KEY, info);
 
