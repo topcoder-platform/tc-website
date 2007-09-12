773145d5b6320b548b0ac39a075215b292ebaa63
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-09-12 20:37:07 +0000

    do stuff

--- src/main/com/topcoder/web/wiki/themes/tc/ConfigureThemeAction.java
+++ src/main/com/topcoder/web/wiki/themes/tc/ConfigureThemeAction.java
@@ -4,9 +4,11 @@ import com.atlassian.bandana.BandanaManager;
 import com.atlassian.confluence.spaces.Space;
 import com.atlassian.confluence.spaces.actions.AbstractSpaceAction;
 import com.opensymphony.xwork.ActionContext;
+import com.topcoder.shared.util.logging.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author dok
@@ -14,6 +16,7 @@ import java.util.List;
  *          Create Date: Aug 14, 2007
  */
 public class ConfigureThemeAction extends AbstractSpaceAction {
+    private static final Logger log = Logger.getLogger(ConfigureThemeAction.class);
     private BandanaManager bandanaManager;
     private List navKeys;
 
@@ -24,6 +27,7 @@ public class ConfigureThemeAction extends AbstractSpaceAction {
      * @throws Exception
      */
     public String doDefault() throws Exception {
+        log.debug("doDefault called");
         ClassLoader callersLoader = Thread.currentThread().getContextClassLoader();
         try {
             // TEMPORARY HACK: until we work out a way to make the plugin class loaders available to the system correctly.
@@ -37,6 +41,9 @@ public class ConfigureThemeAction extends AbstractSpaceAction {
             navKeys=new ArrayList(spaces.size());
             for (Object o : spaces) {
                 s = (Space)o;
+                if (log.isDebugEnabled()) {
+                    log.debug("space " + s.getKey());
+                }
                 LeftNavSettings setting = new LeftNavSettings();
                 setting.setSpace(s.getKey());
                 if (settingsManager.getSpaceThemeSettings(s.getKey())!=null) {
@@ -59,17 +66,23 @@ public class ConfigureThemeAction extends AbstractSpaceAction {
      * @throws Exception
      */
     public String execute() throws Exception {
+        log.debug("execute called");
         // after submission of the form we will have to create a new settings manager
         SettingsManager settingsManager = new SettingsManager(bandanaManager);
         //preset the object we want to serialize with the values of the form fields.
+
+        Map params = ActionContext.getContext().getParameters();
 
         Space s;
         String navKey;
         for (Object o : getSpaces()) {
             s = (Space)o;
+            if (log.isDebugEnabled()) {
+                log.debug("space " + s.getKey());
+            }
             //accessing the parameters directly because i can't figure out how to do it
             //correctly within the framework with the autowiring
-            navKey = (String)ActionContext.getContext().getParameters().get(s.getKey());
+            navKey = (String)params.get(s.getKey());
             if (navKey!=null) {
                 LeftNavSettings settings = new LeftNavSettings();
                 settings.setSpace(s.getKey());
