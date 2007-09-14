6be6bb91deae357a8b13f7e6d84b9ad251736534
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-09-14 18:49:17 +0000

    Educational Platform - annotations test

--- web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/web/common/model/educ/Professor.java
+++ web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/web/common/model/educ/Professor.java
@@ -11,6 +11,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -18,7 +19,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 
-import org.hibernate.annotations.Entity;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.Table;
