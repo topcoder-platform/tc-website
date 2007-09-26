cdea667693b0e97ddd3c8de5fd06ad90827f4665
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-09-26 21:33:52 +0000

    add

---
+++ web_module/trunk/src/main/com/topcoder/web/common/Pair.java
@@ -0,0 +1,28 @@
+package com.topcoder.web.common;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Sep 26, 2007
+ */
+public class Pair<T,S> {
+    private T a;
+    private S b;
+
+
+    public T getA() {
+        return a;
+    }
+
+    public void setA(T a) {
+        this.a = a;
+    }
+
+    public S getB() {
+        return b;
+    }
+
+    public void setB(S b) {
+        this.b = b;
+    }
+}
