daac200ee25e6d3550347f3b58a968e72d4ab197
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-10-13 20:50:04 +0000

    EducationalPlatform

--- web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/web/ep/controller/request/professor/reports/Home.java
+++ web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/web/ep/controller/request/professor/reports/Home.java
@@ -5,7 +5,6 @@
 */
 package com.topcoder.web.ep.controller.request.professor.reports;
 
-import com.topcoder.shared.util.logging.Logger;
 import com.topcoder.web.common.ShortHibernateProcessor;
 import com.topcoder.web.common.dao.DAOUtil;
 
@@ -14,12 +13,6 @@ import com.topcoder.web.common.dao.DAOUtil;
  * @version $Id$
  */
 public class Home extends ShortHibernateProcessor {
-
-    private static Logger log = Logger.getLogger(Home.class);
-
-    /* (non-Javadoc)
-     * @see com.topcoder.web.common.LongHibernateProcessor#dbProcessing()
-     */
     @Override
     protected void dbProcessing() throws Exception {
         getRequest().setAttribute("classrooms", DAOUtil.getFactory().getClassroomDAO().getClassroomsUsingProfessorId(new Long(getUser().getId())));
