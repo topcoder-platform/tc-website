ed3ee154ca610f4dee4122014c3681952276bdc4
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-10-18 15:28:33 +0000

    EducationalPlatform

--- web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/web/ep/controller/request/reports/ProblemAssignmentReport.java
+++ web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/web/ep/controller/request/reports/ProblemAssignmentReport.java
@@ -8,98 +8,169 @@ package com.topcoder.web.ep.controller.request.reports;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.topcoder.shared.dataAccess.DataAccessConstants;
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer.ResultSetRow;
 import com.topcoder.web.common.NavigationException;
-import com.topcoder.web.common.ShortHibernateProcessor;
 import com.topcoder.web.common.StringUtils;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.dao.DAOUtil;
+import com.topcoder.web.common.model.Coder;
 import com.topcoder.web.common.model.SortInfo;
 import com.topcoder.web.common.model.algo.Component;
 import com.topcoder.web.common.model.algo.Round;
 import com.topcoder.web.common.model.algo.RoundProperty;
+import com.topcoder.web.common.model.educ.AssignmentScoreType;
 import com.topcoder.web.common.model.educ.Classroom;
 import com.topcoder.web.ep.Constants;
+import com.topcoder.web.ep.controller.request.SharedBaseProcessor;
 
 /**
  * @author Pablo Wolfus (pulky)
  * @version $Id$
  */
-public class ProblemAssignmentReport extends ShortHibernateProcessor {
+public class ProblemAssignmentReport extends SharedBaseProcessor {
     public static final Integer STUDENT_COL = 1;
     public static final Integer SCORE_COL = 2;
     public static final Integer NUM_TESTS_COL = 3;
     public static final Integer PERCENT_TESTS_COL = 4;
     
     @Override
-    protected void dbProcessing() throws Exception {
-        // get classroom parameter
-        Long assignmentId = getAssignmentParam();
+    protected void professorProcessing() throws Exception {
+        Round a = validateAssignment();
 
-        // get component parameter
-        Long componentId = getComponentParam();
-
-        // check if it's a valid assignment
-        Round a = DAOUtil.getFactory().getRoundDAO().find(assignmentId);
-        if (a == null) {
-            throw new TCWebException("Couldn't find assignment");
-        }
-        
-        // check if it's a valid component
-        Component cmp = DAOUtil.getFactory().getComponentDAO().find(componentId);
-        if (cmp == null) {
-            throw new TCWebException("Couldn't find component");
-        }
+        Component cmp = validateComponent();
         
         // check if this assignment belongs to the logged professor
         Classroom c = DAOUtil.getFactory().getClassroomDAO().find((Long) a.getProperty(RoundProperty.CLASSROOM_ID_PROPERTY_ID));
         if (!c.getProfessor().getId().equals(getUser().getId())) {
             throw new NavigationException("You don't have permission to see this page.");
         }
-
-        // get and prepare system test results so they are available to construct the rows
-        List<Object> systemTestResults = DAOUtil.getFactory().getSystemTestResultDAO().getSystemTestResultsByComponent(a.getId(), cmp.getId());
-        Map<Long, Integer> total = new HashMap<Long, Integer>();
-        Map<Long, Integer> succeeded = new HashMap<Long, Integer>();
         
-        for (Object o : systemTestResults ) {
-            Object[] lo = (Object[]) o; 
-            total.put((Long)lo[2], (Integer)lo[0]);
-            succeeded.put((Long)lo[2], (Integer)lo[1]);
+        List<AssignmentReportRow> larr = processReport(a, c, cmp, null);
+        commonPostProcessing(larr, a, c);
+    }
+
+    @Override
+    protected void studentProcessing() throws Exception {
+        Round a = validateAssignment();
+        
+        Component cmp = validateComponent();
+
+        // check if this student belongs to the class
+        Classroom c = DAOUtil.getFactory().getClassroomDAO().find((Long) a.getProperty(RoundProperty.CLASSROOM_ID_PROPERTY_ID));
+        Coder s = c.getActiveStudent(getUser().getId());
+        if (s == null) {
+            throw new NavigationException("You don't have permission to see this page.");
         }
 
-        // we need assignment and component results (problem score, #tests passed, %tests passed)
-        // get the room
-        // Iterate results and generate report
-        List<Object> l = DAOUtil.getFactory().getComponentStateDAO().getResultsSummary(a.getId(), cmp.getId());
+        Long showAllCoders = (Long) a.getProperty(RoundProperty.SHOW_ALL_SCORES_PROPERTY_ID);
+
+        Long studentId = null;
+        if (!showAllCoders.equals(1l)) {
+            // If show all is off, he can only see his results
+            studentId = s.getId();
+        }
+        List<AssignmentReportRow> larr = processReport(a, c, cmp, studentId);
+
+        Long scoreType = (Long) a.getProperty(RoundProperty.SCORE_TYPE_PROPERTY_ID);
+        applyStudentRestrictions(larr, scoreType);
+
+        getRequest().setAttribute("score_type", scoreType);
+        
+        commonPostProcessing(larr, a, c);
+    }
+
+    protected List<AssignmentReportRow> processReport(Round a, Classroom c, Component cmp, Long studentId) throws Exception {
+
+        ResultSetContainer rsc = getData(a.getId(), studentId, cmp.getId());
 
         List<AssignmentReportRow> larr = new ArrayList<AssignmentReportRow>();
-        for (Object o : l) {
-            Object[] lo = (Object[]) o; 
-
-            if (total.containsKey((Long)lo[0])) {
-                larr.add(new AssignmentReportRow((Long)lo[0], (String)lo[1] + ", " + (String)lo[2], (Double)lo[3], 
-                        succeeded.get((Long)lo[0]), (succeeded.get((Long)lo[0]) * 100d) / total.get((Long)lo[0])));
+        
+        for (ResultSetRow rsr : rsc) {
+            if (rsr.getItem("succeeded").getResultData() != null) {  
+                larr.add(new AssignmentReportRow(
+                    rsr.getLongItem("coder_id"), 
+                    rsr.getStringItem("last_name") + ", " + rsr.getStringItem("first_name"),
+                    rsr.getDoubleItem("points"),
+                    rsr.getIntItem("succeeded"),
+                    rsr.getLongItem("succeeded") * 100d / rsr.getLongItem("total")));
             } else {
-                larr.add(new AssignmentReportRow((Long)lo[0], (String)lo[1] + ", " + (String)lo[2], (Double)lo[3], 
-                        -1, 0d));
+                larr.add(new AssignmentReportRow(
+                    rsr.getLongItem("coder_id"), 
+                    rsr.getStringItem("last_name") + ", " + rsr.getStringItem("first_name"),
+                    rsr.getDoubleItem("points"),
+                    -1,
+                    0d));
             }   
         }
-        
+
+        return larr;
+    }
+
+    private ResultSetContainer getData(Long roundId, Long coderId, Long componentId) throws TCWebException {
+        try {
+            Request r = new Request();
+            String queryName;
+            if (coderId == null) {
+                queryName = "assignment_problem_results_all";
+            } else {
+                queryName = "assignment_problem_results_student";
+                r.setProperty("cr", coderId.toString());
+            }
+            r.setContentHandle(queryName);
+            r.setProperty("rd", roundId.toString());
+            r.setProperty("compid", componentId.toString());
+
+            DataAccessInt dai = getDataAccess();
+            Map result = dai.getData(r);
+            ResultSetContainer rsc = (ResultSetContainer) result.get(queryName);
+
+            return rsc;
+        } catch (Exception e) {
+            throw new TCWebException("Could not get data from DB", e);
+        }
+    }
+
+    private void commonPostProcessing(List<AssignmentReportRow> larr, Round a, Classroom c) {
         // sort results
         sortResult(larr);
         
         getRequest().setAttribute("results", larr);
         getRequest().setAttribute("assignment", a);
         getRequest().setAttribute("classroom", c);
-        getRequest().setAttribute("component", cmp);
         
-        setNextPage("/reports/probAssignment.jsp");
+        setNextPage("/reports/assignment.jsp");
         setIsNextPageInContext(true);
+    }
+    
+    private void applyStudentRestrictions(List<AssignmentReportRow> larr, Long scoreType) {
+        for (AssignmentReportRow srr : larr) {
+           if (scoreType.equals(AssignmentScoreType.TC_SCORE_TYPE)) {
+               srr.setNumTestsPassed(-999);
+               srr.setPercentTestsPassed(-999d);
+           } else if (scoreType.equals(AssignmentScoreType.PASSED_SCORE_TYPE)) {
+               srr.setScore(-999d);
+               srr.setPercentTestsPassed(-999d);
+           } else if (scoreType.equals(AssignmentScoreType.SUCCESS_FAIL_SCORE_TYPE)) {
+               srr.setScore(-999d);
+               srr.setNumTestsPassed(-999);
+           }  
+        }
+    }
+
+    private Round validateAssignment() throws TCWebException {
+        // check if it's a valid assignment
+        Round a = DAOUtil.getFactory().getRoundDAO().find(getAssignmentParam());
+        if (a == null) {
+            throw new TCWebException("Couldn't find assignment");
+        }
+        return a;
     }
     
     private Long getAssignmentParam() throws TCWebException {
@@ -119,6 +190,16 @@ public class ProblemAssignmentReport extends ShortHibernateProcessor {
         return id;
     }
     
+
+    private Component validateComponent() throws TCWebException {
+        // check if it's a valid component
+        Component cmp = DAOUtil.getFactory().getComponentDAO().find(getComponentParam());
+        if (cmp == null) {
+            throw new TCWebException("Couldn't find component");
+        }
+        return cmp;
+    }
+
     private Long getComponentParam() throws TCWebException {
         String componentId = StringUtils.checkNull(getRequest().getParameter(Constants.COMPONENT_ID));
 
@@ -135,7 +216,7 @@ public class ProblemAssignmentReport extends ShortHibernateProcessor {
 
         return id;
     }
-    
+
     private void sortResult(List<AssignmentReportRow> larr) {
         String sortDir = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.SORT_DIRECTION));
         String sortCol = StringUtils.checkNull(getRequest().getParameter(DataAccessConstants.SORT_COLUMN));
