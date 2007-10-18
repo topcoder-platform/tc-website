e3a83fb9aac66db4a281239627cbb68eaaed6a3c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-10-18 20:06:55 +0000

    fix segment id

--- web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/web/common/dao/hibernate/RoundDAOHibernate.java
+++ web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/web/common/dao/hibernate/RoundDAOHibernate.java
@@ -1,45 +1,43 @@
 package com.topcoder.web.common.dao.hibernate;
 
-import java.util.Date;
-import java.util.List;
-
+import com.topcoder.web.common.dao.RoundDAO;
+import com.topcoder.web.common.model.algo.Round;
+import com.topcoder.web.common.model.algo.RoundProperty;
 import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.criterion.Restrictions;
 
-import com.topcoder.web.common.dao.RoundDAO;
-import com.topcoder.web.common.model.algo.Round;
-import com.topcoder.web.common.model.algo.RoundProperty;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author cucu
  */
 public class RoundDAOHibernate extends Base implements RoundDAO {
-    
 
     @SuppressWarnings("unchecked")
     public List<Round> getRoundsAfter(Date date) {
         Query q = session.createQuery(
                 " select r from Round r, RoundSegment rs " +
-                " where rs.id.round.id = r.id " +
-                " and rs.id.segment.id = 1 " +
-                " and rs.startTime > :start");
+                        " where rs.id.roundId = r.id " +
+                        " and rs.id.segmentId = 1 " +
+                        " and rs.startTime > :start");
 
         q.setDate("start", date);
         return q.list();
     }
-        
+
     public List<Round> findDuplicateName(Long classroomId, String assignmentName, Long roundId) {
         Criteria c = session.createCriteria(Round.class)
-        .add(Restrictions.eq("name", assignmentName));
+                .add(Restrictions.eq("name", assignmentName));
 
         if (roundId != null) {
             c.add(Restrictions.ne("id", roundId));
         }
 
         c.createCriteria("roundProperties")
-        .add(Restrictions.eq("id.typeId", RoundProperty.CLASSROOM_ID_PROPERTY_ID))
-        .add(Restrictions.eq("intValue", classroomId));
+                .add(Restrictions.eq("id.typeId", RoundProperty.CLASSROOM_ID_PROPERTY_ID))
+                .add(Restrictions.eq("intValue", classroomId));
 
         return c.list();
     }
