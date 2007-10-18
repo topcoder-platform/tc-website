ab76f375d1820d67a684371558781d58ee158a35
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-10-18 21:19:50 +0000

    merge out from head

--- web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/security/login/LoginRemoteHome.java
+++ web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/security/login/LoginRemoteHome.java
@@ -6,7 +6,7 @@ import java.rmi.RemoteException;
 
 public interface LoginRemoteHome extends javax.ejb.EJBHome {
 
-    public static final String EJB_REF_NAME = "security/Login";
+    public static final String EJB_REF_NAME = LoginRemoteHome.class.getName();
 
     public LoginRemote create()
             throws RemoteException, CreateException, EJBException;
