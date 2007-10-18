c7f3faa2a335f17b1b44cf57bb7901945f84d95a
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-10-18 18:40:46 +0000

    fix query

--- web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/web/common/dao/hibernate/SecurityGroupDAOHibernate.java
+++ web_module/branches/educational_platform_2007_09_04/src/main/com/topcoder/web/common/dao/hibernate/SecurityGroupDAOHibernate.java
@@ -6,7 +6,6 @@ import com.topcoder.web.common.model.SecurityGroup;
 import com.topcoder.web.common.model.User;
 import org.hibernate.Query;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -19,8 +18,8 @@ public class SecurityGroupDAOHibernate extends Base implements SecurityGroupDAO 
     public List getSecurityGroups(Set registrationTypes) {
         StringBuffer query = new StringBuffer(100);
         query.append("SELECT rt.securityGroup FROM RegistrationType rt WHERE rt.id in (");
-        for (Iterator it = registrationTypes.iterator(); it.hasNext();) {
-            query.append(((RegistrationType) it.next()).getId()).append(",");
+        for (Object registrationType : registrationTypes) {
+            query.append(((RegistrationType) registrationType).getId()).append(",");
         }
         query.delete(query.length() - 1, query.length());
         query.append(")");
@@ -32,12 +31,12 @@ public class SecurityGroupDAOHibernate extends Base implements SecurityGroupDAO 
 
     public boolean hasInactiveHSGroup(User u) {
         StringBuffer query = new StringBuffer(100);
-        query.append("FROM UserGroup ug ");
-        query.append(" WHERE ug.securityGroup.registrationTypes.id = ? AND ug.securityStatusId = ? AND ug.user.id = ?");
+        query.append("FROM UserGroup ug join ug.securityGroup.registrationTypes r ");
+        query.append(" WHERE r.id = ? AND ug.securityStatusId = ? AND ug.user.id = ?");
         Query q = session.createQuery(query.toString());
-        q.setInteger(0, RegistrationType.HIGH_SCHOOL_ID.intValue());
-        q.setInteger(1, SecurityGroup.INACTIVE.intValue());
-        q.setLong(2, u.getId().longValue());
+        q.setInteger(0, RegistrationType.HIGH_SCHOOL_ID);
+        q.setInteger(1, SecurityGroup.INACTIVE);
+        q.setLong(2, u.getId());
         return !q.list().isEmpty();
     }
 }
