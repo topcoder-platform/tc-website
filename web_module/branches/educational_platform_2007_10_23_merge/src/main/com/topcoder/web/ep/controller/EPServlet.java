100d4d96db79e44f6397e93cef7000abb7288a16
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-10-24 18:24:19 +0000

    EducationalPlatform

--- web_module/branches/educational_platform_2007_10_23_merge/src/main/com/topcoder/web/ep/controller/EPServlet.java
+++ web_module/branches/educational_platform_2007_10_23_merge/src/main/com/topcoder/web/ep/controller/EPServlet.java
@@ -19,13 +19,6 @@ import com.topcoder.web.ep.controller.request.RoundEventInitializer;
 public class EPServlet extends BaseServlet {
     private final static Logger log = Logger.getLogger(EPServlet.class);
 
-//    @Override
-//    protected WebAuthentication createAuthentication(TCRequest request,
-//                                                     TCResponse response) throws Exception {
-//        return new BasicAuthentication(new SessionPersistor(request.getSession()), request, response,
-//                BasicAuthentication.EP_SITE);
-//    }
-
     @Override
     protected String getProcessor(String key) {
         String ret = super.getProcessor(key);
@@ -41,25 +34,6 @@ public class EPServlet extends BaseServlet {
         return ret;
     }
 
-//    @Override
-//    protected void handleException(HttpServletRequest request, HttpServletResponse response, Throwable e) throws Exception {
-//        log.error("caught exception, forwarding to error page", e);
-//        if (e instanceof PermissionException) {
-//            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-//            request.setAttribute(MESSAGE_KEY, "Sorry, you do not have permission to access the specified resource.  Please confirm that you are a member of Educational Platform.");
-//        } else if (e instanceof NavigationException) {
-//            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-//            request.setAttribute(MESSAGE_KEY, e.getMessage());
-//            if (((NavigationException) e).hasUrl())
-//                request.setAttribute(URL_KEY, ((NavigationException) e).getUrl());
-//        } else {
-//            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-//            request.setAttribute(MESSAGE_KEY, "An error has occurred when attempting to process your request.");
-//        }
-//        request.setAttribute("exception", e);
-//        fetchRegularPage(request, response, ERROR_PAGE, true);
-//    }
-//    
     
     @Override
     public void destroy() {
