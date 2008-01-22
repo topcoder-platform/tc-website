fe1a6d894780509bb507270fc864c4bb6547ecd8
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-01-22 17:20:57 +0000

    Added support for when the user only agrees to terms without selecting any particular component

--- web_module/trunk/src/main/com/topcoder/dde/request/ViewComponentTerms.java
+++ web_module/trunk/src/main/com/topcoder/dde/request/ViewComponentTerms.java
@@ -13,6 +13,7 @@ import com.topcoder.security.TCSubject;
 import com.topcoder.shared.security.ClassResource;
 import com.topcoder.web.common.BaseProcessor;
 import com.topcoder.web.common.PermissionException;
+import com.topcoder.web.common.StringUtils;
 
 /**
  * @author  dok
@@ -39,28 +40,34 @@ public class ViewComponentTerms extends BaseProcessor {
                     PortableRemoteObject.narrow(objUserManager, UserManagerRemoteHome.class);
             UserManagerRemote userManager = userManagerHome.create();
 
-            // check if the user meets the requirements for the download before he gets to the terms page
-            ComponentManagerHome componentManagerHome = (ComponentManagerHome) PortableRemoteObject.narrow(
-                    getInitialContext().lookup(ComponentManagerHome.EJB_REF_NAME), ComponentManagerHome.class);
-
-            TCSubject tcSubject = (TCSubject) getRequest().getSession().getAttribute("TCSUBJECT");
-            long compId = Long.parseLong((String) getRequest().getParameter("comp"));
-            ComponentManager compMgr = componentManagerHome.create(compId);
-
-            try {
-                // we don't care here if the user has permission
-                // he must get to the terms page in order to get those permissions
-                compMgr.canDownload(tcSubject);
+            // the user is only agreeing to terms, no component selected
+            if (StringUtils.checkNull(getRequest().getParameter("comp")).equals("")) {
                 getRequest().setAttribute(Constants.TERMS, userManager.getComponentTerms());
                 setNextPage("/terms/componentTerms.jsp");
-            } catch (ReachedQuotaException e) {
-                getRequest().setAttribute("failure_reason", QUOTA_REACHED_REASON);
-                getRequest().setAttribute("max_downloads", compMgr.getMaxPublicDownloads());
-                setNextPage("/catalog/sorry.jsp");
-            } catch (NonPublicComponentException e) {
-                getRequest().setAttribute("failure_reason", NON_PUBLIC_REASON);                
-                getRequest().setAttribute("max_downloads", compMgr.getMaxPublicDownloads());
-                setNextPage("/catalog/sorry.jsp");
+            } else {
+                // check if the user meets the requirements for the download before he gets to the terms page
+                ComponentManagerHome componentManagerHome = (ComponentManagerHome) PortableRemoteObject.narrow(
+                        getInitialContext().lookup(ComponentManagerHome.EJB_REF_NAME), ComponentManagerHome.class);
+    
+                TCSubject tcSubject = (TCSubject) getRequest().getSession().getAttribute("TCSUBJECT");
+                long compId = Long.parseLong((String) getRequest().getParameter("comp"));
+                ComponentManager compMgr = componentManagerHome.create(compId);
+    
+                try {
+                    // we don't care here if the user has permission
+                    // he must get to the terms page in order to get those permissions
+                    compMgr.canDownload(tcSubject);
+                    getRequest().setAttribute(Constants.TERMS, userManager.getComponentTerms());
+                    setNextPage("/terms/componentTerms.jsp");
+                } catch (ReachedQuotaException e) {
+                    getRequest().setAttribute("failure_reason", QUOTA_REACHED_REASON);
+                    getRequest().setAttribute("max_downloads", compMgr.getMaxPublicDownloads());
+                    setNextPage("/catalog/sorry.jsp");
+                } catch (NonPublicComponentException e) {
+                    getRequest().setAttribute("failure_reason", NON_PUBLIC_REASON);                
+                    getRequest().setAttribute("max_downloads", compMgr.getMaxPublicDownloads());
+                    setNextPage("/catalog/sorry.jsp");
+                }
             }
             setIsNextPageInContext(true);
         }
