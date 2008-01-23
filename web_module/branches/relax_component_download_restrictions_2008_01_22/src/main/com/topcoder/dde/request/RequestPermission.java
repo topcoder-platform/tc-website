d3c16e3a81df431ec5140eeb041d30d0985657e3
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-01-23 14:59:09 +0000

    relax component download restrictions project

---
+++ web_module/branches/relax_component_download_restrictions_2008_01_22/src/main/com/topcoder/dde/request/RequestPermission.java
@@ -0,0 +1,75 @@
+package com.topcoder.dde.request;
+
+import java.util.Map;
+
+import com.topcoder.security.TCSubject;
+import com.topcoder.security.admin.PrincipalMgrRemote;
+import com.topcoder.security.admin.PrincipalMgrRemoteHome;
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer.ResultSetRow;
+import com.topcoder.shared.security.ClassResource;
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.web.common.BaseProcessor;
+import com.topcoder.web.common.CachedDataAccess;
+import com.topcoder.web.common.PermissionException;
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.common.TCWebException;
+
+/**
+ * @author  pulky
+ * @version  $Revision$ $Date$
+ */
+public class RequestPermission extends BaseProcessor {
+
+    protected void businessProcessing() throws Exception {
+
+        if (getUser().isAnonymous()) {
+            throw new PermissionException(getUser(), new ClassResource(this.getClass()));
+        } else {
+
+            String code = StringUtils.checkNull(getRequest().getParameter("code"));
+            
+            if (! "".equals(code)) {    
+                ResultSetContainer rsc = getData(code);            
+
+                if (rsc.size() > 0) {
+                    PrincipalMgrRemoteHome principalMgrHome = (PrincipalMgrRemoteHome)
+                    getInitialContext().lookup(PrincipalMgrRemoteHome.EJB_REF_NAME);
+                    PrincipalMgrRemote principalMgr = principalMgrHome.create();
+            
+                    for (ResultSetRow rsr : rsc) {
+                        principalMgr.addUserToGroup(principalMgr.getGroup(rsr.getLongItem("group_id")), principalMgr.getUser(getUser().getId()), new TCSubject(0));
+                    }
+                } else {
+                    // if the code is not found, throw permission exception
+                    throw new PermissionException(getUser(), new ClassResource(this.getClass()));
+                }
+                
+                setNextPage("/catalog/index.jsp");
+            } else {
+                setNextPage("/catalog/requestPermission.jsp");
+            }
+            setIsNextPageInContext(false);
+        }
+    }
+
+    private ResultSetContainer getData(String code) throws TCWebException {
+        try {
+            Request r = new Request();
+            String queryName = "code_groups";
+
+            r.setContentHandle(queryName);
+            r.setProperty("code", code);
+
+            DataAccessInt dai = new CachedDataAccess(DBMS.TCS_OLTP_DATASOURCE_NAME);
+            Map result = dai.getData(r);
+            ResultSetContainer rsc = (ResultSetContainer) result.get(queryName);
+
+            return rsc;
+        } catch (Exception e) {
+            throw new TCWebException("Could not get data from DB", e);
+        }
+    }
+}
---
+++ web_module/branches/relax_component_download_restrictions_2008_01_22/web/dde/catalog/requestPermission.jsp
@@ -0,0 +1,86 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>Request permission</title>
+    <link rel="stylesheet" type="text/css" href="/includes/tcs_style.css" />
+    <script language="JavaScript" type="text/javascript" src="/scripts/javascript.js"></script>
+</head>
+
+<body class="body">
+
+<!-- Header begins -->
+<jsp:include page="/includes/top.jsp"/>
+<jsp:include page="/includes/menu.jsp" >
+    <jsp:param name="isSoftwarePage" value="true"/>
+</jsp:include>
+<!-- Header ends -->
+
+<table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" class="middle">
+    <tr valign="top">
+
+<!-- Left Column begins -->
+        <td width="180" class="leftColumn">
+            <jsp:include page="/includes/left.jsp" >
+            <jsp:param name="level1" value="index"/>
+            </jsp:include>
+        </td>
+<!-- Left Column ends -->
+
+<!-- Gutter begins -->
+        <td width="15"><img src="/images/clear.gif" alt="" width="15" height="10" border="0" /></td>
+<!-- Gutter ends -->
+
+<!-- Middle Column begins -->
+        <td width="100%">
+            <form name="f" action="${sessionInfo.servletPath}" method="post">
+                <input type="hidden" name="module" value="RequestPermission">
+                <div align="center">
+                    <table cellpadding="4" cellspacing="0">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <strong>Enter Code:</strong><br/>
+                                <input type="text" name="code" value ="" size="32" maxlength="32">
+                            </td>
+                        </tr>
+                    </tbody>
+                    </table>
+                </div>
+            </form>            
+        </td>
+<!-- Middle Column ends -->
+
+<!-- Gutter 2 begins -->
+        <td width="15"><img src="/images/clear.gif" alt="" width="15" height="10" border="0" /></td>
+<!-- Gutter 2 ends -->
+
+<!-- Right Column begins -->
+        <td width="170">
+            &nbsp;
+        </td>
+<!--Right Column ends -->
+
+<!-- Gutter 3 begins -->
+        <td width="15"><img src="/images/clear.gif" alt="" width="10" height="10" border="0" /></td>
+<!-- Gutter 3 ends -->
+
+    </tr>
+</table>
+
+<!-- Footer begins -->
+<jsp:include page="/includes/foot.jsp" flush="true" />
+<!-- Footer ends -->
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-3257193-2");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+
+</body>
+</html>
