4b37d1124ec49808532255e9e6d29f3fcb7d7c02
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-02-12 22:12:26 +0000

    no message

--- src/testcases/com/topcoder/web/studio/dao/SubmissionDAOTestCase.java
+++ src/testcases/com/topcoder/web/studio/dao/SubmissionDAOTestCase.java
@@ -4,7 +4,10 @@ import com.topcoder.web.common.dao.DAOUtil;
 import com.topcoder.web.common.model.User;
 import com.topcoder.web.studio.TCHibernateTestCase;
 import com.topcoder.web.studio.model.Contest;
+import com.topcoder.web.studio.model.ContestResult;
 import com.topcoder.web.studio.model.FilePath;
+import com.topcoder.web.studio.model.Prize;
+import com.topcoder.web.studio.model.PrizeType;
 import com.topcoder.web.studio.model.Submission;
 import com.topcoder.web.studio.model.SubmissionStatus;
 import com.topcoder.web.studio.model.SubmissionType;
@@ -15,38 +18,38 @@ import com.topcoder.web.studio.model.SubmissionType;
  *          Create Date: Jul 20, 2006
  */
 public class SubmissionDAOTestCase extends TCHibernateTestCase {
-/*
+
     public void testSaveOrUpdate() {
         Submission s = new Submission();
 
-        User dok = DAOUtil.getFactory().getUserDAO().find(new Long(132456));
+        User dok = DAOUtil.getFactory().getUserDAO().find((long) 132456);
         Contest c = (Contest) StudioDAOUtil.getFactory().getContestDAO().getContests().get(0);
         s.setContest(c);
         s.setSubmitter(dok);
-        s.setMimeType(StudioDAOUtil.getFactory().getMimeTypeDAO().find(new Integer(1)));
+        s.setMimeType(StudioDAOUtil.getFactory().getMimeTypeDAO().find(1));
         s.setOriginalFileName("kickin");
         s.setSystemFileName("kicking it");
         FilePath p = new FilePath();
         p.setPath("stuff");
         s.setPath(p);
         s.setType(StudioDAOUtil.getFactory().getSubmissionTypeDAO().find(SubmissionType.INITIAL_CONTEST_SUBMISSION_TYPE));
-        s.setRank(new Integer(1));
+        s.setRank(1);
+        s.setStatus(StudioDAOUtil.getFactory().getSubmissionStatusDAO().find(SubmissionStatus.ACTIVE));
 
         StudioDAOUtil.getFactory().getSubmissionDAO().saveOrUpdate(s);
 
-        ContestResult cr = new ContestResult();
+        ContestResult cr = new ContestResult(s);
         cr.setContest(c);
         cr.setSubmission(s);
         Prize pr = new Prize();
-        pr.setAmount(new Float(100));
-        pr.setPlace(new Integer(1));
+        pr.setAmount((float) 100);
+        pr.setPlace(1);
         pr.setType(StudioDAOUtil.getFactory().getPrizeTypeDAO().find(PrizeType.CONTEST));
         c.addPrize(pr);
         StudioDAOUtil.getFactory().getContestDAO().saveOrUpdate(c);
         tearDown();
         setUp();
-        c = (Contest) StudioDAOUtil.getFactory().getContestDAO().getContests().get(0);
-        cr.setPrize((Prize) c.getPrizes().iterator().next());
+        cr.setPlaced(1);
         s.setResult(cr);
         StudioDAOUtil.getFactory().getSubmissionDAO().saveOrUpdate(s);
 
@@ -59,7 +62,6 @@ public class SubmissionDAOTestCase extends TCHibernateTestCase {
 
 
     }
-*/
 
 
 
@@ -67,17 +69,17 @@ public class SubmissionDAOTestCase extends TCHibernateTestCase {
 
         Submission s = new Submission();
 
-        User dok = DAOUtil.getFactory().getUserDAO().find(new Long(132456));
+        User dok = DAOUtil.getFactory().getUserDAO().find((long) 132456);
         Contest c = (Contest) StudioDAOUtil.getFactory().getContestDAO().getContests().get(0);
         s.setContest(c);
         s.setSubmitter(dok);
-        s.setMimeType(StudioDAOUtil.getFactory().getMimeTypeDAO().find(new Integer(1)));
+        s.setMimeType(StudioDAOUtil.getFactory().getMimeTypeDAO().find(1));
         s.setOriginalFileName("kickin");
         s.setSystemFileName("kicking it");
         FilePath p = new FilePath();
         p.setPath("stuff");
         s.setPath(p);
-        s.setRank(new Integer(1));
+        s.setRank(1);
         s.setType(StudioDAOUtil.getFactory().getSubmissionTypeDAO().find(SubmissionType.INITIAL_CONTEST_SUBMISSION_TYPE));
         s.setStatus(StudioDAOUtil.getFactory().getSubmissionStatusDAO().find(SubmissionStatus.ACTIVE));
 
@@ -91,12 +93,12 @@ public class SubmissionDAOTestCase extends TCHibernateTestCase {
         Submission new1 = dao.find(s.getId());
         log.debug("in between");
 //        dao.changeRank(new Integer(2), new1.getId(), new1.getSubmitter().getId());
-        dao.changeRank(new Integer(2), new1);
+        dao.changeRank(2, new1);
         tearDown();
         setUp();
         dao = StudioDAOUtil.getFactory().getSubmissionDAO();
         Submission new2 = dao.find(s.getId());
-        assertTrue("new rank not set", new2.getRank().intValue() == 2);
+        assertTrue("new rank not set", new2.getRank() == 2);
     }
 
 
