5cad988b7c52542022f672cf808fe3ba77fbdffc
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-02-15 15:39:39 +0000

    add

---
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/tournament/tco08/TravelInfoSubmit.java
@@ -0,0 +1,20 @@
+package com.topcoder.web.tc.controller.request.tournament.tco08;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: root
+ * Date: Feb 15, 2008
+ * Time: 10:35:47 AM
+ * To change this template use File | Settings | File Templates.
+ *
+ *
+ *   $Author$
+ *   $Source$
+ *   $Revision$
+ *   $Date$
+ *   $Id$ 
+ *
+ *
+ */
+public class TravelInfoSubmit {
+}
