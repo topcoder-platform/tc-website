1bfbb8ea0723078c403ebf22094bb4db70635d13
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-02-21 20:10:52 +0000

    comment out the annotations until we have them

--- src/main/com/topcoder/web/ejb/security/AuthenticationRemote.java
+++ src/main/com/topcoder/web/ejb/security/AuthenticationRemote.java
@@ -1,12 +1,10 @@
 package com.topcoder.web.ejb.security;
 
-import javax.ejb.Remote;
-
 /**
  * @author dok
  * @version $Id$
  *          Create Date: Feb 21, 2008
  */
-@Remote
+//@Remote
 public interface AuthenticationRemote extends Authentication {
 }
