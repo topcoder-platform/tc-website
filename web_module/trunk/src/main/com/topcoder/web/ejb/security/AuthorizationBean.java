b8d0ab8062846ce0a3bccb6612e944edac97ec6c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-02-28 20:38:45 +0000

    use the cached subject

--- web_module/trunk/src/main/com/topcoder/web/ejb/security/AuthorizationBean.java
+++ web_module/trunk/src/main/com/topcoder/web/ejb/security/AuthorizationBean.java
@@ -1,15 +1,16 @@
 package com.topcoder.web.ejb.security;
 
 import com.topcoder.security.NoSuchUserException;
-import com.topcoder.security.admin.PrincipalMgrLocal;
 import com.topcoder.security.policy.GenericPermission;
 import com.topcoder.security.policy.PolicyLocal;
 import com.topcoder.shared.security.Resource;
-import com.topcoder.web.common.security.Constants;
+import com.topcoder.web.common.SecurityHelper;
 
+import javax.ejb.CreateException;
 import javax.ejb.Stateless;
 import javax.naming.NamingException;
 import java.lang.reflect.InvocationTargetException;
+import java.rmi.RemoteException;
 
 /**
  * @author dok
@@ -21,9 +22,8 @@ public class AuthorizationBean implements AuthorizationLocal, AuthorizationRemot
     public boolean hasPermission(long userId, Resource resource) {
         try {
             PolicyLocal pl = (PolicyLocal) com.topcoder.web.common.security.Constants.createLocalEJB(PolicyLocal.class);
-            PrincipalMgrLocal pmgr = (PrincipalMgrLocal) Constants.createLocalEJB(PrincipalMgrLocal.class);
             try {
-                return pl.checkPermission(pmgr.getUserSubject(userId), new GenericPermission(resource.getName()));
+                return pl.checkPermission(SecurityHelper.getUserSubject(userId), new GenericPermission(resource.getName()));
             } catch (NoSuchUserException e) {
                 return false;
             }
@@ -37,6 +37,11 @@ public class AuthorizationBean implements AuthorizationLocal, AuthorizationRemot
             throw new GeneralSecurityException(e);
         } catch (IllegalAccessException e) {
             throw new GeneralSecurityException(e);
+        } catch (CreateException e) {
+            throw new GeneralSecurityException(e);
+        } catch (RemoteException e) {
+            throw new GeneralSecurityException(e);
         }
+
     }
 }
