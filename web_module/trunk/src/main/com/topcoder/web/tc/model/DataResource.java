e33098390f15ed37f457e5e41b8a58772f071113
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-02-28 20:27:38 +0000

    make serializable

--- web_module/trunk/src/main/com/topcoder/web/tc/model/DataResource.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/model/DataResource.java
@@ -3,17 +3,20 @@ package com.topcoder.web.tc.model;
 import com.topcoder.shared.security.Resource;
 
 /**
- * @author  dok
- * @version  $Revision$ $Date$
- * Create Date: May 13, 2005
+ * @author dok
+ * @version $Revision$ $Date$
+ *          Create Date: May 13, 2005
  */
 public class DataResource implements Resource {
+
+    private static final long serialVersionUID = 666L;
 
     private String name = null;
 
     public DataResource(String name) {
         this.name = "DataDump:" + name;
     }
+
     public String getName() {
         return this.name;
     }
