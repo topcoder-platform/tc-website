551d61885d77efedcbc4f882984d78cdd3c41451
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-03-14 17:29:10 +0000

    try again to exclude method

--- src/main/com/topcoder/web/ejb/security/AuthorizationRemote.java
+++ src/main/com/topcoder/web/ejb/security/AuthorizationRemote.java
@@ -1,14 +1,23 @@
 package com.topcoder.web.ejb.security;
 
+import com.topcoder.shared.security.Resource;
+
 import javax.ejb.Remote;
+import javax.jws.WebMethod;
 
 /**
  * @author dok
  * @version $Id$
  *          Create Date: Feb 28, 2008
- * @see Authorization
  */
 @Remote
 public interface AuthorizationRemote extends Authorization {
+
+    @WebMethod(exclude = true)
+    boolean hasPermission(long userId, Resource resource);
+
+    @WebMethod
+    boolean hasPermision(long userId, String resource);
+
 
 }
