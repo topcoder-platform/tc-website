f08410474a2b5c6b40be8d073c0c12806c69be71
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-04-08 22:20:01 +0000

    [TCWEB-181] update list of mediums

--- web_module/branches/studio_ass_2_2008_03_25/src/testcases/com/topcoder/web/studio/dao/MediumDAOTestCase.java
+++ web_module/branches/studio_ass_2_2008_03_25/src/testcases/com/topcoder/web/studio/dao/MediumDAOTestCase.java
@@ -3,13 +3,13 @@
  */
 package com.topcoder.web.studio.dao;
 
+import com.topcoder.web.common.dao.GenericDAO;
 import com.topcoder.web.studio.TCHibernateTestCase;
 import com.topcoder.web.studio.model.Medium;
-import com.topcoder.web.common.dao.GenericDAO;
 import junit.framework.Assert;
 
-import java.util.List;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * <p>A unit test for default implementation of {@link MediumDAO} interface.</p>
@@ -21,17 +21,19 @@ import java.io.Serializable;
 public class MediumDAOTestCase extends TCHibernateTestCase {
 
     /**
-     * <p>An <code>Integer</code> array listing the IDs of pre-defined medium types.</p>
-     */
-    private final Integer[] ids = {Medium.APPAREL, Medium.BROCHURE, Medium.COMPUTER_SCREEN, Medium.EMAIL_NEWSLETTER,
-                                   Medium.LARGE_PRINT, Medium.POSTER, Medium.PRINT, Medium.STATIONARY, Medium.WEB};
-
-    /**
      * <p>A <code>String</code> array listing the descriptions of pre-defined medium types.</p>
      */
-    private final String[] descriptions = {"Aparrel (T-Shirt, Embroidery, Hat etc.)", "Brochure", "Computer Screen",
-                                           "Email Newsletter", "Sign/Banner (Large Print)", "Poster", "Print",
-                                           "Stationary (letterhead, business cards, envelopes, etc.)", "Web"};
+    private final String[] descriptions = {"Web",
+            "Application",
+            "Desktop",
+            "Email",
+            "Print",
+            "Large Print",
+            "Mobile",
+            "Apparel",
+            "MediaSize",
+            "Other"
+    };
 
     /**
      * <p>The instance of {@link MediumDAO} which is tested. This instance is initialized in {@link #setUp()} method and
@@ -66,24 +68,26 @@ public class MediumDAOTestCase extends TCHibernateTestCase {
 
     /**
      * <p>Accuracy test. Tests the {@link MediumDAO#find(Serializable)} method for accurate behavior.</p>
-     *
+     * <p/>
      * <p>Passes the IDs of existing medium types and expects the method to return the valid {@link Medium} instances
      * matching the provided IDs.</p>
      */
     public void testFind_Integer() {
-        int cnt = this.ids.length;
+        int cnt = this.descriptions.length;
+
+        Integer firstMediumId = 1;
 
         for (int i = 0; i < cnt; i++) {
-            Medium type = this.testedInstance.find(this.ids[i]);
+            Medium type = this.testedInstance.find(firstMediumId);
             Assert.assertNotNull("The existing medium type is not found", type);
-            Assert.assertEquals("Wrong medium type ID retrieved", this.ids[i], type.getId());
+            Assert.assertEquals("Wrong medium type ID retrieved", firstMediumId, type.getId());
             Assert.assertEquals("Wrong medium type description retrieved", this.descriptions[i], type.getDescription());
         }
     }
 
     /**
      * <p>Accuracy test. Tests the {@link MediumDAO#find(Serializable)} method for accurate behavior.</p>
-     *
+     * <p/>
      * <p>Passes the IDs of non-existing medium types and expects the method to return <code>nuill</code>.</p>
      */
     public void testFind_Integer_NonExisting() {
@@ -96,11 +100,11 @@ public class MediumDAOTestCase extends TCHibernateTestCase {
 
     /**
      * <p>Accuracy test. Tests the {@link MediumDAO#findAll()} method for accurate behavior.</p>
-     *
+     * <p/>
      * <p>Verifies that the method returns the valid {@link Medium} instances for all existing medium types.</p>
      */
     public void testFindAll() {
-        int cnt = this.ids.length;
+        int cnt = this.descriptions.length;
 
         List<Medium> types = this.testedInstance.findAll();
         Assert.assertEquals("Wrong number of medium types is retrieved", cnt, types.size());
@@ -108,25 +112,23 @@ public class MediumDAOTestCase extends TCHibernateTestCase {
         for (int i = 0; i < cnt; i++) {
             boolean found = false;
             for (Medium returnedType : types) {
-                if (returnedType.getId().equals(this.ids[i])) {
+                if (returnedType.getDescription().equals(this.descriptions[i])) {
                     found = true;
-                    Assert.assertEquals("Wrong medium type description retrieved", this.descriptions[i],
-                                        returnedType.getDescription());
                 }
             }
-            Assert.assertTrue("The medium type " + this.ids[i] + " is not retrieved", found);
+            Assert.assertTrue("The medium type " + this.descriptions[i] + " is not retrieved", found);
         }
     }
 
     /**
      * <p>Accuracy test. Tests the declaration of the tested class.</p>
-     *
+     * <p/>
      * <p>Verifies that the interface is declared correctly.</p>
      */
     public void testDeclaration() {
         Class<GenericDAO> superClass = GenericDAO.class;
         Class<MediumDAO> testedClass = MediumDAO.class;
         Assert.assertTrue(testedClass.getName() + " does not extend " + superClass.getName(),
-                          superClass.isAssignableFrom(testedClass));
+                superClass.isAssignableFrom(testedClass));
     }
 }
