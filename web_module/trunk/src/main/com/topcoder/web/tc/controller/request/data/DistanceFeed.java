fa0637ed4068e58d7b6ea6ffa18d865223946b62
geldridge <geldridge@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-04-15 21:46:09 +0000

    greg testing

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/data/DistanceFeed.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/data/DistanceFeed.java
@@ -2,6 +2,7 @@ package com.topcoder.web.tc.controller.request.data;
 
 import com.topcoder.shared.dataAccess.DataAccessInt;
 import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
 import com.topcoder.shared.util.DBMS;
 import com.topcoder.web.common.PermissionException;
 import com.topcoder.web.common.datafeed.AllColumns;
@@ -15,6 +16,9 @@ import com.topcoder.web.tc.Constants;
 import com.topcoder.web.tc.controller.request.Base;
 import com.topcoder.web.common.security.DataResource;
 
+import java.util.Map;
+
+
 /**
  * @author geldridge
  * @version $Revision$ Date: 2008/04/15 00:00:00
@@ -23,14 +27,22 @@ import com.topcoder.web.common.security.DataResource;
 public class DistanceFeed extends Base {
 
     protected void businessProcessing() throws Exception {
-        /*Request r = new Request();
+        Request r = new Request();
         r.setContentHandle("dd_fast_overlap");
         r.setProperty("cr", getRequest().getParameter("cr"));
         DataAccessInt da = getDataAccess(DBMS.TCS_DW_DATASOURCE_NAME, false);
-        CommandRunner cmd = new CommandRunner(da, r);*/
-
+        CommandRunner cmd = new CommandRunner(da, r);
+	Map<String, ResultSetContainer> dm = cmd.getData();
+	
+	ResultSetContainer rsc = dm.get("dd_fast_overlap");
         getResponse().setContentType("text/xml");
-        getResponse().getOutputStream().print("<map></map>");
+        getResponse().getOutputStream().println("<map>");
+
+
+	for (ResultSetContainer.ResultSetRow row : rsc) {
+		getResponse().getOutputStream().println("<row>" + row.toString() + "</row>");		
+	}
+	getResponse().getOutputStream().println("</map>");
         getResponse().flushBuffer();
     }
 }
