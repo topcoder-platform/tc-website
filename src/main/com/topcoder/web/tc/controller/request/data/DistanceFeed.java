cd229924400a2444317b0ea9c7db98aa4ca2c2af
geldridge <geldridge@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-04-15 21:09:32 +0000

    greg testing

---
+++ src/main/com/topcoder/web/tc/controller/request/data/DistanceFeed.java
@@ -0,0 +1,36 @@
+package com.topcoder.web.tc.controller.request.data;
+
+import com.topcoder.shared.dataAccess.DataAccessInt;
+import com.topcoder.shared.dataAccess.Request;
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.web.common.PermissionException;
+import com.topcoder.web.common.datafeed.AllColumns;
+import com.topcoder.web.common.datafeed.Column;
+import com.topcoder.web.common.datafeed.CommandRunner;
+import com.topcoder.web.common.datafeed.DataFeeder;
+import com.topcoder.web.common.datafeed.RSCDataFeed;
+import com.topcoder.web.common.datafeed.RSCParamDataFeed;
+import com.topcoder.web.common.security.TCSAuthorization;
+import com.topcoder.web.tc.Constants;
+import com.topcoder.web.tc.controller.request.Base;
+import com.topcoder.web.common.security.DataResource;
+
+/**
+ * @author geldridge
+ * @version $Revision$ Date: 2008/04/15 00:00:00
+ *          Create Date: Apr 15, 2008
+ */
+public class DistanceFeed extends Base {
+
+    protected void businessProcessing() throws Exception {
+        /*Request r = new Request();
+        r.setContentHandle("dd_fast_overlap");
+        r.setProperty("cr", getRequest().getParameter("cr"));
+        DataAccessInt da = getDataAccess(DBMS.TCS_DW_DATASOURCE_NAME, false);
+        CommandRunner cmd = new CommandRunner(da, r);*/
+
+        getResponse().setContentType("text/xml");
+        getResponse().getOutputStream().print("<map></map>");
+        getResponse().flushBuffer();
+    }
+}
