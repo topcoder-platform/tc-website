0a2efb1a23d367ae7b56d563231ce538d74d733d
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-04-22 14:30:07 +0000

    use release instead of end tag method clear out instance variables

--- web_module/trunk/src/main/com/topcoder/web/common/tag/ListIteratorTag.java
+++ web_module/trunk/src/main/com/topcoder/web/common/tag/ListIteratorTag.java
@@ -4,8 +4,7 @@ import javax.servlet.jsp.JspException;
 import java.util.List;
 
 /**
- *
- * @author  Greg Paul
+ * @author Greg Paul
  */
 public class ListIteratorTag extends IteratorTag {
 
@@ -25,14 +24,10 @@ public class ListIteratorTag extends IteratorTag {
         return super.doStartTag();
     }
 
-    /**
-     * Just in case the app server is caching tag (jboss!!!)
-     * we have to clear out all the instance variables at the
-     * end of execution
-     */
-    public int doEndTag() throws JspException {
+    public void release() {
         this.list = null;
-        return super.doEndTag();
+        super.release();
     }
+
 
 }
