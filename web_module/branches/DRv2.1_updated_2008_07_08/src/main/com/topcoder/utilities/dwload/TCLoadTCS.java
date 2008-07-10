7267b1b35f001f73952eb143166e0068afeb73b8
pwolfus <pwolfus@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-07-10 19:54:01 +0000

    Digital Run v2.0

--- web_module/branches/DRv2.1_updated_2008_07_08/src/main/com/topcoder/utilities/dwload/TCLoadTCS.java
+++ web_module/branches/DRv2.1_updated_2008_07_08/src/main/com/topcoder/utilities/dwload/TCLoadTCS.java
@@ -4404,14 +4404,14 @@ public class TCLoadTCS extends TCLoad {
        log.debug("load digital run track contests");
 
        final String SELECT_CONTESTS =
-           " select tc.track_contest_id, tc.track_id, tc.track_contest_desc, tctl.track_contest_type_desc " +
+           " select tc.track_contest_id, tc.track_id, tc.track_contest_desc, tctl.track_contest_type_id, tctl.track_contest_type_desc " +
            " from track_contest tc, track_contest_type_lu tctl " +
            " where tc.track_contest_type_id = tctl.track_contest_type_id " +
            " and tc.create_date > ?";
 
        final String INSERT =
-           "insert into track_contest (track_contest_id, track_id, track_contest_desc, track_contest_type_desc) " +
-                   " values (?,?,?,?)";
+           "insert into track_contest (track_contest_id, track_id, track_contest_desc, track_contest_type_id, track_contest_type_desc) " +
+                   " values (?,?,?,?,?)";
 
        PreparedStatement selectContests = prepareStatement(SELECT_CONTESTS, SOURCE_DB);
        PreparedStatement insert = prepareStatement(INSERT, TARGET_DB);
@@ -4429,7 +4429,8 @@ public class TCLoadTCS extends TCLoad {
                insert.setInt(1, rsContests.getInt("track_contest_id"));
                insert.setInt(2, rsContests.getInt("track_id"));
                insert.setString(3, rsContests.getString("track_contest_desc"));
-               insert.setString(4, rsContests.getString("track_contest_type_desc"));
+               insert.setInt(4, rsContests.getInt("track_contest_type_id"));
+               insert.setString(5, rsContests.getString("track_contest_type_desc"));
 
                insert.executeUpdate();
                count++;
