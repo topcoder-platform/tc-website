058b19afe7f3b3bb302311fd723e6de07b726ee0
pwolfus <pwolfus@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-07-15 20:59:33 +0000

    Copied remotely

---
+++ web_module/tags/before_merge_DRv2.1_updated_2008_07_08/src/main/com/topcoder/web/common/dao/hibernate/ProblemSetDAOHibernate.java
@@ -0,0 +1,13 @@
+package com.topcoder.web.common.dao.hibernate;
+
+import com.topcoder.web.common.dao.ProblemSetDAO;
+import com.topcoder.web.common.model.algo.ProblemSet;
+ 
+
+/**
+ * @author pulky
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Jan 18, 2007
+ */
+public class ProblemSetDAOHibernate extends GenericBase<ProblemSet, Integer> implements ProblemSetDAO {
+}
