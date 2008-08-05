14cbca437fea396dea9f039a95fdbfb51fcb5884
dbelfer <dbelfer@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-08-05 00:11:19 +0000

    initial commit for testing stage 2

---
+++ web_module/branches/winformula_stage_2_2008_07_29/src/main/com/topcoder/web/winformula/algorithm/services/SubmissionHistoryItem.java
@@ -0,0 +1,80 @@
+/*
+ * SubmissionHistoryItem
+ * 
+ * Created Aug 3, 2008
+ */
+package com.topcoder.web.winformula.algorithm.services;
+
+import java.util.Date;
+
+/**
+ * @author Diego Belfer (Mural)
+ * @version $Id$
+ */
+public class SubmissionHistoryItem {
+    //Unique id for the coder submissions
+    private int number;
+    private Date time;
+    
+    //Submission on round data
+    private int roundId;
+    private String roundName;
+    
+    private boolean example; //Only first round containing this submission
+    private double examplePoints;
+    private boolean lockedIn;
+
+    private double accuracy;
+    private boolean inQueue;
+
+    
+    public SubmissionHistoryItem(int submissionNumber, Date submissionTime, int roundId, String roundName,
+            boolean examplesForSubmission, double examplePoints, boolean lockedIn, double accuracyPercentage, boolean inqueue) {
+        this.number = submissionNumber;
+        this.time = submissionTime;
+        this.roundId = roundId;
+        this.roundName = roundName;
+        this.example = examplesForSubmission;
+        this.examplePoints = examplePoints;
+        this.lockedIn = lockedIn;
+        this.accuracy = accuracyPercentage;
+        this.inQueue = inqueue;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public int getRoundId() {
+        return roundId;
+    }
+
+    public String getRoundName() {
+        return roundName;
+    }
+
+    public boolean isExample() {
+        return example;
+    }
+
+    public boolean isLockedIn() {
+        return lockedIn;
+    }
+
+    public double getAccuracy() {
+        return accuracy;
+    }
+
+    public double getExamplePoints() {
+        return examplePoints;
+    }
+
+    public boolean isInQueue() {
+        return inQueue;
+    }
+
+}
---
+++ web_module/branches/winformula_stage_2_2008_07_29/src/main/com/topcoder/web/winformula/algorithm/services/WinFormulaServicesImpl.java
@@ -0,0 +1,174 @@
+/*
+ * WinFormulaServicesBean
+ * 
+ * Created Jul 29, 2008
+ */
+package com.topcoder.web.winformula.algorithm.services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.util.DBMS;
+
+/**
+ * @author Diego Belfer (Mural)
+ * @version $Id$
+ */
+public class WinFormulaServicesImpl {
+   /**
+    * Get all submissions for the given coder in the contest
+    * 
+    * @param contestId The id of the contest
+    * @param coderId The id of the coder
+    * @return List<SubmissionHistoryItem> containing submissions
+    * @throws WinFormulaServicesException if data could not be retrieved
+    */
+    public List<SubmissionHistoryItem> getSubmissionHistory(int contestId, int coderId) throws WinFormulaServicesException {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            Connection cnn = DBUtils.initDBBlock();
+            String cmd = "SELECT s.submission_number"
+                         + "     , s.submit_time"
+                         + "     , s.submission_points " //Example results points
+                         + "     , case when cs.submission_number = s.submission_number then cs.status_id else 0 end as status_id"
+                         + "     , r.round_id"
+                         + "     , r.name"
+                         + "     , rs.end_time < current as locked" 
+                         + "     , p.prediction_accuracy" 
+                         + "     , EXISTS(SELECT coder_id FROM long_system_test_result ltr "
+                         + "              WHERE ltr.round_id = r.round_id AND ltr.coder_id = cs.coder_id AND test_action = 10) as example"
+                         + "  FROM long_submission s"
+                         + "     , long_component_state cs"
+                         + "     , round r"
+                         + "     , round_component rc" 
+                         + "     , round_segment rs"
+                         + "     , OUTER mini_season_prediction p"
+                         + " WHERE r.contest_id = ?"
+                         + "   AND rc.round_id = r.round_id"
+                         + "   AND cs.round_id = r.round_id AND cs.coder_id = ? AND cs.component_id = rc.component_id"
+                         + "   AND s.long_component_state_id = cs.long_component_state_id AND s.example = 0"
+                         + "   AND rs.round_id = r.round_id AND rs.segment_id = 2"
+                         + "   AND p.coder_id = cs.coder_id AND p.round_id = r.round_id"
+                         + " ORDER BY s.submission_number desc, r.round_id desc";
+            ps = cnn.prepareStatement(cmd);
+            ps.setInt(1, contestId);
+            ps.setInt(2, coderId);
+            rs = ps.executeQuery();
+            
+            List<SubmissionHistoryItem> list = new LinkedList();
+            while (rs.next()) {
+                int submissionNumber = rs.getInt(1);;
+                long submitTime = rs.getLong(2);
+                double submissionPoints = rs.getDouble(3);
+                int statusId = rs.getInt(4);
+                int roundId = rs.getInt(5);
+                String name = rs.getString(6);
+                boolean lockedIn = rs.getBoolean(7); 
+                double accuracy = rs.getDouble(8);
+                if (rs.wasNull()) {
+                    accuracy = -1;
+                }
+                boolean example = rs.getBoolean(9);
+                boolean inqueue = (statusId == 130);
+                list.add(new SubmissionHistoryItem(submissionNumber, new Date(submitTime), roundId, name, example, submissionPoints, lockedIn, accuracy, inqueue));
+            }
+            return list;
+        } catch (Exception e) {
+            throw new WinFormulaServicesException("INTERNAL_SERVER");
+        } finally {
+            DBMS.close(ps, rs);
+            DBUtils.endDBBlock();
+        }
+    }
+    
+    
+    /**
+     * Get all rounds of the given contest
+     * Resulset: 
+     *   round_id
+     *   name
+     *   start_time
+     *   end_time
+     *   coding_phase is accepting submissions
+     *   future will the round start in the future
+     * 
+     * @param contestId The id of the contest
+     * @param coderId The id of the coder
+     * @return a result container
+     * @throws WinFormulaServicesException if data could not be retrieved
+     */
+    public ResultSetContainer getRoundsByContest(int contestId) throws WinFormulaServicesException {
+        try {
+            Connection cnn = DBUtils.initDBBlock();
+            String cmd = "SELECT rs.round_id, r.name, rs.start_time, rs.end_time, " +
+            	         "       (current > start_time AND current < rs.end_time) as coding_phase, (current < rs.start_time) as future" + 
+                         " FROM round_segment rs, round r " +
+                         " WHERE rs.round_id = r.round_id and rs.segment_id = 2 and r.contest_id = ?" +
+                         " ORDER BY 2";
+            PreparedStatement ps = cnn.prepareStatement(cmd);
+            ps.setInt(1, contestId);
+            ResultSet rs = ps.executeQuery();
+            ResultSetContainer rsc = new ResultSetContainer(rs);
+            return rsc;
+        } catch (Exception e) {
+            throw new WinFormulaServicesException("INTERNAL_SERVER");
+        } finally {
+            DBUtils.endDBBlock();
+        }
+    }
+
+
+    public LastCompilationInfo getLastCompilation(int contestId, int coderId) throws WinFormulaServicesException {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            Connection cnn = DBUtils.initDBBlock();
+            String cmd = "SELECT FIRST 1 lc.compilation_text, lc.language_id" +
+                         " FROM long_compilation lc, long_component_state cs, round r " +
+                         " WHERE r.contest_id = ? AND " +
+                         "       cs.round_id = r.round_id AND cs.coder_id = ? AND" +
+                         "       lc.long_component_state_id = cs.long_component_state_id AND lc.code IS NOT NULL" +
+                         " ORDER BY r.round_id desc";
+            ps = cnn.prepareStatement(cmd);
+            ps.setInt(1, contestId);
+            ps.setInt(2, coderId);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return new LastCompilationInfo(rs.getInt(2), rs.getString(1));
+            }
+            return null;
+        } catch (Exception e) {
+            throw new WinFormulaServicesException("INTERNAL_SERVER");
+        } finally {
+            DBMS.close(ps, rs);
+            DBUtils.endDBBlock();
+        }
+    }
+
+
+    public boolean isUserRegisteredInContest(int contestId, int userId) throws WinFormulaServicesException {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            Connection cnn = DBUtils.initDBBlock();
+            String cmd = "SELECT contest_id FROM contest_registration" +
+                         " WHERE contest_id = ? AND user_id = ?";
+            ps = cnn.prepareStatement(cmd);
+            ps.setInt(1, contestId);
+            ps.setInt(2, userId);
+            rs = ps.executeQuery();
+            return rs.next();
+        } catch (Exception e) {
+            throw new WinFormulaServicesException("INTERNAL_SERVER");
+        } finally {
+            DBMS.close(ps, rs);
+            DBUtils.endDBBlock();
+        }
+    }
+}
