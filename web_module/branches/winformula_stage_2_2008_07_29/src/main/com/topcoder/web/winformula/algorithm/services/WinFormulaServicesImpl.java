b105a91569ee7b0b1299d418be9cdb8c0b646ff8
dbelfer <dbelfer@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-08-07 15:15:02 +0000

    using 1.5 method

--- web_module/branches/winformula_stage_2_2008_07_29/src/main/com/topcoder/web/winformula/algorithm/services/WinFormulaServicesImpl.java
+++ web_module/branches/winformula_stage_2_2008_07_29/src/main/com/topcoder/web/winformula/algorithm/services/WinFormulaServicesImpl.java
@@ -89,7 +89,7 @@ public class WinFormulaServicesImpl {
                 boolean inqueue = (statusId == ContestConstants.NOT_CHALLENGED);
                 list.add(new SubmissionHistoryItem(submissionNumber, new Date(submitTime), roundId, name, example, submissionPoints, lockedIn, accuracy, inqueue));
                 if (prvRoundId != roundId && prvSubmission == submissionNumber) {
-                    SubmissionHistoryItem lastOne = list.peekLast();
+                    SubmissionHistoryItem lastOne = list.getLast();
                     if (!lastOne.isExample() && !lastOne.isLockedIn()) {
                         //The submission is just an initial copy that may be/was overwriten by the coder
                         list.removeLast();
