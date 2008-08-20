047f056f974b4ac52598b8ad279d842ddca7c374
dbelfer <dbelfer@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-08-20 14:59:51 +0000

    Event handler implementation for MM events

--- web_module/branches/winformula_stage_3_2008_08_15/src/main/com/topcoder/web/winformula/algorithm/eventhandler/PredictionHandlerMDB.java
+++ web_module/branches/winformula_stage_3_2008_08_15/src/main/com/topcoder/web/winformula/algorithm/eventhandler/PredictionHandlerMDB.java
@@ -11,14 +11,14 @@ import javax.ejb.MessageDrivenContext;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
+import com.topcoder.server.ejb.TestServices.LongContestServiceEventListener;
+import com.topcoder.server.ejb.TestServices.LongRoundOverallScore;
+import com.topcoder.shared.serviceevent.ServiceEventMessageListener;
 import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.web.winformula.algorithm.services.PredictionLoader;
 
 /**
- * @author Diego Belfer (Mural)
- * @version $Id$
- */
-/**
- * Message driven bean responsible for handling asynchronous service calls.
+ * Message driven bean responsible for handling events from the MM Engine
  * 
  * @author Diego Belfer (mural)
  * @version $Id$
@@ -26,21 +26,68 @@ import com.topcoder.shared.util.logging.Logger;
 public class PredictionHandlerMDB implements MessageListener, MessageDrivenBean  {
     private static final Logger log = Logger.getLogger(PredictionHandlerMDB.class);
     private static final Logger alertLog = Logger.getLogger("ALERT."+PredictionHandlerMDB.class.getName());
+    private final ServiceEventMessageListener listener;
+    private final PredictionLoader loader = new PredictionLoader();
+    
+    @SuppressWarnings("unused")
     private MessageDrivenContext ctx;
+    
+    public PredictionHandlerMDB() {
+        listener = new ServiceEventMessageListener(LongContestServiceEventListener.SERVICE_NAME);
+        LongContestServiceEventListener.addEventHandler(listener, new LongContestServiceEventListener.Handler() {
+            public void testCompleted(int roundId, int coder, int componentId, int submissionNumber, boolean example) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Test completed roundId="+roundId+" coderId="+coder+" submissionNumber="+submissionNumber);
+                }
+            }
+        
+            public void systemTestCompleted(int roundId, int coderId, int componentId, int submissionNumber) {
+                log.info("System Test complete roundId="+roundId+" coderId="+coderId+" submissionNumber="+submissionNumber);
+                loader.handleSystemTestCompleted(roundId, coderId, submissionNumber);
+            }
+        
+            public void submissionMade(int roundId, int coderId, int componentId, boolean example, int submissionNumber) {
+                if (log.isDebugEnabled()) {
+                    log.debug("submissionMade roundId="+roundId+" coderId="+coderId+" submissionNumber="+submissionNumber);
+                }
+            }
+        
+            public void saved(int roundId, int coderId, int componentId, String source, int language) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Saved roundId="+roundId+" coderId="+coderId+" language="+language);
+                }
+            }
+        
+            public void overallScoreRecalculated(LongRoundOverallScore scores) {
+            }
+        
+            public void componentOpened(int roundId, int coderId, int componentId, long openTime) {
+                if (log.isDebugEnabled()) {
+                    log.debug("componentOpened roundId="+roundId+" coderId="+coderId+" openTime="+openTime);
+                }
+            }
+        
+            public void coderRegistered(int roundId, int coderId) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Coder registered roundId="+roundId+" coderId="+coderId);
+                }
+            }
+        });
+    }
     
     public void onMessage(Message srcMsg) {
         if (log.isDebugEnabled()) {
             log.debug("Processing new incoming message: "+srcMsg);
         }
         try {
-            
-//        } catch (JMSException e) {
-//            log.error("Exception while sending response" , e);
-//            log.error(srcMsg);
-//            throw new EJBException("Exception while sending response, forcing MDB removal");
+            listener.onMessage(srcMsg);
+        } catch (Exception e) {
+            log.error("Exception while processing Event message " , e);
+            alertLog.error("Exception while processing Event message " , e);
+            throw new EJBException("Exception while sending response, forcing MDB removal");
         } finally {
             if (log.isDebugEnabled()) {
-                log.debug("Message processed");
+                log.debug("Message processing finished");
             }
         }
     }
