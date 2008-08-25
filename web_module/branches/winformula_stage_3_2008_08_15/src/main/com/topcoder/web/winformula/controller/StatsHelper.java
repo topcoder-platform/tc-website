79d27c14e5f662b4ad051603e51b9da89fe9f758
pwolfus <pwolfus@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-08-25 23:22:23 +0000

    misc fixes

--- web_module/branches/winformula_stage_3_2008_08_15/src/main/com/topcoder/web/winformula/controller/StatsHelper.java
+++ web_module/branches/winformula_stage_3_2008_08_15/src/main/com/topcoder/web/winformula/controller/StatsHelper.java
@@ -199,7 +199,7 @@ public class StatsHelper {
         int startRank = getStartRank(Integer.parseInt(numPage), Integer.parseInt(numRecords));
         int endRank = getEndRank(Integer.parseInt(numPage), Integer.parseInt(numRecords));
         if (endRank >= sizeBeforeCrop) {
-            endRank = sizeBeforeCrop - 1;
+            endRank = sizeBeforeCrop;
         }
             
         List<StandingsItem> result;
@@ -211,7 +211,7 @@ public class StatsHelper {
 
         log.debug("startRank: " + startRank);
         log.debug("startRank: " + endRank);
-        log.debug("startRank: " + sizeBeforeCrop);
+        log.debug("sizeBeforeCrop: " + sizeBeforeCrop);
         
         request.setAttribute("croppedDataBefore", new Boolean(startRank > 1));
         request.setAttribute("croppedDataAfter", new Boolean(endRank != sizeBeforeCrop - 1));
