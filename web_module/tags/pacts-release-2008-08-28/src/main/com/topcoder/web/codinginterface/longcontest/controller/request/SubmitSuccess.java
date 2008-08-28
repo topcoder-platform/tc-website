701c03a6fa44cf8eb2fd5afea238f16f0471878d
pwolfus <pwolfus@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-08-28 22:56:41 +0000

    https://www.topcoder.com/bugs/browse/PACTS/fixforversion/11466

---
+++ web_module/tags/pacts-release-2008-08-28/src/main/com/topcoder/web/codinginterface/longcontest/controller/request/SubmitSuccess.java
@@ -0,0 +1,29 @@
+package com.topcoder.web.codinginterface.longcontest.controller.request;
+
+import com.topcoder.server.ejb.TestServices.LongContestServicesLocator;
+import com.topcoder.web.codinginterface.longcontest.Constants;
+import com.topcoder.web.common.TCWebException;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Nov 29, 2005
+ */
+public class SubmitSuccess extends Base {
+
+    protected void longContestProcessing() throws TCWebException {
+        try {
+            int numberOfSubmissionOnLongTestQueue = LongContestServicesLocator.getService().getNumberOfSubmissionOnLongTestQueue();
+            getRequest().setAttribute(Constants.QUEUE_LENGTH, new Long(numberOfSubmissionOnLongTestQueue));
+            getRequest().setAttribute(Constants.ROUND_ID, getRequest().getParameter(Constants.ROUND_ID));
+            getRequest().setAttribute(Constants.COMPONENT_ID, getRequest().getParameter(Constants.COMPONENT_ID));
+            getRequest().setAttribute(Constants.CONTEST_ID, getRequest().getParameter(Constants.CONTEST_ID));
+            getRequest().setAttribute(Constants.MESSAGE, getRequest().getSession().getAttribute(Constants.MESSAGE));
+            getRequest().getSession().removeAttribute(Constants.MESSAGE);
+            setNextPage(Constants.PAGE_SUBMIT_SUCCESS);
+            setIsNextPageInContext(true);
+        } catch (Exception e) {
+            throw new TCWebException(e);
+        }
+    }
+}
