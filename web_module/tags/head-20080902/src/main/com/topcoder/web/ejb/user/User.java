0ee4ee9963a893bfc5cbec56c04f789edfb52086
dbelfer <dbelfer@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-09-03 22:01:30 +0000

    New tag from /trunk for member access

---
+++ web_module/tags/head-20080902/src/main/com/topcoder/web/ejb/user/User.java
@@ -0,0 +1,60 @@
+package com.topcoder.web.ejb.user;
+
+import com.topcoder.web.common.RowNotFoundException;
+
+import javax.ejb.EJBException;
+import javax.ejb.EJBObject;
+import java.rmi.RemoteException;
+
+public interface User extends EJBObject {
+
+    long createNewUser(String handle, char status, String dataSource, String idDataSource)
+            throws EJBException, RemoteException;
+
+    void createUser(long userId, String _handle, char status, String dataSource)
+            throws EJBException, RemoteException;
+
+
+    void setFirstName(long userId, String firstName, String dataSource)
+            throws EJBException, RemoteException;
+
+    void setMiddleName(long userId, String middleName, String dataSource)
+            throws EJBException, RemoteException;
+
+    void setLastName(long userId, String lastName, String dataSource)
+            throws EJBException, RemoteException;
+
+    void setStatus(long userId, char status, String dataSource)
+            throws EJBException, RemoteException;
+
+    void setActivationCode(long userId, String code, String dataSource)
+            throws EJBException, RemoteException;
+
+    void setPassword(long userId, String passwrd, String dataSource)
+            throws EJBException, RemoteException;
+
+    public String getHandle(long userId, String dataSource)
+            throws EJBException, RemoteException;
+
+    public void setHandle(long userId, String handle, String dataSource)
+            throws EJBException, RemoteException;
+
+    String getFirstName(long userId, String dataSource)
+            throws EJBException, RemoteException;
+
+    String getLastName(long userId, String dataSource) throws EJBException, RemoteException;
+
+    String getMiddleName(long userId, String dataSource) throws EJBException, RemoteException;
+
+    String getActivationCode(long userId, String dataSource) throws EJBException, RemoteException;
+
+    String getPassword(long userId, String dataSource) throws RowNotFoundException, RemoteException;
+
+    char getStatus(long userId, String dataSource)
+            throws EJBException, RemoteException;
+
+    boolean userExists(long userId, String dataSource) throws RemoteException, EJBException;
+
+    public boolean userExists(String handle, String dataSource) throws RemoteException, EJBException;
+}
+
