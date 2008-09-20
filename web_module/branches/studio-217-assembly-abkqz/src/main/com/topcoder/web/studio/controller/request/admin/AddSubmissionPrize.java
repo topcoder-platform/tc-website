577ceb59d5d4603088e75298c4839aea34e73991
abkqz <abkqz@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-09-20 13:24:58 +0000

    back to prize

--- web_module/branches/studio-217-assembly-abkqz/src/main/com/topcoder/web/studio/controller/request/admin/AddSubmissionPrize.java
+++ web_module/branches/studio-217-assembly-abkqz/src/main/com/topcoder/web/studio/controller/request/admin/AddSubmissionPrize.java
@@ -1,17 +1,40 @@
 package com.topcoder.web.studio.controller.request.admin;
 
-import com.topcoder.web.studio.model.ContestResult;
-import com.topcoder.web.studio.model.Prize;
-import com.topcoder.web.studio.model.PrizeType;
-import com.topcoder.web.studio.model.Submission;
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.studio.Constants;
 
 /**
  * @author dok
  * @version $Revision$ Date: 2005/01/01 00:00:00
  *          Create Date: Aug 29, 2006
  */
-public class AddSubmissionPrize extends SubmissionPrizeBase {
-    protected void submissionProcessing(Submission s, Prize p) throws Exception {
+public class AddSubmissionPrize extends Base {
+
+	@Override
+	protected void dbProcessing() throws Exception {
+		Long submissionId;
+        try {
+            submissionId = new Long(getRequest().getParameter(Constants.SUBMISSION_ID));
+        } catch (NumberFormatException e) {
+            throw new NavigationException("Invalid Submission Specified");
+        }
+
+        Long prizeId = null;
+        if (!"".equals(StringUtils.checkNull(getRequest().getParameter(Constants.PRIZE_ID)))) {
+            try {
+                prizeId = new Long(getRequest().getParameter(Constants.PRIZE_ID));
+            } catch (NumberFormatException e) {
+                throw new NavigationException("Invalid prize Specified");
+            }
+        } else {
+            throw new NavigationException("Invalid prize Specified");
+        }
+        throw new RuntimeException("AddSubmissionPrize_new: submissionId=" + submissionId + " prizeId=" + prizeId);
+		
+	}
+	
+    /*protected void submissionProcessing(Submission s, Prize p) throws Exception {
         s.addPrize(p);
         //if the prize is a contest prize and we haven't set a contest result placed record for this
         //then set one.  the site uses contest result to show place, so we need to set that up
@@ -25,6 +48,6 @@ public class AddSubmissionPrize extends SubmissionPrizeBase {
                 cr.setPlaced(p.getPlace());
             }
         }
-    }
+    }*/
 }
 
