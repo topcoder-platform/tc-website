bf06dcae41d829d628b759c451a4734ea0c41e07
Margarita <Margarita@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-09-23 10:49:01 +0000

    w

--- web_module/branches/studio-217-assembly-Margarita/src/main/com/topcoder/web/studio/controller/request/admin/AddSubmissionPrize.java
+++ web_module/branches/studio-217-assembly-Margarita/src/main/com/topcoder/web/studio/controller/request/admin/AddSubmissionPrize.java
@@ -49,7 +49,7 @@ public class AddSubmissionPrize extends Base /*extends SubmissionPrizeBase*/ {
         p.setProperty(Context.SECURITY_CREDENTIALS, "password");
         p.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.security.jndi.LoginInitialContextFactory");
         p.setProperty(Context.PROVIDER_URL, ApplicationServer.STUDIO_SERVICES_PROVIDER_URL);
-        p.setProperty(Context.SECURITY_PROTOCOL, "cockpitDomain");
+        //p.setProperty(Context.SECURITY_PROTOCOL, "cockpitDomain");
         Context context = new InitialContext(p);
 
         // get context to Cockpit Jboss Instance.
