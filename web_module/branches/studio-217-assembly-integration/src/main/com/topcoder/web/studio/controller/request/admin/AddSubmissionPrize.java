d187b22cbdd022aa546a3922a0a10bac08db247b
aisacovich <aisacovich@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-10-07 21:10:48 +0000

    STUDIO-217 assembly merged submission

--- web_module/branches/studio-217-assembly-integration/src/main/com/topcoder/web/studio/controller/request/admin/AddSubmissionPrize.java
+++ web_module/branches/studio-217-assembly-integration/src/main/com/topcoder/web/studio/controller/request/admin/AddSubmissionPrize.java
@@ -1,30 +1,56 @@
 package com.topcoder.web.studio.controller.request.admin;
 
-import com.topcoder.web.studio.model.ContestResult;
-import com.topcoder.web.studio.model.Prize;
-import com.topcoder.web.studio.model.PrizeType;
-import com.topcoder.web.studio.model.Submission;
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.web.common.NavigationException;
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.studio.Constants;
+import com.topcoder.web.studio.util.StudioServiceLocator;
 
 /**
+ * AddSubmissionPrize has been refactored in order to delegate the submission placement 
+ * to EJB 3 Studio services located in Cockpit Application Server. <br>
+ * See : Assembly Contest - TopCoder Studio setSubmissionPlacement Service Integration  version 1.0
+ * 
  * @author dok
- * @version $Revision$ Date: 2005/01/01 00:00:00
- *          Create Date: Aug 29, 2006
+ * @author TCSDEVELOPER
+ * @version $Revision$ Date: 2005/01/01 00:00:00 Create Date: Aug 29, 2006
  */
-public class AddSubmissionPrize extends SubmissionPrizeBase {
-    protected void submissionProcessing(Submission s, Prize p) throws Exception {
-        s.addPrize(p);
-        //if the prize is a contest prize and we haven't set a contest result placed record for this
-        //then set one.  the site uses contest result to show place, so we need to set that up
-        //in the case that we're not using online review.
-        if (p.getType().getId().equals(PrizeType.CONTEST)) {
-            ContestResult cr = s.getResult();
-            if (cr == null) {
-                cr = new ContestResult(s);
-            }
-            if (cr.getPlaced() == null) {
-                cr.setPlaced(p.getPlace());
-            }
-        }
-    }
-}
+public class AddSubmissionPrize extends Base {
+	
+	private static Logger log = Logger.getLogger(AddSubmissionPrize.class);
+	
+	protected void dbProcessing() throws Exception {
 
+        Long submissionId;
+        try {
+            submissionId = new Long(getRequest().getParameter(Constants.SUBMISSION_ID));
+        } catch (NumberFormatException e) {
+            throw new NavigationException("Invalid Submission Specified");
+        }
+
+        Long prizeId = null;
+        if (!"".equals(StringUtils.checkNull(getRequest().getParameter(Constants.PRIZE_ID)))) {
+            try {
+                prizeId = new Long(getRequest().getParameter(Constants.PRIZE_ID));
+            } catch (NumberFormatException e) {
+                throw new NavigationException("Invalid Prize Specified");
+            }
+        } else {
+            throw new NavigationException("Invalid Prize Specified");
+        }
+        
+		try {
+			log.debug("About to call StudioService setSubmissionPlacement(submissionId:" + submissionId + ", prizeId:" + prizeId + ")");
+			StudioServiceLocator.getService().setSubmissionPlacement(submissionId, prizeId);
+			log.debug("setSubmissionPlacement successfull");
+		} catch (Exception e) {
+			throw new NavigationException("Submission could not be assigned to a prize");
+		}
+
+        setNextPage(getSessionInfo().getServletPath() + "?" + Constants.MODULE_KEY +
+                "=ViewSubmissionDetail&" + Constants.SUBMISSION_ID + "=" + submissionId);
+        setIsNextPageInContext(false);
+
+    }
+
+}
