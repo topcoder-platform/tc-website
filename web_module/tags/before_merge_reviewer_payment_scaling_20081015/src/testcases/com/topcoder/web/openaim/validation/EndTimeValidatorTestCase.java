154351b2728d4ad50f8457320b7ce7665e71701a
ivern <ivern@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-10-20 13:23:00 +0000

    Tagged trunk before merging branch reviewer_payment_scaling_20081015

---
+++ web_module/tags/before_merge_reviewer_payment_scaling_20081015/src/testcases/com/topcoder/web/openaim/validation/EndTimeValidatorTestCase.java
@@ -0,0 +1,44 @@
+package com.topcoder.web.openaim.validation;
+
+import com.topcoder.web.common.validation.StringInput;
+import com.topcoder.web.openaim.Constants;
+import com.topcoder.web.openaim.TCHibernateTestCase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Jul 18, 2006
+ */
+public class EndTimeValidatorTestCase extends TCHibernateTestCase {
+    public void testEndBeforeStart() {
+        SimpleDateFormat sdf = new SimpleDateFormat(Constants.JAVA_DATE_FORMAT);
+        sdf.setLenient(false);
+        Date start = new Date();
+        Date end = new Date(start.getTime() - 1000 * 60 * 60);
+        assertFalse("validated the case where the contest ends before it starts",
+                new EndTimeValidator(sdf.format(start)).validate(new StringInput(sdf.format(end))).isValid());
+    }
+
+    public void testValidEnd() {
+        SimpleDateFormat sdf = new SimpleDateFormat(Constants.JAVA_DATE_FORMAT);
+        sdf.setLenient(false);
+        Date start = new Date();
+        Date end = new Date(start.getTime() + 1000 * 60 * 60);
+        assertTrue("failed to validate a valid end",
+                new EndTimeValidator(sdf.format(start)).validate(new StringInput(sdf.format(end))).isValid());
+
+    }
+
+    public void testInvalidStart() {
+        SimpleDateFormat sdf = new SimpleDateFormat(Constants.JAVA_DATE_FORMAT);
+        sdf.setLenient(false);
+        Date start = new Date();
+        Date end = new Date(start.getTime() + 1000 * 60 * 60);
+        assertFalse("validated an invalid start",
+                new EndTimeValidator("x" + sdf.format(start)).validate(new StringInput(sdf.format(end))).isValid());
+
+    }
+}
