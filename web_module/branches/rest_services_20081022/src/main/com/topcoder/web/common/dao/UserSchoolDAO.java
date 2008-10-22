64ce127660961b34dfaade2b06d21b19e25efea2
dbelfer <dbelfer@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-10-22 21:51:26 +0000

    Ivern branch for adding rest services

---
+++ web_module/branches/rest_services_20081022/src/main/com/topcoder/web/common/dao/UserSchoolDAO.java
@@ -0,0 +1,12 @@
+package com.topcoder.web.common.dao;
+
+import com.topcoder.web.common.model.UserSchool;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Oct 16, 2007
+ */
+public interface UserSchoolDAO extends GenericDAO<UserSchool, Long> {
+
+}
