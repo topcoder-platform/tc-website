2e4923f146e113e9d618da1f06637ba1eb0c6080
scampion <scampion@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-12-12 20:48:09 +0000

    Merged BUGR-722 with trunk.

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/contest/ActiveContests.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/contest/ActiveContests.java
@@ -3,6 +3,8 @@
  */
 package com.topcoder.web.tc.controller.request.contest;
 
+import com.topcoder.util.config.ConfigManager;
+import com.topcoder.util.config.UnknownNamespaceException;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.tc.Constants;
 
@@ -15,30 +17,65 @@ import com.topcoder.web.tc.Constants;
  * Create Date: Apr 16, 2008
  */
 public class ActiveContests extends ActiveContestsBase {
+    public static final int[] DEFAULT_PROJECT_TYPES = new int[] {Constants.ASSEMBLY_PROJECT_TYPE,
+        Constants.ARCHITECTURE_PROJECT_TYPE, Constants.APPLICATION_TESTING_PROJECT_TYPE,
+        Constants.SPECIFICATION_PROJECT_TYPE, Constants.CONCEPTUALIZATION_PROJECT_TYPE};
+
+    public static final String DEFAULT_NAMESPACE = "com.topcoder.web.tc.controller.request.contest.ActiveContests";
+
     protected void developmentProcessing() throws TCWebException {
         int projectTypeId =  com.topcoder.web.tc.controller.request.development.Base.getProjectTypeId(getRequest());
 
-        if (Constants.ASSEMBLY_PROJECT_TYPE==projectTypeId ||
-                Constants.ARCHITECTURE_PROJECT_TYPE==projectTypeId ||
-                Constants.APPLICATION_TESTING_PROJECT_TYPE==projectTypeId) {
+        if (isValidProjectType(projectTypeId)) {
             setProjectType(projectTypeId);
         } else {
             throw new TCWebException("Invalid project type specified " + projectTypeId);
         }
-        
+
         super.developmentProcessing();
     }
-    
+
     protected String getCommandName() {
         if (getProjectType()==Constants.ARCHITECTURE_PROJECT_TYPE) {
-            return "active_architecture_contests";            
+            return "active_architecture_contests";
         } else {
             return super.getCommandName();
         }
     }
-    
+
     @Override
     protected Boolean hasDR() {
         return Boolean.TRUE;
+    }
+
+    private static boolean isValidProjectType(int projectType) {
+        ConfigManager configManager = ConfigManager.getInstance();
+
+        String[] projectTypes = null;
+        if (configManager.existsNamespace(DEFAULT_NAMESPACE)) {
+            try {
+                projectTypes = configManager.getStringArray(DEFAULT_NAMESPACE, "ValidProjectTypes");
+            } catch (UnknownNamespaceException e) {
+                e.printStackTrace();
+                projectTypes = null;
+            }
+        }
+
+        if (projectTypes == null) {
+            for (int i = 0; i < DEFAULT_PROJECT_TYPES.length; i++) {
+                if (projectType == DEFAULT_PROJECT_TYPES[i]) {
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; i < projectTypes.length; i++) {
+                String strValue = projectType + "";
+                if (strValue.equals(projectTypes[i])) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
