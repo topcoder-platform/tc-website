5f6b1830407f82193e5fc893340a56796408d289
elkhawajah <elkhawajah@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2009-03-01 11:19:11 +0000

    submission

---
+++ web_module/branches/tcs_release_assembly_2.2.2_elkhawajah/docs/tc/Release_2.2.2/test_files/tc/1-setup-review-board.sql
@@ -0,0 +1,72 @@
+--
+-- Sets up the review board for the three competitions types
+-- (Conceptualization, Specification, Testing).
+-- (handle - status):
+--
+-- heffan - Active
+-- super  - Active
+-- user  - Active
+-- Hung - Active
+-- Partha - Invited
+-- twight - Applied
+-- sandking - Rejected
+-- lightspeed - Not Interested
+-- reassembler - Contact Again
+-- annej9ny - Suspended
+-- plinehan - Resigned
+-- chelseasimon - Kicked Out
+-- wyzmo - Currently Inactive
+-- cartajs - Temporarily Deactivated (No Longer Meets Minimum Requirements)
+-- Yoshi - Permanently Deactivated (No Longer Meets Minimum Requirements)
+-- ksmith - Permanently Deactivated (No Longer Meets Minimum Requirements), Immune
+--
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (132456, 23, 1, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (132457, 23, 2, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (132458, 23, 1, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124764, 23, 3, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124772, 23, 1, 10, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124766, 23, 1, 20, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124776, 23, 1, 30, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124834, 23, 1, 40, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124835, 23, 1, 50, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124836, 23, 1, 60, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124852, 23, 1, 70, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124853, 23, 1, 80, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124856, 23, 1, 90, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124857, 23, 1, 110, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124916, 23, 1, 120, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124861, 23, 1, 120, 1);
+
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (132456, 6, 1, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (132457, 6, 2, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (132458, 6, 1, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124764, 6, 3, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124772, 6, 1, 10, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124766, 6, 1, 20, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124776, 6, 1, 30, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124834, 6, 1, 40, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124835, 6, 1, 50, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124836, 6, 1, 60, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124852, 6, 1, 70, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124853, 6, 1, 80, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124856, 6, 1, 90, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124857, 6, 1, 110, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124916, 6, 1, 120, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124861, 6, 1, 120, 1);
+
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (132456, 13, 1, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (132457, 13, 2, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (132458, 13, 1, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124764, 13, 3, 100, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124772, 13, 1, 10, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124766, 13, 1, 20, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124776, 13, 1, 30, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124834, 13, 1, 40, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124835, 13, 1, 50, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124836, 13, 1, 60, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124852, 13, 1, 70, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124853, 13, 1, 80, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124856, 13, 1, 90, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124857, 13, 1, 110, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124916, 13, 1, 120, 0);
+INSERT INTO rboard_user (user_id, project_type_id, catalog_id, status_id, immune_ind) VALUES (124861, 13, 1, 120, 1);
