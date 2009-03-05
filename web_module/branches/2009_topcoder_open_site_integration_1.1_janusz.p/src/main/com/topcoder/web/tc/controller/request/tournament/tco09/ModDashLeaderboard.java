ab3371357cf3998628a9e36a6d3985dd4f4f402c
janusz.p <janusz.p@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2009-03-05 23:03:25 +0000

    abc

--- web_module/branches/2009_topcoder_open_site_integration_1.1_janusz.p/src/main/com/topcoder/web/tc/controller/request/tournament/tco09/ModDashLeaderboard.java
+++ web_module/branches/2009_topcoder_open_site_integration_1.1_janusz.p/src/main/com/topcoder/web/tc/controller/request/tournament/tco09/ModDashLeaderboard.java
@@ -1,31 +1,53 @@
+/**
+ * Copyright (C) 2004 - 2009 TopCoder Inc., All Rights Reserved.  
+ */
 package com.topcoder.web.tc.controller.request.tournament.tco09;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 import com.topcoder.json.object.JSONArray;
 import com.topcoder.json.object.JSONObject;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.tc.controller.request.tournament.ModDashStatBase;
 
+/** 
+ * Responsibility of this class is to process Mod Dash leaderboard feed data 
+ * and pass them to appropriate view. Most of the work is done by super class
+ * which is provided with feed URL and view file path. 
+ * 
+ * Main task that is done by this class is regarded to processing retrieved 
+ * feed data. For each user from leaderboard its user_id is retrieved from db.
+ * This is done in order to allow pretty rendering of user handle.  
+ * 
+ * Author TCSDEVELOPER
+ * Version 1.0
+ * Since 2009 TopCoder Open Site Integration 1.1
+ */
 public class ModDashLeaderboard extends ModDashStatBase {
-    
-    //private static String CONTEST_PREFIX = "tco09";
-
+   
+    /**
+     * Returns url that should be used for feed data retrieval.
+     * @return feed URL
+     */
     @Override
     protected String getFeedURL() {
         return "http://66.37.210.86/tc?module=BasicData&c=dd_mod_dash_tco_leaderboard&dsid=34&json=true";
     }
 
+    /**
+     * Returns view which should be used.
+     * @return path to JSP file defining view
+     */
     @Override
     protected String getPageName() {
         return "/tournaments/tco09/moddash/advancers/leaderboard.jsp";
     }
         
+    /**
+     * This method is responsible for retrieval of userIDs for each user 
+     * that competed in Mod Dash. 
+     */
     @Override
     protected void statProcessing() throws TCWebException {
+        // get feedData (it will be retrieved by abstract class)
         JSONArray feedData = (JSONArray) getRequest().getAttribute(WEB_REQUEST_DATA_PARAMETER_NAME);
         for (int i=0; i < feedData.getSize(); i++) {
             // process feed row
@@ -33,7 +55,6 @@ public class ModDashLeaderboard extends ModDashStatBase {
             String handle = row.getString(USER_HANDLE_KEY);
             row.setInt(USER_ID_PROPERTY_NAME, getUserIdForHandle(handle));
         }
-        getRequest().setAttribute(WEB_REQUEST_DATA_PARAMETER_NAME, feedData);
     }
 
 }
