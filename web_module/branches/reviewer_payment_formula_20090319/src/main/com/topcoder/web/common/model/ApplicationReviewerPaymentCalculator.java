b0376a54c4b54413f503184dfc9fa964fe040c12
ivern <ivern@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2009-03-22 20:32:38 +0000

    Documentation and minor code reorganization.

--- web_module/branches/reviewer_payment_formula_20090319/src/main/com/topcoder/web/common/model/ApplicationReviewerPaymentCalculator.java
+++ web_module/branches/reviewer_payment_formula_20090319/src/main/com/topcoder/web/common/model/ApplicationReviewerPaymentCalculator.java
@@ -4,40 +4,64 @@
 package com.topcoder.web.common.model;
 
 public class ApplicationReviewerPaymentCalculator extends BaseReviewerPaymentCalculator {
+	private static final float STANDARD_PRIZE = 750f;
+	private static final float STANDARD_SUBMISSIONS = 3f;
+	
     public ApplicationReviewerPaymentCalculator(float firstPlacePrize, int submissionCount, int passedScreeningCount) {
         super(firstPlacePrize, submissionCount, passedScreeningCount);
     }
 
-    // TODO: Switch everything to BigDecimal?
-    // TODO: Get rid of magic numbers.
     public float getScreeningCost() {
-        return getPrimaryExtraCost() - getAggregationCost() - getFinalReviewCost();
+        return getActualBaseRate() * 0.5f * getSubmissionCount();
     }
 
     public float getReviewCost() {
-        return (Math.max(0f, 4f - getPassedScreeningCount()) * 1.5f + 2f * getPassedScreeningCount()) * getActualBaseRate();
+        return (Math.max(0f, STANDARD_SUBMISSIONS + 1 - getPassedScreeningCount()) * 1.5f + 2f * getPassedScreeningCount())
+        		* getActualBaseRate();
     }
 
     public float getAggregationCost() {
-        return 10f;
+        return 2f * getActualBaseRate() * 0.25f;
     }
 
     public float getFinalReviewCost() {
-        return 30f;
+        return 2f * getActualBaseRate() * 0.75f;
     }
     
+    /**
+     * Returns the calculated base hourly rate for the reviewers, scaled by the first place prize.
+     * 
+     * @return
+     */
     protected float getCalculatedBaseRate() {
-        return 15f + (getFirstPlacePrize() - 750) * 0.01f;
+        return 15f + (getFirstPlacePrize() - STANDARD_PRIZE) * 0.01f;
     }
 
+    /**
+     * Returns the calculated review cost for this project, assuming it has three submissions and they all pass
+     * screening.
+     * 
+     * @return
+     */
     protected float getCalculatedReviewCost() {
         return 26f * getCalculatedBaseRate();
     }
 
+    /**
+     * Returns the actual base hourly rate for the reviewers, which right now is the same as the calculated base rate.
+     * This is in place should an override be added later.
+     * 
+     * @return
+     */
     protected float getActualBaseRate() {
-        return getCalculatedBaseRate(); // No override or cap.
+        return getCalculatedBaseRate();
     }
 
+    /**
+     * Returns the additional payment due to the primary reviewer.
+     * 
+     * @return
+     */
     protected float getPrimaryExtraCost() {
         return 2f * getActualBaseRate() + getActualBaseRate() * 0.5f * getSubmissionCount();
     }
