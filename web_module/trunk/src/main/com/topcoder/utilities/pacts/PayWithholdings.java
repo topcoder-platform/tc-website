ed7610460d55440b5451a00729445462e3827592
ivern <ivern@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2009-04-28 16:38:15 +0000

    Several small fixes.

--- web_module/trunk/src/main/com/topcoder/utilities/pacts/PayWithholdings.java
+++ web_module/trunk/src/main/com/topcoder/utilities/pacts/PayWithholdings.java
@@ -58,23 +58,28 @@ public class PayWithholdings extends DBUtility {
         // Get partial payments to release withholdings.
         // If releasePeriodDays were reached since project completion, make the payment.
         query.append("SELECT user_id, client, pd.payment_type_id, pd.payment_method_id ");
-        query.append(" , pd.component_project_id, to_date(pi.value, '%m.%d.%Y %H:%M %p') as project_completion_date ");
+        query.append(" , pd.component_project_id ");
+        query.append(" , (select max(pp.actual_end_time) from tcs_catalog:project_phase pp "); 
+        query.append("         where pp.project_id = pd.component_project_id ");
+        query.append("         and pp.phase_status_id = 3) as project_completion_date "); 
         query.append(" , sum(gross_amount) as amount_paid ");
         query.append(" , max(total_amount) as total_amount ");
         query.append(" , max(installment_number) as installment_number ");
-        query.append(" FROM payment p, payment_detail pd, tcs_catalog:project_info pi ");
+        query.append(" FROM payment p, payment_detail pd ");
         query.append(" WHERE p.most_recent_detail_id = pd.payment_detail_id ");
-        query.append(" AND gross_amount <> total_amount ");
-        query.append(" AND pi.project_info_type_id = 21 ");
-        query.append(" AND pi.project_id = pd.component_project_id ");
+        query.append(" AND gross_amount < total_amount ");
         query.append(" AND pd.payment_type_id in (");
         query.append(PactsConstants.ARCHITECTURE_PAYMENT).append(", ");
         query.append(PactsConstants.ASSEMBLY_PAYMENT).append(", ");
         query.append(PactsConstants.REVIEW_BOARD_PAYMENT).append(", ");
         query.append(PactsConstants.COMPONENT_PAYMENT).append(") ");
-        query.append(" AND to_date(pi.value, '%m.%d.%Y %H:%M %p') + ").append(releasePeriodDays);
+        query.append(" AND (select max(pp.actual_end_time) from tcs_catalog:project_phase pp "); 
+        query.append("         where pp.project_id = pd.component_project_id ");
+        query.append("         and pp.phase_status_id = 3) + ").append(releasePeriodDays); 
         query.append(" UNITS DAY < current ");
-        query.append(" AND to_date(pi.value, '%m.%d.%Y %H:%M %p') >= " + START_DATE);
+        query.append(" AND (select max(pp.actual_end_time) from tcs_catalog:project_phase pp "); 
+        query.append("         where pp.project_id = pd.component_project_id ");
+        query.append("         and pp.phase_status_id = 3) >= " + START_DATE);
         query.append(" GROUP BY 1, 2, 3, 4, 5, 6 ");
         query.append(" HAVING sum(gross_amount) < max(total_amount) ");
 
