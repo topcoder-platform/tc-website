4a63f8bbcb4382615b7dab7fc62ca4c67437b391
vkachurovskyi <vkachurovskyi@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2009-07-28 13:47:15 +0000

    branch

---
+++ web_module/branches/appeals_early_completion_release_assembly_1.0_BeBetter/src/main/com/topcoder/web/tc/controller/request/development/ViewRegistrants.java
@@ -0,0 +1,46 @@
+package com.topcoder.web.tc.controller.request.development;
+
+import com.topcoder.web.common.StringUtils;
+import com.topcoder.web.common.TCWebException;
+import com.topcoder.web.tc.Constants;
+import com.topcoder.shared.dataAccess.Request;
+
+/**
+ * Added some code to permit other projects different from component competitions.
+ * 
+ * @author dok, pulky
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Jan 4, 2006
+ */
+public class ViewRegistrants extends Base {
+    protected void developmentProcessing() throws TCWebException {
+        try {
+            String projectId = StringUtils.checkNull(getRequest().getParameter(Constants.PROJECT_ID));
+            
+            if (projectId == "") {
+                throw new TCWebException("parameter " + Constants.PROJECT_ID + " expected.");
+            }
+            
+            int projectTypeId = getProjectTypeId(Long.parseLong(projectId)); 
+            
+            if (projectTypeId == -1) {
+                throw new TCWebException("Could not find project information.");
+            }
+
+            getRequest().setAttribute(Constants.PROJECT_TYPE_ID, new Integer(projectTypeId));
+
+            Request r = new Request();
+            r.setContentHandle(getRegistrantsCommandName(projectTypeId));
+            r.setProperty(Constants.PROJECT_ID, String.valueOf(projectId));
+            getRequest().setAttribute("resultMap", getDataAccess().getData(r));
+            setNextPage("/contest/registrants.jsp");
+            setIsNextPageInContext(true);
+        } catch (TCWebException e) {
+            throw e;
+          } catch (NumberFormatException nfe) {
+            throw new TCWebException("Wrong parameter " + Constants.PROJECT_ID + ".");            
+        } catch (Exception e) {
+            throw new TCWebException(e);
+        }
+    }
+}
