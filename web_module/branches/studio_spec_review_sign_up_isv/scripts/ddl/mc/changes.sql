27ccba8b72d55db720d6a09ee5801c542083d707
isv <isv@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2009-08-27 09:48:17 +0000

    Testing write permission for SVN branch

--- web_module/branches/studio_spec_review_sign_up_isv/scripts/ddl/mc/changes.sql
+++ web_module/branches/studio_spec_review_sign_up_isv/scripts/ddl/mc/changes.sql
@@ -2,6 +2,6 @@ insert into preference_lu (preference_id, preference_name, preference_desc, pref
 values(24, 'contact member enabled', 'Do you want to be contacted by other members?', 4, 1, 1);
 
 insert into user_preference(user_id, preference_id, value)
-select user_id, 24, 'false' from user;
+select user_id, 24, 'false' from user; 
 
 insert into id_sequences values ('MEMBER_CONTACT_MESSAGE_SEQ', 1,10,0);
