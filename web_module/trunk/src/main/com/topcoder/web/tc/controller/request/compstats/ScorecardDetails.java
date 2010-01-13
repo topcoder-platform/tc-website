288ec2366d932584ac1c153c3a187c6c62522d43
sma <sma@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2010-01-13 22:51:25 +0000

    bugr 3062

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/request/compstats/ScorecardDetails.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/request/compstats/ScorecardDetails.java
@@ -1,31 +1,51 @@
 /*
- * CompList.java
- *
- * Created on August 12, 2005 by Cucu
- *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ * Copyright (C) 2004 - 2010 TopCoder Inc., All Rights Reserved.
  */
-
 package com.topcoder.web.tc.controller.request.compstats;
+
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Map;
+
+import javax.ejb.CreateException;
+import javax.ejb.EJBException;
+import javax.naming.NamingException;
 
 import com.topcoder.shared.dataAccess.DataAccessInt;
 import com.topcoder.shared.dataAccess.Request;
 import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer;
+import com.topcoder.shared.dataAccess.resultSet.ResultSetContainer.ResultSetRow;
+import com.topcoder.shared.util.DBMS;
 import com.topcoder.web.common.BaseServlet;
 import com.topcoder.web.common.NavigationException;
 import com.topcoder.web.common.SessionInfo;
 import com.topcoder.web.common.StringUtils;
 import com.topcoder.web.common.TCWebException;
 import com.topcoder.web.common.WebConstants;
-import com.topcoder.web.tc.Constants;
 import com.topcoder.web.common.eligibility.ContestEligibilityServiceLocator;
-
-import java.util.Map;
+import com.topcoder.web.ejb.project.ProjectRoleTermsOfUse;
+import com.topcoder.web.ejb.project.ProjectRoleTermsOfUseLocator;
+import com.topcoder.web.ejb.user.UserTermsOfUse;
+import com.topcoder.web.ejb.user.UserTermsOfUseLocator;
+import com.topcoder.web.tc.Constants;
 
 /**
- * @author cucu
+ * <p><strong>Purpose</strong>: This processor handle registration to a specific project.</p>
+ *
+ * <p>
+ *   Version 1.1 (BUGR-3062) Change notes:
+ *   <ol>
+ *     <li>Added terms of use validation with the following logic:
+ *         to determine if a user u has the required terms for a project p's scorecards: 
+ *         for r in possible roles in a project (not just submitter or reviewer, any role):
+ *            if u has all of the terms listed for role r in project p, return true
+ *            return false.
+ *     </li>
+ *   </ol>
+ * </p>
+ *
+ * @author cucu, pulky
+ * @version 1.1
  */
 public class ScorecardDetails extends Base {
     protected void businessProcessing() throws TCWebException {
@@ -123,7 +143,12 @@ public class ScorecardDetails extends Base {
         if (reviewerId != null && reviewerId.equals("" + userId)) {
             return true;
         }
-            
+
+        // check if the user has necessary terms agreed
+        if (!hasRequiredTerms(pid, userId)) {
+            return false;
+        }
+
         Request r = new Request();
         r.setContentHandle("comp_contest_details");
 
@@ -177,10 +202,93 @@ public class ScorecardDetails extends Base {
             }
         }
         
-
         return false;        
         
     }
 
-}
+    /**
+     * Helper method to check if a user agreed do all terms in any associated project role 
+     * 
+     * @param projectId the projectId to look for
+     * @param userId the userId to look for
+     * @return true if the user agreed do all terms in any associated project role and false otherwise
+     * @throws Exception if any error occurs
+     * 
+     * @since 1.1
+     */
+    private boolean hasRequiredTerms(long projectId, long userId) throws Exception {
+        // check if the user agreed to all terms of use
+        ProjectRoleTermsOfUse projectRoleTermsOfUse = ProjectRoleTermsOfUseLocator.getService();
+        UserTermsOfUse userTermsOfUse = UserTermsOfUseLocator.getService();
 
+        ResultSetContainer roleIds = getPossibleRolesIds(projectId);
+        for (ResultSetRow roleIdRow : roleIds) {
+            if (checkTermsForRole(projectRoleTermsOfUse, userTermsOfUse, 
+                new int[] {roleIdRow.getIntItem("resource_role_id")}, projectId, userId)) { 
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    /**
+     * This method retrieves matching role ids for a specific project
+     * 
+     * @param projectId the projectId to look for
+     *
+     * @return a ResultSetContainer with matching role ids for a specific project
+     * 
+     * @throws Exception if any error occurs
+     * 
+     * @since 1.1
+     */
+    private ResultSetContainer getPossibleRolesIds(long projectId) throws Exception {
+        Request r = new Request();
+        r.setContentHandle("project_role_ids");
+
+        // Find all the projects that match with the component id, version and phase
+        r.setProperty(Constants.PROJECT_ID, String.valueOf(projectId));
+
+        DataAccessInt dai = getDataAccess(DBMS.TCS_OLTP_DATASOURCE_NAME, false);
+        Map<String, ResultSetContainer> result = dai.getData(r);
+        return (ResultSetContainer) result.get("project_role_ids");
+    }
+
+    /**
+     * Helper method to check if a user agreed to all terms associated to a particular role
+     * 
+     * @param projectRoleTermsOfUse the ProjectRoleTermsOfUse used to call service
+     * @param userTermsOfUse the UserTermsOfUse used to call service
+     * @param roleIds the roleId to look for
+     * @param projectId the projectId to look for
+     * @param userId the userId to look for
+     * @return true if the user agreed to all terms for the role or false otherwise
+     * 
+     * @throws EJBException if any error occurs during EJB call
+     * @throws RemoteException if any error occurs during remote EJB call
+     * 
+     * @since 1.1
+     */
+    private boolean checkTermsForRole(ProjectRoleTermsOfUse projectRoleTermsOfUse, UserTermsOfUse userTermsOfUse, 
+            int[] roleIds, long projectId, long userId) throws EJBException, RemoteException {
+
+        // iterate through all terms for this role
+        List<Long>[] necessaryTerms = projectRoleTermsOfUse.getTermsOfUse((int) projectId,
+                roleIds, DBMS.COMMON_OLTP_DATASOURCE_NAME);
+
+        for (int i = 0; i < necessaryTerms.length; i++) {
+            if (necessaryTerms[i] != null) {
+                for (int j = 0; j < necessaryTerms[i].size(); j++) {
+                    Long termsId = necessaryTerms[i].get(j);
+        
+                    // check if the user has this terms
+                    if (!userTermsOfUse.hasTermsOfUse(userId, termsId, DBMS.COMMON_OLTP_DATASOURCE_NAME)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
