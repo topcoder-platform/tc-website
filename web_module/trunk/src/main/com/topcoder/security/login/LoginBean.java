6d9abef408d13b5346e3b04107d69b4dce8501b2
ivern <ivern@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2010-03-31 15:06:12 +0000

    Committing the LDAP branch...again

--- web_module/trunk/src/main/com/topcoder/security/login/LoginBean.java
+++ web_module/trunk/src/main/com/topcoder/security/login/LoginBean.java
@@ -1,6 +1,16 @@
+/*
+ * Copyright (C) 2002-2010 TopCoder Inc., All Rights Reserved.
+ */
 package com.topcoder.security.login;
 
-import com.topcoder.security.*;
+import com.topcoder.security.BaseEJB;
+import com.topcoder.security.GeneralSecurityException;
+import com.topcoder.security.RolePrincipal;
+import com.topcoder.security.SecurityDB;
+import com.topcoder.security.TCSubject;
+import com.topcoder.security.Util;
+import com.topcoder.security.ldap.LDAPClient;
+import com.topcoder.security.ldap.LDAPClientException;
 import org.apache.log4j.Logger;
 
 import javax.naming.InitialContext;
@@ -11,93 +21,99 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Use for logging users into a system.
+ * <p>A stateless EJB to be used for authenticating users to application.</p>
  *
- * @author Heather Van Aelst
- * @version 0.1
+ * <p>Version 2.0 (LDAP Authentication Release Assembly v1.0) change notes:
+ *   <ul>
+ *     <li>Re-written the whole logic for user authentication to authenticate users against <code>LDAP</code> server
+ *     instead of database.</li>
+ *   </ul>
+ * </p>
+ *
+ * @author Heather Van Aelst, isv
+ * @version 2.0
  */
 public class LoginBean extends BaseEJB {
 
+    /**
+     * <p>A <code>Logger</code> to be used for logging the events encountered during user authentication.</p>
+     */
     private static final Logger logger = Logger.getLogger(LoginBean.class);
+
+    /**
+     * <p>A <code>String</code> providing the default JNDI name for the data source to be used for establishing
+     * connection to target database.</p>
+     */
     private static final String DATA_SOURCE = "java:comp/env/jdbc/DefaultDS";
 
+    /**
+     * <p>Constructs new <code>LoginBean</code> instance. This implementation does nothing.</p>
+     *
+     * @since 2.0
+     */
+    public LoginBean() {
+    }
 
-    public TCSubject login(String username, String password)
-            throws AuthenticationException, GeneralSecurityException {
+    /**
+     * <p>Logs the user to application using specified credentials for authentication.</p>
+     *
+     * @param username a <code>String</code> providing the username for user authentication.
+     * @param password a <code>String</code> providing the plain text password for user authentication.
+     * @return a <code>TCSubject</code> providing the identity for the user successfully authenticated to application.
+     * @throws AuthenticationException if provided credentials are invalid.
+     * @throws GeneralSecurityException if an unexpected error occurs while authenticating user to application.
+     */
+    public TCSubject login(String username, String password) throws GeneralSecurityException {
         return login(username, password, DATA_SOURCE);
     }
 
-
     /**
-     * Logs users into a system.  Checks username and password, returns a
-     * TCSubject representation of the user that includes the user's roles.
-     * Encrypts password, compares to stored, encrypted password.
+     * <p>Logs the user to application using the specified credentials. Checks username and password, returns a
+     * TCSubject representation of the user that includes the user's roles.</p>
      *
-     * @param username
-     * @param password Plain text password
-     * @return TCSubject
-     * @throws AuthenticationException Thrown when username/password
-     *                                 combination does not exist in the db.
-     * @throws GeneralSecurityException Thrown when SLQExcpetion occurs.
+     * @param username a <code>String</code> providing the username for user authentication.
+     * @param password a <code>String</code> providing the plain text password for user authentication.
+     * @param dataSource a <code>String</code> referencing the data source to be used for establishing connection to
+     *        target database.
+     * @return a <code>TCSubject</code> providing the identity for the user successfully authenticated to application.
+     * @throws AuthenticationException if provided credentials are invalid.
+     * @throws GeneralSecurityException if an unexpected error occurs while authenticating user to application.
      */
-    public TCSubject login(String username, String password, String dataSource)
-            throws AuthenticationException, GeneralSecurityException {
+    public TCSubject login(String username, String password, String dataSource) throws GeneralSecurityException {
 
         logger.debug("LoginBean.login: " + username);
 
         checkLength(username, SecurityDB.maxUsernameLength);
         checkLength(password, SecurityDB.maxPasswordLength);
 
-        long userId;
-        Set pl = new HashSet();
+        // Authenticate user against LDAP server and map user to user ID
+        long userId = loginToLDAPDirectory(username, password);
+        logger.debug("Logging in login_id: " + userId);
 
-        //encode password
-        String encPassword = Util.encodePassword(password, "users");
-
-        String query =
-                "SELECT login_id FROM security_user " +
-                "WHERE user_id = ? AND password = ?";
+        // Collect user's roles from database
+        Set userRoles = new HashSet();
 
         InitialContext ctx = null;
-        ResultSet rs1 = null;
         ResultSet rs2 = null;
-        PreparedStatement ps1 = null;
         PreparedStatement ps2 = null;
         Connection conn = null;
         try {
             ctx = new InitialContext();
             conn = Util.getConnection(ctx, dataSource);
-            ps1 = conn.prepareStatement(query);
-            ps1.setString(1, username);
-            ps1.setString(2, encPassword);
-            rs1 = ps1.executeQuery();
 
-            //if this returns a result there was an exact username and password match
-            //ResultSet rs1 = Util.getResults(conn, query);
-            logger.debug("LoginBean.login got ResultSet");
-
-            if (rs1.next()) { //was a match
-                userId = rs1.getLong(1);
-                logger.debug("Logging in login_id: " + userId);
-            } else { //username and password didn't match or weren't in db
-                throw new AuthenticationException("Username and/or password are incorrect");
-            }
-            //get user's id and roles
-
-            query =
-                    "SELECT security_roles.role_id, description " +
-                    "FROM user_role_xref, security_roles " +
-                    "WHERE user_role_xref.login_id = ? " +
-                    "AND user_role_xref.role_id = security_roles.role_id" +
-                    " AND user_role_xref.security_status_id = ?"+
-                    " UNION " +
-                    "SELECT security_roles.role_id, description " +
-                    "FROM security_roles, user_group_xref, group_role_xref " +
-                    "WHERE user_group_xref.login_id = ? " +
-                    "AND user_group_xref.group_id = group_role_xref.group_id " +
-                    " and user_group_xref.security_status_id = ? " + 
-                    " and group_role_xref.security_status_id = ? " +
-                    "AND group_role_xref.role_id = security_roles.role_id";
+            String query = "SELECT security_roles.role_id, description " +
+                           "FROM   user_role_xref, security_roles " +
+                           "WHERE  user_role_xref.login_id = ? " +
+                           "AND    user_role_xref.role_id = security_roles.role_id " +
+                           "AND    user_role_xref.security_status_id = ? "+
+                           "UNION " +
+                           "SELECT security_roles.role_id, description " +
+                           "FROM   security_roles, user_group_xref, group_role_xref " +
+                           "WHERE  user_group_xref.login_id = ? " +
+                           "AND    user_group_xref.group_id = group_role_xref.group_id " +
+                           "AND    user_group_xref.security_status_id = ? " +
+                           "AND    group_role_xref.security_status_id = ? " +
+                           "AND    group_role_xref.role_id = security_roles.role_id";
             ps2 = conn.prepareStatement(query);
             ps2.setLong(1, userId);
             ps2.setInt(2, SecurityDB.STATUS_ACTIVE);
@@ -106,27 +122,47 @@ public class LoginBean extends BaseEJB {
             ps2.setInt(5, SecurityDB.STATUS_ACTIVE);
             rs2 = ps2.executeQuery();
 
-            //build list of RolePrincipals
-            RolePrincipal rp = null;
+            // Build list of RolePrincipals associated with user
+            RolePrincipal userRole = null;
             while (rs2.next()) {
-                //logger.debug(rs2.getRow() + " adding principal: " + rs2.getLong(1));
-                rp = new RolePrincipal(rs2.getString(2), rs2.getLong(1));
-                pl.add(rp);
+                userRole = new RolePrincipal(rs2.getString(2), rs2.getLong(1));
+                userRoles.add(userRole);
             }
-        } catch (GeneralSecurityException e) {
-            throw e;
         } catch (Exception e) {
             throw new GeneralSecurityException(e);
         } finally {
-            close(rs1);
             close(rs2);
-            close(ps1);
             close(ps2);
             close(conn);
             close(ctx);
         }
-        //create TCSubject
-        TCSubject sub = new TCSubject(pl, userId);
-        return sub;
+
+        return new TCSubject(userRoles, userId);
+    }
+
+    /**
+     * <p>Authenticates user against <code>LDAP</code> directory based on provided credentials. Verifies that there is
+     * an <code>LDAP</code> entry with DN matching the specified username and that it has associated password equal to
+     * specified one.</p>
+     *
+     * @param username a <code>String</code> providing the username for user authentication.
+     * @param password a <code>String</code> providing the encrypted text password for user authentication.
+     * @return a <code>long</code> providing the ID for the user authenticated based on provided credentials.
+     * @throws AuthenticationException if provided credentials are invalid or user account is not active.
+     * @throws GeneralSecurityException if an unexpected error occurs while authenticating user to application.
+     * @since 2.0
+     */
+    private long loginToLDAPDirectory(String username, String password) throws GeneralSecurityException {
+        try {
+            return LDAPClient.authenticateTopCoderMember(username, password);
+        } catch (LDAPClientException e) {
+            if (e.isUserStatusNotActive()) {
+                throw new AuthenticationException("User account is not active");
+            } else if (e.isInvalidCredentialProvided()) {
+                throw new AuthenticationException("Username and/or password are incorrect");
+            } else {
+                throw new GeneralSecurityException("Could not authenticate user due to unexpected error", e);
+            }
+        }
     }
 }
