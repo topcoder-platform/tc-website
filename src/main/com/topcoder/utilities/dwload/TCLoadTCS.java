eae32c268d34326ae1bf758c896f9b07f193c1c1
Volodymyr Kachurovskyi <Volodymyr-K@users.noreply.github.com>
2010-06-04 13:21:27 +0000

    DW load script is modified to load the spec review links

--- src/main/com/topcoder/utilities/dwload/TCLoadTCS.java
+++ src/main/com/topcoder/utilities/dwload/TCLoadTCS.java
@@ -74,7 +74,7 @@ import com.topcoder.utilities.dwload.contestresult.drv2.ContestResultCalculatorV
  *   </ol>
  * </p>
  *
- * @author rfairfax, pulky, ivern
+ * @author rfairfax, pulky, ivern, VolodymyrK
  * @version 1.1.6
  */
 public class TCLoadTCS extends TCLoad {
@@ -207,6 +207,8 @@ public class TCLoadTCS extends TCLoad {
             doLoadUserContestPrize();
 
             doLoadProjects();
+
+            doLoadSpecReviews();
 
             // load scorecard template before submission review because submission_review will use this table
             doLoadScorecardTemplate();
@@ -1148,6 +1150,7 @@ public class TCLoadTCS extends TCLoad {
         simpleDelete("submission", "project_id", projectId);
         simpleDelete("appeal", "project_id", projectId);
         simpleDelete("project_result", "project_id", projectId);
+        simpleDelete("project_spec_review_xref", "project_id", projectId);
         simpleDelete("project", "project_id", projectId);
     }
 
@@ -1165,6 +1168,81 @@ public class TCLoadTCS extends TCLoad {
         delete.setLong(1, id);
         long count = delete.executeUpdate();
         log.info("" + count + " records deleted in " + table + " table");
+    }
+
+     /**
+     * <p/>
+     * Load links between projects and spec reviews to the DW.
+     * IMPORTANT: This will deprecate with the new spec review process when spec reviews are just phases in the parent
+     * project.
+     * </p>
+     *
+     * @throws Exception if any error occurs
+     */
+    public void doLoadSpecReviews() throws Exception {
+        log.info("load spec reviews");
+        PreparedStatement select = null;
+        PreparedStatement selectDW = null;
+        PreparedStatement update = null;
+        PreparedStatement insert = null;
+        ResultSet rs = null;
+
+        try {
+            long start = System.currentTimeMillis();
+
+            //get data from source DB
+            final String SELECT = "select base_p.project_id as base_project_id, spec_review_p.project_id as spec_review_project_id " +
+                    "from project base_p, project spec_review_p, linked_project_xref lpx where " +
+                    "spec_review_p.project_category_id = 27 " +
+                    "and spec_review_p.project_id = lpx.dest_project_id " +
+                    "and lpx.link_type_id = 3 " +
+                    "and lpx.source_project_id = base_p.project_id ";
+
+            final String SELECT_DW = "select 1 from project where project_id = ? ";
+
+            final String UPDATE = "update project_spec_review_xref set spec_review_project_id = ? where project_id = ? ";
+
+            final String INSERT = "insert into project_spec_review_xref (project_id, spec_review_project_id) values (?, ?) ";
+
+            select = prepareStatement(SELECT, SOURCE_DB);
+            selectDW = prepareStatement(SELECT_DW, TARGET_DB);
+            update = prepareStatement(UPDATE, TARGET_DB);
+            insert = prepareStatement(INSERT, TARGET_DB);
+
+            rs = select.executeQuery();
+            int count = 0;
+            while (rs.next()) {
+                selectDW.setLong(1, rs.getLong("base_project_id"));
+                if (selectDW.executeQuery().next()==false) {
+                    continue;
+                }
+
+                update.setLong(1, rs.getLong("spec_review_project_id"));
+                update.setLong(2, rs.getLong("base_project_id"));
+                int retVal = update.executeUpdate();
+
+                if (retVal == 0) {
+                    //need to insert
+                    insert.setLong(1, rs.getLong("base_project_id"));
+                    insert.setLong(2, rs.getLong("spec_review_project_id"));
+                    insert.executeUpdate();
+                }
+                count++;
+            }
+            log.info("loaded " + count + " records in " + (System.currentTimeMillis() - start) / 1000 + " seconds");
+
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            DBMS.printSqlException(true, sqle);
+            throw new Exception("Load of 'project_spec_review_xref' table failed.\n" +
+                    sqle.getMessage());
+        } finally {
+            close(rs);
+            close(select);
+            close(selectDW);
+            close(insert);
+            close(update);
+        }
     }
 
     private static final DateFormat[] DATE_FORMATS = new DateFormat[]{
