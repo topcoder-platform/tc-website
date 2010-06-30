a63676300fb985ba1705a8db44376b260844672a
VolodymyrK <VolodymyrK@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2010-06-30 14:25:00 +0000

    Copilot made a payable role in OR.

--- web_module/trunk/src/main/com/topcoder/web/tc/controller/legacy/pacts/controller/request/internal/GenerateComponentPayments.java
+++ web_module/trunk/src/main/com/topcoder/web/tc/controller/legacy/pacts/controller/request/internal/GenerateComponentPayments.java
@@ -97,8 +97,8 @@ public class GenerateComponentPayments extends BaseProcessor implements PactsCon
                 DataInterfaceBean bean = new DataInterfaceBean();
                 // [BUGR-1452] - add support for paying other project types
                 // [BUGR-1842] - add support for UI/RIA project types
-                int[] counts = new int[13];
-                for (int i = 0; i < 13; i++) {
+                int[] counts = new int[14];
+                for (int i = 0; i < 14; i++) {
                     counts[i] =0;
                 }
                 log.debug("status type " + getRequest().getParameter(PROJECT_TERMINATION_STATUS));
@@ -148,6 +148,8 @@ public class GenerateComponentPayments extends BaseProcessor implements PactsCon
 
                     if (p.getPaymentType() == PactsConstants.TEST_SCENARIOS_PAYMENT) counts[12]++;
 
+                    if (p.getPaymentType() == PactsConstants.COPILOT_PAYMENT) counts[13]++;
+
                     ids.add(p.getId() + "");
 
                     List refer = bean.findPayments(CODER_REFERRAL_PAYMENT, p.getId());
@@ -194,11 +196,11 @@ public class GenerateComponentPayments extends BaseProcessor implements PactsCon
     private String generateSuccessMessage(int[] counts) {
         // TODO: find a better way to avoid duplicating these magic numbers, to ensure they're always in sync
         // [BUGR-1842] - add support for UI/RIA project types
-        final int[] countIndex = new int[] { 0, 4, 5, 6, 8, 7, 1, 3, 2, 9, 10, 11, 12 };
+        final int[] countIndex = new int[] { 0, 4, 5, 6, 8, 7, 1, 3, 2, 9, 10, 11, 12, 13 };
         final String[] countType = new String[] {
                 " design/development", " conceptualization", " specification", " architecture",
                 " assembly", "  test suites", " review board", " review board bonus", " referral",
-                " UI prototype", " RIA Build", " RIA Component", " test scenarios"
+                " UI prototype", " RIA Build", " RIA Component", " test scenarios", " copilot"
         };
 
         StringBuffer sb = new StringBuffer();
