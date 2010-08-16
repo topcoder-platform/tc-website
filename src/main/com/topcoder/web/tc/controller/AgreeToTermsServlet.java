ef2265c709d20ed2a73674b1cf87b7800ad83586
veredox <veredox@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2010-08-16 17:36:42 +0000

    added the agree to terms servlet to facilitate joining the paypalx community

---
+++ src/main/com/topcoder/web/tc/controller/AgreeToTermsServlet.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2010 TopCoder Inc., All Rights Reserved.
+ */
+package com.topcoder.web.tc.controller;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.topcoder.web.ejb.termsofuse.TermsOfUse;
+import com.topcoder.web.ejb.termsofuse.TermsOfUseEntity;
+import com.topcoder.web.ejb.termsofuse.TermsOfUseLocator;
+import com.topcoder.web.ejb.user.UserTermsOfUse;
+import com.topcoder.web.ejb.user.UserTermsOfUseLocator;
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.security.login.LoginLocal;
+import com.topcoder.security.TCSubject;
+import com.topcoder.web.common.security.Constants;
+import com.topcoder.security.login.AuthenticationException;
+
+/**
+ */
+public class AgreeToTermsServlet extends HttpServlet {
+
+    /**
+     * <p>
+     * Creates an instance of AgreeToTermsServlet.
+     * </p>
+     */
+    public AgreeToTermsServlet() {
+    }
+
+    /**
+     * <p>
+     * Initializes the servlet with use of the given servlet configuration.
+     * </p>
+     * @param servletConfig the servlet configuration object to be used
+     * @throws IllegalArgumentException if servletConfig is null
+     * @throws ServletException if some error occurred when initializing this
+     *             class using the given configuration
+     */
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
+    }
+
+    /**
+     * <p>
+     * Processes the POST request sent by the client to the server.
+     * </p>
+     * @throws IllegalArgumentException if any argument is null
+     * @param response the HttpServletResponse object that contains the
+     *            response the servlet sends to the client
+     * @param request the HttpServletRequest object that contains the
+     *            request the client has send to the servlet
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String handle = request.getParameter("handle");
+            String password = request.getParameter("password");
+            String terms = request.getParameter("terms");
+
+            TCSubject tcSubject = authenticate(handle, password);
+
+            if (tcSubject == null) {
+                response.getOutputStream().println("<response>bad login</response>");
+            } else {
+
+                long userId = tcSubject.getUserId();
+                long termsId = Long.parseLong(terms);
+
+                UserTermsOfUse userTermsOfUse = UserTermsOfUseLocator.getService();
+                if (userTermsOfUse.hasTermsOfUse(userId, termsId, DBMS.COMMON_OLTP_DATASOURCE_NAME)) {
+                    // already joined
+                    response.getOutputStream().println("<response>already agreed</response>");
+                } else {
+                    try {
+                        userTermsOfUse.createUserTermsOfUse(userId, termsId, DBMS.COMMON_OLTP_DATASOURCE_NAME);
+                        // success
+                        response.getOutputStream().println("<response>success</response>");
+                    } catch (Exception e) {
+                        response.getOutputStream().println("<response>bad terms</response>");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        doPost(request, response);
+    }
+
+    private TCSubject authenticate(final String userName, final String password) {
+        TCSubject ret = null;
+        try {
+            LoginLocal login = (LoginLocal) Constants.createLocalEJB(LoginLocal.class);
+            ret = login.login(userName, password);
+        } catch (AuthenticationException e){
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return ret;
+    }
+}
+
