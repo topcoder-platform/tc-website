544db580ae3ad56f71398e32183c2cf6bd7d9481
hohosky <hohosky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2010-11-07 08:58:47 +0000

    Add COPILOT_POSTING_PHASE to DefaultPriceComponent#getReviewerPaymentCalculator

--- web_module/trunk/src/main/com/topcoder/web/common/model/DefaultPriceComponent.java
+++ web_module/trunk/src/main/com/topcoder/web/common/model/DefaultPriceComponent.java
@@ -208,7 +208,7 @@ public class DefaultPriceComponent implements SoftwareComponent {
         } else if (phaseId == ARCHITECTURE_PHASE) {
             return new ArchitectureReviewerPaymentCalculator(prize, submissionCount, submissionsPassedScreening);
         } else if (phaseId == CONCEPTUALIZATION_PHASE || phaseId == SPECIFICATION_PHASE
-                   || phaseId == TEST_SUITES_PHASE || phaseId == TEST_SCENARIOS_PHASE) {
+                   || phaseId == TEST_SUITES_PHASE || phaseId == TEST_SCENARIOS_PHASE || phaseId == COPILOT_POSTING_PHASE) {
             return new ApplicationReviewerPaymentCalculator(prize, submissionCount, submissionsPassedScreening);
         } else if (phaseId == UI_PROTOTYPE_PHASE || phaseId == RIA_BUILD_PHASE ||
                 phaseId == RIA_COMPONENT_PHASE) {
