6f151483a9c47e6769fefc4d67f38bdd1ad9cb91
mashannon168 <mashannon168@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2011-03-07 18:24:55 +0000

    excluded status 9/10.

--- web_module/trunk/src/main/com/topcoder/utilities/dwload/AggregationDataLoader.java
+++ web_module/trunk/src/main/com/topcoder/utilities/dwload/AggregationDataLoader.java
@@ -95,7 +95,7 @@ public class AggregationDataLoader extends TCLoad {
         
         // Statement to select aggregation data with failed contests cnt
         final String AGGRE_FAIL = "SELECT a.client_project_id, a.tc_direct_project_id, a.project_category_id, b.week_of_year week, b.year, COUNT(a.status_id) fail_cnt " +
-                                "FROM project a, calendar b WHERE a.status_id IN (4,5,6,8,9,10,11) and a.client_project_id IS NOT NULL " +
+                                "FROM project a, calendar b WHERE a.status_id IN (4,5,6,8,11) and a.client_project_id IS NOT NULL " +
                                 "AND DAY(a.complete_date)=b.day_of_month AND MONTH(a.complete_date)=b.month_numeric AND YEAR(a.complete_date)=b.year " +
                                 "AND a.complete_date is not null AND a.posting_date is not null " +
                                 // dont include copilot posting, dont include content creation for now
@@ -208,7 +208,7 @@ public class AggregationDataLoader extends TCLoad {
         
         // Statement to select aggregation data with failed contests cnt
         final String AGGRE_FAIL = "SELECT a.client_project_id, a.tc_direct_project_id, a.project_category_id, b.month_numeric month, b.year, COUNT(a.status_id) fail_cnt " +
-                                "FROM project a, calendar b WHERE a.status_id IN (4,5,6,8,9,10,11) and a.client_project_id IS NOT NULL " +
+                                "FROM project a, calendar b WHERE a.status_id IN (4,5,6,8,11) and a.client_project_id IS NOT NULL " +
                                 "AND DAY(a.complete_date)=b.day_of_month AND MONTH(a.complete_date)=b.month_numeric AND YEAR(a.complete_date)=b.year " +
                                 "AND a.complete_date is not null AND a.posting_date is not null " +
                                 // dont include copilot posting, dont include content creation for now
