2e6feb09f01ab646e149c84c0cdca01c72e2ca26
lmmortal <lmmortal@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2011-04-01 08:29:15 +0000

    reviewers payments will be set to 0.0 when registering through studio.

--- web_module/branches/replatforming/src/main/com/topcoder/web/studio/Constants.java
+++ web_module/branches/replatforming/src/main/com/topcoder/web/studio/Constants.java
@@ -532,20 +532,6 @@ public class Constants implements WebConstants {
     public static final String PAGE_SIZE_KEY = "ps";
 
     /**
-     * <p>A <code>double</code> providing the payment amount for specification reviews.</p>
-     *
-     * @since 1.2
-     */
-    public static double SPEC_REVIEW_PAYMENT_AMOUNT;
-
-    /**
-     * <p>A <code>double</code> providing the payment amount for screening reviews.</p>
-     *
-     * @since 1.2
-     */
-    public static double SCREENING_REVIEW_PAYMENT_AMOUNT;
-
-    /**
      * <p>A <code>String</code> providing the name of default request attribute which refers to
      * specification review id.</p>
      *
