b456cdcdbfbaf2c68b2ef646394181cda3845129
VolodymyrK <VolodymyrK@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2011-10-24 10:00:32 +0000

    ExpireOldAffidavitsAndPayments utility has been rewritten as a standalone Java utility instead of hasingleton.

--- web_module/trunk/src/main/com/topcoder/utilities/pacts/ExpireOldAffidavitsAndPayments.java
+++ web_module/trunk/src/main/com/topcoder/utilities/pacts/ExpireOldAffidavitsAndPayments.java
@@ -1,130 +1,85 @@
+/*
+ * Copyright (C) 2004 - 2011 TopCoder Inc., All Rights Reserved.
+ */
 package com.topcoder.utilities.pacts;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+import javax.naming.InitialContext;
+import javax.rmi.PortableRemoteObject;
 
-import javax.naming.NameNotFoundException;
+import com.topcoder.shared.util.TCContext;
+import com.topcoder.shared.util.sql.DBUtility;
+import com.topcoder.web.ejb.pacts.PactsClientServices;
+import com.topcoder.web.ejb.pacts.PactsClientServicesHome;
+import com.topcoder.web.tc.controller.legacy.pacts.common.PactsConstants;
 
-import org.apache.log4j.Logger;
-import org.jboss.system.ServiceMBeanSupport;
+/**
+ * Utility class for expiring old payments, affidavits, assignment documents
+ * and canceling payments of banned members.
+ *
+ * @author VolodymyrK
+ * @version 1.0
+ */
+public class ExpireOldAffidavitsAndPayments extends DBUtility {
 
-import com.topcoder.web.tc.controller.legacy.pacts.bean.DataInterfaceBean;
-
-public class ExpireOldAffidavitsAndPayments extends ServiceMBeanSupport implements ExpireOldAffidavitsAndPaymentsMBean { 
-	
-    private static Logger logger = Logger.getLogger(ExpireOldAffidavitsAndPayments.class);
-    private static final String NAME = "ExpireOldAffidavits";
-    private Timer timer = null;
-    private static String status = "Not Initialized";
-    private String runningTime = null;
-    private boolean isMasterNode = false;
-
-    /* (non-Javadoc)
-     * @see org.jboss.system.ServiceMBean#getName()
+    /**
+     * This method will be executed after connections are created.
+     *
+     * @throws Exception if any error occurs.
      */
-    public String getName() {
-        return NAME;
-    }
+    protected void runUtility() throws Exception {
+        PactsClientServices  ejb = (PactsClientServices) createEJB();
 
-    public String getInitStatus() {
-        return status;
-    }
-
-    public String getRunningTime() {
-		return runningTime;
-	}
-
-	public void setRunningTime(String runningTime) {
-		this.runningTime = runningTime;
-	}
-    
-    
-    public void startService() throws Exception {
         try {
-        	Date start = getStartTime();
-        	
-            timer = new Timer();
-            timer.scheduleAtFixedRate(new ExpireTask(), start, 24 * 60 * 60 * 1000); // Run daily
-
-            logger.info("ExpireOldAffidavitsAndPayments will run daily at " + getRunningTime() + " starting at " + start);
-        } catch (Exception e) {
-            logger.error("ExpireOldAffidavitsAndPayments: Exception on init: " + e);
-            status = "Exception on init: " + e;
-            throw e;
-        }
-        status = "Initialized";
-    }
-    
-    private Date getStartTime() throws ParseException {
-    	SimpleDateFormat sdfDay = new SimpleDateFormat("yyyy.MM.dd");
-    	SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy.MM.dd h:mm a");
-    	
-    	String date = sdfDay.format(new Date()) + " " + getRunningTime();
-    	Date d = sdfDate.parse(date);
-    	
-    	// if this was early today, move it for tomorrow same time.
-    	if (d.before(new Date())) {
-    		d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
-    	}
-    	return d;
-    }
-
-    private class ExpireTask extends TimerTask {
-        public void run() {
-            if (!isMasterNode()) {
-                logger.info("This is not the master, process ExpireOldAffidavitsAndPayments skipped.");
-                return;
-            }
-            logger.info("ExpireOldAffidavitsAndPayments Fired");
-
-            try {
-            	DataInterfaceBean dib = new DataInterfaceBean();
-            	int n = dib.expireOldAffidavits();
-            	logger.info(n + " affidavits expired");
+            int n = ejb.expireOldAffidavits();
+            log.info(n + " affidavits expired");
             	
-                n = dib.expireOldPayments();
-                logger.info(n + " payments expired");
+            n = ejb.expireOldPayments();
+            log.info(n + " payments expired");
                 
-                n = dib.expireOldAssignmentDocuments();
-                logger.info(n + " assignment documents expired");
+            n = ejb.expireOldAssignmentDocuments();
+            log.info(n + " assignment documents expired");
                 
-                n = dib.checkInactiveCoders();
-                logger.info("Inactive coder were checked and related payments were notified");
+            n = ejb.checkInactiveCoders(0);
+            log.info(n + " inactive coders were checked and related payments were notified");
                 
             } catch (Exception e) {
-                if (!(e instanceof NameNotFoundException))
-                    logger.error(e.getMessage());
+                log.error(e.getMessage());
             }
-        }
     }
 
+    /**
+     * Private helper method to retrieve PACTS EJB.
+     *
+     * @return a <code>PactsClientServices</code> with PACTS EJB.
+     * @throws Exception if any error occurs
+     */
+    private static PactsClientServices createEJB() throws Exception {
+        InitialContext initial = TCContext.getInitial();
 
-    
-    public void startSingleton() {
-        isMasterNode = true;
-        logger.debug("StartSingleton");
-        try {
-            if (timer == null) {
-                startService();
-            }
-        } catch (Exception e) {
-            logger.error(e);
-        }
+        Object objref = initial.lookup(PactsClientServicesHome.class.getName());
+        PactsClientServicesHome home = (PactsClientServicesHome)
+            PortableRemoteObject.narrow(objref, PactsClientServicesHome.class);
+
+        return ((PactsClientServices) home.create());
     }
 
-    public boolean isMasterNode() {
-        return isMasterNode;  
+    /**
+     * This method processes and validates the parameters.
+     */
+    protected void processParams() {
+        super.processParams();
     }
 
-    public void stopSingleton() {
-        isMasterNode = false;  
-        timer.cancel();
-        timer = null;
-        logger.debug("StopSingleton");
+    /**
+     * This method shows usage of the ExpireOldAffidavitsAndPayments utility.
+     *
+     * @param msg The error message.
+     */
+    protected void setUsageError(String msg) {
+        sErrorMsg.setLength(0);
+        sErrorMsg.append(msg + "\n");
+        sErrorMsg.append("ExpireOldAffidavitsAndPayments:\n");
+        sErrorMsg.append("   No specific parameters required to run this utility.");
+        fatal_error();
     }
-
-
 }
