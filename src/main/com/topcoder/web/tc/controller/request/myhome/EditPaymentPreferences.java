997e93e2dccade7a83bfed6fb8906bd188406be8
Volodymyr Kachurovskyi <Volodymyr-K@users.noreply.github.com>
2011-11-18 08:28:38 +0000

    Changed PACTs to save payment method ID instead of payment method name for member payment preferences.

--- src/main/com/topcoder/web/tc/controller/request/myhome/EditPaymentPreferences.java
+++ src/main/com/topcoder/web/tc/controller/request/myhome/EditPaymentPreferences.java
@@ -7,7 +7,11 @@ import com.topcoder.shared.security.ClassResource;
 import com.topcoder.web.common.PermissionException;
 import com.topcoder.web.common.ShortHibernateProcessor;
 import com.topcoder.web.common.TCRequest;
+import com.topcoder.web.common.model.PaymentMethod;
 import com.topcoder.web.tc.controller.legacy.pacts.bean.DataInterfaceBean;
+import com.topcoder.web.tc.controller.legacy.pacts.common.*;
+import com.topcoder.shared.dataAccess.resultSet.*;
+import java.util.*;
 
 import java.rmi.RemoteException;
 
@@ -57,6 +61,8 @@ public class EditPaymentPreferences extends ShortHibernateProcessor {
             throw new PermissionException(getUser(), new ClassResource(this.getClass()));
         }
 
+        loadPaymentMethods();
+
         // Analyze the type of request.
         if ("POST".equalsIgnoreCase(getRequest().getMethod())) {
             // POST request is treated as submission of Payment Preferences form
@@ -78,9 +84,30 @@ public class EditPaymentPreferences extends ShortHibernateProcessor {
             if (currentPaymentAccrualAmount == 0) {
                 currentPaymentAccrualAmount = MINIMUM_PAYMENT_ACCRUAL_AMOUNT;
             }
-            String paymentMethod = dataBean.getUserPaymentMethod(getUser().getId());
-            forwardToEditPaymentPreferencesView(String.valueOf(currentPaymentAccrualAmount), paymentMethod);
+            Long paymentMethodId = dataBean.getUserPaymentMethod(getUser().getId());
+            forwardToEditPaymentPreferencesView(String.valueOf(currentPaymentAccrualAmount),
+			    paymentMethodId == null ? "" : paymentMethodId.toString());
         }
+    }
+
+    /**
+     * <p>Load all payment methods and saves them in the response.</p>
+     *
+     * @throws Exception if an error is encountered.
+     */
+    private void loadPaymentMethods() throws Exception {
+        DataInterfaceBean dataBean = new DataInterfaceBean();
+        ResultSetContainer rsc = (ResultSetContainer) dataBean.getPaymentMethods().get(PactsConstants.PAYMENT_METHOD_LIST);
+
+        List<PaymentMethod> paymentMethods = new ArrayList<PaymentMethod>();
+        for (int i=0;i<rsc.getRowCount();i++) {
+            ResultSetContainer.ResultSetRow rsr = rsc.getRow(i);
+            PaymentMethod paymentMethod = new PaymentMethod();
+            paymentMethod.setId(TCData.getTCLong(rsr, "payment_method_id", 0, true));
+            paymentMethod.setName(TCData.getTCString(rsr, "payment_method_desc", "method", true));
+            paymentMethods.add(paymentMethod);
+        }
+        getRequest().setAttribute("paymentMethods", paymentMethods);
     }
 
     /**
@@ -89,9 +116,9 @@ public class EditPaymentPreferences extends ShortHibernateProcessor {
      *
      * <p>Parses the new payment accrual amount value from respective request parameter and verifies that such a value
      * is provided and is numeric value greater than {@link com.topcoder.web.tc.Constants#MINIMUM_PAYMENT_ACCRUAL_AMOUNT}.
-	 * Also checks that the specified payment method is either PayPal, Wire or ACH.
-	 * If any of those validation rules is violated then appropriate error message is bound to incoming request.
-	 * Otherwise the new payment preferences for current user are saved to persistent data store.</p>
+     * Also checks that the specified payment method is either PayPal, Wire or ACH.
+     * If any of those validation rules is violated then appropriate error message is bound to incoming request.
+     * Otherwise the new payment preferences for current user are saved to persistent data store.</p>
      *
      * @throws RemoteException if an error is encountered while communicating to <code>PACTS Services EJB</code>
      *         remotely.
@@ -108,17 +135,18 @@ public class EditPaymentPreferences extends ShortHibernateProcessor {
             addError(ACCRUAL_AMOUNT_PARAM, "You must specify a payment accrual amount");
         } else if (isEmpty(paymentMethodValue)) {
             addError(PAYMENT_METHOD_PARAM, "You must specify a preferred payment method");
-        } else if (!paymentMethodValue.equals("PayPal") && !paymentMethodValue.equals("Wire") && !paymentMethodValue.equals("ACH")) {
-            addError(PAYMENT_METHOD_PARAM, "Incorrect payment method specified");
         } else {
             try {
                 int newAccrualAmount = Integer.parseInt(accrualAmountValue);
+                long paymentMethodId = Long.parseLong(paymentMethodValue);
                 if (newAccrualAmount < MINIMUM_PAYMENT_ACCRUAL_AMOUNT) {
                     addError(ACCRUAL_AMOUNT_PARAM,
                             "Payment accrual amount must be greater or equal to $" + MINIMUM_PAYMENT_ACCRUAL_AMOUNT);
+                } else if (paymentMethodId <= 0) {
+                    addError(PAYMENT_METHOD_PARAM, "Payment method is incorrect");
                 } else {
                     dataBean.saveUserAccrualThreshold(getUser().getId(), newAccrualAmount);
-                    dataBean.saveUserPaymentMethod(getUser().getId(), paymentMethodValue);
+                    dataBean.saveUserPaymentMethod(getUser().getId(), paymentMethodId);
                 }
             } catch (NumberFormatException e) {
                 addError(ACCRUAL_AMOUNT_PARAM, "Payment accrual amount must be integer number");
@@ -132,11 +160,11 @@ public class EditPaymentPreferences extends ShortHibernateProcessor {
      * <p>Binds specified payment preference values to request so they could be accessed by sub-sequent view.</p>
      *
      * @param paymentAccrualAmount a <code>String</code> providing the payment accrual amount to be displayed to user.
-     * @param paymentMethod a <code>String</code> providing the payment method to be displayed to user.
+     * @param paymentMethodId a <code>String</code> providing the ID of the the payment method to be displayed to user.
      */
-    private void forwardToEditPaymentPreferencesView(String paymentAccrualAmount, String paymentMethod) {
+    private void forwardToEditPaymentPreferencesView(String paymentAccrualAmount, String paymentMethodId) {
         setDefault(ACCRUAL_AMOUNT_PARAM, paymentAccrualAmount);
-        setDefault(PAYMENT_METHOD_PARAM, paymentMethod);
+        setDefault(PAYMENT_METHOD_PARAM, paymentMethodId);
         setIsNextPageInContext(true);
         setNextPage("/my_home/paymentPreferences.jsp");
     }
