f7505ebe0e8d588cc0083d5dbd7e8d62c42207f5
elkhawajah <elkhawajah@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2012-08-16 12:56:40 +0000

    Fix SQLs - replace earned_date with current-date

--- web_module/trunk/scripts/sql/five_hundred_forum_posts.sql
+++ web_module/trunk/scripts/sql/five_hundred_forum_posts.sql
@@ -1,6 +1,4 @@
-select tjm.userid::INTEGER AS user_id, extend(dbinfo("UTC_TO_DATETIME",tjm.creationdate/1000), year to fraction) AS earned_date
-from table(multiset(
-  select jm.userid, jm.creationdate,
-  (select count(*) from table(multiset(select userid, creationdate from jive:jivemessage)) as i where i.userid = jm.userid and i.creationdate < jm.creationdate) + 1 as row_num
-  from jive:jivemessage jm)) as tjm
-where tjm.row_num=500
+SELECT userid::INTEGER AS user_id, CURRENT AS earned_date
+FROM jive:jivemessage
+GROUP BY userid
+HAVING COUNT(*) >= 500
