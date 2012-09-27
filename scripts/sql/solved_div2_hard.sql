ce5368fb2657269a0454fd3a2b6520720fb9028a
Ahmad Alkhawaja <elkhawajah@users.noreply.github.com>
2012-09-27 16:31:31 +0000

    SQL Queries of Algorithm and Marathon Matches Badges/Achievements

---
+++ scripts/sql/solved_div2_hard.sql
@@ -0,0 +1,6 @@
+SELECT coder_id as user_id, CURRENT as earned_date
+FROM coder_level
+WHERE level_id = 3
+AND division_id = 2
+AND algo_rating_type_id = 1
+GROUP BY coder_id
---
+++ scripts/sql/won_srm_div2.sql
@@ -0,0 +1,5 @@
+select coder_id as user_id, CURRENT as date_earned
+  from division_winners
+ where division_id = 2
+   and round_type_id = 1
+group by coder_id
