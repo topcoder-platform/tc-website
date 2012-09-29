a2f33ebe4151b4d68c613d4efa616b0d53a74cb4
elkhawajah <elkhawajah@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2012-09-29 00:31:43 +0000

    date_earned should be earned_date

--- web_module/trunk/scripts/sql/won_srm_div1.sql
+++ web_module/trunk/scripts/sql/won_srm_div1.sql
@@ -1,4 +1,4 @@
-select coder_id as user_id, CURRENT as date_earned
+select coder_id as user_id, CURRENT as earned_date
   from division_winners
  where division_id = 1
    and round_type_id = 1
--- web_module/trunk/scripts/sql/won_srm_div2.sql
+++ web_module/trunk/scripts/sql/won_srm_div2.sql
@@ -1,4 +1,4 @@
-select coder_id as user_id, CURRENT as date_earned
+select coder_id as user_id, CURRENT as earned_date
   from division_winners
  where division_id = 2
    and round_type_id = 1
