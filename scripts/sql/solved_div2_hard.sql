22657fda639d66eff626fa8298de4324a1bb576a
Ahmad Alkhawaja <elkhawajah@users.noreply.github.com>
2012-10-01 12:11:00 +0000

    Fixed solved div1/2 hard sql query

--- scripts/sql/solved_div2_hard.sql
+++ scripts/sql/solved_div2_hard.sql
@@ -1,6 +1,7 @@
 SELECT coder_id as user_id, CURRENT as earned_date
 FROM coder_level
 WHERE level_id = 3
+AND problems_correct > 0
 AND division_id = 2
 AND algo_rating_type_id = 1
 GROUP BY coder_id
