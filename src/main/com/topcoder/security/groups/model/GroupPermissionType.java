a0d959503caa38f0957256f88f1a2dc1f93b6c19
xjtufreeman <xjtufreeman@users.noreply.github.com>
2012-11-08 03:14:05 +0000

    TCCC-4704:fixed

--- src/main/com/topcoder/security/groups/model/GroupPermissionType.java
+++ src/main/com/topcoder/security/groups/model/GroupPermissionType.java
@@ -7,12 +7,32 @@ package com.topcoder.security.groups.model;
  * <p>
  * This enum represents the types of group permissions.
  * </p>
+ *
+ * <p>
+ * Version 1.0 (Topcoder Security Groups Backend Model and Authorization Assembly v1.0 ) change notes:
+ * <ol>
+ *   <li>Added this enum<li>
+ * </ol>
+ * </p>
+ *
+ * <p>
+ * Version 1.1 (https://apps.topcoder.com/bugs/browse/TCCC-4704) change notes:
+ * <ol>
+ *   <li>Added the REPORT permission<li>
+ * </ol>
+ * </p>
  * 
- * @author leo_lol
- * @version 1.0 (Topcoder Security Groups Backend Model and Authorization Assembly v1.0 )
- * @since 1.0
+ * @author leo_lol, xjtufreeman
+ * @version 1.1
+ * @since 1.1
  */
 public enum GroupPermissionType {
+    /**
+     * <p>
+     * Represents the group permission to report.
+     * </p>
+     */
+    REPORT,
     /**
      * <p>
      * Represents the group permission to read.
