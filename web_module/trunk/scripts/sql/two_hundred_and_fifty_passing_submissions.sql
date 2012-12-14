594a6949a957b555d9dc95b29292427c0f882e40
elkhawajah <elkhawajah@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2012-12-14 13:13:25 +0000

    Fix bug in submissions badges - remove joining with review

--- web_module/trunk/scripts/sql/two_hundred_and_fifty_passing_submissions.sql
+++ web_module/trunk/scripts/sql/two_hundred_and_fifty_passing_submissions.sql
@@ -1,12 +1,11 @@
 SELECT m.user_id, CURRENT AS earned_date
 FROM table(multiset(
-SELECT ri.value::INTEGER AS user_id, rev.create_date as earned_date
+SELECT ri.value::INTEGER AS user_id, CURRENT as earned_date
 FROM resource_info AS ri
 	JOIN upload AS u ON ri.resource_id = u.resource_id
 	JOIN submission AS s ON s.upload_id = u.upload_id
 	JOIN project AS p ON u.project_id = p.project_id
 	JOIN project_category_lu AS pc ON p.project_category_id = pc.project_category_id
-    JOIN review AS rev ON rev.submission_id = s.submission_id
 WHERE s.submission_status_id IN (1, 4)
 	AND ri.resource_info_type_id = 1
 	AND u.upload_type_id = 1
