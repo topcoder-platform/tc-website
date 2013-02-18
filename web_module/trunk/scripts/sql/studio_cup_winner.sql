3cce00e95cd4239e8bffa544888335adbf74c1a4
elkhawajah <elkhawajah@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2013-02-18 12:39:23 +0000

    Fix bug in placements query, digital run queries - that are part of badges.

--- web_module/trunk/scripts/sql/studio_cup_winner.sql
+++ web_module/trunk/scripts/sql/studio_cup_winner.sql
@@ -3,7 +3,8 @@ select distinct tcr.user_id, min(t.track_end_date) AS earned_date
      , track_contest_results tcr
 	 , track t
  where tc.track_contest_id = tcr.track_contest_id
-   and tc.track_contest_type_id = 2 
+   and tc.track_contest_type_id = 1
+   and tcr.track_contest_placement = 1
    and tc.track_id = t.track_id
    and t.track_type_id = 3
    and t.track_status_id != 3
