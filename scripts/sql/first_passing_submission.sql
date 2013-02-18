295dbb117374616c1561ed1ee27a6c03acc3d29e
Ahmad Alkhawaja <elkhawajah@users.noreply.github.com>
2013-02-18 13:05:09 +0000

    Fix bug in passing submission queries - related to badges

--- scripts/sql/first_passing_submission.sql
+++ scripts/sql/first_passing_submission.sql
@@ -11,7 +11,7 @@ WHERE s.submission_status_id IN (1, 4)
 	AND u.upload_type_id = 1
 	AND s.submission_type_id in (1, 3)
 	AND pc.project_type_id in (1, 2)
-	AND p.project_status_id in (1, 7)
+	AND p.project_status_id in (1, 7, 8, 9)
 	AND p.project_category_id != 27
 	AND NOT EXISTS (SELECT 'has_eligibility_constraints' FROM contest_eligibility ce
 		WHERE ce.contest_id = p.project_id)
