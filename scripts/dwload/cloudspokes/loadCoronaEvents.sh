198a9d538cdaec308d3f2b4d667c203fe3bfec3e
Shannon Ma <mashannon168@users.noreply.github.com>
2013-10-02 03:30:20 +0000

    corona.

--- scripts/dwload/cloudspokes/loadCoronaEvents.sh
+++ scripts/dwload/cloudspokes/loadCoronaEvents.sh
@@ -16,6 +16,13 @@ CP=$CP:../../../lib/jars/commons-logging.jar
 CP=$CP:../../../lib/jars/commons-codec-1.3.jar
 CP=$CP:../../../lib/jars/jackson-core-asl.jar
 CP=$CP:../../../lib/jars/jackson-mapper-asl.jar
+CP=$CP:../../../lib/jars/commons-lang-2.4.jar
+CP=$CP:../../../lib/jars/ezmorph-1.0.6.jar
+CP=$CP:../../../lib/jars/commons-collections-3.2.jar
+CP=$CP:../../../lib/jars/commons-beanutils-1.8.0.jar
+CP=$CP:../../../lib/jars/commons-io-2.0.1.jar
+
+
 
 nohup java  -cp $CP com.topcoder.shared.util.sql.DBUtilityLauncher com.topcoder.utilities.dwload.cloudspokes.CoronaEventLoadUtility -xmlfile loadCoronaEvents.xml$@ > ./loadCoronaEvents.out 2>&1 &
 tail -f loadCoronaEvents.out
