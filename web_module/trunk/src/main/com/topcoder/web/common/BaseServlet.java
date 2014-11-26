10380c7a76f31810f86d2eaab97600e6dda9618e
kohata <kohata@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2014-11-26 03:37:07 +0000

    Fix for I-137967

--- web_module/trunk/src/main/com/topcoder/web/common/BaseServlet.java
+++ web_module/trunk/src/main/com/topcoder/web/common/BaseServlet.java
@@ -240,7 +240,15 @@ public abstract class BaseServlet extends HttpServlet {
                 loginfo.append("[* ");
                 loginfo.append(info.getHandle());
                 loginfo.append(" * ");
-                loginfo.append(request.getRemoteAddr());
+
+		//I-137967(https://appirio.my.salesforce.com/a3v50000000D2Lt)
+		//loginfo.append(request.getRemoteAddr());
+		String remoteAddr = request.getHeader("X-Forwarded-For");
+		if(remoteAddr == null || remoteAddr.trim().length()==0) {
+			remoteAddr = request.getRemoteAddr();
+		}
+		loginfo.append(remoteAddr);
+
                 loginfo.append(" * ");
                 loginfo.append(request.getMethod());
                 loginfo.append(" ");
