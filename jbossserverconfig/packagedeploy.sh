e39fe9c17d9c1a38fa354be442e0ce748c41c4c5
Gunasekar-K <Gunasekar.Kesavalu@wipro.com>
2017-11-06 19:05:25 +0530

    Update packagedeploy.sh
    [ci skip]

--- jbossserverconfig/packagedeploy.sh
+++ jbossserverconfig/packagedeploy.sh
@@ -1,4 +1,7 @@
+mv /data/tc-website/run.conf /home/web/jboss-4.0.4.GA/bin/
+mv /data/tc-website/topcoder.jar /data/tc-website/package/
 cd /data/tc-website/package
 jar xvf topcoder.jar
 ant -f build_tc.xml expand
-
+cd ..
+rm -rf /data/tc-website/package
