4b86b51fb6793dbb8a356f862e89218ac46205d4
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2021-06-01 17:44:26 +0530

    integrating deploy script

--- deployscripts/remote-cmd-backup-app.sh
+++ deployscripts/remote-cmd-backup-app.sh
@@ -7,7 +7,7 @@ TARGET_JAR1=${BACKUP_DIR}/tc_cache.jar
 BACKUP_JAR1=${TARGET_JAR1}.bak
 
 # app
-echo [Taking backup of existing direct.jar]
+echo [Taking backup of existing topcoder.jar]
 if [ -e $BACKUP_JAR ]; then
   rm -rf $BACKUP_JAR
 fi
@@ -15,7 +15,7 @@ if [ -e $TARGET_JAR ]; then
   mv $TARGET_JAR $BACKUP_JAR
 fi
 
-echo [Taking backup of existing direct.jar]
+echo [Taking backup of existing tc_cache.jar]
 if [ -e $BACKUP_JAR1 ]; then
   rm -rf $BACKUP_JAR1
 fi
