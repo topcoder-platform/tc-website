3ffdf90839660a098bcd41fc29c4057f54ebedab
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2021-06-11 14:34:44 +0530

    deployment testing

--- deployscripts/remote-cmd-backup-app.sh
+++ deployscripts/remote-cmd-backup-app.sh
@@ -3,8 +3,8 @@
 BACKUP_DIR=/home/web/web_backup
 TARGET_JAR=${BACKUP_DIR}/topcoder.jar
 BACKUP_JAR=${TARGET_JAR}.bak
-TARGET_JAR1=${BACKUP_DIR}/tc_cache.jar
-BACKUP_JAR1=${TARGET_JAR1}.bak
+# TARGET_JAR1=${BACKUP_DIR}/tc_cache.jar
+# BACKUP_JAR1=${TARGET_JAR1}.bak
 
 # app
 echo [Taking backup of existing topcoder.jar]
@@ -15,10 +15,10 @@ if [ -e $TARGET_JAR ]; then
   mv $TARGET_JAR $BACKUP_JAR
 fi
 
-echo [Taking backup of existing tc_cache.jar]
-if [ -e $BACKUP_JAR1 ]; then
-  rm -rf $BACKUP_JAR1
-fi
-if [ -e $TARGET_JAR1 ]; then
-  mv $TARGET_JAR1 $BACKUP_JAR1
-fi
+# echo [Taking backup of existing tc_cache.jar]
+# if [ -e $BACKUP_JAR1 ]; then
+#   rm -rf $BACKUP_JAR1
+# fi
+# if [ -e $TARGET_JAR1 ]; then
+#   mv $TARGET_JAR1 $BACKUP_JAR1
+# fi
--- deployscripts/remote-cmd-expand-app.sh
+++ deployscripts/remote-cmd-expand-app.sh
@@ -6,16 +6,16 @@ WORK_DIR=/home/web/web
 BACKUP_DIR=/home/web/web_backup
 
 BACKUP_JAR=${WORK_DIR}/topcoder.jar.bak
-BACKUP_JAR1=${WORK_DIR}/tc_cache.jar.bak
+# BACKUP_JAR1=${WORK_DIR}/tc_cache.jar.bak
 
 if [ ! -e ${BACKUP_DIR}/topcoder.jar ]; then
   echo "[FATAL] ${BACKUP_DIR}/topcoder.jar not found.."
   exit 1;
 fi
-if [ ! -e ${BACKUP_DIR}/tc_cache.jar ]; then
-  echo "[FATAL] ${BACKUP_DIR}/tc_cache.jar not found.."
-  exit 1;
-fi
+# if [ ! -e ${BACKUP_DIR}/tc_cache.jar ]; then
+#   echo "[FATAL] ${BACKUP_DIR}/tc_cache.jar not found.."
+#   exit 1;
+# fi
 
 #
 # Backup and deploy JAR
@@ -26,23 +26,23 @@ if [ -e $BACKUP_JAR ]; then
 fi
 
 
-echo "Making backup ${WORK_DIR}/tc_cache.jar --> $BACKUP_JAR"
-if [ -e $BACKUP_JAR1 ]; then
-  rm -rf $BACKUP_JAR1
-fi
+# echo "Making backup ${WORK_DIR}/tc_cache.jar --> $BACKUP_JAR"
+# if [ -e $BACKUP_JAR1 ]; then
+#   rm -rf $BACKUP_JAR1
+# fi
 
 echo "Copying topcoder.jar"
 cp -rf ${BACKUP_DIR}/topcoder.jar $WORK_DIR/
-cp -rf ${BACKUP_DIR}/tc_cache.jar $WORK_DIR/
-cp -rf ${BACKUP_DIR}/lib.zip $WORK_DIR/
+# cp -rf ${BACKUP_DIR}/tc_cache.jar $WORK_DIR/
+# cp -rf ${BACKUP_DIR}/lib.zip $WORK_DIR/
 #cp -rf ${BACKUP_DIR}/resources.zip $WORK_DIR/
 
 pushd $WORK_DIR
-unzip -o lib.zip
-unzip -o resources.zip
+# unzip -o lib.zip
+# unzip -o resources.zip
 jar xvf topcoder.jar
-mkdir -p lib/jars/tcs/tc_cache/1.0.0/
-cp -rf tc_cache.jar lib/jars/tcs/tc_cache/1.0.0/
+# mkdir -p lib/jars/tcs/tc_cache/1.0.0/
+# cp -rf tc_cache.jar lib/jars/tcs/tc_cache/1.0.0/
 
 popd
 
